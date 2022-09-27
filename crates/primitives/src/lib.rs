eea99a12d20019096845c71dbd20fd490a7710fe
Matthias Seitz <matthias.seitz@outlook.de>
2022-09-27 19:19:01 +0200

    feat: add core crate (#3)
    * feat: add core crate
    
    * rename primitives
    
    * Update crates/primitives/Cargo.toml
    
    -.-
    
    Co-authored-by: rakita <rakita@users.noreply.github.com>
    
    Co-authored-by: rakita <rakita@users.noreply.github.com>

---
+++ crates/primitives/src/lib.rs
@@ -0,0 +1,10 @@
+#![warn(missing_debug_implementations, missing_docs, unreachable_pub)]
+#![deny(unused_must_use, rust_2018_idioms)]
+#![doc(test(
+    no_crate_inject,
+    attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
+))]
+
+//! Commonly used types in reth.
+
+pub use ethers_core::types::*;
