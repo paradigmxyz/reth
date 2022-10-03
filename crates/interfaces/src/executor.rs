b5dd0c67681c92bb9b2c16a1afdd29383addb638
rakita <dragan0rakita@gmail.com>
2022-10-03 17:15:43 +0200

    wip

--- crates/interfaces/src/executor.rs
+++ crates/interfaces/src/executor.rs
@@ -1,11 +1,11 @@
-
+use reth_primitives::Block;
 
 pub trait BlockExecutor {
-    /*pub fn execute(&self, Block) {
+    pub fn execute(&self, block: Block) -> Error {
 
-    }*/
+    }
 }
 
 pub enum Error {
-
+    VerificationFailed,
 }
