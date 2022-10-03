3423a7b59978f2e48c739a41d3df7a62dad5e669
Oliver Nordbjerg <hi@notbjerg.me>
2022-10-03 17:25:40 +0200

    feat: block number in validation error

--- crates/stages/src/lib.rs
+++ crates/stages/src/lib.rs
@@ -60,7 +60,7 @@ pub enum StageError {
     ///
     /// TODO: This depends on the consensus engine and should include the validation failure reason
     #[error("Stage encountered a validation error.")]
-    Validation,
+    Validation { block: U64 },
     /// The stage encountered an internal error.
     #[error(transparent)]
     Internal(Box<dyn std::error::Error + Send + Sync>),
