f5dc9b4a89e1194dba27b84bf8e3818971ab8fb1
Oliver Nordbjerg <hi@notbjerg.me>
2022-10-09 11:49:44 +0200

    test: add tests for `opt::max` and `opt::min`

--- crates/stages/src/util.rs
+++ crates/stages/src/util.rs
@@ -36,4 +36,23 @@ pub(crate) mod opt {
             Ok(())
         }
     }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn opt_max() {
+            assert_eq!(max(None, 5), Some(5));
+            assert_eq!(max(Some(1), 5), Some(5));
+            assert_eq!(max(Some(10), 5), Some(10));
+        }
+
+        #[test]
+        fn opt_min() {
+            assert_eq!(min(None, 5), Some(5));
+            assert_eq!(min(Some(1), 5), Some(1));
+            assert_eq!(min(Some(10), 5), Some(5));
+        }
+    }
 }
