a759201b40217af147e069498a7d7db869439510
Bjerg <onbjerg@users.noreply.github.com>
2022-10-10 15:52:23 +0200

    refactor(stages): split `Pipeline::run` (#30)

---
+++ crates/stages/src/pipeline/ctrl.rs
@@ -0,0 +1,14 @@
+use reth_primitives::BlockNumber;
+
+/// Determines the control flow during pipeline execution.
+pub(crate) enum ControlFlow {
+    /// An unwind was requested and must be performed before continuing.
+    Unwind {
+        /// The block to unwind to.
+        target: BlockNumber,
+        /// The block that caused the unwind.
+        bad_block: Option<BlockNumber>,
+    },
+    /// The pipeline is allowed to continue executing stages.
+    Continue,
+}
