ae5935e6b2692cb06118eebe4fe03094592cb9f3
rakita <rakita@users.noreply.github.com>
2022-10-12 17:43:01 +0200

    feat: reth binary and parsing of eth chain tests (#38)
    * Scaffolding for reth test binary. wip for blockchain tests
    
    * wip models for chain json tests
    
    * reth binary and chain test
    
    * fmt,clippy
    
    * Update bin/reth/src/lib.rs
    
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>
    
    * Update bin/reth/src/lib.rs
    
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>
    
    * Update bin/reth/src/test_eth_chain/mod.rs
    
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>
    
    * Update bin/reth/src/test_eth_chain/mod.rs
    
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>
    
    * Update bin/reth/src/test_eth_chain/mod.rs
    
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>
    
    * Update bin/reth/src/test_eth_chain/models.rs
    
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>
    
    * Update bin/reth/src/test_eth_chain/models.rs
    
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>
    
    * Update bin/reth/src/test_eth_chain/mod.rs
    
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>
    
    * Update bin/reth/src/test_eth_chain/runner.rs
    
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>
    
    * Move JsonU256 to primitives
    
    * fmt
    
    * Use eyre
    
    * nits
    
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>

---
+++ crates/primitives/src/jsonu256.rs
@@ -0,0 +1,94 @@
+use crate::U256;
+use serde::{
+    de::{Error, Visitor},
+    Deserialize, Deserializer, Serialize, Serializer,
+};
+use std::{fmt, str::FromStr};
+
+/// Wrapper around primitive U256 type to handle edge cases of json parser
+#[derive(Default, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
+pub struct JsonU256(pub U256);
+
+impl Serialize for JsonU256 {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        self.0.to_string().serialize(serializer)
+    }
+}
+
+impl<'a> Deserialize<'a> for JsonU256 {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'a>,
+    {
+        deserializer.deserialize_any(JsonU256Visitor)
+    }
+}
+
+struct JsonU256Visitor;
+
+impl<'a> Visitor<'a> for JsonU256Visitor {
+    type Value = JsonU256;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(formatter, "a hex encoding or decimal number")
+    }
+
+    fn visit_u64<E>(self, value: u64) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        Ok(JsonU256(U256::from(value)))
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        let value = match value.len() {
+            0 => U256::from(0),
+            2 if value.starts_with("0x") => U256::zero(),
+            _ if value.starts_with("0x") => U256::from_str(&value[2..]).map_err(|e| {
+                Error::custom(format!("Parsin JsonU256 as hex failed {}: {}", value, e))
+            })?,
+            _ => U256::from_dec_str(value).map_err(|e| {
+                Error::custom(format!("Parsin JsonU256 as decimal failed {}: {:?}", value, e))
+            })?,
+        };
+
+        Ok(JsonU256(value))
+    }
+
+    fn visit_string<E>(self, value: String) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        self.visit_str(value.as_ref())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::JsonU256;
+    use crate::U256;
+    use serde_json;
+
+    #[test]
+    fn jsonu256_deserialize() {
+        let deserialized: Vec<JsonU256> =
+            serde_json::from_str(r#"["","0", "0x","10",10,"0x10"]"#).unwrap();
+        assert_eq!(
+            deserialized,
+            vec![
+                JsonU256(U256::from(0)),
+                JsonU256(U256::from(0)),
+                JsonU256(U256::from(0)),
+                JsonU256(U256::from(10)),
+                JsonU256(U256::from(10)),
+                JsonU256(U256::from(16)),
+            ]
+        );
+    }
+}
