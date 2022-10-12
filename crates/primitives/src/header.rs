2534e1c4a6407aa15804f1d92570872e47e71efc
joshieDo <ranriver@protonmail.com>
2022-10-12 09:29:19 +0000

    set header filed to bytes Bytes

--- crates/primitives/src/header.rs
+++ crates/primitives/src/header.rs
@@ -48,7 +48,7 @@ pub struct Header {
     pub timestamp: u64,
     /// An arbitrary byte array containing data relevant to this block. This must be 32 bytes or
     /// fewer; formally Hx.
-    pub extra_data: Vec<u8>,
+    pub extra_data: bytes::Bytes,
     /// A 256-bit hash which, combined with the
     /// nonce, proves that a sufficient amount of computation has been carried out on this block;
     /// formally Hm.
