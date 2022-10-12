477ec952828d6003daceae1c26524280ad60bcba
rakita <rakita@users.noreply.github.com>
2022-10-12 14:58:46 +0200

    Update bin/reth/src/test_eth_chain/runner.rs
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>

--- bin/reth/src/test_eth_chain/runner.rs
+++ bin/reth/src/test_eth_chain/runner.rs
@@ -2,7 +2,7 @@ use super::models::Test;
 use anyhow::Result;
 use std::path::Path;
 
-/// Run one json ethereum blockchain test
+/// Run one JSON-encoded Ethereum blockchain test at the specified path.
 pub async fn run_test(path: &Path) -> Result<()> {
     let json_file = std::fs::read(path)?;
     let suits: Test = serde_json::from_reader(&*json_file)?;
