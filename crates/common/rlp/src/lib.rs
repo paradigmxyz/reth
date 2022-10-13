dff05d8418e1c2c216e53a3c312a3f76242b7ded
rakita <dragan0rakita@gmail.com>
2022-10-13 20:39:38 +0200

    include apache licenced fastrlp as reth-rlp

---
+++ crates/common/rlp/src/lib.rs
@@ -0,0 +1,22 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+mod decode;
+mod encode;
+mod types;
+
+pub use bytes::BufMut;
+
+pub use decode::{Decodable, DecodeError, Rlp};
+pub use encode::{
+    const_add, encode_fixed_size, encode_list, length_of_length, list_length, Encodable,
+    MaxEncodedLen, MaxEncodedLenAssoc,
+};
+pub use types::*;
+
+#[cfg(feature = "derive")]
+pub use reth_rlp_derive::{
+    RlpDecodable, RlpDecodableWrapper, RlpEncodable, RlpEncodableWrapper, RlpMaxEncodedLen,
+};
