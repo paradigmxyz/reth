18b4345ff2b348e0ca1466437ecb15ca6ede1599
joshieDo <ranriver@protonmail.com>
2022-10-13 11:04:17 +0000

    cleanup

--- crates/primitives/codecs/src/lib.rs
+++ crates/primitives/codecs/src/lib.rs
@@ -4,7 +4,6 @@ use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_attribute]
 #[rustfmt::skip]
-#[clippy::skip]
 #[allow(unreachable_code)]
 pub fn main_codec(args: TokenStream, input: TokenStream) -> TokenStream {
     #[cfg(feature = "scale")]
@@ -22,52 +21,43 @@ pub fn use_scale(_args: TokenStream, input: TokenStream) -> TokenStream {
     let mut ast = parse_macro_input!(input as DeriveInput);
     let compactable_types = ["u8", "u16", "u32", "i32", "i64", "u64", "f32", "f64"];
 
-    match &mut ast.data {
-        syn::Data::Struct(ref mut data) => {
-            match &mut data.fields {
-                syn::Fields::Named(fields) => {
-                    for field in fields.named.iter_mut() {
-                        if let syn::Type::Path(ref path) = field.ty {
-                            if !path.path.segments.is_empty() {
-                                let _type = format!("{}", path.path.segments[0].ident);
-                                if compactable_types.contains(&_type.as_str()) {
-                                    field.attrs.push(syn::parse_quote! {
-                                        #[codec(compact)]
-                                    });
-                                }
-                            }
+    if let syn::Data::Struct(ref mut data) = &mut ast.data {
+        if let syn::Fields::Named(fields) = &mut data.fields {
+            for field in fields.named.iter_mut() {
+                if let syn::Type::Path(ref path) = field.ty {
+                    if !path.path.segments.is_empty() {
+                        let _type = format!("{}", path.path.segments[0].ident);
+                        if compactable_types.contains(&_type.as_str()) {
+                            field.attrs.push(syn::parse_quote! {
+                                #[codec(compact)]
+                            });
                         }
                     }
                 }
-                _ => (),
             }
-            return quote! {
-                #[derive(parity_scale_codec::Encode, parity_scale_codec::Decode)]
-                #ast
-            }
-            .into()
         }
-        _ => unreachable!(),
     }
+
+    quote! {
+        #[derive(parity_scale_codec::Encode, parity_scale_codec::Decode)]
+        #ast
+    }
+    .into()
 }
 
 #[proc_macro_attribute]
 pub fn use_postcard(_args: TokenStream, input: TokenStream) -> TokenStream {
-    let mut ast = parse_macro_input!(input as DeriveInput);
-    match &mut ast.data {
-        syn::Data::Struct(_) => {
-            return quote! {
-                #[derive(serde::Serialize, serde::Deserialize)]
-                #ast
-            }
-            .into()
-        }
-        _ => unreachable!(),
+    let ast = parse_macro_input!(input as DeriveInput);
+
+    quote! {
+        #[derive(serde::Serialize, serde::Deserialize)]
+        #ast
     }
+    .into()
 }
 
 #[proc_macro_attribute]
 pub fn no_codec(_args: TokenStream, input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    return quote! { #ast }.into()
+    quote! { #ast }.into()
 }
