38da95a47ea984f779f3a7f50c07c925e797a30d
Georgios Konstantopoulos <me@gakonst.com>
2022-10-15 22:46:21 -0700

    wip: test eth connection

--- crates/net/eth-wire/src/stream.rs
+++ crates/net/eth-wire/src/stream.rs
@@ -86,3 +86,83 @@ where
         Pin::new(&mut self.get_mut().stream).poll_close(cx).map_err(Into::into)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use bytes::Bytes;
+    use futures::{SinkExt, StreamExt};
+    use reth_ecies::{stream::ECIESStream, util::pk2id};
+    use secp256k1::{rand, SecretKey, SECP256K1};
+    use tokio::net::{TcpListener, TcpStream};
+
+    use crate::types::broadcast::BlockHashNumber;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn can_write_and_read_cleartext() {
+        let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+        let test_msg = EthMessage::NewBlockHashes(
+            vec![
+                BlockHashNumber { hash: reth_primitives::H256::random(), number: 5 },
+                BlockHashNumber { hash: reth_primitives::H256::random(), number: 6 },
+            ]
+            .into(),
+        );
+
+        let test_msg_clone = test_msg.clone();
+        let handle = tokio::spawn(async move {
+            // roughly based off of the design of tokio::net::TcpListener
+            let (incoming, _) = listener.accept().await.unwrap();
+            let mut stream = EthStream { stream: incoming };
+
+            // use the stream to get the next messagse
+            let message = stream.next().await.unwrap().unwrap();
+            // assert_eq!(message, test_msg_clone);
+        });
+
+        let outgoing = TcpStream::connect("127.0.0.1:8080").await.unwrap();
+        let mut client_stream = EthStream { stream: outgoing };
+        client_stream.send(test_msg).await.unwrap();
+
+        // make sure the server receives the message and asserts before ending the test
+        handle.await.unwrap();
+    }
+
+    // #[tokio::test]
+    // async fn can_write_and_read() {
+    //     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    //     let server_key = SecretKey::new(&mut rand::thread_rng());
+    //     let test_msg = EthMessage::NewBlockHashes(
+    //         vec![
+    //             BlockHashNumber { hash: reth_primitives::H256::random(), number: 5 },
+    //             BlockHashNumber { hash: reth_primitives::H256::random(), number: 6 },
+    //         ]
+    //         .into(),
+    //     );
+
+    //     let test_msg_clone = test_msg.clone();
+    //     let handle = tokio::spawn(async move {
+    //         // roughly based off of the design of tokio::net::TcpListener
+    //         let (incoming, _) = listener.accept().await.unwrap();
+    //         let stream = ECIESStream::incoming(incoming, server_key).await.unwrap();
+    //         let mut stream = EthStream { stream };
+
+    //         // use the stream to get the next messagse
+    //         let message = stream.next().await.unwrap().unwrap();
+    //         // assert_eq!(message, test_msg_clone);
+    //     });
+
+    //     // create the server pubkey
+    //     let server_id = pk2id(&server_key.public_key(SECP256K1));
+
+    //     let client_key = SecretKey::new(&mut rand::thread_rng());
+    //     let outgoing = TcpStream::connect("127.0.0.1:8080").await.unwrap();
+    //     let stream = ECIESStream::connect(outgoing, client_key, server_id).await.unwrap();
+    //     let mut client_stream = EthStream { stream };
+    //     client_stream.send(test_msg).await.unwrap();
+
+    //     // make sure the server receives the message and asserts before ending the test
+    //     handle.await.unwrap();
+    // }
+}
