f9e805550b7a43f553f6a630b088f1382f8a5f52
joshieDo <ranriver@protonmail.com>
2022-10-18 11:31:45 +0000

    don't sum results

--- crates/db/benches/encoding_crit.rs
+++ crates/db/benches/encoding_crit.rs
@@ -3,16 +3,20 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 macro_rules! impl_benchmark {
     ($name:tt) => {
         pub fn criterion_benchmark(c: &mut Criterion) {
-            let mut total_size = 0;
+            let mut size = 0;
             c.bench_function(stringify!($name), |b| {
                 b.iter(|| {
-                    total_size += reth_db::kv::codecs::fuzz::Header::encode_and_decode(black_box(
-                        reth_primitives::Header::default(),
-                    ))
+                    let encoded_size = reth_db::kv::codecs::fuzz::Header::encode_and_decode(
+                        black_box(reth_primitives::Header::default()),
+                    )
                     .0;
+
+                    if size == 0 {
+                        size = encoded_size;
+                    }
                 })
             });
-            println!("Size (bytes): `{total_size}`");
+            println!("Size (bytes): `{size}`");
         }
 
         criterion_group!(benches, criterion_benchmark);
