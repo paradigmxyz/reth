630baf5d703173e86f780b8a2f137a7131f416c5
joshieDo <93316087+joshieDo@users.noreply.github.com>
2022-10-20 04:08:07 +0800

    feat(db): fuzzing & benchmark (#86)
    * fuzz with test-fuzz
    
    * move fuzzing to db/codecs
    
    * add criterion & iai
    
    * print encoded size sum on criterion benchmark
    
    * fix BlockNumHash encode
    
    * add gh action for benchmarks
    
    * don't sum results
    
    * test ci values
    
    * Revert "test ci values"
    
    This reverts commit cc47fd80538b2c0073592f824c2693c927021f8f.
    
    * specify criterion version
    
    * add docs
    
    * remove benchmark job

---
+++ crates/db/benches/encoding_iai.rs
@@ -0,0 +1,20 @@
+use iai::{black_box, main};
+
+/// Benchmarks the encoding and decoding of `Header` using iai.
+macro_rules! impl_iai_encoding_benchmark {
+    ($name:tt) => {
+        fn $name() {
+            reth_db::kv::codecs::fuzz::Header::encode_and_decode(black_box(
+                reth_primitives::Header::default(),
+            ));
+        }
+
+        main!($name);
+    };
+}
+
+#[cfg(not(feature = "bench-postcard"))]
+impl_iai_encoding_benchmark!(scale);
+
+#[cfg(feature = "bench-postcard")]
+impl_iai_encoding_benchmark!(postcard);
