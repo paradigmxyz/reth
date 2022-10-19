e4f9244c30936415c028ca479eda7e01e2fbba8e
Matthias Seitz <matthias.seitz@outlook.de>
2022-10-19 22:12:53 +0200

    feat(rpc): more rpc scaffolding (#99)
    * feat(rpc): more rpc scaffolding
    
    * use Into<String>
    
    * rm unused error

--- crates/interfaces/src/provider/storage.rs
+++ crates/interfaces/src/provider/storage.rs
@@ -2,7 +2,7 @@ use crate::Result;
 use reth_primitives::{rpc::BlockId, Address, H256, U256};
 
 /// Provides access to storage data
-pub trait StorageProvider {
+pub trait StorageProvider: Send + Sync + 'static {
     /// Returns the value from a storage position at a given address and `BlockId`
     fn storage_at(&self, address: Address, index: U256, at: BlockId) -> Result<Option<H256>>;
 }
