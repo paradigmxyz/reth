664b6d3048473fcb15cb1773d03ba9f97169a2a8
Matthias Seitz <matthias.seitz@outlook.de>
2022-10-20 17:34:04 +0200

    wip: feat(txpool): add on block update  (#69)
    * feat(txpool): add on new block handler
    
    * feat(txpool): work on update routine
    
    * feat(txpool): start update process
    
    * feat: implement update loop

---
+++ crates/transaction-pool/src/pool/update.rs
@@ -0,0 +1,32 @@
+//! Support types for updating the pool.
+use crate::{
+    identifier::TransactionId,
+    pool::{state::SubPool, txpool::PoolInternalTransaction},
+    PoolTransaction,
+};
+use reth_primitives::TxHash;
+use std::ops::{Deref, DerefMut};
+
+/// A change of the transaction's location
+///
+/// NOTE: this guarantees that `current` and `destination` differ.
+#[derive(Debug)]
+pub(crate) struct PoolUpdate {
+    /// Internal tx id.
+    pub(crate) id: TransactionId,
+    /// Hash of the transaction.
+    pub(crate) hash: TxHash,
+    /// Where the transaction is currently held.
+    pub(crate) current: SubPool,
+    /// Where to move the transaction to.
+    pub(crate) destination: Destination,
+}
+
+/// Where to move an existing transaction.
+#[derive(Debug)]
+pub(crate) enum Destination {
+    /// Discard the transaction.
+    Discard,
+    /// Move transaction to pool
+    Pool(SubPool),
+}
