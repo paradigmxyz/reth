570cfff79a41c69385393dccc984fa73c3099440
joshieDo <ranriver@protonmail.com>
2022-10-21 09:52:42 +0000

    add elias

---
+++ crates/interfaces/src/db/models/integer_list.rs
@@ -0,0 +1,22 @@
+//! Block related models and types.
+
+use crate::db::{
+    error::Error,
+    table::{Decode, Encode},
+};
+use bytes::Bytes;
+use reth_primitives::IntegerList;
+
+impl Encode for IntegerList {
+    type Encoded = Vec<u8>;
+
+    fn encode(self) -> Self::Encoded {
+        self.to_bytes()
+    }
+}
+
+impl Decode for IntegerList {
+    fn decode<B: Into<Bytes>>(value: B) -> Result<Self, Error> {
+        IntegerList::from_bytes(&value.into()).map_err(|e| Error::Decode(eyre::eyre!("{e}")))
+    }
+}
