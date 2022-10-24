fcb81f164982cd809ff66d6b7ce47843c1d11904
Georgios Konstantopoulos <me@gakonst.com>
2022-10-24 04:38:17 -0700

    headers(part 3) feat: implement Linear downloader (#119)
    * feat: add headers downloaders crate
    
    * feat: more scaffolding
    
    * interfaces: generalize retryable erros
    
    * feat: implement linear downloader
    
    * fix linear downloader tests & add builder
    
    * extend & reverse
    
    * feat: linear downloader generics behind arc and reversed return order (#120)
    
    * put client & consensus behind arc and return headers in rev
    
    * cleanup
    
    Co-authored-by: Roman Krasiuk <rokrassyuk@gmail.com>

--- crates/interfaces/src/consensus.rs
+++ crates/interfaces/src/consensus.rs
@@ -7,7 +7,8 @@ use tokio::sync::watch::Receiver;
 /// Consensus is a protocol that chooses canonical chain.
 /// We are checking validity of block header here.
 #[async_trait]
-pub trait Consensus {
+#[auto_impl::auto_impl(&, Arc)]
+pub trait Consensus: Send + Sync {
     /// Get a receiver for the fork choice state
     fn fork_choice_state(&self) -> Receiver<ForkchoiceState>;
 
