a0d391349e97640b88fd098d33f7ea4383537b37
Matthias Seitz <matthias.seitz@outlook.de>
2022-11-10 21:24:47 +0100

    feat(net): more request handling (#186)
    * feat(net): more request handling
    
    * rm uneccessary checks
    
    * rm box

--- crates/net/network/src/state.rs
+++ crates/net/network/src/state.rs
@@ -3,22 +3,26 @@
 use crate::{
     discovery::{Discovery, DiscoveryEvent},
     fetch::StateFetcher,
-    message::{EthResponse, PeerRequestSender, PeerResponse},
+    message::{PeerRequestSender, PeerResponse},
     peers::{PeerAction, PeersManager},
     NodeId,
 };
 
 use reth_eth_wire::{capability::Capabilities, Status};
 use reth_interfaces::provider::BlockProvider;
-use reth_primitives::{H256, U256};
+use reth_primitives::H256;
 use std::{
     collections::{HashMap, VecDeque},
     net::SocketAddr,
     sync::Arc,
     task::{Context, Poll},
-    time::Instant,
 };
+use tokio::sync::oneshot;
 
+use crate::{
+    fetch::BlockResponseOutcome,
+    message::{BlockRequest, PeerRequest, PeerResponseResult},
+};
 use tracing::trace;
 
 /// The [`NetworkState`] keeps track of the state of all peers in the network.
@@ -36,14 +40,14 @@ pub struct NetworkState<C> {
     connected_peers: HashMap<NodeId, ConnectedPeer>,
     /// Manages connections to peers.
     peers_manager: PeersManager,
-    /// Tracks the state of connected peers
-    peers_state: HashMap<NodeId, PeerSessionState>,
     /// Buffered messages until polled.
     queued_messages: VecDeque<StateAction>,
     /// The client type that can interact with the chain.
     client: Arc<C>,
     /// Network discovery.
     discovery: Discovery,
+    /// The genesis hash of the network we're on
+    genesis_hash: H256,
     /// The type that handles requests.
     ///
     /// The fetcher streams RLPx related requests on a per-peer basis to this type. This type will
@@ -56,31 +60,57 @@ where
     C: BlockProvider,
 {
     /// Create a new state instance with the given params
-    pub(crate) fn new(client: Arc<C>, discovery: Discovery, peers_manager: PeersManager) -> Self {
+    pub(crate) fn new(
+        client: Arc<C>,
+        discovery: Discovery,
+        peers_manager: PeersManager,
+        genesis_hash: H256,
+    ) -> Self {
         Self {
             connected_peers: Default::default(),
             peers_manager,
-            peers_state: Default::default(),
             queued_messages: Default::default(),
             client,
             discovery,
+            genesis_hash,
             state_fetcher: Default::default(),
         }
     }
 
-    /// Event hook for an authenticated session for the peer.
-    pub(crate) fn on_session_authenticated(
+    /// Event hook for an activated session for the peer.
+    ///
+    /// Returns `Ok` if the session is valid, returns an `Err` if the session is not accepted and
+    /// should be rejected.
+    pub(crate) fn on_session_activated(
         &mut self,
-        _node_id: NodeId,
-        _capabilities: Arc<Capabilities>,
-        _status: Status,
-        _messages: PeerRequestSender,
-    ) {
-        // TODO notify fetecher as well
+        peer: NodeId,
+        capabilities: Arc<Capabilities>,
+        status: Status,
+        request_tx: PeerRequestSender,
+    ) -> Result<(), AddSessionError> {
+        // TODO add capacity check
+        debug_assert!(self.connected_peers.contains_key(&peer), "Already connected; not possible");
+
+        self.state_fetcher.new_connected_peer(peer, status.blockhash);
+
+        self.connected_peers.insert(
+            peer,
+            ConnectedPeer {
+                best_hash: status.blockhash,
+                capabilities,
+                request_tx,
+                pending_response: None,
+            },
+        );
+
+        Ok(())
     }
 
     /// Event hook for a disconnected session for the peer.
-    pub(crate) fn on_session_closed(&mut self, _node_id: NodeId) {}
+    pub(crate) fn on_session_closed(&mut self, peer: NodeId) {
+        self.connected_peers.remove(&peer);
+        self.state_fetcher.on_session_closed(&peer);
+    }
 
     /// Propagates Block to peers.
     pub(crate) fn announce_block(&mut self, _hash: H256, _block: ()) {
@@ -103,21 +133,75 @@ where
     fn on_peer_action(&mut self, action: PeerAction) {
         match action {
             PeerAction::Connect { node_id, remote_addr } => {
-                self.peers_state.insert(node_id, PeerSessionState::Connecting);
                 self.queued_messages.push_back(StateAction::Connect { node_id, remote_addr });
             }
             PeerAction::Disconnect { node_id } => {
-                self.peers_state.remove(&node_id);
+                self.state_fetcher.on_pending_disconnect(&node_id);
                 self.queued_messages.push_back(StateAction::Disconnect { node_id });
             }
         }
     }
 
     /// Disconnect the session
-    fn disconnect_session(&mut self, _node: NodeId) {}
+    fn on_session_disconnected(&mut self, peer: NodeId) {
+        self.connected_peers.remove(&peer);
+    }
+
+    /// Sends The message to the peer's session and queues in a response.
+    ///
+    /// Caution: this will replace an already pending response. It's the responsibility of the
+    /// caller to select the peer.
+    fn handle_block_request(&mut self, peer: NodeId, request: BlockRequest) {
+        if let Some(ref mut peer) = self.connected_peers.get_mut(&peer) {
+            let (request, response) = match request {
+                BlockRequest::GetBlockHeaders(request) => {
+                    let (response, rx) = oneshot::channel();
+                    let request = PeerRequest::GetBlockHeaders { request, response };
+                    let response = PeerResponse::BlockHeaders { response: rx };
+                    (request, response)
+                }
+                BlockRequest::GetBlockBodies(request) => {
+                    let (response, rx) = oneshot::channel();
+                    let request = PeerRequest::GetBlockBodies { request, response };
+                    let response = PeerResponse::BlockBodies { response: rx };
+                    (request, response)
+                }
+            };
+            let _ = peer.request_tx.to_session_tx.try_send(request);
+            peer.pending_response = Some(response);
+        }
+    }
+
+    /// Handle the outcome of processed response, for example directly queue another request.
+    fn on_block_response_outcome(&mut self, outcome: BlockResponseOutcome) -> Option<StateAction> {
+        match outcome {
+            BlockResponseOutcome::Request(peer, request) => {
+                self.handle_block_request(peer, request);
+            }
+            BlockResponseOutcome::BadResponse(_) => {
+                // TODO handle reputation change
+            }
+        }
+        None
+    }
 
     /// Invoked when received a response from a connected peer.
-    fn on_eth_response(&mut self, _node: NodeId, _resp: EthResponse) {}
+    fn on_eth_response(&mut self, peer: NodeId, resp: PeerResponseResult) -> Option<StateAction> {
+        match resp {
+            PeerResponseResult::BlockHeaders(res) => {
+                let outcome = self.state_fetcher.on_block_headers_response(peer, res)?;
+                return self.on_block_response_outcome(outcome)
+            }
+            PeerResponseResult::BlockBodies(res) => {
+                let outcome = self.state_fetcher.on_block_bodies_response(peer, res)?;
+                return self.on_block_response_outcome(outcome)
+            }
+            PeerResponseResult::PooledTransactions(_) => {}
+            PeerResponseResult::NodeData(_) => {}
+            PeerResponseResult::Receipts(_) => {}
+        }
+        None
+    }
 
     /// Advances the state
     pub(crate) fn poll(&mut self, cx: &mut Context<'_>) -> Poll<StateAction> {
@@ -137,7 +221,6 @@ where
             for (id, peer) in self.connected_peers.iter_mut() {
                 if let Some(response) = peer.pending_response.as_mut() {
                     match response.poll(cx) {
-                        Poll::Ready(Ok(resp)) => received_responses.push((*id, resp)),
                         Poll::Ready(Err(_)) => {
                             trace!(
                                 ?id,
@@ -146,6 +229,7 @@ where
                             );
                             disconnect_sessions.push(*id);
                         }
+                        Poll::Ready(Ok(resp)) => received_responses.push((*id, resp)),
                         Poll::Pending => continue,
                     };
                 }
@@ -155,11 +239,13 @@ where
             }
 
             for node in disconnect_sessions {
-                self.disconnect_session(node)
+                self.on_session_disconnected(node)
             }
 
             for (id, resp) in received_responses {
-                self.on_eth_response(id, resp);
+                if let Some(action) = self.on_eth_response(id, resp) {
+                    self.queued_messages.push_back(action);
+                }
             }
 
             // poll peer manager
@@ -180,32 +266,12 @@ where
 pub struct ConnectedPeer {
     /// Best block of the peer.
     pub(crate) best_hash: H256,
-    /// Best block number of the peer.
-    pub(crate) best_number: U256,
-    /// A communication channel directly to the session service.
-    pub(crate) message_tx: PeerRequestSender,
+    /// The capabilities of the connected peer.
+    pub(crate) capabilities: Arc<Capabilities>,
+    /// A communication channel directly to the session task.
+    pub(crate) request_tx: PeerRequestSender,
     /// The response receiver for a currently active request to that peer.
     pub(crate) pending_response: Option<PeerResponse>,
-}
-
-/// Tracks the current state of the peer session
-pub enum PeerSessionState {
-    /// Starting state for outbound connections.
-    ///
-    /// This will be triggered by a [`PeerAction::Connect`] action.
-    /// The peer will reside in the state until the connection has been authenticated.
-    Connecting,
-    /// Established connection that hasn't been authenticated yet.
-    Incoming {
-        /// How long to keep this open.
-        until: Instant,
-        sender: PeerRequestSender,
-    },
-    /// Node is connected to the peer and is ready to
-    Ready {
-        /// Communication channel directly to the session task
-        sender: PeerRequestSender,
-    },
 }
 
 /// Message variants triggered by the [`State`]
@@ -214,4 +280,13 @@ pub enum StateAction {
     Connect { remote_addr: SocketAddr, node_id: NodeId },
     /// Disconnect an existing connection
     Disconnect { node_id: NodeId },
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum AddSessionError {
+    #[error("No capacity for new sessions")]
+    AtCapacity {
+        /// The peer of the session
+        peer: NodeId,
+    },
 }
