42ceb91cf44438c8ba583a54800d6a8a37c9edca
Matthias Seitz <matthias.seitz@outlook.de>
2022-11-10 19:17:36 +0100

    feat(test): add TestApi type (#183)

--- crates/interfaces/src/provider/mod.rs
+++ crates/interfaces/src/provider/mod.rs
@@ -3,7 +3,7 @@ pub mod db_provider;
 mod error;
 mod storage;
 
-pub use block::{BlockProvider, HeaderProvider};
+pub use block::{BlockProvider, ChainInfo, HeaderProvider};
 pub use db_provider::{self as db, ProviderImpl};
 pub use error::Error;
 pub use storage::{StateProvider, StateProviderFactory, StorageProvider};
