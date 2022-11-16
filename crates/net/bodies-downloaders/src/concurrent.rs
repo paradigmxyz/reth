322744c41b49070c79d1e2fd5f3b1076360f1600
Oliver Nordbjerg <hi@notbjerg.me>
2022-11-16 16:31:29 +0100

    feat: downloader request retries

--- crates/net/bodies-downloaders/src/concurrent.rs
+++ crates/net/bodies-downloaders/src/concurrent.rs
@@ -1,4 +1,6 @@
-use futures_util::{stream, StreamExt, TryFutureExt};
+use backon::{ExponentialBackoff, Retryable};
+use futures_util::{stream, StreamExt};
+use reth_eth_wire::BlockBody;
 use reth_interfaces::p2p::bodies::{
     client::BodiesClient,
     downloader::{BodiesStream, BodyDownloader},
@@ -14,8 +16,10 @@ use std::sync::Arc;
 pub struct ConcurrentDownloader<C> {
     /// The bodies client
     client: Arc<C>,
+    /// The number of retries for each request.
+    retries: usize,
     /// The batch size per one request
-    pub batch_size: usize,
+    batch_size: usize,
 }
 
 impl<C: BodiesClient> BodyDownloader for ConcurrentDownloader<C> {
@@ -32,23 +36,53 @@ impl<C: BodiesClient> BodyDownloader for ConcurrentDownloader<C> {
         <I as IntoIterator>::IntoIter: Send + 'b,
         'b: 'a,
     {
-        // TODO: Retry
         Box::pin(
             stream::iter(headers.into_iter().map(|(block_number, header_hash)| {
-                {
-                    self.client
-                        .get_block_body(*header_hash)
-                        .map_ok(move |body| (*block_number, *header_hash, body))
-                        .map_err(|err| match err {
-                            BodiesClientError::Timeout { header_hash } => {
-                                DownloadError::Timeout { header_hash }
-                            }
-                            err => DownloadError::Client { source: err },
-                        })
-                }
+                (|| self.fetch_body(*block_number, *header_hash))
+                    .retry(ExponentialBackoff::default().with_max_times(self.retries))
             }))
             .buffered(self.batch_size),
         )
+    }
+}
+
+impl<C: BodiesClient> ConcurrentDownloader<C> {
+    /// Create a new concurrent downloader instance.
+    pub fn new(client: Arc<C>) -> Self {
+        Self { client, retries: 3, batch_size: 100 }
+    }
+
+    /// Set the number of blocks to fetch at the same time.
+    ///
+    /// Defaults to 100.
+    pub fn with_batch_size(mut self, batch_size: usize) -> Self {
+        self.batch_size = batch_size;
+        self
+    }
+
+    /// Set the number of times to retry body fetch requests.
+    ///
+    /// Defaults to 3.
+    pub fn with_retries(mut self, retries: usize) -> Self {
+        self.retries = retries;
+        self
+    }
+
+    /// Fetch a single block body.
+    async fn fetch_body(
+        &self,
+        block_number: BlockNumber,
+        header_hash: H256,
+    ) -> Result<(BlockNumber, H256, BlockBody), DownloadError> {
+        match self.client.get_block_body(header_hash).await {
+            Ok(body) => Ok((block_number, header_hash, body)),
+            Err(err) => Err(match err {
+                BodiesClientError::Timeout { header_hash } => {
+                    DownloadError::Timeout { header_hash }
+                }
+                err => DownloadError::Client { source: err },
+            }),
+        }
     }
 }
 
