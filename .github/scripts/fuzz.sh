92cad97eb0eca2ddf8d1832dd8688b2ca97176f7
Oliver Nordbjerg <hi@notbjerg.me>
2022-11-23 20:52:03 +0100

    ci: move fuzz to own job

--- .github/scripts/fuzz.sh
+++ .github/scripts/fuzz.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Runs fuzz tests using `cargo test-fuzz`. These should only be run after a `cargo test` has been executed once: https://github.com/trailofbits/test-fuzz#usage
+# Runs fuzz tests using `cargo test-fuzz`.
+# These should only be run after a `cargo test` has been executed once: https://github.com/trailofbits/test-fuzz#usage
 
 PACKAGE=$1
 TEST_TIME=${2:-5}
+
+echo "::group::$PACKAGE"
 
 # Gets the list of tests present in the package.
 TESTS=$(cargo test-fuzz --list -p $PACKAGE | head -n -3 | tail -n+9 | cat - <(echo \"--list\"]) | cat - | jq -r ".[]")
@@ -14,3 +17,5 @@ do
     cargo test-fuzz --no-ui  -p "$PACKAGE" $test  -- -V $TEST_TIME
     set +x
 done;
+
+echo "::endgroup::"
