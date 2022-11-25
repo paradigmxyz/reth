dda8df734183574ba95182f006295842ee98f404
Matthias Seitz <matthias.seitz@outlook.de>
2022-11-25 14:00:06 +0100

    refactor: headers client (#249)
    * refactor: headers client
    
    * chore: rustfmt
    
    * chore(clippy): make clippy happy
    
    * address comments
    
    * use Error instead
    
    * fix(sync): headers test client & stage tests (#255)
    
    * headers test client & stage tests
    
    * fix timeout test
    
    * fix import
    
    Co-authored-by: Roman Krasiuk <rokrassyuk@gmail.com>

--- crates/interfaces/src/p2p/headers/client.rs
+++ crates/interfaces/src/p2p/headers/client.rs
@@ -1,31 +1,8 @@
-use crate::p2p::MessageStream;
-
-use reth_primitives::{Header, H256, H512};
-
+use crate::p2p::error::RequestResult;
 use async_trait::async_trait;
-use reth_primitives::BlockHashOrNumber;
-use std::{collections::HashSet, fmt::Debug};
-
-/// Each peer returns a list of headers and the request id corresponding
-/// to these headers. This allows clients to make multiple requests in parallel
-/// and multiplex the responses accordingly.
-pub type HeadersStream = MessageStream<HeadersResponse>;
-
-/// The item contained in each [`MessageStream`] when used to fetch [`Header`]s via
-/// [`HeadersClient`].
-#[derive(Clone, Debug)]
-pub struct HeadersResponse {
-    /// The request id associated with this response.
-    pub id: u64,
-    /// The headers the peer replied with.
-    pub headers: Vec<Header>,
-}
-
-impl From<(u64, Vec<Header>)> for HeadersResponse {
-    fn from((id, headers): (u64, Vec<Header>)) -> Self {
-        HeadersResponse { id, headers }
-    }
-}
+pub use reth_eth_wire::BlockHeaders;
+use reth_primitives::{BlockHashOrNumber, H256};
+use std::fmt::Debug;
 
 /// The header request struct to be sent to connected peers, which
 /// will proceed to ask them to stream the requested headers to us.
@@ -47,12 +24,9 @@ pub trait HeadersClient: Send + Sync + Debug {
     /// Update the node's Status message.
     ///
     /// The updated Status message will be used during any new eth/65 handshakes.
-    async fn update_status(&self, height: u64, hash: H256, td: H256);
+    fn update_status(&self, height: u64, hash: H256, td: H256);
 
-    /// Sends the header request to the p2p network.
-    // TODO: What does this return?
-    async fn send_header_request(&self, id: u64, request: HeadersRequest) -> HashSet<H512>;
-
-    /// Stream the header response messages
-    async fn stream_headers(&self) -> HeadersStream;
+    /// Sends the header request to the p2p network and returns the header response received from a
+    /// peer.
+    async fn get_headers(&self, request: HeadersRequest) -> RequestResult<BlockHeaders>;
 }
