e364ac3f84bdb04f1296128b3eb2527fcdf90441
Matthias Seitz <matthias.seitz@outlook.de>
2022-11-27 11:56:24 +0100

    feat(net): add missing report on bad response

--- crates/net/network/src/peers/mod.rs
+++ crates/net/network/src/peers/mod.rs
@@ -5,5 +5,4 @@ mod reputation;
 
 pub(crate) use manager::{PeerAction, PeersManager};
 pub use manager::{PeersConfig, PeersHandle};
-pub(crate) use reputation::ReputationChange;
 pub use reputation::{ReputationChangeKind, ReputationChangeWeights};
