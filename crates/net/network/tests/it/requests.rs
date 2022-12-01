c8f78b61b29f1407668d52cdf159be04c01c6275
Matthias Seitz <matthias.seitz@outlook.de>
2022-12-01 15:19:06 +0100

    test(net): add e2e request header test

--- crates/net/network/tests/it/requests.rs
+++ crates/net/network/tests/it/requests.rs
@@ -4,9 +4,13 @@ use super::testnet::Testnet;
 use crate::{MockEthProvider, NetworkEventStream};
 use rand::Rng;
 use reth_eth_wire::BlockBody;
-use reth_interfaces::p2p::bodies::client::BodiesClient;
+use reth_interfaces::p2p::{
+    bodies::client::BodiesClient,
+    headers::client::{HeadersClient, HeadersRequest},
+};
 use reth_primitives::{
-    Block, Bytes, Signature, Transaction, TransactionKind, TransactionSigned, TxEip2930, H256, U256,
+    Block, Bytes, Header, Signature, Transaction, TransactionKind, TransactionSigned, TxEip2930,
+    H256, U256,
 };
 use std::sync::Arc;
 
@@ -67,5 +71,50 @@ async fn test_get_body() {
         assert_eq!(blocks.len(), 1);
         let expected = BlockBody { transactions: block.body, ommers: block.ommers };
         assert_eq!(blocks[0], expected);
+    }
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_get_header() {
+    reth_tracing::init_tracing();
+    let mut rng = rand::thread_rng();
+    let mock_provider = Arc::new(MockEthProvider::default());
+
+    let mut net = Testnet::create_with(2, mock_provider.clone()).await;
+
+    // install request handlers
+    net.for_each_mut(|peer| peer.install_request_handler());
+
+    let handle0 = net.peers()[0].handle();
+    let mut events0 = NetworkEventStream::new(handle0.event_listener());
+
+    let handle1 = net.peers()[1].handle();
+
+    let _handle = net.spawn();
+
+    let fetch0 = handle0.fetch_client().await.unwrap();
+
+    handle0.add_peer(*handle1.peer_id(), handle1.local_addr());
+    let connected = events0.next_session_established().await.unwrap();
+    assert_eq!(connected, *handle1.peer_id());
+
+    let start: u64 = rng.gen();
+    let mut hash = H256::random();
+    // request some headers
+    for idx in 0..100 {
+        // Set a new random header to the mock storage and request it via the network
+        let header = Header { number: start + idx, parent_hash: hash, ..Default::default() };
+        hash = H256::random();
+
+        mock_provider.add_header(hash, header.clone());
+
+        let req = HeadersRequest { start: hash.into(), limit: 1, reverse: false };
+
+        let res = fetch0.get_headers(req).await;
+        assert!(res.is_ok());
+
+        let headers = res.unwrap().0;
+        assert_eq!(headers.len(), 1);
+        assert_eq!(headers[0], header);
     }
 }
