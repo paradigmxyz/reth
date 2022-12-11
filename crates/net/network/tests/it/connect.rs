09fb133b57f4310632432063b232d06d30231faf
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-12-10 22:24:21 -0500

    cargo fmt

--- crates/net/network/tests/it/connect.rs
+++ crates/net/network/tests/it/connect.rs
@@ -9,8 +9,8 @@ use ethers_providers::{Http, Middleware, Provider};
 use futures::StreamExt;
 use reth_discv4::{bootnodes::mainnet_nodes, Discv4Config};
 use reth_network::{NetworkConfig, NetworkEvent, NetworkManager};
-use reth_provider::test_utils::TestApi;
 use reth_primitives::PeerId;
+use reth_provider::test_utils::TestApi;
 use secp256k1::SecretKey;
 use std::{collections::HashSet, net::SocketAddr, sync::Arc};
 
