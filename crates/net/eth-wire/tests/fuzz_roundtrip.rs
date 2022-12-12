542e6ccf88576622cd311a46653b44e68297dee0
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-12-12 17:58:57 -0500

    use more explicit imports in fuzz_rlp

--- crates/net/eth-wire/tests/fuzz_roundtrip.rs
+++ crates/net/eth-wire/tests/fuzz_roundtrip.rs
@@ -1,9 +1,7 @@
 //! Round-trip encoding fuzzing for the `eth-wire` crate.
-use std::fmt::Debug;
-
 use reth_rlp::{Decodable, Encodable};
 use serde::Serialize;
-use test_fuzz::test_fuzz;
+use std::fmt::Debug;
 
 /// Creates a fuzz test for a type that should be [`Encodable`](reth_rlp::Encodable) and
 /// [`Decodable`](reth_rlp::Decodable).
@@ -21,11 +19,10 @@ where
 
 /// Creates a fuzz test for a rlp encodable and decodable type.
 macro_rules! fuzz_type_and_name {
-    ( $x:ty, $fuzzname:ident) => {
+    ( $x:ty, $fuzzname:ident ) => {
         /// Fuzzes the round-trip encoding of the type.
-        #[cfg(test)]
-        #[allow(dead_code)]
         #[test_fuzz]
+        #[allow(non_snake_case)]
         fn $fuzzname(thing: $x) {
             roundtrip_encoding::<$x>(thing)
         }
@@ -35,13 +32,15 @@ macro_rules! fuzz_type_and_name {
 #[allow(non_snake_case)]
 #[cfg(any(test, feature = "bench"))]
 pub mod fuzz_rlp {
-    use super::*;
     use reth_eth_wire::{
         BlockBodies, BlockHeaders, DisconnectReason, GetBlockBodies, GetBlockHeaders, GetNodeData,
         GetPooledTransactions, GetReceipts, HelloMessage, NewBlock, NewBlockHashes,
         NewPooledTransactionHashes, NodeData, P2PMessage, PooledTransactions, Receipts, Status,
         Transactions,
     };
+    use test_fuzz::test_fuzz;
+
+    use crate::roundtrip_encoding;
 
     // p2p subprotocol messages
     fuzz_type_and_name!(HelloMessage, fuzz_HelloMessage);
