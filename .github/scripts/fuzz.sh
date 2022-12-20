5fcaba2980d95bd028a7390b5969aaac59388e86
Oliver Nordbjerg <hi@notbjerg.me>
2022-12-20 18:07:50 +0100

    ci: fix fuzz test target matching

--- .github/scripts/fuzz.sh
+++ .github/scripts/fuzz.sh
@@ -5,9 +5,6 @@ set -e
 PACKAGE=$1
 TEST_TIME=${2:-5}
 
-echo Building corpus.
-cargo test -p $PACKAGE --no-run --all-features
-
 # Gets the list of tests present in the package.
 TESTS=$(cargo test-fuzz --list -p $PACKAGE | head -n -3 | tail -n+9 | cat - <(echo \"--list\"]) | cat - | jq -r ".[]")
 
@@ -15,6 +12,6 @@ for test in $TESTS
 do
     echo Running test: $test
     set -x
-    cargo test-fuzz --no-ui  -p "$PACKAGE" $test  -- -V $TEST_TIME
+    cargo test-fuzz --no-ui --exact -p "$PACKAGE" $test -- -V $TEST_TIME
     set +x
 done;
