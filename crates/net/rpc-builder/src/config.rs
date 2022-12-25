b9c4cc5cf61ea7232d54470fa5c203c9ea29f4da
Matthias Seitz <matthias.seitz@outlook.de>
2022-12-25 15:32:22 +0100

    add rpc builder

---
+++ crates/net/rpc-builder/src/config.rs
@@ -0,0 +1,6 @@
+
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct RpcConfig {
+
+}
---
+++ crates/net/rpc-builder/src/lib.rs
@@ -0,0 +1,10 @@
+#![warn(missing_docs, unreachable_pub)]
+#![deny(unused_must_use, rust_2018_idioms)]
+#![doc(test(
+    no_crate_inject,
+    attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
+))]
+
+//! Configure reth RPC
+
+mod config;
