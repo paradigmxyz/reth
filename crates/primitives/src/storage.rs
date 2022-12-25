28bd003641f4de619579054d480087dc09e318c7
rakita <rakita@users.noreply.github.com>
2022-12-25 01:13:03 +0100

    fix(db): Dont compress DupSort SubKey (#594)
    * bug(db): Dont compress DupSort SubKey
    
    * unwrap or default config, Added notes
    
    * fmt empty lines

--- crates/primitives/src/storage.rs
+++ crates/primitives/src/storage.rs
@@ -1,12 +1,31 @@
 use super::{H256, U256};
-use reth_codecs::{main_codec, Compact};
+use reth_codecs::Compact;
 
 /// Account storage entry.
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
-#[main_codec]
 pub struct StorageEntry {
     /// Storage key.
     pub key: H256,
     /// Value on storage key.
     pub value: U256,
+}
+
+// NOTE: Removing main_codec and manually encode subkey
+// and compress second part of the value. If we have compression
+// over whole value (Even SubKey) that would mess up fetching of values with seek_by_key_subkey
+impl Compact for StorageEntry {
+    fn to_compact(self, buf: &mut impl bytes::BufMut) -> usize {
+        // for now put full bytes and later compress it.
+        buf.put_slice(&self.key.to_fixed_bytes()[..]);
+        self.value.to_compact(buf) + 32
+    }
+
+    fn from_compact(buf: &[u8], len: usize) -> (Self, &[u8])
+    where
+        Self: Sized,
+    {
+        let key = H256::from_slice(&buf[..32]);
+        let (value, out) = U256::from_compact(&buf[32..], len - 32);
+        (Self { key, value }, out)
+    }
 }
