d18ca92ca3b5a8ed799f2f3e3ad67bedc4c3b7f1
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2022-12-26 14:30:36 -0300

    Apply metrics macro to TxPoolMetrics

--- crates/transaction-pool/src/metrics.rs
+++ crates/transaction-pool/src/metrics.rs
@@ -1,9 +1,11 @@
 //! Transaction pool metrics.
 
 use metrics::{describe_counter, register_counter, Counter};
-use std::fmt;
+use reth_metrics_derive::Metrics;
 
 /// Transaction pool metrics
+#[derive(Metrics)]
+#[metrics(scope = "transaction_pool")]
 pub struct TxPoolMetrics {
     /// Number of transactions inserted in the pool
     pub(crate) inserted_transactions: Counter,
@@ -13,37 +15,20 @@ pub struct TxPoolMetrics {
     pub(crate) removed_transactions: Counter,
 }
 
-impl TxPoolMetrics {
-    /// Describe transaction pool metrics
-    pub fn describe() {
-        describe_counter!(
-            "transaction_pool.inserted_transactions",
-            "Number of transactions inserted in the pool"
-        );
-        describe_counter!(
-            "transaction_pool.invalid_transactions",
-            "Number of invalid transactions"
-        );
-        describe_counter!(
-            "transaction_pool.removed_transactions",
-            "Number of removed transactions from the pool"
-        );
-    }
-}
-
-impl Default for TxPoolMetrics {
-    /// Initialize txpool metrics struct and register them
-    fn default() -> Self {
-        Self {
-            inserted_transactions: register_counter!("transaction_pool.inserted_transactions"),
-            invalid_transactions: register_counter!("transaction_pool.invalid_transactions"),
-            removed_transactions: register_counter!("transaction_pool.removed_transactions"),
-        }
-    }
-}
-
-impl fmt::Debug for TxPoolMetrics {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("TxPoolMetrics").finish()
-    }
-}
+// impl TxPoolMetrics {
+//     /// Describe transaction pool metrics
+//     pub fn describe() {
+//         describe_counter!(
+//             "transaction_pool.inserted_transactions",
+//             "Number of transactions inserted in the pool"
+//         );
+//         describe_counter!(
+//             "transaction_pool.invalid_transactions",
+//             "Number of invalid transactions"
+//         );
+//         describe_counter!(
+//             "transaction_pool.removed_transactions",
+//             "Number of removed transactions from the pool"
+//         );
+//     }
+// }
