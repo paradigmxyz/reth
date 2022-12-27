4745a055ad918580cdcfc849a0d9b7c3adf823ff
Tomás <tomas.gruner@lambdaclass.com>
2022-12-27 12:28:08 -0300

    Hide NodeKey struct

--- crates/net/discv4/src/node.rs
+++ crates/net/discv4/src/node.rs
@@ -3,7 +3,7 @@ use reth_primitives::{keccak256, NodeRecord, PeerId};
 
 /// The key type for the table.
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct NodeKey(pub(crate) PeerId);
+pub(crate) struct NodeKey(pub(crate) PeerId);
 
 impl From<PeerId> for NodeKey {
     fn from(value: PeerId) -> Self {
@@ -21,12 +21,12 @@ impl From<NodeKey> for discv5::Key<NodeKey> {
 
 /// Converts a `PeerId` into the required `Key` type for the table
 #[inline]
-pub fn kad_key(node: PeerId) -> discv5::Key<NodeKey> {
+pub(crate) fn kad_key(node: PeerId) -> discv5::Key<NodeKey> {
     discv5::kbucket::Key::from(NodeKey::from(node))
 }
 
 /// Returns the key type for the kademlia table
 #[inline]
-pub fn record_key(node: &NodeRecord) -> discv5::Key<NodeKey> {
+pub(crate) fn record_key(node: &NodeRecord) -> discv5::Key<NodeKey> {
     NodeKey(node.id).into()
 }
