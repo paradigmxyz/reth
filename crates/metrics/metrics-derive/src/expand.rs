5bb14ecb7c01ec046c11daa0d99da66f23e9075a
Roman Krasiuk <rokrassyuk@gmail.com>
2022-12-27 13:16:41 +0200

    fix(metrics): metrics derive separator (#625)
    * fix(metrics): metrics derive separator
    
    * fix existing metrics
    
    * rm log
    
    * static regex

--- crates/metrics/metrics-derive/src/expand.rs
+++ crates/metrics/metrics-derive/src/expand.rs
@@ -1,10 +1,17 @@
+use once_cell::sync::Lazy;
 use quote::{quote, ToTokens};
+use regex::Regex;
 use syn::{
     punctuated::Punctuated, Attribute, Data, DeriveInput, Error, Lit, LitStr, MetaNameValue,
     Result, Token,
 };
 
 use crate::{metric::Metric, with_attrs::WithAttrs};
+
+/// Metric name regex according to Prometheus data model
+/// https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels
+static METRIC_NAME_RE: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"^[a-zA-Z_:][a-zA-Z0-9_:]*$").unwrap());
 
 pub(crate) fn derive(node: &DeriveInput) -> Result<proc_macro2::TokenStream> {
     let ty = &node.ident;
@@ -17,7 +24,7 @@ pub(crate) fn derive(node: &DeriveInput) -> Result<proc_macro2::TokenStream> {
         .iter()
         .map(|metric| {
             let field_name = &metric.field.ident;
-            let register_stmt = metric.register_stmt(&metrics_attr.scope)?;
+            let register_stmt = metric.register_stmt(&metrics_attr)?;
             Ok(quote! {
                 #field_name: #register_stmt,
             })
@@ -26,7 +33,7 @@ pub(crate) fn derive(node: &DeriveInput) -> Result<proc_macro2::TokenStream> {
 
     let describe_stmts = metric_fields
         .iter()
-        .map(|metric| metric.describe_stmt(&metrics_attr.scope))
+        .map(|metric| metric.describe_stmt(&metrics_attr))
         .collect::<Result<Vec<_>>>()?;
 
     Ok(quote! {
@@ -56,23 +63,42 @@ pub(crate) fn derive(node: &DeriveInput) -> Result<proc_macro2::TokenStream> {
 }
 
 pub(crate) struct MetricsAttr {
-    scope: LitStr,
+    pub(crate) scope: LitStr,
+    pub(crate) separator: Option<LitStr>,
 }
 
 fn parse_metrics_attr(node: &DeriveInput) -> Result<MetricsAttr> {
     let metrics_attr = parse_single_required_attr(node, "metrics")?;
     let parsed =
         metrics_attr.parse_args_with(Punctuated::<MetaNameValue, Token![,]>::parse_terminated)?;
-    let mut parsed_iter = parsed.into_iter();
-    if let Some(kv) = parsed_iter.next() {
-        if !kv.path.is_ident("scope") || parsed_iter.next().is_some() {
-            Err(Error::new_spanned(kv, "Only single `scope = ..` value is supported."))
+    let (mut scope, mut separator) = (None, None);
+    for kv in parsed {
+        if kv.path.is_ident("scope") {
+            if scope.is_some() {
+                return Err(Error::new_spanned(kv, "Duplicate `scope` value provided."))
+            }
+            let scope_lit = parse_str_lit(&kv.lit)?;
+            validate_metric_name(&scope_lit)?;
+            scope = Some(scope_lit);
+        } else if kv.path.is_ident("separator") {
+            if separator.is_some() {
+                return Err(Error::new_spanned(kv, "Duplicate `separator` value provided."))
+            }
+            let separator_lit = parse_str_lit(&kv.lit)?;
+            if separator_lit.value() != ":" && separator_lit.value() != "_" {
+                return Err(Error::new_spanned(
+                    kv,
+                    "Unsupported `separator` value. Supported: `:` and `_`.",
+                ))
+            }
+            separator = Some(separator_lit);
         } else {
-            Ok(MetricsAttr { scope: parse_str_lit(kv.lit)? })
+            return Err(Error::new_spanned(kv, "Unsupported attribute entry."))
         }
-    } else {
-        Err(Error::new_spanned(metrics_attr, "`scope = ..` must be set."))
     }
+
+    let scope = scope.ok_or(Error::new_spanned(node, "`scope = ..` must be set."))?;
+    Ok(MetricsAttr { scope, separator })
 }
 
 fn parse_metric_fields(node: &DeriveInput) -> Result<Vec<Metric<'_>>> {
@@ -91,12 +117,14 @@ fn parse_metric_fields(node: &DeriveInput) -> Result<Vec<Metric<'_>>> {
                     if describe.is_some() {
                         return Err(Error::new_spanned(kv, "Duplicate `describe` value provided."))
                     }
-                    describe = Some(parse_str_lit(kv.lit)?);
+                    describe = Some(parse_str_lit(&kv.lit)?);
                 } else if kv.path.is_ident("rename") {
                     if rename.is_some() {
                         return Err(Error::new_spanned(kv, "Duplicate `rename` value provided."))
                     }
-                    rename = Some(parse_str_lit(kv.lit)?)
+                    let rename_lit = parse_str_lit(&kv.lit)?;
+                    validate_metric_name(&rename_lit)?;
+                    rename = Some(rename_lit)
                 } else {
                     return Err(Error::new_spanned(kv, "Unsupported attribute entry."))
                 }
@@ -111,7 +139,7 @@ fn parse_metric_fields(node: &DeriveInput) -> Result<Vec<Metric<'_>>> {
                 None => {
                     return Err(Error::new_spanned(
                         field,
-                        "Either doc comment or `describe` must be provided.",
+                        "Either doc comment or `describe = ..` must be set.",
                     ))
                 }
             },
@@ -121,6 +149,14 @@ fn parse_metric_fields(node: &DeriveInput) -> Result<Vec<Metric<'_>>> {
     }
 
     Ok(metrics)
+}
+
+fn validate_metric_name(name: &LitStr) -> Result<()> {
+    if METRIC_NAME_RE.is_match(&name.value()) {
+        Ok(())
+    } else {
+        Err(Error::new_spanned(name, format!("Value must match regex {}", METRIC_NAME_RE.as_str())))
+    }
 }
 
 fn parse_single_attr<'a, T: WithAttrs + ToTokens>(
@@ -171,9 +207,9 @@ fn parse_docs_to_string<T: WithAttrs>(token: &T) -> Result<Option<String>> {
     Ok(doc_str)
 }
 
-fn parse_str_lit(lit: Lit) -> Result<LitStr> {
+fn parse_str_lit(lit: &Lit) -> Result<LitStr> {
     match lit {
-        Lit::Str(lit_str) => Ok(lit_str),
+        Lit::Str(lit_str) => Ok(lit_str.to_owned()),
         _ => Err(Error::new_spanned(lit, "Value **must** be a string literal.")),
     }
 }
--- crates/metrics/metrics-derive/tests/compile-fail/metrics_attr.rs
+++ crates/metrics/metrics-derive/tests/compile-fail/metrics_attr.rs
@@ -10,3 +10,43 @@ struct CustomMetrics;
 #[metrics()]
 #[metrics()]
 struct CustomMetrics2;
+
+#[derive(Metrics)]
+#[metrics()]
+struct CustomMetrics3;
+
+#[derive(Metrics)]
+#[metrics(scope = value)]
+struct CustomMetrics4;
+
+#[derive(Metrics)]
+#[metrics(scope = 123)]
+struct CustomMetrics5;
+
+#[derive(Metrics)]
+#[metrics(scope = "some.scope")]
+struct CustomMetrics6;
+
+#[derive(Metrics)]
+#[metrics(scope = "some_scope", scope = "another_scope")]
+struct CustomMetrics7;
+
+#[derive(Metrics)]
+#[metrics(separator = value)]
+struct CustomMetrics8;
+
+#[derive(Metrics)]
+#[metrics(separator = 123)]
+struct CustomMetrics9;
+
+#[derive(Metrics)]
+#[metrics(separator = ".")]
+struct CustomMetrics10;
+
+#[derive(Metrics)]
+#[metrics(separator = "_", separator = ":")]
+struct CustomMetrics11;
+
+#[derive(Metrics)]
+#[metrics(random = "value")]
+struct CustomMetrics12;
