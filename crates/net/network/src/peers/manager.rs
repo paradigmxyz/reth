b5b86b143249409ac7179444fa4f5d54b103b4f7
Matthias Seitz <matthias.seitz@outlook.de>
2023-01-01 12:17:21 +0100

    docs(net): update best_unconnected docs

--- crates/net/network/src/peers/manager.rs
+++ crates/net/network/src/peers/manager.rs
@@ -455,7 +455,11 @@ impl PeersManager {
 
     /// Returns the idle peer with the highest reputation.
     ///
-    /// Peers with a `forkId` are considered better than peers without.
+    /// Peers that are `trusted`, see [PeerKind], are prioritized as long as they're not currently
+    /// marked as banned. Peers with a `forkId` are considered better than peers without.
+    ///
+    /// If `connect_trusted_nodes_only` is enabled, see [PeersConfig], then this will only consider
+    /// `trusted` peers.
     ///
     /// Returns `None` if no peer is available.
     fn best_unconnected(&mut self) -> Option<(PeerId, &mut Peer)> {
