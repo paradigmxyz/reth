e7a1fd9a1fff8a4a9856f30d8e1e0f43bffd5d1f
Andrew Kirillov <andrew@paradigm.xyz>
2023-01-03 21:34:15 -0500

    added basic test

--- crates/net/common/src/bandwidth_monitor.rs
+++ crates/net/common/src/bandwidth_monitor.rs
@@ -69,12 +69,21 @@ impl BandwidthMonitor {
 
 /// Wraps around a single stream that implements [`AsyncRead`] + [`AsyncWrite`] and monitors the bandwidth through it
 #[pin_project::pin_project]
-pub(crate) struct MonitoredStream<Stream> {
-    /// The stream this wraps around
+pub(crate) struct MonitoredStream<S> {
+    /// The stream this instruments
     #[pin]
-    stream: Stream,
+    inner: S,
     /// The [`BandwidthMonitor`] struct this uses to monitor bandwidth
     monitor: Arc<BandwidthMonitor>,
+}
+
+impl<S> MonitoredStream<S> {
+    fn new(inner: S) -> Self {
+        Self {
+            inner,
+            monitor: BandwidthMonitor::new()
+        }
+    }
 }
 
 impl<Stream: AsyncRead> AsyncRead for MonitoredStream<Stream> {
@@ -86,7 +95,7 @@ impl<Stream: AsyncRead> AsyncRead for MonitoredStream<Stream> {
         let this = self.project();
         let num_bytes = {
             let init_num_bytes = buf.filled().len();
-            ready!(this.stream.poll_read(cx, buf))?;
+            ready!(this.inner.poll_read(cx, buf))?;
             buf.filled().len() - init_num_bytes
         };
         this.monitor.inbound.fetch_add(
@@ -104,23 +113,64 @@ impl<Stream: AsyncWrite> AsyncWrite for MonitoredStream<Stream> {
         buf: &[u8],
     ) -> Poll<io::Result<usize>> {
         let this = self.project();
-        let num_bytes = ready!(this.stream.poll_write(cx, buf))?;
-        this.monitor.inbound.fetch_add(
+        let num_bytes = ready!(this.inner.poll_write(cx, buf))?;
+        this.monitor.outbound.fetch_add(
             u64::try_from(num_bytes).unwrap_or(u64::max_value()),
             Ordering::Relaxed,
         );
-        Poll::Ready(Ok((num_bytes)))
+        Poll::Ready(Ok(num_bytes))
     }
 
     fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
         let this = self.project();
-        this.stream.poll_flush(cx)
+        this.inner.poll_flush(cx)
     }
 
     fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
         let this = self.project();
-        this.stream.poll_shutdown(cx)
+        this.inner.poll_shutdown(cx)
     }
 }
 
 // TODO: Something similar to rust-libp2p's `StreamMuxer` to monitor bandwidth across _all_ streams?
+
+// TODO: Tests
+// - Test reading + writing to/from a basic stream
+// - Test reeading + writing to/from a TCP stream
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio::io::{duplex, AsyncReadExt, AsyncWriteExt};
+    
+    #[tokio::test]
+    async fn test_count_read_write() {
+        // Taken in large part from https://docs.rs/tokio/latest/tokio/io/struct.DuplexStream.html#example
+
+        let (client, server) = duplex(64);
+        let mut monitored_client = MonitoredStream::new(client);
+        let mut monitored_server = MonitoredStream::new(server);
+
+        monitored_client.write_all(b"ping").await.unwrap();
+        // Assert that the client stream wrote 4 bytes
+        let client_outbound = monitored_client.monitor.total_outbound();
+        assert_eq!(client_outbound, 4, "Expected client to write 4 bytes, but it wrote {}", client_outbound);
+
+        let mut buf = [0u8; 4];
+        monitored_server.read(&mut buf).await.unwrap();
+        // Assert that the server stream read 4 bytes
+        let server_inbound = monitored_server.monitor.total_inbound();
+        assert_eq!(server_inbound, 4, "Expected server to read 4 bytes, but it read {}", server_inbound);
+
+        monitored_server.write_all(b"pong").await.unwrap();
+        // Assert that the server stream wrote 4 bytes
+        let server_outbound = monitored_server.monitor.total_outbound();
+        assert_eq!(server_outbound, 4, "Expected server to write 4 bytes, but it wrote {}", server_outbound);
+
+        monitored_client.read(&mut buf).await.unwrap();
+        // Assert that the client stream read 4 bytes
+        let client_inbound = monitored_client.monitor.total_inbound();
+        assert_eq!(client_inbound, 4, "Expected client to read 4 bytes, but it read {}", client_inbound);
+    }
+
+}
