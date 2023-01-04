312d6062e6e6efc527d9ad1ed7e10a0ca503a7e8
Roman Krasiuk <rokrassyuk@gmail.com>
2023-01-04 21:21:48 +0200

    display err

--- bin/reth/src/p2p/mod.rs
+++ bin/reth/src/p2p/mod.rs
@@ -114,7 +114,7 @@ impl Command {
             Subcommands::Header { id } => {
                 let header = (move || self.get_single_header(fetch_client.clone(), id))
                     .retry(backoff)
-                    .notify(|err, _| println!("Error requesting header: {err:?}. Retrying..."))
+                    .notify(|err, _| println!("Error requesting header: {err}. Retrying..."))
                     .await?;
                 println!("Successfully downloaded header: {header:?}");
             }
@@ -131,7 +131,7 @@ impl Command {
                             )
                         })
                         .retry(backoff.clone())
-                        .notify(|err, _| println!("Error requesting header: {err:?}. Retrying..."))
+                        .notify(|err, _| println!("Error requesting header: {err}. Retrying..."))
                         .await?;
                         header.hash()
                     }
@@ -141,7 +141,7 @@ impl Command {
                     async move { client.get_block_bodies(vec![hash]).await }
                 })
                 .retry(backoff)
-                .notify(|err, _| println!("Error requesting block: {err:?}. Retrying..."))
+                .notify(|err, _| println!("Error requesting block: {err}. Retrying..."))
                 .await?
                 .split();
                 if result.len() != 1 {
