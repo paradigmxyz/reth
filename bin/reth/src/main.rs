1616a0be1dcf3fa92012aaaa38e354c3e6cc1dc0
Roman Krasiuk <rokrassyuk@gmail.com>
2023-01-04 15:45:18 +0200

    clippy

--- bin/reth/src/main.rs
+++ bin/reth/src/main.rs
@@ -1,7 +1,7 @@
 #[tokio::main]
 async fn main() {
     if let Err(err) = reth::cli::run().await {
-        eprintln!("Error: {:?}", err);
+        eprintln!("Error: {err:?}");
         std::process::exit(1);
     }
 }
