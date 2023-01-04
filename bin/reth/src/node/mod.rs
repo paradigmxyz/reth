028bcd6db8a97052ff87f6ffc17cc90c09ba5781
Roman Krasiuk <rokrassyuk@gmail.com>
2023-01-04 11:25:40 +0200

    feat(sync): update sync state within pipeline (#697)
    * feat(sync): update sync state within pipeline
    
    * address pr comments
    
    * fix comment

--- bin/reth/src/node/mod.rs
+++ bin/reth/src/node/mod.rs
@@ -119,6 +119,7 @@ impl Command {
         // TODO: Remove magic numbers
         let fetch_client = Arc::new(network.fetch_client().await?);
         let mut pipeline = reth_stages::Pipeline::default()
+            .with_sync_state_updater(network.clone())
             .push(HeaderStage {
                 downloader: headers::linear::LinearDownloadBuilder::default()
                     .batch_size(config.stages.headers.downloader_batch_size)
