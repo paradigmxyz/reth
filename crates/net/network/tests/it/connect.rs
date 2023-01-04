83f82dfa2501f91fea61ec5adb981cae136bd66f
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-01-04 16:46:18 -0500

    log genesis hash
     * fixes lint errors

--- crates/net/network/tests/it/connect.rs
+++ crates/net/network/tests/it/connect.rs
@@ -732,6 +732,7 @@ async fn sync_from_clique_geth() {
         let genesis_block =
             provider.get_block(0).await.unwrap().expect("a genesis block should exist");
         let genesis_hash = genesis_block.hash.unwrap();
+        println!("genesis hash: {genesis_hash}");
 
         // build the forkhash
 
