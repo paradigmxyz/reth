adc1e4fb6848a7de562b2b3dfc10ce36d4fca489
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-01-04 23:02:02 -0500

    create status and connect reth with geth

--- crates/net/network/tests/it/connect.rs
+++ crates/net/network/tests/it/connect.rs
@@ -12,14 +12,14 @@ use ethers_providers::{Http, Middleware, Provider};
 use ethers_signers::{LocalWallet, Signer};
 use futures::StreamExt;
 use reth_discv4::{bootnodes::mainnet_nodes, Discv4Config};
-use reth_eth_wire::DisconnectReason;
+use reth_eth_wire::{DisconnectReason, EthVersion, Status};
 use reth_interfaces::{
     p2p::headers::client::{HeadersClient, HeadersRequest},
     sync::{SyncState, SyncStateUpdater},
 };
 use reth_net_common::ban_list::BanList;
-use reth_network::{NetworkConfig, NetworkEvent, NetworkManager, PeersConfig};
-use reth_primitives::{HeadersDirection, NodeRecord, PeerId};
+use reth_network::{NetworkConfig, NetworkEvent, NetworkHandle, NetworkManager, PeersConfig};
+use reth_primitives::{Chain, ForkHash, ForkId, Header, HeadersDirection, NodeRecord, PeerId};
 use reth_provider::test_utils::NoopProvider;
 use reth_transaction_pool::test_utils::testing_pool;
 use secp256k1::SecretKey;
@@ -51,6 +51,144 @@ fn create_new_geth() -> (Geth, tempfile::TempDir) {
     let geth = Geth::new().data_dir(temp_dir.path()).p2p_port(unused_port());
 
     (geth, temp_dir)
+}
+
+/// Extracts the genesis block header from an ethers [`Genesis`](ethers_core::utils::Genesis).
+fn genesis_header(genesis: &Genesis) -> Header {
+    Header {
+        gas_limit: genesis.gas_limit.as_u64(),
+        difficulty: genesis.difficulty.into(),
+        nonce: genesis.nonce.as_u64(),
+        extra_data: genesis.extra_data.0.clone(),
+        // state_root: genesis.state_root, TODO field not in Genesis, might need to add
+        timestamp: genesis.timestamp.as_u64(),
+        mix_hash: genesis.mix_hash.0.into(),
+        beneficiary: genesis.coinbase.0.into(),
+        ..Default::default()
+    }
+}
+
+/// Obtains a [`ForkId`](reth_primitives::ForkId) from an ethers
+/// [`Genesis`](ethers_core::utils::Genesis).
+fn extract_fork_hash(genesis: &Genesis) -> ForkHash {
+    // first create header and get hash
+    let sealed_header = genesis_header(genesis).seal();
+    let fork_blocks = extract_fork_blocks(genesis);
+    let chain_forkhash =
+        fork_blocks.iter().fold(ForkHash::from(sealed_header.hash()), |acc, block| acc + *block);
+
+    chain_forkhash
+}
+
+/// Obtains an initial [`Status`](reth_eth_wire::Status) from an ethers
+/// [`Genesis`](ethers_core::utils::Genesis).
+///
+/// Sets the `blockhash` and `genesis` fields to the genesis block hash, and initializes the
+/// `total_difficulty` as zero.
+fn extract_status(genesis: &Genesis) -> Status {
+    let sealed_header = genesis_header(genesis).seal();
+    let chain_forkhash = extract_fork_hash(genesis);
+
+    Status {
+        version: EthVersion::Eth67 as u8,
+        chain: Chain::Id(genesis.config.chain_id),
+        total_difficulty: genesis.difficulty.into(),
+        blockhash: sealed_header.hash(),
+        genesis: sealed_header.hash(),
+        forkid: ForkId { hash: chain_forkhash, next: 0 },
+    }
+}
+
+/// Obtains the list of fork block numbers in order from an ethers
+/// [`Genesis`](ethers_core::utils::Genesis).
+///
+/// This should be the same as [Geth's `gather_forks`
+/// method](https://github.com/ethereum/go-ethereum/blob/6c149fd4ad063f7c24d726a73bc0546badd1bc73/core/forkid/forkid.go#L215).
+fn extract_fork_blocks(genesis: &Genesis) -> Vec<u64> {
+    // will just put each consecutive fork in a vec
+    let mut fork_blocks_opt = vec![
+        // genesis.config.frontier,
+        genesis.config.homestead_block,
+        genesis.config.dao_fork_block,
+        genesis.config.eip150_block,
+        genesis.config.eip155_block,
+        genesis.config.eip158_block,
+        genesis.config.byzantium_block,
+        genesis.config.constantinople_block,
+        genesis.config.petersburg_block,
+        genesis.config.istanbul_block,
+        genesis.config.muir_glacier_block,
+        genesis.config.berlin_block,
+        genesis.config.london_block,
+        genesis.config.arrow_glacier_block,
+        genesis.config.gray_glacier_block,
+        genesis.config.merge_netsplit_block,
+        genesis.config.shanghai_block,
+        genesis.config.cancun_block,
+    ];
+
+    // filter out the None values
+    fork_blocks_opt.retain(|block| block.is_some());
+    fork_blocks_opt.iter().map(|block| block.unwrap()).collect()
+}
+
+/// Creates a
+
+/// Starts the reth pipeline with the given config, consensus, db, and fetch client.
+/// .... TODO: doc
+/// TODO: need to figure out where to put this test as it imports every part of the node.
+async fn start_reth(network: NetworkHandle) {
+    let _fetch_client = Arc::new(network.fetch_client().await.unwrap());
+    // let mut pipeline = reth_stages::Pipeline::default()
+    //     .with_sync_state_updater(network.clone())
+    //     .push(HeaderStage {
+    //         downloader: headers::linear::LinearDownloadBuilder::default()
+    //             .batch_size(config.stages.headers.downloader_batch_size)
+    //             .retries(config.stages.headers.downloader_retries)
+    //             .build(consensus.clone(), fetch_client.clone()),
+    //         consensus: consensus.clone(),
+    //         client: fetch_client.clone(),
+    //         network_handle: network.clone(),
+    //         commit_threshold: config.stages.headers.commit_threshold,
+    //         metrics: HeaderMetrics::default(),
+    //     })
+    //     .push(TotalDifficultyStage {
+    //         commit_threshold: config.stages.total_difficulty.commit_threshold,
+    //     })
+    //     .push(BodyStage {
+    //         downloader: Arc::new(
+    //             bodies::concurrent::ConcurrentDownloader::new(
+    //                 fetch_client.clone(),
+    //                 consensus.clone(),
+    //             )
+    //             .with_batch_size(config.stages.bodies.downloader_batch_size)
+    //             .with_retries(config.stages.bodies.downloader_retries)
+    //             .with_concurrency(config.stages.bodies.downloader_concurrency),
+    //         ),
+    //         consensus: consensus.clone(),
+    //         commit_threshold: config.stages.bodies.commit_threshold,
+    //     })
+    //     .push(SenderRecoveryStage {
+    //         batch_size: config.stages.sender_recovery.batch_size,
+    //         commit_threshold: config.stages.sender_recovery.commit_threshold,
+    //     })
+    //     .push(ExecutionStage {
+    //         config: ExecutorConfig::new_ethereum(),
+    //         commit_threshold: config.stages.execution.commit_threshold,
+    //     });
+
+    // if let Some(tip) = self.tip {
+    //     debug!("Tip manually set: {}", tip);
+    //     consensus.notify_fork_choice_state(ForkchoiceState {
+    //         head_block_hash: tip,
+    //         safe_block_hash: tip,
+    //         finalized_block_hash: tip,
+    //     })?;
+    // }
+
+    // // Run pipeline
+    // info!("Starting pipeline");
+    // pipeline.run(db.clone()).await?;
 }
 
 // copied from ethers-rs
@@ -654,7 +792,7 @@ async fn sync_from_clique_geth() {
         // create a pre-funded geth and turn on p2p
         let genesis = genesis_funded(chain_id, wallet.address());
         println!("genesis: {:#?}", genesis);
-        let geth = geth.genesis(genesis).chain_id(chain_id).disable_discovery().insecure_unlock();
+        let geth = geth.genesis(genesis.clone()).chain_id(chain_id).disable_discovery().insecure_unlock();
 
         // geth starts in dev mode, we can spawn it, mine blocks, and shut it down
         // we need to clone it because we will be reusing the geth config when we restart p2p
@@ -666,7 +804,7 @@ async fn sync_from_clique_geth() {
         let provider =
             SignerMiddleware::new_with_provider_chain(provider, wallet.clone()).await.unwrap();
 
-        // === produce blocks ===
+        // === start geth and produce blocks ===
 
         // first get the balance and make sure its not zero
         let balance = provider.get_balance(our_address, None).await.unwrap();
@@ -714,6 +852,7 @@ async fn sync_from_clique_geth() {
             }
         });
 
+        // TODO: remove
         // wait for stuff to happen (we are using period 1, so we expect about 10 blocks)
         tokio::time::sleep(Duration::from_secs(10)).await;
 
@@ -722,32 +861,38 @@ async fn sync_from_clique_geth() {
         println!("block num before restarting geth: {block}");
         assert!(block > U64::zero());
 
-        // TODO: start rest of reth - so we can test syncing
-        // can we make better test utils to simplify some of the initialization code?
-        // and reduce some of the reuse?
-
-        // === create a status ===
-
         // get genesis hash
         let genesis_block =
             provider.get_block(0).await.unwrap().expect("a genesis block should exist");
         let genesis_hash = genesis_block.hash.unwrap();
-        println!("genesis hash: {genesis_hash}");
+        println!("genesis hash from geth: {genesis_hash}");
 
-        // build the forkhash
+        // make sure it's the same thing we are calculating
+        let sealed_genesis = genesis_header(&genesis.clone()).seal();
+        println!("locally computed genesis: {}", sealed_genesis.hash());
 
-        // === network ===
+        // === initialize reth networking stack ===
 
         let secret_key = SecretKey::new(&mut rand::thread_rng());
 
         let (reth_p2p, reth_disc) = unused_tcp_udp();
+
+        // get correct status using genesis information
+        let status = extract_status(&genesis);
+
+        // may not need to set up a hello as we should be advertising compatible capabilities and
+        // protocol versions anyways
         let config = NetworkConfig::builder(Arc::new(NoopProvider::default()), secret_key)
             .listener_addr(reth_p2p)
             .discovery_addr(reth_disc)
+            .status(status)
             .build();
         let network = NetworkManager::new(config).await.unwrap();
 
         let handle = network.handle().clone();
+
+        // start reth then manually connect geth
+        tokio::task::spawn(start_reth(handle.clone()));
         tokio::task::spawn(network);
 
         // create networkeventstream to get the next session established event easily
@@ -763,16 +908,42 @@ async fn sync_from_clique_geth() {
         // add geth as a peer then wait for `PeerAdded` and `SessionEstablished` events.
         handle.add_peer(geth_peer_id, geth_socket);
 
-        match events.next().await {
-            Some(NetworkEvent::PeerAdded(peer_id)) => assert_eq!(peer_id, geth_peer_id),
-            _ => panic!("Expected a peer added event"),
+        match events.next().await.expect("the network should emit at least one event") {
+            // TODO: should the first event be a peeradded, and the second to be a
+            // sessionestablished?
+            // or should we just wait for a PeerAdded and SessionEstablished event?
+            // I guess, is there any relationship between the event variants and their ordering?
+            NetworkEvent::SessionEstablished { peer_id, .. } => assert_eq!(peer_id, geth_peer_id),
+            NetworkEvent::SessionClosed { peer_id, reason } => {
+                panic!("Expected a session established event, got a session closed event instead: {peer_id:?} {reason:?}")
+            },
+            NetworkEvent::PeerAdded(peer_id) => {
+                assert_eq!(peer_id, geth_peer_id);
+            },
+            NetworkEvent::PeerRemoved(peer_id) => {
+                panic!("The first event from the network should not be a peer removed event: {peer_id:?}")
+            },
         }
 
-        if let Some(NetworkEvent::SessionEstablished { peer_id, .. }) = events.next().await {
-            assert_eq!(peer_id, geth_peer_id);
-        } else {
-            panic!("Expected a session established event");
+        // see TODO above ^. assuming the first event we get is a PeerAdded event, the second
+        // should be a SessionEstablished event
+        // TODO: these matches seem not great, also can we log the pending session authentication
+        // error here somehow?
+        match events.next().await.expect("the network should emit a second event") {
+            NetworkEvent::SessionEstablished { peer_id, .. } => assert_eq!(peer_id, geth_peer_id),
+            NetworkEvent::SessionClosed { peer_id, reason } => {
+                panic!("Expected a session established event, got a session closed event instead: {peer_id:?} {reason:?}")
+            },
+            NetworkEvent::PeerAdded(peer_id) => {
+                panic!("The second event from the network should not be a peer added event: {peer_id:?}")
+            },
+            NetworkEvent::PeerRemoved(peer_id) => {
+                panic!("The second event from the network should not be a peer removed event: {peer_id:?}")
+            },
         }
+
+        // what kinds of events are we waiting for from reth? what kinds of assertions should we
+        // make?
     })
     .await
     .unwrap();
