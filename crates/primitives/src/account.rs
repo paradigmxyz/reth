617bc921d37ac153ca44dce0d3fc6b9b79c64224
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-01-06 16:36:19 -0500

    impl state root calc and genesis account encoding

--- crates/primitives/src/account.rs
+++ crates/primitives/src/account.rs
@@ -1,8 +1,15 @@
 use std::collections::HashMap;
 
-use crate::{Bytes, H256, U256};
+use crate::{
+    keccak256,
+    proofs::{KeccakHasher, EMPTY_ROOT},
+    Bytes, H256, KECCAK_EMPTY, U256,
+};
+use bytes::BytesMut;
 use reth_codecs::{main_codec, Compact};
+use reth_rlp::{length_of_length, Encodable, Header};
 use serde::{Deserialize, Serialize};
+use triehash::sec_trie_root;
 
 /// An Ethereum account.
 #[main_codec]
@@ -30,6 +37,49 @@ pub struct GenesisAccount {
     /// The account's storage at genesis.
     #[serde(flatten, skip_serializing_if = "Option::is_none")]
     pub storage: Option<HashMap<H256, H256>>,
+}
+
+impl GenesisAccount {
+    /// Determines the RLP payload length, without the RLP header.
+    fn payload_len(&self) -> usize {
+        let mut len = 0;
+        len += self.nonce.unwrap_or_default().length();
+        len += self.balance.length();
+        // rather than rlp-encoding the storage, we just return the length of a single hash
+        // hashes are a fixed size, so it is safe to use the empty root for this
+        len += EMPTY_ROOT.length();
+        len += self.code.as_ref().map_or(KECCAK_EMPTY, keccak256).length();
+        len
+    }
+}
+
+impl Encodable for GenesisAccount {
+    fn length(&self) -> usize {
+        let len = self.payload_len();
+        // RLP header length + payload length
+        len + length_of_length(len)
+    }
+    fn encode(&self, out: &mut dyn bytes::BufMut) {
+        let header = Header { list: true, payload_length: self.payload_len() };
+        header.encode(out);
+
+        self.nonce.unwrap_or_default().encode(out);
+        self.balance.encode(out);
+        self.storage
+            .as_ref()
+            .map_or(EMPTY_ROOT, |storage| {
+                let storage_values =
+                    storage.iter().filter(|(_k, &v)| v != KECCAK_EMPTY).map(|(&k, v)| {
+                        let mut value_rlp = BytesMut::new();
+                        v.encode(&mut value_rlp);
+                        (k, value_rlp.freeze())
+                    });
+
+                sec_trie_root::<KeccakHasher, _, _, _>(storage_values)
+            })
+            .encode(out);
+        self.code.as_ref().map_or(KECCAK_EMPTY, keccak256).encode(out);
+    }
 }
 
 impl Account {
