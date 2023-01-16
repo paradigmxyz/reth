a0a182aa8dfc6f379c32701ac773e14bfb3f3273
Ikechukwu Ahiara Marvellous <ahiaraikechukwu@gmail.com>
2023-01-16 20:04:07 +0100

    Add a --bootnodes flag for reth node (#867)

--- bin/reth/src/config.rs
+++ bin/reth/src/config.rs
@@ -6,7 +6,7 @@ use reth_network::{
     config::{mainnet_nodes, rng_secret_key},
     NetworkConfig, PeersConfig,
 };
-use reth_primitives::H256;
+use reth_primitives::{NodeRecord, H256};
 use reth_provider::ProviderImpl;
 use serde::{Deserialize, Serialize};
 
@@ -29,12 +29,13 @@ impl Config {
         chain_id: u64,
         genesis_hash: H256,
         disable_discovery: bool,
+        bootnodes: &Option<Vec<NodeRecord>>,
     ) -> NetworkConfig<ProviderImpl<DB>> {
         let peer_config = reth_network::PeersConfig::default()
             .with_trusted_nodes(self.peers.trusted_nodes.clone())
             .with_connect_trusted_nodes_only(self.peers.connect_trusted_nodes_only);
         NetworkConfig::builder(Arc::new(ProviderImpl::new(db)), rng_secret_key())
-            .boot_nodes(mainnet_nodes())
+            .boot_nodes(bootnodes.unwrap_or_else(|| mainnet_nodes()))
             .peer_config(peer_config)
             .genesis_hash(genesis_hash)
             .chain_id(chain_id)
