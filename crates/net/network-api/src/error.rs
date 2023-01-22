46c7cc86548a08cfb4cdfce4170cda29836b6847
Ikechukwu Ahiara Marvellous <ahiaraikechukwu@gmail.com>
2023-01-22 20:59:50 +0100

    Create Network variant in reth_interfaces::Error (#950)

---
+++ crates/net/network-api/src/error.rs
@@ -0,0 +1,22 @@
+use thiserror::Error;
+use tokio::sync::{mpsc, oneshot};
+
+/// Network Errors
+#[allow(missing_docs)]
+#[derive(Error, Debug, Clone, PartialEq, Eq)]
+pub enum NetworkError {
+    #[error("Sender has been dropped")]
+    ChannelClosed,
+}
+
+impl<T> From<mpsc::error::SendError<T>> for NetworkError {
+    fn from(_: mpsc::error::SendError<T>) -> Self {
+        NetworkError::ChannelClosed
+    }
+}
+
+impl From<oneshot::error::RecvError> for NetworkError {
+    fn from(_: oneshot::error::RecvError) -> Self {
+        NetworkError::ChannelClosed
+    }
+}
--- crates/net/rpc/src/result.rs
+++ crates/net/rpc/src/result.rs
@@ -1,6 +1,7 @@
 //! Additional helpers for converting errors.
 
 use jsonrpsee::core::{Error as RpcError, RpcResult};
+use reth_network_api::NetworkError;
 
 /// Helper trait to easily convert various `Result` types into [`RpcResult`]
 pub(crate) trait ToRpcResult<Ok, Err> {
@@ -62,6 +63,61 @@ impl<Ok> ToRpcResult<Ok, reth_interfaces::Error> for reth_interfaces::Result<Ok>
     fn map_internal_err_with_data<'a, F, M>(self, op: F) -> RpcResult<Ok>
     where
         F: FnOnce(reth_interfaces::Error) -> (M, &'a [u8]),
+        M: Into<String>,
+    {
+        match self {
+            Ok(t) => Ok(t),
+            Err(err) => {
+                let (msg, data) = op(err);
+                Err(internal_rpc_err_with_data(msg, data))
+            }
+        }
+    }
+
+    #[inline]
+    fn with_message(self, msg: &str) -> RpcResult<Ok> {
+        match self {
+            Ok(t) => Ok(t),
+            Err(err) => {
+                let msg = format!("{msg}: {err:?}");
+                Err(internal_rpc_err(msg))
+            }
+        }
+    }
+}
+
+impl<Ok> ToRpcResult<Ok, NetworkError> for Result<Ok, NetworkError> {
+    #[inline]
+    fn map_rpc_err<'a, F, M>(self, op: F) -> RpcResult<Ok>
+    where
+        F: FnOnce(NetworkError) -> (i32, M, Option<&'a [u8]>),
+        M: Into<String>,
+    {
+        match self {
+            Ok(t) => Ok(t),
+            Err(err) => {
+                let (code, msg, data) = op(err);
+                Err(rpc_err(code, msg, data))
+            }
+        }
+    }
+
+    #[inline]
+    fn map_internal_err<'a, F, M>(self, op: F) -> RpcResult<Ok>
+    where
+        F: FnOnce(NetworkError) -> M,
+        M: Into<String>,
+    {
+        match self {
+            Ok(t) => Ok(t),
+            Err(err) => Err(internal_rpc_err(op(err))),
+        }
+    }
+
+    #[inline]
+    fn map_internal_err_with_data<'a, F, M>(self, op: F) -> RpcResult<Ok>
+    where
+        F: FnOnce(NetworkError) -> (M, &'a [u8]),
         M: Into<String>,
     {
         match self {
