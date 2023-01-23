6f047a5de0c3b8ebfad5033b0139856a98cd0955
Aurélien <aurelien.catinon@gmail.com>
2023-01-23 22:53:15 +0100

    feat: add Web3 namespace RPC handler (#990)

--- crates/net/rpc-api/src/web3.rs
+++ crates/net/rpc-api/src/web3.rs
@@ -4,10 +4,11 @@ use reth_primitives::{Bytes, H256};
 /// Web3 rpc interface.
 #[cfg_attr(not(feature = "client"), rpc(server))]
 #[cfg_attr(feature = "client", rpc(server, client))]
+#[async_trait::async_trait]
 pub trait Web3Api {
     /// Returns current client version.
     #[method(name = "web3_clientVersion")]
-    fn client_version(&self) -> Result<String>;
+    async fn client_version(&self) -> Result<String>;
 
     /// Returns sha3 of the given data.
     #[method(name = "web3_sha3")]
