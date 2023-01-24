167aa60ed20ec25645722f624ea5151e6acd0ca3
Matthias Seitz <matthias.seitz@outlook.de>
2023-01-24 12:14:50 +0100

    chore(deps): rm unused deps in downloaders (#1004)

--- crates/net/downloaders/src/lib.rs
+++ crates/net/downloaders/src/lib.rs
@@ -1,4 +1,4 @@
-#![warn(missing_docs, unreachable_pub)]
+#![warn(missing_docs, unreachable_pub, unused_crate_dependencies)]
 #![deny(unused_must_use, rust_2018_idioms)]
 #![doc(test(
     no_crate_inject,
@@ -10,7 +10,7 @@
 /// The collection of algorithms for downloading block bodies.
 pub mod bodies;
 
-/// The collection of alhgorithms for downloading block headers.
+/// The collection of algorithms for downloading block headers.
 pub mod headers;
 
 #[cfg(test)]
