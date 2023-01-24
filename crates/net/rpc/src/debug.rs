d74ae6fe154b269c9b4b742f80663de612b21f2b
TurboFish <87058147+CryptoTurboFish@users.noreply.github.com>
2023-01-24 02:19:28 -0800

    add DebugApi struct, functions to be implemented (#1000)

---
+++ crates/net/rpc/src/debug.rs
@@ -0,0 +1,41 @@
+use async_trait::async_trait;
+use jsonrpsee::core::RpcResult as Result;
+use reth_primitives::{rpc::BlockId, Bytes, H256};
+use reth_rpc_api::DebugApiServer;
+use reth_rpc_types::RichBlock;
+
+/// `debug` API implementation.
+///
+/// This type provides the functionality for handling `debug` related requests.
+pub struct DebugApi {
+    // TODO: add proper handler when implementing functions
+}
+
+#[async_trait]
+impl DebugApiServer for DebugApi {
+    async fn raw_header(&self, _block_id: BlockId) -> Result<Bytes> {
+        todo!()
+    }
+
+    async fn raw_block(&self, _block_id: BlockId) -> Result<Bytes> {
+        todo!()
+    }
+
+    async fn raw_transaction(&self, _hash: H256) -> Result<Bytes> {
+        todo!()
+    }
+
+    async fn raw_receipts(&self, _block_id: BlockId) -> Result<Vec<Bytes>> {
+        todo!()
+    }
+
+    async fn bad_blocks(&self) -> Result<Vec<RichBlock>> {
+        todo!()
+    }
+}
+
+impl std::fmt::Debug for DebugApi {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("DebugApi").finish_non_exhaustive()
+    }
+}
