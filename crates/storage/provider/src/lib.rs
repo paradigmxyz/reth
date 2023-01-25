551d72831c9a6297e379910510bd0ec4920bf313
Roman Krasiuk <rokrassyuk@gmail.com>
2023-01-25 13:06:08 +0200

    provider crate description

--- crates/storage/provider/src/lib.rs
+++ crates/storage/provider/src/lib.rs
@@ -5,7 +5,8 @@
     attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
 ))]
 
-//! <reth crate template>
+//! This crate contains a collection of traits and trait implementations for common database
+//! operations.
 
 /// Various provider traits.
 mod traits;
