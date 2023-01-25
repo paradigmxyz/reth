c2e05b9f67449d2ed507ff9918d6f331d8c757f1
Roman Krasiuk <rokrassyuk@gmail.com>
2023-01-25 12:22:27 +0200

    fix docs and add example

--- crates/net/downloaders/src/metrics.rs
+++ crates/net/downloaders/src/metrics.rs
@@ -2,25 +2,37 @@ use metrics::Counter;
 use reth_interfaces::p2p::error::DownloadError;
 use reth_metrics_derive::Metrics;
 
-/// The header downloader metrics.
+/// Common downloader metrics.
+///
+/// These metrics will be dynamically initialized with the provided scope
+/// by corresponding downloaders.
+/// ```
+/// use reth_downloaders::metrics::DownloaderMetrics;
+/// use reth_interfaces::p2p::error::DownloadError;
+///
+/// // Initialized metrics.
+/// let metrics = DownloaderMetrics::new("downloaders.headers");
+/// // Increment `downloaders.headers.timeout_errors` counter by 1.
+/// metrics.increment_errors(&DownloadError::Timeout);
+/// ```
 #[derive(Clone, Metrics)]
 #[metrics(dynamic = true)]
 pub struct DownloaderMetrics {
     /// The number of items that were successfully sent to the poller (stage)
-    pub(crate) total_flushed: Counter,
+    pub total_flushed: Counter,
     /// Number of items that were successfully downloaded
-    pub(crate) total_downloaded: Counter,
+    pub total_downloaded: Counter,
     /// Number of timeout errors while requesting items
-    pub(crate) timeout_errors: Counter,
+    pub timeout_errors: Counter,
     /// Number of validation errors while requesting items
-    pub(crate) validation_errors: Counter,
+    pub validation_errors: Counter,
     /// Number of unexpected errors while requesting items
-    pub(crate) unexpected_errors: Counter,
+    pub unexpected_errors: Counter,
 }
 
 impl DownloaderMetrics {
     /// Increment errors counter.
-    pub(crate) fn increment_errors(&self, error: &DownloadError) {
+    pub fn increment_errors(&self, error: &DownloadError) {
         match error {
             DownloadError::Timeout => self.timeout_errors.increment(1),
             DownloadError::HeaderValidation { .. } | DownloadError::BodyValidation { .. } => {
