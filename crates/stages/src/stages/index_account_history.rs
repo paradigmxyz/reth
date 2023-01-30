6f89ad086b2242897fdf26569c11c2bb79afbfb3
Roman Krasiuk <rokrassyuk@gmail.com>
2023-01-30 13:36:39 +0200

    chore(sync): consistent stage ids (#1085)

--- crates/stages/src/stages/index_account_history.rs
+++ crates/stages/src/stages/index_account_history.rs
@@ -14,7 +14,7 @@ use reth_primitives::{Address, TransitionId};
 use std::{collections::BTreeMap, fmt::Debug};
 use tracing::*;
 
-const INDEX_ACCOUNT_HISTORY: StageId = StageId("IndexAccountHistoryStage");
+const INDEX_ACCOUNT_HISTORY: StageId = StageId("IndexAccountHistory");
 
 /// Stage is indexing history the account changesets generated in
 /// [`ExecutionStage`][crate::stages::ExecutionStage]. For more information
