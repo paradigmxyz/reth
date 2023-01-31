21723f4e23aaa5e052a0cbf90c7d5ca4da658049
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-01-31 16:52:07 -0500

    create downloaders and init pipeline

--- bin/reth/src/chain/file_client.rs
+++ bin/reth/src/chain/file_client.rs
@@ -1,11 +1,21 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    path::Path,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
 
 use reth_eth_wire::BlockBody;
-use reth_interfaces::p2p::{
-    bodies::client::{BodiesClient, BodiesFut},
-    download::DownloadClient,
-    headers::client::{HeadersClient, HeadersFut, HeadersRequest},
-    priority::Priority,
+use reth_interfaces::{
+    p2p::{
+        bodies::client::{BodiesClient, BodiesFut},
+        download::DownloadClient,
+        headers::client::{HeadersClient, HeadersFut, HeadersRequest},
+        priority::Priority,
+    },
+    sync::{SyncState, SyncStateProvider, SyncStateUpdater},
 };
 use reth_primitives::{BlockHash, BlockNumber, Header, PeerId, H256};
 use tokio::{fs::File, io::BufReader};
@@ -32,15 +42,23 @@ pub struct FileClient {
 
     /// The buffered bodies retrieved when fetching new headers.
     bodies: HashMap<BlockHash, BlockBody>,
+
+    /// Represents if we are currently syncing.
+    is_syncing: Arc<AtomicBool>,
 }
 
 impl FileClient {
     /// Create a new file client from a file path.
-    pub async fn new(path: &str) -> Result<Self, std::io::Error> {
+    pub async fn new<P: AsRef<Path>>(path: P) -> Result<Self, std::io::Error> {
         let file = File::open(path).await?;
         let reader = BufReader::new(file);
 
-        Ok(Self { reader, headers: HashMap::new(), bodies: HashMap::new() })
+        Ok(Self {
+            reader,
+            headers: HashMap::new(),
+            bodies: HashMap::new(),
+            is_syncing: Arc::new(Default::default()),
+        })
     }
 }
 
@@ -52,6 +70,9 @@ impl HeadersClient for FileClient {
         request: HeadersRequest,
         _priority: Priority,
     ) -> Self::Output {
+        // first search the buffer
+
+        // then read from the file if not found
         todo!()
     }
 }
@@ -64,18 +85,34 @@ impl BodiesClient for FileClient {
         hashes: Vec<H256>,
         _priority: Priority,
     ) -> Self::Output {
+        // first search the buffer
+
+        // then read from the file if not found
         todo!()
     }
 }
 
 impl DownloadClient for FileClient {
     fn report_bad_message(&self, _peer_id: PeerId) {
-        // this should never happen? but we should return an error of some sort
+        // this should never happen? but we should report an error of some sort
         todo!()
     }
 
     fn num_connected_peers(&self) -> usize {
         // no such thing as connected peers when we are just using a file
         1
+    }
+}
+
+impl SyncStateProvider for FileClient {
+    fn is_syncing(&self) -> bool {
+        self.is_syncing.load(Ordering::Relaxed)
+    }
+}
+
+impl SyncStateUpdater for FileClient {
+    fn update_sync_state(&self, state: SyncState) {
+        let is_syncing = state.is_syncing();
+        self.is_syncing.store(is_syncing, Ordering::Relaxed)
     }
 }
--- bin/reth/src/chain/import.rs
+++ bin/reth/src/chain/import.rs
@@ -1,44 +1,187 @@
+use crate::{
+    chain::FileClient,
+    dirs::{ConfigPath, DbPath, ImportPath, PlatformPath},
+    node::handle_events,
+    utils::chainspec::chain_spec_value_parser,
+};
 use clap::Parser;
-use eyre::WrapErr;
-use reth_primitives::Block;
-use std::path::PathBuf;
+use eyre::Context;
+use futures::StreamExt;
+use reth_consensus::BeaconConsensus;
+use reth_db::mdbx::{Env, WriteMap};
+use reth_downloaders::{bodies, headers};
+use reth_interfaces::consensus::Consensus;
+use reth_primitives::ChainSpec;
+use reth_staged_sync::{
+    utils::init::{init_db, init_genesis},
+    Config,
+};
+use reth_stages::{
+    sets::{OfflineStages, OnlineStages},
+    stages::{ExecutionStage, SenderRecoveryStage, TotalDifficultyStage},
+    Pipeline, StageSet,
+};
+use std::sync::Arc;
+use tracing::info;
 
 #[derive(Debug, Parser)]
 pub struct ImportCommand {
+    /// The path to the configuration file to use.
+    #[arg(long, value_name = "FILE", verbatim_doc_comment, default_value_t)]
+    config: PlatformPath<ConfigPath>,
+
+    /// The path to the database folder.
+    ///
+    /// Defaults to the OS-specific data directory:
+    ///
+    /// - Linux: `$XDG_DATA_HOME/reth/db` or `$HOME/.local/share/reth/db`
+    /// - Windows: `{FOLDERID_RoamingAppData}/reth/db`
+    /// - macOS: `$HOME/Library/Application Support/reth/db`
+    #[arg(long, value_name = "PATH", verbatim_doc_comment, default_value_t)]
+    db: PlatformPath<DbPath>,
+
+    /// The chain this node is running.
+    ///
+    /// Possible values are either a built-in chain or the path to a chain specification file.
+    ///
+    /// Built-in chains:
+    /// - mainnet
+    /// - goerli
+    /// - sepolia
+    #[arg(
+        long,
+        value_name = "CHAIN_OR_PATH",
+        verbatim_doc_comment,
+        default_value = "mainnet",
+        value_parser = chain_spec_value_parser
+    )]
+    chain: ChainSpec,
+
     /// The file to import.
-    path: PathBuf,
+    #[arg(long, value_name = "IMPORT_PATH", verbatim_doc_comment, default_value_t)]
+    path: PlatformPath<ImportPath>,
 }
 
 impl ImportCommand {
     pub async fn execute(&self) -> eyre::Result<()> {
-        // read file
-        let mut contents = &std::fs::read(&self.path).wrap_err("Could not open file")?[..];
+        info!(target: "reth::cli", "reth import starting");
 
-        // open or create db
-        let mut imported = 0;
-        while !contents.is_empty() {
-            match <Block as reth_rlp::Decodable>::decode(&mut contents) {
-                Ok(block) => {
-                    println!("{:?}", block.header.hash_slow());
-                    imported += 1;
-                }
-                Err(err) => {
-                    println!("Error: {:?}", err);
-                    break
-                }
-            }
-            // do what headers does
-            // do what bodies does
-            // 1. check if we have the block
-            // 2. if we do, error out
-            // 3. check if the block connects
-            // 4. if not, error out
-            // 5. otherwise, import the block
-        }
+        let config: Config = self.load_config()?;
+        info!(target: "reth::cli", path = %self.db, "Configuration loaded");
 
-        // execute all stages
+        info!(target: "reth::cli", path = %self.db, "Opening database");
+        let db = Arc::new(init_db(&self.db)?);
+        info!(target: "reth::cli", "Database opened");
 
-        println!("Imported {imported} blocks");
+        // self.start_metrics_endpoint()?;
+
+        init_genesis(db.clone(), self.chain.clone())?;
+
+        let consensus = self.init_consensus()?;
+        info!(target: "reth::cli", "Consensus engine initialized");
+
+        // create a new FileClient
+        let file_client = Arc::new(FileClient::new(&self.path).await?);
+
+        // construct downloaders and start pipeline
+        let mut pipeline = self.build_pipeline(&config, &file_client, &consensus, &db).await?;
+
+        tokio::spawn(handle_events(pipeline.events().map(Into::into)));
+
+        // Run pipeline
+        info!(target: "reth::cli", "Starting sync pipeline");
+        pipeline.run(db.clone()).await?;
+
+        info!(target: "reth::cli", "Finishing up");
         Ok(())
+    }
+
+    async fn build_pipeline(
+        &self,
+        config: &Config,
+        file_client: &Arc<FileClient>,
+        consensus: &Arc<dyn Consensus>,
+        db: &Arc<Env<WriteMap>>,
+    ) -> eyre::Result<Pipeline<Env<WriteMap>, Arc<FileClient>>> {
+        let header_downloader =
+            self.spawn_headers_downloader(config, consensus, &file_client.clone());
+        let body_downloader =
+            self.spawn_bodies_downloader(config, consensus, &file_client.clone(), db);
+        let stage_conf = &config.stages;
+
+        let pipeline = Pipeline::builder()
+            .with_sync_state_updater(file_client.clone())
+            .add_stages(
+                OnlineStages::new(consensus.clone(), header_downloader, body_downloader).set(
+                    TotalDifficultyStage {
+                        commit_threshold: stage_conf.total_difficulty.commit_threshold,
+                    },
+                ),
+            )
+            .add_stages(
+                OfflineStages::default()
+                    .set(SenderRecoveryStage {
+                        batch_size: stage_conf.sender_recovery.batch_size,
+                        commit_threshold: stage_conf.execution.commit_threshold,
+                    })
+                    .set(ExecutionStage {
+                        chain_spec: self.chain.clone(),
+                        commit_threshold: stage_conf.execution.commit_threshold,
+                    }),
+            )
+            .build();
+
+        Ok(pipeline)
+    }
+
+    fn init_consensus(&self) -> eyre::Result<Arc<dyn Consensus>> {
+        let beacon_consensus = Arc::new(BeaconConsensus::new(self.chain.clone()));
+
+        // TODO: the node command requires a tip update, so we need to figure out how to do that
+        // with only a file
+        todo!()
+    }
+
+    fn spawn_headers_downloader(
+        &self,
+        config: &Config,
+        consensus: &Arc<dyn Consensus>,
+        file_client: &Arc<FileClient>,
+    ) -> reth_downloaders::headers::task::TaskDownloader {
+        // TODO: how to deal with the fact that this is a reverse downloader, and the blocks are
+        // written forwards in the file? RLP can only be parsed forwards
+        // do we need a forward downloader?
+        let headers_conf = &config.stages.headers;
+        headers::task::TaskDownloader::spawn(
+            headers::linear::LinearDownloadBuilder::default()
+                .request_limit(headers_conf.downloader_batch_size)
+                .stream_batch_size(headers_conf.commit_threshold as usize)
+                .build(consensus.clone(), file_client.clone()),
+        )
+    }
+
+    fn spawn_bodies_downloader(
+        &self,
+        config: &Config,
+        consensus: &Arc<dyn Consensus>,
+        file_client: &Arc<FileClient>,
+        db: &Arc<Env<WriteMap>>,
+    ) -> reth_downloaders::bodies::task::TaskDownloader {
+        let bodies_conf = &config.stages.bodies;
+        bodies::task::TaskDownloader::spawn(
+            bodies::concurrent::ConcurrentDownloaderBuilder::default()
+                .with_stream_batch_size(bodies_conf.downloader_stream_batch_size)
+                .with_request_limit(bodies_conf.downloader_request_limit)
+                .with_max_buffered_responses(bodies_conf.downloader_max_buffered_responses)
+                .with_concurrent_requests_range(
+                    bodies_conf.downloader_min_concurrent_requests..=
+                        bodies_conf.downloader_max_concurrent_requests,
+                )
+                .build(file_client.clone(), consensus.clone(), db.clone()),
+        )
+    }
+
+    fn load_config(&self) -> eyre::Result<Config> {
+        confy::load_path::<Config>(&self.config).wrap_err("Could not load config")
     }
 }
