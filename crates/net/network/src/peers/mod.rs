67be80cfeadc2c6a4df0a0dda365fe3ca22da6ed
Matthias Seitz <matthias.seitz@outlook.de>
2023-02-03 15:02:31 +0100

    perf(net): increase default capacity for incoming messages

--- crates/net/network/src/peers/mod.rs
+++ crates/net/network/src/peers/mod.rs
@@ -7,3 +7,9 @@ pub(crate) use manager::{InboundConnectionError, PeerAction, PeersManager};
 pub use manager::{PeersConfig, PeersHandle};
 pub use reputation::ReputationChangeWeights;
 pub use reth_network_api::PeerKind;
+
+/// Maximum number of available slots for outbound sessions.
+pub(crate) const DEFAULT_MAX_PEERS_OUTBOUND: usize = 100;
+
+/// Maximum number of available slots for inbound sessions.
+pub(crate) const DEFAULT_MAX_PEERS_INBOUND: usize = 30;
