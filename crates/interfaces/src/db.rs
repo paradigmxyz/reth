0721ec2f888e9816e00fc49e8420e54edb7225c8
Tomás <tomas.gruner@lambdaclass.com>
2023-02-08 17:22:08 -0300

    Make db::Error wrap libmdbx-rs Error
    Co-authored-by: lambdaclass-user <github@lambdaclass.com>

--- crates/interfaces/src/db.rs
+++ crates/interfaces/src/db.rs
@@ -1,30 +1,32 @@
+use reth_libmdbx::Error as MdbxError;
+
 /// Database error type. They are using u32 to represent error code.
 #[derive(Debug, thiserror::Error, PartialEq, Eq, Clone)]
 pub enum Error {
     /// Failed to open database.
-    #[error("{0:?}")]
-    DatabaseLocation(i32),
+    #[error("{0}")]
+    DatabaseLocation(MdbxError),
     /// Failed to create a table in database.
-    #[error("Table Creating error code: {0:?}")]
-    TableCreation(i32),
+    #[error("Table Creating error code: {0}")]
+    TableCreation(MdbxError),
     /// Failed to insert a value into a table.
-    #[error("Database write error code: {0:?}")]
-    Write(i32),
+    #[error("Database write error code: {0}")]
+    Write(MdbxError),
     /// Failed to get a value into a table.
-    #[error("Database read error code: {0:?}")]
-    Read(i32),
+    #[error("Database read error code: {0}")]
+    Read(MdbxError),
     /// Failed to delete a `(key, value)` pair into a table.
-    #[error("Database delete error code: {0:?}")]
-    Delete(i32),
+    #[error("Database delete error code: {0}")]
+    Delete(MdbxError),
     /// Failed to commit transaction changes into the database.
-    #[error("Database commit error code: {0:?}")]
-    Commit(i32),
+    #[error("Database commit error code: {0}")]
+    Commit(MdbxError),
     /// Failed to initiate a transaction.
-    #[error("Initialization of transaction errored with code: {0:?}")]
-    InitTransaction(i32),
+    #[error("Initialization of transaction errored with code: {0}")]
+    InitTransaction(MdbxError),
     /// Failed to initiate a cursor.
-    #[error("Initialization of cursor errored with code: {0:?}")]
-    InitCursor(i32),
+    #[error("Initialization of cursor errored with code: {0}")]
+    InitCursor(MdbxError),
     /// Failed to decode a key from a table..
     #[error("Error decoding value.")]
     DecodeError,
