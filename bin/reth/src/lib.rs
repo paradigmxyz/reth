666a588b690219048bdee0f63a7de9344473322d
literallymarvellous <ahiaraikechukwu@gmail.com>
2023-02-08 14:17:41 +0100

    refactor: RpcServerOpts args changes

--- bin/reth/src/lib.rs
+++ bin/reth/src/lib.rs
@@ -15,7 +15,9 @@ pub mod prometheus_exporter;
 pub mod stage;
 pub mod test_eth_chain;
 pub mod test_vectors;
-use reth_rpc_builder::RethRpcModule;
+use std::net::IpAddr;
+
+use reth_rpc_builder::{RethRpcModule, RpcModuleConfig};
 pub use reth_staged_sync::utils;
 
 use clap::Args;
@@ -49,33 +51,43 @@ struct NetworkOpts {
 #[derive(Debug, Args)]
 #[command(next_help_heading = "Rpc")]
 struct RpcServerOpts {
+    /// Enable the HTTP-RPC server
     #[arg(long, default_value = "true")]
     http: bool,
 
+    /// Http server address to listen on
     #[arg(long = "http.addr")]
-    http_addr: Option<String>,
+    http_addr: Option<IpAddr>,
 
+    /// Http server port to listen on
     #[arg(long = "http.port")]
-    http_port: Option<u128>,
+    http_port: Option<u16>,
 
+    /// Rpc Modules to be configured for http server
     #[arg(long = "http.api", value_delimiter = ',')]
-    http_api: Option<Vec<RethRpcModule>>,
+    http_api: Option<RpcModuleConfig>,
 
+    /// Enable the WS-RPC server
     #[arg(long, default_value = "true")]
     ws: bool,
 
+    /// Ws server address to listen on
     #[arg(long = "ws.addr")]
-    ws_addr: Option<String>,
+    ws_addr: Option<IpAddr>,
 
+    /// Http server port to listen on
     #[arg(long = "ws.port")]
-    ws_port: Option<u128>,
+    ws_port: Option<u16>,
 
+    /// Rpc Modules to be configured for Ws server
     #[arg(long = "ws.api", value_delimiter = ',')]
-    ws_api: Option<Vec<RethRpcModule>>,
+    ws_api: Option<RpcModuleConfig>,
 
+    /// Disable the IPC-RPC  server
     #[arg(long, default_value = "true")]
     ipcdisable: bool,
 
+    /// Filename for IPC socket/pipe within the datadir
     #[arg(long)]
     ipcpath: Option<String>,
 }
