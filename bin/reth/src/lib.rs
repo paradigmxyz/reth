7e5554bf95e68bdf4f27ec9cda0115f7656b06fc
literallymarvellous <ahiaraikechukwu@gmail.com>
2023-02-08 13:25:28 +0100

    add: RpcServerOpts

--- bin/reth/src/lib.rs
+++ bin/reth/src/lib.rs
@@ -15,6 +15,7 @@ pub mod prometheus_exporter;
 pub mod stage;
 pub mod test_eth_chain;
 pub mod test_vectors;
+use reth_rpc_builder::RethRpcModule;
 pub use reth_staged_sync::utils;
 
 use clap::Args;
@@ -42,4 +43,39 @@ struct NetworkOpts {
     /// Will fall back to a network-specific default if not specified.
     #[arg(long, value_delimiter = ',')]
     bootnodes: Option<Vec<NodeRecord>>,
+}
+
+/// Parameters for configuring the network more granularly via CLI
+#[derive(Debug, Args)]
+#[command(next_help_heading = "Rpc")]
+struct RpcServerOpts {
+    #[arg(long, default_value = "true")]
+    http: bool,
+
+    #[arg(long = "http.addr")]
+    http_addr: Option<String>,
+
+    #[arg(long = "http.port")]
+    http_port: Option<u128>,
+
+    #[arg(long = "http.api", value_delimiter = ',')]
+    http_api: Option<Vec<RethRpcModule>>,
+
+    #[arg(long, default_value = "true")]
+    ws: bool,
+
+    #[arg(long = "ws.addr")]
+    ws_addr: Option<String>,
+
+    #[arg(long = "ws.port")]
+    ws_port: Option<u128>,
+
+    #[arg(long = "ws.api", value_delimiter = ',')]
+    ws_api: Option<Vec<RethRpcModule>>,
+
+    #[arg(long, default_value = "true")]
+    ipcdisable: bool,
+
+    #[arg(long)]
+    ipcpath: Option<String>,
 }
