810b01eb677bb2ef641b211f897fb63da071330c
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-02-09 01:01:04 -0500

    return copied u64 from transaction chain_id

--- crates/primitives/src/transaction/mod.rs
+++ crates/primitives/src/transaction/mod.rs
@@ -182,11 +182,11 @@ impl Transaction {
     }
 
     /// Get chain_id.
-    pub fn chain_id(&self) -> Option<&u64> {
+    pub fn chain_id(&self) -> Option<u64> {
         match self {
-            Transaction::Legacy(TxLegacy { chain_id, .. }) => chain_id.as_ref(),
-            Transaction::Eip2930(TxEip2930 { chain_id, .. }) => Some(chain_id),
-            Transaction::Eip1559(TxEip1559 { chain_id, .. }) => Some(chain_id),
+            Transaction::Legacy(TxLegacy { chain_id, .. }) => *chain_id,
+            Transaction::Eip2930(TxEip2930 { chain_id, .. }) => Some(*chain_id),
+            Transaction::Eip1559(TxEip1559 { chain_id, .. }) => Some(*chain_id),
         }
     }
 
@@ -735,7 +735,7 @@ impl proptest::arbitrary::Arbitrary for TransactionSigned {
 
         any::<(Transaction, Signature)>()
             .prop_map(move |(mut transaction, sig)| {
-                if let Some(chain_id) = transaction.chain_id().cloned() {
+                if let Some(chain_id) = transaction.chain_id() {
                     // Otherwise we might overflow when calculating `v` on `recalculate_hash`
                     transaction.set_chain_id(chain_id % (u64::MAX / 2 - 36));
                 }
@@ -752,7 +752,7 @@ impl proptest::arbitrary::Arbitrary for TransactionSigned {
 impl<'a> arbitrary::Arbitrary<'a> for TransactionSigned {
     fn arbitrary(u: &mut arbitrary::Unstructured<'a>) -> arbitrary::Result<Self> {
         let mut transaction = Transaction::arbitrary(u)?;
-        if let Some(chain_id) = transaction.chain_id().cloned() {
+        if let Some(chain_id) = transaction.chain_id() {
             // Otherwise we might overflow when calculating `v` on `recalculate_hash`
             transaction.set_chain_id(chain_id % (u64::MAX / 2 - 36));
         }
