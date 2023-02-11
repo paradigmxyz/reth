a681bdff95bddfb97c1ed1ec03b1e4b90e080692
navie <naviechan@gmail.com>
2023-02-11 20:06:45 +0800

    Added partial ETH tests

--- crates/rpc/rpc-builder/tests/it/http.rs
+++ crates/rpc/rpc-builder/tests/it/http.rs
@@ -2,9 +2,20 @@
 
 use crate::utils::{launch_http, launch_http_ws, launch_ws};
 use jsonrpsee::core::client::{ClientT, SubscriptionClientT};
-use reth_primitives::NodeRecord;
+use reth_primitives::{NodeRecord, H256, BlockNumber, U256, H64};
 use reth_rpc_api::{clients::AdminApiClient, EthApiClient};
 use reth_rpc_builder::RethRpcModule;
+use jsonrpsee::core::error::Error;
+use jsonrpsee::types::error::{CallError, ErrorCode};
+
+fn is_unimplemented(err: Error) -> bool {
+    match err {
+        Error::Call(CallError::Custom(error_obj)) => {
+            error_obj.code() == ErrorCode::InternalError.code() && error_obj.message() == "unimplemented"
+        }
+        _ => return false,
+    }
+}
 
 async fn test_basic_admin_calls<C>(client: &C)
 where
@@ -23,10 +34,50 @@ where
 async fn test_basic_eth_calls<C>(client: &C)
 where C: ClientT + SubscriptionClientT + Sync {
 
+    // Implemented
     EthApiClient::protocol_version(client).await.unwrap();
-    EthApiClient::syncing(client).await.unwrap();
-    EthApiClient::author(client).await.unwrap();
-    panic!();
+    EthApiClient::chain_id(client).await.unwrap();
+    EthApiClient::chain_id(client).await.unwrap();
+    EthApiClient::accounts(client).await.unwrap();
+    EthApiClient::block_number(client).await.unwrap();
+
+
+    // Unimplemented
+    assert!(is_unimplemented(EthApiClient::syncing(client).await.err().unwrap()));
+    EthApiClient::author(client).await.unwrap_err();
+    EthApiClient::block_by_hash(client, H256::default(), false).await.unwrap_err();
+    EthApiClient::block_by_number(client, BlockNumber::default(), false).await.unwrap_err();
+    EthApiClient::block_transaction_count_by_hash(client, H256::default()).await.unwrap_err();
+    EthApiClient::block_transaction_count_by_number(client, BlockNumber::default()).await.unwrap_err();
+    EthApiClient::block_uncles_count_by_hash(client, H256::default()).await.unwrap_err();
+    EthApiClient::block_uncles_count_by_number(client, BlockNumber::default()).await.unwrap_err();
+    // EthApiClient::uncle_by_block_hash_and_index(client, H256::default(), Index::default()).await.unwrap_err();
+    // EthApiClient::uncle_by_block_number_and_index(client).await.unwrap_err();
+    // EthApiClient::transaction_by_hash(client, H256::default()).await.unwrap_err();
+    // EthApiClient::transaction_by_block_hash_and_index(client).await.unwrap_err();
+    // EthApiClient::transaction_by_block_number_and_index(client).await.unwrap_err();
+    EthApiClient::transaction_receipt(client, H256::default()).await.unwrap_err();
+    // EthApiClient::balance(client).await.unwrap_err();
+    // EthApiClient::storage_at(client).await.unwrap_err();
+    // EthApiClient::transaction_count(client).await.unwrap_err();
+    // EthApiClient::get_code(client).await.unwrap_err();
+    // EthApiClient::call(client).await.unwrap_err();
+    // EthApiClient::create_access_list(client).await.unwrap_err();
+    // EthApiClient::estimate_gas(client).await.unwrap_err();
+    EthApiClient::gas_price(client).await.unwrap_err();
+    EthApiClient::fee_history(client, U256::default(), BlockNumber::default(), None).await.unwrap_err();
+    EthApiClient::max_priority_fee_per_gas(client).await.unwrap_err();
+    EthApiClient::is_mining(client).await.unwrap_err();
+    EthApiClient::hashrate(client).await.unwrap_err();
+    EthApiClient::get_work(client).await.unwrap_err();
+    EthApiClient::submit_hashrate(client, U256::default(), H256::default()).await.unwrap_err();
+    EthApiClient::submit_work(client, H64::default(), H256::default(), H256::default()).await.unwrap_err();
+    // EthApiClient::send_transaction(client).await.unwrap_err();
+    // EthApiClient::send_raw_transaction(client).await.unwrap_err();
+    // EthApiClient::sign(client).await.unwrap_err();
+    // EthApiClient::sign_transaction(client).await.unwrap_err();
+    // EthApiClient::sign_typed_data(client).await.unwrap_err();
+    // EthApiClient::get_proof(client).await.unwrap_err();
 
 }
     
