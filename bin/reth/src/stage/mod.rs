2a160d8162489d8015ddbc0bff9aaefae7f520e9
Chirag Baghasingh <76247491+chirag-bgh@users.noreply.github.com>
2023-02-11 10:51:14 +0530

    refactor: move structs to args mod (#1250)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- bin/reth/src/stage/mod.rs
+++ bin/reth/src/stage/mod.rs
@@ -2,17 +2,18 @@
 //!
 //! Stage debugging tool
 use crate::{
+    args::NetworkArgs,
     dirs::{ConfigPath, DbPath, PlatformPath},
     prometheus_exporter,
-    utils::{chainspec::chain_spec_value_parser, init::init_db},
-    NetworkOpts,
 };
 use reth_consensus::beacon::BeaconConsensus;
 use reth_downloaders::bodies::bodies::BodiesDownloaderBuilder;
-
 use reth_net_nat::NatResolver;
 use reth_primitives::ChainSpec;
-use reth_staged_sync::Config;
+use reth_staged_sync::{
+    utils::{chainspec::chain_spec_value_parser, init::init_db},
+    Config,
+};
 use reth_stages::{
     stages::{BodyStage, ExecutionStage, SenderRecoveryStage},
     ExecInput, Stage, StageId, Transaction, UnwindInput,
@@ -83,7 +84,7 @@ pub struct Command {
     skip_unwind: bool,
 
     #[clap(flatten)]
-    network: NetworkOpts,
+    network: NetworkArgs,
 
     #[arg(long, default_value = "any")]
     nat: NatResolver,
