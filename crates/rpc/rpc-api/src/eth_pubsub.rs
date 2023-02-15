78d06fd76940fdef8ca30103de4f76a1bff2e11c
Matthias Seitz <matthias.seitz@outlook.de>
2023-02-15 06:37:26 +0100

    feat(rpc): work on subscriptions (#1290)

--- crates/rpc/rpc-api/src/eth_pubsub.rs
+++ crates/rpc/rpc-api/src/eth_pubsub.rs
@@ -1,14 +1,14 @@
 use jsonrpsee::proc_macros::rpc;
-use reth_rpc_types::pubsub::{Kind, Params};
+use reth_rpc_types::pubsub::{Params, SubscriptionKind};
 
 /// Ethereum pub-sub rpc interface.
 #[rpc(server)]
 pub trait EthPubSubApi {
-    /// Create an ethereum subscription.
+    /// Create an ethereum subscription for the given params
     #[subscription(
         name = "eth_subscribe",
         unsubscribe = "eth_unsubscribe",
         item = reth_rpc_types::pubsub::SubscriptionResult
     )]
-    fn subscribe(&self, kind: Kind, params: Option<Params>);
+    fn subscribe(&self, kind: SubscriptionKind, params: Option<Params>);
 }
--- crates/rpc/rpc-types/src/eth/pubsub.rs
+++ crates/rpc/rpc-types/src/eth/pubsub.rs
@@ -58,14 +58,34 @@ impl Serialize for SubscriptionResult {
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Hash, Clone)]
 #[serde(deny_unknown_fields)]
 #[serde(rename_all = "camelCase")]
-pub enum Kind {
+pub enum SubscriptionKind {
     /// New block headers subscription.
+    ///
+    /// Fires a notification each time a new header is appended to the chain, including chain
+    /// reorganizations. In case of a chain reorganization the subscription will emit all new
+    /// headers for the new chain. Therefore the subscription can emit multiple headers on the same
+    /// height.
     NewHeads,
     /// Logs subscription.
+    ///
+    /// Returns logs that are included in new imported blocks and match the given filter criteria.
+    /// In case of a chain reorganization previous sent logs that are on the old chain will be
+    /// resent with the removed property set to true. Logs from transactions that ended up in the
+    /// new chain are emitted. Therefore, a subscription can emit logs for the same transaction
+    /// multiple times.
     Logs,
     /// New Pending Transactions subscription.
+    ///
+    /// Returns the hash for all transactions that are added to the pending state and are signed
+    /// with a key that is available in the node. When a transaction that was previously part of
+    /// the canonical chain isn't part of the new canonical chain after a reogranization its again
+    /// emitted.
     NewPendingTransactions,
     /// Node syncing status subscription.
+    ///
+    /// Indicates when the node starts or stops synchronizing. The result can either be a boolean
+    /// indicating that the synchronization has started (true), finished (false) or an object with
+    /// various progress indicators.
     Syncing,
 }
 
