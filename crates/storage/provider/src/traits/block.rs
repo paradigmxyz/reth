931aed0b98191989e486de836d525cb505939e24
Matthias Seitz <matthias.seitz@outlook.de>
2023-02-17 16:43:57 +0100

    rustfmt

--- crates/storage/provider/src/traits/block.rs
+++ crates/storage/provider/src/traits/block.rs
@@ -5,7 +5,7 @@ use reth_primitives::{rpc::BlockId, Block};
 /// Api trait for fetching `Block` related data.
 #[auto_impl::auto_impl(&, Arc)]
 pub trait BlockProvider:
-BlockIdProvider + HeaderProvider + TransactionsProvider + Send + Sync
+    BlockIdProvider + HeaderProvider + TransactionsProvider + Send + Sync
 {
     /// Returns the block. Returns `None` if block is not found.
     fn block(&self, id: BlockId) -> Result<Option<Block>>;
