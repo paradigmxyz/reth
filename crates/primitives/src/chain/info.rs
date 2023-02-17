42d90cb83561b9bc411f73b05617f1a29ba44702
Matthias Seitz <matthias.seitz@outlook.de>
2023-02-17 18:02:36 +0100

    feat(rpc): impl filter changes (#1373)
    Co-authored-by: Georgios Konstantopoulos <me@gakonst.com>

--- crates/primitives/src/chain/info.rs
+++ crates/primitives/src/chain/info.rs
@@ -1,4 +1,4 @@
-use crate::{BlockNumber, H256};
+use crate::{rpc::BlockNumber as RpcBlockNumber, BlockNumber, H256};
 
 /// Current status of the blockchain's head.
 #[derive(Default, Debug, Eq, PartialEq)]
@@ -11,4 +11,18 @@ pub struct ChainInfo {
     pub last_finalized: Option<BlockNumber>,
     /// Safe block
     pub safe_finalized: Option<BlockNumber>,
+}
+
+impl ChainInfo {
+    /// Attempts to convert a [BlockNumber](crate::rpc::BlockNumber) enum to a numeric value
+    pub fn convert_block_number(&self, number: RpcBlockNumber) -> Option<u64> {
+        match number {
+            RpcBlockNumber::Finalized => self.last_finalized,
+            RpcBlockNumber::Safe => self.safe_finalized,
+            RpcBlockNumber::Earliest => Some(0),
+            RpcBlockNumber::Number(num) => Some(num.as_u64()),
+            RpcBlockNumber::Pending => None,
+            RpcBlockNumber::Latest => Some(self.best_number),
+        }
+    }
 }
