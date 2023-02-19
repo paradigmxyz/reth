b22b6e636b81915a1fd33042a13bfa219a5806cd
Matthias Seitz <matthias.seitz@outlook.de>
2023-02-19 12:01:56 +0100

    test update test

--- crates/primitives/src/jsonu256.rs
+++ crates/primitives/src/jsonu256.rs
@@ -111,6 +111,9 @@ mod test {
         let data = JsonU256(U256::from(16));
         let serialized = serde_json::to_string(&data).unwrap();
 
-        assert_eq!(serialized, r#""16""#);
+        assert_eq!(
+            serialized,
+            r#""0x0000000000000000000000000000000000000000000000000000000000000010""#
+        );
     }
 }
