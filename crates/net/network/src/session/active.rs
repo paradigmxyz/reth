e124f9a7068c530f00530b6d0f7eb1f7905da1ea
Kim, JinSan <plotonix@gmail.com>
2023-02-19 13:38:08 +0900

    Update crates/net/network/src/session/active.rs
    Co-authored-by: Georgios Konstantopoulos <me@gakonst.com>

--- crates/net/network/src/session/active.rs
+++ crates/net/network/src/session/active.rs
@@ -1054,9 +1054,7 @@ mod tests {
 
         let fut = builder.with_client_stream(local_addr, move |mut client_stream| async move {
             client_stream
-                .send(EthMessage::NewPooledTransactionHashes66(NewPooledTransactionHashes66(
-                    vec![],
-                )))
+                .send(EthMessage::NewPooledTransactionHashes66(Vec::new().into()))
                 .await
                 .unwrap();
             let _ = tokio::time::timeout(Duration::from_secs(100), client_stream.next()).await;
