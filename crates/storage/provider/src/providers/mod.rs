8094ab18dbb230f177babceeb3d72c7a4c424490
Matthias Seitz <matthias.seitz@outlook.de>
2023-02-22 15:28:00 +0100

    feat: add BlockProvider::ommers (#1509)

--- crates/storage/provider/src/providers/mod.rs
+++ crates/storage/provider/src/providers/mod.rs
@@ -135,6 +135,18 @@ impl<DB: Database> BlockProvider for ShareableDatabase<DB> {
 
         Ok(None)
     }
+
+    fn ommers(&self, id: BlockId) -> Result<Option<Vec<Header>>> {
+        if let Some(number) = self.block_number_for_id(id)? {
+            let tx = self.db.tx()?;
+            // TODO: this can be optimized to return empty Vec post-merge
+            let ommers =
+                tx.get::<tables::BlockOmmers>(number)?.map(|o| o.ommers).unwrap_or_default();
+            return Ok(Some(ommers))
+        }
+
+        Ok(None)
+    }
 }
 
 impl<DB: Database> TransactionsProvider for ShareableDatabase<DB> {
