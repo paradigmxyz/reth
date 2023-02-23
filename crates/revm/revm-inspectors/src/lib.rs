4c75b45644efff1d431e7641c32a21674f072352
Matthias Seitz <matthias.seitz@outlook.de>
2023-02-23 16:06:37 +0100

    feat: add revm-inspectors crate and accesslist inspector

---
+++ crates/revm/revm-inspectors/src/lib.rs
@@ -0,0 +1,11 @@
+#![warn(missing_docs, unreachable_pub, unused_crate_dependencies)]
+#![deny(unused_must_use, rust_2018_idioms)]
+#![doc(test(
+    no_crate_inject,
+    attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
+))]
+
+//! revm [Inspector](revm::Inspector) implementations
+
+/// An inspector implementation for an EIP2930 Accesslist
+pub mod access_list;
