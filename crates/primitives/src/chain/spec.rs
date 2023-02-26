bfa464181a998a83d3f8673e7a73653a86577f4c
Roman Krasiuk <rokrassyuk@gmail.com>
2023-02-26 14:13:36 +0200

    chainspec and forks

--- crates/primitives/src/chain/spec.rs
+++ crates/primitives/src/chain/spec.rs
@@ -16,118 +16,94 @@ use std::{
 };
 
 /// The Ethereum mainnet spec
-pub static MAINNET: Lazy<Arc<ChainSpec>> = Lazy::new(|| {
-    ChainSpec {
-        chain: Chain::mainnet(),
-        genesis: serde_json::from_str(include_str!("../../res/genesis/mainnet.json"))
-            .expect("Can't deserialize Mainnet genesis json"),
-        genesis_hash: Some(H256(hex!(
-            "d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3"
-        ))),
-        // <https://etherscan.io/block/15537394>
-        paris_block_and_final_difficulty: Some((
-            15537394,
-            U256::from(58_750_003_716_598_352_816_469u128),
-        )),
-        fork_timestamps: ForkTimestamps::default().shanghai(1681338455),
-        hardforks: BTreeMap::from([
-            (Hardfork::Frontier, ForkCondition::Block(0)),
-            (Hardfork::Homestead, ForkCondition::Block(1150000)),
-            (Hardfork::Dao, ForkCondition::Block(1920000)),
-            (Hardfork::Tangerine, ForkCondition::Block(2463000)),
-            (Hardfork::SpuriousDragon, ForkCondition::Block(2675000)),
-            (Hardfork::Byzantium, ForkCondition::Block(4370000)),
-            (Hardfork::Constantinople, ForkCondition::Block(7280000)),
-            (Hardfork::Petersburg, ForkCondition::Block(7280000)),
-            (Hardfork::Istanbul, ForkCondition::Block(9069000)),
-            (Hardfork::MuirGlacier, ForkCondition::Block(9200000)),
-            (Hardfork::Berlin, ForkCondition::Block(12244000)),
-            (Hardfork::London, ForkCondition::Block(12965000)),
-            (Hardfork::ArrowGlacier, ForkCondition::Block(13773000)),
-            (Hardfork::GrayGlacier, ForkCondition::Block(15050000)),
-            (
-                Hardfork::Paris,
-                ForkCondition::TTD {
-                    fork_block: None,
-                    total_difficulty: U256::from(58_750_000_000_000_000_000_000_u128),
-                },
-            ),
-            (Hardfork::Shanghai, ForkCondition::Timestamp(1681338455)),
-        ]),
-    }
-    .into()
+pub static MAINNET: Lazy<ChainSpec> = Lazy::new(|| ChainSpec {
+    chain: Chain::mainnet(),
+    genesis: serde_json::from_str(include_str!("../../res/genesis/mainnet.json"))
+        .expect("Can't deserialize Mainnet genesis json"),
+    genesis_hash: Some(H256(hex!(
+        "d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3"
+    ))),
+    hardforks: BTreeMap::from([
+        (Hardfork::Frontier, ForkCondition::Block(0)),
+        (Hardfork::Homestead, ForkCondition::Block(1150000)),
+        (Hardfork::Dao, ForkCondition::Block(1920000)),
+        (Hardfork::Tangerine, ForkCondition::Block(2463000)),
+        (Hardfork::SpuriousDragon, ForkCondition::Block(2675000)),
+        (Hardfork::Byzantium, ForkCondition::Block(4370000)),
+        (Hardfork::Constantinople, ForkCondition::Block(7280000)),
+        (Hardfork::Petersburg, ForkCondition::Block(7280000)),
+        (Hardfork::Istanbul, ForkCondition::Block(9069000)),
+        (Hardfork::MuirGlacier, ForkCondition::Block(9200000)),
+        (Hardfork::Berlin, ForkCondition::Block(12244000)),
+        (Hardfork::London, ForkCondition::Block(12965000)),
+        (Hardfork::ArrowGlacier, ForkCondition::Block(13773000)),
+        (Hardfork::GrayGlacier, ForkCondition::Block(15050000)),
+        (
+            Hardfork::Paris,
+            ForkCondition::TTD {
+                fork_block: None,
+                total_difficulty: U256::from(58_750_000_000_000_000_000_000_u128),
+            },
+        ),
+    ]),
+    #[cfg(feature = "optimism")]
+    optimism: None,
 });
 
 /// The Goerli spec
-pub static GOERLI: Lazy<Arc<ChainSpec>> = Lazy::new(|| {
-    ChainSpec {
-        chain: Chain::goerli(),
-        genesis: serde_json::from_str(include_str!("../../res/genesis/goerli.json"))
-            .expect("Can't deserialize Goerli genesis json"),
-        genesis_hash: Some(H256(hex!(
-            "bf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a"
-        ))),
-        // <https://goerli.etherscan.io/block/7382818>
-        paris_block_and_final_difficulty: Some((7382818, U256::from(10_790_000))),
-        fork_timestamps: ForkTimestamps::default().shanghai(1678832736),
-        hardforks: BTreeMap::from([
-            (Hardfork::Frontier, ForkCondition::Block(0)),
-            (Hardfork::Homestead, ForkCondition::Block(0)),
-            (Hardfork::Dao, ForkCondition::Block(0)),
-            (Hardfork::Tangerine, ForkCondition::Block(0)),
-            (Hardfork::SpuriousDragon, ForkCondition::Block(0)),
-            (Hardfork::Byzantium, ForkCondition::Block(0)),
-            (Hardfork::Constantinople, ForkCondition::Block(0)),
-            (Hardfork::Petersburg, ForkCondition::Block(0)),
-            (Hardfork::Istanbul, ForkCondition::Block(1561651)),
-            (Hardfork::Berlin, ForkCondition::Block(4460644)),
-            (Hardfork::London, ForkCondition::Block(5062605)),
-            (
-                Hardfork::Paris,
-                ForkCondition::TTD { fork_block: None, total_difficulty: U256::from(10_790_000) },
-            ),
-            (Hardfork::Shanghai, ForkCondition::Timestamp(1678832736)),
-        ]),
-    }
-    .into()
+pub static GOERLI: Lazy<ChainSpec> = Lazy::new(|| ChainSpec {
+    chain: Chain::goerli(),
+    genesis: serde_json::from_str(include_str!("../../res/genesis/goerli.json"))
+        .expect("Can't deserialize Goerli genesis json"),
+    genesis_hash: Some(H256(hex!(
+        "bf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a"
+    ))),
+    hardforks: BTreeMap::from([
+        (Hardfork::Frontier, ForkCondition::Block(0)),
+        (Hardfork::Istanbul, ForkCondition::Block(1561651)),
+        (Hardfork::Berlin, ForkCondition::Block(4460644)),
+        (Hardfork::London, ForkCondition::Block(5062605)),
+        (
+            Hardfork::Paris,
+            ForkCondition::TTD { fork_block: None, total_difficulty: U256::from(10_790_000) },
+        ),
+    ]),
+    #[cfg(feature = "optimism")]
+    optimism: None,
 });
 
 /// The Sepolia spec
-pub static SEPOLIA: Lazy<Arc<ChainSpec>> = Lazy::new(|| {
-    ChainSpec {
-        chain: Chain::sepolia(),
-        genesis: serde_json::from_str(include_str!("../../res/genesis/sepolia.json"))
-            .expect("Can't deserialize Sepolia genesis json"),
-        genesis_hash: Some(H256(hex!(
-            "25a5cc106eea7138acab33231d7160d69cb777ee0c2c553fcddf5138993e6dd9"
-        ))),
-        // <https://sepolia.etherscan.io/block/1450409>
-        paris_block_and_final_difficulty: Some((1450409, U256::from(17_000_018_015_853_232u128))),
-        fork_timestamps: ForkTimestamps::default().shanghai(1677557088),
-        hardforks: BTreeMap::from([
-            (Hardfork::Frontier, ForkCondition::Block(0)),
-            (Hardfork::Homestead, ForkCondition::Block(0)),
-            (Hardfork::Dao, ForkCondition::Block(0)),
-            (Hardfork::Tangerine, ForkCondition::Block(0)),
-            (Hardfork::SpuriousDragon, ForkCondition::Block(0)),
-            (Hardfork::Byzantium, ForkCondition::Block(0)),
-            (Hardfork::Constantinople, ForkCondition::Block(0)),
-            (Hardfork::Petersburg, ForkCondition::Block(0)),
-            (Hardfork::Istanbul, ForkCondition::Block(0)),
-            (Hardfork::MuirGlacier, ForkCondition::Block(0)),
-            (Hardfork::Berlin, ForkCondition::Block(0)),
-            (Hardfork::London, ForkCondition::Block(0)),
-            (
-                Hardfork::Paris,
-                ForkCondition::TTD {
-                    fork_block: Some(1735371),
-                    total_difficulty: U256::from(17_000_000_000_000_000u64),
-                },
-            ),
-            (Hardfork::Shanghai, ForkCondition::Timestamp(1677557088)),
-        ]),
-    }
-    .into()
+pub static SEPOLIA: Lazy<ChainSpec> = Lazy::new(|| ChainSpec {
+    chain: Chain::sepolia(),
+    genesis: serde_json::from_str(include_str!("../../res/genesis/sepolia.json"))
+        .expect("Can't deserialize Sepolia genesis json"),
+    genesis_hash: Some(H256(hex!(
+        "25a5cc106eea7138acab33231d7160d69cb777ee0c2c553fcddf5138993e6dd9"
+    ))),
+    hardforks: BTreeMap::from([
+        (Hardfork::Frontier, ForkCondition::Block(0)),
+        (Hardfork::Homestead, ForkCondition::Block(0)),
+        (Hardfork::Dao, ForkCondition::Block(0)),
+        (Hardfork::Tangerine, ForkCondition::Block(0)),
+        (Hardfork::SpuriousDragon, ForkCondition::Block(0)),
+        (Hardfork::Byzantium, ForkCondition::Block(0)),
+        (Hardfork::Constantinople, ForkCondition::Block(0)),
+        (Hardfork::Petersburg, ForkCondition::Block(0)),
+        (Hardfork::Istanbul, ForkCondition::Block(0)),
+        (Hardfork::MuirGlacier, ForkCondition::Block(0)),
+        (Hardfork::Berlin, ForkCondition::Block(0)),
+        (Hardfork::London, ForkCondition::Block(0)),
+        (
+            Hardfork::Paris,
+            ForkCondition::TTD {
+                fork_block: Some(1735371),
+                total_difficulty: U256::from(17_000_000_000_000_000u64),
+            },
+        ),
+        (Hardfork::Shanghai, ForkCondition::Timestamp(1677557088)),
+    ]),
+    #[cfg(feature = "optimism")]
+    optimism: None,
 });
 
 /// Dev testnet specification
@@ -201,6 +177,20 @@ pub struct ChainSpec {
 
     /// The active hard forks and their activation conditions
     pub hardforks: BTreeMap<Hardfork, ForkCondition>,
+
+    #[cfg(feature = "optimism")]
+    /// Optimism configuration
+    pub optimism: Option<OptimismConfig>,
+}
+
+#[cfg(feature = "optimism")]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+/// Optimism configuration.
+pub struct OptimismConfig {
+    /// Elasticity multiplier as defined in [EIP-1559](https://eips.ethereum.org/EIPS/eip-1559)
+    pub eip_1559_elasticity: u64,
+    /// Base fee max change denominator as defined in [EIP-1559](https://eips.ethereum.org/EIPS/eip-1559)
+    pub eip_1559_denominator: u64,
 }
 
 impl ChainSpec {
@@ -423,7 +413,8 @@ impl From<Genesis> for ChainSpec {
             genesis_hash: None,
             fork_timestamps: ForkTimestamps::from_hardforks(&hardforks),
             hardforks,
-            paris_block_and_final_difficulty: None,
+            #[cfg(feature = "optimism")]
+            optimism: None,
         }
     }
 }
@@ -495,6 +486,8 @@ pub struct ChainSpecBuilder {
     chain: Option<Chain>,
     genesis: Option<Genesis>,
     hardforks: BTreeMap<Hardfork, ForkCondition>,
+    #[cfg(feature = "optimism")]
+    optimism: Option<OptimismConfig>,
 }
 
 impl ChainSpecBuilder {
@@ -504,6 +497,8 @@ impl ChainSpecBuilder {
             chain: Some(MAINNET.chain),
             genesis: Some(MAINNET.genesis.clone()),
             hardforks: MAINNET.hardforks.clone(),
+            #[cfg(feature = "optimism")]
+            optimism: None,
         }
     }
 
@@ -614,6 +609,20 @@ impl ChainSpecBuilder {
         self
     }
 
+    #[cfg(feature = "optimism")]
+    /// Enable Bedrock at genesis
+    pub fn bedrock_activated(mut self) -> Self {
+        self.hardforks.insert(Hardfork::Bedrock, ForkCondition::Block(0));
+        self
+    }
+
+    #[cfg(feature = "optimism")]
+    /// Enable Bedrock at genesis
+    pub fn regolith_activated(mut self) -> Self {
+        self.hardforks.insert(Hardfork::Regolith, ForkCondition::Timestamp(0));
+        self
+    }
+
     /// Build the resulting [`ChainSpec`].
     ///
     /// # Panics
@@ -627,7 +636,8 @@ impl ChainSpecBuilder {
             genesis_hash: None,
             fork_timestamps: ForkTimestamps::from_hardforks(&self.hardforks),
             hardforks: self.hardforks,
-            paris_block_and_final_difficulty: None,
+            #[cfg(feature = "optimism")]
+            optimism: self.optimism,
         }
     }
 }
@@ -638,6 +648,8 @@ impl From<&Arc<ChainSpec>> for ChainSpecBuilder {
             chain: Some(value.chain),
             genesis: Some(value.genesis.clone()),
             hardforks: value.hardforks.clone(),
+            #[cfg(feature = "optimism")]
+            optimism: value.optimism.clone(),
         }
     }
 }
