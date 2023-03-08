bc06f662d5212fa51872647c1b3061ef2fd13af6
Francisco Krause Arnim <fkrausear@gmail.com>
2023-03-08 11:42:21 -0300

    Remove unnecessary Generics, add find_signer
    Co-authored-by: lambdaclass-user <github@lambdaclass.com>

--- crates/rpc/rpc/src/eth/api/sign.rs
+++ crates/rpc/rpc/src/eth/api/sign.rs
@@ -1,29 +1,31 @@
 //! Contains RPC handler implementations specific to sign endpoints
-
 use crate::{
-    eth::error::{EthApiError, EthResult},
+    eth::{
+        error::{EthApiError, EthResult},
+        signer::EthSigner
+    },
     EthApi,
 };
 use reth_primitives::{Address, Bytes, Signature};
-use reth_provider::{BlockProvider, EvmEnvProvider, StateProviderFactory};
-use reth_transaction_pool::TransactionPool;
-
 impl<Client, Pool, Network> EthApi<Client, Pool, Network>
 where
-    Pool: TransactionPool + 'static,
-    Client: BlockProvider + StateProviderFactory + EvmEnvProvider + 'static,
-    Network: 'static,
 {
     pub(crate) async fn sign(&self, account: Address, message: Bytes) -> EthResult<Bytes> {
         let signer = self
+            .find_signer(&account)
+            .ok_or(EthApiError::NoSigner)?;
+        let signature =
+            signer.sign(account, &message).await.map_err(|_err| EthApiError::NoSigner)?;
+        let mut buff = vec![];
+        Signature::encode(&signature, &mut buff);
+        Ok(buff.into())
+    }
+
+    pub(crate) fn find_signer(&self, account: &Address) -> Option<&Box<(dyn EthSigner + 'static)>> {
+        self
             .inner
             .signers
             .iter()
             .find(|signer| signer.is_signer_for(&account))
-            .ok_or(EthApiError::NoSigner)?;
-        let signature = signer.sign(account, &message).await.map_err(|_err| EthApiError::NoSigner)?;
-        let mut buff = vec![];
-        Signature::encode(&signature, &mut buff);
-        Ok(buff.into())
     }
 }
