29a2e1ab3a81f75cf6b5fefd41ed66c3381a2af1
Matthias Seitz <matthias.seitz@outlook.de>
2023-03-13 03:36:19 +0100

    feat(rpc): add EthSubscriptionIdProvider (#1721)

--- crates/rpc/rpc/src/eth/mod.rs
+++ crates/rpc/rpc/src/eth/mod.rs
@@ -4,9 +4,11 @@ mod api;
 pub mod cache;
 pub(crate) mod error;
 mod filter;
+mod id_provider;
 mod pubsub;
 mod signer;
 
 pub use api::{EthApi, EthApiSpec};
 pub use filter::EthFilter;
+pub use id_provider::EthSubscriptionIdProvider;
 pub use pubsub::EthPubSub;
