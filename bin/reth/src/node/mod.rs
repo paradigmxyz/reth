3a01ad9cd7ca0c5f0e0d57b1af1b89308c17f9a8
Roman Krasiuk <rokrassyuk@gmail.com>
2023-03-17 14:19:12 +0200

    fix(cli): create net dir if non existent (#1809)

--- bin/reth/src/node/mod.rs
+++ bin/reth/src/node/mod.rs
@@ -551,7 +551,8 @@ async fn run_network_until_shutdown<C>(
         let known_peers = network.all_peers().collect::<Vec<_>>();
         if let Ok(known_peers) = serde_json::to_string_pretty(&known_peers) {
             trace!(target : "reth::cli", peers_file =?file_path, num_peers=%known_peers.len(), "Saving current peers");
-            match std::fs::write(&file_path, known_peers) {
+            let parent_dir = file_path.parent().map(std::fs::create_dir_all).transpose();
+            match parent_dir.and_then(|_| std::fs::write(&file_path, known_peers)) {
                 Ok(_) => {
                     info!(target: "reth::cli", peers_file=?file_path, "Wrote network peers to file");
                 }
