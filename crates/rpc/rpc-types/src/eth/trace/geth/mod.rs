89e94bf7777beab26053676b04f9bb543fe4069a
Matthias Seitz <matthias.seitz@outlook.de>
2023-03-17 18:52:18 +0100

    feat(rpc): port all geth tracing types (#1812)

--- crates/rpc/rpc-types/src/eth/trace/geth/mod.rs
+++ crates/rpc/rpc-types/src/eth/trace/geth/mod.rs
@@ -1,11 +1,22 @@
 #![allow(missing_docs)]
+
 /// Geth tracing types
 use reth_primitives::{Bytes, JsonU256, H256, U256};
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 
-// re-exported for geth tracing types
-pub use ethers_core::types::{GethDebugTracingOptions, GethTraceFrame};
+// re-exports
+pub use self::{
+    call::{CallConfig, CallFrame},
+    four_byte::FourByteFrame,
+    noop::NoopFrame,
+    pre_state::{PreStateConfig, PreStateFrame},
+};
+
+mod call;
+mod four_byte;
+mod noop;
+mod pre_state;
 
 /// Result type for geth style transaction trace
 pub type TraceResult = crate::trace::common::TraceResult<serde_json::Value, String>;
@@ -56,4 +67,144 @@ pub struct StructLog {
     pub stack: Option<Vec<U256>>,
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub storage: Option<BTreeMap<H256, H256>>,
+}
+
+/// Tracing response
+#[derive(Debug, PartialEq, Eq, Clone, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum GethTraceFrame {
+    Default(DefaultFrame),
+    NoopTracer(NoopFrame),
+    FourByteTracer(FourByteFrame),
+    CallTracer(CallFrame),
+    PreStateTracer(PreStateFrame),
+}
+
+impl From<DefaultFrame> for GethTraceFrame {
+    fn from(value: DefaultFrame) -> Self {
+        GethTraceFrame::Default(value)
+    }
+}
+
+impl From<FourByteFrame> for GethTraceFrame {
+    fn from(value: FourByteFrame) -> Self {
+        GethTraceFrame::FourByteTracer(value)
+    }
+}
+
+impl From<CallFrame> for GethTraceFrame {
+    fn from(value: CallFrame) -> Self {
+        GethTraceFrame::CallTracer(value)
+    }
+}
+
+impl From<PreStateFrame> for GethTraceFrame {
+    fn from(value: PreStateFrame) -> Self {
+        GethTraceFrame::PreStateTracer(value)
+    }
+}
+
+impl From<NoopFrame> for GethTraceFrame {
+    fn from(value: NoopFrame) -> Self {
+        GethTraceFrame::NoopTracer(value)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum GethTrace {
+    Known(GethTraceFrame),
+    Unknown(serde_json::Value),
+}
+
+impl From<GethTraceFrame> for GethTrace {
+    fn from(value: GethTraceFrame) -> Self {
+        GethTrace::Known(value)
+    }
+}
+
+impl From<serde_json::Value> for GethTrace {
+    fn from(value: serde_json::Value) -> Self {
+        GethTrace::Unknown(value)
+    }
+}
+
+/// Available built-in tracers
+///
+/// See <https://geth.ethereum.org/docs/developers/evm-tracing/built-in-tracers>
+#[derive(Debug, PartialEq, Eq, Clone, Deserialize, Serialize)]
+pub enum GethDebugBuiltInTracerType {
+    #[serde(rename = "4byteTracer")]
+    FourByteTracer,
+    #[serde(rename = "callTracer")]
+    CallTracer,
+    #[serde(rename = "prestateTracer")]
+    PreStateTracer,
+    #[serde(rename = "noopTracer")]
+    NoopTracer,
+}
+
+/// Configuration for the builtin tracer
+#[derive(Debug, PartialEq, Eq, Clone, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum GethDebugBuiltInTracerConfig {
+    CallTracer(CallConfig),
+    PreStateTracer(PreStateConfig),
+}
+
+/// Available tracers
+///
+/// See <https://geth.ethereum.org/docs/developers/evm-tracing/built-in-tracers> and <https://geth.ethereum.org/docs/developers/evm-tracing/custom-tracer>
+#[derive(Debug, PartialEq, Eq, Clone, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum GethDebugTracerType {
+    /// built-in tracer
+    BuiltInTracer(GethDebugBuiltInTracerType),
+    /// custom JS tracer
+    JsTracer(String),
+}
+
+/// Configuration of the tracer
+#[derive(Debug, PartialEq, Eq, Clone, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum GethDebugTracerConfig {
+    /// built-in tracer
+    BuiltInTracer(GethDebugBuiltInTracerConfig),
+    /// custom JS tracer
+    JsTracer(serde_json::Value),
+}
+
+/// Bindings for additional `debug_traceTransaction` options
+///
+/// See <https://geth.ethereum.org/docs/rpc/ns-debug#debug_tracetransaction>
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct GethDebugTracingOptions {
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub disable_storage: Option<bool>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub disable_stack: Option<bool>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub enable_memory: Option<bool>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub enable_return_data: Option<bool>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub tracer: Option<GethDebugTracerType>,
+    /// tracerConfig is slated for Geth v1.11.0
+    /// See <https://github.com/ethereum/go-ethereum/issues/26513>
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub tracer_config: Option<GethDebugTracerConfig>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub timeout: Option<String>,
+}
+
+/// Bindings for additional `debug_traceCall` options
+///
+/// See <https://geth.ethereum.org/docs/rpc/ns-debug#debug_tracecall>
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct GethDebugTracingCallOptions {
+    #[serde(flatten)]
+    pub tracing_options: GethDebugTracingOptions,
+    // TODO: Add stateoverrides and blockoverrides options
 }
---
+++ crates/rpc/rpc-types/src/eth/trace/geth/pre_state.rs
@@ -0,0 +1,89 @@
+use reth_primitives::{serde_helper::num::from_int_or_hex_opt, Address, H256, U256};
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+
+/// <https://github.com/ethereum/go-ethereum/blob/91cb6f863a965481e51d5d9c0e5ccd54796fd967/eth/tracers/native/prestate.go#L38>
+#[derive(Debug, PartialEq, Eq, Clone, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum PreStateFrame {
+    Default(PreStateMode),
+    Diff(DiffMode),
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub struct PreStateMode(pub BTreeMap<Address, AccountState>);
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub struct DiffMode {
+    pub pre: BTreeMap<Address, AccountState>,
+    pub post: BTreeMap<Address, AccountState>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub struct AccountState {
+    #[serde(
+        default,
+        deserialize_with = "from_int_or_hex_opt",
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub balance: Option<U256>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub code: Option<String>,
+    #[serde(
+        default,
+        deserialize_with = "from_int_or_hex_opt",
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub nonce: Option<U256>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub storage: Option<BTreeMap<H256, H256>>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PreStateConfig {
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub diff_mode: Option<bool>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::trace::geth::*;
+
+    // See <https://github.com/ethereum/go-ethereum/tree/master/eth/tracers/internal/tracetest/testdata>
+    const DEFAULT: &str = include_str!("../../../../test_data/pre_state_tracer/default.json");
+    const LEGACY: &str = include_str!("../../../../test_data/pre_state_tracer/legacy.json");
+    const DIFF_MODE: &str = include_str!("../../../../test_data/pre_state_tracer/diff_mode.json");
+
+    #[test]
+    fn test_serialize_pre_state_trace() {
+        let mut opts = GethDebugTracingCallOptions::default();
+        opts.tracing_options.disable_storage = Some(false);
+        opts.tracing_options.tracer =
+            Some(GethDebugTracerType::BuiltInTracer(GethDebugBuiltInTracerType::PreStateTracer));
+        opts.tracing_options.tracer_config = Some(GethDebugTracerConfig::BuiltInTracer(
+            GethDebugBuiltInTracerConfig::PreStateTracer(PreStateConfig { diff_mode: Some(true) }),
+        ));
+
+        assert_eq!(
+            serde_json::to_string(&opts).unwrap(),
+            r#"{"disableStorage":false,"tracer":"prestateTracer","tracerConfig":{"diffMode":true}}"#
+        );
+    }
+
+    #[test]
+    fn test_deserialize_pre_state_trace() {
+        let trace: PreStateFrame = serde_json::from_str(DEFAULT).unwrap();
+        match trace {
+            PreStateFrame::Default(PreStateMode(_)) => {}
+            _ => unreachable!(),
+        }
+        let _trace: PreStateFrame = serde_json::from_str(LEGACY).unwrap();
+        let trace: PreStateFrame = serde_json::from_str(DIFF_MODE).unwrap();
+        match trace {
+            PreStateFrame::Diff(DiffMode { pre: _pre, post: _post }) => {}
+            _ => unreachable!(),
+        }
+    }
+}
