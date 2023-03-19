9180f2b2b6bec5ca2c226d6738853872ac78596f
clabby <ben@clab.by>
2023-03-19 19:53:10 -0400

    Add Optimism EIP1559 elasticity multiplier / max base fee change denominator

--- crates/primitives/src/transaction/tx_type.rs
+++ crates/primitives/src/transaction/tx_type.rs
@@ -17,7 +17,7 @@ pub const EIP4844_TX_TYPE_ID: u8 = 3;
 
 /// Identifier for [TxDeposit](crate::TxDeposit) transaction.
 #[cfg(feature = "optimism")]
-pub const DEPOSIT_TX_TYPE_ID: u8 = 126;
+use crate::DEPOSIT_TX_TYPE;
 
 /// Transaction Type
 ///
@@ -40,7 +40,7 @@ pub enum TxType {
     EIP4844 = 3_isize,
     /// OP Deposit transaction.
     #[cfg(feature = "optimism")]
-    DEPOSIT = 126_isize,
+    DEPOSIT = DEPOSIT_TX_TYPE as isize,
 }
 
 impl From<TxType> for u8 {
@@ -51,7 +51,7 @@ impl From<TxType> for u8 {
             TxType::EIP1559 => EIP1559_TX_TYPE_ID,
             TxType::EIP4844 => EIP4844_TX_TYPE_ID,
             #[cfg(feature = "optimism")]
-            TxType::DEPOSIT => DEPOSIT_TX_TYPE_ID,
+            TxType::DEPOSIT => DEPOSIT_TX_TYPE,
         }
     }
 }
