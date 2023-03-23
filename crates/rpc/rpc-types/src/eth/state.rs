89ef4b507e63ca79baae33f3b250664bee4145fb
Matthias Seitz <matthias.seitz@outlook.de>
2023-03-23 23:52:14 +0100

    make optional

--- crates/rpc/rpc-types/src/eth/state.rs
+++ crates/rpc/rpc-types/src/eth/state.rs
@@ -13,15 +13,20 @@ pub type StateOverride = HashMap<Address, AccountOverride>;
 #[allow(missing_docs)]
 pub struct AccountOverride {
     /// Fake balance to set for the account before executing the call.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub balance: Option<U256>,
     /// Fake nonce to set for the account before executing the call.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub nonce: Option<U64>,
     /// Fake EVM bytecode to inject into the account before executing the call.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub code: Option<Bytes>,
     /// Fake key-value mapping to override all slots in the account storage before executing the
     /// call.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub state: Option<HashMap<H256, H256>>,
     /// Fake key-value mapping to override individual slots in the account storage before executing
     /// the call.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub state_diff: Option<HashMap<H256, H256>>,
 }
