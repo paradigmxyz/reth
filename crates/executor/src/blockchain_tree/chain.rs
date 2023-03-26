2548110591c1842902355d7843ef08220bc00558
Thor <7041313+thor314@users.noreply.github.com>
2023-03-26 04:32:55 -0700

    fix: clippy warnings in tests (#1954)

--- crates/executor/src/blockchain_tree/chain.rs
+++ crates/executor/src/blockchain_tree/chain.rs
@@ -389,8 +389,7 @@ mod tests {
     #[test]
     fn test_number_split() {
         let mut base_state = PostState::default();
-        let mut account = Account::default();
-        account.nonce = 10;
+        let account = Account { nonce: 10, ..Default::default() };
         base_state.create_account(H160([1; 20]), account);
         base_state.finish_transition();
 
