873a17bc41c1151324d2c9bec7a3c423fe27e6a2
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-03-28 18:51:02 -0400

    use chain_info instead of latest block

--- crates/rpc/rpc/src/eth/api/state.rs
+++ crates/rpc/rpc/src/eth/api/state.rs
@@ -54,27 +54,15 @@ where
         keys: Vec<H256>,
         block_id: Option<BlockId>,
     ) -> EthResult<EIP1186AccountProofResponse> {
-        let latest_block = self.client().block(BlockId::Number(BlockNumberOrTag::Latest))?;
+        let chain_info = self.client().chain_info()?;
         let block_id = block_id.unwrap_or(BlockId::Number(BlockNumberOrTag::Latest));
 
         // if we are trying to create a proof for the latest block, but have a BlockId as input
         // that is not BlockNumberOrTag::Latest, then we need to figure out whether or not the
         // BlockId corresponds to the latest block
         let is_blockid_latest = match block_id {
-            BlockId::Number(BlockNumberOrTag::Number(num)) => {
-                if let Some(block) = latest_block {
-                    block.number == num
-                } else {
-                    false
-                }
-            }
-            BlockId::Hash(hash) => {
-                if let Some(block) = latest_block {
-                    block.hash_slow() == hash.into()
-                } else {
-                    false
-                }
-            }
+            BlockId::Number(BlockNumberOrTag::Number(num)) => num == chain_info.best_number,
+            BlockId::Hash(hash) => hash == chain_info.best_hash.into(),
             BlockId::Number(BlockNumberOrTag::Latest) => true,
             _ => false,
         };
