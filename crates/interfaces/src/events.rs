77a41e5edfcd19a74bd844129a1ce48c7e447db1
Matthias Seitz <matthias.seitz@outlook.de>
2023-04-05 14:16:03 +0200

    feat: emit new block events in --auto-mine (#2079)
    Co-authored-by: Dan Cline <6798349+Rjected@users.noreply.github.com>

--- crates/interfaces/src/events.rs
+++ crates/interfaces/src/events.rs
@@ -1,6 +1,7 @@
+use parking_lot::Mutex;
 use reth_primitives::SealedHeader;
 use std::sync::Arc;
-use tokio::sync::broadcast::{Receiver, Sender};
+use tokio::sync::broadcast::{error::SendError, Receiver, Sender};
 
 /// New block notification that is Arc around [SealedHeader].
 pub type NewBlockNotification = Arc<SealedHeader>;
@@ -15,4 +16,39 @@ pub type NewBlockNotificationsSender = Sender<NewBlockNotification>;
 pub trait ChainEventSubscriptions: Send + Sync {
     /// Get notified when a new block was imported.
     fn subscribe_new_blocks(&self) -> NewBlockNotifications;
+}
+
+/// A shareable Sender that allows to send [NewBlockNotification] to all receivers.
+#[derive(Debug, Clone)]
+pub struct NewBlockNotificationSink {
+    inner: Arc<Mutex<Sender<NewBlockNotification>>>,
+}
+
+// === impl NewBlockNotificationSink ===
+
+impl NewBlockNotificationSink {
+    /// Creates a new NewBlockNotificationSink with the given capacity.
+    // // size of the broadcast is double of max reorg depth because at max reorg depth we can have
+    //         // send at least N block at the time.
+    pub fn new(capacity: usize) -> Self {
+        let inner = tokio::sync::broadcast::channel(capacity);
+        Self { inner: Arc::new(Mutex::new(inner.0)) }
+    }
+
+    /// Attempts to send a value to all active Receiver handles, returning it back if it could not
+    /// be sent.
+    pub fn send(
+        &self,
+        header: NewBlockNotification,
+    ) -> Result<usize, SendError<NewBlockNotification>> {
+        let sender = self.inner.lock();
+        sender.send(header)
+    }
+
+    /// Creates a new Receiver handle that will receive notifications sent after this call to
+    /// subscribe.
+    pub fn subscribe(&self) -> Receiver<NewBlockNotification> {
+        let sender = self.inner.lock();
+        sender.subscribe()
+    }
 }
