3142556ec90449ad831e514372015ba30448a3f0
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2023-04-05 17:27:37 +0100

    fmt

--- bin/reth/src/node/mod.rs
+++ bin/reth/src/node/mod.rs
@@ -488,7 +488,7 @@ impl Command {
         // try to look up the header in the database
         if let Some(header) = header {
             info!(target: "reth::cli", ?tip, "Successfully looked up tip block in the database");
-            return Ok(header.seal_slow());
+            return Ok(header.seal_slow())
         }
 
         info!(target: "reth::cli", ?tip, "Fetching tip block from the network.");
@@ -496,7 +496,7 @@ impl Command {
             match get_single_header(&client, tip).await {
                 Ok(tip_header) => {
                     info!(target: "reth::cli", ?tip, "Successfully fetched tip");
-                    return Ok(tip_header);
+                    return Ok(tip_header)
                 }
                 Err(error) => {
                     error!(target: "reth::cli", %error, "Failed to fetch the tip. Retrying...");
