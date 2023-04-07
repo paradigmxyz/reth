43640f582e3febae2eb343c4be098cde1bc3fecb
Matthias Seitz <matthias.seitz@outlook.de>
2023-04-07 13:03:17 +0200

    feat: add blockValue to execution payload (#2150)

--- crates/rpc/rpc-api/src/engine.rs
+++ crates/rpc/rpc-api/src/engine.rs
@@ -1,8 +1,8 @@
 use jsonrpsee::{core::RpcResult as Result, proc_macros::rpc};
 use reth_primitives::{BlockHash, U64};
 use reth_rpc_types::engine::{
-    ExecutionPayload, ExecutionPayloadBodies, ForkchoiceState, ForkchoiceUpdated,
-    PayloadAttributes, PayloadId, PayloadStatus, TransitionConfiguration,
+    ExecutionPayload, ExecutionPayloadBodies, ExecutionPayloadEnvelope, ForkchoiceState,
+    ForkchoiceUpdated, PayloadAttributes, PayloadId, PayloadStatus, TransitionConfiguration,
 };
 
 #[cfg_attr(not(feature = "client"), rpc(server))]
@@ -43,7 +43,7 @@ pub trait EngineApi {
 
     /// See also <https://github.com/ethereum/execution-apis/blob/6709c2a795b707202e93c4f2867fa0bf2640a84f/src/engine/shanghai.md#engine_getpayloadv2>
     #[method(name = "engine_getPayloadV2")]
-    async fn get_payload_v2(&self, payload_id: PayloadId) -> Result<ExecutionPayload>;
+    async fn get_payload_v2(&self, payload_id: PayloadId) -> Result<ExecutionPayloadEnvelope>;
 
     /// See also <https://github.com/ethereum/execution-apis/blob/6452a6b194d7db269bf1dbd087a267251d3cc7f8/src/engine/shanghai.md#engine_getpayloadbodiesbyhashv1>
     #[method(name = "engine_getPayloadBodiesByHashV1")]
