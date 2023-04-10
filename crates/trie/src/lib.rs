cbbd8345750228fb123af5e002ae8601222e6f86
Roman Krasiuk <rokrassyuk@gmail.com>
2023-04-10 11:34:50 +0300

    feat(trie): setup crate (#2166)

---
+++ crates/trie/src/lib.rs
@@ -0,0 +1,10 @@
+#![warn(missing_docs, unreachable_pub)]
+#![deny(unused_must_use, rust_2018_idioms)]
+#![doc(test(
+    no_crate_inject,
+    attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
+))]
+
+//! The implementation of Merkle Patricia Trie, a cryptographically
+//! authenticated radix trie that is used to store key-value bindings.
+//! <https://ethereum.org/en/developers/docs/data-structures-and-encoding/patricia-merkle-trie/>
