a6beed86fb797d2dc160575e0f8eb00c6dbd5a7d
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-04-11 17:18:50 -0400

    fix: add initial goerli forks (#2196)

--- crates/primitives/src/chain/spec.rs
+++ crates/primitives/src/chain/spec.rs
@@ -56,6 +56,13 @@ pub static GOERLI: Lazy<ChainSpec> = Lazy::new(|| ChainSpec {
     ))),
     hardforks: BTreeMap::from([
         (Hardfork::Frontier, ForkCondition::Block(0)),
+        (Hardfork::Homestead, ForkCondition::Block(0)),
+        (Hardfork::Dao, ForkCondition::Block(0)),
+        (Hardfork::Tangerine, ForkCondition::Block(0)),
+        (Hardfork::SpuriousDragon, ForkCondition::Block(0)),
+        (Hardfork::Byzantium, ForkCondition::Block(0)),
+        (Hardfork::Constantinople, ForkCondition::Block(0)),
+        (Hardfork::Petersburg, ForkCondition::Block(0)),
         (Hardfork::Istanbul, ForkCondition::Block(1561651)),
         (Hardfork::Berlin, ForkCondition::Block(4460644)),
         (Hardfork::London, ForkCondition::Block(5062605)),
