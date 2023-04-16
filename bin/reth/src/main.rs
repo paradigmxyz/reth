7b22f78a50c119b7f5324ae4405e6945c93b064e
chirag-bgh <chirag2bsingh@gmail.com>
2023-04-16 19:33:10 +0530

    feat: Generate constants for runtime version and OS

--- bin/reth/src/main.rs
+++ bin/reth/src/main.rs
@@ -1,4 +1,17 @@
+// The file `built.rs` was placed there by cargo and `build.rs`
+mod built_info {
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
+
 fn main() {
+    let version = built_info::PKG_VERSION;
+    let name = built_info::PKG_NAME;
+    let sha = built_info::GIT_COMMIT_HASH_SHORT;
+    let os = std::env::consts::OS;
+    let rustc = built_info::RUSTC_VERSION;
+
+    println!("{name}/v{version}-{sha}/{os}/{rustc} \n", name, version, sha, os, rustc);
+
     if let Err(err) = reth::cli::run() {
         eprintln!("Error: {err:?}");
         std::process::exit(1);
