4885a212e81d8fd535bb007bb95dc630bf7b72d6
Matthias Seitz <matthias.seitz@outlook.de>
2023-04-25 21:20:47 +0200

    Update crates/transaction-pool/src/traits.rs
    Co-authored-by: Georgios Konstantopoulos <me@gakonst.com>

--- crates/transaction-pool/src/traits.rs
+++ crates/transaction-pool/src/traits.rs
@@ -252,7 +252,7 @@ impl TransactionOrigin {
 /// block hash. In other words, the first added block of the range must be the child of the last
 /// known block hash.
 ///
-/// This is used to update the pool state according.
+/// This is used to update the pool state accordingly.
 #[derive(Debug, Clone)]
 pub struct CanonicalStateUpdate {
     /// Hash of the tip block.
