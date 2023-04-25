057cc5612717fc7e56827a33803d821633e805e4
Roman Krasiuk <rokrassyuk@gmail.com>
2023-04-25 09:22:32 +0300

    fix(evm): goerli coinbase

--- crates/revm/revm-primitives/src/env.rs
+++ crates/revm/revm-primitives/src/env.rs
@@ -1,7 +1,7 @@
 use crate::config::revm_spec;
 use reth_primitives::{
-    Address, ChainSpec, Head, Header, Transaction, TransactionKind, TransactionSignedEcRecovered,
-    TxEip1559, TxEip2930, TxLegacy, U256,
+    recover_signer, Address, Chain, ChainSpec, Head, Header, Transaction, TransactionKind,
+    TransactionSignedEcRecovered, TxEip1559, TxEip2930, TxLegacy, U256,
 };
 use revm::primitives::{AnalysisKind, BlockEnv, CfgEnv, SpecId, TransactTo, TxEnv};
 
@@ -55,6 +55,27 @@ pub fn fill_block_env(block_env: &mut BlockEnv, header: &Header, after_merge: bo
     }
     block_env.basefee = U256::from(header.base_fee_per_gas.unwrap_or_default());
     block_env.gas_limit = U256::from(header.gas_limit);
+}
+
+/// Return the coinbase address for the given header and chain spec.
+pub fn block_coinbase(chain_spec: &ChainSpec, header: &Header, after_merge: bool) -> Address {
+    if chain_spec.chain == Chain::goerli() && !after_merge {
+        recover_header_signer(&header).expect("failed to recover signer")
+    } else {
+        header.beneficiary
+    }
+}
+
+/// Recover the account from signed header per clique consensus rules.
+pub fn recover_header_signer(header: &Header) -> Option<Address> {
+    let extra_data_len = header.extra_data.len();
+    let signature = extra_data_len
+        // Fixed number of extra-data suffix bytes reserved for signer signature.
+        // 65 bytes fixed as signatures are based on the standard secp256k1 curve.
+        // Filled with zeros on genesis block.
+        .checked_sub(65)
+        .and_then(|start| -> Option<[u8; 65]> { header.extra_data[start..].try_into().ok() })?;
+    recover_signer(&signature, header.hash_slow().as_fixed_bytes()).ok()
 }
 
 /// Returns a new [TxEnv] filled with the transaction's data.
