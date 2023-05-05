6ac6c730380631ea845631bf7bd4ae6f0ae58627
Georgios Konstantopoulos <me@gakonst.com>
2023-05-05 10:40:52 -0700

    perf: use jemalloc + switch to lto/low codegen units (#2567)

--- bin/reth/src/main.rs
+++ bin/reth/src/main.rs
@@ -1,3 +1,8 @@
+// We use jemalloc for performance reasons
+#[cfg(all(not(windows), not(target_env = "musl")))]
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 fn main() {
     if let Err(err) = reth::cli::run() {
         eprintln!("Error: {err:?}");
