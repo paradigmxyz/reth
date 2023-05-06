5dd5f71fb1b3fac94b2266f9b2c84c96fd7a493b
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-05-06 12:51:21 +0200

    add get_block_values

--- crates/rpc/rpc/src/eth/gas_oracle.rs
+++ crates/rpc/rpc/src/eth/gas_oracle.rs
@@ -11,7 +11,10 @@ use std::{
     pin::Pin,
     task::{ready, Context, Poll},
 };
-use tokio::sync::mpsc::{unbounded_channel, UnboundedSender};
+use tokio::sync::{
+    mpsc::{unbounded_channel, UnboundedSender},
+    oneshot,
+};
 use tokio_stream::wrappers::UnboundedReceiverStream;
 
 /// The number of transactions sampled in a block
@@ -22,6 +25,9 @@ pub const DEFAULT_MAX_PRICE: U256 = U256::from_limbs([500_000_000_000u64, 0, 0, 
 
 /// The default minimum gas price, under which the sample will be ignored
 pub const DEFAULT_IGNORE_PRICE: U256 = U256::from_limbs([2u64, 0, 0, 0]);
+
+/// The type that can send the response to the requested max priority fee.
+type MaxPriorityFeeSender = oneshot::Sender<Result<U256>>;
 
 /// Settings for the [GasPriceOracle]
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
@@ -40,14 +46,16 @@ pub struct GasOracleConfig {
     pub max_block_history: u64,
 
     /// The default gas price to use if there are no blocks in the cache
-    pub default: U256,
+    pub default: Option<U256>,
 
     /// The maximum gas price to use for the estimate
-    pub max_price: U256,
+    pub max_price: Option<U256>,
 
     /// The minimum gas price, under which the sample will be ignored
-    pub ignore_price: U256,
+    pub ignore_price: Option<U256>,
 }
+
+// TODO: other config defaults
 
 // the go code to port to rust
 // // Oracle recommends gas prices based on the content of recent
@@ -77,9 +85,8 @@ pub struct GasPriceOracle {
 }
 
 /// All message variants sent to the oracle through the channel
-#[derive(Debug, Clone)]
 pub enum GasPriceAction {
-    // TODO
+    SuggestMaxPriorityFee { response_tx: MaxPriorityFeeSender },
 }
 
 impl GasPriceOracle {
@@ -155,6 +162,104 @@ where
     fn on_new_block(&mut self, block_hash: H256, res: Result<Option<Block>>) {
         todo!()
     }
+
+    //// SuggestTipCap returns a tip cap so that newly created transaction can have a
+    //// very high chance to be included in the following blocks.
+    ////
+    //// Note, for legacy transactions and the legacy eth_gasPrice RPC call, it will be
+    //// necessary to add the basefee to the returned number to fall back to the legacy
+    //// behavior.
+    //func (oracle *Oracle) SuggestTipCap(ctx context.Context) (*big.Int, error) {
+    fn suggest_tip_cap(&self) -> Result<U256> {
+        todo!()
+    }
+
+    // go code to port to rust
+    // // getBlockValues calculates the lowest transaction gas price in a given block
+    // // and sends it to the result channel. If the block is empty or all transactions
+    // // are sent by the miner itself(it doesn't make any sense to include this kind of
+    // // transaction prices for sampling), nil gasprice is returned.
+    // func (oracle *Oracle) getBlockValues(ctx context.Context, blockNum uint64, limit int,
+    // ignoreUnder *big.Int, result chan results, quit chan struct{}) { 	block, err :=
+    // oracle.backend.BlockByNumber(ctx, rpc.BlockNumber(blockNum)) 	if block == nil {
+    // 		select {
+    // 		case result <- results{nil, err}:
+    // 		case <-quit:
+    // 		}
+    // 		return
+    // 	}
+    // 	signer := types.MakeSigner(oracle.backend.ChainConfig(), block.Number(), block.Time())
+
+    // 	// Sort the transaction by effective tip in ascending sort.
+    // 	txs := make([]*types.Transaction, len(block.Transactions()))
+    // 	copy(txs, block.Transactions())
+    // 	sorter := newSorter(txs, block.BaseFee())
+    // 	sort.Sort(sorter)
+
+    // 	var prices []*big.Int
+    // 	for _, tx := range sorter.txs {
+    // 		tip, _ := tx.EffectiveGasTip(block.BaseFee())
+    // 		if ignoreUnder != nil && tip.Cmp(ignoreUnder) == -1 {
+    // 			continue
+    // 		}
+    // 		sender, err := types.Sender(signer, tx)
+    // 		if err == nil && sender != block.Coinbase() {
+    // 			prices = append(prices, tip)
+    // 			if len(prices) >= limit {
+    // 				break
+    // 			}
+    // 		}
+    // 	}
+    // 	select {
+    // 	case result <- results{prices, nil}:
+    // 	case <-quit:
+    // 	}
+    // }
+    fn get_block_values(&self, block_num: u64, limit: usize) -> Result<Option<Vec<Option<U256>>>> {
+        let block = match self.client.block_by_number(block_num)? {
+            Some(block) => block,
+            None => return Ok(None),
+        };
+
+        // sort the transactions by effective tip
+        // but first filter those that should be ignored
+        let txs = block.body.iter();
+        let mut txs = txs
+            .filter(|tx| {
+                if let Some(ignore_under) = self.oracle_config.ignore_price {
+                    if tx.effective_gas_tip(block.base_fee_per_gas).map(U256::from) <
+                        Some(ignore_under)
+                    {
+                        return true
+                    }
+                }
+
+                // recover sender, check if coinbase
+                let sender = tx.recover_signer();
+                match sender {
+                    Some(addr) => addr == block.beneficiary,
+                    // invalid signature - should not happen, but ignore?
+                    // TODO: figure out this case
+                    None => true,
+                }
+            })
+            .collect::<Vec<_>>();
+
+        // now do the sort
+        // TODO: could we cache tx effective_gas_tip? might be a lot of complexity for little use
+        txs.sort_by(|first, second| {
+            first
+                .effective_gas_tip(block.base_fee_per_gas)
+                .cmp(&second.effective_gas_tip(block.base_fee_per_gas))
+        });
+
+        Ok(Some(
+            txs[..limit]
+                .iter()
+                .map(|tx| tx.effective_gas_tip(block.base_fee_per_gas).map(U256::from))
+                .collect(),
+        ))
+    }
 }
 
 impl<Client, Tasks> Future for GasPriceOracleService<Client, Tasks>
@@ -172,7 +277,12 @@ where
                 None => {
                     unreachable!("can't close")
                 }
-                Some(action) => match action {},
+                Some(action) => match action {
+                    GasPriceAction::SuggestMaxPriorityFee { response_tx } => {
+                        todo!()
+                        // let _ = response_tx.send(result);
+                    }
+                },
             }
         }
     }
