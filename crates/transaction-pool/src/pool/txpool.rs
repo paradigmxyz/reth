84227752933021a1c295d90690ff36524ab9a821
Matthias Seitz <matthias.seitz@outlook.de>
2023-05-09 14:38:55 +0200

    simplify update metrics

--- crates/transaction-pool/src/pool/txpool.rs
+++ crates/transaction-pool/src/pool/txpool.rs
@@ -253,15 +253,15 @@ impl<T: TransactionOrdering> TxPool<T> {
         // Process the sub-pool updates
         let UpdateOutcome { promoted, discarded } = self.process_updates(updates);
 
-        // Compute pool metrics
-        let stats = self.size();
-        self.update_size_metrics(stats);
+        // update the metrics after the update
+        self.update_size_metrics();
 
         OnNewCanonicalStateOutcome { block_hash, mined: mined_transactions, promoted, discarded }
     }
 
     /// Update sub-pools size metrics.
-    pub(crate) fn update_size_metrics(&mut self, stats: PoolSize) {
+    pub(crate) fn update_size_metrics(&mut self) {
+        let stats = self.size();
         self.metrics.pending_pool_transactions.set(stats.pending as f64);
         self.metrics.pending_pool_size_bytes.set(stats.pending_size as f64);
         self.metrics.basefee_pool_transactions.set(stats.basefee as f64);
