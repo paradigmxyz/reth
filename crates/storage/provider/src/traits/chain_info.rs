fa716d695888df2139bf3bd8bfbda7e3cad92f91
Matthias Seitz <matthias.seitz@outlook.de>
2023-05-12 16:11:59 +0200

    feat: add getter for last FCU timestamp

--- crates/storage/provider/src/traits/chain_info.rs
+++ crates/storage/provider/src/traits/chain_info.rs
@@ -1,10 +1,15 @@
 use reth_interfaces::consensus::ForkchoiceState;
 use reth_primitives::SealedHeader;
+use std::time::Instant;
 
 /// A type that can track updates related to fork choice updates.
 pub trait CanonChainTracker: Send + Sync {
     /// Notify the tracker about a received fork choice update.
     fn on_forkchoice_update_received(&self, update: &ForkchoiceState);
+
+    /// Returns the last time a fork choice update was received from the CL
+    /// ([CanonChainTracker::on_forkchoice_update_received])
+    fn last_received_update_timestamp(&self) -> Option<Instant>;
 
     /// Sets the canonical head of the chain.
     fn set_canonical_head(&self, header: SealedHeader);
