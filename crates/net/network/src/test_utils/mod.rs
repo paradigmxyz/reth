7bf06a2c6cbc8d8d5b153880782e87d6a18676d5
Matthias Seitz <matthias.seitz@outlook.de>
2023-05-18 15:38:37 +0200

    test: ensure port is unused for udp and tcp

--- crates/net/network/src/test_utils/mod.rs
+++ crates/net/network/src/test_utils/mod.rs
@@ -5,5 +5,8 @@
 mod init;
 mod testnet;
 
-pub use init::{enr_to_peer_id, unused_port, unused_tcp_udp, GETH_TIMEOUT};
+pub use init::{
+    enr_to_peer_id, unused_port, unused_tcp_addr, unused_tcp_and_udp_port, unused_tcp_udp,
+    unused_udp_addr, unused_udp_port, GETH_TIMEOUT,
+};
 pub use testnet::{NetworkEventStream, PeerConfig, Testnet};
