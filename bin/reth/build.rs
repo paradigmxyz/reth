35814739e35ced672e52ea285d8f5eb298f92634
Oliver Nordbjerg <hi@notbjerg.me>
2023-05-19 19:13:19 +0200

    fix: make vergen emit cargo target triple

--- bin/reth/build.rs
+++ bin/reth/build.rs
@@ -3,6 +3,11 @@ use vergen::EmitBuilder;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Emit the instructions
-    EmitBuilder::builder().git_sha(true).build_timestamp().cargo_features().emit()?;
+    EmitBuilder::builder()
+        .git_sha(true)
+        .build_timestamp()
+        .cargo_features()
+        .cargo_target_triple()
+        .emit()?;
     Ok(())
 }
