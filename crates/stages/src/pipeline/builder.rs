a1380d40654b970a7978a02f2fb7b0e28370a698
Alexey Shekhirin <a.shekhirin@gmail.com>
2023-05-23 17:40:33 +0400

    chore(pipeline): remove `tip_tx` debugging-only comments

--- crates/stages/src/pipeline/builder.rs
+++ crates/stages/src/pipeline/builder.rs
@@ -13,9 +13,7 @@ where
     stages: Vec<BoxedStage<DB>>,
     /// The maximum block number to sync to.
     max_block: Option<BlockNumber>,
-    /// A receiver for the current chain tip to sync to
-    ///
-    /// Note: this is only used for debugging purposes.
+    /// A receiver for the current chain tip to sync to.
     tip_tx: Option<watch::Sender<H256>>,
 }
 
