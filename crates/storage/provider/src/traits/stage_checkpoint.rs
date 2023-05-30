c28b8a8ef916180df09f969f7012af91d322dc80
Roman Krasiuk <rokrassyuk@gmail.com>
2023-05-30 13:22:09 +0300

    address pr comments

--- crates/storage/provider/src/traits/stage_checkpoint.rs
+++ crates/storage/provider/src/traits/stage_checkpoint.rs
@@ -7,10 +7,10 @@ pub trait StageCheckpointProvider: Send + Sync {
     /// Fetch the checkpoint for the given stage.
     fn get_stage_checkpoint(&self, id: StageId) -> Result<Option<StageCheckpoint>>;
 
-    /// Fetch the checkpoint for the last stage ([reth_primitives::stage::StageId::Finish]).
+    /// Fetch the checkpoint for [reth_primitives::stage::StageId::Finish] stage.
     /// Should return `0` if no progress has been made yet.
     ///
-    /// NOTE: This checkpoint is always less than the checkpoint of any other stage. Hence it's used
-    /// to denote the minimum number of the block that was **fully** processed by the pipeline.
-    fn get_last_stage_checkpoint(&self) -> Result<BlockNumber>;
+    /// NOTE: This checkpoint is always `<=` to the checkpoint of any other stage. It essentially
+    /// represents the fully synced block.
+    fn get_finish_stage_checkpoint_block_number(&self) -> Result<BlockNumber>;
 }
