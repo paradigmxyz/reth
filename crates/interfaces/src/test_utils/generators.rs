6353792a8406110fe7b7921be3e34eb5e0327fee
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-06-02 05:37:21 -0400

    feat: add Transaction setters and separate random tx signing (#2951)

--- crates/interfaces/src/test_utils/generators.rs
+++ crates/interfaces/src/test_utils/generators.rs
@@ -72,6 +72,11 @@ pub fn random_signed_tx() -> TransactionSigned {
     let secp = Secp256k1::new();
     let key_pair = KeyPair::new(&secp, &mut rand::thread_rng());
     let tx = random_tx();
+    sign_tx_with_key_pair(key_pair, tx)
+}
+
+/// Signs the [Transaction] with the given key pair.
+pub fn sign_tx_with_key_pair(key_pair: KeyPair, tx: Transaction) -> TransactionSigned {
     let signature =
         sign_message(H256::from_slice(&key_pair.secret_bytes()[..]), tx.signature_hash()).unwrap();
     TransactionSigned::from_transaction_and_signature(tx, signature)
