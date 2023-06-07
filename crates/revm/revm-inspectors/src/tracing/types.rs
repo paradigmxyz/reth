11f01aeac004838cf2915dfabdb3168f0e9a550e
Matthias Seitz <matthias.seitz@outlook.de>
2023-06-08 01:32:11 +0200

    feat(rpc): track gas remaining (#3049)

--- crates/revm/revm-inspectors/src/tracing/types.rs
+++ crates/revm/revm-inspectors/src/tracing/types.rs
@@ -141,6 +141,8 @@ pub(crate) struct CallTrace {
     pub(crate) last_call_return_value: Option<Bytes>,
     /// The gas cost of the call
     pub(crate) gas_used: u64,
+    /// The gas limit of the call
+    pub(crate) gas_limit: u64,
     /// The status of the trace's call
     pub(crate) status: InstructionResult,
     /// call context of the runtime
@@ -176,6 +178,7 @@ impl Default for CallTrace {
             output: Default::default(),
             last_call_return_value: None,
             gas_used: Default::default(),
+            gas_limit: Default::default(),
             status: InstructionResult::Continue,
             call_context: Default::default(),
             steps: Default::default(),
@@ -347,7 +350,7 @@ impl CallTraceNode {
                     from: self.trace.caller,
                     to: self.trace.address,
                     value: self.trace.value,
-                    gas: self.trace.gas_used.into(),
+                    gas: self.trace.gas_limit.into(),
                     input: self.trace.data.clone().into(),
                     call_type: self.kind().into(),
                 })
@@ -355,7 +358,7 @@ impl CallTraceNode {
             CallKind::Create | CallKind::Create2 => Action::Create(CreateAction {
                 from: self.trace.caller,
                 value: self.trace.value,
-                gas: self.trace.gas_used.into(),
+                gas: self.trace.gas_limit.into(),
                 init: self.trace.data.clone().into(),
             }),
         }
