4bfbd3a53d0f91701d101894f5f11df559c32966
Filip Laurentiu <f.laurentiu26@gmail.com>
2023-06-09 19:21:06 +0300

    Set namespace in one place (#3084)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/rpc/rpc-api/src/net.rs
+++ crates/rpc/rpc-api/src/net.rs
@@ -2,19 +2,19 @@ use jsonrpsee::{core::RpcResult, proc_macros::rpc};
 use reth_rpc_types::PeerCount;
 
 /// Net rpc interface.
-#[cfg_attr(not(feature = "client"), rpc(server))]
-#[cfg_attr(feature = "client", rpc(server, client))]
+#[cfg_attr(not(feature = "client"), rpc(server, namespace = "net"))]
+#[cfg_attr(feature = "client", rpc(server, client, namespace = "net"))]
 pub trait NetApi {
     /// Returns protocol version.
-    #[method(name = "net_version")]
+    #[method(name = "version")]
     fn version(&self) -> RpcResult<String>;
 
     /// Returns number of peers connected to node.
-    #[method(name = "net_peerCount")]
+    #[method(name = "peerCount")]
     fn peer_count(&self) -> RpcResult<PeerCount>;
 
     /// Returns true if client is actively listening for network connections.
     /// Otherwise false.
-    #[method(name = "net_listening")]
+    #[method(name = "listening")]
     fn is_listening(&self) -> RpcResult<bool>;
 }
