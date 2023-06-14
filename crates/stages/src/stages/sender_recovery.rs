386f28e7b2ab07291d66f9c685b4fe12d16a078f
Matthias Seitz <matthias.seitz@outlook.de>
2023-06-14 15:31:32 +0200

    fix: chunk size edge case

--- crates/stages/src/stages/sender_recovery.rs
+++ crates/stages/src/stages/sender_recovery.rs
@@ -101,6 +101,11 @@ impl<DB: Database> Stage<DB> for SenderRecoveryStage {
         // Chunks are submitted instead of individual transactions to reduce the overhead of work
         // stealing in the threadpool workers.
         let chunk_size = self.commit_threshold as usize / rayon::current_num_threads();
+        // prevents an edge case
+        // where the chunk size is either 0 or too small
+        // to gain anything from using more than 1 thread
+        let chunk_size = chunk_size.max(16);
+
         for chunk in &tx_walker.chunks(chunk_size) {
             // An _unordered_ channel to receive results from a rayon job
             let (recovered_senders_tx, recovered_senders_rx) = mpsc::unbounded_channel();
