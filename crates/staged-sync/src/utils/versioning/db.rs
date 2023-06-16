e42ce36c2ece01c5d96b2edaa46a411b59bd475b
Alexey Shekhirin <a.shekhirin@gmail.com>
2023-06-16 17:18:20 +0100

    db.version -> database.version

--- crates/staged-sync/src/utils/versioning/db.rs
+++ crates/staged-sync/src/utils/versioning/db.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 /// The name of the file that contains the version of the database.
-const DB_VERSION_FILE_NAME: &str = "db.version";
+const DB_VERSION_FILE_NAME: &str = "database.version";
 /// The version of the database stored in the [DB_VERSION_FILE_NAME] file in the same directory as
 /// database. Example: `1`
 const DB_VERSION: u64 = 1;
