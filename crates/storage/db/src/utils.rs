d25b0a4ad8bc6b1493131957975ec2b9245b1783
Alexey Shekhirin <a.shekhirin@gmail.com>
2023-06-19 17:43:17 +0100

    feat(bin, storage): db versioning (#3130)

--- crates/storage/db/src/utils.rs
+++ crates/storage/db/src/utils.rs
@@ -1,5 +1,7 @@
 //! Utils crate for `db`.
 
+use std::path::Path;
+
 /// Returns the default page size that can be used in this OS.
 pub(crate) fn default_page_size() -> usize {
     let os_page_size = page_size::get();
@@ -12,4 +14,18 @@ pub(crate) fn default_page_size() -> usize {
     let min_page_size = 4096;
 
     os_page_size.clamp(min_page_size, libmdbx_max_page_size)
+}
+
+/// Check if a db is empty. It does not provide any information on the
+/// validity of the data in it. We consider a database as non empty when it's a non empty directory.
+pub fn is_database_empty<P: AsRef<Path>>(path: P) -> bool {
+    let path = path.as_ref();
+
+    if !path.exists() {
+        true
+    } else if let Ok(dir) = path.read_dir() {
+        dir.count() == 0
+    } else {
+        true
+    }
 }
