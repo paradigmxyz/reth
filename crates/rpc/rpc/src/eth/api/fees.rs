d3e7becb2a6dd37f1ad77a4eac0e332a97a2f46d
Oliver Nordbjerg <hi@notbjerg.me>
2023-06-21 00:23:54 +0200

    refactor: re-implement `eth_feeHistory`

--- crates/rpc/rpc/src/eth/api/fees.rs
+++ crates/rpc/rpc/src/eth/api/fees.rs
@@ -1,15 +1,14 @@
 //! Contains RPC handler implementations for fee history.
 
 use crate::{
-    eth::error::{EthApiError, EthResult, RpcInvalidTransactionError},
+    eth::error::{EthApiError, EthResult},
     EthApi,
 };
 use reth_network_api::NetworkInfo;
-use reth_primitives::{BlockId, BlockNumberOrTag, U256};
-use reth_provider::{BlockReaderIdExt, EvmEnvProvider, StateProviderFactory};
+use reth_primitives::{basefee::calculate_next_block_base_fee, BlockId, BlockNumberOrTag, U256};
+use reth_provider::{BlockProviderIdExt, BlockReaderIdExt, EvmEnvProvider, StateProviderFactory};
 use reth_rpc_types::{FeeHistory, FeeHistoryCacheItem, TxGasAndReward};
 use reth_transaction_pool::TransactionPool;
-use std::collections::BTreeMap;
 
 impl<Provider, Pool, Network> EthApi<Provider, Pool, Network>
 where
@@ -45,160 +44,113 @@ where
             return Ok(FeeHistory::default())
         }
 
-        let Some(previous_to_end_block) = self.inner.provider.block_number_for_id(newest_block)? else { return Err(EthApiError::UnknownBlockNumber)};
-        let end_block = previous_to_end_block + 1;
+        // The spec states that you can request a maximum of 1024 blocks.
+        if block_count > 1024 {
+            return Err(EthApiError::InvalidBlockRange)
+        }
 
+        let Some(end_block) = self.inner.provider.block_number_for_id(newest_block)? else { return Err(EthApiError::UnknownBlockNumber) };
+
+        // Check that we would not be querying outside of genesis
         if end_block < block_count {
             return Err(EthApiError::InvalidBlockRange)
         }
 
-        let mut start_block = end_block - block_count;
-
-        if block_count == 1 {
-            start_block = previous_to_end_block;
-        }
-
-        // if not provided the percentiles are []
-        let reward_percentiles = reward_percentiles.unwrap_or_default();
-
-        // checks for rewardPercentile's sorted-ness
-        // check if any of rewardPercentile is greater than 100
-        // pre 1559 blocks, return 0 for baseFeePerGas
-        for window in reward_percentiles.windows(2) {
-            if window[0] >= window[1] {
-                return Err(EthApiError::InvalidRewardPercentile(window[1]))
-            }
-
-            if window[0] < 0.0 || window[0] > 100.0 {
-                return Err(EthApiError::InvalidRewardPercentile(window[0]))
+        // If reward percentiles were specified, we need to validate that they are monotonically
+        // increasing and 0 <= p <= 100
+        //
+        // Note: The types used ensure that the percentiles are never < 0
+        if let Some(percentiles) = &reward_percentiles {
+            if percentiles.windows(2).any(|w| w[0] > w[1] || w[0] > 100.) {
+                return Err(EthApiError::InvalidRewardPercentiles)
             }
         }
 
-        let mut fee_history_cache = self.inner.fee_history_cache.0.lock().await;
-
-        // Sorted map that's populated in two rounds:
-        // 1. Cache entries until first non-cached block
-        // 2. Database query from the first non-cached block
-        let mut fee_history_cache_items = BTreeMap::new();
-
-        let mut first_non_cached_block = None;
-        let mut last_non_cached_block = None;
-        for block in start_block..=end_block {
-            // Check if block exists in cache, and move it to the head of the list if so
-            if let Some(fee_history_cache_item) = fee_history_cache.get(&block) {
-                fee_history_cache_items.insert(block, fee_history_cache_item.clone());
-            } else {
-                // If block doesn't exist in cache, set it as a first non-cached block to query it
-                // from the database
-                first_non_cached_block.get_or_insert(block);
-                // And last non-cached block, so we could query the database until we reach it
-                last_non_cached_block = Some(block);
-            }
+        // Fetch the headers and ensure we got all of them
+        let start_block = end_block - block_count;
+        let headers = self.inner.provider.headers_range(start_block..=end_block)?;
+        if headers.len() != block_count as usize {
+            return Err(EthApiError::InvalidBlockRange)
         }
 
-        // If we had any cache misses, query the database starting with the first non-cached block
-        // and ending with the last
-        if let (Some(start_block), Some(end_block)) =
-            (first_non_cached_block, last_non_cached_block)
-        {
-            let header_range = start_block..=end_block;
+        // Collect base fees, gas usage ratios and (optionally) reward percentile data
+        let mut base_fee_per_gas: Vec<U256> = Vec::new();
+        let mut gas_used_ratio: Vec<f64> = Vec::new();
+        let mut rewards: Vec<Vec<U256>> = Vec::new();
+        for header in &headers {
+            let header_base_fee_per_gas = header.base_fee_per_gas.unwrap_or_default();
+            // todo: calc next base reward since we need it
+            base_fee_per_gas.push(U256::try_from(header_base_fee_per_gas).unwrap());
+            gas_used_ratio.push(header.gas_used as f64 / header.gas_limit as f64);
 
-            let headers = self.inner.provider.headers_range(header_range.clone())?;
-            let transactions_by_block =
-                self.inner.provider.transactions_by_block_range(header_range)?;
+            // Percentiles were specified, so we need to collect reward percentile ino
+            if let Some(percentiles) = &reward_percentiles {
+                let Some(receipts) =
+                    self.inner.provider.receipts_by_block(header.number.into())? else {
+                    // If there are no receipts, then we do not have all info on the block
+                    return Err(EthApiError::InvalidBlockRange)
+                };
+                let Some(mut transactions): Option<Vec<_>> = self
+                    .inner
+                    .provider
+                    .transactions_by_block(header.number.into())?
+                    .map(|txs|txs
+                    .into_iter()
+                    .zip(receipts.into_iter())
+                    .map(|(tx, receipt)| TxGasAndReward {
+                        gas_used: header.gas_used - receipt.cumulative_gas_used,
+                        reward: tx.effective_gas_tip(header.base_fee_per_gas).unwrap_or_default(),
+                    })
+                    .collect()) else {
+                        // If there are no transactions, then we do not have all info on the block
+                        return Err(EthApiError::InvalidBlockRange)
+                    };
 
-            let header_tx = headers.iter().zip(&transactions_by_block);
+                // Sort the transactions by their rewards in ascending order
+                transactions.sort_by_key(|tx| tx.reward);
 
-            // We should receive exactly the amount of blocks missing from the cache
-            if headers.len() != (end_block - start_block + 1) as usize {
-                return Err(EthApiError::InvalidBlockRange)
-            }
-
-            // We should receive exactly the amount of blocks missing from the cache
-            if transactions_by_block.len() != (end_block - start_block + 1) as usize {
-                return Err(EthApiError::InvalidBlockRange)
-            }
-
-            for (header, transactions) in header_tx {
-                let base_fee_per_gas: U256 = header.base_fee_per_gas.
-                        unwrap_or_default(). // Zero for pre-EIP-1559 blocks
-                        try_into().unwrap(); // u64 -> U256 won't fail
-                let gas_used_ratio = header.gas_used as f64 / header.gas_limit as f64;
-
-                let mut sorter = Vec::with_capacity(transactions.len());
-                for transaction in transactions.iter() {
-                    let reward = transaction
-                        .effective_gas_tip(header.base_fee_per_gas)
-                        .ok_or(RpcInvalidTransactionError::FeeCapTooLow)?;
-
-                    sorter.push(TxGasAndReward { gas_used: header.gas_used as u128, reward })
-                }
-
-                sorter.sort();
-
-                let mut rewards = Vec::with_capacity(reward_percentiles.len());
-                let mut sum_gas_used = sorter.first().map(|tx| tx.gas_used).unwrap_or_default();
+                // Find the transaction that corresponds to the given percentile
+                //
+                // We use a `tx_index` here that is shared across all percentiles, since we know
+                // the percentiles are monotonically increasing.
                 let mut tx_index = 0;
-
-                for percentile in reward_percentiles.iter() {
-                    let threshold_gas_used = (header.gas_used as f64) * percentile / 100_f64;
-                    while sum_gas_used < threshold_gas_used as u128 && tx_index < transactions.len()
-                    {
-                        tx_index += 1;
-                        sum_gas_used += sorter[tx_index].gas_used;
+                let mut cumulative_gas_used =
+                    transactions.first().map(|tx| tx.gas_used).unwrap_or_default();
+                let mut rewards_in_block = Vec::new();
+                for percentile in percentiles {
+                    // Empty blocks should return in a zero row
+                    if transactions.is_empty() {
+                        rewards_in_block.push(U256::ZERO);
+                        continue
                     }
 
-                    rewards.push(U256::from(sorter[tx_index].reward));
+                    let threshold = (header.gas_used as f64 * percentile / 100.) as u64;
+                    while cumulative_gas_used < threshold && tx_index < transactions.len() - 1 {
+                        tx_index += 1;
+                        cumulative_gas_used += transactions[tx_index].gas_used
+                    }
+                    rewards_in_block.push(U256::from(transactions[tx_index].reward));
                 }
-
-                let fee_history_cache_item = FeeHistoryCacheItem {
-                    hash: None,
-                    base_fee_per_gas,
-                    gas_used_ratio,
-                    reward: Some(rewards),
-                };
-
-                // Insert missing cache entries in the map for further response composition from
-                // it
-                fee_history_cache_items.insert(header.number, fee_history_cache_item.clone());
-                // And populate the cache with new entries
-                fee_history_cache.push(header.number, fee_history_cache_item);
+                rewards.push(rewards_in_block);
             }
         }
 
-        // get the first block in the range from the db
-        let oldest_block_hash =
-            self.inner.provider.block_hash(start_block)?.ok_or(EthApiError::UnknownBlockNumber)?;
-
-        // Set the hash in cache items if the block is present in the cache
-        if let Some(cache_item) = fee_history_cache_items.get_mut(&start_block) {
-            cache_item.hash = Some(oldest_block_hash);
-        }
-
-        if let Some(cache_item) = fee_history_cache.get_mut(&start_block) {
-            cache_item.hash = Some(oldest_block_hash);
-        }
-
-        // `fee_history_cache_items` now contains full requested block range (populated from both
-        // cache and database), so we can iterate over it in order and populate the response fields
-        let base_fee_per_gas =
-            fee_history_cache_items.values().map(|item| item.base_fee_per_gas).collect();
-
-        let mut gas_used_ratio: Vec<f64> =
-            fee_history_cache_items.values().map(|item| item.gas_used_ratio).collect();
-
-        let mut rewards: Vec<Vec<_>> =
-            fee_history_cache_items.values().filter_map(|item| item.reward.clone()).collect();
-
-        // gasUsedRatio doesn't have data for next block in this case the last block
-        gas_used_ratio.pop();
-        rewards.pop();
+        // The spec states that `base_fee_per_gas` "[..] includes the next block after the newest of
+        // the returned range, because this value can be derived from the newest block"
+        //
+        // The unwrap is safe since we checked earlier that we got at least 1 header.
+        let last_header = headers.last().unwrap();
+        base_fee_per_gas.push(U256::from(calculate_next_block_base_fee(
+            last_header.gas_used,
+            last_header.gas_limit,
+            last_header.base_fee_per_gas.unwrap_or_default(),
+        )));
 
         Ok(FeeHistory {
             base_fee_per_gas,
             gas_used_ratio,
             oldest_block: U256::from(start_block),
-            reward: Some(rewards),
+            reward: reward_percentiles.map(|_| rewards),
         })
     }
 }
