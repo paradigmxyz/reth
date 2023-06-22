795d261c0a62f302644e9bd66ad662e0636531b4
Roman Krasiuk <rokrassyuk@gmail.com>
2023-06-22 15:02:36 +0300

    chore(cli): display latest available block number in status logs (#3316)

--- bin/reth/src/debug_cmd/execution.rs
+++ bin/reth/src/debug_cmd/execution.rs
@@ -234,26 +234,27 @@ impl Command {
             &ctx.task_executor,
         )?;
 
+        let factory = ProviderFactory::new(&db, self.chain.clone());
+        let provider = factory.provider().map_err(PipelineError::Interface)?;
+
+        let latest_block_number =
+            provider.get_stage_checkpoint(StageId::Finish)?.map(|ch| ch.block_number);
+        if latest_block_number.unwrap_or_default() >= self.to {
+            info!(target: "reth::cli", latest = latest_block_number, "Nothing to run");
+            return Ok(())
+        }
+
         let pipeline_events = pipeline.events();
         let events = stream_select(
             network.event_listener().map(Into::into),
             pipeline_events.map(Into::into),
         );
-        ctx.task_executor
-            .spawn_critical("events task", events::handle_events(Some(network.clone()), events));
+        ctx.task_executor.spawn_critical(
+            "events task",
+            events::handle_events(Some(network.clone()), latest_block_number, events),
+        );
 
-        let factory = ProviderFactory::new(&db, self.chain.clone());
-        let provider = factory.provider().map_err(PipelineError::Interface)?;
-
-        let latest_block_number =
-            provider.get_stage_checkpoint(StageId::Finish)?.unwrap_or_default().block_number;
-        if latest_block_number >= self.to {
-            info!(target: "reth::cli", latest = latest_block_number, "Nothing to run");
-            return Ok(())
-        }
-
-        let mut current_max_block = latest_block_number;
-
+        let mut current_max_block = latest_block_number.unwrap_or_default();
         while current_max_block < self.to {
             let next_block = current_max_block + 1;
             let target_block = self.to.min(current_max_block + self.interval);
