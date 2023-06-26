091f4d2e448088df049b187583c0372ac6e69b3e
Léo Vincent <28714795+leovct@users.noreply.github.com>
2023-06-26 17:41:11 +0200

    doc: add reth logo to docs (#3317)
    Co-authored-by: Oliver Nordbjerg <hi@notbjerg.me>

--- crates/storage/db/src/lib.rs
+++ crates/storage/db/src/lib.rs
@@ -1,3 +1,9 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
+    html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
+    issue_tracker_base_url = "https://github.com/paradigmxzy/reth/issues/"
+)]
 //! reth's database abstraction layer with concrete implementations.
 //!
 //! The database abstraction assumes that the underlying store is a KV store subdivided into tables.
