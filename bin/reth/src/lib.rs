d3d44fd46f03481ae0ab42adc5e6334ee64cd3b5
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2023-06-27 01:21:34 +0200

    feat(cli): clean up log level features (#3414)

--- bin/reth/src/lib.rs
+++ bin/reth/src/lib.rs
@@ -16,8 +16,12 @@
 //!   and leak detection functionality. See [jemalloc's opt.prof](https://jemalloc.net/jemalloc.3.html#opt.prof)
 //!   documentation for usage details. This is **not recommended on Windows**. See [here](https://rust-lang.github.io/rfcs/1974-global-allocators.html#jemalloc)
 //!   for more info.
-//! - `only-info-logs`: Disables all logs below `info` level. This can speed up the node, since
-//!   fewer calls to the logging component is made.
+//! - `min-error-logs`: Disables all logs below `error` level.
+//! - `min-warn-logs`: Disables all logs below `warn` level.
+//! - `min-info-logs`: Disables all logs below `info` level. This can speed up the node, since fewer
+//!   calls to the logging component is made.
+//! - `min-debug-logs`: Disables all logs below `debug` level.
+//! - `min-trace-logs`: Disables all logs below `trace` level.
 
 pub mod args;
 pub mod chain;
