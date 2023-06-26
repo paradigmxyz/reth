738e54ac5d8e8a933fc8d266ba7c80a58b9d894f
Oliver Nordbjerg <hi@notbjerg.me>
2023-06-26 17:06:17 +0200

    feat: use new logo

--- crates/tracing/src/lib.rs
+++ crates/tracing/src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc(
-    html_logo_url = "https://avatars0.githubusercontent.com/u/97369466?s=128",
+    html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
     html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
     issue_tracker_base_url = "https://github.com/paradigmxzy/reth/issues/"
 )]
