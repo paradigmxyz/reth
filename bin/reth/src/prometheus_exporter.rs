c3432e57e5425a01bb54a91a10870747aa8d7d24
Alexey Shekhirin <a.shekhirin@gmail.com>
2023-06-27 19:53:30 +0100

    add jemalloc metrics

--- bin/reth/src/prometheus_exporter.rs
+++ bin/reth/src/prometheus_exporter.rs
@@ -106,19 +106,107 @@ pub(crate) async fn initialize(
         });
     };
 
-    {
-        let process = process.clone();
-        let hooks: Vec<Box<dyn Hook<Output = ()>>> =
-            vec![Box::new(db_stats), Box::new(move || process.collect())];
-        initialize_with_hooks(listen_addr, hooks).await?;
-    }
+    // Clone `process` to move it into the hook and use the original `process` for describe below.
+    let cloned_process = process.clone();
+    let hooks: Vec<Box<dyn Hook<Output = ()>>> = vec![
+        Box::new(db_stats),
+        Box::new(move || cloned_process.collect()),
+        Box::new(collect_memory_stats),
+    ];
+    initialize_with_hooks(listen_addr, hooks).await?;
 
     // We describe the metrics after the recorder is installed, otherwise this information is not
     // registered
     describe_counter!("db.table_size", Unit::Bytes, "The size of a database table (in bytes)");
     describe_counter!("db.table_pages", "The number of database pages for a table");
-
     process.describe();
+    describe_memory_stats();
 
     Ok(())
 }
+
+#[cfg(feature = "jemalloc")]
+fn collect_memory_stats() {
+    use jemalloc_ctl::stats;
+    use reth_metrics::metrics::gauge;
+    use tracing::error;
+
+    if let Ok(value) = stats::active::read()
+        .map_err(|error| error!(?error, "Failed to read jemalloc.stats.active"))
+    {
+        gauge!("jemalloc.active", value as f64);
+    }
+
+    if let Ok(value) = stats::allocated::read()
+        .map_err(|error| error!(?error, "Failed to read jemalloc.stats.allocated"))
+    {
+        gauge!("jemalloc.allocated", value as f64);
+    }
+
+    if let Ok(value) = stats::mapped::read()
+        .map_err(|error| error!(?error, "Failed to read jemalloc.stats.mapped"))
+    {
+        gauge!("jemalloc.mapped", value as f64);
+    }
+
+    if let Ok(value) = stats::metadata::read()
+        .map_err(|error| error!(?error, "Failed to read jemalloc.stats.metadata"))
+    {
+        gauge!("jemalloc.metadata", value as f64);
+    }
+
+    if let Ok(value) = stats::resident::read()
+        .map_err(|error| error!(?error, "Failed to read jemalloc.stats.resident"))
+    {
+        gauge!("jemalloc.resident", value as f64);
+    }
+
+    if let Ok(value) = stats::retained::read()
+        .map_err(|error| error!(?error, "Failed to read jemalloc.stats.retained"))
+    {
+        gauge!("jemalloc.retained", value as f64);
+    }
+}
+
+#[cfg(feature = "jemalloc")]
+fn describe_memory_stats() {
+    use reth_metrics::metrics::describe_gauge;
+
+    describe_gauge!(
+        "jemalloc.active",
+        Unit::Bytes,
+        "Total number of bytes in active pages allocated by the application"
+    );
+    describe_gauge!(
+        "jemalloc.allocated",
+        Unit::Bytes,
+        "Total number of bytes allocated by the application"
+    );
+    describe_gauge!(
+        "jemalloc.mapped",
+        Unit::Bytes,
+        "Total number of bytes in active extents mapped by the allocator"
+    );
+    describe_gauge!(
+        "jemalloc.metadata",
+        Unit::Bytes,
+        "Total number of bytes dedicated to jemalloc metadata"
+    );
+    describe_gauge!(
+        "jemalloc.resident",
+        Unit::Bytes,
+        "Total number of bytes in physically resident data pages mapped by the allocator"
+    );
+    describe_gauge!(
+        "jemalloc.retained",
+        Unit::Bytes,
+        "Total number of bytes in virtual memory mappings that were retained rather than \
+        being returned to the operating system via e.g. munmap(2)"
+    );
+}
+
+#[cfg(not(feature = "jemalloc"))]
+fn collect_memory_stats() {}
+
+#[cfg(not(feature = "jemalloc"))]
+fn describe_memory_stats() {}
