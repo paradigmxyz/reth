78e1d1d394832d08853ce352b3b881e1a8b6cff1
Alexey Shekhirin <a.shekhirin@gmail.com>
2023-06-28 17:15:51 +0100

    remove manual Compact impl for PruneMode

--- crates/primitives/src/prune/mode.rs
+++ crates/primitives/src/prune/mode.rs
@@ -1,11 +1,9 @@
 use crate::BlockNumber;
-use bytes::{Buf, BufMut};
-use reth_codecs::{derive_arbitrary, Compact};
-use serde::{Deserialize, Serialize};
+use reth_codecs::{main_codec, Compact};
 
 /// Prune mode.
-#[derive_arbitrary(compact)]
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Serialize, Deserialize)]
+#[main_codec]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 #[serde(rename_all = "lowercase")]
 pub enum PruneMode {
     /// Prune all blocks.
@@ -20,42 +18,6 @@ pub enum PruneMode {
 impl Default for PruneMode {
     fn default() -> Self {
         Self::Distance(0)
-    }
-}
-
-impl Compact for PruneMode {
-    fn to_compact<B>(self, buf: &mut B) -> usize
-    where
-        B: BufMut + AsMut<[u8]>,
-    {
-        match self {
-            PruneMode::Full => {
-                buf.put_u8(0);
-                1
-            }
-            PruneMode::Distance(distance) => {
-                buf.put_u8(1);
-                buf.put_u64(distance);
-                1 + 8
-            }
-            PruneMode::Before(block_number) => {
-                buf.put_u8(2);
-                buf.put_u64(block_number);
-                1 + 8
-            }
-        }
-    }
-
-    fn from_compact(mut buf: &[u8], _len: usize) -> (Self, &[u8])
-    where
-        Self: Sized,
-    {
-        match buf.get_u8() {
-            0 => (Self::Full, buf),
-            1 => (Self::Distance(buf.get_u64()), buf),
-            2 => (Self::Before(buf.get_u64()), buf),
-            _ => unreachable!("Junk data in database: unknown PruneMode variant"),
-        }
     }
 }
 
