582b56da0729f7543116191ede776dc08278a4ea
nicolas <48695862+merklefruit@users.noreply.github.com>
2023-06-29 00:35:17 +0200

    feat: parse l1 block info from l2 block

--- crates/revm/src/optimism.rs
+++ crates/revm/src/optimism.rs
@@ -1,69 +1,102 @@
 use std::str::FromStr;
 
-use reth_primitives::{Address, TransactionSigned, U256};
-use revm::db::DatabaseRef;
+use reth_interfaces::executor;
+use reth_primitives::{Address, Block, TransactionKind, TransactionSigned, U256};
 
 const L1_FEE_RECIPIENT: &str = "0x420000000000000000000000000000000000001A";
 const BASE_FEE_RECIPIENT: &str = "0x4200000000000000000000000000000000000019";
 const L1_BLOCK_CONTRACT: &str = "0x4200000000000000000000000000000000000015";
 
-const L1_BASE_FEE_SLOT: u64 = 1;
-const OVERHEAD_SLOT: u64 = 5;
-const SCALAR_SLOT: u64 = 6;
 const ZERO_BYTE_COST: u64 = 4;
 const NON_ZERO_BYTE_COST: u64 = 16;
 
-/// Optimism Gas Cost Oracle
+/// L1 block info
 ///
-/// This is used to calculate the gas cost of a transaction on the L1 chain.
-/// The L1 block contract on L2 is used to propagate changes to the gas cost over time,
-/// so we need to fetch this data from the DB.
+/// We can extract L1 epoch data from each L2 block, by looking at the `setL1BlockValues`
+/// transaction data. This data is then used to calculate the L1 cost of a transaction.
 ///
-/// To make this slightly more efficient when executing entire blocks, the values are cached and
-/// only fetched again when the block number changes.
+/// Here is the format of the `setL1BlockValues` transaction data:
+///
+/// setL1BlockValues(uint64 _number, uint64 _timestamp, uint256 _basefee, bytes32 _hash,
+/// uint64 _sequenceNumber, bytes32 _batcherHash, uint256 _l1FeeOverhead, uint256 _l1FeeScalar)
+///
+/// For now, we only care about the fields necessary for L1 cost calculation.
 #[derive(Default)]
-pub struct L1GasCostOracle {
-    /// The cached block number
-    block_number: Option<u64>,
-    /// The base fee of the L1 chain
+pub struct L1BlockInfo {
     l1_base_fee: U256,
-    /// The overhead value used to calculate the gas cost
-    overhead: U256,
-    /// The scalar value used to calculate the gas cost
-    scalar: U256,
+    l1_fee_overhead: U256,
+    l1_fee_scalar: U256,
 }
 
-impl L1GasCostOracle {
+impl L1BlockInfo {
+    /// Create a new L1 block info struct from a L2 block
+    pub fn new(block: &Block) -> Result<Self, executor::Error> {
+        let l1_block_contract = Address::from_str(L1_BLOCK_CONTRACT).unwrap();
+
+        let l1_info_tx_data = block
+            .body
+            .iter()
+            .find(|tx| matches!(tx.kind(), TransactionKind::Call(to) if to == &l1_block_contract))
+            .ok_or(executor::Error::L1BlockInfoError {
+                message: "could not find l1 block info tx in the L2 block".to_string(),
+            })
+            .and_then(|tx| {
+                tx.input().get(4..).ok_or(executor::Error::L1BlockInfoError {
+                    message: "could not get l1 block info tx calldata bytes".to_string(),
+                })
+            })?;
+
+        // The setL1BlockValues tx calldata must be exactly 184 bytes long, considering that
+        // we already removed the first 4 bytes (the function selector). Detailed breakdown:
+        //   8  bytes for the block number
+        // + 8  bytes for the block timestamp
+        // + 32 bytes for the base fee
+        // + 32 bytes for the block hash
+        // + 8  bytes for the block sequence number
+        // + 32 bytes for the batcher hash
+        // + 32 bytes for the fee overhead
+        // + 32 bytes for the fee scalar
+        if l1_info_tx_data.len() != 184 {
+            return Err(executor::Error::L1BlockInfoError {
+                message: "unexpected l1 block info tx calldata length found".to_string(),
+            })
+        }
+
+        let l1_base_fee = U256::try_from_le_slice(&l1_info_tx_data[16..48]).ok_or(
+            executor::Error::L1BlockInfoError {
+                message: "could not convert l1 base fee".to_string(),
+            },
+        )?;
+        let l1_fee_overhead = U256::try_from_le_slice(&l1_info_tx_data[120..152]).ok_or(
+            executor::Error::L1BlockInfoError {
+                message: "could not convert l1 fee overhead".to_string(),
+            },
+        )?;
+        let l1_fee_scalar = U256::try_from_le_slice(&l1_info_tx_data[152..184]).ok_or(
+            executor::Error::L1BlockInfoError {
+                message: "could not convert l1 fee scalar".to_string(),
+            },
+        )?;
+
+        Ok(Self { l1_base_fee, l1_fee_overhead, l1_fee_scalar })
+    }
+
     /// Calculate the gas cost of a transaction based on L1 block data posted on L2
-    pub fn calculate_l1_cost<DB: DatabaseRef>(
-        &mut self,
-        db: &mut DB,
-        block_num: u64,
-        tx: &TransactionSigned,
-    ) -> Result<U256, DB::Error> {
+    pub fn calculate_tx_l1_cost(&mut self, tx: &TransactionSigned) -> U256 {
         let rollup_data_gas_cost = U256::from(tx.input().iter().fold(0, |acc, byte| {
             acc + if byte == &0x00 { ZERO_BYTE_COST } else { NON_ZERO_BYTE_COST }
         }));
 
         if tx.is_deposit() || rollup_data_gas_cost == U256::ZERO {
-            return Ok(U256::ZERO)
+            return U256::ZERO
         }
 
-        if self.block_number.cmp(&Some(block_num)).is_ne() {
-            let l1_block_address = Address::from_str(L1_BLOCK_CONTRACT).unwrap();
-
-            // TODO: Check: Is the db data always up-to-date?
-            self.l1_base_fee = db.storage(l1_block_address, U256::from(L1_BASE_FEE_SLOT))?;
-            self.overhead = db.storage(l1_block_address, U256::from(OVERHEAD_SLOT))?;
-            self.scalar = db.storage(l1_block_address, U256::from(SCALAR_SLOT))?;
-        }
-
-        Ok(rollup_data_gas_cost
-            .saturating_add(self.overhead)
+        rollup_data_gas_cost
+            .saturating_add(self.l1_fee_overhead)
             .saturating_mul(self.l1_base_fee)
-            .saturating_mul(self.scalar)
+            .saturating_mul(self.l1_fee_scalar)
             .checked_div(U256::from(1_000_000))
-            .unwrap_or_default())
+            .unwrap_or_default()
     }
 }
 
