c72c3e6e77e6c6d8c68fddda4a9c48d121440766
Alexey Shekhirin <a.shekhirin@gmail.com>
2023-06-28 17:00:12 +0100

    remove prune_checkpoint_roundtrip

--- crates/primitives/src/prune/checkpoint.rs
+++ crates/primitives/src/prune/checkpoint.rs
@@ -11,26 +11,3 @@ pub struct PruneCheckpoint {
     /// Highest pruned block number.
     block_number: BlockNumber,
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::prune::{PruneCheckpoint, PruneMode};
-    use rand::Rng;
-    use reth_codecs::Compact;
-
-    #[test]
-    fn prune_checkpoint_roundtrip() {
-        let mut rng = rand::thread_rng();
-        let checkpoints = vec![
-            PruneCheckpoint { prune_mode: PruneMode::Distance(rng.gen()), block_number: rng.gen() },
-            PruneCheckpoint { prune_mode: PruneMode::Before(rng.gen()), block_number: rng.gen() },
-        ];
-
-        for checkpoint in checkpoints {
-            let mut buf = Vec::new();
-            let encoded = checkpoint.to_compact(&mut buf);
-            let (decoded, _) = PruneCheckpoint::from_compact(&buf, encoded);
-            assert_eq!(decoded, checkpoint);
-        }
-    }
-}
