fd8c93bd0302e40620d4a6a2d3e8735c45c6925f
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-06-29 18:55:04 -0400

    add from impl for stream

--- crates/interfaces/src/p2p/full_block.rs
+++ crates/interfaces/src/p2p/full_block.rs
@@ -513,6 +513,15 @@ where
     blocks: Option<Vec<SealedBlock>>,
 }
 
+impl<Client> From<FetchFullBlockRangeFuture<Client>> for FullBlockRangeStream<Client>
+where
+    Client: BodiesClient + HeadersClient,
+{
+    fn from(inner: FetchFullBlockRangeFuture<Client>) -> Self {
+        Self { inner, blocks: None }
+    }
+}
+
 impl<Client> Stream for FullBlockRangeStream<Client>
 where
     Client: BodiesClient + HeadersClient + Unpin + 'static,
@@ -739,4 +748,7 @@ mod tests {
             assert_eq!(block.header.number, expected_number);
         }
     }
+
+    #[tokio::test]
+    async fn download_full_block_range_stream() {}
 }
