de30b0ca13385272b60640245f4eae1b3e5597fe
Matthias Seitz <matthias.seitz@outlook.de>
2023-06-29 18:09:13 +0200

    test: run geth blacklist test also serial (#3469)

--- crates/net/network/tests/it/connect.rs
+++ crates/net/network/tests/it/connect.rs
@@ -309,6 +309,7 @@ async fn test_connect_to_trusted_peer() {
 }
 
 #[tokio::test(flavor = "multi_thread")]
+#[serial_test::serial]
 #[cfg_attr(not(feature = "geth-tests"), ignore)]
 async fn test_incoming_node_id_blacklist() {
     reth_tracing::init_test_tracing();
