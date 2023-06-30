5c7d5a3b3e81431a75eb5b910ea93a70904ab7bb
int88 <106391185+int88@users.noreply.github.com>
2023-06-30 20:01:30 +0800

    feat: make chain canonical if new payload is the missing block for current sync target (#3459)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/consensus/beacon/src/engine/mod.rs
+++ crates/consensus/beacon/src/engine/mod.rs
@@ -830,6 +830,7 @@ where
             Err(status) => return Ok(status),
         };
         let block_hash = block.hash();
+        let block_num_hash = block.num_hash();
 
         // now check the block itself
         if let Some(status) = self.check_invalid_ancestor_with_head(block.parent_hash, block.hash) {
@@ -847,6 +848,13 @@ where
         let status = match res {
             Ok(status) => {
                 if status.is_valid() {
+                    if let Some(target) = self.forkchoice_state_tracker.sync_target_state() {
+                        // if we're currently syncing and the inserted block is the targeted FCU
+                        // head block, we can try to make it canonical.
+                        if block_hash == target.head_block_hash {
+                            self.try_make_sync_target_canonical(block_num_hash);
+                        }
+                    }
                     // block was successfully inserted, so we can cancel the full block request, if
                     // any exists
                     self.sync.cancel_full_block_request(block_hash);
