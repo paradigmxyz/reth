b62a4b621e96adaf80d8b9a4975c3f5d8778e422
Enrique Ortiz <hi@enriqueortiz.dev>
2023-07-03 14:03:52 -0400

    chore: jettison staged-sync crate

--- crates/staged-sync/src/test_utils/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Common helpers for staged sync integration testing.
-
-pub mod clique;
-pub mod clique_middleware;
-
-pub use clique::CliqueGethInstance;
-pub use clique_middleware::{CliqueError, CliqueMiddleware, CliqueMiddlewareError};
