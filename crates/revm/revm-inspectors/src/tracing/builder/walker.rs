f5298c878682a5d94436c43db8e1a3e7878698eb
N <mail@nuhhtyy.xyz>
2023-07-04 12:45:10 -0400

    comment out vm trace walker for now

--- crates/revm/revm-inspectors/src/tracing/builder/walker.rs
+++ crates/revm/revm-inspectors/src/tracing/builder/walker.rs
@@ -30,13 +30,10 @@ pub(crate) struct CallTraceNodeWalker<'trace, W> {
 
 impl<'trace> CallTraceNodeWalker<'trace, DFWalk> {
     pub(crate) fn new(nodes: &'trace Vec<CallTraceNode>) -> Self {
-        let mut stack = Vec::with_capacity(nodes.len());
-        stack.push(0);
-
         Self {
             nodes,
             curr_idx: 0,
-            stack,
+            stack: Vec::with_capacity(nodes.len()),
             visited: vec![false; nodes.len()],
             phantom: std::marker::PhantomData,
         }
@@ -151,80 +148,80 @@ impl<'trace> Iterator for CallTraceNodeWalker<'trace, DFWalk> {
     }
 }
 
-#[derive(Debug)]
-pub(crate) struct VmTraceWalker<'trace, W> {
-    parents: Vec<Option<&'trace VmTrace>>,
-    current: &'trace VmTrace,
+// #[derive(Debug)]
+// pub(crate) struct VmTraceWalker<'trace, W> {
+//     parents: Vec<Option<&'trace VmTrace>>,
+//     current: &'trace VmTrace,
 
-    idx_stack: Vec<usize>,
-    curr_idx: usize,
-    started: bool,
+//     idx_stack: Vec<usize>,
+//     curr_idx: usize,
+//     started: bool,
 
-    phantom: std::marker::PhantomData<W>,
-}
+//     phantom: std::marker::PhantomData<W>,
+// }
 
-impl<'trace> VmTraceWalker<'trace, DFWalk> {
-    pub(crate) fn new(root: &'trace VmTrace) -> Self {
-        VmTraceWalker {
-            parents: vec![None],
-            current: root,
-            idx_stack: Vec::new(),
-            curr_idx: 0,
-            started: false,
+// impl<'trace> VmTraceWalker<'trace, DFWalk> {
+//     pub(crate) fn new(root: &'trace VmTrace) -> Self {
+//         VmTraceWalker {
+//             parents: vec![None],
+//             current: root,
+//             idx_stack: Vec::new(),
+//             curr_idx: 0,
+//             started: false,
 
-            phantom: std::marker::PhantomData,
-        }
-    }
-}
+//             phantom: std::marker::PhantomData,
+//         }
+//     }
+// }
 
-impl<'trace> Iterator for VmTraceWalker<'trace, DFWalk> {
-    type Item = &'trace VmTrace;
+// impl<'trace> Iterator for VmTraceWalker<'trace, DFWalk> {
+//     type Item = &'trace VmTrace;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.started {
-            true => loop {
-                match self.current.ops.get(self.curr_idx) {
-                    Some(op) => match &op.sub {
-                        Some(sub) => {
-                            self.parents.push(Some(self.current));
-                            self.current = &sub;
+//     fn next(&mut self) -> Option<Self::Item> {
+//         match self.started {
+//             true => loop {
+//                 match self.current.ops.get(self.curr_idx) {
+//                     Some(op) => match &op.sub {
+//                         Some(sub) => {
+//                             self.parents.push(Some(self.current));
+//                             self.current = &sub;
 
-                            self.idx_stack.push(self.curr_idx);
-                            self.curr_idx = 0;
-                        }
-                        // if theres no subcall continue to the next opcode
-                        None => {
-                            self.curr_idx += 1;
-                        }
-                    },
-                    // there is no more opcodes check for parent
-                    None => {
-                        match self.parents.pop().expect("There should be a parent here") {
-                            Some(parent) => {
-                                self.current = parent;
+//                             self.idx_stack.push(self.curr_idx);
+//                             self.curr_idx = 0;
+//                         }
+//                         // if theres no subcall continue to the next opcode
+//                         None => {
+//                             self.curr_idx += 1;
+//                         }
+//                     },
+//                     // there is no more opcodes check for parent
+//                     None => {
+//                         match self.parents.pop().expect("There should be a parent here") {
+//                             Some(parent) => {
+//                                 self.current = parent;
 
-                                self.curr_idx =
-                                    self.idx_stack.pop().expect("There should be an index here")
-                                        + 1;
-                            }
-                            // we are back at the root node, so we are done
-                            None => break,
-                        }
-                    }
-                }
-            },
-            false => {
-                self.started = true;
-            }
-        };
+//                                 self.curr_idx =
+//                                     self.idx_stack.pop().expect("There should be an index here")
+//                                         + 1;
+//                             }
+//                             // we are back at the root node, so we are done
+//                             None => break,
+//                         }
+//                     }
+//                 }
+//             },
+//             false => {
+//                 self.started = true;
+//             }
+//         };
 
-        Some(self.current)
-    }
-}
+//         Some(self.current)
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
-    use crate::tracing::builder::walker::{CallTraceNodeWalker, VmTraceWalker};
+    use crate::tracing::builder::walker::CallTraceNodeWalker;
     use crate::tracing::types::CallTraceNode;
     use crate::tracing::TracingInspectorConfig;
 
@@ -269,43 +266,43 @@ mod tests {
         assert_eq!(i, nodes.len());
     }
 
-    #[test]
-    fn test_vm_trace_len_eq_arena() {
-        let config = TracingInspectorConfig {
-            record_steps: false,
-            record_memory_snapshots: false,
-            record_stack_snapshots: false,
-            record_state_diff: false,
-            exclude_precompile_calls: false,
-            record_logs: false,
-        };
+    // #[test]
+    // fn test_vm_trace_len_eq_arena() {
+    //     let config = TracingInspectorConfig {
+    //         record_steps: false,
+    //         record_memory_snapshots: false,
+    //         record_stack_snapshots: false,
+    //         record_state_diff: false,
+    //         exclude_precompile_calls: false,
+    //         record_logs: false,
+    //     };
 
-        let nodes = vec![
-            CallTraceNode { idx: 0, parent: None, children: vec![1, 2], ..Default::default() },
-            CallTraceNode { idx: 1, parent: Some(0), children: vec![3], ..Default::default() },
-            CallTraceNode { idx: 2, parent: Some(0), children: vec![4], ..Default::default() },
-            CallTraceNode { idx: 3, parent: Some(1), children: vec![], ..Default::default() },
-            CallTraceNode { idx: 4, parent: Some(2), children: vec![], ..Default::default() },
-        ];
+    //     let nodes = vec![
+    //         CallTraceNode { idx: 0, parent: None, children: vec![1, 2], ..Default::default() },
+    //         CallTraceNode { idx: 1, parent: Some(0), children: vec![3], ..Default::default() },
+    //         CallTraceNode { idx: 2, parent: Some(0), children: vec![4], ..Default::default() },
+    //         CallTraceNode { idx: 3, parent: Some(1), children: vec![], ..Default::default() },
+    //         CallTraceNode { idx: 4, parent: Some(2), children: vec![], ..Default::default() },
+    //     ];
 
-        let mut walker = CallTraceNodeWalker::new(&nodes);
+    //     let mut walker = CallTraceNodeWalker::new(&nodes);
 
-        let vm_trace = walker.into_vm_trace(&config);
+    //     let vm_trace = walker.into_vm_trace(&config);
 
-        println!("here");
+    //     println!("here");
 
-        println!("trace: {:#?}", vm_trace);
+    //     println!("trace: {:#?}", vm_trace);
 
-        // let mut vm_trace_len = 0;
-        // for _ in VmTraceWalker::new(&vm_trace) {
-        //     vm_trace_len += 1;
-        // }
+    //     // let mut vm_trace_len = 0;
+    //     // for _ in VmTraceWalker::new(&vm_trace) {
+    //     //     vm_trace_len += 1;
+    //     // }
 
-        // let mut node_walk_len = 0;
-        // for _ in CallTraceNodeWalker::new(&nodes) {
-        //     node_walk_len += 1;
-        // }
+    //     // let mut node_walk_len = 0;
+    //     // for _ in CallTraceNodeWalker::new(&nodes) {
+    //     //     node_walk_len += 1;
+    //     // }
 
-        // assert_eq!(1, 0);
-    }
+    //     // assert_eq!(1, 0);
+    // }
 }
