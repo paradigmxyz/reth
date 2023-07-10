30e7586a5244d03eaeea58e51a2492d313178b42
Matthias Seitz <matthias.seitz@outlook.de>
2023-07-10 13:05:39 +0200

    rustfmt

--- crates/rpc/rpc-types/src/eth/engine/payload.rs
+++ crates/rpc/rpc-types/src/eth/engine/payload.rs
@@ -574,7 +574,7 @@ mod tests {
         let q = PayloadStatus {
             latest_valid_hash: None,
             status: PayloadStatusEnum::Invalid {
-                validation_error: "Failed to decode block".to_string()
+                validation_error: "Failed to decode block".to_string(),
             },
         };
         assert_eq!(q, serde_json::from_str(s).unwrap());
@@ -603,11 +603,14 @@ mod tests {
             latest_valid_hash: None,
             status: PayloadStatusEnum::Invalid {
                 validation_error: PayloadValidationError::InvalidStateRoot {
-                    remote:
-                        "0x3f77fb29ce67436532fee970e1add8f5cc80e8878c79b967af53b1fd92a0cab7".parse().unwrap(),
-                    local:
-                        "0x603b9628dabdaadb442a3bb3d7e0360efc110e1948472909230909f1690fed17".parse().unwrap(),
-                }.to_string(),
+                    remote: "0x3f77fb29ce67436532fee970e1add8f5cc80e8878c79b967af53b1fd92a0cab7"
+                        .parse()
+                        .unwrap(),
+                    local: "0x603b9628dabdaadb442a3bb3d7e0360efc110e1948472909230909f1690fed17"
+                        .parse()
+                        .unwrap(),
+                }
+                .to_string(),
             },
         };
         similar_asserts::assert_eq!(q, serde_json::from_str(s).unwrap());
