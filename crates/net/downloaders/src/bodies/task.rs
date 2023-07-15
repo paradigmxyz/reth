c9f88749c8109bdc0795ccdf5519d25e66e4222c
Roman Krasiuk <rokrassyuk@gmail.com>
2023-07-15 10:30:38 +0200

    clone PollSender to ensure availability

--- crates/net/downloaders/src/bodies/task.rs
+++ crates/net/downloaders/src/bodies/task.rs
@@ -120,9 +120,9 @@ impl<T: BodyDownloader> Future for SpawnedDownloader<T> {
                     if let Err(err) = this.downloader.set_download_range(range) {
                         tracing::error!(target: "downloaders::bodies", ?err, "Failed to set bodies download range");
 
-                        let forward_error_result = ready!(this.bodies_tx.poll_reserve(cx))
-                            .and_then(|_| this.bodies_tx.send_item(Err(err)));
-                        if forward_error_result.is_err() {
+                        // Clone the sender ensure its availability. See [PollSender::clone].
+                        let mut bodies_tx = this.bodies_tx.clone();
+                        if bodies_tx.send_item(Err(err)).is_err() {
                             // channel closed, this means [TaskDownloader] was dropped,
                             // so we can also exit
                             return Poll::Ready(())
