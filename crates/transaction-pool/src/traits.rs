1b3886d0c2d6682bc18e7e8366d52d87464354a0
Panagiotis Ganelis <50522617+PanGan21@users.noreply.github.com>
2023-07-18 18:02:13 +0300

    feat: add timestamp to pool update (#3833)

--- crates/transaction-pool/src/traits.rs
+++ crates/transaction-pool/src/traits.rs
@@ -402,6 +402,8 @@ pub struct CanonicalStateUpdate {
     pub changed_accounts: Vec<ChangedAccount>,
     /// All mined transactions in the block range.
     pub mined_transactions: Vec<H256>,
+    /// Timestamp of the latest chain update
+    pub timestamp: u64,
 }
 
 impl fmt::Display for CanonicalStateUpdate {
