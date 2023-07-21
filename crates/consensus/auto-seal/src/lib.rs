44c24c2b5b12692768ad253817908fd33873e530
pistomat <pistomat@users.noreply.github.com>
2023-07-21 23:13:50 +0200

    fmt

--- crates/consensus/auto-seal/src/lib.rs
+++ crates/consensus/auto-seal/src/lib.rs
@@ -27,9 +27,9 @@ use reth_interfaces::{
 };
 use reth_primitives::{
     constants::{EMPTY_RECEIPTS, EMPTY_TRANSACTIONS, ETHEREUM_BLOCK_GAS_LIMIT},
-    proofs, Address, Block, BlockBody, BlockHash, BlockHashOrNumber, BlockNumber, ChainSpec,
+    proofs, Address, Block, BlockBody, BlockHash, BlockHashOrNumber, BlockNumber, Bloom, ChainSpec,
     Header, ReceiptWithBloom, SealedBlock, SealedHeader, TransactionSigned, EMPTY_OMMER_ROOT, H256,
-    U256, Bloom,
+    U256,
 };
 use reth_provider::{BlockReaderIdExt, CanonStateNotificationSender, PostState, StateProvider};
 use reth_revm::executor::Executor;
@@ -315,7 +315,8 @@ impl StorageInner {
         } else {
             let receipts_with_bloom =
                 receipts.iter().map(|r| r.clone().into()).collect::<Vec<ReceiptWithBloom>>();
-            header.logs_bloom = receipts_with_bloom.iter().fold(Bloom::zero(), |bloom, r| bloom | r.bloom);
+            header.logs_bloom =
+                receipts_with_bloom.iter().fold(Bloom::zero(), |bloom, r| bloom | r.bloom);
             proofs::calculate_receipt_root(&receipts_with_bloom)
         };
 
