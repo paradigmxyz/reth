2bb76f87ecab04dc6dab828d4cffb1b4616ddb27
Alexey Shekhirin <a.shekhirin@gmail.com>
2023-07-26 17:50:38 +0100

    min_distance -> min_blocks

--- crates/primitives/src/prune/target.rs
+++ crates/primitives/src/prune/target.rs
@@ -11,7 +11,7 @@ pub struct PruneModes {
     // dynamically on blockchain tree unwind.
     #[serde(
         skip_serializing_if = "Option::is_none",
-        deserialize_with = "deserialize_opt_prune_mode_with_min_distance::<64, _>"
+        deserialize_with = "deserialize_opt_prune_mode_with_min_blocks::<64, _>"
     )]
     pub sender_recovery: Option<PruneMode>,
     /// Transaction Lookup pruning configuration.
