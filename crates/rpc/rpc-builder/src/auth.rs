49e112789ba0d769ce58648c8c10a2eec821bced
Matthias Seitz <matthias.seitz@outlook.de>
2023-07-26 18:33:59 +0200

    feat: use tracing pool for tracing calls (#3914)

--- crates/rpc/rpc-builder/src/auth.rs
+++ crates/rpc/rpc-builder/src/auth.rs
@@ -17,7 +17,7 @@ use reth_provider::{
 use reth_rpc::{
     eth::{cache::EthStateCache, gas_oracle::GasPriceOracle},
     AuthLayer, Claims, EngineEthApi, EthApi, EthFilter, EthSubscriptionIdProvider,
-    JwtAuthValidator, JwtSecret,
+    JwtAuthValidator, JwtSecret, TracingCallPool,
 };
 use reth_rpc_api::{servers::*, EngineApiServer};
 use reth_tasks::TaskSpawner;
@@ -64,6 +64,7 @@ where
         gas_oracle,
         EthConfig::default().rpc_gas_cap,
         Box::new(executor.clone()),
+        TracingCallPool::build().expect("failed to build tracing pool"),
     );
     let eth_filter = EthFilter::new(
         provider,
--- crates/rpc/rpc/src/eth/api/state.rs
+++ crates/rpc/rpc/src/eth/api/state.rs
@@ -146,7 +146,10 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::eth::{cache::EthStateCache, gas_oracle::GasPriceOracle};
+    use crate::{
+        eth::{cache::EthStateCache, gas_oracle::GasPriceOracle},
+        TracingCallPool,
+    };
     use reth_primitives::{constants::ETHEREUM_BLOCK_GAS_LIMIT, StorageKey, StorageValue};
     use reth_provider::test_utils::{ExtendedAccount, MockEthProvider, NoopProvider};
     use reth_transaction_pool::test_utils::testing_pool;
@@ -165,6 +168,7 @@ mod tests {
             cache.clone(),
             GasPriceOracle::new(NoopProvider::default(), Default::default(), cache),
             ETHEREUM_BLOCK_GAS_LIMIT,
+            TracingCallPool::build().expect("failed to build tracing pool"),
         );
         let address = Address::random();
         let storage = eth_api.storage_at(address, U256::ZERO.into(), None).unwrap();
@@ -186,6 +190,7 @@ mod tests {
             cache.clone(),
             GasPriceOracle::new(mock_provider, Default::default(), cache),
             ETHEREUM_BLOCK_GAS_LIMIT,
+            TracingCallPool::build().expect("failed to build tracing pool"),
         );
 
         let storage_key: U256 = storage_key.into();
