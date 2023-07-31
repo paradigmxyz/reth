c20b10aeb69f53d47de26c80e4715a3f33309165
Alexey Shekhirin <a.shekhirin@gmail.com>
2023-07-31 17:32:46 +0100

    use walker

--- crates/prune/src/pruner.rs
+++ crates/prune/src/pruner.rs
@@ -177,7 +177,7 @@ impl<DB: Database> Pruner<DB> {
         let total = range.clone().count();
 
         let mut processed = 0;
-        provider.prune_table_in_batches::<tables::Receipts, _>(
+        provider.prune_table_with_iterator_in_batches::<tables::Receipts, _>(
             range,
             self.batch_sizes.receipts,
             |entries| {
@@ -245,7 +245,7 @@ impl<DB: Database> Pruner<DB> {
             // Pre-sort hashes to prune them in order
             hashes.sort_unstable();
 
-            let entries = provider.prune_table::<tables::TxHashNumber, _>(hashes)?;
+            let entries = provider.prune_table_with_iterator::<tables::TxHashNumber, _>(hashes)?;
             processed += entries;
             trace!(
                 target: "pruner",
@@ -279,7 +279,7 @@ impl<DB: Database> Pruner<DB> {
         let total = range.clone().count();
 
         let mut processed = 0;
-        provider.prune_dupsort_table_in_batches::<tables::AccountChangeSet, _>(
+        provider.prune_table_with_range_in_batches::<tables::AccountChangeSet, _>(
             range,
             self.batch_sizes.account_history,
             |entries| {
--- crates/storage/provider/src/providers/database/provider.rs
+++ crates/storage/provider/src/providers/database/provider.rs
@@ -17,7 +17,7 @@ use reth_db::{
         sharded_key, storage_sharded_key::StorageShardedKey, AccountBeforeTx, BlockNumberAddress,
         ShardedKey, StoredBlockBodyIndices, StoredBlockOmmers, StoredBlockWithdrawals,
     },
-    table::{DupSort, Key, Table},
+    table::{Key, Table},
     tables,
     transaction::{DbTx, DbTxMut},
     BlockNumberList, DatabaseError,
@@ -621,7 +621,7 @@ impl<'this, TX: DbTxMut<'this> + DbTx<'this>> DatabaseProvider<'this, TX> {
 
     /// Prune the table for the specified pre-sorted key iterator.
     /// Returns number of rows pruned.
-    pub fn prune_table<T, K>(
+    pub fn prune_table_with_iterator<T, K>(
         &self,
         keys: impl IntoIterator<Item = K>,
     ) -> std::result::Result<usize, DatabaseError>
@@ -629,14 +629,14 @@ impl<'this, TX: DbTxMut<'this> + DbTx<'this>> DatabaseProvider<'this, TX> {
         T: Table<Key = K>,
         K: Key,
     {
-        self.prune_table_in_batches::<T, K>(keys, usize::MAX, |_| {})
+        self.prune_table_with_iterator_in_batches::<T, K>(keys, usize::MAX, |_| {})
     }
 
     /// Prune the table for the specified pre-sorted key iterator, calling `chunk_callback` after
     /// every `batch_size` pruned rows.
     ///
     /// Returns number of rows pruned.
-    pub fn prune_table_in_batches<T, K>(
+    pub fn prune_table_with_iterator_in_batches<T, K>(
         &self,
         keys: impl IntoIterator<Item = K>,
         batch_size: usize,
@@ -667,40 +667,30 @@ impl<'this, TX: DbTxMut<'this> + DbTx<'this>> DatabaseProvider<'this, TX> {
         Ok(deleted)
     }
 
-    /// Prune the dupsort table for the specified pre-sorted key iterator, calling `chunk_callback`
-    /// after every `batch_size` pruned rows (subkeys).
+    /// Prune the table for the specified key range, calling `chunk_callback` after every
+    /// `batch_size` pruned rows.
     ///
-    /// Returns number of rows (subkeys) pruned.
-    pub fn prune_dupsort_table_in_batches<T, K>(
+    /// Returns number of rows pruned.
+    pub fn prune_table_with_range_in_batches<T, K>(
         &self,
-        keys: impl IntoIterator<Item = K>,
+        keys: impl RangeBounds<K>,
         batch_size: usize,
         mut batch_callback: impl FnMut(usize),
     ) -> std::result::Result<usize, DatabaseError>
     where
-        T: DupSort<Key = K>,
+        T: Table<Key = K>,
         K: Key,
     {
-        let mut cursor = self.tx.cursor_dup_write::<T>()?;
+        let mut cursor = self.tx.cursor_write::<T>()?;
+        let mut walker = cursor.walk_range(keys)?;
         let mut deleted = 0;
 
-        for key in keys {
-            if cursor.seek_exact(key)?.is_some() {
-                cursor.delete_current()?;
-                deleted += 1;
+        while walker.next().transpose()?.is_some() {
+            walker.delete_current()?;
+            deleted += 1;
 
-                if deleted % batch_size == 0 {
-                    batch_callback(batch_size);
-                }
-
-                while cursor.next_dup()?.is_some() {
-                    cursor.delete_current()?;
-                    deleted += 1;
-
-                    if deleted % batch_size == 0 {
-                        batch_callback(batch_size);
-                    }
-                }
+            if deleted % batch_size == 0 {
+                batch_callback(batch_size);
             }
         }
 
