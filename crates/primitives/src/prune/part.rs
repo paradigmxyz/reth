2da99069e61a84c1eac815c4d6a6bcb45e24688c
joshieDo <ranriver@protonmail.com>
2023-08-04 12:56:00 +0000

    add sanity tests to PruneMode

--- crates/primitives/src/prune/part.rs
+++ crates/primitives/src/prune/part.rs
@@ -21,7 +21,7 @@ pub enum PrunePart {
 }
 
 /// PrunePart error type.
-#[derive(Debug, Error)]
+#[derive(Debug, Error, PartialEq, Eq)]
 pub enum PrunePartError {
     /// Invalid configuration of a prune part.
     #[error("The configuration provided for {0} is invalid.")]
