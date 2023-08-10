91d21e21ab34ecca3269cf2c51148b1b2f00d361
Matthias Seitz <matthias.seitz@outlook.de>
2023-08-10 18:07:44 +0200

    feat: add is eip4844 fn (#4147)

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -90,7 +90,7 @@ pub use transaction::{
     IntoRecoveredTransaction, InvalidTransactionError, Signature, Transaction, TransactionKind,
     TransactionMeta, TransactionSigned, TransactionSignedEcRecovered, TransactionSignedNoHash,
     TxEip1559, TxEip2930, TxEip4844, TxLegacy, TxType, EIP1559_TX_TYPE_ID, EIP2930_TX_TYPE_ID,
-    LEGACY_TX_TYPE_ID,
+    EIP4844_TX_TYPE_ID, LEGACY_TX_TYPE_ID,
 };
 pub use withdrawal::Withdrawal;
 
