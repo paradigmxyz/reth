b7f273ed53d768e24dfe80484ec3de419718cf47
clabby <ben@clab.by>
2023-08-12 16:59:57 -0400

    Remove `DepositTx` from txpool mock tests, they never go into the txpool
    fmt

--- crates/transaction-pool/src/test_utils/mock.rs
+++ crates/transaction-pool/src/test_utils/mock.rs
@@ -42,10 +42,6 @@ macro_rules! set_value {
     ($this:ident => $field:ident) => {
         let new_value = $field;
         match $this {
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { ref mut $field, .. } => {
-                *$field = new_value;
-            }
             MockTransaction::Legacy { ref mut $field, .. } => {
                 *$field = new_value;
             }
@@ -63,8 +59,6 @@ macro_rules! set_value {
 macro_rules! get_value {
     ($this:ident => $field:ident) => {
         match $this {
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { $field, .. } => $field,
             MockTransaction::Legacy { $field, .. } => $field,
             MockTransaction::Eip1559 { $field, .. } => $field,
             MockTransaction::Eip4844 { $field, .. } => $field,
@@ -127,18 +121,6 @@ pub enum MockTransaction {
         max_priority_fee_per_gas: u128,
         gas_limit: u64,
         to: TransactionKind,
-        value: U256,
-    },
-    #[cfg(feature = "optimism")]
-    DepositTx {
-        hash: H256,
-        sender: Address,
-        nonce: u64,
-        to: TransactionKind,
-        mint: Option<u128>,
-        gas_limit: u64,
-        is_system_transaction: bool,
-        input: Bytes,
         value: U256,
     },
 }
@@ -239,8 +221,6 @@ impl MockTransaction {
 
     pub fn set_gas_price(&mut self, val: u128) -> &mut Self {
         match self {
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { .. } => {}
             MockTransaction::Legacy { gas_price, .. } => {
                 *gas_price = val;
             }
@@ -258,8 +238,6 @@ impl MockTransaction {
 
     pub fn with_gas_price(mut self, val: u128) -> Self {
         match self {
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { .. } => {}
             MockTransaction::Legacy { ref mut gas_price, .. } => {
                 *gas_price = val;
             }
@@ -285,8 +263,6 @@ impl MockTransaction {
 
     pub fn get_gas_price(&self) -> u128 {
         match self {
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { .. } => 0,
             MockTransaction::Legacy { gas_price, .. } => *gas_price,
             MockTransaction::Eip1559 { max_fee_per_gas, .. } => *max_fee_per_gas,
             MockTransaction::Eip4844 { max_fee_per_gas, .. } => *max_fee_per_gas,
@@ -372,8 +348,6 @@ impl MockTransaction {
 impl PoolTransaction for MockTransaction {
     fn hash(&self) -> &TxHash {
         match self {
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { hash, .. } => hash,
             MockTransaction::Legacy { hash, .. } => hash,
             MockTransaction::Eip1559 { hash, .. } => hash,
             MockTransaction::Eip4844 { hash, .. } => hash,
@@ -382,8 +356,6 @@ impl PoolTransaction for MockTransaction {
 
     fn sender(&self) -> Address {
         match self {
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { sender, .. } => *sender,
             MockTransaction::Legacy { sender, .. } => *sender,
             MockTransaction::Eip1559 { sender, .. } => *sender,
             MockTransaction::Eip4844 { sender, .. } => *sender,
@@ -392,8 +364,6 @@ impl PoolTransaction for MockTransaction {
 
     fn nonce(&self) -> u64 {
         match self {
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { nonce, .. } => *nonce,
             MockTransaction::Legacy { nonce, .. } => *nonce,
             MockTransaction::Eip1559 { nonce, .. } => *nonce,
             MockTransaction::Eip4844 { nonce, .. } => *nonce,
@@ -402,8 +372,6 @@ impl PoolTransaction for MockTransaction {
 
     fn cost(&self) -> U256 {
         match self {
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { .. } => U256::ZERO,
             MockTransaction::Legacy { gas_price, value, gas_limit, .. } => {
                 U256::from(*gas_limit) * U256::from(*gas_price) + *value
             }
@@ -413,8 +381,6 @@ impl PoolTransaction for MockTransaction {
             MockTransaction::Eip4844 { max_fee_per_gas, value, gas_limit, .. } => {
                 U256::from(*gas_limit) * U256::from(*max_fee_per_gas) + *value
             }
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { .. } => U256::ZERO,
         }
     }
 
@@ -427,15 +393,11 @@ impl PoolTransaction for MockTransaction {
             MockTransaction::Legacy { gas_price, .. } => *gas_price,
             MockTransaction::Eip1559 { max_fee_per_gas, .. } => *max_fee_per_gas,
             MockTransaction::Eip4844 { max_fee_per_gas, .. } => *max_fee_per_gas,
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { .. } => 0,
         }
     }
 
     fn max_priority_fee_per_gas(&self) -> Option<u128> {
         match self {
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { .. } => None,
             MockTransaction::Legacy { .. } => None,
             MockTransaction::Eip1559 { max_priority_fee_per_gas, .. } => {
                 Some(*max_priority_fee_per_gas)
@@ -466,15 +428,11 @@ impl PoolTransaction for MockTransaction {
             MockTransaction::Legacy { gas_price, .. } => *gas_price,
             MockTransaction::Eip1559 { max_priority_fee_per_gas, .. } => *max_priority_fee_per_gas,
             MockTransaction::Eip4844 { max_priority_fee_per_gas, .. } => *max_priority_fee_per_gas,
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { .. } => 0,
         }
     }
 
     fn kind(&self) -> &TransactionKind {
         match self {
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { to, .. } => to,
             MockTransaction::Legacy { to, .. } => to,
             MockTransaction::Eip1559 { to, .. } => to,
             MockTransaction::Eip4844 { to, .. } => to,
@@ -487,8 +445,6 @@ impl PoolTransaction for MockTransaction {
 
     fn tx_type(&self) -> u8 {
         match self {
-            #[cfg(feature = "optimism")]
-            MockTransaction::DepositTx { .. } => TxType::DEPOSIT.into(),
             MockTransaction::Legacy { .. } => TxType::Legacy.into(),
             MockTransaction::Eip1559 { .. } => TxType::EIP1559.into(),
             MockTransaction::Eip4844 { .. } => TxType::EIP4844.into(),
@@ -510,27 +466,6 @@ impl FromRecoveredTransaction for MockTransaction {
         let transaction = tx.into_signed();
         let hash = transaction.hash();
         match transaction.transaction {
-            #[cfg(feature = "optimism")]
-            Transaction::Deposit(TxDeposit {
-                source_hash,
-                from,
-                to,
-                mint,
-                value,
-                gas_limit,
-                is_system_transaction,
-                input,
-            }) => MockTransaction::DepositTx {
-                nonce: 0u64,
-                hash: source_hash,
-                sender: from,
-                to,
-                mint,
-                value: U256::from(value),
-                gas_limit,
-                is_system_transaction,
-                input,
-            },
             Transaction::Legacy(TxLegacy {
                 chain_id,
                 nonce,
@@ -591,6 +526,10 @@ impl FromRecoveredTransaction for MockTransaction {
                 value: U256::from(value),
             },
             Transaction::Eip2930 { .. } => {
+                unimplemented!()
+            }
+            #[cfg(feature = "optimism")]
+            Transaction::Deposit { .. } => {
                 unimplemented!()
             }
         }
@@ -695,6 +634,7 @@ impl proptest::arbitrary::Arbitrary for MockTransaction {
                     to: *to,
                     value: U256::from(*value),
                 },
+                _ => unimplemented!(),
             })
             .boxed()
     }
