04319ede992d2803f1f18e7960bcc227bbc8cb9e
perama-v <83961755+perama-v@users.noreply.github.com>
2023-08-13 21:20:03 +1000

    feat(primitives): impl address detection in bytes

--- crates/primitives/src/address.rs
+++ crates/primitives/src/address.rs
@@ -8,6 +8,19 @@ use revm_primitives::{TransactTo, TxEnv};
 
 use crate::{Address, Block, H160};
 
+/// For an address to be found in 32 by sequence, the sequence MUST NOT be smaller than this.
+///
+/// This excludes vanity addresses that start with 8 empty bytes.
+const SMALLEST: &[u8; 32] =
+    &hex_literal::hex!("00000000000000000000000000000000000000ffffffffffffffffffffffffff");
+
+/// For an address to be found in 32 by sequence, the sequence MUST start with 12 empty bytes.
+const LARGEST: &[u8; 32] =
+    &hex_literal::hex!("000000000000000000000000ffffffffffffffffffffffffffffffffffffffff");
+
+/// For an address to be found in 32 by sequence, the sequence MUST NOT end with 8 empty bytes.
+const FOUR_EMPTY_BYTES: &[u8; 4] = &[0u8; 4];
+
 /// Detects addresses present in a transaction object
 pub fn get_addresses_from_tx(tx: &TxEnv) -> HashSet<Address> {
     let mut set = HashSet::<Address>::new();
@@ -79,14 +92,97 @@ pub fn bytes_to_possible_addresses(bytes: &[u8]) -> Option<PossibleAddresses> {
     if bytes.len() < 20 {
         return None
     }
-    // todo
-    None
+    let mut set = HashSet::<Address>::new();
+    bytes
+        .chunks_exact(32)
+        .map(|c| {
+            let slice: &[u8; 32] = c.try_into().expect("Size of chunk already checked");
+            slice
+        })
+        .filter_map(bytes32_to_possible_address)
+        .for_each(|addr| {
+            set.insert(addr);
+        });
+    match set.is_empty() {
+        true => None,
+        false => Some(set),
+    }
 }
 
 /// Detects an address if present in 32 bytes.
 ///
 /// See also: <https://github.com/ethereum/execution-apis/pull/456> for the algorithm specification.
-pub fn bytes32_to_possible_address(_logs: &[u8; 32]) -> Option<PossibleAddress> {
-    // todo
-    None
+pub fn bytes32_to_possible_address(bytes: &[u8; 32]) -> Option<PossibleAddress> {
+    if bytes > LARGEST || bytes < SMALLEST {
+        return None
+    }
+    if bytes.ends_with(FOUR_EMPTY_BYTES) {
+        return None
+    }
+    Some(H160(bytes[12..].try_into().expect("Length of bytes already checked")))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // Test cases from: <https://github.com/ethereum/execution-apis/pull/456>
+    #[test]
+    fn valid_addresses_in_bytes32() {
+        let maximum =
+            hex_literal::hex!("000000000000000000000000ffffffffffffffffffffffffffffffffffffffff");
+        let maximum_expected = H160(hex_literal::hex!("ffffffffffffffffffffffffffffffffffffffff"));
+        assert_eq!(bytes32_to_possible_address(&maximum).unwrap(), maximum_expected);
+
+        let minimum =
+            hex_literal::hex!("00000000000000000000000000000000000000ffffffffffffffffffffffffff");
+        let minimum_expected = H160(hex_literal::hex!("00000000000000ffffffffffffffffffffffffff"));
+        assert_eq!(bytes32_to_possible_address(&minimum).unwrap(), minimum_expected);
+
+        let max_trailing =
+            hex_literal::hex!("000000000000000000000000fffffffffffffffffffffffffffffffff0000000");
+        let maxtrail_expected = H160(hex_literal::hex!("fffffffffffffffffffffffffffffffff0000000"));
+        assert_eq!(bytes32_to_possible_address(&max_trailing).unwrap(), maxtrail_expected);
+
+        let deposit =
+            hex_literal::hex!("00000000000000000000000000000000219ab540356cbb839cbe05303d7705fa");
+        let deposit_expected = H160(hex_literal::hex!("00000000219ab540356cbb839cbe05303d7705fa"));
+        assert_eq!(bytes32_to_possible_address(&deposit).unwrap(), deposit_expected);
+
+        let nondeposit =
+            hex_literal::hex!("0000000000000000000000000219ab540356cbb839cbe05303d7705fa0000000");
+        let nondeposit_expected =
+            H160(hex_literal::hex!("0219ab540356cbb839cbe05303d7705fa0000000"));
+        assert_eq!(bytes32_to_possible_address(&nondeposit).unwrap(), nondeposit_expected);
+    }
+
+    #[test]
+    fn invalid_addresses_in_bytes32() {
+        let invalid_long_trail =
+            hex_literal::hex!("000000000000000000000000ffffffffffffffffffffffffffffffff00000000");
+        assert!(bytes32_to_possible_address(&invalid_long_trail).is_none());
+
+        let nondeposit_8_trailing =
+            hex_literal::hex!("000000000000000000000000219ab540356cbb839cbe05303d7705fa00000000");
+        assert!(bytes32_to_possible_address(&nondeposit_8_trailing).is_none());
+
+        let nondeposit_too_few_leading =
+            hex_literal::hex!("00000000219ab540356cbb839cbe05303d7705faffffffffffffffffffffffff");
+        assert!(bytes32_to_possible_address(&nondeposit_too_few_leading).is_none());
+    }
+
+    #[test]
+    fn test_addresses_in_bytes() {
+        let bytes = hex_literal::hex!("00000000000000000000000000000000219ab540356cbb839cbe05303d7705fa0000000000000000000000000219ab540356cbb839cbe05303d7705fa0000000");
+        let expected_1 = H160(hex_literal::hex!("0219ab540356cbb839cbe05303d7705fa0000000"));
+        let expected_2 = H160(hex_literal::hex!("00000000219ab540356cbb839cbe05303d7705fa"));
+        assert!(bytes_to_possible_addresses(&bytes).unwrap().contains(&expected_1));
+        assert!(bytes_to_possible_addresses(&bytes).unwrap().contains(&expected_2));
+
+        let bytes_with_extra = hex_literal::hex!("00000000000000000000000000000000219ab540356cbb839cbe05303d7705fa0000000000000000000000000219ab540356cbb839cbe05303d7705fa000000000000000");
+        assert!(bytes_to_possible_addresses(&bytes_with_extra).unwrap().contains(&expected_1));
+        assert!(bytes_to_possible_addresses(&bytes_with_extra).unwrap().contains(&expected_2));
+
+        let address_straddling_bytes32_divide = hex_literal::hex!("000000000000000000000000000000000000000000000000219ab540356cbb839cbe05303d7705fa000000000000000000000000000000000000000000000000");
+        assert!(bytes_to_possible_addresses(&address_straddling_bytes32_divide).is_none());
+    }
 }
