22782e55bd9019f95e729a8616bc6fb515dd8015
Matthias Seitz <matthias.seitz@outlook.de>
2023-08-15 15:19:31 +0200

    feat: store trait

--- crates/transaction-pool/src/blobstore/mem.rs
+++ crates/transaction-pool/src/blobstore/mem.rs
@@ -1,5 +1,9 @@
-use crate::blobstore::BlobStore;
-use reth_primitives::H256;
+use crate::blobstore::{BlobSideCar, BlobStore, BlobStoreError};
+use reth_primitives::{
+    bytes::{Bytes, BytesMut},
+    H256,
+};
+use reth_rlp::Encodable;
 use std::{
     collections::HashMap,
     sync::{atomic::AtomicUsize, Arc},
@@ -7,19 +11,74 @@ use std::{
 use tokio::sync::RwLock;
 
 /// An in-memory blob store.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct InMemoryBlobStore {
     inner: Arc<InMemoryBlobStoreInner>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct InMemoryBlobStoreInner {
     /// Storage for all blob data.
-    store: RwLock<HashMap<H256, ()>>,
+    store: RwLock<HashMap<H256, BlobSideCar>>,
     size: AtomicUsize,
 }
 
-// #[async_trait::async_trait]
-// impl BlobStore for InMemoryBlobStore {
-//
-// }
+#[async_trait::async_trait]
+impl BlobStore for InMemoryBlobStore {
+    async fn insert(&self, tx: H256, data: BlobSideCar) -> Result<(), BlobStoreError> {
+        let mut store = self.inner.store.write().await;
+        store.insert(tx, data);
+        Ok(())
+    }
+
+    async fn insert_all(&self, txs: Vec<(H256, BlobSideCar)>) -> Result<(), BlobStoreError> {
+        let mut store = self.inner.store.write().await;
+        store.extend(txs);
+        Ok(())
+    }
+
+    async fn delete(&self, tx: H256) -> Result<(), BlobStoreError> {
+        let mut store = self.inner.store.write().await;
+        store.remove(&tx);
+        Ok(())
+    }
+
+    async fn delete_all(&self, txs: Vec<H256>) -> Result<(), BlobStoreError> {
+        let mut store = self.inner.store.write().await;
+        for tx in txs {
+            store.remove(&tx);
+        }
+        Ok(())
+    }
+
+    // Retrieves the decoded blob data for the given transaction hash.
+    async fn get(&self, tx: H256) -> Result<Option<BlobSideCar>, BlobStoreError> {
+        let store = self.inner.store.write().await;
+        Ok(store.get(&tx).cloned())
+    }
+
+    async fn get_all(&self, txs: Vec<H256>) -> Result<Vec<(H256, BlobSideCar)>, BlobStoreError> {
+        let mut items = Vec::new();
+        let store = self.inner.store.write().await;
+        for tx in txs {
+            if let Some(item) = store.get(&tx) {
+                items.push((tx, item.clone()));
+            }
+        }
+
+        Ok(items)
+    }
+
+    async fn get_raw(&self, tx: H256) -> Result<Option<Bytes>, BlobStoreError> {
+        let item = self.get(tx).await?;
+        Ok(item.map(|item| {
+            let mut buf = BytesMut::new();
+            item.encode(&mut buf);
+            buf.freeze()
+        }))
+    }
+
+    fn data_size(&self) -> usize {
+        self.inner.size.load(std::sync::atomic::Ordering::Relaxed)
+    }
+}
--- crates/transaction-pool/src/blobstore/mod.rs
+++ crates/transaction-pool/src/blobstore/mod.rs
@@ -1,9 +1,22 @@
 //! Storage for blob data of EIP4844 transactions.
 
-use reth_primitives::H256;
+#![allow(missing_docs)]
 
-mod mem;
 pub use mem::InMemoryBlobStore;
+use reth_primitives::{bytes::Bytes, H256};
+use reth_rlp::{Decodable, RlpDecodable, RlpEncodable};
+use std::collections::BTreeMap;
+mod mem;
+
+// TODO replace with the actual type
+#[derive(Debug, Clone, RlpDecodable, RlpEncodable)]
+pub struct BlobSideCar;
+
+impl BlobSideCar {
+    pub fn size(&self) -> usize {
+        0
+    }
+}
 
 /// A blob store that can be used to store blob data of EIP4844 transactions.
 ///
@@ -11,16 +24,71 @@ pub use mem::InMemoryBlobStore;
 /// finalization).
 #[async_trait::async_trait]
 pub trait BlobStore: Send + Sync + Clone + 'static {
-    // TODO add fn for fetch many, delete many, etc.
-    async fn get(&self, x: H256) -> Option<()>;
+    /// Inserts the blob sidecar into the store
+    async fn insert(&self, tx: H256, data: BlobSideCar) -> Result<(), BlobStoreError>;
+
+    /// Inserts the blob sidecar into the store
+    async fn insert_all(&self, txs: Vec<(H256, BlobSideCar)>) -> Result<(), BlobStoreError>;
+
+    /// Deletes the blob sidecar from the store
+    async fn delete(&self, tx: H256) -> Result<(), BlobStoreError>;
+
+    /// Deletes all blob sidecars from the store
+    async fn delete_all(&self, txs: Vec<H256>) -> Result<(), BlobStoreError>;
+
+    /// Retrieves the decoded blob data for the given transaction hash.
+    async fn get(&self, tx: H256) -> Result<Option<BlobSideCar>, BlobStoreError> {
+        if let Some(raw) = self.get_raw(tx).await? {
+            Ok(Some(BlobSideCar::decode(&mut raw.as_ref())?))
+        } else {
+            Ok(None)
+        }
+    }
+
+    /// Retrieves all decoded blob data for the given transaction hashes
+    async fn get_all(&self, txs: Vec<H256>) -> Result<Vec<(H256, BlobSideCar)>, BlobStoreError>;
+
+    /// Retrieves the raw blob data for the given transaction hash.
+    async fn get_raw(&self, tx: H256) -> Result<Option<Bytes>, BlobStoreError>;
 
     /// Data size of all transactions in the blob store.
     fn data_size(&self) -> usize;
 }
 
+/// Error variants that can occur when interacting with a blob store.
+#[derive(Debug, thiserror::Error)]
+pub enum BlobStoreError {
+    /// Failed to decode the stored blob data.
+    #[error("failed to decode blob data: {0}")]
+    DecodeError(#[from] reth_rlp::DecodeError),
+    /// Other implementation specific error.
+    #[error(transparent)]
+    Other(Box<dyn std::error::Error + Send + Sync>),
+}
+
+/// The type that is used to maintain the blob store and discard finalized transactions.
+#[derive(Debug)]
+pub struct BlobStoreMaintenance<S> {
+    store: S,
+    /// Keeps track of the blob transactions that are in blocks.
+    blob_txs_in_blocks: BTreeMap<u64, Vec<H256>>,
+}
+
+impl<S> BlobStoreMaintenance<S> {
+    /// Creates a new blob store maintenance instance.
+    pub fn new(store: S) -> Self {
+        Self { store, blob_txs_in_blocks: Default::default() }
+    }
+}
+
+impl<S: BlobStore> BlobStoreMaintenance<S> {
+    /// Invoked when a block is finalized.
+    pub async fn on_finalized(&mut self, _block_number: u64) {}
+}
+
 // TODO add as additional param for Pool struct
 // Add functions to fetch many, delete many, etc to TransactionPool trait
-// TODO add  get pooled transactions to transaction pool trait that is async and returns Vec<PooledTransactionResponse>
-// TODO keep track of pooled transaction responses in transaction manager, since async and we want to keep track which peers requesting blobs
-// TODO soft limit on getpooledtransactions
-// https://github.com/ethereum/devp2p/blob/master/caps/eth.md#getpooledtransactions-0x09
+// TODO add  get pooled transactions to transaction pool trait that is async and returns
+// Vec<PooledTransactionResponse> TODO keep track of pooled transaction responses in transaction
+// manager, since async and we want to keep track which peers requesting blobs TODO soft limit on
+// getpooledtransactions https://github.com/ethereum/devp2p/blob/master/caps/eth.md#getpooledtransactions-0x09
