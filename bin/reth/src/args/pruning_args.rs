8a2c3abd2a5c8b3d3b299d9f40916fec50fec207
joshieDo <93316087+joshieDo@users.noreply.github.com>
2023-08-16 17:38:36 +0100

    feat(pipeline): prune receipts based on log emitters (#4044)

--- bin/reth/src/args/pruning_args.rs
+++ bin/reth/src/args/pruning_args.rs
@@ -2,7 +2,9 @@
 
 use clap::Args;
 use reth_config::config::PruneConfig;
-use reth_primitives::{ChainSpec, PruneMode, PruneModes};
+use reth_primitives::{
+    ChainSpec, ContractLogsPruneConfig, PruneMode, PruneModes, MINIMUM_PRUNING_DISTANCE,
+};
 use std::sync::Arc;
 
 /// Parameters for pruning and full node
@@ -25,14 +27,22 @@ impl PruningArgs {
             Some(PruneConfig {
                 block_interval: 5,
                 parts: PruneModes {
-                    sender_recovery: Some(PruneMode::Distance(128)),
+                    sender_recovery: Some(PruneMode::Distance(MINIMUM_PRUNING_DISTANCE)),
                     transaction_lookup: None,
                     receipts: _chain_spec
                         .deposit_contract
                         .as_ref()
                         .map(|contract| PruneMode::Before(contract.block)),
-                    account_history: Some(PruneMode::Distance(128)),
-                    storage_history: Some(PruneMode::Distance(128)),
+                    account_history: Some(PruneMode::Distance(MINIMUM_PRUNING_DISTANCE)),
+                    storage_history: Some(PruneMode::Distance(MINIMUM_PRUNING_DISTANCE)),
+                    contract_logs_filter: ContractLogsPruneConfig(
+                        _chain_spec
+                            .deposit_contract
+                            .as_ref()
+                            .map(|contract| (contract.address, PruneMode::Before(contract.block)))
+                            .into_iter()
+                            .collect(),
+                    ),
                 },
             })
         } else {
