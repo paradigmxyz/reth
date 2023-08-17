1bdcf03ff6b2b97937c92ed88065594b5e85bfc8
Matthias Seitz <matthias.seitz@outlook.de>
2023-08-17 14:40:24 +0200

    add size tracking

--- crates/transaction-pool/src/blobstore/mem.rs
+++ crates/transaction-pool/src/blobstore/mem.rs
@@ -1,7 +1,6 @@
 use crate::blobstore::{BlobStore, BlobStoreError, BlobTransactionSidecar};
 use parking_lot::RwLock;
 use reth_primitives::H256;
-
 use std::{
     collections::HashMap,
     sync::{atomic::AtomicUsize, Arc},
@@ -21,51 +20,64 @@ struct InMemoryBlobStoreInner {
 }
 
 impl InMemoryBlobStoreInner {
-
     fn add_size(&self, add: usize) {
-
+        self.size.fetch_add(add, std::sync::atomic::Ordering::Relaxed);
     }
 
     fn sub_size(&self, sub: usize) {
-
+        self.size.fetch_sub(sub, std::sync::atomic::Ordering::Relaxed);
     }
 
     fn update_size(&self, add: usize, sub: usize) {
-
+        if add > sub {
+            self.add_size(add - sub);
+        } else {
+            self.sub_size(sub - add);
+        }
     }
 }
 
 impl BlobStore for InMemoryBlobStore {
     fn insert(&self, tx: H256, data: BlobTransactionSidecar) -> Result<(), BlobStoreError> {
-        let add = data.size();
         let mut store = self.inner.store.write();
-        let sub = store.insert(tx, data).map(|rem| rem.size()).unwrap_or_default();
+        let (add, sub) = insert_size(&mut store, tx, data);
         self.inner.update_size(add, sub);
         Ok(())
     }
 
     fn insert_all(&self, txs: Vec<(H256, BlobTransactionSidecar)>) -> Result<(), BlobStoreError> {
-        let mut store = self.inner.store.write();
-        let mut add = 0;
-        for (tx, data) in txs {
-            add += data.size();
-            store.insert(tx, data);
+        if txs.is_empty() {
+            return Ok(())
         }
-        store.extend(txs);
+        let mut store = self.inner.store.write();
+        let mut total_add = 0;
+        let mut total_sub = 0;
+        for (tx, data) in txs {
+            let (add, sub) = insert_size(&mut store, tx, data);
+            total_add += add;
+            total_sub += sub;
+        }
+        self.inner.update_size(total_add, total_sub);
         Ok(())
     }
 
     fn delete(&self, tx: H256) -> Result<(), BlobStoreError> {
         let mut store = self.inner.store.write();
-        store.remove(&tx);
+        let sub = remove_size(&mut store, &tx);
+        self.inner.sub_size(sub);
         Ok(())
     }
 
     fn delete_all(&self, txs: Vec<H256>) -> Result<(), BlobStoreError> {
-        let mut store = self.inner.store.write();
-        for tx in txs {
-            store.remove(&tx);
+        if txs.is_empty() {
+            return Ok(())
         }
+        let mut store = self.inner.store.write();
+        let mut total_sub = 0;
+        for tx in txs {
+            total_sub += remove_size(&mut store, &tx);
+        }
+        self.inner.sub_size(total_sub);
         Ok(())
     }
 
@@ -95,12 +107,20 @@ impl BlobStore for InMemoryBlobStore {
     }
 }
 
+/// Removes the given blob from the store and returns the size of the blob that was removed.
 #[inline]
 fn remove_size(store: &mut HashMap<H256, BlobTransactionSidecar>, tx: &H256) -> usize {
     store.remove(tx).map(|rem| rem.size()).unwrap_or_default()
 }
 
+/// Inserts the given blob into the store and returns the size of the blob that was (added,removed)
 #[inline]
-fn insert_size(store: &mut HashMap<H256, BlobTransactionSidecar>, tx: H256, blob: BlobTransactionSidecar) -> usize {
-    store.insert(tx).map(|rem| rem.size()).unwrap_or_default()
+fn insert_size(
+    store: &mut HashMap<H256, BlobTransactionSidecar>,
+    tx: H256,
+    blob: BlobTransactionSidecar,
+) -> (usize, usize) {
+    let add = blob.size();
+    let sub = store.insert(tx, blob).map(|rem| rem.size()).unwrap_or_default();
+    (add, sub)
 }
