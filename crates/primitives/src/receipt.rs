fc6bb2084ae756538d963c31ff74ff8e7081eb83
Andreas Bigger <abigger87@gmail.com>
2023-08-17 16:31:19 -0400

    Fixed internal unwrapping issue

--- crates/primitives/src/receipt.rs
+++ crates/primitives/src/receipt.rs
@@ -109,7 +109,8 @@ impl ReceiptWithBloom {
             TxType::DEPOSIT => {
                 let consumed = started_len - b.len();
                 let has_nonce = rlp_head.payload_length - consumed > 0;
-                let deposit_nonce = has_nonce.then_some(reth_rlp::Decodable::decode(b)?);
+                let deposit_nonce =
+                    if has_nonce { Some(reth_rlp::Decodable::decode(b)?) } else { None };
 
                 Receipt { tx_type, success, cumulative_gas_used, logs, deposit_nonce }
             }
