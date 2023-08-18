fdafd4c31df313cf42a9326235ad53536990fa99
Matthias Seitz <matthias.seitz@outlook.de>
2023-08-18 14:59:57 +0200

    feat: maintain blobs

--- crates/transaction-pool/src/blobstore/maintain.rs
+++ crates/transaction-pool/src/blobstore/maintain.rs
@@ -1,7 +1,7 @@
 //! Support for maintaining the blob pool.
 
 use crate::blobstore::BlobStore;
-use reth_primitives::H256;
+use reth_primitives::{BlockNumber, Header, H256};
 use std::collections::BTreeMap;
 
 /// The type that is used to maintain the blob store and discard finalized transactions.
@@ -10,8 +10,8 @@ use std::collections::BTreeMap;
 pub struct BlobStoreMaintainer<S> {
     /// The blob store that holds all the blob data.
     store: S,
-    /// Keeps track of the blob transactions that are in blocks.
-    blob_txs_in_blocks: BTreeMap<u64, Vec<H256>>,
+    /// Keeps track of the blob transactions included in blocks.
+    blob_txs_in_blocks: BTreeMap<BlockNumber, Vec<H256>>,
 }
 
 impl<S> BlobStoreMaintainer<S> {
@@ -22,6 +22,11 @@ impl<S> BlobStoreMaintainer<S> {
 }
 
 impl<S: BlobStore> BlobStoreMaintainer<S> {
+    /// Adds a block to the blob store maintenance.
+    pub fn add_block(&mut self, block_number: BlockNumber, blob_txs: Vec<H256>) {
+        self.blob_txs_in_blocks.insert(block_number, blob_txs);
+    }
+
     /// Invoked when a block is finalized.
-    pub fn on_finalized(&mut self, _block_number: u64) {}
+    pub fn on_finalized(&mut self, header: &Header) {}
 }
