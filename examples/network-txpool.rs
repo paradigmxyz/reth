774e0ee7993bdfd247a4e8a99cec2027597ff7cb
Matthias Seitz <matthias.seitz@outlook.de>
2023-08-20 12:56:04 +0200

    chore: simplify builder fn

--- examples/network-txpool.rs
+++ examples/network-txpool.rs
@@ -34,8 +34,7 @@ async fn main() -> eyre::Result<()> {
     let local_key = rng_secret_key();
 
     // Configure the network
-    let config =
-        NetworkConfig::<NoopProvider>::builder(local_key).mainnet_boot_nodes().build(client);
+    let config = NetworkConfig::builder(local_key).mainnet_boot_nodes().build(client);
 
     // create the network instance
     let (_handle, network, txpool, _) =
