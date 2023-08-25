8fcbb5eb3a104d9de76088c4b812889466497853
PatStiles <pdstiles78@gmail.com>
2023-08-25 01:43:41 -0400

    use DEFAULT_DISCOVERY_PORT

--- crates/net/eth-wire/src/p2pstream.rs
+++ crates/net/eth-wire/src/p2pstream.rs
@@ -9,6 +9,7 @@ use crate::{
 use futures::{Sink, SinkExt, StreamExt};
 use pin_project::pin_project;
 use reth_codecs::derive_arbitrary;
+use reth_discv4::DEFAULT_DISCOVERY_PORT;
 use reth_metrics::metrics::counter;
 use reth_primitives::{
     bytes::{Buf, BufMut, Bytes, BytesMut},
@@ -839,7 +840,7 @@ mod tests {
             protocol_version: ProtocolVersion::V5,
             client_version: "bitcoind/1.0.0".to_string(),
             capabilities: vec![EthVersion::Eth67.into()],
-            port: 30303,
+            port: DEFAULT_DISCOVERY_PORT,
             id: pk2id(&server_key.public_key(SECP256K1)),
         };
         (hello, server_key)
