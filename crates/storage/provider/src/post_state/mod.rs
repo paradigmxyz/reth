33219552c664700ed12094dc344bd68ceac6b88c
refcell.eth <abigger87@gmail.com>
2023-08-28 09:43:51 -0700

    Update crates/storage/provider/src/post_state/mod.rs
    Co-authored-by: Roman Krasiuk <rokrassyuk@gmail.com>

--- crates/storage/provider/src/post_state/mod.rs
+++ crates/storage/provider/src/post_state/mod.rs
@@ -196,9 +196,9 @@ impl PostState {
         #[cfg(feature = "optimism")]
         {
             // There is a minor bug in op-geth and op-erigon where in the Regolith hardfork,
-            // the receipt root calculation does not include the deposit nonce in the receipt
-            // encoding. This will be fixd in the next hardfork, however for now, we must strip
-            // the deposit nonce from the receipts before calculating the receipt root.
+            // the receipts root calculation does not include the deposit nonce in the receipt
+            // encoding. This will be fixed in the next hardfork, however, for now, we must strip
+            // the deposit nonce from the receipts before calculating the receipts root.
             let receipts = self
                 .receipts(block)
                 .iter()
