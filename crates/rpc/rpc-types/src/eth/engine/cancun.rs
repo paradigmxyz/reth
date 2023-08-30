c9fa0c0f91ff8769ef0f69737c8995a8946c9202
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-08-29 18:55:09 -0700

    add link to cancun engine spec

--- crates/rpc/rpc-types/src/eth/engine/cancun.rs
+++ crates/rpc/rpc-types/src/eth/engine/cancun.rs
@@ -4,6 +4,9 @@ use reth_primitives::H256;
 
 /// Fields introduced in `engine_newPayloadV3` that are not present in the `ExecutionPayload` RPC
 /// object.
+///
+/// See also:
+/// <https://github.com/ethereum/execution-apis/blob/fe8e13c288c592ec154ce25c534e26cb7ce0530d/src/engine/cancun.md#request>
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
 pub struct CancunPayloadFields {
     /// The parent beacon block root.
