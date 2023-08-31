d0a5a19b0dcfdb6201af6ef4cda73783ce15a28f
joshieDo <93316087+joshieDo@users.noreply.github.com>
2023-08-31 06:10:17 -0700

    fix(pruning): dont check pruning on every storage change for the same block (#4402)

--- crates/storage/provider/src/post_state/mod.rs
+++ crates/storage/provider/src/post_state/mod.rs
@@ -529,11 +529,11 @@ impl PostState {
         for (block_number, storage_changes) in
             std::mem::take(&mut self.storage_changes).inner.into_iter()
         {
-            for (address, mut storage) in storage_changes.into_iter() {
-                if self.prune_modes.should_prune_storage_history(block_number, tip) {
-                    continue
-                }
+            if self.prune_modes.should_prune_storage_history(block_number, tip) {
+                continue
+            }
 
+            for (address, mut storage) in storage_changes.into_iter() {
                 let storage_id = BlockNumberAddress((block_number, address));
 
                 // If the account was created and wiped at the same block, skip all storage changes
