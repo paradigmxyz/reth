5dfcceb75c9f38f9a16999373ccb967d164388b0
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-09-05 23:14:09 -0400

    add test with hive rpc test data

--- crates/net/eth-wire/tests/pooled_transactions.rs
+++ crates/net/eth-wire/tests/pooled_transactions.rs
@@ -21,3 +21,13 @@ fn decode_blob_transaction_data() {
     let hex_data = hex::decode(data.trim()).unwrap();
     let _txs = PooledTransactionsElement::decode(&mut &hex_data[..]).unwrap();
 }
+
+#[test]
+fn decode_blob_rpc_transaction() {
+    // test data pulled from hive test that sends blob transactions
+    let network_data_path =
+        PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("testdata/rpc_blob_transaction");
+    let data = fs::read_to_string(network_data_path).expect("Unable to read file");
+    let hex_data = hex::decode(data.trim()).unwrap();
+    let _txs = PooledTransactionsElement::decode_enveloped(&mut &hex_data[..]).unwrap();
+}
