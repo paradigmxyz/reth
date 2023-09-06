6299c26b56edc245e622aef88914995190f78215
Alessandro Mazza <121622391+alessandromazza98@users.noreply.github.com>
2023-09-06 14:38:15 +0200

    add metrics counter for finished spawned tasks (#4481)

--- crates/tasks/src/metrics.rs
+++ crates/tasks/src/metrics.rs
@@ -7,9 +7,12 @@ use reth_metrics::{metrics::Counter, Metrics};
 pub struct TaskExecutorMetrics {
     /// Number of spawned critical tasks
     pub(crate) critical_tasks: Counter,
-
+    /// Number of finished spawned critical tasks
+    pub(crate) finished_critical_tasks: Counter,
     /// Number of spawned regular tasks
     pub(crate) regular_tasks: Counter,
+    /// Number of finished spawned regular tasks
+    pub(crate) finished_regular_tasks: Counter,
 }
 
 impl TaskExecutorMetrics {
@@ -17,7 +20,23 @@ impl TaskExecutorMetrics {
         self.critical_tasks.increment(1);
     }
 
-    pub(crate) fn inc_regular_task(&self) {
+    pub(crate) fn inc_regular_tasks(&self) {
         self.regular_tasks.increment(1);
+    }
+}
+
+/// Helper type for increasing counters even if a task fails.
+pub struct IncCounterOnDrop(Counter);
+
+impl IncCounterOnDrop {
+    /// Create a new `IncCounterOnDrop`.
+    pub fn new(counter: Counter) -> Self {
+        IncCounterOnDrop(counter)
+    }
+}
+
+impl Drop for IncCounterOnDrop {
+    fn drop(&mut self) {
+        self.0.increment(1);
     }
 }
