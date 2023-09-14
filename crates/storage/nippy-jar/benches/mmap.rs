caee69d028f65fed3ec015ecbddf6f9f1951a940
Roman Krasiuk <rokrassyuk@gmail.com>
2023-09-14 16:57:05 +0300

    add mdbxx bench

--- crates/storage/nippy-jar/benches/mmap.rs
+++ crates/storage/nippy-jar/benches/mmap.rs
@@ -1,14 +1,10 @@
 use criterion::{
     criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, Criterion,
 };
-use proptest::{
-    prelude::*,
-    strategy::{Strategy, ValueTree},
-    test_runner::TestRunner,
-};
-use rand::seq::SliceRandom;
+use rand::{seq::SliceRandom, Rng};
+use reth_interfaces::test_utils::generators::random_block_range;
 use reth_nippy_jar::mmap::Mmap;
-use reth_primitives::SealedBlock;
+use reth_primitives::{SealedBlock, H256};
 use reth_rlp::Encodable;
 use std::{
     fs::{File, OpenOptions},
@@ -18,10 +14,14 @@ use std::{
 };
 use tempfile::tempdir;
 
-trait SnapshotReader<'a> {
-    fn new(file: &'a File) -> Self;
+trait SnapshotReader: Sized {
+    /// Returns (reader, offsets).
+    fn setup<R: Rng>(
+        rng: &mut R,
+        dir: &Path,
+        data: &[SealedBlock],
+    ) -> std::io::Result<(Self, Vec<(usize, usize)>)>;
 
-    // Should return
     fn read(&mut self, offset: usize, len: usize) -> std::io::Result<Vec<u8>>;
 
     fn read_multiple(&mut self, offsets: &[(usize, usize)]) -> std::io::Result<Vec<Vec<u8>>>;
@@ -30,13 +30,16 @@ trait SnapshotReader<'a> {
 pub fn snapshots_reads(c: &mut Criterion) {
     let mut group = c.benchmark_group("Snapshot Reads");
     group.measurement_time(Duration::from_secs(10));
+    group.warm_up_time(Duration::from_nanos(1));
 
+    let mut rng = rand::thread_rng();
     let dir = tempdir().expect("failed to create tempdir");
-    let range = 5;
+    let dir_path = dir.path();
+    let window_range = 5;
 
     let num_snapshots = 10;
     for blocks_per_snapshot in [10, 100, 1000] {
-        let test_data = generate_test_data(dir.path(), num_snapshots, blocks_per_snapshot).unwrap();
+        let test_data = generate_test_data(&mut rng, num_snapshots, blocks_per_snapshot);
 
         let bench_description = |reader| {
             format!("snapshots: {num_snapshots} | blocks per snapshot: {blocks_per_snapshot} | {reader}")
@@ -44,110 +47,119 @@ pub fn snapshots_reads(c: &mut Criterion) {
 
         use implementations::*;
 
-        reader_bench::<FileReader>(&mut group, &bench_description("FileReader"), &test_data, range);
+        reader_bench::<_, FileReader>(
+            &mut group,
+            &mut rng,
+            &dir_path,
+            &bench_description("FileReader"),
+            &test_data,
+            window_range,
+        );
 
-        reader_bench::<MmapReader>(&mut group, &bench_description("MmapReader"), &test_data, range);
+        reader_bench::<_, MdbxReader>(
+            &mut group,
+            &mut rng,
+            &dir_path,
+            &bench_description("MdbxReader"),
+            &test_data,
+            window_range,
+        );
+
+        reader_bench::<_, MmapReader>(
+            &mut group,
+            &mut rng,
+            &dir_path,
+            &bench_description("MmapReader"),
+            &test_data,
+            window_range,
+        );
     }
 }
 
-fn generate_test_data(
-    dir: &Path,
+fn generate_test_data<R: Rng>(
+    rng: &mut R,
     num_snapshots: usize,
     blocks_per_snapshot: usize,
-) -> std::io::Result<Vec<(File, Vec<(usize, usize)>)>> {
-    let config = ProptestConfig::default();
-    let mut runner = TestRunner::new(config);
-
+) -> Vec<Vec<SealedBlock>> {
     let mut data = Vec::with_capacity(num_snapshots);
     for i in 0..num_snapshots {
-        let blocks: Vec<SealedBlock> =
-            prop::collection::vec(any::<SealedBlock>(), blocks_per_snapshot)
-                .new_tree(&mut runner)
-                .unwrap()
-                .current();
-
-        let filename = format!("snapshot-{num_snapshots}-{blocks_per_snapshot}-{i}");
-        let mut file = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .append(true)
-            .open(dir.join(filename))?;
-
-        let mut offset = 0;
-        let mut offsets = Vec::with_capacity(blocks.len());
-        for block in &blocks {
-            let mut buf = Vec::new();
-            SealedBlock::encode(&block, &mut buf);
-            let len = file.write(&buf)?;
-            offsets.push((offset, len));
-            offset += len;
-        }
-
-        file.seek(SeekFrom::Start(0))?;
-
-        data.push((file, offsets));
+        let range_start = (i * num_snapshots) as u64;
+        let blocks = random_block_range(
+            rng,
+            range_start..=range_start + blocks_per_snapshot as u64 - 1,
+            H256::random(),
+            0..6,
+        );
+        data.push(blocks);
     }
 
-    Ok(data)
+    data
 }
 
-fn reader_bench<'a, T: SnapshotReader<'a>>(
+fn reader_bench<R: Rng, T: SnapshotReader>(
     group: &mut BenchmarkGroup<WallTime>,
+    rng: &mut R,
+    dir: &Path,
     description: &str,
-    test_data: &'a [(File, Vec<(usize, usize)>)],
-    range: usize,
+    test_data: &[Vec<SealedBlock>],
+    window_range: usize,
 ) {
     let group_id = |ty| format!("{ty} | {description}");
 
-    let setup = || {
+    let setup = |rng: &mut R| {
         let mut readers = Vec::with_capacity(test_data.len());
-        for (file, _) in test_data {
-            readers.push(T::new(file));
+        let mut all_offsets = Vec::with_capacity(test_data.len());
+        for blocks in test_data {
+            let (reader, offsets) = T::setup(rng, dir, &blocks).expect("setup successful");
+            readers.push(reader);
+            all_offsets.push(offsets);
         }
-        readers
+
+        (readers, all_offsets)
     };
 
     group.bench_function(group_id("single seq reads"), |b| {
-        b.iter_with_setup(setup, |readers| {
-            {
-                for (mut reader, (_, offsets)) in readers.into_iter().zip(test_data) {
-                    for (offset, len) in offsets {
-                        reader.read(*offset, *len).unwrap();
+        b.iter_with_setup(
+            || setup(rng),
+            |(readers, offsets)| {
+                {
+                    for (mut reader, offsets) in readers.into_iter().zip(offsets) {
+                        for (offset, len) in offsets {
+                            reader.read(offset, len).unwrap();
+                        }
                     }
-                }
-            };
-            std::hint::black_box(());
-        });
+                };
+                std::hint::black_box(());
+            },
+        );
     });
 
     group.bench_function(group_id("multiple seq reads"), |b| {
-        b.iter_with_setup(setup, |readers| {
-            {
-                for (mut reader, (_, offsets)) in readers.into_iter().zip(test_data) {
-                    for offsets_window in offsets.windows(range) {
-                        reader.read_multiple(offsets_window).unwrap();
+        b.iter_with_setup(
+            || setup(rng),
+            |(readers, offsets)| {
+                {
+                    for (mut reader, offsets) in readers.into_iter().zip(offsets) {
+                        for offsets_window in offsets.windows(window_range) {
+                            reader.read_multiple(offsets_window).unwrap();
+                        }
                     }
-                }
-            };
-            std::hint::black_box(());
-        });
+                };
+                std::hint::black_box(());
+            },
+        );
     });
 
     group.bench_function(group_id("random reads"), |b| {
         b.iter_with_setup(
             || {
-                let readers = setup();
+                let (readers, mut all_offsets) = setup(rng);
 
-                let mut rng = rand::thread_rng();
-                let mut shuffled_offsets = Vec::with_capacity(test_data.len());
-                for (_, offsets) in test_data {
-                    let mut offsets = offsets.clone();
-                    offsets.shuffle(&mut rng);
-                    shuffled_offsets.push(offsets);
+                for offsets in all_offsets.iter_mut() {
+                    offsets.shuffle(rng);
                 }
 
-                (readers, shuffled_offsets)
+                (readers, all_offsets)
             },
             |(readers, shuffled_offsets)| {
                 {
@@ -168,16 +180,63 @@ criterion_main!(mmap_reads);
 
 mod implementations {
     use super::*;
-    use std::io::{Read, Result};
+    use reth_db::{
+        cursor::DbCursorRO,
+        database::Database,
+        mdbx::{DatabaseFlags, EnvKind},
+        table::Table,
+        transaction::{DbTx, DbTxMut},
+        DatabaseEnv,
+    };
+    use reth_primitives::BlockNumber;
+    use std::{
+        io::{Error, ErrorKind, Read, Result},
+        sync::Arc,
+    };
 
-    /// This loads whole file in memory on each read. Never use it pls.
-    pub struct FileReader<'a> {
-        file: &'a File,
+    fn create_file_with_rlp_blocks<R: Rng>(
+        rng: &mut R,
+        dir: &Path,
+        data: &[SealedBlock],
+    ) -> Result<(File, Vec<(usize, usize)>)> {
+        let filename_seed: u64 = rng.gen();
+        let filename = format!("snapshot-{}-{}", data.len(), filename_seed);
+
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(dir.join(filename))?;
+
+        let mut offset = 0;
+        let mut offsets = Vec::with_capacity(data.len());
+        for block in data {
+            let mut buf = Vec::new();
+            SealedBlock::encode(&block, &mut buf);
+            let len = file.write(&buf)?;
+            offsets.push((offset, len));
+            offset += len;
+        }
+
+        file.seek(SeekFrom::Start(0))?;
+
+        Ok((file, offsets))
     }
 
-    impl<'a> SnapshotReader<'a> for FileReader<'a> {
-        fn new(file: &'a File) -> Self {
-            Self { file }
+    /// This loads whole file in memory on each read. Never use it pls.
+    pub struct FileReader {
+        file: File,
+    }
+
+    impl SnapshotReader for FileReader {
+        fn setup<R: Rng>(
+            rng: &mut R,
+            dir: &Path,
+            data: &[SealedBlock],
+        ) -> Result<(Self, Vec<(usize, usize)>)> {
+            let (file, offsets) = create_file_with_rlp_blocks(rng, dir, data)?;
+            Ok((Self { file }, offsets))
         }
 
         fn read(&mut self, offset: usize, len: usize) -> Result<Vec<u8>> {
@@ -215,12 +274,20 @@ mod implementations {
     }
 
     pub struct MmapReader {
+        #[allow(dead_code)]
+        file: File,
         mmap: Mmap,
     }
 
-    impl<'a> SnapshotReader<'a> for MmapReader {
-        fn new(file: &'a File) -> Self {
-            Self { mmap: unsafe { Mmap::map(file).unwrap() } }
+    impl SnapshotReader for MmapReader {
+        fn setup<R: Rng>(
+            rng: &mut R,
+            dir: &Path,
+            data: &[SealedBlock],
+        ) -> Result<(Self, Vec<(usize, usize)>)> {
+            let (file, offsets) = create_file_with_rlp_blocks(rng, dir, data)?;
+            let mmap = unsafe { Mmap::map(&file).unwrap() };
+            Ok((Self { file, mmap }, offsets))
         }
 
         fn read(&mut self, offset: usize, len: usize) -> Result<Vec<u8>> {
@@ -234,6 +301,81 @@ mod implementations {
             }
 
             Ok(result)
+        }
+    }
+
+    // De facto this is a `SealedBlocks` table.
+    // It is aliased as a different one to bypass some checks.
+    #[derive(Debug)]
+    pub struct BlockBodyIndices;
+
+    impl Table for BlockBodyIndices {
+        const NAME: &'static str = BlockBodyIndices::const_name();
+        type Key = BlockNumber;
+        type Value = Vec<u8>;
+    }
+
+    impl BlockBodyIndices {
+        /// Return BlockBodyIndices as it is present inside the database.
+        pub const fn const_name() -> &'static str {
+            stringify!(BlockBodyIndices)
+        }
+    }
+
+    pub struct MdbxReader {
+        db: Arc<DatabaseEnv>,
+    }
+
+    impl SnapshotReader for MdbxReader {
+        fn setup<R: Rng>(
+            rng: &mut R,
+            dir: &Path,
+            data: &[SealedBlock],
+        ) -> Result<(Self, Vec<(usize, usize)>)> {
+            let db_dir_seed: u64 = rng.gen();
+            let db_path = dir.join(format!("{db_dir_seed}"));
+            std::fs::create_dir_all(&db_path)?;
+            let db = Arc::new(DatabaseEnv::open(&db_path, EnvKind::RW, None).unwrap());
+
+            // Create the table.
+            let tx = db.begin_rw_txn().unwrap();
+            tx.create_db(Some(BlockBodyIndices::NAME), DatabaseFlags::default()).unwrap();
+            tx.commit().unwrap();
+
+            // Use block numbers as offsets.
+            let tx = db.tx_mut().unwrap();
+            let mut offsets = Vec::with_capacity(data.len());
+            for block in data {
+                let mut buf = Vec::new();
+                SealedBlock::encode(&block, &mut buf);
+                tx.put::<BlockBodyIndices>(block.number, buf).unwrap();
+                offsets.push((block.number as usize, 0));
+            }
+
+            tx.commit().unwrap();
+
+            Ok((MdbxReader { db }, offsets))
+        }
+
+        fn read(&mut self, offset: usize, _len: usize) -> Result<Vec<u8>> {
+            let tx = self.db.tx().unwrap();
+            tx.get::<BlockBodyIndices>(offset as u64).unwrap().ok_or(ErrorKind::InvalidData.into())
+        }
+
+        fn read_multiple(&mut self, offsets: &[(usize, usize)]) -> Result<Vec<Vec<u8>>> {
+            let tx = self.db.tx().unwrap();
+            let mut cursor = tx.cursor_read::<BlockBodyIndices>().unwrap();
+
+            let mut results = Vec::with_capacity(offsets.len());
+            for (offset, _) in offsets {
+                let (_, block) = cursor
+                    .seek_exact(*offset as u64)
+                    .unwrap()
+                    .ok_or(<ErrorKind as Into<Error>>::into(ErrorKind::InvalidData))?;
+                results.push(block);
+            }
+
+            Ok(results)
         }
     }
 }
--- crates/storage/nippy-jar/src/mmap/mod.rs
+++ crates/storage/nippy-jar/src/mmap/mod.rs
@@ -1,3 +1,4 @@
+#[allow(clippy::missing_safety_doc)]
 pub use memmap2::*;
 
 #[cfg(test)]
