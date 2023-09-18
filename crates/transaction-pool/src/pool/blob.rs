cbb74346bd77fbc7f5d2c1f122dc5c5aed721ea6
Matthias Seitz <matthias.seitz@outlook.de>
2023-09-18 15:02:20 +0200

    test: add assert_invariants (#4623)

--- crates/transaction-pool/src/pool/blob.rs
+++ crates/transaction-pool/src/pool/blob.rs
@@ -98,6 +98,12 @@ impl<T: PoolTransaction> BlobTransactions<T> {
     pub(crate) fn contains(&self, id: &TransactionId) -> bool {
         self.by_id.contains_key(id)
     }
+
+    /// Asserts that the bijection between `by_id` and `all` is valid.
+    #[cfg(any(test, feature = "test-utils"))]
+    pub(crate) fn assert_invariants(&self) {
+        assert_eq!(self.by_id.len(), self.all.len(), "by_id.len() != all.len()");
+    }
 }
 
 impl<T: PoolTransaction> Default for BlobTransactions<T> {
