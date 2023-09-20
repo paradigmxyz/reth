ff9f028c59810b708a530fd5739bff6c2d437671
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-09-20 17:44:18 +0200

    revert decode_revert_reason changes

--- crates/primitives/src/abi.rs
+++ crates/primitives/src/abi.rs
@@ -1,10 +1,19 @@
 //! Eth ABI helpers.
 
+// TODO: Use `alloy_sol_types::ContractError`
+
+use crate::constants::SELECTOR_LEN;
+use ethers_core::abi::AbiDecode;
+
 /// Returns the revert reason from the given output data, if it's an abi encoded String. Returns
 /// `None` if the output is not long enough to contain a function selector or the content is not a
 /// valid abi encoded String.
 ///
 /// **Note:** it's assumed the `out` buffer starts with the call's signature
-pub fn decode_revert_reason(_out: impl AsRef<[u8]>) -> Option<String> {
-    todo!("use alloy_sol_types")
+pub fn decode_revert_reason(out: impl AsRef<[u8]>) -> Option<String> {
+    let out = out.as_ref();
+    if out.len() < SELECTOR_LEN {
+        return None
+    }
+    String::decode(&out[SELECTOR_LEN..]).ok()
 }
