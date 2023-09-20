cb7a00c1b3d17958c91c3fb8c6063f09362462b0
joshieDo <ranriver@protonmail.com>
2023-09-20 14:48:54 +0000

    add example doc to is_ready

--- crates/storage/nippy-jar/src/compression/mod.rs
+++ crates/storage/nippy-jar/src/compression/mod.rs
@@ -14,6 +14,8 @@ pub trait Compression: Serialize + for<'a> Deserialize<'a> {
     fn compress_to<W: Write>(&self, src: &[u8], dest: &mut W) -> Result<(), NippyJarError>;
 
     /// Returns `true` if it's ready to compress.
+    ///
+    /// Example: it will return false, if `zstd` with dictionary is set, but wasn't generated.
     fn is_ready(&self) -> bool {
         true
     }
