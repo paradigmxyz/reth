1f6241850592467d8fb6f7362230e44fac5279fe
Matthias Seitz <matthias.seitz@outlook.de>
2023-09-25 13:27:27 +0200

    chore: pin breaking icu releases

--- crates/revm/revm-inspectors/src/lib.rs
+++ crates/revm/revm-inspectors/src/lib.rs
@@ -4,7 +4,7 @@
     html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
     issue_tracker_base_url = "https://github.com/paradigmxzy/reth/issues/"
 )]
-#![warn(missing_docs, unreachable_pub, unused_crate_dependencies)]
+#![warn(missing_docs, unreachable_pub)]
 #![deny(unused_must_use, rust_2018_idioms)]
 #![doc(test(
     no_crate_inject,
