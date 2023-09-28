b85aa94033aaafe626b0438014bc35f3a9a5992b
Alexey Shekhirin <a.shekhirin@gmail.com>
2023-09-28 15:27:30 +0100

    docs: paradigmxzy -> paradigmxyz (#4837)

--- crates/storage/libmdbx-rs/mdbx-sys/src/lib.rs
+++ crates/storage/libmdbx-rs/mdbx-sys/src/lib.rs
@@ -3,7 +3,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
     html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
-    issue_tracker_base_url = "https://github.com/paradigmxzy/reth/issues/"
+    issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
 )]
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, clippy::all)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
