76b52b126360bf27e1ae037d9892c61b6316edfd
Matthias Seitz <matthias.seitz@outlook.de>
2023-10-10 15:36:27 +0200

    chore: rename fn

--- crates/rpc/rpc-types-compat/src/log.rs
+++ crates/rpc/rpc-types-compat/src/log.rs
@@ -1,5 +1,7 @@
+//! Compatibility functions for rpc `Log` type.
+
 /// Creates a new rpc Log from a primitive log type from DB
-pub fn from_primitive(log: reth_primitives::Log) -> reth_rpc_types::Log {
+pub fn from_primitive_log(log: reth_primitives::Log) -> reth_rpc_types::Log {
     reth_rpc_types::Log {
         address: log.address,
         topics: log.topics,
