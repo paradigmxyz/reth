8ae12f978fd6388f30474393d2ec148b8d5f0d81
alessandromazza <alessandro98.mazza@gmail.com>
2023-10-10 15:01:24 +0200

    only override state if it's first time to meet that account

--- crates/storage/provider/src/providers/database/provider.rs
+++ crates/storage/provider/src/providers/database/provider.rs
@@ -263,14 +263,23 @@ impl<'this, TX: DbTxMut<'this> + DbTx<'this>> DatabaseProvider<'this, TX> {
         let mut plain_accounts_cursor = self.tx.cursor_write::<tables::PlainAccountState>()?;
         let mut plain_storage_cursor = self.tx.cursor_dup_write::<tables::PlainStorageState>()?;
 
+        // list of all seen addresses
+        let mut addresses_seen = vec![];
+
         // add account changeset changes
         for (block_number, account_before) in account_changeset.into_iter().rev() {
             let AccountBeforeTx { info: old_info, address } = account_before;
-            let new_info = plain_accounts_cursor.seek_exact(address)?.map(|kv| kv.1);
+            if !addresses_seen.contains(&address) {
+                // save address as seen
+                addresses_seen.push(address);
+                let new_info = plain_accounts_cursor.seek_exact(address)?.map(|kv| kv.1);
+                bundle_builder = bundle_builder.state_present_account_info(
+                    address,
+                    into_revm_acc(new_info.unwrap_or_default()),
+                );
+            }
             bundle_builder = bundle_builder
                 .state_original_account_info(address, into_revm_acc(old_info.unwrap_or_default()));
-            bundle_builder = bundle_builder
-                .state_present_account_info(address, into_revm_acc(new_info.unwrap_or_default()));
             // insert old info into reverts.
             bundle_builder = bundle_builder.revert_account_info(
                 block_number,
@@ -286,16 +295,17 @@ impl<'this, TX: DbTxMut<'this> + DbTx<'this>> DatabaseProvider<'this, TX> {
         // add storage changeset changes
         for (block_and_address, old_storage) in storage_changeset.into_iter().rev() {
             let BlockNumberAddress((block_number, address)) = block_and_address;
-            let present_info = plain_accounts_cursor.seek_exact(address)?.map(|kv| kv.1);
-            bundle_builder = bundle_builder.state_original_account_info(
-                address,
-                into_revm_acc(present_info.unwrap_or_default()),
-            );
-            bundle_builder = bundle_builder.state_present_account_info(
-                address,
-                into_revm_acc(present_info.unwrap_or_default()),
-            );
-
+            if !addresses_seen.contains(&address) {
+                let present_info = plain_accounts_cursor.seek_exact(address)?.map(|kv| kv.1);
+                bundle_builder = bundle_builder.state_original_account_info(
+                    address,
+                    into_revm_acc(present_info.unwrap_or_default()),
+                );
+                bundle_builder = bundle_builder.state_present_account_info(
+                    address,
+                    into_revm_acc(present_info.unwrap_or_default()),
+                );
+            }
             let new_storage = plain_storage_cursor
                 .seek_by_key_subkey(address, old_storage.key)?
                 .filter(|storage| storage.key == old_storage.key)
