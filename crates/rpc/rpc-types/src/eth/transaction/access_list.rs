f49ac8c67076e80dd4d02bc4a33998d13d816da3
DoTheBestToGetTheBest <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2023-10-12 02:32:29 -0700

    Create access_list.rs

---
+++ crates/rpc/rpc-types/src/eth/transaction/access_list.rs
@@ -0,0 +1,101 @@
+// use crate::{Address, B256};
+use alloy_primitives::bytes;
+//use alloy_primitives::B256;
+use alloy_primitives::U256;
+//use reth_primitives::Address;
+use alloy_primitives::aliases::B256;
+use alloy_primitives::Address;
+use alloy_rlp::{RlpDecodable, RlpDecodableWrapper, RlpEncodable, RlpEncodableWrapper};
+
+use reth_codecs::{main_codec, Compact};
+use serde::{Deserialize, Serialize};
+use std::mem;
+/// A list of addresses and storage keys that the transaction plans to access.
+/// Accesses outside the list are possible, but become more expensive.
+
+#[main_codec(rlp)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Default, RlpDecodable, RlpEncodable)]
+#[serde(rename_all = "camelCase")]
+pub struct AccessListItem {
+    /// Account addresses that would be loaded at the start of execution
+    pub address: Address,
+    /// Keys of storage that would be loaded at the start of execution
+    #[cfg_attr(
+        any(test, feature = "arbitrary"),
+        proptest(
+            strategy = "proptest::collection::vec(proptest::arbitrary::any::<B256>(), 0..=20)"
+        )
+    )]
+    pub storage_keys: Vec<B256>,
+}
+
+impl AccessListItem {
+    /// Calculates a heuristic for the in-memory size of the [AccessListItem].
+    #[inline]
+    pub fn size(&self) -> usize {
+        mem::size_of::<Address>() + self.storage_keys.capacity() * mem::size_of::<B256>()
+    }
+}
+
+/// AccessList as defined in EIP-2930
+#[main_codec(rlp)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Default, RlpDecodableWrapper, RlpEncodableWrapper)]
+pub struct AccessList(
+    #[cfg_attr(
+        any(test, feature = "arbitrary"),
+        proptest(
+            strategy = "proptest::collection::vec(proptest::arbitrary::any::<AccessListItem>(), 0..=20)"
+        )
+    )]
+    pub Vec<AccessListItem>,
+);
+
+impl AccessList {
+    /// Converts the list into a vec, expected by revm
+    pub fn flattened(&self) -> Vec<(Address, Vec<U256>)> {
+        self.flatten().collect()
+    }
+
+    /// Consumes the type and converts the list into a vec, expected by revm
+    pub fn into_flattened(self) -> Vec<(Address, Vec<U256>)> {
+        self.into_flatten().collect()
+    }
+
+    /// Consumes the type and returns an iterator over the list's addresses and storage keys.
+    pub fn into_flatten(self) -> impl Iterator<Item = (Address, Vec<U256>)> {
+        self.0.into_iter().map(|item| {
+            (
+                item.address,
+                item.storage_keys.into_iter().map(|slot| U256::from_be_bytes(slot.0)).collect(),
+            )
+        })
+    }
+
+    /// Returns an iterator over the list's addresses and storage keys.
+    pub fn flatten(&self) -> impl Iterator<Item = (Address, Vec<U256>)> + '_ {
+        self.0.iter().map(|item| {
+            (
+                item.address,
+                item.storage_keys.iter().map(|slot| U256::from_be_bytes(slot.0)).collect(),
+            )
+        })
+    }
+
+    /// Calculates a heuristic for the in-memory size of the [AccessList].
+    #[inline]
+    pub fn size(&self) -> usize {
+        // take into account capacity
+        self.0.iter().map(AccessListItem::size).sum::<usize>()
+            + self.0.capacity() * mem::size_of::<AccessListItem>()
+    }
+}
+
+/// Access list with gas used appended.
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct AccessListWithGasUsed {
+    /// List with accounts accessed during transaction.
+    pub access_list: AccessList,
+    /// Estimated gas used with access list.
+    pub gas_used: U256,
+}
