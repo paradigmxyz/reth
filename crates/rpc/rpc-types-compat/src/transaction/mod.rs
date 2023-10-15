27d46c181f3ac0dc46d5d4cec5a4f0c86116eefb
DoTheBestToGetTheBest <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2023-10-15 07:23:22 -0700

    Update mod.rs

--- crates/rpc/rpc-types-compat/src/transaction/mod.rs
+++ crates/rpc/rpc-types-compat/src/transaction/mod.rs
@@ -4,8 +4,7 @@ use reth_primitives::{
     BlockNumber, Transaction as PrimitiveTransaction, TransactionKind as PrimitiveTransactionKind,
     TransactionSignedEcRecovered, TxType, B256, U128, U256, U64,
 };
-use reth_rpc_types::access_list::AccessListItem;
-use reth_rpc_types::Transaction;
+use reth_rpc_types::{access_list::AccessListItem, Transaction};
 use signature::from_primitive_signature;
 /// Create a new rpc transaction result for a mined transaction, using the given block hash,
 /// number, and tx index fields to populate the corresponding fields in the rpc result.
