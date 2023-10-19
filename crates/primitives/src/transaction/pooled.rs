88bf202cef27db5e8c333135c31f49ac5f3642a0
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-10-19 07:55:18 -0400

    feat: re-add Arbitrary for PooledTransactionsElement (#5064)

--- crates/primitives/src/transaction/pooled.rs
+++ crates/primitives/src/transaction/pooled.rs
@@ -1,17 +1,19 @@
 //! Defines the types for blob transactions, legacy, and other EIP-2718 transactions included in a
 //! response to `GetPooledTransactions`.
 use crate::{
-    Address, BlobTransaction, Bytes, Signature, Transaction, TransactionSigned,
-    TransactionSignedEcRecovered, TxEip1559, TxEip2930, TxHash, TxLegacy, B256, EIP4844_TX_TYPE_ID,
+    Address, BlobTransaction, BlobTransactionSidecar, Bytes, Signature, Transaction,
+    TransactionSigned, TransactionSignedEcRecovered, TxEip1559, TxEip2930, TxHash, TxLegacy, B256,
+    EIP4844_TX_TYPE_ID,
 };
 use alloy_rlp::{Decodable, Encodable, Error as RlpError, Header, EMPTY_LIST_CODE};
 use bytes::Buf;
 use derive_more::{AsRef, Deref};
+use reth_codecs::add_arbitrary_tests;
 use serde::{Deserialize, Serialize};
 
 /// A response to `GetPooledTransactions`. This can include either a blob transaction, or a
 /// non-4844 signed transaction.
-// TODO: redo arbitrary for this encoding - the previous encoding was incorrect
+#[add_arbitrary_tests]
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum PooledTransactionsElement {
     /// A legacy transaction
@@ -415,6 +417,49 @@ impl From<TransactionSigned> for PooledTransactionsElement {
                 })
             }
         }
+    }
+}
+
+#[cfg(any(test, feature = "arbitrary"))]
+impl<'a> arbitrary::Arbitrary<'a> for PooledTransactionsElement {
+    fn arbitrary(u: &mut arbitrary::Unstructured<'a>) -> arbitrary::Result<Self> {
+        let transaction = TransactionSigned::arbitrary(u)?;
+
+        // this will have an empty sidecar
+        let pooled_txs_element = PooledTransactionsElement::from(transaction);
+
+        // generate a sidecar for blob txs
+        if let PooledTransactionsElement::BlobTransaction(mut tx) = pooled_txs_element {
+            tx.sidecar = BlobTransactionSidecar::arbitrary(u)?;
+            Ok(PooledTransactionsElement::BlobTransaction(tx))
+        } else {
+            Ok(pooled_txs_element)
+        }
+    }
+}
+
+#[cfg(any(test, feature = "arbitrary"))]
+impl proptest::arbitrary::Arbitrary for PooledTransactionsElement {
+    type Parameters = ();
+    type Strategy = proptest::strategy::BoxedStrategy<PooledTransactionsElement>;
+
+    fn arbitrary_with(_: Self::Parameters) -> Self::Strategy {
+        use proptest::prelude::{any, Strategy};
+
+        any::<(TransactionSigned, BlobTransactionSidecar)>()
+            .prop_map(move |(transaction, sidecar)| {
+                // this will have an empty sidecar
+                let pooled_txs_element = PooledTransactionsElement::from(transaction);
+
+                // generate a sidecar for blob txs
+                if let PooledTransactionsElement::BlobTransaction(mut tx) = pooled_txs_element {
+                    tx.sidecar = sidecar;
+                    PooledTransactionsElement::BlobTransaction(tx)
+                } else {
+                    pooled_txs_element
+                }
+            })
+            .boxed()
     }
 }
 
