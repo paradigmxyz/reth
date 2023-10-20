20fedd238bb414d24dbdd2cd6fa7b9e89c5ce9f2
Matthias Seitz <matthias.seitz@outlook.de>
2023-10-20 17:16:33 +0200

    feat: add call bundle (#5100)

--- crates/rpc/rpc/src/eth/mod.rs
+++ crates/rpc/rpc/src/eth/mod.rs
@@ -1,6 +1,7 @@
 //! `eth` namespace handler implementation.
 
 mod api;
+pub mod bundle;
 pub mod cache;
 pub mod error;
 mod filter;
@@ -13,6 +14,7 @@ mod signer;
 pub(crate) mod utils;
 
 pub use api::{EthApi, EthApiSpec, EthTransactions, TransactionSource, RPC_DEFAULT_GAS_CAP};
+pub use bundle::EthBundle;
 pub use filter::EthFilter;
 pub use id_provider::EthSubscriptionIdProvider;
 pub use pubsub::EthPubSub;
