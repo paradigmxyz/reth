8a04b4ece320916552395fa0a0a143f76fd90296
DoTheBestToGetTheBest <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2023-10-22 16:21:29 -0700

    Update event_listeners.rs

--- crates/tokio-util/src/event_listeners.rs
+++ crates/tokio-util/src/event_listeners.rs
@@ -33,4 +33,25 @@ impl<T: Clone> EventListeners<T> {
     pub fn push_listener(&mut self, listener: mpsc::UnboundedSender<T>) {
         self.listeners.push(listener);
     }
+
+    /// Removes a listener if it exists.
+    pub fn remove_listener(&mut self, listener: &mpsc::UnboundedSender<T>) -> bool {
+        let listener_ptr = listener as *const _;
+        if let Some(pos) = self.listeners.iter().position(|l| l as *const _ == listener_ptr) {
+            self.listeners.remove(pos);
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Returns the number of registered listeners.
+    pub fn listener_count(&self) -> usize {
+        self.listeners.len()
+    }
+
+    /// Returns true if there are no registered listeners.
+    pub fn is_empty(&self) -> bool {
+        self.listeners.is_empty()
+    }
 }
