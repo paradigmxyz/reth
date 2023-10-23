a11116f473f3ddf97acae51ffcbc8f948a43db8f
Alexey Shekhirin <a.shekhirin@gmail.com>
2023-10-23 21:43:13 +0100

    improve metric docs and names

--- crates/storage/db/src/metrics/metrics.rs
+++ crates/storage/db/src/metrics/metrics.rs
@@ -1,22 +1,29 @@
 use crate::metrics::{Operation, Transaction, TransactionMode, TransactionOutcome};
-use metrics::Histogram;
+use metrics::{Gauge, Histogram};
 use reth_metrics::{metrics::Counter, Metrics};
 use std::{
     collections::HashMap,
     time::{Duration, Instant},
 };
 
-#[derive(Debug, Default)]
+#[derive(Metrics)]
+#[metrics(scope = "database")]
 pub(crate) struct Metrics {
-    transactions: HashMap<u64, Transaction>,
-    transaction_metrics: HashMap<(TransactionMode, TransactionOutcome), TransactionMetrics>,
+    /// Total number of currently open database transactions
+    open_transactions_total: Gauge,
 
+    #[metric(skip)]
+    transactions: HashMap<u64, Transaction>,
+    #[metric(skip)]
+    transaction_metrics: HashMap<(TransactionMode, TransactionOutcome), TransactionMetrics>,
+    #[metric(skip)]
     operation_metrics: HashMap<Operation, OperationMetrics>,
 }
 
 impl Metrics {
     pub(crate) fn record_open_transaction(&mut self, txn_id: u64, mode: TransactionMode) {
         self.transactions.insert(txn_id, Transaction { begin: Instant::now(), mode });
+        self.open_transactions_total.set(self.transactions.len() as f64)
     }
 
     pub(crate) fn record_close_transaction(
@@ -33,8 +40,10 @@ impl Metrics {
                         ("outcome", outcome.to_string()),
                     ])
                 });
-            metrics.open_duration.record(transaction.begin.elapsed());
-            metrics.commit_duration.record(commit_duration);
+            metrics.open_duration_seconds.record(transaction.begin.elapsed());
+            metrics.commit_duration_seconds.record(commit_duration);
+
+            self.open_transactions_total.set(self.transactions.len() as f64)
         }
     }
 
@@ -43,25 +52,25 @@ impl Metrics {
             OperationMetrics::new_with_labels(&[("operation", operation.to_string())])
         });
 
-        metrics.duration.record(duration);
-        metrics.called.increment(1);
+        metrics.calls_total.increment(1);
+        metrics.duration_seconds.record(duration);
     }
 }
 
 #[derive(Metrics)]
 #[metrics(scope = "database.transaction")]
 struct TransactionMetrics {
-    /// open duration
-    pub(crate) open_duration: Histogram,
-    /// commit duration
-    pub(crate) commit_duration: Histogram,
+    /// The time a database transaction has been open
+    pub(crate) open_duration_seconds: Histogram,
+    /// Database transaction commit duration
+    pub(crate) commit_duration_seconds: Histogram,
 }
 
 #[derive(Metrics)]
 #[metrics(scope = "database.operation")]
 struct OperationMetrics {
-    /// called
-    pub(crate) called: Counter,
-    /// duration
-    pub(crate) duration: Histogram,
+    /// Total number of database operations made
+    pub(crate) calls_total: Counter,
+    /// Database operation duration
+    pub(crate) duration_seconds: Histogram,
 }
