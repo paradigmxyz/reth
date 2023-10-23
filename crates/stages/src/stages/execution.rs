0c794849f9181ce1fc0363fb8f7a624bea623626
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2023-10-23 19:22:18 +0200

    feat(stage): consolidate `Validation` and `ExecutionError` variants in `StageError` enum (#5106)

--- crates/stages/src/stages/execution.rs
+++ crates/stages/src/stages/execution.rs
@@ -1,6 +1,6 @@
 use crate::{
-    stages::MERKLE_STAGE_DEFAULT_CLEAN_THRESHOLD, ExecInput, ExecOutput, MetricEvent,
-    MetricEventsSender, Stage, StageError, UnwindInput, UnwindOutput,
+    stages::MERKLE_STAGE_DEFAULT_CLEAN_THRESHOLD, BlockErrorKind, ExecInput, ExecOutput,
+    MetricEvent, MetricEventsSender, Stage, StageError, UnwindInput, UnwindOutput,
 };
 use num_traits::Zero;
 use reth_db::{
@@ -161,7 +161,10 @@ impl<EF: ExecutorFactory> ExecutionStage<EF> {
             // Execute the block
             let (block, senders) = block.into_components();
             executor.execute_and_verify_receipt(&block, td, Some(senders)).map_err(|error| {
-                StageError::ExecutionError { block: block.header.clone().seal_slow(), error }
+                StageError::Block {
+                    block: block.header.clone().seal_slow(),
+                    error: BlockErrorKind::Execution(error),
+                }
             })?;
 
             execution_duration += time.elapsed();
