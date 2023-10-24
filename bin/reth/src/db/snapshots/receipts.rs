c562e0c77e3b7a2c569fb94b85d0805497147760
joshieDo <93316087+joshieDo@users.noreply.github.com>
2023-10-24 11:49:28 +0100

    Update bin/reth/src/db/snapshots/receipts.rs
    Co-authored-by: Alexey Shekhirin <a.shekhirin@gmail.com>

--- bin/reth/src/db/snapshots/receipts.rs
+++ bin/reth/src/db/snapshots/receipts.rs
@@ -61,7 +61,7 @@ impl Command {
         let mut rng = rand::thread_rng();
         let mut dictionaries = None;
         let mut jar = NippyJar::load(&get_snapshot_segment_file_name(
-            SnapshotSegment::Headers,
+            SnapshotSegment::Receipts,
             filters,
             compression,
             &block_range,
