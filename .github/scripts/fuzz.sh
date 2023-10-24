e29e57abd7f2c2079e804739e886cf3a9b812c0e
Matthias Seitz <matthias.seitz@outlook.de>
2023-10-24 16:32:25 +0200

    chore: rm lcov support (#5159)

--- .github/scripts/fuzz.sh
+++ .github/scripts/fuzz.sh
@@ -8,12 +8,6 @@ TEST_TIME=${2:-5}
 echo Building corpus.
 cargo test -p $PACKAGE
 
-# We configure coverage after building a corpus to only include
-# fuzz tests in the coverage report.
-echo Configuring coverage.
-source <(cargo llvm-cov show-env --export-prefix)
-cargo llvm-cov clean --workspace
-
 # Gets the list of tests present in the package.
 TESTS=$(cargo test-fuzz --list -p $PACKAGE | head -n -3 | tail -n+9 | cat - <(echo \"--list\"]) | cat - | jq -r ".[]")
 
@@ -24,6 +18,3 @@ do
     cargo test-fuzz --no-ui --exact -p "$PACKAGE" $test -- -V $TEST_TIME
     set +x
 done;
-
-echo Building coverage report.
-cargo llvm-cov report --lcov --output-path lcov.info
