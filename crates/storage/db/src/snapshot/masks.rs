56f36c776de4fe6a6895713df22f6c836ff6dec5
joshieDo <ranriver@protonmail.com>
2023-10-27 10:55:44 +0000

    order masks

--- crates/storage/db/src/snapshot/masks.rs
+++ crates/storage/db/src/snapshot/masks.rs
@@ -10,16 +10,17 @@ use reth_primitives::{BlockHash, Header};
 // HEADER MASKS
 
 add_snapshot_mask!(HeaderMask, Header, 0b001);
-add_snapshot_mask!(HeaderMask, BlockHash, 0b100);
 add_snapshot_mask!(HeaderMask, <HeaderTD as Table>::Value, 0b010);
+add_snapshot_mask!(HeaderMask, BlockHash, 0b100);
 
+add_snapshot_mask!(HeaderMask, Header, BlockHash, 0b101);
 add_snapshot_mask!(
     HeaderMask,
     <HeaderTD as Table>::Value,
     <CanonicalHeaders as Table>::Value,
     0b110
 );
-add_snapshot_mask!(HeaderMask, Header, BlockHash, 0b101);
+
 
 // RECEIPT MASKS
 add_snapshot_mask!(ReceiptMask, <Receipts as Table>::Value, 0b1);
