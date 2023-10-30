d10dcb7e67229ed9ef5e7ed237e6e09a74c4d2e7
refcell <abigger87@gmail.com>
2023-10-30 18:59:24 -0400

    fix remove double operator formatter error

--- crates/consensus/beacon/src/engine/mod.rs
+++ crates/consensus/beacon/src/engine/mod.rs
@@ -844,7 +844,7 @@ where
         // This ensures that the safe block is consistent with the head block, i.e. the safe
         // block is an ancestor of the head block.
         if !state.safe_block_hash.is_zero()
-            && && !self.blockchain.is_canonical(state.safe_block_hash)?
+            && !self.blockchain.is_canonical(state.safe_block_hash)?
         {
             return Ok(Some(OnForkChoiceUpdated::invalid_state()));
         }
@@ -996,7 +996,7 @@ where
             // find the appropriate target to sync to, if we don't have the safe block hash then we
             // start syncing to the safe block via pipeline first
             let target = if !state.safe_block_hash.is_zero()
-                && && self.blockchain.block_number(state.safe_block_hash).ok().flatten().is_none()
+                && self.blockchain.block_number(state.safe_block_hash).ok().flatten().is_none()
             {
                 state.safe_block_hash
             } else {
@@ -1189,7 +1189,7 @@ where
             Ok(block) => {
                 // make sure there are no blob transactions in the payload if it is pre-cancun
                 if !self.chain_spec().is_cancun_active_at_timestamp(block.timestamp)
-                    && && block.has_blob_transactions()
+                    && block.has_blob_transactions()
                 {
                     Err(PayloadError::PreCancunBlockWithBlobTransactions)
                 } else {
@@ -1339,7 +1339,7 @@ where
                 PayloadStatusEnum::Accepted
             }
             InsertPayloadOk::Inserted(BlockStatus::Disconnected { .. })
-            | | InsertPayloadOk::AlreadySeen(BlockStatus::Disconnected { .. }) => {
+            | InsertPayloadOk::AlreadySeen(BlockStatus::Disconnected { .. }) => {
                 // check if the block's parent is already marked as invalid
                 if let Some(status) =
                     self.check_invalid_ancestor_with_head(block.parent_hash, block.hash)
