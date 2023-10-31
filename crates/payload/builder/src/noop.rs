3ed8e43aeda2d9a2436165bb661a5d6626b63b6c
Matthias Seitz <matthias.seitz@outlook.de>
2023-10-31 18:14:10 +0100

    feat: add NoopPayloadBuilderService (#5205)
    Co-authored-by: joshieDo <93316087+joshieDo@users.noreply.github.com>

---
+++ crates/payload/builder/src/noop.rs
@@ -0,0 +1,49 @@
+//! A payload builder service task that does nothing.
+
+use crate::{service::PayloadServiceCommand, PayloadBuilderHandle};
+use futures_util::{ready, StreamExt};
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::UnboundedReceiverStream;
+
+/// A service task that does not build any payloads.
+#[derive(Debug)]
+pub struct NoopPayloadBuilderService {
+    /// Receiver half of the command channel.
+    command_rx: UnboundedReceiverStream<PayloadServiceCommand>,
+}
+
+impl NoopPayloadBuilderService {
+    /// Creates a new [NoopPayloadBuilderService].
+    pub fn new() -> (Self, PayloadBuilderHandle) {
+        let (service_tx, command_rx) = mpsc::unbounded_channel();
+        let handle = PayloadBuilderHandle::new(service_tx);
+        (Self { command_rx: UnboundedReceiverStream::new(command_rx) }, handle)
+    }
+}
+
+impl Future for NoopPayloadBuilderService {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.get_mut();
+        loop {
+            let Some(cmd) = ready!(this.command_rx.poll_next_unpin(cx)) else {
+                return Poll::Ready(())
+            };
+            match cmd {
+                PayloadServiceCommand::BuildNewPayload(attr, tx) => {
+                    let id = attr.payload_id();
+                    tx.send(Ok(id)).ok()
+                }
+                PayloadServiceCommand::BestPayload(_, tx) => tx.send(None).ok(),
+                PayloadServiceCommand::PayloadAttributes(_, tx) => tx.send(None).ok(),
+                PayloadServiceCommand::Resolve(_, tx) => tx.send(None).ok(),
+            };
+        }
+    }
+}
