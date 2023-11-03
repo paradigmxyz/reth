e697c282184778df5e23f628bbec172f5c33c111
Alexey Shekhirin <a.shekhirin@gmail.com>
2023-11-03 16:53:59 +0000

    feat(tree): measure different parts of canonicalization (#5266)

--- crates/blockchain-tree/src/metrics.rs
+++ crates/blockchain-tree/src/metrics.rs
@@ -1,7 +1,9 @@
+use metrics::Histogram;
 use reth_metrics::{
     metrics::{Counter, Gauge},
     Metrics,
 };
+use std::time::{Duration, Instant};
 
 /// Metrics for the entire blockchain tree
 #[derive(Metrics)]
@@ -25,4 +27,74 @@ pub struct TreeMetrics {
 pub struct BlockBufferMetrics {
     /// Total blocks in the block buffer
     pub blocks: Gauge,
+}
+
+#[derive(Debug)]
+pub(crate) struct MakeCanonicalDurationsRecorder {
+    start: Instant,
+    pub(crate) actions: Vec<(MakeCanonicalAction, Duration)>,
+    latest: Option<Duration>,
+}
+
+impl Default for MakeCanonicalDurationsRecorder {
+    fn default() -> Self {
+        Self { start: Instant::now(), actions: Vec::new(), latest: None }
+    }
+}
+
+impl MakeCanonicalDurationsRecorder {
+    /// Records the duration since last record, saves it for future logging and instantly reports as
+    /// a metric with `action` label.
+    pub(crate) fn record_relative(&mut self, action: MakeCanonicalAction) {
+        let elapsed = self.start.elapsed();
+        let duration = elapsed - self.latest.unwrap_or_default();
+
+        self.actions.push((action, duration));
+        MakeCanonicalMetrics::new_with_labels(&[("action", action.as_str())])
+            .duration
+            .record(duration);
+
+        self.latest = Some(elapsed);
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub(crate) enum MakeCanonicalAction {
+    CloneOldBlocks,
+    FindCanonicalHeader,
+    SplitChain,
+    SplitChainForks,
+    MergeAllChains,
+    UpdateCanonicalIndex,
+    CommitCanonicalChainToDatabase,
+    RevertCanonicalChainFromDatabase,
+    InsertOldCanonicalChain,
+}
+
+impl MakeCanonicalAction {
+    fn as_str(&self) -> &'static str {
+        match self {
+            MakeCanonicalAction::CloneOldBlocks => "clone old blocks",
+            MakeCanonicalAction::FindCanonicalHeader => "find canonical header",
+            MakeCanonicalAction::SplitChain => "split chain",
+            MakeCanonicalAction::SplitChainForks => "split chain forks",
+            MakeCanonicalAction::MergeAllChains => "merge all chains",
+            MakeCanonicalAction::UpdateCanonicalIndex => "update canonical index",
+            MakeCanonicalAction::CommitCanonicalChainToDatabase => {
+                "commit canonical chain to database"
+            }
+            MakeCanonicalAction::RevertCanonicalChainFromDatabase => {
+                "revert canonical chain from database"
+            }
+            MakeCanonicalAction::InsertOldCanonicalChain => "insert old canonical chain",
+        }
+    }
+}
+
+#[derive(Metrics)]
+#[metrics(scope = "blockchain_tree.make_canonical")]
+/// Canonicalization metrics
+struct MakeCanonicalMetrics {
+    /// The time it took to execute an action
+    duration: Histogram,
 }
