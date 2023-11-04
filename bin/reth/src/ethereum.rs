9423c95d871e22dc715b6b328fd65b8dfe28cf0b
nicolas <48695862+merklefruit@users.noreply.github.com>
2023-11-04 13:03:31 +0100

    wip: splitting bins

--- bin/reth/src/ethereum.rs
+++ bin/reth/src/ethereum.rs
@@ -3,6 +3,10 @@
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
+#[cfg(feature = "optimism")]
+compile_error!("run --bin op-reth for Optimism");
+
+#[cfg(not(feature = "optimism"))]
 fn main() {
     if let Err(err) = reth::cli::run() {
         eprintln!("Error: {err:?}");
