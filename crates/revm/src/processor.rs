08762f517cf7a4923b12f85d791b32dea7dab02b
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-11-05 13:46:13 +0100

    chore: clippy

--- crates/revm/src/processor.rs
+++ crates/revm/src/processor.rs
@@ -236,7 +236,7 @@ impl<'a> EVMProcessor<'a> {
         }
         // increment balances
         self.db_mut()
-            .increment_balances(balance_increments.into_iter().map(|(k, v)| (k, v)))
+            .increment_balances(balance_increments)
             .map_err(|_| BlockValidationError::IncrementBalanceFailed)?;
 
         Ok(())
