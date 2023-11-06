c1c047547ca7de4f80c7f2b432f2b0cb4d368aec
DoTheBestToGetTheBest <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2023-11-06 09:46:20 -0800

    Update config.rs

--- bin/reth/src/cli/config.rs
+++ bin/reth/src/cli/config.rs
@@ -63,8 +63,15 @@ pub trait RethRpcConfig {
     ///
     /// The `default_jwt_path` provided as an argument will be used as the default location for the
     /// jwt secret in case the `auth_jwtsecret` argument is not provided.
-    fn auth_jwt_secret(&self, default_jwt_path: PathBuf) -> Result<JwtSecret, JwtError>;
+    // fn auth_jwt_secret(&self, default_jwt_path: PathBuf) -> Result<JwtSecret, JwtError>;
+    // This function tries to read the JWT secret from the specified path.
+    // If the file doesn't exist at the path, it will create a new JWT secret.
 
+    ///
+    fn get_or_create_jwt_secret_from_path(path: &PathBuf) -> Result<JwtSecret, JwtError>;
+    ///
+    fn auth_jwt_secret(&self, default_jwt_path: PathBuf) -> Result<Option<JwtSecret>, JwtError>;
+    ///
     /// Returns the configured jwt secret key for the regular rpc servers, if any.
     ///
     /// Note: this is not used for the auth server (engine API).
