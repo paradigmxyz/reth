c358ccadc8d433802d5b4b46dbaf19c19ef73912
Matthias Seitz <matthias.seitz@outlook.de>
2023-11-07 19:48:47 +0100

    chore: serde misc CallRequest

--- crates/rpc/rpc/src/eth/api/transactions.rs
+++ crates/rpc/rpc/src/eth/api/transactions.rs
@@ -531,7 +531,7 @@ where
                     gas_price: Some(U256::from(gas_price)),
                     max_fee_per_gas: Some(U256::from(max_fee_per_gas)),
                     value: request.value,
-                    input: request.data.clone().into(),
+                    input: request.input.clone().into(),
                     nonce: request.nonce,
                     chain_id: Some(chain_id),
                     access_list: request.access_list.clone(),
