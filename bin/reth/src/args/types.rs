921332a2753f54c79adf2ec6b0bbfc993a3d9fc0
DoTheBestToGetTheBest <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2023-11-10 04:48:57 -0800

    Update types.rs

--- bin/reth/src/args/types.rs
+++ bin/reth/src/args/types.rs
@@ -2,6 +2,48 @@
 
 use std::{fmt, str::FromStr};
 
+/// A helper type that maps `u32::MAX` to `None` when parsing CLI arguments.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct MaxAsNone(pub Option<u32>);
+
+impl MaxAsNone {
+    /// Returns the inner value.
+    pub const fn new(value: u32) -> Self {
+        Self(Some(value))
+    }
+
+    /// Returns the inner value or `u32::MAX` if `None`.
+    pub fn unwrap_or_max(self) -> u32 {
+        self.0.unwrap_or(u32::MAX)
+    }
+}
+
+impl fmt::Display for MaxAsNone {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.0 {
+            Some(value) => write!(f, "{}", value),
+            None => write!(f, "max"),
+        }
+    }
+}
+
+impl fmt::Display for ZeroAsNone {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.0 {
+            Some(value) => write!(f, "{}", value),
+            None => write!(f, "0"),
+        }
+    }
+}
+impl FromStr for ZeroAsNone {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let value = s.parse::<u64>()?;
+        Ok(Self(if value == 0 { None } else { Some(value) }))
+    }
+}
+
 /// A helper type that maps `0` to `None` when parsing CLI arguments.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct ZeroAsNone(pub Option<u64>);
@@ -18,21 +60,23 @@ impl ZeroAsNone {
     }
 }
 
-impl fmt::Display for ZeroAsNone {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self.0 {
-            Some(value) => write!(f, "{}", value),
-            None => write!(f, "0"),
-        }
-    }
-}
-
-impl FromStr for ZeroAsNone {
+impl FromStr for MaxAsNone {
     type Err = std::num::ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let value = s.parse::<u64>()?;
-        Ok(Self(if value == 0 { None } else { Some(value) }))
+        if s == "max" {
+            Ok(Self(None))
+        } else {
+            s.parse::<u32>().map(
+                |value| {
+                    if value == u32::MAX {
+                        Self(None)
+                    } else {
+                        Self(Some(value))
+                    }
+                },
+            )
+        }
     }
 }
 
@@ -45,5 +89,12 @@ mod tests {
         let val = "0".parse::<ZeroAsNone>().unwrap();
         assert_eq!(val, ZeroAsNone(None));
         assert_eq!(val.unwrap_or_max(), u64::MAX);
+    }
+
+    #[test]
+    fn test_max_parse() {
+        let val = "max".parse::<MaxAsNone>().unwrap();
+        assert_eq!(val, MaxAsNone(None));
+        assert_eq!(val.unwrap_or_max(), u32::MAX);
     }
 }
