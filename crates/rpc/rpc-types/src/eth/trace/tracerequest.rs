02502adb962204f995ffd1aebe3b06c401ff2e23
DoTheBestToGetTheBest <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2023-11-12 05:21:44 -0800

    feat(rpc-type) : make  TraceCallRequest with Default Trait  (#5394)

--- crates/rpc/rpc-types/src/eth/trace/tracerequest.rs
+++ crates/rpc/rpc-types/src/eth/trace/tracerequest.rs
@@ -8,7 +8,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 
 /// Container type for `trace_call` arguments
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Default)]
 pub struct TraceCallRequest {
     /// call request object
     pub call: CallRequest,
