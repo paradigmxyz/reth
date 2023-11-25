384e1c7b85ab41063aeae5dc9f1684834a0b30d3
DoTheBestToGetTheBest <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2023-11-25 12:24:16 -0800

    Update utils.rs

--- crates/storage/db/src/utils.rs
+++ crates/storage/db/src/utils.rs
@@ -1,31 +1,21 @@
 //! Utils crate for `db`.
+use std::{fs, path::Path};
 
-use std::path::Path;
+// Constants for page size calculations
+const LIBMDBX_MAX_PAGE_SIZE: usize = 0x10000; // Maximum page size defined by libmdbx
+const MIN_PAGE_SIZE: usize = 4096; // Minimum page size threshold
 
 /// Returns the default page size that can be used in this OS.
 pub(crate) fn default_page_size() -> usize {
     let os_page_size = page_size::get();
-
-    // source: https://gitflic.ru/project/erthink/libmdbx/blob?file=mdbx.h#line-num-821
-    let libmdbx_max_page_size = 0x10000;
-
-    // May lead to errors if it's reduced further because of the potential size of the
-    // data.
-    let min_page_size = 4096;
-
-    os_page_size.clamp(min_page_size, libmdbx_max_page_size)
+    os_page_size.clamp(MIN_PAGE_SIZE, LIBMDBX_MAX_PAGE_SIZE)
 }
 
 /// Check if a db is empty. It does not provide any information on the
 /// validity of the data in it. We consider a database as non empty when it's a non empty directory.
 pub fn is_database_empty<P: AsRef<Path>>(path: P) -> bool {
-    let path = path.as_ref();
-
-    if !path.exists() {
-        true
-    } else if let Ok(dir) = path.read_dir() {
-        dir.count() == 0
-    } else {
-        true
+    match fs::read_dir(path.as_ref()) {
+        Ok(mut dir) => dir.next().is_none(),
+        Err(_) => true,
     }
 }
