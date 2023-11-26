b18a154489a48b08369ef2d32cc9c6b898e0331b
Matthias Seitz <matthias.seitz@outlook.de>
2023-11-26 09:25:57 +0100

    fmt

--- bin/reth/src/args/txpool_args.rs
+++ bin/reth/src/args/txpool_args.rs
@@ -1,7 +1,11 @@
 //! Transaction pool arguments
 
 use clap::Args;
-use reth_transaction_pool::{PoolConfig, PriceBumpConfig, SubPoolLimit, DEFAULT_PRICE_BUMP, REPLACE_BLOB_PRICE_BUMP, TXPOOL_MAX_ACCOUNT_SLOTS_PER_SENDER, TXPOOL_SUBPOOL_MAX_SIZE_MB_DEFAULT, TXPOOL_SUBPOOL_MAX_TXS_DEFAULT, LocalTransactionConfig};
+use reth_transaction_pool::{
+    LocalTransactionConfig, PoolConfig, PriceBumpConfig, SubPoolLimit, DEFAULT_PRICE_BUMP,
+    REPLACE_BLOB_PRICE_BUMP, TXPOOL_MAX_ACCOUNT_SLOTS_PER_SENDER,
+    TXPOOL_SUBPOOL_MAX_SIZE_MB_DEFAULT, TXPOOL_SUBPOOL_MAX_TXS_DEFAULT,
+};
 
 /// Parameters for debugging purposes
 #[derive(Debug, Args, PartialEq, Default)]
@@ -48,9 +52,7 @@ impl TxPoolArgs {
     /// Returns transaction pool configuration.
     pub fn pool_config(&self) -> PoolConfig {
         PoolConfig {
-            local_transactions_config: LocalTransactionConfig {
-                no_exemptions: self.no_locals,
-            },
+            local_transactions_config: LocalTransactionConfig { no_exemptions: self.no_locals },
             pending_limit: SubPoolLimit {
                 max_txs: self.pending_max_count,
                 max_size: self.pending_max_size * 1024 * 1024,
