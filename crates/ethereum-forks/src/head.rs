9e04594e9fc30dd215b18e6842c8e3bfc77c0979
DoTheBestToGetTheBest <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2023-12-01 08:31:07 -0800

    Update head.rs

--- crates/ethereum-forks/src/head.rs
+++ crates/ethereum-forks/src/head.rs
@@ -22,3 +22,80 @@ pub struct Head {
     /// The timestamp of the head block.
     pub timestamp: u64,
 }
+impl Head {
+    /// Creates a new `Head` instance.
+    pub fn new(
+        number: BlockNumber,
+        hash: B256,
+        difficulty: U256,
+        total_difficulty: U256,
+        timestamp: u64,
+    ) -> Self {
+        Self { number, hash, difficulty, total_difficulty, timestamp }
+    }
+
+    /// Updates the head block with new information.
+    pub fn update(
+        &mut self,
+        number: BlockNumber,
+        hash: B256,
+        difficulty: U256,
+        total_difficulty: U256,
+        timestamp: u64,
+    ) {
+        self.number = number;
+        self.hash = hash;
+        self.difficulty = difficulty;
+        self.total_difficulty = total_difficulty;
+        self.timestamp = timestamp;
+    }
+
+    /// Returns a formatted string representing the head block details.
+    pub fn to_string(&self) -> String {
+        format!(
+            "Head Block:\n Number: {}\n Hash: {:?}\n Difficulty: {:?}\n Total Difficulty: {:?}\n Timestamp: {}",
+            self.number, self.hash, self.difficulty, self.total_difficulty, self.timestamp
+        )
+    }
+
+    /// Returns the hash of the head block.
+    pub fn hash(&self) -> B256 {
+        self.hash
+    }
+
+    /// Returns the block number of the head block.
+    pub fn number(&self) -> BlockNumber {
+        self.number
+    }
+
+    /// Returns the difficulty of the head block.
+    pub fn difficulty(&self) -> U256 {
+        self.difficulty
+    }
+
+    /// Returns the total difficulty of the blockchain up to the head block.
+    pub fn total_difficulty(&self) -> U256 {
+        self.total_difficulty
+    }
+
+    /// Returns the timestamp of the head block.
+    pub fn timestamp(&self) -> u64 {
+        self.timestamp
+    }
+
+    /// Checks if the head block is an empty block (i.e., has default values).
+    pub fn is_empty(&self) -> bool {
+        *self == Self::default()
+    }
+
+    /// Default implementation for Head struct.
+    pub fn default() -> Self {
+        Self {
+            number: 0,
+            hash: B256::default(),
+            difficulty: U256::default(),
+            total_difficulty: U256::default(),
+            timestamp: 0,
+        }
+    }
+}
