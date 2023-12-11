53923fd88a2f9a0d6060b5332814f7a40bd9bd82
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-12-11 11:54:41 -0500

    cargo fmt

--- bin/reth/src/node/events.rs
+++ bin/reth/src/node/events.rs
@@ -432,7 +432,7 @@ impl Display for Eta {
                     f,
                     "{}",
                     humantime::format_duration(Duration::from_secs(remaining.as_secs()))
-                );
+                )
             }
         }
 
