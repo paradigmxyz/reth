b723209343e16995fba3f8a90066bc6c9e833f20
joshieDo <ranriver@protonmail.com>
2023-12-15 10:09:09 +0000

    fix merge

--- crates/storage/provider/src/providers/database/mod.rs
+++ crates/storage/provider/src/providers/database/mod.rs
@@ -46,6 +46,57 @@ pub struct ProviderFactory<DB> {
     snapshot_provider: Option<Arc<SnapshotProvider>>,
 }
 
+impl<DB: Clone> Clone for ProviderFactory<DB> {
+    fn clone(&self) -> Self {
+        Self {
+            db: self.db.clone(),
+            chain_spec: Arc::clone(&self.chain_spec),
+            snapshot_provider: self.snapshot_provider.clone(),
+        }
+    }
+}
+
+impl<DB: Database> ProviderFactory<DB> {}
+
+impl<DB> ProviderFactory<DB> {
+    /// Create new database provider factory.
+    pub fn new(db: DB, chain_spec: Arc<ChainSpec>) -> Self {
+        Self { db, chain_spec, snapshot_provider: None }
+    }
+
+    /// Create new database provider by passing a path. [`ProviderFactory`] will own the database
+    /// instance.
+    pub fn new_with_database_path<P: AsRef<Path>>(
+        path: P,
+        chain_spec: Arc<ChainSpec>,
+        log_level: Option<LogLevel>,
+    ) -> RethResult<ProviderFactory<DatabaseEnv>> {
+        Ok(ProviderFactory::<DatabaseEnv> {
+            db: init_db(path, log_level).map_err(|e| RethError::Custom(e.to_string()))?,
+            chain_spec,
+            snapshot_provider: None,
+        })
+    }
+
+    /// Database provider that comes with a shared snapshot provider.
+    pub fn with_snapshots(
+        mut self,
+        snapshots_path: PathBuf,
+        highest_snapshot_tracker: watch::Receiver<Option<HighestSnapshots>>,
+    ) -> ProviderResult<Self> {
+        self.snapshot_provider = Some(Arc::new(
+            SnapshotProvider::new(snapshots_path)?
+                .with_highest_tracker(Some(highest_snapshot_tracker)),
+        ));
+        Ok(self)
+    }
+
+    /// Returns reference to the underlying database.
+    pub fn db_ref(&self) -> &DB {
+        &self.db
+    }
+}
+
 impl<DB: Database> ProviderFactory<DB> {
     /// Returns a provider with a created `DbTx` inside, which allows fetching data from the
     /// database using different types of providers. Example: [`HeaderProvider`]
@@ -73,60 +124,7 @@ impl<DB: Database> ProviderFactory<DB> {
 
         Ok(DatabaseProviderRW(provider))
     }
-}
 
-impl<DB> ProviderFactory<DB> {
-    /// create new database provider
-    pub fn new(db: DB, chain_spec: Arc<ChainSpec>) -> Self {
-        Self { db, chain_spec, snapshot_provider: None }
-    }
-
-    /// database provider comes with a shared snapshot provider
-    pub fn with_snapshots(
-        mut self,
-        snapshots_path: PathBuf,
-        highest_snapshot_tracker: watch::Receiver<Option<HighestSnapshots>>,
-    ) -> ProviderResult<Self> {
-        self.snapshot_provider = Some(Arc::new(
-            SnapshotProvider::new(snapshots_path)?
-                .with_highest_tracker(Some(highest_snapshot_tracker)),
-        ));
-        Ok(self)
-    }
-
-    /// Returns reference to the underlying database.
-    pub fn db_ref(&self) -> &DB {
-        &self.db
-    }
-}
-
-impl<DB: Database> ProviderFactory<DB> {
-    /// create new database provider by passing a path. [`ProviderFactory`] will own the database
-    /// instance.
-    pub fn new_with_database_path<P: AsRef<Path>>(
-        path: P,
-        chain_spec: Arc<ChainSpec>,
-        log_level: Option<LogLevel>,
-    ) -> RethResult<ProviderFactory<DatabaseEnv>> {
-        Ok(ProviderFactory::<DatabaseEnv> {
-            db: init_db(path, log_level).map_err(|e| RethError::Custom(e.to_string()))?,
-            chain_spec,
-            snapshot_provider: None,
-        })
-    }
-}
-
-impl<DB: Clone> Clone for ProviderFactory<DB> {
-    fn clone(&self) -> Self {
-        Self {
-            db: self.db.clone(),
-            chain_spec: Arc::clone(&self.chain_spec),
-            snapshot_provider: self.snapshot_provider.clone(),
-        }
-    }
-}
-
-impl<DB: Database> ProviderFactory<DB> {
     /// Storage provider for latest block
     pub fn latest(&self) -> ProviderResult<StateProviderBox> {
         trace!(target: "providers::db", "Returning latest state provider");
