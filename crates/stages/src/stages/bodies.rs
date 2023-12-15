6a33e752dc509218a2d0d347f57e67ae39d7471e
joshieDo <ranriver@protonmail.com>
2023-12-15 15:03:23 +0000

    commit snapshotter at the end of execution

--- crates/stages/src/stages/bodies.rs
+++ crates/stages/src/stages/bodies.rs
@@ -191,6 +191,12 @@ impl<DB: Database, D: BodyDownloader> Stage<DB> for BodyStage<D> {
             highest_block = block_number;
         }
 
+        // Committing static file can be done, since we unwind it if the db tx is not committed.
+        snapshotter.commit()?;
+
+        // Updates the inner snapshot provider index with the next max block and tx num
+        snapshot_provider.update_index()?;
+
         // The stage is "done" if:
         // - We got fewer blocks than our target
         // - We reached our target and the target was not limited by the batch size of the stage
