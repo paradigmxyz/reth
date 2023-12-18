dd0b1933eb755a01e81a448d89df35e53a8a9866
Matthias Seitz <matthias.seitz@outlook.de>
2023-12-18 17:10:54 +0100

    feat: add validation API endpoints (#5816)

---
+++ crates/rpc/rpc-api/src/validation.rs
@@ -0,0 +1,24 @@
+//! API for block submission validation.
+
+use jsonrpsee::proc_macros::rpc;
+use reth_rpc_types::relay::{BuilderBlockValidationRequest, BuilderBlockValidationRequestV2};
+
+/// Block validation rpc interface.
+#[cfg_attr(not(feature = "client"), rpc(server, namespace = "flashbots"))]
+#[cfg_attr(feature = "client", rpc(server, client, namespace = "flashbots"))]
+#[async_trait::async_trait]
+pub trait BlockSubmissionValidationApi {
+    /// A Request to validate a block submission.
+    #[method(name = "validateBuilderSubmissionV1")]
+    async fn validate_builder_submission_v1(
+        &self,
+        request: BuilderBlockValidationRequest,
+    ) -> jsonrpsee::core::RpcResult<()>;
+
+    /// A Request to validate a block submission.
+    #[method(name = "validateBuilderSubmissionV2")]
+    async fn validate_builder_submission_v2(
+        &self,
+        request: BuilderBlockValidationRequestV2,
+    ) -> jsonrpsee::core::RpcResult<()>;
+}
