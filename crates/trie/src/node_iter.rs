30a191ed138913ce2226665cd72fc0e647ccbf38
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2023-12-28 12:37:42 +0100

    Update crates/trie/src/node_iter.rs
    Co-authored-by: Roman Krasiuk <rokrassyuk@gmail.com>

--- crates/trie/src/node_iter.rs
+++ crates/trie/src/node_iter.rs
@@ -71,7 +71,8 @@ impl<C, H> AccountNodeIter<C, H> {
         }
     }
 
-    /// Sets the previous account key and returns the modified `AccountNodeIter`.
+    /// Sets the last iterated account key and returns the modified `AccountNodeIter`.
+    /// This is used to resume iteration from the last checkpoint. 
     pub(crate) fn with_last_account_key(mut self, previous_account_key: B256) -> Self {
         self.previous_account_key = Some(previous_account_key);
         self
