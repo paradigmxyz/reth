d0295a93a6e5e73e53e6470046aa035dcb257d0b
Thomas Coratger <thomas.coratger@gmail.com>
2023-12-28 21:22:48 +0100

    some improvements

--- crates/storage/db/build.rs
+++ crates/storage/db/build.rs
@@ -1,7 +1,8 @@
+#![allow(missing_docs)]
+
 use std::error::Error;
 use vergen::EmitBuilder;
 
-/// Emits instructions with a Git SHA enabled.
 fn main() -> Result<(), Box<dyn Error>> {
     // Emit the instructions
     EmitBuilder::builder().git_sha(true).emit()?;
