c80b66da598c02b3cd1c509f80b4c7b5d63b1645
Muhammed Abiola <hi@mabiola.net>
2023-12-29 11:28:46 +0100

    feat(crates/net/network): refactor lint configurations

--- crates/net/network/src/lib.rs
+++ crates/net/network/src/lib.rs
@@ -106,8 +106,7 @@
     html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
     issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
 )]
-#![warn(missing_debug_implementations, missing_docs, rustdoc::all)] // TODO(danipopes): unreachable_pub
-#![deny(unused_must_use, rust_2018_idioms)]
+ // TODO(danipopes): unreachable_pub
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 #[cfg(any(test, feature = "test-utils"))]
