7836f318d074fa8b7b6a2256f2fb6ee70353888a
Matthias Seitz <matthias.seitz@outlook.de>
2024-01-03 18:42:32 +0100

    wip:feat: declarative node builder

--- bin/reth/src/builder/service.rs
+++ bin/reth/src/builder/service.rs
@@ -1,15 +1,24 @@
 //! Customizable node builder service
 
+use crate::builder::{
+    traits::{FullNodeTypes, FullNodeTypesAdapter, NodeComponentsBuilder, NodeTypes},
+    NodeConfig, NodeHandle,
+};
+use reth_blockchain_tree::ShareableBlockchainTree;
 use reth_db::{
     database::Database,
     database_metrics::{DatabaseMetadata, DatabaseMetrics},
 };
+use reth_provider::providers::BlockchainProvider;
+use reth_revm::EvmProcessorFactory;
 use reth_tasks::TaskExecutor;
-use crate::{
-    builder::{NodeConfig, NodeHandle},
-    cli::ext::RethNodeCommandExt,
-};
-use crate::builder::traits::PoolBuilder;
+use std::{marker::PhantomData, sync::Arc};
+
+/// The builtin provider type of the reth node.
+// Note: we need to hardcode this because custom components might depend on it in associated types.
+// TODO: this will eventually depend on node primitive types and evm
+type RethFullProviderType<DB> =
+    BlockchainProvider<Arc<DB>, ShareableBlockchainTree<Arc<DB>, EvmProcessorFactory>>;
 
 /// Declaratively construct a node.
 ///
@@ -17,87 +26,108 @@ use crate::builder::traits::PoolBuilder;
 /// components of a node.
 ///
 /// [builder]: https://doc.rust-lang.org/1.0.0/style/ownership/builders.html
-pub struct NodeBuilder<DB, Pool, Ext, State> {
+pub struct NodeBuilder<DB, State> {
     /// All settings for how the node should be configured.
     config: NodeConfig,
     /// State of the node builder process.
     state: State,
-    /// Context for the node builder process.
-    ctx: NodeBuilderContext<DB, Pool, Ext>,
+    /// The configured database for the node.
+    database: DB,
 }
 
-impl<DB, Pool, Ext, State> NodeBuilder<DB, Pool, Ext, State> {
-
+impl<DB, State> NodeBuilder<DB, State> {
     /// Returns a reference to the node builder's config.
     pub fn config(&self) -> &NodeConfig {
         &self.config
     }
-
 }
 
-impl NodeBuilder<(), (), (), InitState> {
+impl NodeBuilder<(), InitState> {
     /// Create a new [`NodeBuilder`].
     pub fn new(config: NodeConfig) -> Self {
-        Self { config, state: InitState::default(), ctx: Default::default() }
+        Self { config, database: (), state: InitState::default() }
     }
 }
 
-impl<DB, Pool, Ext> NodeBuilder<DB, Pool, Ext, InitState> {
+impl<DB> NodeBuilder<DB, InitState> {
     /// Configures the additional external context, e.g. additional context captured via CLI args.
-    pub fn with_ext<E>(self, ext: E) -> NodeBuilder<DB, Pool, E, InitState> {
-        NodeBuilder { config: self.config, state: self.state, ctx: NodeBuilderContext { database: self.ctx.database, pool_builder: self.ctx.pool_builder, ext } }
-    }
-
-    /// Configures the additional external context, e.g. additional context captured via CLI args.
-    pub fn with_database<D>(self, database: D) -> NodeBuilder<D, Pool, Ext, InitState> {
-        NodeBuilder { config: self.config, state: self.state,  ctx: NodeBuilderContext { database, pool_builder: self.ctx.pool_builder, ext: self.ctx.ext } }
+    pub fn with_database<D>(self, database: D) -> NodeBuilder<D, InitState> {
+        NodeBuilder { config: self.config, state: self.state, database }
     }
 }
 
-impl<DB, Pool, Ext> NodeBuilder<DB, Pool, Ext, InitState>
+impl<DB> NodeBuilder<DB, InitState>
+where
+    DB: Database,
+{
+    /// Configures the types of the node.
+    pub fn with_types<T>(self) -> NodeBuilder<DB, TypesState<T, DB>> {
+        NodeBuilder { config: self.config, state: Default::default(), database: self.database }
+    }
+}
+
+impl<DB, Types> NodeBuilder<DB, TypesState<Types, DB>>
+where
+    Types: NodeTypes,
+    DB: Database + 'static,
+{
+    // /// Configures the node's components.
+    pub fn with_components<Builder>(
+        self,
+        builder: Builder,
+    ) -> NodeBuilder<DB, ComponentsState<Types, Builder>>
+    where
+        Builder: NodeComponentsBuilder<FullNodeTypesAdapter<Types, RethFullProviderType<DB>>>,
+    {
+        NodeBuilder {
+            config: self.config,
+            database: self.database,
+            state: ComponentsState { _maker: Default::default(), builder },
+        }
+    }
+}
+
+impl<DB, Types, Components> NodeBuilder<DB, ComponentsState<Types, Components>>
 where
     DB: Database + DatabaseMetrics + DatabaseMetadata + Clone + 'static,
-    Ext: RethNodeCommandExt,
-    Pool: PoolBuilder,
+    Types: NodeTypes,
+    Components: NodeComponentsBuilder<FullNodeTypesAdapter<Types, RethFullProviderType<DB>>>,
 {
     /// Launches the node.
     pub async fn launch(mut self, executor: TaskExecutor) -> eyre::Result<NodeHandle> {
-
         todo!()
     }
 }
 
+/// The initial state of the node builder process.
 #[derive(Debug, Default)]
 #[non_exhaustive]
 pub struct InitState;
 
-
-/// Captures configurable context for the node builder process.
-///
-/// This is responsible for creating the node's components.
-struct NodeBuilderContext<DB, Pool, Ext> {
-    /// Holds the database
-    database: DB,
-    /// Holds additional external context, e.g. additional context captured via CLI args.
-    ext: Ext,
-    /// The transaction pool
-    pool_builder: Pool,
-
-    // TODO add hooks here: transform the hooks from RethNodeCommandExt into fields here
+/// The state after all types of the node have been configured.
+#[derive(Debug)]
+pub struct TypesState<Types, DB>
+where
+    DB: Database,
+{
+    adapter: FullNodeTypesAdapter<Types, RethFullProviderType<DB>>,
 }
 
-impl Default for NodeBuilderContext<(), (), ()> {
+impl<Types, DB> Default for TypesState<Types, DB>
+where
+    DB: Database,
+{
     fn default() -> Self {
-        Self { database: (), ext: (), pool_builder: () }
+        Self { adapter: Default::default() }
     }
 }
 
-
-/// Customizable components for the node.
-struct NodeComponentsBuilder<Pool: PoolBuilder> {
-    pool_builder: Pool,
-    // TODO this needs to depend on the pool type
-    // TODO merge this in one trait + helper types for ethereum? this!
-    // Then make NodeCommand hooks generic over this components trait + additional generic arguments for builtins like provider etc.
-    payload_service: ()
+/// The state of the node builder process after the node's components have been configured.
+///
+/// With this state all types and components of the node are known and the node can be launched.
+#[derive(Debug)]
+pub struct ComponentsState<Types, Builder> {
+    _maker: PhantomData<Types>,
+    builder: Builder,
+    // TODO add additional hooks for rpc, on_node_launched etc. here or in another state
 }
--- bin/reth/src/builder/traits.rs
+++ bin/reth/src/builder/traits.rs
@@ -1,15 +1,105 @@
 //! Traits for the builder process.
 
+use crate::cli::components::FullProvider;
 use reth_transaction_pool::TransactionPool;
+use std::marker::PhantomData;
 
-/// A trait for building a transaction pool.
+/// Type configuration for the engine.
+pub trait EngineConfig {
+    /// The execution payload type the CL node emits via the engine API.
+    // TODO make this a trait
+    type Payload: serde::de::DeserializeOwned
+        + serde::Serialize
+        + std::fmt::Debug
+        + Clone
+        + Send
+        + Sync
+        + 'static;
+
+    /// The execution payload attribute type the CL node emits via the engine API.
+    ///
+    /// This type is emitted as part of the fork choice update call
+    // TODO make this a trait
+    type PayloadAttribute: serde::de::DeserializeOwned
+        + serde::Serialize
+        + std::fmt::Debug
+        + Clone
+        + Send
+        + Sync
+        + 'static;
+}
+
+/// Configures all the primitive types of the node.
+pub trait NodePrimitives {}
+
+/// Configures all the EVM types of the node.
+pub trait EvmConfig {}
+
+/// The type that configures the entire node.
+///
+/// TODO naming
+pub trait NodeTypes {
+    type Primitives: NodePrimitives;
+    type Engine: EngineConfig;
+    type Evm: EvmConfig;
+}
+
+// TODO add generic helpers NodeTypes
+
+/// A helper type that also provides access to the builtin provider type of the node.
+// TODO naming
+pub trait FullNodeTypes: NodeTypes {
+    type Provider: FullProvider;
+}
+
+/// An adapter type that adds the builtin provider type to the user configured node types.
+#[derive(Debug)]
+pub struct FullNodeTypesAdapter<Types, Provider> {
+    _types: PhantomData<Types>,
+    _provider: PhantomData<Provider>,
+}
+
+impl<Types, Provider> Default for FullNodeTypesAdapter<Types, Provider> {
+    fn default() -> Self {
+        Self { _types: Default::default(), _provider: Default::default() }
+    }
+}
+
+impl<Types, Provider> NodeTypes for FullNodeTypesAdapter<Types, Provider>
+where
+    Types: NodeTypes,
+{
+    type Primitives = Types::Primitives;
+    type Engine = Types::Engine;
+    type Evm = Types::Evm;
+}
+
+impl<Types, Provider> FullNodeTypes for FullNodeTypesAdapter<Types, Provider>
+where
+    Types: NodeTypes,
+    Provider: FullProvider,
+{
+    type Provider = Provider;
+}
+
+/// A type that configures all the customizable components of the node and knows how to build them.
 #[async_trait::async_trait]
-pub trait PoolBuilder {
+pub trait NodeComponentsBuilder<Node: FullNodeTypes> {
     /// The transaction pool to use.
     type Pool: TransactionPool;
 
     /// Builds the transaction pool.
     ///
-    /// Note: Implementors are responsible spawning any background tasks required by the pool, e,g, [maintain_transaction_pool](reth_transaction_pool::maintain::maintain_transaction_pool).
-    async fn build_pool(self) -> eyre::Result<Self::Pool>;
+    /// Note: Implementors are responsible spawning any background tasks required by the pool, e,g,
+    /// [maintain_transaction_pool](reth_transaction_pool::maintain::maintain_transaction_pool).
+    ///
+    /// TODO: this needs required arguments
+    async fn build_pool(&mut self) -> eyre::Result<Self::Pool>;
+
+    /// Spawns the payload service and returns a handle to it.
+    ///
+    /// TODO: this needs required arguments
+    async fn spawn_payload_service(&mut self) -> eyre::Result<()>;
 }
+
+// TODO add generic builder impl for NodeComponentsBuilder
