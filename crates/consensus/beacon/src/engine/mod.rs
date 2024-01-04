b2570fdf46e11f6413151df4d6f3d182aa30de99
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-01-04 12:47:15 -0500

    remove commented process_payload_attributes

--- crates/consensus/beacon/src/engine/mod.rs
+++ crates/consensus/beacon/src/engine/mod.rs
@@ -1070,55 +1070,6 @@ where
             .unwrap_or_else(|| hash)
     }
 
-    //    /// Validates the payload attributes with respect to the header and fork choice state.
-    //    ///
-    //    /// Note: At this point, the fork choice update is considered to be VALID, however, we can
-    // still    /// return an error if the payload attributes are invalid.
-    //    fn process_payload_attributes(
-    //        &self,
-    //        attrs: PayloadAttributes,
-    //        head: Header,
-    //        state: ForkchoiceState,
-    //    ) -> OnForkChoiceUpdated {
-    //        // 7. Client software MUST ensure that payloadAttributes.timestamp is greater than
-    // timestamp        //    of a block referenced by forkchoiceState.headBlockHash. If this
-    // condition isn't held        //    client software MUST respond with -38003: `Invalid
-    // payload attributes` and MUST NOT        //    begin a payload build process. In such an
-    // event, the forkchoiceState update MUST NOT        //    be rolled back.
-    //        if attrs.timestamp <= head.timestamp {
-    //            return OnForkChoiceUpdated::invalid_payload_attributes();
-    //        }
-
-    //        // 8. Client software MUST begin a payload build process building on top of
-    //        //    forkchoiceState.headBlockHash and identified via buildProcessId value if
-    //        //    payloadAttributes is not null and the forkchoice state has been updated
-    // successfully.        //    The build process is specified in the Payload building
-    // section.        match PayloadBuilderAttributes::try_new(state.head_block_hash, attrs) {
-    //            Ok(attributes) => {
-    //                // send the payload to the builder and return the receiver for the pending
-    // payload                // id, initiating payload job is handled asynchronously
-    //                let pending_payload_id = self.payload_builder.send_new_payload(attributes);
-
-    //                // Client software MUST respond to this method call in the following way:
-    //                // {
-    //                //      payloadStatus: {
-    //                //          status: VALID,
-    //                //          latestValidHash: forkchoiceState.headBlockHash,
-    //                //          validationError: null
-    //                //      },
-    //                //      payloadId: buildProcessId
-    //                // }
-    //                //
-    //                // if the payload is deemed VALID and the build process has begun.
-    //                OnForkChoiceUpdated::updated_with_pending_payload_id(
-    //                    PayloadStatus::new(PayloadStatusEnum::Valid, Some(state.head_block_hash)),
-    //                    pending_payload_id,
-    //                )
-    //            }
-    //            Err(_) => OnForkChoiceUpdated::invalid_payload_attributes(),
-    //        }
-    //    }
-
     /// When the Consensus layer receives a new block via the consensus gossip protocol,
     /// the transactions in the block are sent to the execution layer in the form of a
     /// [`ExecutionPayload`]. The Execution layer executes the transactions and validates the
