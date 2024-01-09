42d5f1cb53749c6963f837289631a09db1f84868
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-01-09 10:46:38 -0500

    impl Default and add non_exhaustive to type structs
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/node-builder/src/engine.rs
+++ crates/node-builder/src/engine.rs
@@ -3,7 +3,8 @@ use reth_payload_builder::{OptimismPayloadBuilderAttributes, PayloadBuilderAttri
 use reth_rpc_types::engine::{OptimismPayloadAttributes, PayloadAttributes};
 
 /// The types used in the default mainnet ethereum beacon consensus engine.
-#[derive(Debug, Clone)]
+#[derive(Debug, Default, Clone)]
+#[non_exhaustive]
 pub struct EthEngineTypes;
 
 impl EngineTypes for EthEngineTypes {
@@ -12,7 +13,8 @@ impl EngineTypes for EthEngineTypes {
 }
 
 /// The types used in the optimism beacon consensus engine.
-#[derive(Debug, Clone)]
+#[derive(Debug, Default, Clone)]
+#[non_exhaustive]
 pub struct OptimismEngineTypes;
 
 impl EngineTypes for OptimismEngineTypes {
