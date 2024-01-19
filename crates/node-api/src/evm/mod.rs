680ce3157b4a3a5b61a2710616f9f690d6942ec4
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-01-19 17:33:57 -0500

    trim down evm env config trait

--- crates/node-api/src/evm/mod.rs
+++ crates/node-api/src/evm/mod.rs
@@ -1,7 +1,4 @@
-use reth_primitives::{
-    revm::env::{fill_block_env_with_coinbase, recover_header_signer},
-    Address, Chain, ChainSpec, Header, Transaction, U256,
-};
+use reth_primitives::{revm::env::fill_block_env, Address, ChainSpec, Header, Transaction, U256};
 use revm_primitives::{BlockEnv, CfgEnv, SpecId, TxEnv};
 
 /// This represents the set of methods used to configure the EVM before execution.
@@ -33,31 +30,6 @@ pub trait EvmEnvConfig: Send + Sync {
     ) {
         Self::fill_cfg_env(cfg, chain_spec, header, total_difficulty);
         let after_merge = cfg.spec_id >= SpecId::MERGE;
-        Self::fill_block_env(block_env, chain_spec, header, after_merge);
-    }
-
-    /// Fill block environment from Block.
-    fn fill_block_env(
-        block_env: &mut BlockEnv,
-        chain_spec: &ChainSpec,
-        header: &Header,
-        after_merge: bool,
-    ) {
-        let coinbase = Self::block_coinbase(chain_spec, header, after_merge);
-        fill_block_env_with_coinbase(block_env, header, after_merge, coinbase);
-    }
-
-    /// Return the coinbase address for the given header and chain spec.
-    fn block_coinbase(chain_spec: &ChainSpec, header: &Header, after_merge: bool) -> Address {
-        if chain_spec.chain == Chain::goerli() && !after_merge {
-            recover_header_signer(header).unwrap_or_else(|err| {
-                panic!(
-                    "Failed to recover goerli Clique Consensus signer from header ({}, {}) using extradata {}: {:?}",
-                    header.number, header.hash_slow(), header.extra_data, err
-                )
-            })
-        } else {
-            header.beneficiary
-        }
+        fill_block_env(block_env, chain_spec, header, after_merge);
     }
 }
