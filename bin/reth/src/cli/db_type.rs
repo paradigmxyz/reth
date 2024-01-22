70632320363607e9f4e45b93a0cc02545ea8f75d
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-01-22 17:25:06 -0500

    remove final import change

--- bin/reth/src/cli/db_type.rs
+++ bin/reth/src/cli/db_type.rs
@@ -7,7 +7,7 @@ use reth_db::{
     DatabaseEnv,
 };
 use reth_interfaces::db::LogLevel;
-use reth_node_core::dirs::{ChainPath, DataDirPath, MaybePlatformPath};
+use crate::dirs::{ChainPath, DataDirPath, MaybePlatformPath};
 use std::{str::FromStr, sync::Arc};
 
 /// A type that represents either a _real_ (represented by a path), or _test_ database, which will
