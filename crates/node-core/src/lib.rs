51837726d02c730673ce58cc1cf1a3334b4eff23
Matthias Seitz <matthias.seitz@outlook.de>
2024-01-23 13:09:34 +0100

    chore(deps): clean up new crate deps (#6174)

--- crates/node-core/src/lib.rs
+++ crates/node-core/src/lib.rs
@@ -1,8 +1,12 @@
 //! The core of the Ethereum node. Collection of utilities and libraries that are used by the node.
 
-#![allow(missing_docs)]
-#![allow(missing_debug_implementations)]
-#![allow(dead_code)]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
+    html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
+    issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
+)]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![warn(unused_crate_dependencies)]
 
 pub mod args;
 pub mod cli;
