81e7008b49044ab0e8ef461d0bd67296cf0fb0c6
Matthias Seitz <matthias.seitz@outlook.de>
2024-01-23 14:03:03 +0100

    add traits for closures

--- crates/node-builder/src/components/network.rs
+++ crates/node-builder/src/components/network.rs
@@ -1,12 +1,22 @@
 //! Network component for the node builder.
 
-use crate::components::BuilderContext;
 use reth_network::NetworkHandle;
 use reth_node_api::node::FullNodeTypes;
 use reth_transaction_pool::TransactionPool;
+use crate::components::BuilderContext;
 
 /// A type that knows how to build the network implementation.
-pub trait NetworkBuilder<Node: FullNodeTypes, Pool: TransactionPool>: Send {
+pub trait NetworkBuilder<Node: FullNodeTypes, Pool: TransactionPool> {
     /// Launches the network implementation and returns the handle to it.
     fn build_network(self, ctx: &BuilderContext<Node>, pool: Pool) -> eyre::Result<NetworkHandle>;
+}
+
+impl<Node: FullNodeTypes, F, Pool> NetworkBuilder<Node, Pool> for F
+where
+    F: FnOnce(&BuilderContext<Node>, Pool) -> eyre::Result<NetworkHandle>,
+    Pool: TransactionPool,
+{
+    fn build_network(self, ctx: &BuilderContext<Node>, pool: Pool) -> eyre::Result<NetworkHandle> {
+        self(ctx, pool)
+    }
 }
--- crates/node-builder/src/components/payload.rs
+++ crates/node-builder/src/components/payload.rs
@@ -1,16 +1,27 @@
 //! Payload service component for the node builder.
 
-use crate::components::BuilderContext;
 use reth_node_api::node::FullNodeTypes;
 use reth_payload_builder::PayloadBuilderHandle;
 use reth_transaction_pool::TransactionPool;
+use crate::components::BuilderContext;
 
 /// A type that knows how to spawn the payload service.
-pub trait PayloadServiceBuilder<Node: FullNodeTypes, Pool: TransactionPool>: Send {
+pub trait PayloadServiceBuilder<Node: FullNodeTypes, Pool: TransactionPool> {
     /// Spawns the payload service and returns the handle to it.
     fn spawn_payload_service(
         self,
         ctx: &BuilderContext<Node>,
         pool: Pool,
     ) -> eyre::Result<PayloadBuilderHandle<Node::Engine>>;
+}
+
+
+impl<Node: FullNodeTypes, F, Pool> PayloadServiceBuilder<Node, Pool> for F
+    where
+        F: FnOnce(&BuilderContext<Node>, Pool) -> eyre::Result<PayloadBuilderHandle<Node::Engine>>,
+        Pool: TransactionPool,
+{
+    fn spawn_payload_service(self, ctx: &BuilderContext<Node>, pool: Pool) -> eyre::Result<PayloadBuilderHandle<Node::Engine>> {
+        self(ctx, pool)
+    }
 }
