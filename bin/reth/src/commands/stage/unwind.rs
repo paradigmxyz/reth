87f50c9356b62b96b8c4bf3bc1c782e5323f8c70
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-01-23 12:24:56 +0000

    feat(storage, mdbx): transaction manager (#6126)

--- bin/reth/src/commands/stage/unwind.rs
+++ bin/reth/src/commands/stage/unwind.rs
@@ -1,11 +1,5 @@
 //! Unwinding a certain block range
 
-use clap::{Parser, Subcommand};
-use reth_db::{cursor::DbCursorRO, database::Database, open_db, tables, transaction::DbTx};
-use reth_primitives::{BlockHashOrNumber, ChainSpec};
-use reth_provider::{BlockExecutionWriter, ProviderFactory};
-use std::{ops::RangeInclusive, sync::Arc};
-
 use crate::{
     args::{
         utils::{chain_help, genesis_value_parser, SUPPORTED_CHAINS},
@@ -13,6 +7,15 @@ use crate::{
     },
     dirs::{DataDirPath, MaybePlatformPath},
 };
+use clap::{Parser, Subcommand};
+use reth_db::{
+    cursor::DbCursorRO, database::Database, mdbx::DatabaseArguments, open_db, tables,
+    transaction::DbTx,
+};
+use reth_primitives::{BlockHashOrNumber, ChainSpec};
+use reth_provider::{BlockExecutionWriter, ProviderFactory};
+use std::{ops::RangeInclusive, sync::Arc};
+
 /// `reth stage unwind` command
 #[derive(Debug, Parser)]
 pub struct Command {
@@ -56,7 +59,8 @@ impl Command {
             eyre::bail!("Database {db_path:?} does not exist.")
         }
 
-        let db = open_db(db_path.as_ref(), self.db.log_level)?;
+        let db =
+            open_db(db_path.as_ref(), DatabaseArguments::default().log_level(self.db.log_level))?;
 
         let range = self.command.unwind_range(&db)?;
 
