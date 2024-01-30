dd9163acdb6deb23ae6c69fffd02b7e46a29cc7f
i-m-aditya <prblmslvr.aditya@gmail.com>
2024-01-30 16:27:51 +0530

    adding docs and few changes

--- crates/transaction-pool/src/pool/best_filter.rs
+++ crates/transaction-pool/src/pool/best_filter.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 
 use crate::{pool::best::BestTransactions, TransactionOrdering, ValidPoolTransaction};
 
+/// A wrapper around [`BestTransactions`] that also filters transactions based on a predicate.
 pub(crate) struct BestTransactionFilter<T: TransactionOrdering, P> {
     pub(crate) best: BestTransactions<T>,
     pub(crate) predicate: P,
@@ -11,7 +12,6 @@ pub(crate) struct BestTransactionFilter<T: TransactionOrdering, P> {
 impl<T, P> BestTransactionFilter<T, P>
 where
     T: TransactionOrdering,
-    P: FnMut(&Arc<ValidPoolTransaction<T::Transaction>>) -> bool,
 {
     pub(crate) fn new(best: BestTransactions<T>, predicate: P) -> Self {
         Self { best, predicate }
@@ -33,5 +33,26 @@ where
                 self.best.mark_invalid(&best);
             }
         }
+    }
+}
+
+impl<T: TransactionOrdering, P> crate::traits::BestTransactions for BestTransactionFilter<T, P>
+where
+    P: FnMut(&Arc<ValidPoolTransaction<T::Transaction>>) -> bool + std::marker::Send,
+{
+    fn mark_invalid(&mut self, tx: &Self::Item) {
+        BestTransactions::mark_invalid(&mut self.best, tx)
+    }
+
+    fn no_updates(&mut self) {
+        self.best.no_updates()
+    }
+
+    fn skip_blobs(&mut self) {
+        self.set_skip_blobs(true)
+    }
+
+    fn set_skip_blobs(&mut self, skip_blobs: bool) {
+        self.best.set_skip_blobs(skip_blobs)
     }
 }
