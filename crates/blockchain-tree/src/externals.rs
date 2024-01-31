8890dc92ff71616494896fe93a0d2b87f8ad6b3e
joshieDo <ranriver@protonmail.com>
2024-01-31 22:54:04 +0000

    fix remaining calc

--- crates/blockchain-tree/src/externals.rs
+++ crates/blockchain-tree/src/externals.rs
@@ -59,8 +59,10 @@ impl<DB: Database, EF> TreeExternals<DB, EF> {
             let snapshot_provider =
                 self.provider_factory.snapshot_provider().expect("should exist");
             let total_headers = snapshot_provider.count_entries::<tables::Headers>()? as u64;
-            let range =
-                total_headers.saturating_sub(hashes.len() as u64).saturating_sub(1)..total_headers;
+            let range = total_headers
+                .saturating_sub(1)
+                .saturating_sub((num_hashes - hashes.len()) as u64)..
+                total_headers;
 
             hashes.extend(range.clone().zip(snapshot_provider.fetch_range_with_predicate(
                 SnapshotSegment::Headers,
