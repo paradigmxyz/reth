b9ade51c016548da651df0dd1223165cb96ce955
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2024-02-05 16:42:07 +0100

    Deprecate `best_transactions_with_base_fee` (#6394)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/transaction-pool/src/pool/txpool.rs
+++ crates/transaction-pool/src/pool/txpool.rs
@@ -293,34 +293,6 @@ impl<T: TransactionOrdering> TxPool<T> {
     }
 
     /// Returns an iterator that yields transactions that are ready to be included in the block with
-    /// the given base fee.
-    pub(crate) fn best_transactions_with_base_fee(
-        &self,
-        basefee: u64,
-    ) -> Box<dyn crate::traits::BestTransactions<Item = Arc<ValidPoolTransaction<T::Transaction>>>>
-    {
-        match basefee.cmp(&self.all_transactions.pending_fees.base_fee) {
-            Ordering::Equal => {
-                // fee unchanged, nothing to shift
-                Box::new(self.best_transactions())
-            }
-            Ordering::Greater => {
-                // base fee increased, we only need to enforces this on the pending pool
-                Box::new(self.pending_pool.best_with_basefee(basefee))
-            }
-            Ordering::Less => {
-                // base fee decreased, we need to move transactions from the basefee pool to the
-                // pending pool
-                let unlocked = self.basefee_pool.satisfy_base_fee_transactions(basefee);
-                Box::new(
-                    self.pending_pool
-                        .best_with_unlocked(unlocked, self.all_transactions.pending_fees.base_fee),
-                )
-            }
-        }
-    }
-
-    /// Returns an iterator that yields transactions that are ready to be included in the block with
     /// the given base fee and optional blob fee.
     pub(crate) fn best_transactions_with_attributes(
         &self,
