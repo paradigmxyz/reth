0aec8e704501ba36526fafbeefd95b9d94d3d954
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-02-07 16:47:08 +0100

    Move p2p constants to primitives

--- crates/net/network/src/peers/mod.rs
+++ crates/net/network/src/peers/mod.rs
@@ -7,12 +7,3 @@ pub(crate) use manager::InboundConnectionError;
 pub use manager::{ConnectionInfo, Peer, PeerAction, PeersConfig, PeersHandle, PeersManager};
 pub use reputation::ReputationChangeWeights;
 pub use reth_network_api::PeerKind;
-
-/// Maximum number of available slots for outbound sessions.
-pub(crate) const DEFAULT_MAX_PEERS_OUTBOUND: usize = 100;
-
-/// Maximum number of available slots for inbound sessions.
-pub(crate) const DEFAULT_MAX_PEERS_INBOUND: usize = 30;
-
-/// Maximum number of available slots concurrent outgoing dials.
-pub(crate) const DEFAULT_MAX_CONCURRENT_DIALS: usize = 10;
