a2d4d30f13e4f36bb2fc1c371ee0e10b6eb48790
allnil <nil.medvedev17@gmail.com>
2024-02-07 18:33:00 +0000

    feat: up deps and pass new fields in struct init

--- crates/rpc/rpc/src/eth/api/fees.rs
+++ crates/rpc/rpc/src/eth/api/fees.rs
@@ -215,8 +215,6 @@ where
             blob_gas_used_ratio,
             oldest_block: U256::from(start_block),
             reward: reward_percentiles.map(|_| rewards),
-            base_fee_per_blob_gas: Default::default(),
-            blob_gas_used_ratio: Default::default(),
         })
     }
 
