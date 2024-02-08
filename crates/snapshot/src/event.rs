e61f99112879f5c2ac5d3fff78e35d04b9551ef3
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-02-08 14:55:43 +0000

    add comments

--- crates/snapshot/src/event.rs
+++ crates/snapshot/src/event.rs
@@ -1,9 +1,14 @@
 use crate::SnapshotTargets;
 use std::time::Duration;
 
-/// An event emitted by a [Pruner][crate::Pruner].
+/// An event emitted by a [Snapshotter][crate::Snapshotter].
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum SnapshotterEvent {
     /// Emitted when snapshotter finished running.
-    Finished { targets: SnapshotTargets, elapsed: Duration },
+    Finished {
+        /// Targets that were snapshotted
+        targets: SnapshotTargets,
+        /// Time it took to run the snapshotter
+        elapsed: Duration,
+    },
 }
