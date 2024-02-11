b5002b55d9af229dca1b613f681705e4846b003c
Thomas Coratger <thomas.coratger@gmail.com>
2024-02-11 19:45:54 +0100

    add test_eth_chain_id_rpc_call

--- crates/rpc/rpc-builder/tests/it/http.rs
+++ crates/rpc/rpc-builder/tests/it/http.rs
@@ -745,6 +745,40 @@ async fn test_eth_block_number_rpc_call() {
     }
 }
 
+#[tokio::test(flavor = "multi_thread")]
+async fn test_eth_chain_id_rpc_call() {
+    // Initialize test tracing for logging
+    reth_tracing::init_test_tracing();
+
+    // Launch HTTP server with the specified RPC module
+    let handle = launch_http(vec![RethRpcModule::Eth]).await;
+    let client = handle.http_client().unwrap();
+
+    // Requesting chain ID without any parameter
+    match client.request::<Option<String>, _>("eth_chainId", rpc_params![]).await {
+        Ok(_) => {}
+        Err(e) => {
+            // Panic if an error is encountered
+            panic!("Expected successful response, got error: {:?}", e);
+        }
+    };
+
+    // Define test cases with different invalid parameters
+    let invalid_params = vec!["finalized", "0x2"];
+
+    // Iterate over test cases
+    for param in invalid_params {
+        // Requesting chain ID with invalid parameter should not throw an error
+        match client.request::<Option<String>, _>("eth_chainId", rpc_params![param]).await {
+            Ok(_) => {}
+            Err(e) => {
+                // Panic if an error is encountered
+                panic!("Expected successful response, got error: {:?}", e);
+            }
+        };
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
