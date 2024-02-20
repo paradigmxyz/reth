b623a23b3f8c2b1313553d3e82ac7c2a2fdcf4a5
joshieDo <ranriver@protonmail.com>
2024-02-20 17:30:31 +0000

    fix txlookup unwind

--- crates/stages/src/stages/tx_lookup.rs
+++ crates/stages/src/stages/tx_lookup.rs
@@ -13,7 +13,7 @@ use reth_primitives::{
 };
 use reth_provider::{
     BlockReader, DatabaseProviderRW, PruneCheckpointReader, PruneCheckpointWriter, StatsReader,
-    TransactionsProviderExt,
+    TransactionsProvider, TransactionsProviderExt,
 };
 use tracing::*;
 
@@ -144,7 +144,7 @@ impl<DB: Database> Stage<DB> for TransactionLookupStage {
         // Cursors to unwind tx hash to number
         let mut body_cursor = tx.cursor_read::<tables::BlockBodyIndices>()?;
         let mut tx_hash_number_cursor = tx.cursor_write::<tables::TxHashNumber>()?;
-        let mut transaction_cursor = tx.cursor_read::<tables::Transactions>()?;
+        let snapshot_provider = provider.snapshot_provider();
         let mut rev_walker = body_cursor.walk_back(Some(*range.end()))?;
         while let Some((number, body)) = rev_walker.next().transpose()? {
             if number <= unwind_to {
@@ -154,7 +154,7 @@ impl<DB: Database> Stage<DB> for TransactionLookupStage {
             // Delete all transactions that belong to this block
             for tx_id in body.tx_num_range() {
                 // First delete the transaction and hash to id mapping
-                if let Some((_, transaction)) = transaction_cursor.seek_exact(tx_id)? {
+                if let Some(transaction) = snapshot_provider.transaction_by_id(tx_id)? {
                     if tx_hash_number_cursor.seek_exact(transaction.hash())?.is_some() {
                         tx_hash_number_cursor.delete_current()?;
                     }
@@ -250,7 +250,7 @@ mod tests {
                     total
                 }))
             }, done: true }) if block_number == previous_stage && processed == total &&
-                total == runner.db.table::<tables::Transactions>().unwrap().len() as u64
+                total == runner.db.factory.snapshot_provider().count_entries::<tables::Transactions>().unwrap() as u64
         );
 
         // Validate the stage execution
@@ -278,7 +278,9 @@ mod tests {
             .insert_blocks(seed.iter(), StorageKind::Static)
             .expect("failed to seed execution");
 
-        let total_txs = runner.db.table::<tables::Transactions>().unwrap().len() as u64;
+        let total_txs =
+            runner.db.factory.snapshot_provider().count_entries::<tables::Transactions>().unwrap()
+                as u64;
 
         // Execute first time
         let result = runner.execute(first_input).await.unwrap();
@@ -362,7 +364,7 @@ mod tests {
                     total
                 }))
             }, done: true }) if block_number == previous_stage && processed == total &&
-                total == runner.db.table::<tables::Transactions>().unwrap().len() as u64
+                total == runner.db.factory.snapshot_provider().count_entries::<tables::Transactions>().unwrap() as u64
         );
 
         // Validate the stage execution
