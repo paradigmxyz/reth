577fb7303a6c1467235a4f4e71e25eb3da29a0fa
Abner Zheng <abnerzheng@gmail.com>
2024-02-22 00:35:06 +0800

    refactor: rename `blob_gas_price` to `blob_base_fee`

--- crates/rpc/rpc/src/eth/api/fees.rs
+++ crates/rpc/rpc/src/eth/api/fees.rs
@@ -34,11 +34,11 @@ where
         Ok(suggested_tip + U256::from(base_fee))
     }
 
-    /// Returns a suggestion for a gas price for blob transactions.
-    pub(crate) async fn blob_gas_price(&self) -> EthResult<U256> {
+    /// Returns a suggestion for a base fee for blob transactions.
+    pub(crate) async fn blob_base_fee(&self) -> EthResult<U256> {
         self.block(BlockNumberOrTag::Latest)
             .await?
-            .and_then(|h| h.next_block_blob_fee())
+            .and_then(|h: reth_primitives::SealedBlock| h.next_block_blob_fee())
             .ok_or(EthApiError::ExcessBlobGasNotSet)
             .map(U256::from)
     }
