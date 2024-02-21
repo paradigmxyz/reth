d8818b366e2830a4fa3927ef65c925ff2b77445d
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-02-21 21:56:17 +0100

    chore: don't box some futures

--- bin/reth/src/commands/debug_cmd/replay_engine.rs
+++ bin/reth/src/commands/debug_cmd/replay_engine.rs
@@ -184,7 +184,7 @@ impl Command {
         let (payload_service, payload_builder): (_, PayloadBuilderHandle<EthEngineTypes>) =
             PayloadBuilderService::new(payload_generator, blockchain_db.canonical_state_stream());
 
-        ctx.task_executor.spawn_critical("payload builder service", Box::pin(payload_service));
+        ctx.task_executor.spawn_critical("payload builder service", payload_service);
 
         // Configure the consensus engine
         let network_client = network.fetch_client().await?;
