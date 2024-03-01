68b9170a00de059ef4e98b32aaf2599c91ac6552
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-03-01 12:22:27 +0100

    chore: rm duplicate

--- crates/node-builder/src/lib.rs
+++ crates/node-builder/src/lib.rs
@@ -7,7 +7,6 @@
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
-#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
 /// Node event hooks.
 pub mod hooks;
