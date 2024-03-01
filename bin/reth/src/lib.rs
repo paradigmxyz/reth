573ce56b5dde9dd9dcf492053e46e64ac82a6332
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-03-01 18:39:16 +0200

    feat: print a backtrace on SIGSEGV (#6907)

--- bin/reth/src/lib.rs
+++ bin/reth/src/lib.rs
@@ -150,6 +150,19 @@ pub mod rpc {
     }
 }
 
+#[cfg(all(unix, any(target_env = "gnu", target_os = "macos")))]
+pub mod sigsegv_handler;
+
+#[cfg(not(all(unix, any(target_env = "gnu", target_os = "macos"))))]
+pub mod sigsegv_handler {
+    //! Signal handler to extract a backtrace from stack overflow.
+    //!
+    //! This is a no-op because this platform doesn't support our signal handler's requirements.
+
+    /// No-op function.
+    pub fn install() {}
+}
+
 #[cfg(all(feature = "jemalloc", unix))]
 use jemallocator as _;
 
