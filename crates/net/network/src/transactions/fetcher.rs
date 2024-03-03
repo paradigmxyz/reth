a02ace61551bd8df1c5ec30dce18543ab09aa9e9
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-03-03 15:35:32 +0100

    Fix broken test

--- crates/net/network/src/transactions/fetcher.rs
+++ crates/net/network/src/transactions/fetcher.rs
@@ -1390,7 +1390,8 @@ mod test {
             assert_ne!(hash, signed_tx_2.hash())
         }
 
-        let request_hashes = RequestTxHashes::new(request_hashes.clone().to_vec());
+        let request_hashes =
+            RequestTxHashes::new(request_hashes.into_iter().collect::<HashSet<_>>());
 
         // but response contains tx 1 + another tx
         let response_txns = PooledTransactions(vec![signed_tx_1.clone(), signed_tx_2.clone()]);
