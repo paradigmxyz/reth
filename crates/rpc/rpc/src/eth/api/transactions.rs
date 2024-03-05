8c09b5da0e4d5b8ec6d8561ff797174f82d80141
Matthias Seitz <matthias.seitz@outlook.de>
2024-03-05 21:39:51 +0100

    chore: better docs

--- crates/rpc/rpc/src/eth/api/transactions.rs
+++ crates/rpc/rpc/src/eth/api/transactions.rs
@@ -356,8 +356,9 @@ pub trait EthTransactions: Send + Sync {
     /// 2. configures the EVM evn
     /// 3. loops over all transactions and executes them
     /// 4. calls the callback with the transaction info, the execution result, the changed state
-    /// _after_ the transaction [StateCacheDB] and the database that points to the state
-    /// right _before_ the transaction.
+    /// _after_ the transaction [State] and the database that points to the state
+    /// right _before_ the transaction, in other words the state the transaction was
+    /// executed on: `changed_state = tx(cached_state)`
     ///
     /// This accepts a `inspector_setup` closure that returns the inspector to be used for tracing
     /// the transactions.
