c86a28fd0a0e59ee17726fb03c0dc223441868fd
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-03-06 13:23:50 +0100

    chore: use `IntoIterator` more
    `IntoIterator` is more portable as a function argument than just `Iterator`.

--- crates/trie/src/test_utils.rs
+++ crates/trie/src/test_utils.rs
@@ -9,10 +9,10 @@ pub use triehash;
 /// Compute the state root of a given set of accounts using [triehash::sec_trie_root].
 pub fn state_root<I, S>(accounts: I) -> B256
 where
-    I: Iterator<Item = (Address, (Account, S))>,
+    I: IntoIterator<Item = (Address, (Account, S))>,
     S: IntoIterator<Item = (B256, U256)>,
 {
-    let encoded_accounts = accounts.map(|(address, (account, storage))| {
+    let encoded_accounts = accounts.into_iter().map(|(address, (account, storage))| {
         let storage_root = storage_root(storage.into_iter());
         let mut out = Vec::new();
         TrieAccount::from((account, storage_root)).encode(&mut out);
@@ -23,8 +23,8 @@ where
 }
 
 /// Compute the storage root for a given account using [triehash::sec_trie_root].
-pub fn storage_root<I: Iterator<Item = (B256, U256)>>(storage: I) -> B256 {
-    let encoded_storage = storage.map(|(k, v)| (k, encode_fixed_size(&v).to_vec()));
+pub fn storage_root<I: IntoIterator<Item = (B256, U256)>>(storage: I) -> B256 {
+    let encoded_storage = storage.into_iter().map(|(k, v)| (k, encode_fixed_size(&v).to_vec()));
     triehash::sec_trie_root::<KeccakHasher, _, _, _>(encoded_storage)
 }
 
@@ -32,10 +32,10 @@ pub fn storage_root<I: Iterator<Item = (B256, U256)>>(storage: I) -> B256 {
 /// [triehash::trie_root].
 pub fn state_root_prehashed<I, S>(accounts: I) -> B256
 where
-    I: Iterator<Item = (B256, (Account, S))>,
+    I: IntoIterator<Item = (B256, (Account, S))>,
     S: IntoIterator<Item = (B256, U256)>,
 {
-    let encoded_accounts = accounts.map(|(address, (account, storage))| {
+    let encoded_accounts = accounts.into_iter().map(|(address, (account, storage))| {
         let storage_root = storage_root_prehashed(storage.into_iter());
         let mut out = Vec::new();
         TrieAccount::from((account, storage_root)).encode(&mut out);
@@ -46,7 +46,7 @@ where
 }
 
 /// Compute the storage root for a given account with prehashed slots using [triehash::trie_root].
-pub fn storage_root_prehashed<I: Iterator<Item = (B256, U256)>>(storage: I) -> B256 {
-    let encoded_storage = storage.map(|(k, v)| (k, encode_fixed_size(&v).to_vec()));
+pub fn storage_root_prehashed<I: IntoIterator<Item = (B256, U256)>>(storage: I) -> B256 {
+    let encoded_storage = storage.into_iter().map(|(k, v)| (k, encode_fixed_size(&v).to_vec()));
     triehash::trie_root::<KeccakHasher, _, _, _>(encoded_storage)
 }
