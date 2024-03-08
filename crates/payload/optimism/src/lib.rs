89aca89121fbdd3c4dc5087bd2a9f6dc170183db
rakita <dragan0rakita@gmail.com>
2024-03-09 00:41:13 +0100

    add cloned as log returns ref

--- crates/payload/optimism/src/lib.rs
+++ crates/payload/optimism/src/lib.rs
@@ -115,7 +115,7 @@ mod builder {
             if args.config.attributes.no_tx_pool {
                 if let Ok(BuildOutcome::Better { payload, .. }) = self.try_build(args) {
                     trace!(target: "payload_builder", "[OPTIMISM] Forced best payload");
-                    return Some(payload)
+                    return Some(payload);
                 }
             }
 
@@ -322,14 +322,14 @@ mod builder {
         for sequencer_tx in &attributes.transactions {
             // Check if the job was cancelled, if so we can exit early.
             if cancel.is_cancelled() {
-                return Ok(BuildOutcome::Cancelled)
+                return Ok(BuildOutcome::Cancelled);
             }
 
             // A sequencer's block should never contain blob transactions.
             if matches!(sequencer_tx.tx_type(), TxType::Eip4844) {
                 return Err(PayloadBuilderError::other(
                     OptimismPayloadBuilderError::BlobTransactionRejected,
-                ))
+                ));
             }
 
             // Convert the transaction to a [TransactionSignedEcRecovered]. This is
@@ -372,11 +372,11 @@ mod builder {
                     match err {
                         EVMError::Transaction(err) => {
                             trace!(target: "payload_builder", %err, ?sequencer_tx, "Error in sequencer transaction, skipping.");
-                            continue
+                            continue;
                         }
                         err => {
                             // this is an error that we should treat as fatal for this attempt
-                            return Err(PayloadBuilderError::EvmExecutionError(err))
+                            return Err(PayloadBuilderError::EvmExecutionError(err));
                         }
                     }
                 }
@@ -397,7 +397,7 @@ mod builder {
                 tx_type: sequencer_tx.tx_type(),
                 success: result.is_success(),
                 cumulative_gas_used,
-                logs: result.logs().into_iter().map(Into::into).collect(),
+                logs: result.logs().into_iter().cloned().map(Into::into).collect(),
                 deposit_nonce: depositor.map(|account| account.nonce),
                 // The deposit receipt version was introduced in Canyon to indicate an update to how
                 // receipt hashes should be computed when set. The state transition process
@@ -422,19 +422,19 @@ mod builder {
                     // invalid which also removes all dependent transaction from
                     // the iterator before we can continue
                     best_txs.mark_invalid(&pool_tx);
-                    continue
+                    continue;
                 }
 
                 // A sequencer's block should never contain blob transactions.
                 if pool_tx.tx_type() == TxType::Eip4844 as u8 {
                     return Err(PayloadBuilderError::other(
                         OptimismPayloadBuilderError::BlobTransactionRejected,
-                    ))
+                    ));
                 }
 
                 // check if the job was cancelled, if so we can exit early
                 if cancel.is_cancelled() {
-                    return Ok(BuildOutcome::Cancelled)
+                    return Ok(BuildOutcome::Cancelled);
                 }
 
                 // convert tx to a signed transaction
@@ -465,11 +465,11 @@ mod builder {
                                     best_txs.mark_invalid(&pool_tx);
                                 }
 
-                                continue
+                                continue;
                             }
                             err => {
                                 // this is an error that we should treat as fatal for this attempt
-                                return Err(PayloadBuilderError::EvmExecutionError(err))
+                                return Err(PayloadBuilderError::EvmExecutionError(err));
                             }
                         }
                     }
@@ -490,7 +490,7 @@ mod builder {
                     tx_type: tx.tx_type(),
                     success: result.is_success(),
                     cumulative_gas_used,
-                    logs: result.logs().into_iter().map(Into::into).collect(),
+                    logs: result.logs().into_iter().cloned().map(Into::into).collect(),
                     deposit_nonce: None,
                     deposit_receipt_version: None,
                 }));
@@ -509,7 +509,7 @@ mod builder {
         // check if we have a better block
         if !is_better_payload(best_payload.as_ref(), total_fees) {
             // can skip building the block
-            return Ok(BuildOutcome::Aborted { fees: total_fees, cached_reads })
+            return Ok(BuildOutcome::Aborted { fees: total_fees, cached_reads });
         }
 
         let WithdrawalsOutcome { withdrawals_root, withdrawals } = commit_withdrawals(
