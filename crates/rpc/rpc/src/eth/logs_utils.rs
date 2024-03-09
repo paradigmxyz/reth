44637694baaa06b8d9586908a7f2673ea79a32ba
joshieDo <ranriver@protonmail.com>
2024-03-09 17:27:12 +0000

    cache transaction hash when looping over 1 receipt logs

--- crates/rpc/rpc/src/eth/logs_utils.rs
+++ crates/rpc/rpc/src/eth/logs_utils.rs
@@ -64,6 +64,8 @@ pub(crate) fn append_matching_block_logs(
     // Iterate over receipts and append matching logs.
     for (receipt_idx, receipt) in receipts.iter().enumerate() {
         let logs = &receipt.logs;
+        let mut transaction_hash = None;
+
         for log in logs {
             if log_matches_filter(block_num_hash, log, filter) {
                 let first_tx_num = match loaded_first_tx_num {
@@ -78,11 +80,14 @@ pub(crate) fn append_matching_block_logs(
                     }
                 };
 
-                // This is safe because Transactions and Receipts have the same keys.
-                let transaction_id = first_tx_num + receipt_idx as u64;
-                let transaction = provider
-                    .transaction_by_id(transaction_id)?
-                    .ok_or(ProviderError::TransactionNotFound(transaction_id.into()))?;
+                if transaction_hash.is_none() {
+                    // This is safe because Transactions and Receipts have the same keys.
+                    let transaction_id = first_tx_num + receipt_idx as u64;
+                    let transaction = provider
+                        .transaction_by_id(transaction_id)?
+                        .ok_or(ProviderError::TransactionNotFound(transaction_id.into()))?;
+                    transaction_hash = Some(transaction.hash());
+                }
 
                 let log = Log {
                     address: log.address,
@@ -90,7 +95,7 @@ pub(crate) fn append_matching_block_logs(
                     data: log.data.clone(),
                     block_hash: Some(block_num_hash.hash),
                     block_number: Some(U256::from(block_num_hash.number)),
-                    transaction_hash: Some(transaction.hash()),
+                    transaction_hash,
                     // The transaction and receipt index is always the same.
                     transaction_index: Some(U256::from(receipt_idx)),
                     log_index: Some(U256::from(log_index)),
