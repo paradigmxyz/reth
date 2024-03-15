8e1c28003bd3b0b76261e6945f3f1bac645a2030
Justin Traglia <jtraglia@pm.me>
2024-03-15 11:16:29 -0500

    Add comment for new check

--- crates/consensus/common/src/validation.rs
+++ crates/consensus/common/src/validation.rs
@@ -305,6 +305,8 @@ pub fn validate_4844_header_standalone(header: &SealedHeader) -> Result<(), Cons
         })
     }
 
+    // `excess_blob_gas` must also be a multiple of `DATA_GAS_PER_BLOB`. This will be checked later
+    // (in `calculate_excess_blob_gas`), but it doesn't hurt to catch the problem sooner.
     if excess_blob_gas % DATA_GAS_PER_BLOB != 0 {
         return Err(ConsensusError::ExcessBlobGasNotMultipleOfBlobGasPerBlob {
             excess_blob_gas,
