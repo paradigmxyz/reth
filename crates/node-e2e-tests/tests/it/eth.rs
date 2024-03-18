8ad5e5d9b0889b92f70a52a0289075ca9a8c4442
Loocapro <lucaprovini1989@gmail.com>
2024-03-18 20:21:41 +0100

    feat: added fcu and checking committed block

--- crates/node-e2e-tests/tests/it/eth.rs
+++ crates/node-e2e-tests/tests/it/eth.rs
@@ -1,18 +1,20 @@
 use crate::test_suite::TestSuite;
+use futures_util::StreamExt;
 use reth::{
     builder::{NodeBuilder, NodeHandle},
     payload::EthPayloadBuilderAttributes,
+    providers::{BlockReaderIdExt, CanonStateSubscriptions},
     rpc::{
         api::EngineApiClient,
         compat::engine::payload::convert_payload_field_v2_to_payload,
         eth::EthTransactions,
-        types::engine::{ExecutionPayloadInputV2, PayloadAttributes},
+        types::engine::{ExecutionPayloadInputV2, ForkchoiceState, PayloadAttributes},
     },
     tasks::TaskManager,
 };
 use reth_node_core::{args::RpcServerArgs, node_config::NodeConfig};
 use reth_node_ethereum::{EthEngineTypes, EthereumNode};
-use reth_primitives::{Address, B256};
+use reth_primitives::{Address, BlockNumberOrTag, B256};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 #[tokio::test]
@@ -31,6 +33,8 @@ async fn can_run_eth_node() -> eyre::Result<()> {
         .launch()
         .await?;
 
+    let mut notifications = node.provider.canonical_state_stream();
+
     let eth_api = node.rpc_registry.eth_api();
     eth_api.send_raw_transaction(transfer_tx.envelope_encoded()).await?;
 
@@ -38,8 +42,8 @@ async fn can_run_eth_node() -> eyre::Result<()> {
     let payload_id = node.payload_builder.new_payload(eth_attr).await?;
 
     let client = node.auth_server_handle().http_client();
-    let payload = EngineApiClient::<EthEngineTypes>::get_payload_v2(&client, payload_id).await?;
 
+    let payload = EngineApiClient::<EthEngineTypes>::get_payload_v2(&client, payload_id).await?;
     let exec_payload = convert_payload_field_v2_to_payload(payload.execution_payload);
     let payload_input = ExecutionPayloadInputV2 {
         execution_payload: exec_payload.as_v1().clone(),
@@ -48,11 +52,33 @@ async fn can_run_eth_node() -> eyre::Result<()> {
 
     let submission =
         EngineApiClient::<EthEngineTypes>::new_payload_v2(&client, payload_input).await?;
-
     assert!(submission.is_valid());
+
+    let hash = submission.latest_valid_hash.unwrap();
+    let fcu = EngineApiClient::<EthEngineTypes>::fork_choice_updated_v2(
+        &client,
+        ForkchoiceState {
+            head_block_hash: hash,
+            safe_block_hash: hash,
+            finalized_block_hash: hash,
+        },
+        None,
+    )
+    .await?;
+    assert!(fcu.is_valid());
+
+    let head = notifications.next().await.unwrap();
+    while let Some(tx) = head.tip().transactions().next() {
+        assert_eq!(tx.hash(), transfer_tx.hash);
+        break;
+    }
+
+    let latest_block = node.provider.block_by_number_or_tag(BlockNumberOrTag::Latest)?.unwrap();
+    assert_eq!(latest_block.hash_slow(), hash);
 
     Ok(())
 }
+
 fn eth_payload_attributes() -> EthPayloadBuilderAttributes {
     let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
 
