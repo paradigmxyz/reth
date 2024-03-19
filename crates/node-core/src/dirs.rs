d8ae6ad83e266fcb3c8531b6aa0644702d2df73c
0x0 <coollightning123@gmail.com>
2024-03-19 01:18:48 -0700

    clippy..

--- crates/node-core/src/dirs.rs
+++ crates/node-core/src/dirs.rs
@@ -367,29 +367,29 @@ mod tests {
     #[test]
     fn test_maybe_data_dir_path() {
         let path = MaybePlatformPath::<DataDirPath>::default();
-        let path = path.unwrap_or_chain_default(Chain::mainnet(), DatadirArgs::default());
-        assert!(path.as_ref().ends_with("reth/mainnet"), "{:?}", path);
+        let path = path.unwrap_or_chain_default(Chain::mainnet());
+        assert!(path.as_ref().ends_with("reth/mainnet"), "{path:?}");
 
         let db_path = path.db_path();
         assert!(db_path.ends_with("reth/mainnet/db"), "{db_path:?}");
 
         let path = MaybePlatformPath::<DataDirPath>::from_str("my/path/to/datadir").unwrap();
-        let path = path.unwrap_or_chain_default(Chain::mainnet(), DatadirArgs::default());
-        assert!(path.as_ref().ends_with("my/path/to/datadir"), "{:?}", path);
+        let path = path.unwrap_or_chain_default(Chain::mainnet());
+        assert!(path.as_ref().ends_with("my/path/to/datadir"), "{path:?}");
     }
 
     #[test]
     fn test_maybe_testnet_datadir_path() {
         let path = MaybePlatformPath::<DataDirPath>::default();
-        let path = path.unwrap_or_chain_default(Chain::goerli(), DatadirArgs::default());
-        assert!(path.as_ref().ends_with("reth/goerli"), "{:?}", path);
+        let path = path.unwrap_or_chain_default(Chain::goerli());
+        assert!(path.as_ref().ends_with("reth/goerli"), "{path:?}");
 
         let path = MaybePlatformPath::<DataDirPath>::default();
-        let path = path.unwrap_or_chain_default(Chain::holesky(), DatadirArgs::default());
-        assert!(path.as_ref().ends_with("reth/holesky"), "{:?}", path);
+        let path = path.unwrap_or_chain_default(Chain::holesky());
+        assert!(path.as_ref().ends_with("reth/holesky"), "{path:?}");
 
         let path = MaybePlatformPath::<DataDirPath>::default();
-        let path = path.unwrap_or_chain_default(Chain::sepolia(), DatadirArgs::default());
-        assert!(path.as_ref().ends_with("reth/sepolia"), "{:?}", path);
+        let path = path.unwrap_or_chain_default(Chain::sepolia());
+        assert!(path.as_ref().ends_with("reth/sepolia"), "{path:?}");
     }
 }
