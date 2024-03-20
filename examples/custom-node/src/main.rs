004305c5260c8792e262ee24498d89687b208022
Justin Traglia <95511699+jtraglia@users.noreply.github.com>
2024-03-20 13:26:13 -0500

    fix: do not rlp encode extradata (#7256)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- examples/custom-node/src/main.rs
+++ examples/custom-node/src/main.rs
@@ -241,7 +241,7 @@ where
             .interval(conf.interval())
             .deadline(conf.deadline())
             .max_payload_tasks(conf.max_payload_tasks())
-            .extradata(conf.extradata_rlp_bytes())
+            .extradata(conf.extradata_bytes())
             .max_gas_limit(conf.max_gas_limit());
 
         let payload_generator = BasicPayloadJobGenerator::with_builder(
