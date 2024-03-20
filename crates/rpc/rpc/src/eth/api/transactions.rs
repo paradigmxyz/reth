9144576aef9e3467968d8a39130934982b7f3ede
Matthias Seitz <matthias.seitz@outlook.de>
2024-03-20 15:20:27 +0100

    rm unrelated

--- crates/rpc/rpc/src/eth/api/transactions.rs
+++ crates/rpc/rpc/src/eth/api/transactions.rs
@@ -1054,10 +1054,8 @@ where
         R: Send + 'static,
     {
         let ((cfg, block_env, _), block) =
-            futures::try_join!(self.evm_env_at(block_id), self.block_with_senders(block_id))
-                .unwrap();
+            futures::try_join!(self.evm_env_at(block_id), self.block_with_senders(block_id))?;
 
-        println!("here");
         let Some(block) = block else { return Ok(None) };
 
         // replay all transactions of the block
