9c6447c8f58dceea529fa4009ed80eb8cadf5d01
Arsenii Kulikov <klkvrr@gmail.com>
2024-03-21 20:53:47 +0400

    fix test

--- crates/transaction-pool/src/test_utils/gen.rs
+++ crates/transaction-pool/src/test_utils/gen.rs
@@ -93,7 +93,8 @@ impl<R: Rng> TransactionGenerator<R> {
 
     /// Creates a new transaction with a random signer
     pub fn gen_eip4844(&mut self) -> TransactionSigned {
-        self.transaction().into_eip4844()
+        // `to` is required for EIP-4844
+        self.transaction().to(Default::default()).into_eip4844()
     }
 
     /// Generates and returns a pooled EIP-1559 transaction with a random signer.
