c7db59312a5d76029d2f356e15bded96a167ddde
jn <nxqd.inbox+rhacker@gmail.com>
2024-03-22 02:16:03 +0700

    fix: DisconnectReason::decode should return error on zero length list
    Closes #7279
    
    Currently the DisconnectReason::decode returns DisconnectReason::DisconnectRequested for list input with zero payload_length which shouldn't happen.

--- crates/net/eth-wire/src/disconnect.rs
+++ crates/net/eth-wire/src/disconnect.rs
@@ -129,7 +129,7 @@ impl Decodable for DisconnectReason {
             // this should be a list, so decode the list header. this should advance the buffer so
             // buf[0] is the first (and only) element of the list.
             let header = Header::decode(buf)?;
-            if !header.list {
+            if !header.list || header.payload_length != 1 {
                 return Err(RlpError::UnexpectedString)
             }
         }
@@ -230,6 +230,12 @@ mod tests {
     #[test]
     fn test_reason_too_long() {
         assert!(DisconnectReason::decode(&mut &[0u8; 3][..]).is_err())
+    }
+
+    #[test]
+    fn test_reason_zero_length_list() {
+        let list_with_zero_length = hex::decode("c000").unwrap();
+        assert!(DisconnectReason::decode(&mut &list_with_zero_length[..]).is_err())
     }
 
     #[test]
