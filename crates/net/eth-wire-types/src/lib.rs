4026925180a090909601db987fe831fea778e31e
hawkinsjgh@gmail.com <jack.hawkins@mycelium.ventures>
2024-03-28 13:16:09 +0000

    cleanup:

--- crates/net/eth-wire-types/src/lib.rs
+++ crates/net/eth-wire-types/src/lib.rs
@@ -1,4 +1,12 @@
-//! Types for the eth wire protocol.
+//! types for the eth wire protocol.
+
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
+    html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
+    issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
+)]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 mod status;
 pub use status::Status;
