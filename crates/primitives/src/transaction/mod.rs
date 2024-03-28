36cbac50e9f5038acd4cc12666c596c5e01006ca
joshieDo <ranriver@protonmail.com>
2024-03-28 22:38:29 +0100

    encode TxDeposit on encode_and_recover_unchecked

--- crates/primitives/src/transaction/mod.rs
+++ crates/primitives/src/transaction/mod.rs
@@ -876,15 +876,15 @@ impl TransactionSignedNoHash {
     /// Returns `None` if the transaction's signature is invalid, see also
     /// [Signature::recover_signer_unchecked].
     pub fn encode_and_recover_unchecked(&self, buffer: &mut Vec<u8>) -> Option<Address> {
+        buffer.clear();
+        self.transaction.encode_without_signature(buffer);
+
         // Optimism's Deposit transaction does not have a signature. Directly return the
         // `from` address.
         #[cfg(feature = "optimism")]
         if let Transaction::Deposit(TxDeposit { from, .. }) = self.transaction {
             return Some(from)
         }
-
-        buffer.clear();
-        self.transaction.encode_without_signature(buffer);
 
         self.signature.recover_signer_unchecked(keccak256(buffer))
     }
