7c67112412930a370faabb7d4e4042556a794c68
allnil <nil.medvedev17@gmail.com>
2024-03-30 15:07:22 +0000

    chore: refactor and handle optimism error

--- crates/revm/src/optimism/processor.rs
+++ crates/revm/src/optimism/processor.rs
@@ -2,6 +2,7 @@ use crate::processor::{compare_receipts_root_and_logs_bloom, EVMProcessor};
 use reth_interfaces::executor::{
     BlockExecutionError, BlockValidationError, OptimismBlockExecutionError,
 };
+
 use reth_node_api::ConfigureEvm;
 use reth_primitives::{
     proofs::calculate_receipt_root_optimism, revm_primitives::ResultAndState, BlockWithSenders,
@@ -11,6 +12,7 @@ use reth_provider::{BlockExecutor, BundleStateWithReceipts};
 use revm::DatabaseCommit;
 use std::time::Instant;
 use tracing::{debug, trace};
+use reth_interfaces::executor::OptimismBlockExecutionError::AccountLoadFailed;
 
 /// Verify the calculated receipts root against the expected receipts root.
 pub fn verify_receipt_optimism<'a>(
@@ -145,7 +147,9 @@ where
                         .map(|acc| acc.account_info().unwrap_or_default())
                 })
                 .transpose()
-                .map_err(|e| BlockExectionError::BlockValidationError)?;
+                .map_err(|_| BlockExecutionError::OptimismBlockExecution(
+                    AccountLoadFailed(*sender)
+                ))?;
 
             // Execute transaction.
             let ResultAndState { result, state } = self.transact(transaction, *sender)?;
