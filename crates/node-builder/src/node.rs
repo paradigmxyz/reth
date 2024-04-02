aa99219149b08c975ddbf926a5cf4a3e30e91164
Matthias Seitz <matthias.seitz@outlook.de>
2024-04-02 16:57:05 +0200

    chore: move FullNodeTypes to node-api (#7425)

--- crates/node-builder/src/node.rs
+++ crates/node-builder/src/node.rs
@@ -1,11 +1,8 @@
 use crate::{
     components::{ComponentsBuilder, FullNodeComponents},
     rpc::{RethRpcServerHandles, RpcRegistry},
-    FullProvider,
 };
-use reth_db::database::Database;
 use reth_network::NetworkHandle;
-pub use reth_node_api::NodeTypes;
 use reth_node_core::{
     dirs::{ChainPath, DataDirPath},
     node_config::NodeConfig,
@@ -18,7 +15,10 @@ use reth_payload_builder::PayloadBuilderHandle;
 use reth_primitives::ChainSpec;
 use reth_provider::ChainSpecProvider;
 use reth_tasks::TaskExecutor;
-use std::{marker::PhantomData, sync::Arc};
+use std::sync::Arc;
+
+// re-export the node api types
+pub use reth_node_api::{FullNodeTypes, NodeTypes};
 
 /// A [Node] is a [NodeTypes] that comes with preconfigured components.
 ///
@@ -35,54 +35,6 @@ pub trait Node<N>: NodeTypes + Clone {
     fn components(
         self,
     ) -> ComponentsBuilder<N, Self::PoolBuilder, Self::PayloadBuilder, Self::NetworkBuilder>;
-}
-
-/// A helper type that is downstream of the node types and adds stateful components to the node.
-pub trait FullNodeTypes: NodeTypes + 'static {
-    /// Underlying database type.
-    type DB: Database + Clone + 'static;
-    /// The provider type used to interact with the node.
-    type Provider: FullProvider<Self::DB>;
-}
-
-/// An adapter type that adds the builtin provider type to the user configured node types.
-#[derive(Debug)]
-pub struct FullNodeTypesAdapter<Types, DB, Provider> {
-    pub(crate) types: Types,
-    _db: PhantomData<DB>,
-    _provider: PhantomData<Provider>,
-}
-
-impl<Types, DB, Provider> FullNodeTypesAdapter<Types, DB, Provider> {
-    /// Create a new adapter from the given node types.
-    pub fn new(types: Types) -> Self {
-        Self { types, _db: Default::default(), _provider: Default::default() }
-    }
-}
-
-impl<Types, DB, Provider> NodeTypes for FullNodeTypesAdapter<Types, DB, Provider>
-where
-    Types: NodeTypes,
-    DB: Send + Sync + 'static,
-    Provider: Send + Sync + 'static,
-{
-    type Primitives = Types::Primitives;
-    type Engine = Types::Engine;
-    type Evm = Types::Evm;
-
-    fn evm_config(&self) -> Self::Evm {
-        self.types.evm_config()
-    }
-}
-
-impl<Types, DB, Provider> FullNodeTypes for FullNodeTypesAdapter<Types, DB, Provider>
-where
-    Types: NodeTypes,
-    Provider: FullProvider<DB>,
-    DB: Database + Clone + 'static,
-{
-    type DB = DB;
-    type Provider = Provider;
 }
 
 /// The launched node with all components including RPC handlers.
