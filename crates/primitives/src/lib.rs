ee5207c58afa6df29b783ac06248e3e6104c453b
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-04-03 14:27:46 +0200

    feat(discv5): config via cli (#7394)

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -71,7 +71,7 @@ pub use header::{Header, HeaderValidationError, HeadersDirection, SealedHeader};
 pub use integer_list::IntegerList;
 pub use log::{logs_bloom, Log};
 pub use net::{
-    goerli_nodes, holesky_nodes, mainnet_nodes, parse_nodes, pk_to_id, sepolia_nodes, NodeRecord,
+    goerli_nodes, holesky_nodes, mainnet_nodes, parse_nodes, sepolia_nodes, NodeRecord,
     NodeRecordParseError, GOERLI_BOOTNODES, HOLESKY_BOOTNODES, MAINNET_BOOTNODES,
     SEPOLIA_BOOTNODES,
 };
