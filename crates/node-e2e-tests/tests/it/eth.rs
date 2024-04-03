dbc4f08f2c2b7e9ca0a0efa4dec6a6aedfce8c60
Matthias Seitz <matthias.seitz@outlook.de>
2024-04-03 19:23:28 +0200

    fix: race condition in test setup

--- crates/node-e2e-tests/tests/it/eth.rs
+++ crates/node-e2e-tests/tests/it/eth.rs
@@ -18,6 +18,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 #[tokio::test]
 async fn can_run_eth_node() -> eyre::Result<()> {
+    reth_tracing::init_test_tracing();
     let tasks = TaskManager::current();
     let test_suite = TestSuite::new();
 
@@ -35,6 +36,7 @@ async fn can_run_eth_node() -> eyre::Result<()> {
     // setup engine api events and payload service events
     let mut notifications = node.provider.canonical_state_stream();
     let payload_events = node.payload_builder.subscribe().await?;
+    let mut payload_event_stream = payload_events.into_stream();
 
     // push tx into pool via RPC server
     let eth_api = node.rpc_registry.eth_api();
@@ -48,11 +50,6 @@ async fn can_run_eth_node() -> eyre::Result<()> {
     // resolve best payload via engine api
     let client = node.engine_http_client();
 
-    // ensure we can get the payload over the engine api
-    let _payload = client.get_payload_v3(payload_id).await?;
-
-    let mut payload_event_stream = payload_events.into_stream();
-
     // first event is the payload attributes
     let first_event = payload_event_stream.next().await.unwrap()?;
     if let reth::payload::Events::Attributes(attr) = first_event {
@@ -60,6 +57,19 @@ async fn can_run_eth_node() -> eyre::Result<()> {
     } else {
         panic!("Expect first event as payload attributes.")
     }
+
+    // wait until an actual payload is built before we resolve it via engine api
+    loop {
+        let payload = node.payload_builder.best_payload(payload_id).await.unwrap().unwrap();
+        if payload.block().body.is_empty() {
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+            continue;
+        }
+        break;
+    }
+
+    // trigger resolve payload via engine api
+    let _ = client.get_payload_v3(payload_id).await?;
 
     // second event is built payload
     let second_event = payload_event_stream.next().await.unwrap()?;
@@ -93,8 +103,8 @@ async fn can_run_eth_node() -> eyre::Result<()> {
         // get head block from notifications stream and verify the tx has been pushed to the pool
         // is actually present in the canonical block
         let head = notifications.next().await.unwrap();
-        let tx = head.tip().transactions().next().unwrap();
-        assert_eq!(tx.hash(), transfer_tx.hash);
+        let tx = head.tip().transactions().next();
+        assert_eq!(tx.unwrap().hash(), transfer_tx.hash);
 
         // make sure the block hash we submitted via FCU engine api is the new latest block using an
         // RPC call
