f57d9c4cb1c5f0ba5974158ef8138d7cbde50cb9
Oliver Nordbjerg <hi@notbjerg.me>
2024-04-08 04:30:57 +0200

    feat: add event/notif channels to context

--- crates/exex/src/context.rs
+++ crates/exex/src/context.rs
@@ -4,10 +4,14 @@ use reth_node_core::{
     node_config::NodeConfig,
 };
 use reth_primitives::Head;
+use reth_provider::CanonStateNotification;
 use reth_tasks::TaskExecutor;
+use tokio::sync::mpsc::{Receiver, Sender};
+
+use crate::ExExEvent;
 
 /// Captures the context that an ExEx has access to.
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub struct ExExContext<Node: FullNodeTypes> {
     /// The current head of the blockchain at launch.
     pub head: Head,
@@ -21,5 +25,13 @@ pub struct ExExContext<Node: FullNodeTypes> {
     pub config: NodeConfig,
     /// The loaded node config
     pub reth_config: reth_config::Config,
+    /// Channel used to send [`ExExEvent`]s to the rest of the node.
+    ///
+    /// # Important
+    ///
+    /// The node expects at least one `FinishedHeight` event in order to prune blocks.
+    pub events: Sender<ExExEvent>,
+    /// Channel to receive [`CanonStateNotification`]s on state transitions.
+    pub notifications: Receiver<CanonStateNotification>,
     // TODO(alexey): add pool, payload builder, anything else?
 }
