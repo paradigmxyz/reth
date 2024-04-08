48533f82d22252dfb7d8655a6e2db5a10e841e5e
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-04-08 19:16:17 +0200

    chore: fix some warnings

--- crates/primitives/src/revm/config.rs
+++ crates/primitives/src/revm/config.rs
@@ -142,7 +142,7 @@ mod tests {
         {
             #[inline(always)]
             fn op_cs(f: impl FnOnce(ChainSpecBuilder) -> ChainSpecBuilder) -> ChainSpec {
-                let cs = ChainSpecBuilder::mainnet().chain(alloy_chains::Chain::from_id(10));
+                let cs = ChainSpecBuilder::mainnet().chain(crate::Chain::from_id(10));
                 f(cs).build()
             }
 
