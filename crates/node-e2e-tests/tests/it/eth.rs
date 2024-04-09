652be590e4a892bbe8a098b02ac5aff2708c7223
allnil <nil.medvedev17@gmail.com>
2024-04-09 17:19:38 +0100

    chore: cut test

--- crates/node-e2e-tests/tests/it/eth.rs
+++ crates/node-e2e-tests/tests/it/eth.rs
@@ -160,50 +160,6 @@ async fn can_run_eth_node_with_auth_engina_api_over_ipc() -> eyre::Result<()> {
     } else {
         panic!("Expect first event as payload attributes.")
     }
-
-    // second event is built payload
-    let second_event = payload_event_stream.next().await.unwrap()?;
-    if let reth::payload::Events::BuiltPayload(payload) = second_event {
-        // setup payload for submission
-        let envelope_v3 = ExecutionPayloadEnvelopeV3::from(payload);
-        let payload_v3 = envelope_v3.execution_payload;
-
-        // submit payload to engine api
-        let submission = client
-            .new_payload_v3(payload_v3, vec![], eth_attr.parent_beacon_block_root.unwrap())
-            .await?;
-        assert!(submission.is_valid());
-
-        // get latest valid hash from blockchain tree
-        let hash = submission.latest_valid_hash.unwrap();
-
-        // trigger forkchoice update via engine api to commit the block to the blockchain
-        let fcu = client
-            .fork_choice_updated_v2(
-                ForkchoiceState {
-                    head_block_hash: hash,
-                    safe_block_hash: hash,
-                    finalized_block_hash: hash,
-                },
-                None,
-            )
-            .await?;
-        assert!(fcu.is_valid());
-
-        // get head block from notifications stream and verify the tx has been pushed to the pool
-        // is actually present in the canonical block
-        let head = notifications.next().await.unwrap();
-        let tx = head.tip().transactions().next().unwrap();
-        assert_eq!(tx.hash(), transfer_tx.hash);
-
-        // make sure the block hash we submitted via FCU engine api is the new latest block using an
-        // RPC call
-        let latest_block = node.provider.block_by_number_or_tag(BlockNumberOrTag::Latest)?.unwrap();
-        assert_eq!(latest_block.hash_slow(), hash);
-    } else {
-        panic!("Expect a built payload event.");
-    }
-
     Ok(())
 }
 
