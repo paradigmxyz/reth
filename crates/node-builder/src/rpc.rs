fc049afb99cb517c1a61b04d8c9536cf39968a03
allnil <nil.medvedev17@gmail.com>
2024-04-09 08:22:54 +0100

    chore: fix logs

--- crates/node-builder/src/rpc.rs
+++ crates/node-builder/src/rpc.rs
@@ -300,8 +300,11 @@ where
 
     let launch_auth = auth_module.clone().start_server(auth_config).map_ok(|handle| {
         let addr = handle.local_addr();
-        let ipc_endpoint = handle.ipc_enpodint();
-        info!(target: "reth::cli", url=%addr, ipc_endpoint=%ipc_endpoint,"RPC auth server started");
+        if let Some(ipc_endpoint) = handle.ipc_enpodint() {
+            info!(target: "reth::cli", url=%addr, ipc_endpoint=%ipc_endpoint,"RPC auth server started");
+        } else {
+            info!(target: "reth::cli", url=%addr, "RPC auth server started");
+        }
         handle
     });
 
