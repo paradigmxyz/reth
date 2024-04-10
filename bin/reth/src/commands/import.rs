cce78f7492fc1f1b48361a339036411f4824de97
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-04-10 13:45:55 +0200

    Disable execution stage

--- bin/reth/src/commands/import.rs
+++ bin/reth/src/commands/import.rs
@@ -70,6 +70,10 @@ pub struct ImportCommand {
     /// remaining stages are executed.
     #[arg(value_name = "IMPORT_PATH", verbatim_doc_comment)]
     path: PathBuf,
+
+    /// Disables execution stage.
+    #[arg(value_name = "DISABLE_EXEC_STAGE", verbatim_doc_comment)]
+    disable_execution: bool,
 }
 
 impl ImportCommand {
@@ -118,6 +122,7 @@ impl ImportCommand {
                     provider_factory.static_file_provider(),
                     PruneModes::default(),
                 ),
+                self.disable_execution,
             )
             .await?;
 
@@ -154,6 +159,7 @@ impl ImportCommand {
         consensus: &Arc<C>,
         file_client: Arc<FileClient>,
         static_file_producer: StaticFileProducer<DB>,
+        disable_execution: bool,
     ) -> eyre::Result<(Pipeline<DB>, impl Stream<Item = NodeEvent>)>
     where
         DB: Database + Clone + Unpin + 'static,
@@ -177,40 +183,43 @@ impl ImportCommand {
 
         let max_block = file_client.max_block().unwrap_or(0);
 
+        let mut stages = DefaultStages::new(
+            provider_factory.clone(),
+            HeaderSyncMode::Tip(tip_rx),
+            consensus.clone(),
+            header_downloader,
+            body_downloader,
+            factory.clone(),
+            config.stages.etl,
+        )
+        .set(SenderRecoveryStage {
+            commit_threshold: config.stages.sender_recovery.commit_threshold,
+        });
+
+        if !disable_execution {
+            stages = stages.set(ExecutionStage::new(
+                factory,
+                ExecutionStageThresholds {
+                    max_blocks: config.stages.execution.max_blocks,
+                    max_changes: config.stages.execution.max_changes,
+                    max_cumulative_gas: config.stages.execution.max_cumulative_gas,
+                    max_duration: config.stages.execution.max_duration,
+                },
+                config
+                    .stages
+                    .merkle
+                    .clean_threshold
+                    .max(config.stages.account_hashing.clean_threshold)
+                    .max(config.stages.storage_hashing.clean_threshold),
+                config.prune.map(|prune| prune.segments).unwrap_or_default(),
+            ));
+        }
+
         let mut pipeline = Pipeline::builder()
             .with_tip_sender(tip_tx)
             // we want to sync all blocks the file client provides or 0 if empty
             .with_max_block(max_block)
-            .add_stages(
-                DefaultStages::new(
-                    provider_factory.clone(),
-                    HeaderSyncMode::Tip(tip_rx),
-                    consensus.clone(),
-                    header_downloader,
-                    body_downloader,
-                    factory.clone(),
-                    config.stages.etl,
-                )
-                .set(SenderRecoveryStage {
-                    commit_threshold: config.stages.sender_recovery.commit_threshold,
-                })
-                .set(ExecutionStage::new(
-                    factory,
-                    ExecutionStageThresholds {
-                        max_blocks: config.stages.execution.max_blocks,
-                        max_changes: config.stages.execution.max_changes,
-                        max_cumulative_gas: config.stages.execution.max_cumulative_gas,
-                        max_duration: config.stages.execution.max_duration,
-                    },
-                    config
-                        .stages
-                        .merkle
-                        .clean_threshold
-                        .max(config.stages.account_hashing.clean_threshold)
-                        .max(config.stages.storage_hashing.clean_threshold),
-                    config.prune.map(|prune| prune.segments).unwrap_or_default(),
-                )),
-            )
+            .add_stages(stages)
             .build(provider_factory, static_file_producer);
 
         let events = pipeline.events().map(Into::into);
