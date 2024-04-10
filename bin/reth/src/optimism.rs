458ee1a879230596cb0fb3e34b3b236a43233507
joshieDo <ranriver@protonmail.com>
2024-04-10 18:03:07 +0200

    dont set finalized on op-reth start

--- bin/reth/src/optimism.rs
+++ bin/reth/src/optimism.rs
@@ -42,28 +42,6 @@ fn main() {
             .launch()
             .await?;
 
-        // If `enable_genesis_walkback` is set to true, the rollup client will need to
-        // perform the derivation pipeline from genesis, validating the data dir.
-        // When set to false, set the finalized, safe, and unsafe head block hashes
-        // on the rollup client using a fork choice update. This prevents the rollup
-        // client from performing the derivation pipeline from genesis, and instead
-        // starts syncing from the current tip in the DB.
-        if node.chain_spec().is_optimism() && !rollup_args.enable_genesis_walkback {
-            let client = node.rpc_server_handles.auth.http_client();
-            if let Ok(Some(head)) = node.provider.latest_header() {
-                reth_rpc_api::EngineApiClient::<OptimismEngineTypes>::fork_choice_updated_v2(
-                    &client,
-                    reth_rpc_types::engine::ForkchoiceState {
-                        head_block_hash: head.hash(),
-                        safe_block_hash: head.hash(),
-                        finalized_block_hash: head.hash(),
-                    },
-                    None,
-                )
-                .await?;
-            }
-        }
-
         node_exit_future.await
     }) {
         eprintln!("Error: {err:?}");
