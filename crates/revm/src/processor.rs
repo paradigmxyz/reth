5e8fa0d4c8793fde2776c8e8cc78a1c5d25ab5d6
Matthias Seitz <matthias.seitz@outlook.de>
2024-04-11 17:08:02 +0200

    chore: make function private

--- crates/revm/src/processor.rs
+++ crates/revm/src/processor.rs
@@ -165,7 +165,7 @@ where
     ///
     /// If cancun is not activated or the block is the genesis block, then this is a no-op, and no
     /// state changes are made.
-    pub fn apply_beacon_root_contract_call(
+    fn apply_beacon_root_contract_call(
         &mut self,
         block: &Block,
     ) -> Result<(), BlockExecutionError> {
