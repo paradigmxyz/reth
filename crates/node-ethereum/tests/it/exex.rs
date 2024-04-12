2dcc01210c2002a5bf85f91a09721a45b6c20bb3
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-04-12 17:34:18 +0100

    feat(exex): add `Pool` to `ExExContext` (#7573)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/node-ethereum/tests/it/exex.rs
+++ crates/node-ethereum/tests/it/exex.rs
@@ -1,7 +1,8 @@
 use futures::future;
 use reth_db::test_utils::create_test_rw_db;
 use reth_exex::ExExContext;
-use reth_node_builder::{FullNodeTypes, NodeBuilder, NodeConfig};
+use reth_node_api::FullNodeComponents;
+use reth_node_builder::{NodeBuilder, NodeConfig};
 use reth_node_ethereum::EthereumNode;
 use std::{
     future::Future,
@@ -9,11 +10,14 @@ use std::{
     task::{Context, Poll},
 };
 
-struct DummyExEx<Node: FullNodeTypes> {
+struct DummyExEx<Node: FullNodeComponents> {
     _ctx: ExExContext<Node>,
 }
 
-impl<Node: FullNodeTypes> Future for DummyExEx<Node> {
+impl<Node> Future for DummyExEx<Node>
+where
+    Node: FullNodeComponents,
+{
     type Output = eyre::Result<()>;
 
     fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
