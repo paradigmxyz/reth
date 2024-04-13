3fa448d5f684b231a7698f5a0853c09ebf7316f6
0xqd <nxqd.inbox+rhacker@gmail.com>
2024-04-14 01:24:13 +0700

    dev(refactor): clean Flags impl

--- crates/storage/codecs/src/alloy/withdrawal.rs
+++ crates/storage/codecs/src/alloy/withdrawal.rs
@@ -15,7 +15,6 @@ impl Compact for Withdrawal {
         B: bytes::BufMut + AsMut<[u8]>,
     {
         let mut flags = WithdrawalFlags::default();
-        let mut total_length = 0;
         let mut buffer = bytes::BytesMut::new();
         let index_len = self.index.to_compact(&mut buffer);
         flags.set_index_len(index_len as u8);
@@ -25,7 +24,7 @@ impl Compact for Withdrawal {
         let amount_len = self.amount.to_compact(&mut buffer);
         flags.set_amount_len(amount_len as u8);
         let flags = flags.into_bytes();
-        total_length += flags.len() + buffer.len();
+        let total_length = flags.len() + buffer.len();
         buf.put_slice(&flags);
         buf.put(buffer);
         total_length
@@ -75,104 +74,42 @@ impl WithdrawalFlags {
     /// Returns the value of index_len.
     #[inline]
     pub(crate) fn index_len(&self) -> <B4 as Specifier>::InOut {
-        self.index_len_or_err()
-            .expect("value contains invalid bit pattern for field WithdrawalFlags.index_len")
-    }
-
-    /// Returns the value of index_len.
-    #[inline]
-    pub(crate) fn index_len_or_err(
-        &self,
-    ) -> Result<<B4 as Specifier>::InOut, error::InvalidBitPattern<<B4 as Specifier>::Bytes>> {
-        let __bf_read: <B4 as Specifier>::Bytes =
-            { modular_bitfield::private::read_specifier::<B4>(&self.bytes[..], 0usize) };
+        let __bf_read = modular_bitfield::private::read_specifier::<B4>(&self.bytes[..], 0usize);
         <B4 as Specifier>::from_bytes(__bf_read)
+            .expect("value contains invalid bit pattern for field WithdrawalFlags.index_len")
     }
 
     /// Sets the value of index_len to the given value.
     #[inline]
     pub(crate) fn set_index_len(&mut self, new_val: <B4 as Specifier>::InOut) {
-        self.set_index_len_checked(new_val)
-            .expect("value out of bounds for field WithdrawalFlags.index_len")
-    }
-
-    /// Sets the value of index_len to the given value.
-    #[inline]
-    pub(crate) fn set_index_len_checked(
-        &mut self,
-        new_val: <B4 as Specifier>::InOut,
-    ) -> Result<(), error::OutOfBounds> {
-        let __bf_base_bits: usize = 8usize * core::mem::size_of::<<B4 as Specifier>::Bytes>();
-        let __bf_max_value: <B4 as Specifier>::Bytes =
-            { !0 >> (__bf_base_bits - <B4 as Specifier>::BITS) };
-        let __bf_spec_bits: usize = <B4 as Specifier>::BITS;
-        let __bf_raw_val: <B4 as Specifier>::Bytes = { <B4 as Specifier>::into_bytes(new_val) }?;
-        if !(__bf_base_bits == __bf_spec_bits || __bf_raw_val <= __bf_max_value) {
-            return Err(error::OutOfBounds);
-        }
-        write_specifier::<B4>(&mut self.bytes[..], 0usize, __bf_raw_val);
-        Ok(())
+        self.set_field(0, new_val)
+            .expect("value out of bounds for field WithdrawalFlags.amount_len")
     }
 
     /// Returns the value of validator_index_len.
     #[inline]
     pub(crate) fn validator_index_len(&self) -> <B4 as Specifier>::InOut {
-        self.validator_index_len_or_err().expect(
-            "value contains invalid bit pattern for field WithdrawalFlags.validator_index_len",
-        )
-    }
-
-    /// Returns the value of validator_index_len.
-    #[inline]
-    pub(crate) fn validator_index_len_or_err(
-        &self,
-    ) -> Result<<B4 as Specifier>::InOut, error::InvalidBitPattern<<B4 as Specifier>::Bytes>> {
         let __bf_read: <B4 as Specifier>::Bytes = {
             modular_bitfield::private::read_specifier::<B4>(
                 &self.bytes[..],
                 <B4 as Specifier>::BITS,
             )
         };
-        <B4 as Specifier>::from_bytes(__bf_read)
+        <B4 as Specifier>::from_bytes(__bf_read).expect(
+            "value contains invalid bit pattern for field WithdrawalFlags.validator_index_len",
+        )
     }
 
     /// Sets the value of validator_index_len to the given value.
     #[inline]
     pub(crate) fn set_validator_index_len(&mut self, new_val: <B4 as Specifier>::InOut) {
-        self.set_validator_index_len_checked(new_val)
-            .expect("value out of bounds for field WithdrawalFlags.validator_index_len")
-    }
-
-    /// Sets the value of validator_index_len to the given value.
-    #[inline]
-    pub(crate) fn set_validator_index_len_checked(
-        &mut self,
-        new_val: <B4 as Specifier>::InOut,
-    ) -> Result<(), error::OutOfBounds> {
-        let __bf_base_bits: usize = 8usize * core::mem::size_of::<<B4 as Specifier>::Bytes>();
-        let __bf_max_value: <B4 as Specifier>::Bytes =
-            { !0 >> (__bf_base_bits - <B4 as Specifier>::BITS) };
-        let __bf_spec_bits: usize = <B4 as Specifier>::BITS;
-        let __bf_raw_val: <B4 as Specifier>::Bytes = { <B4 as Specifier>::into_bytes(new_val) }?;
-        if !(__bf_base_bits == __bf_spec_bits || __bf_raw_val <= __bf_max_value) {
-            return Err(error::OutOfBounds);
-        }
-        write_specifier::<B4>(&mut self.bytes[..], <B4 as Specifier>::BITS, __bf_raw_val);
-        Ok(())
+        self.set_field(1, new_val)
+            .expect("value out of bounds for field WithdrawalFlags.amount_len")
     }
 
     /// Returns the value of amount_len.
     #[inline]
     pub(crate) fn amount_len(&self) -> <B4 as Specifier>::InOut {
-        self.amount_len_or_err()
-            .expect("value contains invalid bit pattern for field WithdrawalFlags.amount_len")
-    }
-
-    /// Returns the value of amount_len.
-    #[inline]
-    pub(crate) fn amount_len_or_err(
-        &self,
-    ) -> Result<<B4 as Specifier>::InOut, error::InvalidBitPattern<<B4 as Specifier>::Bytes>> {
         let __bf_read: <B4 as Specifier>::Bytes = {
             modular_bitfield::private::read_specifier::<B4>(
                 &self.bytes[..],
@@ -180,34 +117,32 @@ impl WithdrawalFlags {
             )
         };
         <B4 as Specifier>::from_bytes(__bf_read)
+            .expect("value contains invalid bit pattern for field WithdrawalFlags.amount_len")
     }
 
     /// Sets the value of amount_len to the given value.
     #[inline]
     pub(crate) fn set_amount_len(&mut self, new_val: <B4 as Specifier>::InOut) {
-        self.set_amount_len_checked(new_val)
+        self.set_field(2, new_val)
             .expect("value out of bounds for field WithdrawalFlags.amount_len")
     }
 
-    /// Sets the value of amount_len to the given value.
     #[inline]
-    pub(crate) fn set_amount_len_checked(
+    fn set_field(
         &mut self,
+        spec_bits_multiplier: usize,
         new_val: <B4 as Specifier>::InOut,
     ) -> Result<(), error::OutOfBounds> {
-        let __bf_base_bits: usize = 8usize * core::mem::size_of::<<B4 as Specifier>::Bytes>();
-        let __bf_max_value: <B4 as Specifier>::Bytes =
-            { !0 >> (__bf_base_bits - <B4 as Specifier>::BITS) };
-        let __bf_spec_bits: usize = <B4 as Specifier>::BITS;
-        let __bf_raw_val: <B4 as Specifier>::Bytes = { <B4 as Specifier>::into_bytes(new_val) }?;
-        if !(__bf_base_bits == __bf_spec_bits || __bf_raw_val <= __bf_max_value) {
+        let base_bits: usize = 8 * core::mem::size_of::<<B4 as Specifier>::Bytes>();
+        let max_value: <B4 as Specifier>::Bytes = !0 >> (base_bits - <B4 as Specifier>::BITS);
+        let spec_bits: usize = <B4 as Specifier>::BITS;
+        let raw_val: <B4 as Specifier>::Bytes = <B4 as Specifier>::into_bytes(new_val)?;
+
+        if !(base_bits == spec_bits || raw_val <= max_value) {
             return Err(error::OutOfBounds);
         }
-        write_specifier::<B4>(
-            &mut self.bytes[..],
-            <B4 as Specifier>::BITS + <B4 as Specifier>::BITS,
-            __bf_raw_val,
-        );
+
+        write_specifier::<B4>(&mut self.bytes[..], spec_bits * spec_bits_multiplier, raw_val);
         Ok(())
     }
 }
