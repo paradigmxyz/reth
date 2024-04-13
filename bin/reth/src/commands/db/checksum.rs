d5622fe7a201c2db08e948feab4c2b3b62ceb8cc
Abner Zheng <abnerzheng@gmail.com>
2024-04-14 01:20:13 +0800

    feat: implement table range checksums for reth db checksum

--- bin/reth/src/commands/db/checksum.rs
+++ bin/reth/src/commands/db/checksum.rs
@@ -1,9 +1,12 @@
-use crate::utils::DbTool;
+use crate::{primitives::hex, utils::DbTool};
 use ahash::AHasher;
 use clap::Parser;
 use reth_db::{
-    cursor::DbCursorRO, database::Database, table::Table, transaction::DbTx, DatabaseEnv, RawKey,
-    RawTable, RawValue, TableViewer, Tables,
+    cursor::DbCursorRO,
+    database::Database,
+    table::{Decode, Table},
+    transaction::DbTx,
+    DatabaseEnv, RawKey, RawTable, RawValue, TableViewer, Tables,
 };
 use std::{
     hash::Hasher,
@@ -16,9 +19,13 @@ use tracing::{info, warn};
 pub struct Command {
     /// The table name
     table: Tables,
+
     /// start of range
+    #[arg(long, verbatim_doc_comment)]
     start: Option<String>,
+
     /// end of range
+    #[arg(long, verbatim_doc_comment)]
     end: Option<String>,
 }
 
@@ -41,10 +48,7 @@ impl<DB: Database> ChecksumViewer<'_, DB> {
         ChecksumViewer { tool, start: None, end: None }
     }
 
-    pub(crate) fn get_checksum<T: Table>(&self) -> Result<(u64, Duration), eyre::Report>
-    where
-        <T as Table>::Key: std::str::FromStr,
-    {
+    pub(crate) fn get_checksum<T: Table>(&self) -> Result<(u64, Duration), eyre::Report> {
         let provider =
             self.tool.provider_factory.provider()?.disable_long_read_transaction_safety();
         let tx = provider.tx_ref();
@@ -52,23 +56,39 @@ impl<DB: Database> ChecksumViewer<'_, DB> {
         let mut cursor = tx.cursor_read::<RawTable<T>>()?;
         let walker = match (self.start.as_deref(), self.end.as_deref()) {
             (Some(start), Some(end)) => {
-                let start_key =
-                    start.parse::<T::Key>().map_err(|_| eyre::eyre!("Invalid start key"))?;
-                let end_key = end.parse::<T::Key>().map_err(|_| eyre::eyre!("Invalid end key"))?;
+                info!("start={start} \n end={end}");
+                let start = hex::decode(start).map_err(|s| eyre::eyre!(s.to_string()))?;
+                let start_key = RawKey::<T::Key>::decode(start)
+                    .map_err(|_| eyre::eyre!("Invalid start key"))?;
 
-                RawKey::new(start_key)..=RawKey::new(end_key)
+                let end = hex::decode(end).map_err(|_| eyre::eyre!("Invalid end key"))?;
+                let end_key =
+                    RawKey::<T::Key>::decode(end).map_err(|_| eyre::eyre!("Invalid end key"))?;
+
                 cursor.walk_range(start_key..=end_key)?
             }
             (None, Some(end)) => {
+                info!("start=.. \n end={end}");
+                let end = hex::decode(end).map_err(|_| eyre::eyre!("Invalid end key"))?;
                 let end_key =
-                    end.parse::<RawKey<T::Key>>().map_err(|_| eyre::eyre!("Invalid end key"))?;
+                    RawKey::<T::Key>::decode(end).map_err(|_| eyre::eyre!("Invalid end key"))?;
+
                 cursor.walk_range(..=end_key)?
             }
-            (_, None) => cursor.walk_range(..)?,
+            (Some(start), None) => {
+                info!("start={start} \n end= ");
+                let start = hex::decode(start).map_err(|s| eyre::eyre!(s.to_string()))?;
+                let start_key =
+                    RawKey::<T::Key>::decode(start).map_err(|e| eyre::eyre!(e.to_string()))?;
+
+                cursor.walk_range(start_key..)?
+            }
+            (None, None) => cursor.walk_range(..)?,
         };
 
         let start_time = Instant::now();
         let mut hasher = AHasher::default();
+        let mut total = 0;
         for (index, entry) in walker.enumerate() {
             let (k, v): (RawKey<T::Key>, RawValue<T::Value>) = entry?;
 
@@ -78,7 +98,10 @@ impl<DB: Database> ChecksumViewer<'_, DB> {
 
             hasher.write(k.raw_key());
             hasher.write(v.raw_value());
+            total = index;
         }
+
+        info!("Hashed {total} entries.");
 
         let checksum = hasher.finish();
         let elapsed = start_time.elapsed();
@@ -90,10 +113,7 @@ impl<DB: Database> ChecksumViewer<'_, DB> {
 impl<DB: Database> TableViewer<()> for ChecksumViewer<'_, DB> {
     type Error = eyre::Report;
 
-    fn view<T: Table>(&self) -> Result<(), Self::Error>
-    where
-        <T as Table>::Key: std::str::FromStr,
-    {
+    fn view<T: Table>(&self) -> Result<(), Self::Error> {
         let (checksum, elapsed) = self.get_checksum::<T>()?;
         info!("Checksum for table `{}`: {:#x} (elapsed: {:?})", T::NAME, checksum, elapsed);
 
