c2af29d3ee4cf189e335e26a78375fc4e58f4061
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-04-15 13:46:34 +0200

    feat(op): chunked chain import (#7574)
    Co-authored-by: Atris <vacekj@outlook.com>

--- bin/reth/src/commands/import.rs
+++ bin/reth/src/commands/import.rs
@@ -15,7 +15,8 @@ use reth_beacon_consensus::BeaconConsensus;
 use reth_config::Config;
 use reth_db::{database::Database, init_db};
 use reth_downloaders::{
-    bodies::bodies::BodiesDownloaderBuilder, file_client::FileClient,
+    bodies::bodies::BodiesDownloaderBuilder,
+    file_client::{ChunkedFileReader, FileClient, DEFAULT_BYTE_LEN_CHUNK_CHAIN_FILE},
     headers::reverse_headers::ReverseHeadersDownloaderBuilder,
 };
 use reth_interfaces::{
@@ -76,6 +77,10 @@ pub struct ImportCommand {
     #[arg(long, verbatim_doc_comment, env = OP_RETH_MAINNET_BELOW_BEDROCK)]
     op_mainnet_below_bedrock: bool,
 
+    /// Chunk byte length.
+    #[arg(long, value_name = "CHUNK_LEN", verbatim_doc_comment)]
+    chunk_len: Option<u64>,
+
     #[command(flatten)]
     db: DatabaseArgs,
 
@@ -101,6 +106,10 @@ impl ImportCommand {
             debug!(target: "reth::cli", "Execution stage disabled");
         }
 
+        debug!(target: "reth::cli",
+            chunk_byte_len=self.chunk_len.unwrap_or(DEFAULT_BYTE_LEN_CHUNK_CHAIN_FILE), "Chunking chain import"
+        );
+
         // add network name to data dir
         let data_dir = self.datadir.unwrap_or_chain_default(self.chain.chain);
         let config_path = self.config.clone().unwrap_or_else(|| data_dir.config_path());
@@ -123,49 +132,55 @@ impl ImportCommand {
         let consensus = Arc::new(BeaconConsensus::new(self.chain.clone()));
         info!(target: "reth::cli", "Consensus engine initialized");
 
-        // create a new FileClient
-        info!(target: "reth::cli", "Importing chain file");
-        let file_client = Arc::new(FileClient::new(&self.path).await?);
+        // open file
+        let mut reader = ChunkedFileReader::new(&self.path, self.chunk_len).await?;
 
-        // override the tip
-        let tip = file_client.tip().expect("file client has no tip");
-        info!(target: "reth::cli", "Chain file read");
+        while let Some(file_client) = reader.next_chunk().await? {
+            // create a new FileClient from chunk read from file
+            info!(target: "reth::cli",
+                "Importing chain file chunk"
+            );
 
-        let (mut pipeline, events) = self
-            .build_import_pipeline(
-                config,
-                provider_factory.clone(),
-                &consensus,
-                file_client,
-                StaticFileProducer::new(
+            // override the tip
+            let tip = file_client.tip().expect("file client has no tip");
+            info!(target: "reth::cli", "Chain file chunk read");
+
+            let (mut pipeline, events) = self
+                .build_import_pipeline(
+                    &config,
                     provider_factory.clone(),
-                    provider_factory.static_file_provider(),
-                    PruneModes::default(),
-                ),
-                self.disable_execution,
-            )
-            .await?;
+                    &consensus,
+                    Arc::new(file_client),
+                    StaticFileProducer::new(
+                        provider_factory.clone(),
+                        provider_factory.static_file_provider(),
+                        PruneModes::default(),
+                    ),
+                    self.disable_execution,
+                )
+                .await?;
 
-        // override the tip
-        pipeline.set_tip(tip);
-        debug!(target: "reth::cli", ?tip, "Tip manually set");
+            // override the tip
+            pipeline.set_tip(tip);
+            debug!(target: "reth::cli", ?tip, "Tip manually set");
 
-        let provider = provider_factory.provider()?;
+            let provider = provider_factory.provider()?;
 
-        let latest_block_number =
-            provider.get_stage_checkpoint(StageId::Finish)?.map(|ch| ch.block_number);
-        tokio::spawn(reth_node_core::events::node::handle_events(
-            None,
-            latest_block_number,
-            events,
-            db.clone(),
-        ));
+            let latest_block_number =
+                provider.get_stage_checkpoint(StageId::Finish)?.map(|ch| ch.block_number);
+            tokio::spawn(reth_node_core::events::node::handle_events(
+                None,
+                latest_block_number,
+                events,
+                db.clone(),
+            ));
 
-        // Run pipeline
-        info!(target: "reth::cli", "Starting sync pipeline");
-        tokio::select! {
-            res = pipeline.run() => res?,
-            _ = tokio::signal::ctrl_c() => {},
+            // Run pipeline
+            info!(target: "reth::cli", "Starting sync pipeline");
+            tokio::select! {
+                res = pipeline.run() => res?,
+                _ = tokio::signal::ctrl_c() => {},
+            }
         }
 
         info!(target: "reth::cli", "Chain file imported");
@@ -174,7 +189,7 @@ impl ImportCommand {
 
     async fn build_import_pipeline<DB, C>(
         &self,
-        config: Config,
+        config: &Config,
         provider_factory: ProviderFactory<DB>,
         consensus: &Arc<C>,
         file_client: Arc<FileClient>,
@@ -220,7 +235,7 @@ impl ImportCommand {
                     header_downloader,
                     body_downloader,
                     factory.clone(),
-                    config.stages.etl,
+                    config.stages.etl.clone(),
                 )
                 .set(SenderRecoveryStage {
                     commit_threshold: config.stages.sender_recovery.commit_threshold,
@@ -239,7 +254,7 @@ impl ImportCommand {
                         .clean_threshold
                         .max(config.stages.account_hashing.clean_threshold)
                         .max(config.stages.storage_hashing.clean_threshold),
-                    config.prune.map(|prune| prune.segments).unwrap_or_default(),
+                    config.prune.clone().map(|prune| prune.segments).unwrap_or_default(),
                 ))
                 .disable_if(StageId::Execution, || disable_execution),
             )
