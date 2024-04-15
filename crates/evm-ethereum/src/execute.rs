337980625d5c33e0b7c13a09e1a5b4de1495ae4d
Matthias Seitz <matthias.seitz@outlook.de>
2024-04-15 18:31:09 +0200

    more scaffolding

--- crates/evm-ethereum/src/execute.rs
+++ crates/evm-ethereum/src/execute.rs
@@ -1,31 +1,39 @@
 //! Ethereum executor.
 
 use reth_evm::{
-    execute::{EthBlockInput, EthBlockOutput, Executor},
+    execute::{
+        BatchBlockOutput, BatchExecutor, EthBatchOutput, EthBlockExecutionInput, EthBlockOutput,
+        Executor,
+    },
     ConfigureEvm,
 };
-use reth_primitives::{BlockWithSenders, ChainSpec, Receipts};
+use reth_interfaces::executor::BlockExecutionError;
+use reth_primitives::{Address, BlockWithSenders, ChainSpec, PruneModes, Receipts, U256};
+use reth_revm::{stack::InspectorStack, State};
 use revm_primitives::db::{Database, DatabaseCommit};
 use std::sync::Arc;
-use reth_revm::stack::InspectorStack;
 
 /// A basic Ethereum block executor.
+///
+/// Expected usage:
+/// - Create a new instance of the executor.
+/// - Execute the block.
 #[derive(Debug)]
 pub struct EthBlockExecutor<Evm, DB> {
     /// The chainspec
     chain_spec: Arc<ChainSpec>,
     /// How to create an EVM.
-    evm: Evm,
+    evm_config: Evm,
+    /// The state to use for execution
+    state: State<DB>,
     /// Optional inspector stack for debugging
     inspector: Option<InspectorStack>,
-    /// The database to use for execution
-    db: DB,
 }
 
 impl<Evm, DB> EthBlockExecutor<Evm, DB> {
     /// Creates a new Ethereum block executor.
-    pub fn new(chain_spec: Arc<ChainSpec>, evm: Evm, db: DB) -> Self {
-        Self { chain_spec, evm, inspector: None, db }
+    pub fn new(chain_spec: Arc<ChainSpec>, evm: Evm, state: State<DB>) -> Self {
+        Self { chain_spec, evm_config: evm, state, inspector: None }
     }
 
     /// Sets the inspector stack for debugging.
@@ -35,17 +43,40 @@ impl<Evm, DB> EthBlockExecutor<Evm, DB> {
     }
 }
 
-impl<Evm, DB> EthBlockExecutor<Evm, DB> where Evm: ConfigureEvm {}
-
-impl<Evm, DB, Block> Executor for EthBlockExecutor<Evm, DB>
+impl<Evm, DB> EthBlockExecutor<Evm, DB>
 where
     Evm: ConfigureEvm,
     DB: Database + DatabaseCommit,
-    Block: EthBlockInput<BlockWithSenders>,
 {
-    type Input = Block;
+    /// Execute a single block and apply the state changes to the internal state.
+    pub(crate) fn execute_block(
+        &mut self,
+        block: &BlockWithSenders,
+        total_difficulty: U256,
+    ) -> Result<(Receipts, u64), BlockExecutionError> {
+        todo!()
+    }
+
+    /// Invoked before transactions are executed
+    fn pre_execution(&self) {
+        todo!()
+    }
+
+    /// Apply post execution state changes, including block rewards, withdrawals, and irregular DAO
+    /// hardfork state change.
+    fn post_execution(&self) {
+        todo!()
+    }
+}
+
+impl<Evm, DB> Executor for EthBlockExecutor<Evm, DB>
+where
+    Evm: ConfigureEvm,
+    DB: Database + DatabaseCommit,
+{
+    type Input<'a> = EthBlockExecutionInput<'a, BlockWithSenders>;
     type Output = EthBlockOutput<Receipts>;
-    type Error = ();
+    type Error = BlockExecutionError;
 
     /// Executes the block and commits the state changes.
     ///
@@ -54,8 +85,38 @@ where
     /// Returns an error if the block could not be executed.
     ///
     /// State changes are committed to the database.
-    fn execute(self, _block: Self::Input) -> Result<Self::Output, Self::Error> {
+    fn execute(mut self, input: Self::Input<'_>) -> Result<Self::Output, Self::Error> {
+        let EthBlockExecutionInput { block, total_difficulty } = input;
+        let (receipts, gas_used) = self.execute_block(block, total_difficulty)?;
+        Ok(EthBlockOutput { state: self.state.take_bundle(), receipts, gas_used })
+    }
+}
 
+pub struct EthBatchExecutor<Evm, DB> {
+    /// The
+    executor: EthBlockExecutor<Evm, DB>,
+    /// Pruning configuration.
+    ///
+    /// TODO: make everything related to pruning a separate type.
+    prune_mode: PruneModes,
+
+    stats: (),
+}
+
+impl<Evm, DB> BatchExecutor for EthBatchExecutor<Evm, DB>
+where
+    Evm: ConfigureEvm,
+    DB: Database + DatabaseCommit,
+{
+    type Input<'a> = EthBlockExecutionInput<'a, BlockWithSenders>;
+    type Output = EthBatchOutput<Receipts>;
+    type Error = BlockExecutionError;
+
+    fn execute_one(&mut self, input: Self::Input<'_>) -> Result<BatchBlockOutput, Self::Error> {
+        todo!()
+    }
+
+    fn finalize(self) -> Self::Output {
         todo!()
     }
 }
--- crates/evm/src/execute.rs
+++ crates/evm/src/execute.rs
@@ -1,11 +1,13 @@
 //! Traits for execution.
 
-use reth_primitives::U256;
+use reth_primitives::{BlockNumber, Receipts, U256};
+use revm::db::BundleState;
+use revm_primitives::db::{Database, DatabaseCommit};
 
 /// A general purpose executor trait that executes on an input and produces an output.
 pub trait Executor {
     /// The input type for the executor.
-    type Input;
+    type Input<'a>;
     /// The output type for the executor.
     type Output;
     /// The error type returned by the executor.
@@ -14,26 +16,138 @@ pub trait Executor {
     /// Consumes the type and executes the block.
     ///
     /// Returns the output of the block execution.
-    fn execute(self, block: Self::Input) -> Result<Self::Output, Self::Error>;
+    fn execute(self, input: Self::Input<'_>) -> Result<Self::Output, Self::Error>;
+}
+
+/// An executor that can execute multiple blocks in a row and keep track of the state over the
+/// entire batch.
+pub trait BatchExecutor {
+    /// Input
+    type Input<'a>;
+    /// The output type for the executor.
+    type Output;
+    type Error;
+
+    /// Executes the next block in the batch and update the state internally.
+    fn execute_one(&mut self, input: Self::Input<'_>) -> Result<BatchBlockOutput, Self::Error>;
+
+    /// Finishes the batch and return the final state.
+    fn finalize(self) -> Self::Output;
+}
+
+pub struct BatchBlockOutput {
+    /// The size hint of the batch's tracked state.
+    pub size_hint: Option<usize>,
 }
 
 /// The output of an ethereum block.
 ///
 /// Contains the receipts of the transactions in the block and the total gas used.
+#[derive(Debug)]
 pub struct EthBlockOutput<T> {
+    /// The changed state of the block after execution.
+    pub state: BundleState,
     /// All the receipts of the transactions in the block.
     pub receipts: T,
     /// The total gas used by the block.
     pub gas_used: u64,
 }
 
-/// A helper trait for ethereum block inputs that consists of a block and the total difficulty.
-pub trait EthBlockInput<Block> {
-    /// Returns the total difficulty of the block.
-    fn total_difficulty(&self) -> U256;
-
-    /// Returns the block to execute.
-    fn block(&self) -> &Block;
+/// The final output of a batch of blocks.
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
+pub struct EthBatchOutput<T> {
+    /// Bundle state with reverts.
+    pub bundle: BundleState,
+    /// The collection of receipts.
+    /// Outer vector stores receipts for each block sequentially.
+    /// The inner vector stores receipts ordered by transaction number.
+    ///
+    /// If receipt is None it means it is pruned.
+    pub receipts: T,
+    /// First block of bundle state.
+    pub first_block: BlockNumber,
 }
 
-// TODO impl for tuples
+/// A helper type for ethereum block inputs that consists of a block and the total difficulty.
+#[derive(Debug)]
+pub struct EthBlockExecutionInput<'a, Block> {
+    /// The block to execute.
+    pub block: &'a Block,
+    /// The total difficulty of the block.
+    pub total_difficulty: U256,
+}
+
+/// A type that can create a new executor.
+pub trait ExecutorProvider: Send + Sync + Clone {
+    /// Creates a new batch executor
+    fn batch_executor<DB>(&self, db: DB) -> impl BatchExecutor
+    where
+        DB: Database + DatabaseCommit;
+
+    // Returns a new executor for single block execution.
+    fn executor<DB>(&self, db: DB) -> impl Executor
+    where
+        DB: Database + DatabaseCommit;
+}
+
+#[cfg(test)]
+mod tests {
+    use std::marker::PhantomData;
+
+    use revm::db::{CacheDB, EmptyDB};
+
+    use super::*;
+
+    #[derive(Clone, Default)]
+    struct TestExecutorProvider;
+
+    impl ExecutorProvider for TestExecutorProvider {
+        fn batch_executor<DB>(&self, db: DB) -> impl BatchExecutor
+        where
+            DB: Database + DatabaseCommit,
+        {
+            todo!()
+        }
+
+        fn executor<DB>(&self, db: DB) -> impl Executor
+        where
+            DB: Database + DatabaseCommit,
+        {
+            TestExecutor(PhantomData)
+        }
+    }
+
+    struct TestExecutor<DB>(PhantomData<DB>);
+
+    impl<DB> Executor for TestExecutor<DB> {
+        type Input<'a> = &'a str;
+        type Output = ();
+        type Error = ();
+
+        fn execute(self, input: Self::Input<'_>) -> Result<Self::Output, Self::Error> {
+            Ok(())
+        }
+    }
+
+    impl<DB> BatchExecutor for TestExecutor<DB> {
+        type Input<'a> = &'a str;
+        type Output = ();
+        type Error = ();
+
+        fn execute_one(&mut self, input: Self::Input<'_>) -> Result<BatchBlockOutput, Self::Error> {
+            Ok(BatchBlockOutput { size_hint: None })
+        }
+
+        fn finalize(self) -> Self::Output {
+            ()
+        }
+    }
+
+    #[test]
+    fn test_provider() {
+        let provider = TestExecutorProvider::default();
+        let db = CacheDB::<EmptyDB>::default();
+        let mut executor = provider.executor(db);
+        executor.execute("test").unwrap();
+    }
+}
