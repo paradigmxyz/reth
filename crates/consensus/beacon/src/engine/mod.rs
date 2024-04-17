39455d0ace6a736d91607f1cf7b258b5518f9737
Roman Krasiuk <rokrassyuk@gmail.com>
2024-04-17 10:03:21 +0200

    import PipelineError in tests

--- crates/consensus/beacon/src/engine/mod.rs
+++ crates/consensus/beacon/src/engine/mod.rs
@@ -1885,7 +1885,7 @@ mod tests {
     use reth_provider::{BlockWriter, ProviderFactory};
     use reth_rpc_types::engine::{ForkchoiceState, ForkchoiceUpdated, PayloadStatus};
     use reth_rpc_types_compat::engine::payload::try_block_to_payload_v1;
-    use reth_stages::{ExecOutput, StageError};
+    use reth_stages::{ExecOutput, PipelineError, StageError};
     use std::{collections::VecDeque, sync::Arc};
     use tokio::sync::oneshot::error::TryRecvError;
 
