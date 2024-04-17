e7f2159a1763c9c266b105d33fa4a13e950925d7
Matthias Seitz <matthias.seitz@outlook.de>
2024-04-17 19:15:37 +0200

    make inspector work as well

--- crates/evm-ethereum/src/execute.rs
+++ crates/evm-ethereum/src/execute.rs
@@ -1,4 +1,4 @@
-//! Ethereum executor.
+//! Ethereum block executor.
 
 use reth_evm::{
     execute::{BatchBlockOutput, BatchExecutor, EthBlockExecutionInput, EthBlockOutput, Executor},
@@ -20,7 +20,7 @@ use reth_revm::{
     processor::verify_receipt,
     stack::InspectorStack,
     state_change::{apply_beacon_root_contract_call, post_block_balance_increments},
-    State,
+    Evm, State,
 };
 use revm_primitives::{
     db::{Database, DatabaseCommit},
@@ -29,76 +29,34 @@ use revm_primitives::{
 use std::sync::Arc;
 use tracing::debug;
 
-/// A basic Ethereum block executor.
-///
-/// Expected usage:
-/// - Create a new instance of the executor.
-/// - Execute the block.
-#[derive(Debug)]
-pub struct EthBlockExecutor<EvmConfig, DB> {
+/// Helper container type for EVM with chain spec.
+#[derive(Debug, Clone)]
+struct EthEvmExecutor<EvmConfig> {
     /// The chainspec
     chain_spec: Arc<ChainSpec>,
     /// How to create an EVM.
     evm_config: EvmConfig,
-    /// The state to use for execution
-    state: State<DB>,
-    /// Optional inspector stack for debugging
-    inspector: Option<InspectorStack>,
 }
 
-impl<EvmConfig, DB> EthBlockExecutor<EvmConfig, DB> {
-    /// Creates a new Ethereum block executor.
-    pub fn new(chain_spec: Arc<ChainSpec>, evm: EvmConfig, state: State<DB>) -> Self {
-        Self { chain_spec, evm_config: evm, state, inspector: None }
-    }
-
-    /// Sets the inspector stack for debugging.
-    pub fn with_inspector(mut self, inspector: InspectorStack) -> Self {
-        self.inspector = Some(inspector);
-        self
-    }
-}
-
-impl<EvmConfig, DB> EthBlockExecutor<EvmConfig, DB>
+impl<EvmConfig> EthEvmExecutor<EvmConfig>
 where
     EvmConfig: ConfigureEvm,
-    // TODO: get rid of this
     EvmConfig: ConfigureEvmEnv<TxMeta = ()>,
-    DB: Database<Error = ProviderError> + DatabaseCommit,
 {
-    /// Configures a new evm configuration and block environment for the given block.
+    /// Executes the transactions in the block and returns the receipts.
     ///
-    /// Caution: this does not initialize the tx environment.
-    fn evm_env_for_block(&self, header: &Header, total_difficulty: U256) -> EnvWithHandlerCfg {
-        let mut cfg = CfgEnvWithHandlerCfg::new(Default::default(), Default::default());
-        let mut block_env = BlockEnv::default();
-        EvmConfig::fill_cfg_and_block_env(
-            &mut cfg,
-            &mut block_env,
-            &self.chain_spec,
-            header,
-            total_difficulty,
-        );
-
-        EnvWithHandlerCfg::new_with_cfg_env(cfg, block_env, Default::default())
-    }
-
-    /// Execute a single block and apply the state changes to the internal state.
+    /// This applies the pre-execution changes, executes the transactions.
     ///
-    /// Returns the receipts of the transactions in the block and the total gas used.
-    fn execute_and_verify(
+    /// It does __not__ apply post-execution changes.
+    fn execute_pre_and_transactions<Ext, DB>(
         &mut self,
         block: &BlockWithSenders,
-        total_difficulty: U256,
-    ) -> Result<(Vec<Receipt>, u64), BlockExecutionError> {
-        // 1. prepare state on new block
-        self.on_new_block(&block.header);
-
-        // 2. configure the evm and execute
-        let env = self.evm_env_for_block(&block.header, total_difficulty);
-        let mut evm = self.evm_config.evm_with_env(&mut self.state, env);
-
-        // 3. apply pre execution changes
+        mut evm: Evm<'_, Ext, DB>,
+    ) -> Result<(Vec<Receipt>, u64), BlockExecutionError>
+    where
+        DB: Database<Error = ProviderError> + DatabaseCommit,
+    {
+        //  apply pre execution changes
         apply_beacon_root_contract_call(
             &self.chain_spec,
             block.timestamp,
@@ -134,8 +92,6 @@ where
             })?;
             evm.db_mut().commit(state);
 
-            // self.stats.apply_state_duration += time.elapsed();
-
             // append gas used
             cumulative_gas_used += result.gas_used();
 
@@ -166,14 +122,104 @@ where
             .into())
         }
 
-        // 5. apply post execution changes
+        Ok((receipts, cumulative_gas_used))
+    }
+}
+
+/// A basic Ethereum block executor.
+///
+/// Expected usage:
+/// - Create a new instance of the executor.
+/// - Execute the block.
+#[derive(Debug)]
+pub struct EthBlockExecutor<EvmConfig, DB> {
+    /// Chain specific evm config
+    evm_spec: EthEvmExecutor<EvmConfig>,
+    /// The state to use for execution
+    state: State<DB>,
+    /// Optional inspector stack for debugging
+    inspector: Option<InspectorStack>,
+}
+
+impl<EvmConfig, DB> EthBlockExecutor<EvmConfig, DB> {
+    /// Creates a new Ethereum block executor.
+    pub fn new(chain_spec: Arc<ChainSpec>, evm_config: EvmConfig, state: State<DB>) -> Self {
+        Self { evm_spec: EthEvmExecutor { chain_spec, evm_config }, state, inspector: None }
+    }
+
+    /// Sets the inspector stack for debugging.
+    pub fn with_inspector(mut self, inspector: InspectorStack) -> Self {
+        self.inspector = Some(inspector);
+        self
+    }
+
+    #[inline]
+    fn chain_spec(&self) -> &ChainSpec {
+        &self.evm_spec.chain_spec
+    }
+}
+
+impl<EvmConfig, DB> EthBlockExecutor<EvmConfig, DB>
+where
+    EvmConfig: ConfigureEvm,
+    // TODO: get rid of this
+    EvmConfig: ConfigureEvmEnv<TxMeta = ()>,
+    DB: Database<Error = ProviderError> + DatabaseCommit,
+{
+    /// Configures a new evm configuration and block environment for the given block.
+    ///
+    /// Caution: this does not initialize the tx environment.
+    fn evm_env_for_block(&self, header: &Header, total_difficulty: U256) -> EnvWithHandlerCfg {
+        let mut cfg = CfgEnvWithHandlerCfg::new(Default::default(), Default::default());
+        let mut block_env = BlockEnv::default();
+        EvmConfig::fill_cfg_and_block_env(
+            &mut cfg,
+            &mut block_env,
+            self.chain_spec(),
+            header,
+            total_difficulty,
+        );
+
+        EnvWithHandlerCfg::new_with_cfg_env(cfg, block_env, Default::default())
+    }
+
+    /// Execute a single block and apply the state changes to the internal state.
+    ///
+    /// Returns the receipts of the transactions in the block and the total gas used.
+    fn execute_and_verify(
+        &mut self,
+        block: &BlockWithSenders,
+        total_difficulty: U256,
+    ) -> Result<(Vec<Receipt>, u64), BlockExecutionError> {
+        // 1. prepare state on new block
+        self.on_new_block(&block.header);
+
+        // 2. configure the evm and execute
+        let env = self.evm_env_for_block(&block.header, total_difficulty);
+
+        let (receipts, gas_used) = {
+            if let Some(inspector) = self.inspector.as_mut() {
+                let evm = self.evm_spec.evm_config.evm_with_env_and_inspector(
+                    &mut self.state,
+                    env,
+                    inspector,
+                );
+                self.evm_spec.execute_pre_and_transactions(block, evm)?
+            } else {
+                let evm = self.evm_spec.evm_config.evm_with_env(&mut self.state, env);
+
+                self.evm_spec.execute_pre_and_transactions(block, evm)?
+            }
+        };
+
+        // 3. apply post execution changes
         self.post_execution(block, total_difficulty)?;
 
         // Before Byzantium, receipts contained state root that would mean that expensive
         // operation as hashing that is required for state root got calculated in every
         // transaction This was replaced with is_success flag.
         // See more about EIP here: https://eips.ethereum.org/EIPS/eip-658
-        if self.chain_spec.is_byzantium_active_at_block(block.header.number) {
+        if self.chain_spec().is_byzantium_active_at_block(block.header.number) {
             if let Err(error) =
                 verify_receipt(block.header.receipts_root, block.header.logs_bloom, receipts.iter())
             {
@@ -182,13 +228,13 @@ where
             };
         }
 
-        Ok((receipts, cumulative_gas_used))
+        Ok((receipts, gas_used))
     }
 
     /// Apply settings before a new block is executed.
     pub(crate) fn on_new_block(&mut self, header: &Header) {
         // Set state clear flag if the block is after the Spurious Dragon hardfork.
-        let state_clear_flag = self.chain_spec.is_spurious_dragon_active_at_block(header.number);
+        let state_clear_flag = self.chain_spec().is_spurious_dragon_active_at_block(header.number);
         self.state.set_state_clear_flag(state_clear_flag);
     }
 
@@ -200,7 +246,7 @@ where
         total_difficulty: U256,
     ) -> Result<(), BlockExecutionError> {
         let mut balance_increments = post_block_balance_increments(
-            &self.chain_spec,
+            self.chain_spec(),
             block.number,
             block.difficulty,
             block.beneficiary,
@@ -211,7 +257,7 @@ where
         );
 
         // Irregular state change at Ethereum DAO hardfork
-        if self.chain_spec.fork(Hardfork::Dao).transitions_at_block(block.number) {
+        if self.chain_spec().fork(Hardfork::Dao).transitions_at_block(block.number) {
             // drain balances from hardcoded addresses.
             let drained_balance: u128 = self
                 .state
