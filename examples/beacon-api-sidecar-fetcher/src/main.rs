90860bc1d86ad402e71148229a546513f7fc9bf2
hawkinsjgh@gmail.com <jack.hawkins@mycelium.ventures>
2024-04-17 08:52:14 +0100

    bump v

--- examples/beacon-api-sidecar-fetcher/src/main.rs
+++ examples/beacon-api-sidecar-fetcher/src/main.rs
@@ -21,15 +21,26 @@ use reth::{
 use serde::{self, Deserialize, Serialize};
 use tracing::debug;
 
+//TODO look at PeersManager.
+//TODO Figure out pending_requests/queued_actions
+//TODO Figure out error handling
+//TODO Pass (tx, + blockhash/number + sidecar) alongside value
+//TODO Seperate Default CL URL logic/function
+//TODO Add Tests.
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     Ok(())
 }
 
-//TODO look at PeersManager.
-//TODO Figure out pending_requests/queued_actions
-//Add Reqwest logic
-//Create custom tests.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MinedSidecar {
+    tx: Transaction,
+    block_hash: BlockHash,
+    block_number: BlockNumber,
+    sidecar: BlobTransactionSidecar,
+}
+
 #[derive(Debug)]
 pub struct MinedSidecarStream<St, P>
 where
@@ -38,11 +49,10 @@ where
     events: St,
     pool: P,
     client: reqwest::Client,
-    pending_requests: FuturesUnordered<
-        Pin<Box<dyn Future<Output = Result<BlobTransactionSidecar, reqwest::Error>> + Send>>,
-    >, /* will contant CL queries. */
-    queued_actions: VecDeque<Result<BlobTransactionSidecar, reqwest::Error>>, /* Buffer for
-                                                                               * ready items */
+    pending_requests:
+        FuturesUnordered<Pin<Box<dyn Future<Output = Result<reqwest::Response, reqwest::Error>>>>>, /* will contant CL queries. */
+    queued_actions: VecDeque<BlobTransactionSidecar>, /* Buffer for
+                                                       * ready items */
 }
 
 impl<St, P> Stream for MinedSidecarStream<St, P>
@@ -50,6 +60,7 @@ where
     St: Stream<Item = CanonStateNotification> + Send + Unpin + 'static,
     P: TransactionPoolExt + Unpin + 'static,
 {
+    //TODO Change to custom type
     //type Item = Result<MinedSidecar, SideCarError<E>>;
     type Item = Result<BlobTransactionSidecar, reqwest::Error>;
 
@@ -58,16 +69,21 @@ where
 
         // return any buffered result
         if let Some(blob_sidecar) = this.queued_actions.pop_front() {
-            return Poll::Ready(Some(blob_sidecar));
+            return Poll::Ready(Some(Ok(blob_sidecar)));
         }
 
         // Check if any pending reqwests are ready.
+        // TODO Response/Type for CL Request
         while let Poll::Ready(Some(result)) = this.pending_requests.poll_next_unpin(cx) {
             match result {
-                Ok(blob_sidecar) => return Poll::Ready(Some(Ok(blob_sidecar))),
-                Err(e) => {
-                    debug!(error = %e, "Error processing a pending consensus layer request.");
+                // is this array that we than need to loop through?
+                Ok(response) => {
+                    if response.status().is_success() {
+                        // do something with response.
+                        //return Poll::Ready(Some(Ok(blob_sidecar)))
+                    }
                 }
+                Err(e) => debug!(error = %e, "Error processing a pending consensus layer request."),
             }
         }
 
@@ -79,13 +95,35 @@ where
                         match this.pool.get_blob(tx.hash) {
                             Ok(Some(blob)) => {
                                 // If get_blob returns Ok(Some(blob)), the blob exists
-                                this.queued_actions.push_back(Ok(blob));
+                                this.queued_actions.push_back(blob);
                             }
                             Ok(None) => {
-                                // Fetch from CL
+                                // TODO Move this to default unless specified ot herwise logic?
+                                let client_clone = this.client.clone();
+                                let url = format!(
+                                    "http://{}/eth/v1/beacon/blob_sidecars/{}",
+                                    "your-cl-node.com",
+                                    notification.tip().block.hash()
+                                );
+
+                                // add logic here to get response?
+                                let boxed_future = Box::pin(async move {
+                                    client_clone
+                                        .get(&url)
+                                        .header("Accept", "application/json")
+                                        .send()
+                                        .await
+                                });
+
+                                this.pending_requests.push(boxed_future);
+                                //TODO send along with MinedSideCar
+                                // Return tx with block/etc/with sidecar
                             }
+
                             // change this
                             Err(e) => {
+                                // We store error for this in queued actions?
+                                // Lookup error handling
                                 debug!(
                                     %e,
                                 );
@@ -98,80 +136,4 @@ where
             }
         }
     }
-}
-///TODO Add
-impl<St, P> MinedSidecarStream<St, P>
-where
-    St: Stream<Item = CanonStateNotification> + Send + Unpin + 'static,
-    P: TransactionPoolExt + Unpin + 'static,
-{
-    // Ensure this method transforms a CanonStateNotification into a BlobSidecar
-    fn data_exists(&mut self, item: &CanonStateNotification) -> BlobSidecar {
-        // Transformation logic here
-        // For demonstration, let's return a default BlobSidecar for now
-        BlobSidecar { ..Default::default() }
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct MinedSidecar {
-    tx: Transaction,
-    block_hash: BlockHash,
-    block_number: BlockNumber,
-    sidecar: BlobSidecar,
-}
-
-#[derive(Debug, Clone)]
-pub struct SideCarError<E> {
-    error: E,        // Using a simple ErrorDetail struct for demonstration.
-    tx_hash: String, // Including transaction hash in the error as well.
-    block_hash: BlockHash,
-    block_number: BlockNumber,
-}
-
-/// TODO: Import as feature
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct BlobSidecar {
-    pub data: Vec<Data>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Data {
-    pub index: String,
-    pub blob: String,
-    #[serde(rename = "kzg_commitment")]
-    pub kzg_commitment: String,
-    #[serde(rename = "kzg_proof")]
-    pub kzg_proof: String,
-    #[serde(rename = "signed_block_header")]
-    pub signed_block_header: SignedBlockHeader,
-    #[serde(rename = "kzg_commitment_inclusion_proof")]
-    pub kzg_commitment_inclusion_proof: Vec<String>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct SignedBlockHeader {
-    pub message: Message,
-    pub signature: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Message {
-    pub slot: String,
-    #[serde(rename = "proposer_index")]
-    pub proposer_index: String,
-    #[serde(rename = "parent_root")]
-    pub parent_root: String,
-    #[serde(rename = "state_root")]
-    pub state_root: String,
-    #[serde(rename = "body_root")]
-    pub body_root: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct BlobError {
-    #[serde(rename = "code")]
-    code: u16,
-    #[serde(rename = "message")]
-    message: String,
 }
