3328c75f9a1b13a20fcb80a9499dd664641df4f6
hawkinsjgh@gmail.com <jack.hawkins@mycelium.ventures>
2024-04-19 18:35:40 +0100

    just blob decode left

--- examples/beacon-api-sidecar-fetcher/src/main.rs
+++ examples/beacon-api-sidecar-fetcher/src/main.rs
@@ -10,19 +10,21 @@ use std::{
     task::{ready, Context, Poll},
 };
 
+use thiserror::Error;
+
 use futures_util::{
     future::BoxFuture, stream::FuturesUnordered, Future, FutureExt, Stream, StreamExt,
 };
-use reqwest::{Error, Request, Response};
+use reqwest::{Error, Request, Response, StatusCode};
 use reth::{
+    network::error,
     primitives::{
         alloy_primitives::TxHash, BlobTransaction, BlobTransactionSidecar, BlockHash, BlockNumber,
         Transaction, B256,
     },
     providers::CanonStateNotification,
-    transaction_pool::TransactionPoolExt,
+    transaction_pool::{BlobStoreError, TransactionPoolExt},
 };
-
 use serde::{self, Deserialize, Serialize};
 use tracing::debug;
 
@@ -36,19 +38,40 @@ async fn main() -> eyre::Result<()> {
     Ok(())
 }
 
+//TODO Add nicer errors.
+#[derive(Debug, Error)]
+pub enum SideCarError {
+    #[error("Reqwest encountered an error: {0}")]
+    ReqwestError(Error),
+
+    #[error("There was an error grabbing the blob from the tx pool: {0}")]
+    TranscanctionPoolError(BlobStoreError),
+
+    #[error("400: {0}")]
+    InvalidBlockID(String),
+
+    #[error("404: {0}")]
+    BlockNotFound(String),
+
+    #[error("500: {0}")]
+    InternalError(String),
+
+    #[error("Network error: {0}")]
+    NetworkError(String),
+
+    #[error("Data parsing error: {0}")]
+    DataParsingError(String),
+
+    #[error("{0} Error: {1}")]
+    UnknownError(u16, String),
+}
+
 #[derive(Debug, Clone)]
 pub struct MinedSidecar {
     tx_hash: TxHash,
     block_hash: BlockHash,
     block_number: BlockNumber,
     sidecar: BlobTransactionSidecar,
-}
-
-struct PendingMinedSidecar {
-    cl_request: Pin<Box<dyn Future<Output = Result<Response, Error>> + Send>>,
-    tx_hash: TxHash,
-    block_hash: BlockHash,
-    block_number: BlockNumber,
 }
 
 #[derive(Debug)]
@@ -59,7 +82,8 @@ where
     events: St,
     pool: P,
     client: reqwest::Client,
-    pending_requests: FuturesUnordered<Pin<Box<dyn Future<Output = PendingMinedSidecar>>>>, /* will contant CL queries. */
+    pending_requests:
+        FuturesUnordered<Pin<Box<dyn Future<Output = Result<MinedSidecar, SideCarError>>>>>, /* will contant CL queries. */
     queued_actions: VecDeque<MinedSidecar>, /* Buffer for
                                              * ready items */
 }
@@ -71,7 +95,7 @@ where
 {
     //TODO Change to custom type
     //type Item = Result<MinedSidecar, SideCarError<E>>;
-    type Item = Result<MinedSidecar, reqwest::Error>;
+    type Item = Result<MinedSidecar, SideCarError>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this: &mut MinedSidecarStream<St, P> = self.get_mut();
@@ -85,33 +109,11 @@ where
         // TODO Response/Type for CL Request
         while let Poll::Ready(Some(pending_result)) = this.pending_requests.poll_next_unpin(cx) {
             match pending_result {
-                Ok(mut pending_sidecar) => {
-                    // Now poll the future inside PendingMinedSidecar
-                    return match pending_sidecar.cl_request.poll_unpin(cx) {
-                        Poll::Ready(Ok(response)) => {
-                            if response.status().is_success() {
-                                // TODO add logic to decode json to side car
-                                Poll::Ready(Some(Ok(MinedSidecar {
-                                    tx_hash: pending_sidecar.tx_hash,
-                                    block_hash: pending_sidecar.block_hash,
-                                    block_number: pending_sidecar.block_number,
-                                    sidecar: todo!(),
-                                })));
-                            } else {
-                                // Handle HTTP errors
-                                return Poll::Ready(Some(Err(todo!())));
-                            }
-                        }
-                        Poll::Ready(Err(e)) => Poll::Ready(Some(Err(e))),
-                        Poll::Pending => Poll::Pending,
-                    };
-                }
-                Err(e) => {
-                    // Handle errors in fetching the future itself
-                    return Poll::Ready(Some(Err(e)));
-                }
+                Ok(mined_sidecar) => return Poll::Ready(Some(Ok(mined_sidecar))),
+                Err(err) => return Poll::Ready(Some(Err(err))),
             }
         }
+
         // TODO: Add fetching logic here.
         loop {
             match this.events.poll_next_unpin(cx) {
@@ -119,7 +121,6 @@ where
                     for tx in notification.tip().transactions().filter(|tx| tx.is_eip4844()) {
                         match this.pool.get_blob(tx.hash) {
                             Ok(Some(blob)) => {
-                                // If get_blob returns Ok(Some(blob)), the blob exists
                                 this.queued_actions.push_back(MinedSidecar {
                                     tx_hash: tx.hash,
                                     block_hash: notification.tip().block.hash(),
@@ -128,7 +129,7 @@ where
                                 });
                             }
                             Ok(None) => {
-                                // TODO Move this to default unless specified ot herwise logic?
+                                // TODO Change this
                                 let client_clone = this.client.clone();
                                 let url = format!(
                                     "http://{}/eth/v1/beacon/blob_sidecars/{}",
@@ -136,31 +137,75 @@ where
                                     notification.tip().block.hash()
                                 );
 
-                                // add logic here to get response?
+                                let query = Box::pin(async move {
+                                    let response = match client_clone
+                                        .get(url)
+                                        .header("Accept", "application/json")
+                                        .send()
+                                        .await
+                                    {
+                                        Ok(response) => response,
+                                        Err(err) => return Err(SideCarError::ReqwestError(err)),
+                                    };
 
-                                let pending_request = PendingMinedSidecar {
-                                    cl_request: Box::pin(async move {
-                                        client_clone
-                                            .get(&url)
-                                            .header("Accept", "application/json")
-                                            .send()
-                                            .await
-                                    }),
-                                    tx_hash: tx.hash.clone(),
-                                    block_hash: notification.tip().block.hash().clone(),
-                                    block_number: notification.tip().block.number,
-                                };
+                                    if !response.status().is_success() {
+                                        return match response.status() {
+                                            StatusCode::BAD_REQUEST => {
+                                                Err(SideCarError::InvalidBlockID(
+                                                    "Invalid request to server.".to_string(),
+                                                ))
+                                            }
+                                            StatusCode::NOT_FOUND => {
+                                                Err(SideCarError::BlockNotFound(
+                                                    "Requested block not found.".to_string(),
+                                                ))
+                                            }
+                                            StatusCode::INTERNAL_SERVER_ERROR => {
+                                                Err(SideCarError::InternalError(
+                                                    "Server encountered an error.".to_string(),
+                                                ))
+                                            }
+                                            _ => Err(SideCarError::UnknownError(
+                                                response.status().as_u16(),
+                                                "Unhandled HTTP status.".to_string(),
+                                            )),
+                                        };
+                                    }
 
-                                this.pending_requests.push(pending_request);
+                                    let bytes = match response.bytes().await {
+                                        Ok(b) => b,
+                                        Err(e) => {
+                                            return Err(SideCarError::NetworkError(e.to_string()))
+                                        }
+                                    };
+
+                                    // This should be where we turn bytes into a sidecar
+                                    let data_str = match std::str::from_utf8(&bytes) {
+                                        Ok(s) => s,
+                                        Err(e) => {
+                                            return Err(SideCarError::DataParsingError(
+                                                e.to_string(),
+                                            ))
+                                        }
+                                    };
+
+                                    // TODO Logic to figure out sidecar here.
+                                    Ok(MinedSidecar {
+                                        tx_hash: tx.hash,
+                                        block_hash: notification.tip().block.hash(),
+                                        block_number: notification.tip().block.number,
+                                        sidecar: data_str.to_string(),
+                                    })
+                                });
+
+                                this.pending_requests.push(query);
                             }
 
                             // change this
-                            Err(e) => {
-                                // We store error for this in queued actions?
-                                // Lookup error handling
-                                debug!(
-                                    %e,
-                                );
+                            Err(err) => {
+                                return Poll::Ready(Some(Err(SideCarError::TranscanctionPoolError(
+                                    err,
+                                ))))
                             }
                         }
                     }
