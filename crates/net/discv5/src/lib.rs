c8b1f38f3ee3c99b950fb0dda441072fed1ab8be
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-04-19 23:57:37 +0200

    Update comment

--- crates/net/discv5/src/lib.rs
+++ crates/net/discv5/src/lib.rs
@@ -803,7 +803,7 @@ mod tests {
 
     #[test]
     fn select_lookup_target() {
-        // bucket index ceiled to the next multiple of 4
+        // bucket index ceiled to the next multiple of 8
         const fn expected_bucket_index(kbucket_index: usize) -> u64 {
             let log2distance = kbucket_index + 1;
             if log2distance % 8 == 0 {
