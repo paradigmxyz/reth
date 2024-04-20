f5b81f878347b0e57421cb23ab4fffa75f25f1b6
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-04-20 16:56:47 +0200

    Revert crates/primitives/src/transaction/mod.rs to main

--- crates/primitives/src/transaction/mod.rs
+++ crates/primitives/src/transaction/mod.rs
@@ -45,7 +45,9 @@ mod eip4844;
 mod error;
 mod legacy;
 mod meta;
+#[cfg(feature = "c-kzg")]
 mod pooled;
+#[cfg(feature = "c-kzg")]
 mod sidecar;
 mod signature;
 mod tx_type;
@@ -961,8 +963,8 @@ impl TransactionSignedNoHash {
         // Optimism's Deposit transaction does not have a signature. Directly return the
         // `from` address.
         #[cfg(feature = "optimism")]
-        if let Some(address) = get_deposit_or_null_address(&self.transaction, &self.signature) {
-            return Some(address)
+        if let Transaction::Deposit(TxDeposit { from, .. }) = self.transaction {
+            return Some(from)
         }
 
         let signature_hash = self.signature_hash();
@@ -986,9 +988,20 @@ impl TransactionSignedNoHash {
         buffer.clear();
         self.transaction.encode_without_signature(buffer);
 
+        // Optimism's Deposit transaction does not have a signature. Directly return the
+        // `from` address.
         #[cfg(feature = "optimism")]
-        if let Some(address) = get_deposit_or_null_address(&self.transaction, &self.signature) {
-            return Some(address)
+        {
+            if let Transaction::Deposit(TxDeposit { from, .. }) = self.transaction {
+                return Some(from)
+            }
+
+            // pre bedrock system transactions were sent from the zero address as legacy
+            // transactions with an empty signature Note: this is very hacky and only
+            // relevant for op-mainnet pre bedrock
+            if self.is_legacy() && self.signature == Signature::optimism_deposit_tx_signature() {
+                return Some(Address::ZERO)
+            }
         }
 
         self.signature.recover_signer_unchecked(keccak256(buffer))
@@ -1203,8 +1216,8 @@ impl TransactionSigned {
         // Optimism's Deposit transaction does not have a signature. Directly return the
         // `from` address.
         #[cfg(feature = "optimism")]
-        if let Some(address) = get_deposit_or_null_address(&self.transaction, &self.signature) {
-            return Some(address)
+        if let Transaction::Deposit(TxDeposit { from, .. }) = self.transaction {
+            return Some(from)
         }
         let signature_hash = self.signature_hash();
         self.signature.recover_signer(signature_hash)
@@ -1219,8 +1232,8 @@ impl TransactionSigned {
         // Optimism's Deposit transaction does not have a signature. Directly return the
         // `from` address.
         #[cfg(feature = "optimism")]
-        if let Some(address) = get_deposit_or_null_address(&self.transaction, &self.signature) {
-            return Some(address)
+        if let Transaction::Deposit(TxDeposit { from, .. }) = self.transaction {
+            return Some(from)
         }
         let signature_hash = self.signature_hash();
         self.signature.recover_signer_unchecked(signature_hash)
@@ -1809,26 +1822,6 @@ impl IntoRecoveredTransaction for TransactionSignedEcRecovered {
     }
 }
 
-#[cfg(feature = "optimism")]
-fn get_deposit_or_null_address(
-    transaction: &Transaction,
-    signature: &Signature,
-) -> Option<Address> {
-    // Optimism's Deposit transaction does not have a signature. Directly return the
-    // `from` address.
-    if let Transaction::Deposit(TxDeposit { from, .. }) = transaction {
-        return Some(*from)
-    }
-    // pre bedrock system transactions were sent from the zero address as legacy
-    // transactions with an empty signature Note: this is very hacky and only
-    // relevant for op-mainnet pre bedrock
-    if transaction.is_legacy() && *signature == Signature::optimism_deposit_tx_signature() {
-        return Some(Address::ZERO)
-    }
-
-    None
-}
-
 #[cfg(test)]
 mod tests {
     use crate::{
@@ -2201,16 +2194,7 @@ mod tests {
         );
 
         let encoded = &alloy_rlp::encode(signed_tx);
-
-        assert_eq!(
-            if cfg!(feature = "optimism") {
-                hex!("c9808080808080808080")
-            } else {
-                hex!("c98080808080801b8080")
-            },
-            &encoded[..]
-        );
-
+        assert_eq!(hex!("c98080808080801b8080"), encoded[..]);
         assert_eq!(MIN_LENGTH_LEGACY_TX_ENCODED, encoded.len());
 
         TransactionSigned::decode(&mut &encoded[..]).unwrap();
