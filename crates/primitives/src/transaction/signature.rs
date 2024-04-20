f6f8b1ae16143835625cf292f14b95b141f896e7
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-04-20 05:12:53 +0200

    Re-add custom signature encoding

--- crates/primitives/src/transaction/signature.rs
+++ crates/primitives/src/transaction/signature.rs
@@ -82,6 +82,10 @@ impl Signature {
             // EIP-155: v = {0, 1} + CHAIN_ID * 2 + 35
             self.odd_y_parity as u64 + chain_id * 2 + 35
         } else {
+            #[cfg(feature = "optimism")]
+            if *self == Self::optimism_deposit_tx_signature() {
+                return 0
+            }
             self.odd_y_parity as u64 + 27
         }
     }
