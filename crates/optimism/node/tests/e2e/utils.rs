689db362160ae01e7c2d6da953d1e1edaaf7b485
joshieDo <93316087+joshieDo@users.noreply.github.com>
2024-04-23 15:30:38 +0100

    feat: add UX improvements on e2e testing (#7804)

--- crates/optimism/node/tests/e2e/utils.rs
+++ crates/optimism/node/tests/e2e/utils.rs
@@ -1,7 +1,45 @@
-use reth::rpc::types::engine::PayloadAttributes;
-use reth_node_optimism::OptimismPayloadBuilderAttributes;
+use reth::{rpc::types::engine::PayloadAttributes, tasks::TaskManager};
+use reth_e2e_test_utils::{wallet::Wallet, NodeHelperType};
+use reth_node_optimism::{OptimismBuiltPayload, OptimismNode, OptimismPayloadBuilderAttributes};
 use reth_payload_builder::EthPayloadBuilderAttributes;
-use reth_primitives::{Address, B256};
+use reth_primitives::{Address, ChainSpecBuilder, Genesis, B256, BASE_MAINNET};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+/// Optimism Node Helper type
+pub(crate) type OpNode = NodeHelperType<OptimismNode>;
+
+pub(crate) async fn setup(num_nodes: usize) -> eyre::Result<(Vec<OpNode>, TaskManager, Wallet)> {
+    let genesis: Genesis = serde_json::from_str(include_str!("../assets/genesis.json")).unwrap();
+    reth_e2e_test_utils::setup(
+        num_nodes,
+        Arc::new(
+            ChainSpecBuilder::default()
+                .chain(BASE_MAINNET.chain)
+                .genesis(genesis)
+                .ecotone_activated()
+                .build(),
+        ),
+        false,
+    )
+    .await
+}
+
+pub(crate) async fn advance_chain(
+    length: usize,
+    node: &mut OpNode,
+    wallet: Arc<Mutex<Wallet>>,
+) -> eyre::Result<Vec<(OptimismBuiltPayload, OptimismPayloadBuilderAttributes)>> {
+    node.advance(
+        length as u64,
+        || {
+            let wallet = wallet.clone();
+            Box::pin(async move { wallet.lock().await.optimism_l1_block_info_tx().await })
+        },
+        optimism_payload_attributes,
+    )
+    .await
+}
 
 /// Helper function to create a new eth payload attributes
 pub(crate) fn optimism_payload_attributes(timestamp: u64) -> OptimismPayloadBuilderAttributes {
