0262a573f5b692221ad00f01d051e3a6da1addbe
lakshya-sky <8559992+lakshya-sky@users.noreply.github.com>
2024-04-24 13:10:56 -0400

    fix docs erros

--- crates/rpc/rpc-types/src/eth/error.rs
+++ crates/rpc/rpc-types/src/eth/error.rs
@@ -2,7 +2,7 @@
 
 use jsonrpsee_types::ErrorObject;
 
-/// A tait for custom rpc errors used by [EthApiError::Other].
+/// A tait for custom rpc errors used by Other variants of rpc errors.
 pub trait ToRpcError: std::error::Error + Send + Sync + 'static {
     /// Converts the error to a JSON-RPC error object.
     fn to_rpc_error(&self) -> ErrorObject<'static>;
