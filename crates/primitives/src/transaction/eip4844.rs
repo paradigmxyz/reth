1c17f08ad209a1ae120436461ceb2f7f5391811c
Rodrigo Herrera <rodrigoheitie@hotmail.com>
2024-04-25 06:50:04 -0600

    Replace TransactionKind with alloy_primitives::TxKind (#7859)

--- crates/primitives/src/transaction/eip4844.rs
+++ crates/primitives/src/transaction/eip4844.rs
@@ -1,7 +1,7 @@
 use super::access_list::AccessList;
 use crate::{
-    constants::eip4844::DATA_GAS_PER_BLOB, keccak256, Bytes, ChainId, Signature, TransactionKind,
-    TxType, B256, U256,
+    constants::eip4844::DATA_GAS_PER_BLOB, keccak256, Bytes, ChainId, Signature, TxKind, TxType,
+    B256, U256,
 };
 use alloy_rlp::{length_of_length, Decodable, Encodable, Header};
 use reth_codecs::{main_codec, Compact};
@@ -54,7 +54,7 @@ pub struct TxEip4844 {
     pub max_priority_fee_per_gas: u128,
     /// The 160-bit address of the message call’s recipient or, for a contract creation
     /// transaction, ∅, used here to denote the only member of B0 ; formally Tt.
-    pub to: TransactionKind,
+    pub to: TxKind,
     /// A scalar value equal to the number of Wei to
     /// be transferred to the message call’s recipient or,
     /// in the case of contract creation, as an endowment
