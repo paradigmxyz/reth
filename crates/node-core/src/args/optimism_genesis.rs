3f55a1bcf3c6bbcf67a8c3f4734162a144b04e37
Federico Gimenez <federico.gimenez@gmail.com>
2024-04-27 13:11:57 +0200

    fix tests

--- crates/node-core/src/args/optimism_genesis.rs
+++ crates/node-core/src/args/optimism_genesis.rs
@@ -4,46 +4,45 @@ use reth_primitives::{ChainSpec, ForkCondition, Hardfork};
 use serde::Deserialize;
 
 /// Genesis type for Optimism networks.
-#[derive(Default, Debug, Deserialize)]
-#[serde(default, rename_all = "camelCase")]
-pub struct OptimismGenesis {
-    /// Wraps Eth genesis fields.
-    #[serde(flatten)]
-    pub eth_genesis: Genesis,
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub(crate) enum OptimismGenesis {
+    Reth(ChainSpec),
+    Geth(Genesis),
 }
 
 impl From<OptimismGenesis> for ChainSpec {
     fn from(optimsim_genesis: OptimismGenesis) -> ChainSpec {
-        let mut chain_spec: ChainSpec = optimsim_genesis.eth_genesis.clone().into();
-
-        if let Some(block) = optimsim_genesis.eth_genesis.config.extra_fields.get("bedrock_block") {
-            chain_spec
-                .hardforks
-                .insert(Hardfork::Bedrock, ForkCondition::Block(block.as_u64().unwrap()));
+        match optimsim_genesis {
+            OptimismGenesis::Reth(genesis) => genesis,
+            OptimismGenesis::Geth(genesis) => {
+                let mut chain_spec: ChainSpec = genesis.clone().into();
+                if let Some(block) = genesis.config.extra_fields.get("bedrockBlock") {
+                    chain_spec
+                        .hardforks
+                        .insert(Hardfork::Bedrock, ForkCondition::Block(block.as_u64().unwrap()));
+                }
+                if let Some(timestamp) = genesis.config.extra_fields.get("regolithTime") {
+                    chain_spec.hardforks.insert(
+                        Hardfork::Regolith,
+                        ForkCondition::Timestamp(timestamp.as_u64().unwrap()),
+                    );
+                }
+                if let Some(timestamp) = genesis.config.extra_fields.get("ecotoneTime") {
+                    chain_spec.hardforks.insert(
+                        Hardfork::Ecotone,
+                        ForkCondition::Timestamp(timestamp.as_u64().unwrap()),
+                    );
+                }
+                if let Some(timestamp) = genesis.config.extra_fields.get("canyonTime") {
+                    chain_spec.hardforks.insert(
+                        Hardfork::Canyon,
+                        ForkCondition::Timestamp(timestamp.as_u64().unwrap()),
+                    );
+                }
+                chain_spec
+            }
         }
-        if let Some(timestamp) =
-            optimsim_genesis.eth_genesis.config.extra_fields.get("regolith_timestamp")
-        {
-            chain_spec
-                .hardforks
-                .insert(Hardfork::Regolith, ForkCondition::Timestamp(timestamp.as_u64().unwrap()));
-        }
-        if let Some(timestamp) =
-            optimsim_genesis.eth_genesis.config.extra_fields.get("ecotone_timestamp")
-        {
-            chain_spec
-                .hardforks
-                .insert(Hardfork::Ecotone, ForkCondition::Timestamp(timestamp.as_u64().unwrap()));
-        }
-        if let Some(timestamp) =
-            optimsim_genesis.eth_genesis.config.extra_fields.get("canyon_timestamp")
-        {
-            chain_spec
-                .hardforks
-                .insert(Hardfork::Canyon, ForkCondition::Timestamp(timestamp.as_u64().unwrap()));
-        }
-
-        chain_spec
     }
 }
 
@@ -75,53 +74,46 @@ mod tests {
     "#;
         let optimism_genesis: OptimismGenesis = serde_json::from_str(genesis).unwrap();
 
-        let actual_nonce = optimism_genesis.eth_genesis.nonce;
-        assert_eq!(actual_nonce, 9);
-        let actual_chain_id = optimism_genesis.eth_genesis.config.chain_id;
-        assert_eq!(actual_chain_id, 1);
+        if let OptimismGenesis::Geth(genesis) = optimism_genesis {
+            let actual_nonce = genesis.nonce;
+            assert_eq!(actual_nonce, 9);
+            let actual_chain_id = genesis.config.chain_id;
+            assert_eq!(actual_chain_id, 1);
 
-        let actual_bedrock_block =
-            optimism_genesis.eth_genesis.config.extra_fields.get("bedrock_block");
-        assert_eq!(actual_bedrock_block, Some(Value::from(10)).as_ref());
-        let actual_regolith_timestamp =
-            optimism_genesis.eth_genesis.config.extra_fields.get("regolith_timestamp");
-        assert_eq!(actual_regolith_timestamp, Some(Value::from(20)).as_ref());
-        let actual_ecotone_timestamp =
-            optimism_genesis.eth_genesis.config.extra_fields.get("ecotone_timestamp");
-        assert_eq!(actual_ecotone_timestamp, Some(Value::from(30)).as_ref());
-        let actual_canyon_timestamp =
-            optimism_genesis.eth_genesis.config.extra_fields.get("canyon_timestamp");
-        assert_eq!(actual_canyon_timestamp, Some(Value::from(40)).as_ref());
+            let actual_bedrock_block = genesis.config.extra_fields.get("bedrockBlock");
+            assert_eq!(actual_bedrock_block, Some(Value::from(10)).as_ref());
+            let actual_regolith_timestamp = genesis.config.extra_fields.get("regolithTime");
+            assert_eq!(actual_regolith_timestamp, Some(Value::from(20)).as_ref());
+            let actual_ecotone_timestamp = genesis.config.extra_fields.get("ecotoneTime");
+            assert_eq!(actual_ecotone_timestamp, Some(Value::from(30)).as_ref());
+            let actual_canyon_timestamp = genesis.config.extra_fields.get("canyonTime");
+            assert_eq!(actual_canyon_timestamp, Some(Value::from(40)).as_ref());
 
-        let optimism_object =
-            optimism_genesis.eth_genesis.config.extra_fields.get("optimism").unwrap();
-        assert_eq!(
-            optimism_object,
-            &serde_json::json!({
-                "eip1559Elasticity": 50,
-                "eip1559Denominator": 60,
-                "eip1559DenominatorCanyon": 70
-            })
-        );
+            let optimism_object = genesis.config.extra_fields.get("optimism").unwrap();
+            assert_eq!(
+                optimism_object,
+                &serde_json::json!({
+                    "eip1559Elasticity": 50,
+                    "eip1559Denominator": 60,
+                    "eip1559DenominatorCanyon": 70
+                })
+            );
+        } else {
+            panic!("unexpected OptimismGenesis variant");
+        }
     }
 
     #[test]
     fn optimism_genesis_into_chainspec() {
         let mut extra_fields: BTreeMap<String, Value> = BTreeMap::new();
-        extra_fields
-            .insert("bedrock_block".to_string(), Value::Number(serde_json::Number::from(1)));
-        extra_fields
-            .insert("regolith_timestamp".to_string(), Value::Number(serde_json::Number::from(2)));
-        extra_fields
-            .insert("ecotone_timestamp".to_string(), Value::Number(serde_json::Number::from(3)));
-        extra_fields
-            .insert("canyon_timestamp".to_string(), Value::Number(serde_json::Number::from(4)));
-        let optimism_genesis = OptimismGenesis {
-            eth_genesis: Genesis {
-                config: ChainConfig { extra_fields, ..Default::default() },
-                ..Default::default()
-            },
-        };
+        extra_fields.insert("bedrockBlock".to_string(), Value::Number(serde_json::Number::from(1)));
+        extra_fields.insert("regolithTime".to_string(), Value::Number(serde_json::Number::from(2)));
+        extra_fields.insert("ecotoneTime".to_string(), Value::Number(serde_json::Number::from(3)));
+        extra_fields.insert("canyonTime".to_string(), Value::Number(serde_json::Number::from(4)));
+        let optimism_genesis = OptimismGenesis::Geth(Genesis {
+            config: ChainConfig { extra_fields, ..Default::default() },
+            ..Default::default()
+        });
 
         let chain_spec: ChainSpec = optimism_genesis.into();
 
