bb102efad64d08c1c2362bbc191a6a1aa19feac3
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-04-27 19:48:32 +0200

    Debug rlp receipt encoding

--- crates/net/downloaders/src/op_receipt_codec.rs
+++ crates/net/downloaders/src/op_receipt_codec.rs
@@ -1,12 +1,9 @@
 //! Codec for reading raw receipts from a file.
 
-use alloy_rlp::{
-    Decodable, Encodable, RlpDecodable, RlpDecodableWrapper, RlpEncodable, RlpEncodableWrapper,
-};
+use alloy_rlp::{Decodable, Encodable, RlpDecodable, RlpEncodable};
 use reth_primitives::{
     bytes::{Buf, BytesMut},
-    revm_primitives::LogData,
-    Address, Bloom, Bytes, Log, Receipt, TxType, B256,
+    Address, Bloom, Bytes, ConsensusLog, Log, Receipt, TxType, B256,
 };
 use tokio_util::codec::{Decoder, Encoder};
 
@@ -54,53 +51,16 @@ impl Encoder<Receipt> for ReceiptFileCodec {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, RlpEncodableWrapper, RlpDecodableWrapper)]
-#[rlp(trailing)]
-struct Topics(Vec<B256>);
-
-#[derive(Debug, Clone, PartialEq, Eq, RlpEncodable, RlpDecodable)]
-#[rlp(trailing)]
-struct GethLog {
-    // Consensus fields:
-    // address of the contract that generated the event
-    address: Address,
-    // list of topics provided by the contract.
-    topics: Topics,
-    // supplied by the contract, usually ABI-encoded
-    data: Bytes,
-
-    // Derived fields. These fields are filled in by the node
-    // but not secured by consensus.
-    // block in which the transaction was included
-    block_number: u64,
-    // hash of the transaction
-    transaction_hash: B256,
-    // index of the transaction in the block
-    transaction_index: u32,
-    // hash of the block in which the transaction was included
-    block_hash: B256,
-    // index of the log in the block
-    log_index: u32,
-
-    // The Removed field is true if this log was reverted due to a chain reorganisation.
-    // You must pay attention to this field if you receive logs through a filter query.
-    removed: bool,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, RlpEncodableWrapper, RlpDecodableWrapper)]
-#[rlp(trailing)]
-struct Logs(Vec<GethLog>);
-
 /// See <https://github.com/testinprod-io/op-geth/pull/1>
 #[derive(Debug, Clone, PartialEq, Eq, RlpEncodable, RlpDecodable)]
-#[rlp(trailing)]
 struct HackReceipt {
     tx_type: u8,
     post_state: Bytes,
     status: u64,
     cumulative_gas_used: u64,
     bloom: Bloom,
-    logs: Logs,
+    /// <https://github.com/testinprod-io/op-geth/blob/29062eb0fac595eeeddd3a182a25326405c66e05/core/types/log.go#L67-L72>
+    logs: Vec<ConsensusLog>,
     tx_hash: B256,
     contract_address: Address,
     gas_used: u64,
@@ -122,15 +82,8 @@ impl TryFrom<HackReceipt> for ReceiptWithBlockNumber {
         receipt.success = exported_receipt.status != 0;
         receipt.cumulative_gas_used = exported_receipt.cumulative_gas_used;
 
-        let mut logs = Vec::with_capacity(exported_receipt.logs.0.len());
-        for GethLog { address, topics, data, .. } in exported_receipt.logs.0 {
-            logs.push(Log {
-                address,
-                data: LogData::new(topics.0, data.into()).ok_or("cannot convert to log data")?,
-            })
-        }
-
-        receipt.logs = logs;
+        receipt.logs =
+            exported_receipt.logs.into_iter().map(|log| log.into()).collect::<Vec<Log>>();
 
         Ok(Self { receipt, number: exported_receipt.block_number })
     }
@@ -142,50 +95,6 @@ mod test {
     use reth_primitives::hex;
 
     use super::*;
-
-    #[test]
-    fn encoding_geth_log() {
-        let geth_log = hex!("f90197f89b948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff863a00109fc6f55cf40689f02fbaad7af7fe7bbac8a3d2186600afc7d3e10cac60271a00000000000000000000000000000000000000000000000000000000000014218a000000000000000000000000070b17c0fe982ab4a7ac17a4c25485643151a1f2da000000000000000000000000000000000000000000000000000000000618d8837f89c948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff884a092e98423f8adac6e64d0608e519fd1cefb861498385c6dee70d58fc926ddc68ca000000000000000000000000000000000000000000000000000000000d0e3ebf0a00000000000000000000000000000000000000000000000000000000000014218a000000000000000000000000070b17c0fe982ab4a7ac17a4c25485643151a1f2d80f85a948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff842a0fe25c73e3b9089fac37d55c4c7efcba6f04af04cebd2fc4d6d7dbb07e1e5234fa000000000000000000000000000000000000000000000007edc6ca0bb6834800080a05e77a04531c7c107af1882d76cbff9486d0a9aa53701c30888509d4f5f2b003a9400000000000000000000000000000000000000008303183da0bee7192e575af30420cae0c7776304ac196077ee72b048970549e4f08e8754530180018212c2821c2383312e35");
-
-        let payload = &mut &geth_log[..];
-
-        let header = Header::decode(payload).unwrap();
-
-        println!("Header: {header:?}");
-
-        let mut rlp = Rlp::new(payload).unwrap();
-
-        let address = rlp.get_next::<Address>().expect("address");
-        println!("Address: {:?}", address);
-
-        let topics = rlp.get_next::<Topics>().expect("topics");
-        println!("Topics: {:?}", topics);
-
-        let data = rlp.get_next::<Bytes>().expect("data");
-        println!("Data: {:?}", data);
-
-        let block_number = rlp.get_next::<u64>().expect("block number");
-        println!("Block Number: {:?}", block_number);
-
-        let transaction_hash = rlp.get_next::<B256>().expect("transaction hash");
-        println!("Transaction Hash: {:?}", transaction_hash);
-
-        let transaction_index = rlp.get_next::<u32>().expect("transaction index");
-        println!("Transaction Index: {:?}", transaction_index);
-
-        let block_hash = rlp.get_next::<B256>().expect("block hash");
-        println!("Block Hash: {:?}", block_hash);
-
-        let log_index = rlp.get_next::<u32>().expect("log index");
-        println!("Log Index: {:?}", log_index);
-
-        let removed = rlp.get_next::<bool>().expect("removed");
-        println!("Removed: {:?}", removed);
-
-        let header = Header::decode(payload).unwrap();
-
-        println!("Header: {header:?}");
-    }
 
     #[test]
     fn encode_decode() {
@@ -214,7 +123,7 @@ mod test {
         let bloom = rlp.get_next::<Bloom>().expect("bloom");
         println!("Bloom: {:?}", bloom);
 
-        let logs = rlp.get_next::<Logs>().expect("logs");
+        let logs = rlp.get_next::<Vec<Log>>().expect("logs");
         println!("Logs: {:?}", logs);
 
         let tx_hash = rlp.get_next::<B256>().expect("tx hash");
@@ -246,13 +155,22 @@ mod test {
 
         let fee_scalar = rlp.get_next::<String>().expect("fee scalar");
         println!("Fee Scalar: {:?}", fee_scalar);
+    }
 
-        let mut encoded = BytesMut::from(&hack_receipt[..]);
+    #[test]
+    fn receipts_coded() {
+        let hack_receipt = hex!("f9030ff9030c8080018303183db9010000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000400000000000100000000000000200000000002000000000000001000000000000000000004000000000000000000000000000040000400000100400000000000000100000000000000000000000000000020000000000000000000000000000000000000000000000001000000000000000000000100000000000000000000000000000000000000000000000000000000000000088000000080000000000010000000000000000000000000000800008000120000000000000000000000000000000002000f90197f89b948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff863a00109fc6f55cf40689f02fbaad7af7fe7bbac8a3d2186600afc7d3e10cac60271a00000000000000000000000000000000000000000000000000000000000014218a000000000000000000000000070b17c0fe982ab4a7ac17a4c25485643151a1f2da000000000000000000000000000000000000000000000000000000000618d8837f89c948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff884a092e98423f8adac6e64d0608e519fd1cefb861498385c6dee70d58fc926ddc68ca000000000000000000000000000000000000000000000000000000000d0e3ebf0a00000000000000000000000000000000000000000000000000000000000014218a000000000000000000000000070b17c0fe982ab4a7ac17a4c25485643151a1f2d80f85a948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff842a0fe25c73e3b9089fac37d55c4c7efcba6f04af04cebd2fc4d6d7dbb07e1e5234fa000000000000000000000000000000000000000000000007edc6ca0bb6834800080a05e77a04531c7c107af1882d76cbff9486d0a9aa53701c30888509d4f5f2b003a9400000000000000000000000000000000000000008303183da0bee7192e575af30420cae0c7776304ac196077ee72b048970549e4f08e8754530180018212c2821c2383312e35");
 
-        let mut codec = ReceiptFileCodec;
+        //let mut encoded = BytesMut::from(&hack_receipt[..]);
+
+        /* let mut codec = ReceiptFileCodec;
 
         let decoded_hack_receipt = codec.decode(&mut encoded).unwrap();
 
-        println!("{decoded_hack_receipt:?}")
+        println!("{decoded_hack_receipt:?}")*/
+
+        let receipt = HackReceipt::decode(&mut &hack_receipt[..]).unwrap();
+
+        println!("{receipt:?}")
     }
 }
--- crates/primitives/src/log.rs
+++ crates/primitives/src/log.rs
@@ -1,7 +1,9 @@
-use crate::Bloom;
+use crate::{Address, Bloom, Bytes, B256};
 
 /// Re-export `Log` from `alloy_primitives`.
 pub use alloy_primitives::Log;
+use alloy_rlp::{RlpDecodable, RlpEncodable};
+use reth_codecs::{main_codec, Compact};
 
 /// Calculate receipt logs bloom.
 pub fn logs_bloom<'a, It>(logs: It) -> Bloom
@@ -18,60 +20,61 @@ where
     bloom
 }
 
+/// This type is kept for compatibility tests after the codec support was added to
+/// alloy-primitives Log type natively
+///
+/// See <https://github.com/ethereum/go-ethereum/blob/4253030ef67a2af2e59bbd1fd90a4c1e75939b9f/core/types/log.go#L30-L36>
+#[main_codec(rlp)]
+#[derive(Clone, Debug, PartialEq, Eq, RlpDecodable, RlpEncodable, Default)]
+pub struct ConsensusLog {
+    /// Contract that emitted this log.
+    pub address: Address,
+    /// Topics of the log. The number of logs depend on what `LOG` opcode is used.
+    #[cfg_attr(
+        any(test, feature = "arbitrary"),
+        proptest(
+            strategy = "proptest::collection::vec(proptest::arbitrary::any::<B256>(), 0..=5)"
+        )
+    )]
+    pub topics: Vec<B256>,
+    /// Arbitrary length data.
+    pub data: Bytes,
+}
+
+impl From<Log> for ConsensusLog {
+    fn from(mut log: Log) -> Self {
+        Self {
+            address: log.address,
+            topics: std::mem::take(log.data.topics_mut_unchecked()),
+            data: log.data.data,
+        }
+    }
+}
+
+impl From<ConsensusLog> for Log {
+    fn from(log: ConsensusLog) -> Log {
+        Log::new_unchecked(log.address, log.topics, log.data)
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use alloy_primitives::{Address, Bytes, Log as AlloyLog, B256};
-    use alloy_rlp::{RlpDecodable, RlpEncodable};
     use proptest::proptest;
-    use reth_codecs::{main_codec, Compact};
 
-    /// This type is kept for compatibility tests after the codec support was added to
-    /// alloy-primitives Log type natively
-    #[main_codec(rlp)]
-    #[derive(Clone, Debug, PartialEq, Eq, RlpDecodable, RlpEncodable, Default)]
-    struct Log {
-        /// Contract that emitted this log.
-        address: Address,
-        /// Topics of the log. The number of logs depend on what `LOG` opcode is used.
-        #[cfg_attr(
-            any(test, feature = "arbitrary"),
-            proptest(
-                strategy = "proptest::collection::vec(proptest::arbitrary::any::<B256>(), 0..=5)"
-            )
-        )]
-        topics: Vec<B256>,
-        /// Arbitrary length data.
-        data: Bytes,
-    }
-
-    impl From<AlloyLog> for Log {
-        fn from(mut log: AlloyLog) -> Self {
-            Self {
-                address: log.address,
-                topics: std::mem::take(log.data.topics_mut_unchecked()),
-                data: log.data.data,
-            }
-        }
-    }
-
-    impl From<Log> for AlloyLog {
-        fn from(log: Log) -> AlloyLog {
-            AlloyLog::new_unchecked(log.address, log.topics, log.data)
-        }
-    }
+    use super::*;
 
     proptest! {
         #[test]
-        fn test_roundtrip_conversion_between_log_and_alloy_log(log: Log) {
+        fn test_roundtrip_conversion_between_log_and_alloy_log(log: ConsensusLog) {
             // Convert log to buffer and then create alloy_log from buffer and compare
             let mut compacted_log = Vec::<u8>::new();
             let len = log.clone().to_compact(&mut compacted_log);
 
-            let alloy_log = AlloyLog::from_compact(&compacted_log, len).0;
+            let alloy_log = Log::from_compact(&compacted_log, len).0;
             assert_eq!(log, alloy_log.into());
 
             // Create alloy_log from log and then convert it to buffer and compare compacted_alloy_log and compacted_log
-            let alloy_log = AlloyLog::new_unchecked(log.address, log.topics, log.data);
+            let alloy_log = Log::new_unchecked(log.address, log.topics, log.data);
             let mut compacted_alloy_log = Vec::<u8>::new();
             let alloy_len = alloy_log.to_compact(&mut compacted_alloy_log);
             assert_eq!(len, alloy_len);
