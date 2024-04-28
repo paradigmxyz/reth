1baab51529f17b0bd258809ebf14bd9e42561039
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-04-28 16:29:53 +0200

    Manually impl rlp decoding for hack receipt

--- crates/net/downloaders/src/op_receipt_codec.rs
+++ crates/net/downloaders/src/op_receipt_codec.rs
@@ -1,6 +1,6 @@
 //! Codec for reading raw receipts from a file.
 
-use alloy_rlp::{Decodable, Encodable, RlpDecodable, RlpEncodable};
+use alloy_rlp::{Decodable, Encodable, Rlp};
 use reth_primitives::{
     bytes::{Buf, BytesMut},
     Address, Bloom, Bytes, Log, Receipt, TxType, B256,
@@ -52,7 +52,7 @@ impl Encoder<Receipt> for ReceiptFileCodec {
 }
 
 /// See <https://github.com/testinprod-io/op-geth/pull/1>
-#[derive(Debug, Clone, PartialEq, Eq, RlpEncodable, RlpDecodable)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct HackReceipt {
     tx_type: u8,
     post_state: Bytes,
@@ -73,104 +73,189 @@ struct HackReceipt {
     fee_scalar: String,
 }
 
-#[allow(clippy::field_reassign_with_default)]
+#[allow(clippy::needless_update)]
 impl TryFrom<HackReceipt> for ReceiptWithBlockNumber {
     type Error = &'static str;
     fn try_from(exported_receipt: HackReceipt) -> Result<Self, Self::Error> {
-        let mut receipt = Receipt::default();
-        receipt.tx_type = TxType::try_from(exported_receipt.tx_type.to_be_bytes()[0])?;
-        receipt.success = exported_receipt.status != 0;
-        receipt.cumulative_gas_used = exported_receipt.cumulative_gas_used;
+        let HackReceipt {
+            tx_type, status, cumulative_gas_used, logs, block_number: number, ..
+        } = exported_receipt;
 
-        receipt.logs =
-            exported_receipt.logs.into_iter().map(|log| log.into()).collect::<Vec<Log>>();
+        let receipt = Receipt {
+            tx_type: TxType::try_from(tx_type.to_be_bytes()[0])?,
+            success: status != 0,
+            cumulative_gas_used,
+            logs,
+            ..Default::default()
+        };
 
-        Ok(Self { receipt, number: exported_receipt.block_number })
+        Ok(Self { receipt, number })
+    }
+}
+
+impl Decodable for HackReceipt {
+    fn decode(buf: &mut &[u8]) -> alloy_rlp::Result<Self> {
+        let buf = &mut &alloy_rlp::Header::decode_bytes(buf, true).unwrap()[..];
+
+        let mut rlp = Rlp::new(buf)?;
+
+        let tx_type = rlp.get_next::<u8>()?.ok_or(alloy_rlp::Error::Custom("missing 'tx type'"))?;
+
+        let post_state =
+            rlp.get_next::<Bytes>()?.ok_or(alloy_rlp::Error::Custom("missing 'post state'"))?;
+
+        let status = rlp.get_next::<u64>()?.ok_or(alloy_rlp::Error::Custom("missing 'status'"))?;
+
+        let cumulative_gas_used = rlp
+            .get_next::<u64>()?
+            .ok_or(alloy_rlp::Error::Custom("missing 'cumulative gas used'"))?;
+
+        let bloom = rlp.get_next::<Bloom>()?.ok_or(alloy_rlp::Error::Custom("missing 'bloom'"))?;
+
+        let logs = rlp.get_next::<Vec<Log>>()?.ok_or(alloy_rlp::Error::Custom("missing 'logs'"))?;
+
+        let tx_hash =
+            rlp.get_next::<B256>()?.ok_or(alloy_rlp::Error::Custom("missing 'tx hash'"))?;
+
+        let contract_address = rlp
+            .get_next::<Address>()?
+            .ok_or(alloy_rlp::Error::Custom("missing 'contract address'"))?;
+
+        let gas_used =
+            rlp.get_next::<u64>()?.ok_or(alloy_rlp::Error::Custom("missing 'gas used'"))?;
+
+        let block_hash =
+            rlp.get_next::<B256>()?.ok_or(alloy_rlp::Error::Custom("missing 'block hash'"))?;
+
+        let block_number =
+            rlp.get_next::<u64>()?.ok_or(alloy_rlp::Error::Custom("missing 'block number'"))?;
+
+        let transaction_index =
+            rlp.get_next::<u32>()?.ok_or(alloy_rlp::Error::Custom("missing 'tx index'"))?;
+
+        let l1_gas_price =
+            rlp.get_next::<u64>()?.ok_or(alloy_rlp::Error::Custom("missing 'l1 gas price'"))?;
+
+        let l1_gas_used =
+            rlp.get_next::<u64>()?.ok_or(alloy_rlp::Error::Custom("missing 'l1 gas used'"))?;
+
+        let l1_fee = rlp.get_next::<u64>()?.ok_or(alloy_rlp::Error::Custom("missing 'l1 fee'"))?;
+
+        let fee_scalar =
+            rlp.get_next::<String>()?.ok_or(alloy_rlp::Error::Custom("missing 'fee scalar'"))?;
+
+        Ok(HackReceipt {
+            tx_type,
+            post_state,
+            status,
+            cumulative_gas_used,
+            bloom,
+            logs,
+            tx_hash,
+            contract_address,
+            gas_used,
+            block_hash,
+            block_number,
+            transaction_index,
+            l1_gas_price,
+            l1_gas_used,
+            l1_fee,
+            fee_scalar,
+        })
     }
 }
 
 #[cfg(test)]
 mod test {
-    use alloy_rlp::{Header, Rlp};
-    use reth_primitives::hex;
+    use reth_primitives::{alloy_primitives::LogData, hex};
 
     use super::*;
 
-    #[test]
-    fn encode_decode() {
-        let hack_receipt = hex!("f9030ff9030c8080018303183db9010000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000400000000000100000000000000200000000002000000000000001000000000000000000004000000000000000000000000000040000400000100400000000000000100000000000000000000000000000020000000000000000000000000000000000000000000000001000000000000000000000100000000000000000000000000000000000000000000000000000000000000088000000080000000000010000000000000000000000000000800008000120000000000000000000000000000000002000f90197f89b948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff863a00109fc6f55cf40689f02fbaad7af7fe7bbac8a3d2186600afc7d3e10cac60271a00000000000000000000000000000000000000000000000000000000000014218a000000000000000000000000070b17c0fe982ab4a7ac17a4c25485643151a1f2da000000000000000000000000000000000000000000000000000000000618d8837f89c948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff884a092e98423f8adac6e64d0608e519fd1cefb861498385c6dee70d58fc926ddc68ca000000000000000000000000000000000000000000000000000000000d0e3ebf0a00000000000000000000000000000000000000000000000000000000000014218a000000000000000000000000070b17c0fe982ab4a7ac17a4c25485643151a1f2d80f85a948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff842a0fe25c73e3b9089fac37d55c4c7efcba6f04af04cebd2fc4d6d7dbb07e1e5234fa000000000000000000000000000000000000000000000007edc6ca0bb6834800080a05e77a04531c7c107af1882d76cbff9486d0a9aa53701c30888509d4f5f2b003a9400000000000000000000000000000000000000008303183da0bee7192e575af30420cae0c7776304ac196077ee72b048970549e4f08e8754530180018212c2821c2383312e35");
-
-        let payload = &mut &hack_receipt[..];
-
-        let header = Header::decode(payload).unwrap();
-
-        println!("Header: {header:?}");
-
-        let mut rlp = Rlp::new(payload).unwrap();
-
-        let tx_type = rlp.get_next::<u8>().expect("type");
-        println!("Tx Type: {:?}", tx_type);
-
-        let post_state = rlp.get_next::<Bytes>().expect("post state");
-        println!("Post State: {:?}", post_state);
-
-        let status = rlp.get_next::<u64>().expect("status");
-        println!("Status: {:?}", status);
-
-        let cumulative_gas_used = rlp.get_next::<u64>().expect("cum gas");
-        println!("Cumulative Gas Used: {:?}", cumulative_gas_used);
-
-        let bloom = rlp.get_next::<Bloom>().expect("bloom");
-        println!("Bloom: {:?}", bloom);
-
-        let logs = rlp.get_next::<Vec<Log>>().expect("logs");
-        println!("Logs: {:?}", logs);
-
-        let tx_hash = rlp.get_next::<B256>().expect("tx hash");
-        println!("Tx Hash: {:?}", tx_hash);
-
-        let contract_address = rlp.get_next::<Address>().expect("contract addr");
-        println!("Contract Address: {:?}", contract_address);
-
-        let gas_used = rlp.get_next::<u64>().expect("gas");
-        println!("Gas Used: {:?}", gas_used);
-
-        let block_hash = rlp.get_next::<B256>().expect("block hash");
-        println!("Block Hash: {:?}", block_hash);
-
-        let block_number = rlp.get_next::<u64>().expect("block number");
-        println!("Block Number: {:?}", block_number);
-
-        let transaction_index = rlp.get_next::<u32>().expect("tx index");
-        println!("Transaction Index: {:?}", transaction_index);
-
-        let l1_gas_price = rlp.get_next::<u64>().expect("l1 gas price");
-        println!("L1 Gas Price: {:?}", l1_gas_price);
-
-        let l1_gas_used = rlp.get_next::<u64>().expect("l1 gas used");
-        println!("L1 Gas Used: {:?}", l1_gas_used);
-
-        let l1_fee = rlp.get_next::<u64>().expect("l1 fee");
-        println!("L1 Fee: {:?}", l1_fee);
-
-        let fee_scalar = rlp.get_next::<String>().expect("fee scalar");
-        println!("Fee Scalar: {:?}", fee_scalar);
-    }
+    // RLP encoded OP mainnet `HackReceipt` of tx in block 1
+    const HACK_RECEIPT_ENCODED: [u8; 786] = hex!("f9030ff9030c8080018303183db9010000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000400000000000100000000000000200000000002000000000000001000000000000000000004000000000000000000000000000040000400000100400000000000000100000000000000000000000000000020000000000000000000000000000000000000000000000001000000000000000000000100000000000000000000000000000000000000000000000000000000000000088000000080000000000010000000000000000000000000000800008000120000000000000000000000000000000002000f90197f89b948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff863a00109fc6f55cf40689f02fbaad7af7fe7bbac8a3d2186600afc7d3e10cac60271a00000000000000000000000000000000000000000000000000000000000014218a000000000000000000000000070b17c0fe982ab4a7ac17a4c25485643151a1f2da000000000000000000000000000000000000000000000000000000000618d8837f89c948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff884a092e98423f8adac6e64d0608e519fd1cefb861498385c6dee70d58fc926ddc68ca000000000000000000000000000000000000000000000000000000000d0e3ebf0a00000000000000000000000000000000000000000000000000000000000014218a000000000000000000000000070b17c0fe982ab4a7ac17a4c25485643151a1f2d80f85a948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff842a0fe25c73e3b9089fac37d55c4c7efcba6f04af04cebd2fc4d6d7dbb07e1e5234fa000000000000000000000000000000000000000000000007edc6ca0bb6834800080a05e77a04531c7c107af1882d76cbff9486d0a9aa53701c30888509d4f5f2b003a9400000000000000000000000000000000000000008303183da0bee7192e575af30420cae0c7776304ac196077ee72b048970549e4f08e8754530180018212c2821c2383312e35");
 
     #[test]
+    #[allow(clippy::needless_update)]
     fn receipts_coded() {
-        let hack_receipt = hex!("f9030ff9030c8080018303183db9010000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000400000000000100000000000000200000000002000000000000001000000000000000000004000000000000000000000000000040000400000100400000000000000100000000000000000000000000000020000000000000000000000000000000000000000000000001000000000000000000000100000000000000000000000000000000000000000000000000000000000000088000000080000000000010000000000000000000000000000800008000120000000000000000000000000000000002000f90197f89b948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff863a00109fc6f55cf40689f02fbaad7af7fe7bbac8a3d2186600afc7d3e10cac60271a00000000000000000000000000000000000000000000000000000000000014218a000000000000000000000000070b17c0fe982ab4a7ac17a4c25485643151a1f2da000000000000000000000000000000000000000000000000000000000618d8837f89c948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff884a092e98423f8adac6e64d0608e519fd1cefb861498385c6dee70d58fc926ddc68ca000000000000000000000000000000000000000000000000000000000d0e3ebf0a00000000000000000000000000000000000000000000000000000000000014218a000000000000000000000000070b17c0fe982ab4a7ac17a4c25485643151a1f2d80f85a948ce8c13d816fe6daf12d6fd9e4952e1fc88850aff842a0fe25c73e3b9089fac37d55c4c7efcba6f04af04cebd2fc4d6d7dbb07e1e5234fa000000000000000000000000000000000000000000000007edc6ca0bb6834800080a05e77a04531c7c107af1882d76cbff9486d0a9aa53701c30888509d4f5f2b003a9400000000000000000000000000000000000000008303183da0bee7192e575af30420cae0c7776304ac196077ee72b048970549e4f08e8754530180018212c2821c2383312e35");
+        // rig
 
-        //let mut encoded = BytesMut::from(&hack_receipt[..]);
+        // OP mainnet receipt of tx in block 1
+        let decoded = ReceiptWithBlockNumber {
+            receipt: Receipt {
+                tx_type: TxType::Legacy,
+                success: true,
+                cumulative_gas_used: 202813,
+                logs: vec![
+                    Log {
+                        address: Address::from(hex!("8ce8c13d816fe6daf12d6fd9e4952e1fc88850af")),
+                        data: LogData::new(
+                            vec![
+                                B256::from(hex!(
+                                "0109fc6f55cf40689f02fbaad7af7fe7bbac8a3d2186600afc7d3e10cac60271"
+                            )),
+                                B256::from(hex!(
+                                "0000000000000000000000000000000000000000000000000000000000014218"
+                            )),
+                                B256::from(hex!(
+                                "00000000000000000000000070b17c0fe982ab4a7ac17a4c25485643151a1f2d"
+                            )),
+                            ],
+                            Bytes::from(hex!(
+                                "00000000000000000000000000000000000000000000000000000000618d8837"
+                            )),
+                        )
+                        .unwrap(),
+                    },
+                    Log {
+                        address: Address::from(hex!("8ce8c13d816fe6daf12d6fd9e4952e1fc88850af")),
+                        data: LogData::new(
+                            vec![
+                                B256::from(hex!(
+                                "92e98423f8adac6e64d0608e519fd1cefb861498385c6dee70d58fc926ddc68c"
+                            )),
+                                B256::from(hex!(
+                                "00000000000000000000000000000000000000000000000000000000d0e3ebf0"
+                            )),
+                                B256::from(hex!(
+                                "0000000000000000000000000000000000000000000000000000000000014218"
+                            )),
+                                B256::from(hex!(
+                                "00000000000000000000000070b17c0fe982ab4a7ac17a4c25485643151a1f2d"
+                            )),
+                            ],
+                            Bytes::default(),
+                        )
+                        .unwrap(),
+                    },
+                    Log {
+                        address: Address::from(hex!("8ce8c13d816fe6daf12d6fd9e4952e1fc88850af")),
+                        data: LogData::new(
+                            vec![
+                                B256::from(hex!(
+                                "fe25c73e3b9089fac37d55c4c7efcba6f04af04cebd2fc4d6d7dbb07e1e5234f"
+                            )),
+                                B256::from(hex!(
+                                "00000000000000000000000000000000000000000000007edc6ca0bb68348000"
+                            )),
+                            ],
+                            Bytes::default(),
+                        )
+                        .unwrap(),
+                    },
+                ],
+                ..Default::default()
+            },
+            number: 1,
+        };
 
-        /* let mut codec = ReceiptFileCodec;
+        let mut encoded = BytesMut::from(&HACK_RECEIPT_ENCODED[..]);
 
-        let decoded_hack_receipt = codec.decode(&mut encoded).unwrap();
+        let mut codec = ReceiptFileCodec;
 
-        println!("{decoded_hack_receipt:?}")*/
+        // test
 
-        let receipt = HackReceipt::decode(&mut &hack_receipt[..]).unwrap();
+        let decoded_hack_receipt = codec.decode(&mut encoded).unwrap().unwrap();
 
-        println!("{receipt:?}")
+        assert_eq!(decoded, decoded_hack_receipt);
     }
 }
