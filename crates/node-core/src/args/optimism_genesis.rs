7fd4a22a06269512b8079f24de4f0d5f73458b9b
Federico Gimenez <federico.gimenez@gmail.com>
2024-04-29 11:00:40 +0200

    constants for field names

--- crates/node-core/src/args/optimism_genesis.rs
+++ crates/node-core/src/args/optimism_genesis.rs
@@ -2,6 +2,11 @@
 use reth_primitives::{AllGenesisFormats, ChainSpec, ForkCondition, Hardfork};
 use serde::Deserialize;
 
+const BEDROCK_BLOCK_FIELD_NAME: &str = "bedrockBlock";
+const REGOLITH_TIME_FIELD_NAME: &str = "regolithTime";
+const ECOTONE_TIME_FIELD_NAME: &str = "ecotoneTime";
+const CANYON_TIME_FIELD_NAME: &str = "canyonTime";
+
 /// Genesis type for Optimism networks.
 #[derive(Debug, Deserialize)]
 pub(crate) struct OptimismGenesis(AllGenesisFormats);
@@ -12,27 +17,36 @@ impl From<OptimismGenesis> for ChainSpec {
             AllGenesisFormats::Reth(chain_spec) => chain_spec,
             AllGenesisFormats::Geth(genesis) => {
                 let mut chain_spec: ChainSpec = genesis.clone().into();
-                if let Some(block) = genesis.config.extra_fields.get("bedrockBlock") {
-                    chain_spec
-                        .hardforks
-                        .insert(Hardfork::Bedrock, ForkCondition::Block(block.as_u64().unwrap()));
+                if let Some(block) = genesis.config.extra_fields.get(BEDROCK_BLOCK_FIELD_NAME) {
+                    chain_spec.hardforks.insert(
+                        Hardfork::Bedrock,
+                        ForkCondition::Block(block.as_u64().unwrap_or_else(|| {
+                            panic!("{BEDROCK_BLOCK_FIELD_NAME} should be a number")
+                        })),
+                    );
                 }
-                if let Some(timestamp) = genesis.config.extra_fields.get("regolithTime") {
+                if let Some(timestamp) = genesis.config.extra_fields.get(REGOLITH_TIME_FIELD_NAME) {
                     chain_spec.hardforks.insert(
                         Hardfork::Regolith,
-                        ForkCondition::Timestamp(timestamp.as_u64().unwrap()),
+                        ForkCondition::Timestamp(timestamp.as_u64().unwrap_or_else(|| {
+                            panic!("{REGOLITH_TIME_FIELD_NAME} should be a number")
+                        })),
                     );
                 }
-                if let Some(timestamp) = genesis.config.extra_fields.get("ecotoneTime") {
+                if let Some(timestamp) = genesis.config.extra_fields.get(ECOTONE_TIME_FIELD_NAME) {
                     chain_spec.hardforks.insert(
                         Hardfork::Ecotone,
-                        ForkCondition::Timestamp(timestamp.as_u64().unwrap()),
+                        ForkCondition::Timestamp(timestamp.as_u64().unwrap_or_else(|| {
+                            panic!("{ECOTONE_TIME_FIELD_NAME} should be a number")
+                        })),
                     );
                 }
-                if let Some(timestamp) = genesis.config.extra_fields.get("canyonTime") {
+                if let Some(timestamp) = genesis.config.extra_fields.get(CANYON_TIME_FIELD_NAME) {
                     chain_spec.hardforks.insert(
                         Hardfork::Canyon,
-                        ForkCondition::Timestamp(timestamp.as_u64().unwrap()),
+                        ForkCondition::Timestamp(timestamp.as_u64().unwrap_or_else(|| {
+                            panic!("{CANYON_TIME_FIELD_NAME} should be a number")
+                        })),
                     );
                 }
                 chain_spec
@@ -51,24 +65,30 @@ mod tests {
 
     #[test]
     fn parse_genesis() {
-        let genesis = r#"
-    {
-      "nonce": 9,
-      "config": {
-        "chainId": 1,
-        "bedrockBlock": 10,
-        "regolithTime": 20,
-        "ecotoneTime": 30,
-        "canyonTime": 40,
-        "optimism": {
-          "eip1559Elasticity": 50,
-          "eip1559Denominator": 60,
-          "eip1559DenominatorCanyon": 70
-        }
-      }
-    }
-    "#;
-        let optimism_genesis: OptimismGenesis = serde_json::from_str(genesis).unwrap();
+        let genesis = format!(
+            "
+    {{
+      \"nonce\": 9,
+      \"config\": {{
+        \"chainId\": 1,
+        \"{bedrockBlock}\": 10,
+        \"{regolithTime}\": 20,
+        \"{ecotoneTime}\": 30,
+        \"{canyonTime}\": 40,
+        \"optimism\": {{
+          \"eip1559Elasticity\": 50,
+          \"eip1559Denominator\": 60,
+          \"eip1559DenominatorCanyon\": 70
+        }}
+      }}
+    }}
+    ",
+            bedrockBlock = BEDROCK_BLOCK_FIELD_NAME,
+            regolithTime = REGOLITH_TIME_FIELD_NAME,
+            ecotoneTime = ECOTONE_TIME_FIELD_NAME,
+            canyonTime = CANYON_TIME_FIELD_NAME,
+        );
+        let optimism_genesis: OptimismGenesis = serde_json::from_str(&genesis).unwrap();
 
         if let AllGenesisFormats::Geth(genesis) = optimism_genesis.0 {
             let actual_nonce = genesis.nonce;
@@ -76,13 +96,14 @@ mod tests {
             let actual_chain_id = genesis.config.chain_id;
             assert_eq!(actual_chain_id, 1);
 
-            let actual_bedrock_block = genesis.config.extra_fields.get("bedrockBlock");
+            let actual_bedrock_block = genesis.config.extra_fields.get(BEDROCK_BLOCK_FIELD_NAME);
             assert_eq!(actual_bedrock_block, Some(Value::from(10)).as_ref());
-            let actual_regolith_timestamp = genesis.config.extra_fields.get("regolithTime");
+            let actual_regolith_timestamp =
+                genesis.config.extra_fields.get(REGOLITH_TIME_FIELD_NAME);
             assert_eq!(actual_regolith_timestamp, Some(Value::from(20)).as_ref());
-            let actual_ecotone_timestamp = genesis.config.extra_fields.get("ecotoneTime");
+            let actual_ecotone_timestamp = genesis.config.extra_fields.get(ECOTONE_TIME_FIELD_NAME);
             assert_eq!(actual_ecotone_timestamp, Some(Value::from(30)).as_ref());
-            let actual_canyon_timestamp = genesis.config.extra_fields.get("canyonTime");
+            let actual_canyon_timestamp = genesis.config.extra_fields.get(CANYON_TIME_FIELD_NAME);
             assert_eq!(actual_canyon_timestamp, Some(Value::from(40)).as_ref());
 
             let optimism_object = genesis.config.extra_fields.get("optimism").unwrap();
@@ -102,10 +123,20 @@ mod tests {
     #[test]
     fn optimism_genesis_into_chainspec() {
         let mut extra_fields: BTreeMap<String, Value> = BTreeMap::new();
-        extra_fields.insert("bedrockBlock".to_string(), Value::Number(serde_json::Number::from(1)));
-        extra_fields.insert("regolithTime".to_string(), Value::Number(serde_json::Number::from(2)));
-        extra_fields.insert("ecotoneTime".to_string(), Value::Number(serde_json::Number::from(3)));
-        extra_fields.insert("canyonTime".to_string(), Value::Number(serde_json::Number::from(4)));
+        extra_fields.insert(
+            BEDROCK_BLOCK_FIELD_NAME.to_string(),
+            Value::Number(serde_json::Number::from(1)),
+        );
+        extra_fields.insert(
+            REGOLITH_TIME_FIELD_NAME.to_string(),
+            Value::Number(serde_json::Number::from(2)),
+        );
+        extra_fields.insert(
+            ECOTONE_TIME_FIELD_NAME.to_string(),
+            Value::Number(serde_json::Number::from(3)),
+        );
+        extra_fields
+            .insert(CANYON_TIME_FIELD_NAME.to_string(), Value::Number(serde_json::Number::from(4)));
         let optimism_genesis = OptimismGenesis(AllGenesisFormats::Geth(Genesis {
             config: ChainConfig { extra_fields, ..Default::default() },
             ..Default::default()
