eb609a642097acc83768aa291e183ea94a072b44
lakshya-sky <8559992+lakshya-sky@users.noreply.github.com>
2024-05-02 11:33:38 -0400

    integrate FullNodeComponents in exexcomponents

--- crates/exex/src/context.rs
+++ crates/exex/src/context.rs
@@ -1,4 +1,4 @@
-use reth_node_api::FullNodeComponents;
+use reth_node_api::{FullNodeComponents, FullNodeTypes, NodeTypes};
 use reth_node_core::{
     dirs::{ChainPath, DataDirPath},
     node_config::NodeConfig,
@@ -14,18 +14,12 @@ use crate::{ExExEvent, ExExNotification};
 pub struct ExExContext<Node: FullNodeComponents> {
     /// The current head of the blockchain at launch.
     pub head: Head,
-    /// The configured provider to interact with the blockchain.
-    pub provider: Node::Provider,
-    /// The task executor of the node.
-    pub task_executor: TaskExecutor,
     /// The data dir of the node.
     pub data_dir: ChainPath<DataDirPath>,
     /// The config of the node
     pub config: NodeConfig,
     /// The loaded node config
     pub reth_config: reth_config::Config,
-    /// The transaction pool of the node.
-    pub pool: Node::Pool,
     /// Channel used to send [`ExExEvent`]s to the rest of the node.
     ///
     /// # Important
@@ -41,4 +35,46 @@ pub struct ExExContext<Node: FullNodeComponents> {
     /// Once a an [`ExExNotification`] is sent over the channel, it is considered delivered by the
     /// node.
     pub notifications: Receiver<ExExNotification>,
+
+    /// node components
+    pub components: Node,
+}
+
+impl<Node: FullNodeComponents> NodeTypes for ExExContext<Node> {
+    type Primitives = Node::Primitives;
+    type Engine = Node::Engine;
+    type Evm = Node::Evm;
+
+    fn evm_config(&self) -> Self::Evm {
+        self.components.evm_config()
+    }
+}
+
+impl<Node: FullNodeComponents> FullNodeTypes for ExExContext<Node> {
+    type DB = Node::DB;
+    type Provider = Node::Provider;
+}
+
+impl<Node: FullNodeComponents> FullNodeComponents for ExExContext<Node> {
+    type Pool = Node::Pool;
+
+    fn pool(&self) -> &Self::Pool {
+        self.components.pool()
+    }
+
+    fn provider(&self) -> &Self::Provider {
+        self.components.provider()
+    }
+
+    fn network(&self) -> &reth_network::NetworkHandle {
+        self.components.network()
+    }
+
+    fn payload_builder(&self) -> &reth_payload_builder::PayloadBuilderHandle<Self::Engine> {
+        self.components.payload_builder()
+    }
+
+    fn task_executor(&self) -> &TaskExecutor {
+        self.components.task_executor()
+    }
 }
