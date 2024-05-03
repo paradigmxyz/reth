9a2c745643adf365d0d63a6ed65d58976b2816ed
Matthias Seitz <matthias.seitz@outlook.de>
2024-05-03 17:01:06 +0200

    chore: move dao hardfork constants to evm-ethereum

--- crates/ethereum/evm/src/execute.rs
+++ crates/ethereum/evm/src/execute.rs
@@ -1,6 +1,10 @@
 //! Ethereum block executor.
 
-use crate::{verify::verify_receipts, EthEvmConfig};
+use crate::{
+    eth_dao_fork::{DAO_HARDFORK_BENEFICIARY, DAO_HARDKFORK_ACCOUNTS},
+    verify::verify_receipts,
+    EthEvmConfig,
+};
 use reth_evm::{
     execute::{
         BatchBlockExecutionOutput, BatchExecutor, BlockExecutionInput, BlockExecutionOutput,
@@ -19,7 +23,6 @@ use reth_primitives::{
 use reth_revm::{
     batch::{BlockBatchRecord, BlockExecutorStats},
     db::states::bundle_state::BundleRetention,
-    eth_dao_fork::{DAO_HARDFORK_BENEFICIARY, DAO_HARDKFORK_ACCOUNTS},
     state_change::{apply_beacon_root_contract_call, post_block_balance_increments},
     Evm, State,
 };
