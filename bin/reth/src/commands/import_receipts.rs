81266da0d540b9e99df33c58c241957618d51918
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-05-06 18:04:13 +0200

    fixup! Revert import to start at block 1

--- bin/reth/src/commands/import_receipts.rs
+++ bin/reth/src/commands/import_receipts.rs
@@ -116,21 +116,6 @@ impl ImportReceiptsCommand {
                 "Importing receipt file chunk"
             );
 
-            // It is possible for the first receipt returned by the file client to be block #1
-            // rather than the genesis block. In this case, we just prepend empty receipts to the
-            // current list of receipts. When initially writing to static files, the provider
-            // expects the first block to be block zero. So, if the first block returned by the file
-            // client is block #1, we insert empty receipts for the genesis block.
-            /*if first_block == 1 {
-                // prepend the first empty receipts
-                receipts.insert(0, vec![]);
-                // this ensures the bundle state and static file producer start at zero
-                first_block = 0;
-                // we count this as decoded so the partial import check later does not error if this
-                // branch is executed
-                total_decoded_receipts += 1;
-            }*/
-
             // We're reusing receipt writing code internal to
             // `BundleStateWithReceipts::write_to_storage`, so we just use a default empty
             // `BundleState`.
