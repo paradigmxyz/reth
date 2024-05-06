0dd1825edcc533503c99d32c21b484723241efd5
joshieDo <ranriver@protonmail.com>
2024-05-06 20:18:09 +0100

    add more docs on PipelineTarget

--- crates/primitives/src/stage/mod.rs
+++ crates/primitives/src/stage/mod.rs
@@ -11,24 +11,35 @@ pub use checkpoints::{
     StageUnitCheckpoint, StorageHashingCheckpoint,
 };
 
-/// Direction and target for pipeline operations.
+/// Direction and target block for pipeline operations.
 #[derive(Debug, Clone, Copy)]
 pub enum PipelineTarget {
-    /// Target for forward synchronization.
+    /// Target for forward synchronization, indicating a block hash to sync to.
     Sync(BlockHash),
-    /// Target for backward unwinding.
+    /// Target for backward unwinding, indicating a block number to unwind to.
     Unwind(BlockNumber),
 }
 
 impl PipelineTarget {
-    /// Target for forward synchronization.
+    /// Returns the target block hash for forward synchronization, if applicable.
+    ///
+    /// # Returns
+    ///
+    /// - `Some(BlockHash)`: The target block hash for forward synchronization.
+    /// - `None`: If the target is for backward unwinding.
     pub fn sync_target(self) -> Option<BlockHash> {
         match self {
             PipelineTarget::Sync(hash) => Some(hash),
             PipelineTarget::Unwind(_) => None,
         }
     }
-    /// Target for backward unwinding.
+
+    /// Returns the target block number for backward unwinding, if applicable.
+    ///
+    /// # Returns
+    ///
+    /// - `Some(BlockNumber)`: The target block number for backward unwinding.
+    /// - `None`: If the target is for forward synchronization.
     pub fn unwind_target(self) -> Option<BlockNumber> {
         match self {
             PipelineTarget::Sync(_) => None,
