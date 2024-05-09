684a83ca360540dc2f76751f555049c1524cd8cf
Federico Gimenez <federico.gimenez@gmail.com>
2024-05-09 18:41:58 +0200

    feat: use broadcast channel for event listeners

--- crates/tokio-util/src/event_listeners.rs
+++ crates/tokio-util/src/event_listeners.rs
@@ -1,46 +1,51 @@
-use tokio::sync::mpsc;
-use tokio_stream::wrappers::UnboundedReceiverStream;
+use tokio::sync::broadcast::{self, error::SendError, Sender};
+use tokio_stream::wrappers::BroadcastStream;
 
-/// A collection of event listeners for a task.
+/// A bounded broadcast channel for a task.
 #[derive(Clone, Debug)]
 pub struct EventListeners<T> {
-    /// All listeners for events
-    listeners: Vec<mpsc::UnboundedSender<T>>,
+    /// The sender part of the broadcast channel
+    sender: broadcast::Sender<T>,
+    /// The number of subscribers, needed because the broadcast sender doesn't track this
+    subscriber_count: usize,
 }
 
-impl<T> Default for EventListeners<T> {
+impl<T: Clone + Send + Sync + 'static> Default for EventListeners<T> {
     fn default() -> Self {
-        Self { listeners: Vec::new() }
+        Self::new()
     }
 }
 
-impl<T: Clone> EventListeners<T> {
-    /// Send an event to all listeners.
-    ///
-    /// Channels that were closed are removed.
-    pub fn notify(&mut self, event: T) {
-        self.listeners.retain(|listener| listener.send(event.clone()).is_ok())
+impl<T: Clone + Send + Sync + 'static> EventListeners<T> {
+    /// Creates a new `EventListeners`.
+    pub fn new() -> Self {
+        let (sender, _) = broadcast::channel(100);
+        Self { sender, subscriber_count: 0 }
     }
 
-    /// Add a new event listener.
-    pub fn new_listener(&mut self) -> UnboundedReceiverStream<T> {
-        let (sender, receiver) = mpsc::unbounded_channel();
-        self.listeners.push(sender);
-        UnboundedReceiverStream::new(receiver)
+    /// Sends an event to all listeners. Returns the number of subscribers the event was sent to.
+    pub fn notify(&self, event: T) -> Result<usize, SendError<T>> {
+        self.sender.send(event)
     }
 
-    /// Push new event listener.
-    pub fn push_listener(&mut self, listener: mpsc::UnboundedSender<T>) {
-        self.listeners.push(listener);
+    /// Sender cloner.
+    pub fn clone_sender(&self) -> Sender<T> {
+        self.sender.clone()
+    }
+
+    /// Adds a new event listener and returns the associated receiver.
+    pub fn new_listener(&mut self) -> BroadcastStream<T> {
+        self.subscriber_count += 1;
+        BroadcastStream::new(self.sender.subscribe())
     }
 
     /// Returns the number of registered listeners.
     pub fn len(&self) -> usize {
-        self.listeners.len()
+        self.subscriber_count
     }
 
     /// Returns true if there are no registered listeners.
     pub fn is_empty(&self) -> bool {
-        self.listeners.is_empty()
+        self.subscriber_count == 0
     }
 }
