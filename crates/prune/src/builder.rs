ad54af8406886367fec407750b678c421d39be96
joshieDo <93316087+joshieDo@users.noreply.github.com>
2024-05-09 19:24:57 +0100

    fix:   copy and prune data from database with `move_to_static_files`, before a pipeline run/unwind (#8127)

--- crates/prune/src/builder.rs
+++ crates/prune/src/builder.rs
@@ -1,10 +1,9 @@
-use std::time::Duration;
-
 use crate::{segments::SegmentSet, Pruner};
 use reth_config::PruneConfig;
 use reth_db::database::Database;
 use reth_primitives::{FinishedExExHeight, PruneModes, MAINNET};
 use reth_provider::ProviderFactory;
+use std::time::Duration;
 use tokio::sync::watch;
 
 /// Contains the information required to build a pruner
