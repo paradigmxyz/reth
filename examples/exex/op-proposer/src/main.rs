0c0b29361bea410862e76af40f3238af04f9398a
0xKitsune <0xKitsune@protonmail.com>
2024-05-09 17:18:44 -0400

    updated exex framework

--- examples/exex/op-proposer/src/main.rs
+++ examples/exex/op-proposer/src/main.rs
@@ -1,10 +1,76 @@
+use futures::Future;
+use reth_exex::{ExExContext, ExExNotification};
+use reth_node_api::FullNodeComponents;
 use reth_node_ethereum::EthereumNode;
+use reth_tracing::tracing::info;
+
+async fn init_exex<Node: FullNodeComponents>(
+    ctx: ExExContext<Node>,
+) -> eyre::Result<impl Future<Output = eyre::Result<()>>> {
+    Ok(op_proposer_exex(ctx))
+}
+
+async fn op_proposer_exex<Node: FullNodeComponents>(
+    mut ctx: ExExContext<Node>,
+) -> eyre::Result<()> {
+    while let Some(notification) = ctx.notifications.recv().await {
+        match &notification {
+            ExExNotification::ChainCommitted { new } => {
+                info!(committed_chain = ?new.range(), "Received commit");
+            }
+            ExExNotification::ChainReorged { old, new } => {
+                info!(from_chain = ?old.range(), to_chain = ?new.range(), "Received reorg");
+            }
+            ExExNotification::ChainReverted { old } => {
+                info!(reverted_chain = ?old.range(), "Received revert");
+            }
+        };
+    }
+    Ok(())
+}
+
+struct OpProposer {
+    //TODO: persistent storage configuration
+}
+
+impl OpProposer {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn spawn<Node: FullNodeComponents>(
+        &self,
+        mut ctx: ExExContext<Node>,
+    ) -> eyre::Result<impl Future<Output = eyre::Result<()>>> {
+        //TODO: initialization logic
+
+        let fut = async move {
+            while let Some(notification) = ctx.notifications.recv().await {
+                match &notification {
+                    ExExNotification::ChainCommitted { new } => {
+                        info!(committed_chain = ?new.range(), "Received commit");
+                    }
+                    ExExNotification::ChainReorged { old, new } => {
+                        info!(from_chain = ?old.range(), to_chain = ?new.range(), "Received reorg");
+                    }
+                    ExExNotification::ChainReverted { old } => {
+                        info!(reverted_chain = ?old.range(), "Received revert");
+                    }
+                };
+            }
+
+            Ok(())
+        };
+
+        Ok(fut)
+    }
+}
 
 fn main() -> eyre::Result<()> {
     reth::cli::Cli::parse_args().run(|builder, _| async move {
         let handle = builder
             .node(EthereumNode::default())
-            // .install_exex("OpProposer", |ctx| async move { todo!("implement here") })
+            .install_exex("OpProposer", |ctx| async move { OpProposer::new().spawn(ctx) })
             .launch()
             .await?;
 
