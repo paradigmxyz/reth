e7ac4b1c2631af22a4b1a26e09e1ae7ab7347dec
Federico Gimenez <federico.gimenez@gmail.com>
2024-05-10 18:26:10 +0200

    EventNotifier moved to tokio-util

--- crates/tokio-util/src/event_listeners.rs
+++ crates/tokio-util/src/event_listeners.rs
@@ -1,6 +1,7 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 use tokio::sync::broadcast::{self, error::SendError, Sender};
 use tokio_stream::wrappers::BroadcastStream;
+use tracing::{error, warn};
 
 /// A bounded broadcast channel for a task.
 #[derive(Debug)]
@@ -57,5 +58,36 @@ impl<T: Clone + Send + Sync + 'static> EventListeners<T> {
     /// Returns true if there are no registered listeners.
     pub fn is_empty(&self) -> bool {
         self.len() == 0
+    }
+}
+
+/// Notifies events to the listeners subscribed to the wrapped broadcast sender.
+#[derive(Debug)]
+pub struct EventNotifier<T> {
+    sender: Option<Sender<T>>,
+}
+
+impl<T: Send + Sync + 'static> Default for EventNotifier<T> {
+    fn default() -> Self {
+        Self { sender: None }
+    }
+}
+
+impl<T: Send + Sync + 'static> EventNotifier<T> {
+    /// Broadcast sender setter.
+    pub fn set_sender(&mut self, sender: Sender<T>) {
+        self.sender = Some(sender);
+    }
+
+    /// Sends an event to all listeners. Returns the number of subscribers the event was sent to.
+    pub fn notify(&self, event: T) {
+        match self.sender.as_ref().map(|sender| sender.send(event)).unwrap_or(Ok(0)) {
+            Ok(listener_count) => {
+                if listener_count == 0 {
+                    warn!("notification of network event with 0 listeners");
+                }
+            }
+            Err(e) => error!("channel closed: {e}"),
+        };
     }
 }
