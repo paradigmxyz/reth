4c625c78fe26e0da434e5ae7f42f9f1f0bd02e8d
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-05-13 18:36:38 +0300

    feat(payload): include EIP-6110 deposit requests (#8229)

--- crates/payload/ethereum/src/lib.rs
+++ crates/payload/ethereum/src/lib.rs
@@ -14,6 +14,8 @@ use reth_basic_payload_builder::{
     pre_block_beacon_root_contract_call, BuildArguments, BuildOutcome, PayloadBuilder,
     PayloadConfig, WithdrawalsOutcome,
 };
+use reth_evm_ethereum::eip6110::parse_deposits_from_receipts;
+use reth_interfaces::RethError;
 use reth_payload_builder::{
     error::PayloadBuilderError, EthBuiltPayload, EthPayloadBuilderAttributes,
 };
@@ -389,6 +391,9 @@ where
     // calculate the requests and the requests root
     let (requests, requests_root) =
         if chain_spec.is_prague_active_at_timestamp(attributes.timestamp) {
+            let deposit_requests = parse_deposits_from_receipts(receipts.iter().flatten())
+                .map_err(|err| PayloadBuilderError::Internal(RethError::Execution(err.into())))?;
+
             let withdrawal_requests = post_block_withdrawal_requests_contract_call(
                 &mut db,
                 &chain_spec,
@@ -396,9 +401,8 @@ where
                 &initialized_block_env,
                 &attributes,
             )?;
-            // TODO: add deposit requests (https://github.com/paradigmxyz/reth/pull/8204)
 
-            let requests = withdrawal_requests;
+            let requests = [deposit_requests, withdrawal_requests].concat();
             let requests_root = calculate_requests_root(&requests);
             (Some(requests.into()), Some(requests_root))
         } else {
