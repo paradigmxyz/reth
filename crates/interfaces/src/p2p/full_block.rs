25243033fe32f0cf6f5ed9d20c05ec1ec8b4fdfb
Oliver Nordbjerg <hi@notbjerg.me>
2024-05-13 16:26:48 +0300

    feat: validate headers/bodies for tree & downloader

--- crates/interfaces/src/p2p/full_block.rs
+++ crates/interfaces/src/p2p/full_block.rs
@@ -313,22 +313,36 @@ fn ensure_valid_body_response(
         ))
     }
 
-    let withdrawals = match &block.withdrawals {
-        Some(withdrawals) => withdrawals.as_slice(),
-        None => &[][..],
-    };
-    if let Some(header_withdrawals_root) = header.withdrawals_root {
-        let withdrawals_root = reth_primitives::proofs::calculate_withdrawals_root(withdrawals);
-        if withdrawals_root != header_withdrawals_root {
-            return Err(ConsensusError::BodyWithdrawalsRootDiff(
-                GotExpected { got: withdrawals_root, expected: header_withdrawals_root }.into(),
-            ))
+    match (header.withdrawals_root, &block.withdrawals) {
+        (Some(header_withdrawals_root), Some(withdrawals)) => {
+            let withdrawals = withdrawals.as_slice();
+            let withdrawals_root = reth_primitives::proofs::calculate_withdrawals_root(withdrawals);
+            if withdrawals_root != header_withdrawals_root {
+                return Err(ConsensusError::BodyWithdrawalsRootDiff(
+                    GotExpected { got: withdrawals_root, expected: header_withdrawals_root }.into(),
+                ))
+            }
         }
-        return Ok(())
+        (None, None) => {
+            // this is ok because we assume the fork is not active in this case
+        }
+        _ => return Err(ConsensusError::WithdrawalsRootUnexpected),
     }
 
-    if !withdrawals.is_empty() {
-        return Err(ConsensusError::WithdrawalsRootUnexpected)
+    match (header.requests_root, &block.requests) {
+        (Some(header_requests_root), Some(requests)) => {
+            let requests = requests.0.as_slice();
+            let requests_root = reth_primitives::proofs::calculate_requests_root(requests);
+            if requests_root != header_requests_root {
+                return Err(ConsensusError::BodyRequestsRootDiff(
+                    GotExpected { got: requests_root, expected: header_requests_root }.into(),
+                ))
+            }
+        }
+        (None, None) => {
+            // this is ok because we assume the fork is not active in this case
+        }
+        _ => return Err(ConsensusError::RequestsRootUnexpected),
     }
 
     Ok(())
