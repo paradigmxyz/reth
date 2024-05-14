4ece0deed910ba7c79d9ed8c83e326ff2a02c4aa
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-05-14 16:39:01 +0300

    fix(evm): set the bytecode of EIP-2935 account (#8254)

--- crates/ethereum/evm/src/execute.rs
+++ crates/ethereum/evm/src/execute.rs
@@ -185,16 +185,6 @@ where
         }
         drop(evm);
 
-        // Check if gas used matches the value set in header.
-        if block.gas_used != cumulative_gas_used {
-            let receipts = Receipts::from_block_receipt(receipts);
-            return Err(BlockValidationError::BlockGasUsed {
-                gas: GotExpected { got: cumulative_gas_used, expected: block.gas_used },
-                gas_spent_by_tx: receipts.gas_spent_by_tx()?,
-            }
-            .into())
-        }
-
         Ok((receipts, cumulative_gas_used))
     }
 }
@@ -277,21 +267,6 @@ where
 
         // 3. apply post execution changes
         self.post_execution(block, total_difficulty)?;
-
-        // Before Byzantium, receipts contained state root that would mean that expensive
-        // operation as hashing that is required for state root got calculated in every
-        // transaction This was replaced with is_success flag.
-        // See more about EIP here: https://eips.ethereum.org/EIPS/eip-658
-        if self.chain_spec().is_byzantium_active_at_block(block.header.number) {
-            if let Err(error) = verify_receipts(
-                block.header.receipts_root,
-                block.header.logs_bloom,
-                receipts.iter(),
-            ) {
-                debug!(target: "evm", %error, ?receipts, "receipts verification failed");
-                return Err(error)
-            };
-        }
 
         Ok((receipts, gas_used))
     }
