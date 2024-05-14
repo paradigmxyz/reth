7500152ce2a6448191da74f3b53c028cb4d6d237
Oliver Nordbjerg <hi@notbjerg.me>
2024-05-15 01:47:56 +0300

    rm imports

--- crates/primitives/src/request.rs
+++ crates/primitives/src/request.rs
@@ -2,8 +2,7 @@
 
 use alloy_consensus::Request;
 use alloy_eips::eip7685::{Decodable7685, Encodable7685};
-use alloy_rlp::{encode_iter, encode_list, Decodable, Encodable};
-use bytes::BufMut;
+use alloy_rlp::{Decodable, Encodable};
 use reth_codecs::{main_codec, Compact};
 use revm_primitives::Bytes;
 
