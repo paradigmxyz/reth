425bb096aa304780707acac042baf722021b401b
Federico Gimenez <federico.gimenez@gmail.com>
2024-05-16 10:37:03 +0200

    fix: Chain::split returns NoSplitPending for block number > tip

--- crates/storage/provider/src/chain.rs
+++ crates/storage/provider/src/chain.rs
@@ -289,7 +289,10 @@ impl Chain {
                 block_number
             }
             ChainSplitTarget::Number(block_number) => {
-                if block_number >= chain_tip {
+                if block_number > chain_tip {
+                    return ChainSplit::NoSplitPending(self)
+                }
+                if block_number == chain_tip {
                     return ChainSplit::NoSplitCanonical(self)
                 }
                 if block_number < *self.blocks.first_entry().expect("chain is never empty").key() {
