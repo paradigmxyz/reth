248d6fedd17003048a376988f5bc8f300302b05b
Matthias Seitz <matthias.seitz@outlook.de>
2024-05-22 14:37:46 +0200

    chore: remove p2p traits from interfaces

---
+++ crates/net/p2p/src/lib.rs
@@ -0,0 +1,7 @@
+//! Provides abstractions and commonly used types for p2p.
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
+    html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
+    issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
+)]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
