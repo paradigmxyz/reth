2b231a3e3602424a28c59e540a752a43697da95e
Matthias Seitz <matthias.seitz@outlook.de>
2024-05-23 13:24:20 +0200

    chore: clippy happy (#8362)
    Co-authored-by: Alexey Shekhirin <a.shekhirin@gmail.com>

--- crates/blockchain-tree/src/blockchain_tree.rs
+++ crates/blockchain-tree/src/blockchain_tree.rs
@@ -307,7 +307,7 @@ where
                     *key_value.0
                 } else {
                     debug!(target: "blockchain_tree", ?chain_id, "No blockhashes stored");
-                    return None;
+                    return None
                 };
             let canonical_chain = canonical_chain
                 .iter()
