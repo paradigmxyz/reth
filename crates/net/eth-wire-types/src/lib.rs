d8292921fbcce4c9971ba1c15909b498301b5df3
Matthias Seitz <matthias.seitz@outlook.de>
2024-05-24 10:08:49 +0200

    chore: remove wire-types optimism feature

--- crates/net/eth-wire-types/src/lib.rs
+++ crates/net/eth-wire-types/src/lib.rs
@@ -8,7 +8,9 @@
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 // TODO: remove when https://github.com/proptest-rs/proptest/pull/427 is merged
 #![allow(unknown_lints, non_local_definitions)]
+#![allow(clippy::needless_lifetimes)] // side effect of optimism fields
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+
 
 mod status;
 pub use status::{Status, StatusBuilder};
