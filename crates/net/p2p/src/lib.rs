2c9ad90b931c6edd8fda1d8abc7eb69bd8be25fd
Matthias Seitz <matthias.seitz@outlook.de>
2024-05-24 10:16:36 +0200

    update docs

--- crates/net/p2p/src/lib.rs
+++ crates/net/p2p/src/lib.rs
@@ -1,4 +1,8 @@
 //! Provides abstractions and commonly used types for p2p.
+//!
+//! ## Feature Flags
+//!
+//! - `test-utils`: Export utilities for testing
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
     html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
