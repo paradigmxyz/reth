4dcfd462a43a8be6ca1868fc19858cbb0ea824f6
Matthias Seitz <matthias.seitz@outlook.de>
2024-05-24 10:04:58 +0200

    misc

--- crates/net/p2p/src/full_block.rs
+++ crates/net/p2p/src/full_block.rs
@@ -727,11 +727,10 @@ enum RangeResponseResult {
 
 #[cfg(test)]
 mod tests {
-    use std::ops::Range;
-
     use super::*;
     use crate::test_utils::TestFullBlockClient;
     use futures::StreamExt;
+    use std::ops::Range;
 
     #[tokio::test]
     async fn download_single_full_block() {
