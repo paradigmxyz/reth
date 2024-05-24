095e64d07e921dc14a8591752b42378461eb9e69
Roman Krasiuk <rokrassyuk@gmail.com>
2024-05-24 09:57:10 +0200

    chore(trie): dedup node iters

--- crates/trie/src/hashed_cursor/mod.rs
+++ crates/trie/src/hashed_cursor/mod.rs
@@ -28,7 +28,7 @@ pub trait HashedCursorFactory {
 /// The cursor for iterating over hashed entries.
 pub trait HashedCursor {
     /// Value returned by the cursor.
-    type Value;
+    type Value: std::fmt::Debug;
 
     /// Seek an entry greater or equal to the given key and position the cursor there.
     /// Returns the first entry with the key greater or equal to the sought key.
