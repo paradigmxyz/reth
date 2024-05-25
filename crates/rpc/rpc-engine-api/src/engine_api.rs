19b9abdaa52d14844c3c91af194198afc0479f45
Qiwei Yang <yangqiwei97@gmail.com>
2024-05-25 22:26:15 +0800

    update

--- crates/rpc/rpc-engine-api/src/engine_api.rs
+++ crates/rpc/rpc-engine-api/src/engine_api.rs
@@ -560,9 +560,10 @@ where
     async fn new_payload_v1(&self, payload: ExecutionPayloadV1) -> RpcResult<PayloadStatus> {
         trace!(target: "rpc::engine", "Serving engine_newPayloadV1");
         let start = Instant::now();
-        let res = EngineApi::new_payload_v1(self, payload).await;
-        self.inner.metrics.latency.new_payload_v1.record(start.elapsed());
-        self.inner.metrics.new_payload_response.update_response_metrics(&res);
+        let res = EngineApi::new_payload_v1(self, payload.clone()).await;
+        let elapsed = start.elapsed();
+        self.inner.metrics.latency.new_payload_v1.record(elapsed);
+        self.inner.metrics.new_payload_response.update_response_metrics(&res, payload.gas_used, elapsed);
         Ok(res?)
     }
 
@@ -571,9 +572,10 @@ where
     async fn new_payload_v2(&self, payload: ExecutionPayloadInputV2) -> RpcResult<PayloadStatus> {
         trace!(target: "rpc::engine", "Serving engine_newPayloadV2");
         let start = Instant::now();
-        let res = EngineApi::new_payload_v2(self, payload).await;
-        self.inner.metrics.latency.new_payload_v2.record(start.elapsed());
-        self.inner.metrics.new_payload_response.update_response_metrics(&res);
+        let res = EngineApi::new_payload_v2(self, payload.clone()).await;
+        let elapsed = start.elapsed();
+        self.inner.metrics.latency.new_payload_v2.record(elapsed);
+        self.inner.metrics.new_payload_response.update_response_metrics(&res, payload.execution_payload.gas_used, elapsed);
         Ok(res?)
     }
 
@@ -588,10 +590,11 @@ where
         trace!(target: "rpc::engine", "Serving engine_newPayloadV3");
         let start = Instant::now();
         let res =
-            EngineApi::new_payload_v3(self, payload, versioned_hashes, parent_beacon_block_root)
+            EngineApi::new_payload_v3(self, payload.clone(), versioned_hashes, parent_beacon_block_root)
                 .await;
-        self.inner.metrics.latency.new_payload_v3.record(start.elapsed());
-        self.inner.metrics.new_payload_response.update_response_metrics(&res);
+        let elapsed = start.elapsed();
+        self.inner.metrics.latency.new_payload_v3.record(elapsed);
+        self.inner.metrics.new_payload_response.update_response_metrics(&res, payload.payload_inner.payload_inner.gas_used, elapsed);
         Ok(res?)
     }
 
