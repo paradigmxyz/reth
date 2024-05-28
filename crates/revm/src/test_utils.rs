911a3a4a92cca5550a347e3fcd5561f6f97e3c43
Matthias Seitz <matthias.seitz@outlook.de>
2024-05-28 17:28:50 +0200

    chore: replace reth-provider dep in reth-evm (#8430)

--- crates/revm/src/test_utils.rs
+++ crates/revm/src/test_utils.rs
@@ -2,7 +2,7 @@ use reth_primitives::{
     keccak256, trie::AccountProof, Account, Address, BlockNumber, Bytecode, Bytes, StorageKey,
     B256, U256,
 };
-use reth_provider::{AccountReader, BlockHashReader, StateProvider, StateRootProvider};
+use reth_storage_api::{AccountReader, BlockHashReader, StateProvider, StateRootProvider};
 use reth_storage_errors::provider::ProviderResult;
 use reth_trie::updates::TrieUpdates;
 use revm::db::BundleState;
