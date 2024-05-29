864b31be7c7fbe92a2d372d759a73628aec74c83
Thomas Coratger <thomas.coratger@gmail.com>
2024-05-30 00:17:57 +0200

    fix clippy

--- crates/primitives/src/transaction/signature.rs
+++ crates/primitives/src/transaction/signature.rs
@@ -77,7 +77,7 @@ impl Signature {
 
     /// Output the `v` of the signature depends on chain_id
     #[inline]
-    pub fn v(&self, chain_id: Option<u64>) -> u64 {
+    pub const fn v(&self, chain_id: Option<u64>) -> u64 {
         if let Some(chain_id) = chain_id {
             // EIP-155: v = {0, 1} + CHAIN_ID * 2 + 35
             self.odd_y_parity as u64 + chain_id * 2 + 35
