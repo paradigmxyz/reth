fd59da56463e5971bfe60c54e3550cc3e13a3a6d
jsvisa <delweng@gmail.com>
2024-06-03 21:39:38 +0800

    fix(rpc): add invalid block range error
    Signed-off-by: jsvisa <delweng@gmail.com>

--- crates/rpc/rpc/src/eth/filter.rs
+++ crates/rpc/rpc/src/eth/filter.rs
@@ -431,6 +431,10 @@ where
         trace!(target: "rpc::eth::filter", from=from_block, to=to_block, ?filter, "finding logs in range");
         let best_number = chain_info.best_number;
 
+        if to_block < from_block {
+            return Err(FilterError::InvalidBlockRangeParams)
+        }
+
         if to_block - from_block > self.max_blocks_per_filter {
             return Err(FilterError::QueryExceedsMaxBlocks(self.max_blocks_per_filter))
         }
@@ -682,6 +686,8 @@ enum FilterKind {
 pub enum FilterError {
     #[error("filter not found")]
     FilterNotFound(FilterId),
+    #[error("invalid block range params")]
+    InvalidBlockRangeParams,
     #[error("query exceeds max block range {0}")]
     QueryExceedsMaxBlocks(u64),
     #[error("query exceeds max results {0}")]
@@ -706,6 +712,9 @@ impl From<FilterError> for jsonrpsee::types::error::ErrorObject<'static> {
             }
             FilterError::EthAPIError(err) => err.into(),
             err @ FilterError::QueryExceedsMaxBlocks(_) => {
+                rpc_error_with_code(jsonrpsee::types::error::INVALID_PARAMS_CODE, err.to_string())
+            }
+            err @ FilterError::InvalidBlockRangeParams => {
                 rpc_error_with_code(jsonrpsee::types::error::INVALID_PARAMS_CODE, err.to_string())
             }
             err @ FilterError::QueryExceedsMaxResults(_) => {
