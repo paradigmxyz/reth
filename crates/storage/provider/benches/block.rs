71eb534aef016c0d272834fdffab43321c4df778
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-06-06 16:55:15 -0400

    cargo fmt

--- crates/storage/provider/benches/block.rs
+++ crates/storage/provider/benches/block.rs
@@ -1,15 +1,12 @@
 #![allow(missing_docs, unreachable_pub)]
 //! This benchmarks block insertion in the block provider
 
-use criterion::{
-    criterion_group, criterion_main, BenchmarkId, Criterion,
-    Throughput, black_box
-};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use pprof::criterion::{Output, PProfProfiler};
 use prop::test_runner::TestRunner;
 use proptest::{prelude::*, strategy::ValueTree};
-use reth_provider::{test_utils::create_test_provider_factory, BlockWriter};
 use reth_primitives::{Address, PruneModes, SealedBlock, SealedBlockWithSenders};
+use reth_provider::{test_utils::create_test_provider_factory, BlockWriter};
 
 fn block_provider_insert(c: &mut Criterion) {
     let factory = create_test_provider_factory();
@@ -21,9 +18,10 @@ fn block_provider_insert(c: &mut Criterion) {
     let mut runner = TestRunner::new(ProptestConfig::default());
 
     let block_strategy = proptest::arbitrary::any::<SealedBlock>().prop_flat_map(|sealed_block| {
-        proptest::collection::vec(proptest::arbitrary::any::<Address>(), sealed_block.body.len()).prop_map(move |senders| {
-            SealedBlockWithSenders::new(sealed_block.clone(), senders).unwrap()
-        })
+        proptest::collection::vec(proptest::arbitrary::any::<Address>(), sealed_block.body.len())
+            .prop_map(move |senders| {
+                SealedBlockWithSenders::new(sealed_block.clone(), senders).unwrap()
+            })
     });
 
     // We can test different amounts of blocks
@@ -32,10 +30,11 @@ fn block_provider_insert(c: &mut Criterion) {
     for &size in &block_amounts {
         group.throughput(Throughput::Elements(size as u64));
         group.bench_with_input(BenchmarkId::new("Insert", size), &size, |b, &size| {
-            let blocks: Vec<SealedBlockWithSenders> = prop::collection::vec(block_strategy.clone(), size)
-                .new_tree(&mut runner)
-                .unwrap()
-                .current();
+            let blocks: Vec<SealedBlockWithSenders> =
+                prop::collection::vec(block_strategy.clone(), size)
+                    .new_tree(&mut runner)
+                    .unwrap()
+                    .current();
 
             b.iter(|| {
                 for block in &blocks {
