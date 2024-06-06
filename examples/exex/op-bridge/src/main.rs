45801e1cf4a323b54ed839ea86154a9fa89d222d
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-06-06 22:26:23 +0100

    example(exex): tests for OP Bridge

--- examples/exex/op-bridge/src/main.rs
+++ examples/exex/op-bridge/src/main.rs
@@ -252,3 +252,104 @@ fn main() -> eyre::Result<()> {
         handle.wait_for_node_exit().await
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use std::pin::pin;
+
+    use alloy_sol_types::SolEvent;
+    use reth::revm::db::BundleState;
+    use reth_exex_test_utils::{test_exex_context, PollOnce};
+    use reth_primitives::{
+        Address, Block, Header, Log, Receipt, Receipts, Transaction, TxKind, TxLegacy, TxType, U256,
+    };
+    use reth_provider::{BundleStateWithReceipts, Chain};
+    use reth_testing_utils::generators::sign_tx_with_random_key_pair;
+    use rusqlite::Connection;
+
+    use crate::{
+        L1StandardBridge::{self},
+        OP_BRIDGES,
+    };
+
+    #[tokio::test]
+    async fn test_exex() -> eyre::Result<()> {
+        let (ctx, handle) = test_exex_context().await?;
+
+        let db_file = tempfile::NamedTempFile::new()?;
+
+        let mut exex = pin!(super::init(ctx, Connection::open(&db_file)?).await?);
+
+        let from_address = Address::random();
+        let to_address = Address::random();
+
+        let deposit_tx =
+            Transaction::Legacy(TxLegacy { to: TxKind::Call(OP_BRIDGES[0]), ..Default::default() });
+        let deposit_event = L1StandardBridge::ETHBridgeInitiated {
+            from: from_address,
+            to: to_address,
+            amount: U256::from(100),
+            extraData: Default::default(),
+        };
+        let deposit_log = Log::new(
+            OP_BRIDGES[0],
+            deposit_event.encode_topics().into_iter().map(|t| t.0).collect(),
+            deposit_event.encode_data().into(),
+        )
+        .ok_or_else(|| eyre::eyre!("failed to encode deposit event"))?;
+        let deposit_tx_receipt = Receipt {
+            tx_type: TxType::Legacy,
+            success: true,
+            cumulative_gas_used: 0,
+            logs: vec![deposit_log],
+            ..Default::default()
+        };
+
+        let block = Block {
+            header: Header::default(),
+            body: vec![sign_tx_with_random_key_pair(&mut rand::thread_rng(), deposit_tx)],
+            ..Default::default()
+        }
+        .seal_slow()
+        .seal_with_senders()
+        .ok_or_else(|| eyre::eyre!("failed to recover senders"))?;
+
+        handle
+            .send_notification_chain_committed(Chain::new(
+                vec![block.clone()],
+                BundleStateWithReceipts::new(
+                    BundleState::default(),
+                    Receipts::from_block_receipt(vec![deposit_tx_receipt]),
+                    block.number,
+                ),
+                None,
+            ))
+            .await?;
+
+        exex.poll_once().await;
+
+        let connection = Connection::open(&db_file)?;
+
+        let rows: Vec<(u64, String, String, String, String, String)> = connection
+            .prepare(r#"SELECT block_number, contract_address, "from", "to", amount, tx_hash FROM deposits"#)?
+            .query_map([], |row| {
+                Ok((row.get(0)?, row.get(1)?, row.get(2)?, row.get(3)?, row.get(4)?, row.get(5)?))
+            })?
+            .collect::<Result<Vec<_>, _>>()?;
+
+        assert_eq!(rows.len(), 1);
+        assert_eq!(
+            rows[0],
+            (
+                block.number,
+                OP_BRIDGES[0].to_string(),
+                from_address.to_string(),
+                to_address.to_string(),
+                U256::from(100).to_string(),
+                block.body[0].hash().to_string()
+            )
+        );
+
+        Ok(())
+    }
+}
