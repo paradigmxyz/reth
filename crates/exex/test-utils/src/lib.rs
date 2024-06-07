72bf274010f780a2098e99afd2c2b8bc8cf54b2c
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-06-07 08:13:06 +0100

    test(exex): improve `PollOnce` trait error handling (#8653)

--- crates/exex/test-utils/src/lib.rs
+++ crates/exex/test-utils/src/lib.rs
@@ -41,6 +41,7 @@ use std::{
     sync::Arc,
     task::Poll,
 };
+use thiserror::Error;
 use tokio::sync::mpsc::{Sender, UnboundedReceiver};
 
 /// A test [`PoolBuilder`] that builds a [`TestPool`].
@@ -240,21 +241,36 @@ pub async fn test_exex_context() -> eyre::Result<(ExExContext<Adapter>, TestExEx
 
 /// An extension trait for polling an Execution Extension future.
 pub trait PollOnce {
-    /// Polls the given Execution Extension future __once__ and asserts that it is
-    /// [`Poll::Pending`]. The future should be (pinned)[`std::pin::pin`].
+    /// Polls the given Execution Extension future __once__. The future should be
+    /// (pinned)[`std::pin::pin`].
     ///
-    /// # Panics
-    /// If the future returns [`Poll::Ready`], because Execution Extension future should never
-    /// resolve.
-    fn poll_once(&mut self) -> impl Future<Output = ()> + Send;
+    /// # Returns
+    /// - `Ok(())` if the future returned [`Poll::Pending`]. The future can be polled again.
+    /// - `Err(PollOnceError::FutureIsReady)` if the future returned [`Poll::Ready`] without an
+    ///   error. The future should never resolve.
+    /// - `Err(PollOnceError::FutureError(err))` if the future returned [`Poll::Ready`] with an
+    ///   error. Something went wrong.
+    fn poll_once(&mut self) -> impl Future<Output = Result<(), PollOnceError>> + Send;
+}
+
+/// An Execution Extension future polling error.
+#[derive(Error, Debug)]
+pub enum PollOnceError {
+    /// The future returned [`Poll::Ready`] without an error, but it should never resolve.
+    #[error("Execution Extension future returned Ready, but it should never resolve")]
+    FutureIsReady,
+    /// The future returned [`Poll::Ready`] with an error.
+    #[error(transparent)]
+    FutureError(#[from] eyre::Error),
 }
 
 impl<F: Future<Output = eyre::Result<()>> + Unpin + Send> PollOnce for F {
-    async fn poll_once(&mut self) {
-        poll_fn(|cx| {
-            assert!(self.poll_unpin(cx).is_pending());
-            Poll::Ready(())
+    async fn poll_once(&mut self) -> Result<(), PollOnceError> {
+        poll_fn(|cx| match self.poll_unpin(cx) {
+            Poll::Ready(Ok(())) => Poll::Ready(Err(PollOnceError::FutureIsReady)),
+            Poll::Ready(Err(err)) => Poll::Ready(Err(PollOnceError::FutureError(err))),
+            Poll::Pending => Poll::Ready(Ok(())),
         })
-        .await;
+        .await
     }
 }
