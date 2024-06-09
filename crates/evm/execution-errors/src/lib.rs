c5c636424f6492408941711c984f7f9ff41cc32f
hawkinsjgh@gmail.com <jack.hawkins@mycelium.ventures>
2024-06-09 17:57:34 +0100

    remvoe this error

--- crates/evm/execution-errors/src/lib.rs
+++ crates/evm/execution-errors/src/lib.rs
@@ -7,40 +7,32 @@
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
-
+use core::fmt::Display;
 use reth_consensus::ConsensusError;
 use reth_primitives::{revm_primitives::EVMError, BlockNumHash, B256};
 use reth_prune_types::PruneSegmentError;
 use reth_storage_errors::provider::ProviderError;
-use std::fmt::Display;
-use thiserror::Error;
 
 pub mod trie;
 pub use trie::{StateRootError, StorageRootError};
 
 /// Transaction validation errors
-#[derive(Error, Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum BlockValidationError {
     /// EVM error with transaction hash and message
-    #[error("EVM reported invalid transaction ({hash}): {error}")]
     EVM {
         /// The hash of the transaction
         hash: B256,
         /// The EVM error.
-        #[source]
         error: Box<EVMError<ProviderError>>,
     },
     /// Error when recovering the sender for a transaction
-    #[error("failed to recover sender for transaction")]
     SenderRecoveryError,
     /// Error when incrementing balance in post execution
-    #[error("incrementing balance in post execution failed")]
     IncrementBalanceFailed,
     /// Error when the state root does not match the expected value.
-    #[error(transparent)]
-    StateRoot(#[from] StateRootError),
+    StateRoot(StateRootError),
     /// Error when transaction gas limit exceeds available block gas
-    #[error("transaction gas limit {transaction_gas_limit} is more than blocks available gas {block_available_gas}")]
     TransactionGasLimitMoreThanAvailableBlockGas {
         /// The transaction's gas limit
         transaction_gas_limit: u64,
@@ -48,22 +40,18 @@ pub enum BlockValidationError {
         block_available_gas: u64,
     },
     /// Error for pre-merge block
-    #[error("block {hash} is pre merge")]
     BlockPreMerge {
         /// The hash of the block
         hash: B256,
     },
     /// Error for missing total difficulty
-    #[error("missing total difficulty for block {hash}")]
     MissingTotalDifficulty {
         /// The hash of the block
         hash: B256,
     },
     /// Error for EIP-4788 when parent beacon block root is missing
-    #[error("EIP-4788 parent beacon block root missing for active Cancun block")]
     MissingParentBeaconBlockRoot,
     /// Error for Cancun genesis block when parent beacon block root is not zero
-    #[error("the parent beacon block root is not zero for Cancun genesis block: {parent_beacon_block_root}")]
     CancunGenesisParentBeaconBlockRootNotZero {
         /// The beacon block root
         parent_beacon_block_root: B256,
@@ -71,7 +59,6 @@ pub enum BlockValidationError {
     /// EVM error during [EIP-4788] beacon root contract call.
     ///
     /// [EIP-4788]: https://eips.ethereum.org/EIPS/eip-4788
-    #[error("failed to apply beacon root contract call at {parent_beacon_block_root}: {message}")]
     BeaconRootContractCall {
         /// The beacon block root
         parent_beacon_block_root: Box<B256>,
@@ -81,12 +68,10 @@ pub enum BlockValidationError {
     /// Provider error during the [EIP-2935] block hash account loading.
     ///
     /// [EIP-2935]: https://eips.ethereum.org/EIPS/eip-2935
-    #[error(transparent)]
-    BlockHashAccountLoadingFailed(#[from] ProviderError),
+    BlockHashAccountLoadingFailed(ProviderError),
     /// EVM error during withdrawal requests contract call [EIP-7002]
     ///
     /// [EIP-7002]: https://eips.ethereum.org/EIPS/eip-7002
-    #[error("failed to apply withdrawal requests contract call: {message}")]
     WithdrawalRequestsContractCall {
         /// The error message.
         message: String,
@@ -94,38 +79,29 @@ pub enum BlockValidationError {
     /// Error when decoding deposit requests from receipts [EIP-6110]
     ///
     /// [EIP-6110]: https://eips.ethereum.org/EIPS/eip-6110
-    #[error("failed to decode deposit requests from receipts: {0}")]
     DepositRequestDecode(String),
 }
 
 /// `BlockExecutor` Errors
-#[derive(Error, Debug)]
+#[derive(Debug)]
 pub enum BlockExecutionError {
     /// Validation error, transparently wrapping `BlockValidationError`
-    #[error(transparent)]
-    Validation(#[from] BlockValidationError),
+    Validation(BlockValidationError),
     /// Pruning error, transparently wrapping `PruneSegmentError`
-    #[error(transparent)]
-    Pruning(#[from] PruneSegmentError),
+    Pruning(PruneSegmentError),
     /// Consensus error, transparently wrapping `ConsensusError`
-    #[error(transparent)]
-    Consensus(#[from] ConsensusError),
+    Consensus(ConsensusError),
     /// Transaction error on revert with inner details
-    #[error("transaction error on revert: {inner}")]
     CanonicalRevert {
         /// The inner error message
         inner: String,
     },
     /// Transaction error on commit with inner details
-    #[error("transaction error on commit: {inner}")]
     CanonicalCommit {
         /// The inner error message
         inner: String,
     },
     /// Error when appending chain on fork is not possible
-    #[error(
-        "appending chain on fork (other_chain_fork:?) is not possible as the tip is {chain_tip:?}"
-    )]
     AppendChainDoesntConnect {
         /// The tip of the current chain
         chain_tip: Box<BlockNumHash>,
@@ -133,10 +109,8 @@ pub enum BlockExecutionError {
         other_chain_fork: Box<BlockNumHash>,
     },
     /// Error when fetching latest block state.
-    #[error(transparent)]
-    LatestBlock(#[from] ProviderError),
+    LatestBlock(ProviderError),
     /// Arbitrary Block Executor Errors
-    #[error(transparent)]
     Other(Box<dyn std::error::Error + Send + Sync>),
 }
 
