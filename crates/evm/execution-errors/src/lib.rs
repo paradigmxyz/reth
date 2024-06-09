42af23f5a747f9004a50b08c8bd4b2ba23e4f4f4
hawkinsjgh@gmail.com <jack.hawkins@mycelium.ventures>
2024-06-09 22:36:11 +0100

    attri no -std

--- crates/evm/execution-errors/src/lib.rs
+++ crates/evm/execution-errors/src/lib.rs
@@ -7,6 +7,7 @@
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 use core::fmt::{Display, Formatter, Result};
 use reth_consensus::ConsensusError;
