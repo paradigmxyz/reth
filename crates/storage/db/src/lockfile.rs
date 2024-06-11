748a45efb1bc283d0bc21da118b44b159be4712b
joshieDo <93316087+joshieDo@users.noreply.github.com>
2024-06-11 22:52:23 +0200

    remove disable_lock feature

--- crates/storage/db/src/lockfile.rs
+++ crates/storage/db/src/lockfile.rs
@@ -1,7 +1,5 @@
 //! Storage lock utils.
 
-#![cfg_attr(feature = "disable_lock", allow(dead_code))]
-
 use reth_storage_errors::lockfile::StorageLockError;
 use reth_tracing::tracing::error;
 use std::{
@@ -31,23 +29,13 @@ impl StorageLock {
     /// with the same PID), it will succeed.
     pub fn try_acquire(path: &Path) -> Result<Self, StorageLockError> {
         let file_path = path.join(LOCKFILE_NAME);
-
-        #[cfg(feature = "disable_lock")]
-        {
-            // Too expensive for ef-tests to write/read lock to/from disk.
-            Ok(Self(Arc::new(StorageLockInner { file_path })))
-        }
-
-        #[cfg(not(feature = "disable_lock"))]
-        {
-            if let Some(process_lock) = ProcessUID::parse(&file_path)? {
-                if process_lock.pid != (process::id() as usize) && process_lock.is_active() {
-                    return Err(StorageLockError::Taken(process_lock.pid))
-                }
+        if let Some(process_lock) = ProcessUID::parse(&file_path)? {
+            if process_lock.pid != (process::id() as usize) && process_lock.is_active() {
+                return Err(StorageLockError::Taken(process_lock.pid))
             }
-
-            Ok(Self(Arc::new(StorageLockInner::new(file_path)?)))
         }
+
+        Ok(Self(Arc::new(StorageLockInner::new(file_path)?)))
     }
 }
 
