f8bbf09eec2a2bcfaf85be5b13f1707c8467e76d
kamuik16 <krishang.s@ahduni.edu.in>
2024-06-11 22:32:26 +0530

    nit

--- crates/stages/stages/src/test_utils/test_db.rs
+++ crates/stages/stages/src/test_utils/test_db.rs
@@ -349,7 +349,7 @@ impl TestStageDB {
                 let mut writer = provider.latest_writer(StaticFileSegment::Receipts)?;
                 let res = receipts.into_iter().try_for_each(|(block_num, receipts)| {
                     writer.increment_block(StaticFileSegment::Receipts, block_num)?;
-                    writer.append_receipts(receipts.into_iter().map(Ok))?;
+                    writer.append_receipts(receipts.map(Ok))?;
                     Ok(())
                 });
                 writer.commit_without_sync_all()?;
