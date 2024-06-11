b515640e7e471d0ff71ca273ca83835eed556ffb
hawkinsjgh@gmail.com <jack.hawkins@mycelium.ventures>
2024-06-11 21:55:18 +0100

    disable std

--- crates/evm/execution-errors/src/lib.rs
+++ crates/evm/execution-errors/src/lib.rs
@@ -7,6 +7,7 @@
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 use reth_consensus::ConsensusError;
 use reth_primitives::{revm_primitives::EVMError, BlockNumHash, B256};
