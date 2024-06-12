7286452679fec753e5aa34fd4fffb1143460ccd1
0xAtreides <103257861+JackG-eth@users.noreply.github.com>
2024-06-13 00:29:36 +0100

    feat: support `no_std` for `reth-storage-errors` (#8790)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/storage/errors/src/lockfile.rs
+++ crates/storage/errors/src/lockfile.rs
@@ -1,8 +1,10 @@
 use reth_fs_util::FsPathError;
-use thiserror::Error;
 
-#[derive(Error, Debug, Clone, PartialEq, Eq)]
+#[cfg(not(feature = "std"))]
+use alloc::string::{String, ToString};
+
 /// Storage lock error.
+#[derive(Debug, Clone, PartialEq, Eq, thiserror_no_std::Error)]
 pub enum StorageLockError {
     /// Write lock taken
     #[error("storage directory is currently in use as read-write by another process: PID {0}")]
