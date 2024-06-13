7a47a0f12e3bccecee094c0cdb40cf28f365e84b
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-06-13 16:03:32 -0400

    rename, add some action enums

--- crates/engine/tree/src/background_writer.rs
+++ crates/engine/tree/src/background_writer.rs
@@ -1,26 +1,33 @@
 use std::sync::Arc;
-
 use parking_lot::RwLock;
-use reth_provider::ProviderFactory;
+use reth_primitives::B256;
 use crate::tree::TreeState;
 
+// TODO: design questions:
+// * to support "unwinds" or not? ie, what do we do when we get a request to persist a chain that is
+// overlapping with the one on disk
 /// Writes parts of the tree to the database
-pub struct BackgroundWriter<DB> {
+pub struct Persistence<Writer> {
     /// The db / static file provider to use
-    provider: ProviderFactory<DB>,
+    provider: Writer,
     /// The locked tree state
     tree_state: Arc<RwLock<TreeState>>,
 }
 
-impl<DB> BackgroundWriter<DB> {
-    /// Produces a clone of the tree state used for writing
-    pub fn tree_state(&self) -> TreeState {
-        let reader = self.tree_state.read();
-        reader.clone()
-    }
-
+impl<Writer> Persistence<Writer> {
     /// Writes the cloned tree state to the database
     pub fn write(&self) {
         todo!("need tree state to write")
     }
+}
+
+/// A signal to the persistence task that part of the tree state can be persisted.
+pub enum PersistenceAction {
+    // TODO: support reorgs? ie, save these blocks, and re-add the overwritten blocks to the state?
+    /// The tree state from before this block can be persisted
+    SaveBlocks(u64),
+
+    // TODO: maybe not necessary
+    /// The tree state from before this block hash has been finalized and can be persisted
+    SaveFinalizedBlock(B256),
 }
