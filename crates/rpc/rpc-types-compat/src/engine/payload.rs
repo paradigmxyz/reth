e79377ab12b66a3270b103610de819dc5bc42d6a
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-06-13 16:07:10 -0400

    use is_zero
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/rpc/rpc-types-compat/src/engine/payload.rs
+++ crates/rpc/rpc-types-compat/src/engine/payload.rs
@@ -18,7 +18,7 @@ pub fn try_payload_v1_to_block(payload: ExecutionPayloadV1) -> Result<Block, Pay
         return Err(PayloadError::ExtraData(payload.extra_data))
     }
 
-    if payload.base_fee_per_gas == U256::ZERO {
+    if payload.base_fee_per_gas.is_zero() {
         return Err(PayloadError::BaseFee(payload.base_fee_per_gas))
     }
 
