81f980c52d03a1cacb0972181bc7d81a5460e0de
joshieDo <93316087+joshieDo@users.noreply.github.com>
2024-06-17 17:48:57 +0200

    fix unit doc tests

--- crates/consensus/common/src/calc.rs
+++ crates/consensus/common/src/calc.rs
@@ -46,9 +46,10 @@ pub fn base_block_reward(
 /// # Examples
 ///
 /// ```
+/// # use reth_chainspec::MAINNET;
 /// # use reth_consensus_common::calc::{base_block_reward, block_reward};
 /// # use reth_primitives::constants::ETH_TO_WEI;
-/// # use reth_primitives::{MAINNET, U256};
+/// # use reth_primitives::U256;
 /// #
 /// // This is block 126 on mainnet.
 /// let block_number = 126;
