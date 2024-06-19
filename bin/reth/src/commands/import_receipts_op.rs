fc995175d3561d2c5fe550b5eed561c407b435e9
Omid Chenane <omid.chenane@gmail.com>
2024-06-19 18:17:32 +0330

    make ReceiptFileClient generic

--- bin/reth/src/commands/import_receipts_op.rs
+++ bin/reth/src/commands/import_receipts_op.rs
@@ -7,6 +7,7 @@ use reth_db::tables;
 use reth_db_api::{database::Database, transaction::DbTx};
 use reth_downloaders::{
     file_client::{ChunkedFileReader, DEFAULT_BYTE_LEN_CHUNK_CHAIN_FILE},
+    file_codec_ovm_receipt::HackReceiptFileCodec,
     receipt_file_client::ReceiptFileClient,
 };
 use reth_node_core::version::SHORT_VERSION;
@@ -114,10 +115,16 @@ where
     // open file
     let mut reader = ChunkedFileReader::new(path, chunk_len).await?;
 
-    while let Some(file_client) = reader.next_chunk::<ReceiptFileClient>().await? {
+    while let Some(file_client) =
+        reader.next_chunk::<ReceiptFileClient<HackReceiptFileCodec>>().await?
+    {
         // create a new file client from chunk read from file
-        let ReceiptFileClient { mut receipts, first_block, total_receipts: total_receipts_chunk } =
-            file_client;
+        let ReceiptFileClient {
+            mut receipts,
+            first_block,
+            total_receipts: total_receipts_chunk,
+            ..
+        } = file_client;
 
         // mark these as decoded
         total_decoded_receipts += total_receipts_chunk;
