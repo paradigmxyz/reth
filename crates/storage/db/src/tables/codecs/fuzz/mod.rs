d0b241c0c2c3d9123eb884b17efae120ca8b5e18
joshieDo <93316087+joshieDo@users.noreply.github.com>
2024-06-19 16:13:31 +0200

    chore: move `IntegerList` to `reth-primitives-traits` (#8948)

--- crates/storage/db/src/tables/codecs/fuzz/mod.rs
+++ crates/storage/db/src/tables/codecs/fuzz/mod.rs
@@ -20,6 +20,9 @@ macro_rules! impl_fuzzer_with_input {
                 use reth_primitives::*;
 
                 #[allow(unused_imports)]
+                use reth_primitives_traits::*;
+
+                #[allow(unused_imports)]
                 use super::inputs::*;
 
                 #[allow(unused_imports)]
