46e8f37644a3c3e061108a7b9f63dc734e3638b2
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-06-24 18:01:05 +0100

    copy to static files first

--- bin/reth/src/commands/prune.rs
+++ bin/reth/src/commands/prune.rs
@@ -2,8 +2,10 @@
 
 use crate::commands::common::{AccessRights, Environment, EnvironmentArgs};
 use clap::Parser;
-use reth_provider::BlockNumReader;
+use reth_provider::StageCheckpointReader;
 use reth_prune::PrunerBuilder;
+use reth_stages::StageId;
+use reth_static_file::{HighestStaticFiles, StaticFileProducer};
 
 /// Prunes according to the configuration without any limits
 #[derive(Debug, Parser)]
@@ -16,14 +18,40 @@ impl PruneCommand {
     /// Execute the `prune` command
     pub async fn execute(self) -> eyre::Result<()> {
         let Environment { config, provider_factory, .. } = self.env.init(AccessRights::RW)?;
+        let prune_config = config.prune.unwrap_or_default();
 
-        let tip_block_number = provider_factory.best_block_number()?;
+        let static_file_producer =
+            StaticFileProducer::new(provider_factory.clone(), prune_config.segments.clone());
+        let static_file_producer = static_file_producer.lock();
 
-        let mut pruner = PrunerBuilder::new(config.prune.unwrap_or_default())
-            .prune_delete_limit(usize::MAX)
-            .build(provider_factory);
+        // Copies data from database to static files
+        let lowest_static_file_height = {
+            let provider = provider_factory.provider()?;
+            let stages_checkpoints = [StageId::Headers, StageId::Execution, StageId::Bodies]
+                .into_iter()
+                .map(|stage| {
+                    provider.get_stage_checkpoint(stage).map(|c| c.map(|c| c.block_number))
+                })
+                .collect::<Result<Vec<_>, _>>()?;
 
-        pruner.run(tip_block_number)?;
+            let targets = static_file_producer.get_static_file_targets(HighestStaticFiles {
+                headers: stages_checkpoints[0],
+                receipts: stages_checkpoints[1],
+                transactions: stages_checkpoints[2],
+            })?;
+            static_file_producer.run(targets)?;
+            stages_checkpoints.into_iter().min().expect("exists")
+        };
+
+        // Deletes data which has been copied to static files.
+        if let Some(prune_tip) = lowest_static_file_height {
+            // Run the pruner according to the configuration, and don't enforce any limits on it
+            let mut pruner = PrunerBuilder::new(prune_config)
+                .prune_delete_limit(usize::MAX)
+                .build(provider_factory);
+
+            pruner.run(prune_tip)?;
+        }
 
         Ok(())
     }
