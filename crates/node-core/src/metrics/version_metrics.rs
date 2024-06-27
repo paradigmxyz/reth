672e2f6e97457c10c7fac0f77b4ee87f1f04a0c4
Michael Sproul <michael@sigmaprime.io>
2024-06-27 21:03:40 +1000

    fix: use 8 byte SHA in getClientVersionV1 (#9137)

--- crates/node-core/src/metrics/version_metrics.rs
+++ crates/node-core/src/metrics/version_metrics.rs
@@ -1,13 +1,13 @@
 //! This exposes reth's version information over prometheus.
 
-use crate::version::build_profile_name;
+use crate::version::{build_profile_name, VERGEN_GIT_SHA};
 use metrics::gauge;
 
 const LABELS: [(&str, &str); 6] = [
     ("version", env!("CARGO_PKG_VERSION")),
     ("build_timestamp", env!("VERGEN_BUILD_TIMESTAMP")),
     ("cargo_features", env!("VERGEN_CARGO_FEATURES")),
-    ("git_sha", env!("VERGEN_GIT_SHA")),
+    ("git_sha", VERGEN_GIT_SHA),
     ("target_triple", env!("VERGEN_CARGO_TARGET_TRIPLE")),
     ("build_profile", build_profile_name()),
 ];
