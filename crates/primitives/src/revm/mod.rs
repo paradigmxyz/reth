d8e6d01308b6234fdbf3e4d447c9a10e6702df02
joshieDo <93316087+joshieDo@users.noreply.github.com>
2024-06-27 21:11:29 +0200

    chore: move `revm_spec` methods from `reth-primitives` to chain specific crates (#9152)

--- crates/primitives/src/revm/mod.rs
+++ crates/primitives/src/revm/mod.rs
@@ -1,8 +1,5 @@
 //! Helpers for working with revm.
 
-/// Reth block execution/validation configuration and constants
-pub mod config;
-
 /// The `env` module provides utility methods for filling revm transaction and block environments.
 ///
 /// It includes functions to fill transaction and block environments with relevant data, prepare
