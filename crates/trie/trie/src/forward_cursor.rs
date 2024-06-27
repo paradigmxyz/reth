c3336414b9147e515e66fb4b7173657c7d234cd5
Roman Krasiuk <rokrassyuk@gmail.com>
2024-06-27 11:25:10 +0200

    nits

--- crates/trie/trie/src/forward_cursor.rs
+++ crates/trie/trie/src/forward_cursor.rs
@@ -10,6 +10,7 @@ pub struct ForwardInMemoryCursor<'a, K, V> {
 
 impl<'a, K, V> ForwardInMemoryCursor<'a, K, V> {
     /// Create new forward cursor positioned at the beginning of the collection.
+    /// The cursor expects all of the entries have been sorted in advance.
     pub const fn new(entries: &'a Vec<(K, V)>) -> Self {
         Self { entries, index: 0 }
     }
@@ -27,7 +28,7 @@ where
 {
     /// Advances the cursor forward while `comparator` returns `true` or until the collection is
     /// exhausted. Returns the first entry for which `comparator` returns `false` or `None`.
-    fn advance_while(&mut self, comparator: impl Fn(&K) -> bool) -> Option<(K, V)> {
+    fn advance_while_false(&mut self, comparator: impl Fn(&K) -> bool) -> Option<(K, V)> {
         let mut entry = self.entries.get(self.index);
         while entry.map_or(false, |entry| comparator(&entry.0)) {
             self.index += 1;
@@ -39,12 +40,12 @@ where
     /// Returns the first entry from the current cursor position that's greater or equal to the
     /// provided key. This method advances the cursor forward.
     pub fn seek(&mut self, key: &K) -> Option<(K, V)> {
-        self.advance_while(|k| k < key)
+        self.advance_while_false(|k| k < key)
     }
 
     /// Returns the first entry from the current cursor position that's greater than the provided
     /// key. This method advances the cursor forward.
     pub fn first_after(&mut self, key: &K) -> Option<(K, V)> {
-        self.advance_while(|k| k <= key)
+        self.advance_while_false(|k| k <= key)
     }
 }
