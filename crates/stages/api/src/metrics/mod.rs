9129b97c5bf9bb0bd0a617b32b723681923c25dc
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-06-28 16:22:42 +0100

    feat(exex): backfill executor (#9123)

--- crates/stages/api/src/metrics/mod.rs
+++ crates/stages/api/src/metrics/mod.rs
@@ -1,5 +1,7 @@
+mod execution;
 mod listener;
 mod sync_metrics;
 
+pub use execution::format_gas_throughput;
 pub use listener::{MetricEvent, MetricEventsSender, MetricsListener};
 use sync_metrics::*;
