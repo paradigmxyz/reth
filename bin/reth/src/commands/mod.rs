6db598da1d1f320c9e38e1c96a4acb4f5ab86751
Luca Provini <luca.provini@usemerkle.com>
2024-07-01 18:20:07 +0200

    chore: extract db commands (#9217)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- bin/reth/src/commands/mod.rs
+++ bin/reth/src/commands/mod.rs
@@ -1,7 +1,6 @@
 //! This contains all of the `reth` commands
 
 pub mod config_cmd;
-pub mod db;
 pub mod debug_cmd;
 pub mod dump_genesis;
 pub mod import;
@@ -15,5 +14,3 @@ pub mod prune;
 pub mod recover;
 pub mod stage;
 pub mod test_vectors;
-
-pub mod common;
