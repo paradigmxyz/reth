9d4722eb650563aed650c325407f306b5770b858
joshieDo <93316087+joshieDo@users.noreply.github.com>
2024-07-01 17:24:51 +0200

    chore: remove unused `static-file` code (#9178)

--- crates/storage/nippy-jar/src/lib.rs
+++ crates/storage/nippy-jar/src/lib.rs
@@ -28,7 +28,9 @@ pub mod filter;
 use filter::{Cuckoo, InclusionFilter, InclusionFilters};
 
 pub mod compression;
-use compression::{Compression, Compressors};
+#[cfg(test)]
+use compression::Compression;
+use compression::Compressors;
 
 pub mod phf;
 pub use phf::PHFKey;
@@ -306,6 +308,56 @@ impl<H: NippyJarHeader> NippyJar<H> {
         DataReader::new(self.data_path())
     }
 
+    /// Writes all necessary configuration to file.
+    fn freeze_config(&self) -> Result<(), NippyJarError> {
+        // Atomic writes are hard: <https://github.com/paradigmxyz/reth/issues/8622>
+        let mut tmp_path = self.config_path();
+        tmp_path.set_extension(".tmp");
+
+        // Write to temporary file
+        let mut file = File::create(&tmp_path)?;
+        bincode::serialize_into(&mut file, &self)?;
+
+        // fsync() file
+        file.sync_all()?;
+
+        // Rename file, not move
+        reth_fs_util::rename(&tmp_path, self.config_path())?;
+
+        // fsync() dir
+        if let Some(parent) = tmp_path.parent() {
+            OpenOptions::new().read(true).open(parent)?.sync_all()?;
+        }
+        Ok(())
+    }
+}
+
+impl<H: NippyJarHeader> InclusionFilter for NippyJar<H> {
+    fn add(&mut self, element: &[u8]) -> Result<(), NippyJarError> {
+        self.filter.as_mut().ok_or(NippyJarError::FilterMissing)?.add(element)
+    }
+
+    fn contains(&self, element: &[u8]) -> Result<bool, NippyJarError> {
+        self.filter.as_ref().ok_or(NippyJarError::FilterMissing)?.contains(element)
+    }
+
+    fn size(&self) -> usize {
+        self.filter.as_ref().map(|f| f.size()).unwrap_or(0)
+    }
+}
+
+impl<H: NippyJarHeader> PerfectHashingFunction for NippyJar<H> {
+    fn set_keys<T: PHFKey>(&mut self, keys: &[T]) -> Result<(), NippyJarError> {
+        self.phf.as_mut().ok_or(NippyJarError::PHFMissing)?.set_keys(keys)
+    }
+
+    fn get_index(&self, key: &[u8]) -> Result<Option<u64>, NippyJarError> {
+        self.phf.as_ref().ok_or(NippyJarError::PHFMissing)?.get_index(key)
+    }
+}
+
+#[cfg(test)]
+impl<H: NippyJarHeader> NippyJar<H> {
     /// If required, prepares any compression algorithm to an early pass of the data.
     pub fn prepare_compression(
         &mut self,
@@ -429,53 +481,6 @@ impl<H: NippyJarHeader> NippyJar<H> {
 
         Ok(())
     }
-
-    /// Writes all necessary configuration to file.
-    fn freeze_config(&self) -> Result<(), NippyJarError> {
-        // Atomic writes are hard: <https://github.com/paradigmxyz/reth/issues/8622>
-        let mut tmp_path = self.config_path();
-        tmp_path.set_extension(".tmp");
-
-        // Write to temporary file
-        let mut file = File::create(&tmp_path)?;
-        bincode::serialize_into(&mut file, &self)?;
-
-        // fsync() file
-        file.sync_all()?;
-
-        // Rename file, not move
-        reth_fs_util::rename(&tmp_path, self.config_path())?;
-
-        // fsync() dir
-        if let Some(parent) = tmp_path.parent() {
-            OpenOptions::new().read(true).open(parent)?.sync_all()?;
-        }
-        Ok(())
-    }
-}
-
-impl<H: NippyJarHeader> InclusionFilter for NippyJar<H> {
-    fn add(&mut self, element: &[u8]) -> Result<(), NippyJarError> {
-        self.filter.as_mut().ok_or(NippyJarError::FilterMissing)?.add(element)
-    }
-
-    fn contains(&self, element: &[u8]) -> Result<bool, NippyJarError> {
-        self.filter.as_ref().ok_or(NippyJarError::FilterMissing)?.contains(element)
-    }
-
-    fn size(&self) -> usize {
-        self.filter.as_ref().map(|f| f.size()).unwrap_or(0)
-    }
-}
-
-impl<H: NippyJarHeader> PerfectHashingFunction for NippyJar<H> {
-    fn set_keys<T: PHFKey>(&mut self, keys: &[T]) -> Result<(), NippyJarError> {
-        self.phf.as_mut().ok_or(NippyJarError::PHFMissing)?.set_keys(keys)
-    }
-
-    fn get_index(&self, key: &[u8]) -> Result<Option<u64>, NippyJarError> {
-        self.phf.as_ref().ok_or(NippyJarError::PHFMissing)?.get_index(key)
-    }
 }
 
 /// Manages the reading of static file data using memory-mapped files.
@@ -581,6 +586,7 @@ impl DataReader {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use compression::Compression;
     use rand::{rngs::SmallRng, seq::SliceRandom, RngCore, SeedableRng};
     use std::{collections::HashSet, fs::OpenOptions};
 
