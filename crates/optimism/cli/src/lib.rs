08fc298e5565b546eddc8fd968d0bf0790cf69a4
Luca Provini <luca.provini@usemerkle.com>
2024-07-03 16:39:31 +0200

    feat: moved optimism commands to create and remove from bin (#9242)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/optimism/cli/src/lib.rs
+++ crates/optimism/cli/src/lib.rs
@@ -5,5 +5,11 @@
     html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
     issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
 )]
-#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![cfg_attr(all(not(test), feature = "optimism"), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+// The `optimism` feature must be enabled to use this crate.
+#![cfg(feature = "optimism")]
+
+/// Optimism CLI commands.
+pub mod commands;
+pub use commands::{import::ImportOpCommand, import_receipts::ImportReceiptsOpCommand};
