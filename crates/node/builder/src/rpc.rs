9c0bc8477a85fc973511034ca03f7a79dfceb3d9
Sean Matt <s.matthew.english@gmail.com>
2024-07-08 17:06:52 -0400

    refactor(rpc): remove intermediate types from rpc start up process (#9180)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/node/builder/src/rpc.rs
+++ crates/node/builder/src/rpc.rs
@@ -289,7 +289,8 @@ where
     extend_rpc_modules.extend_rpc_modules(ctx)?;
 
     let server_config = config.rpc.rpc_server_config();
-    let launch_rpc = modules.clone().start_server(server_config).map_ok(|handle| {
+    let cloned_modules = modules.clone();
+    let launch_rpc = server_config.start(&cloned_modules).map_ok(|handle| {
         if let Some(path) = handle.ipc_endpoint() {
             info!(target: "reth::cli", %path, "RPC IPC server started");
         }
