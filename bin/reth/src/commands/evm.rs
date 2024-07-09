690dd1ce6b57f51fa8e0682808023147ff7a767c
Jackmeng1985 <mengweifu1985@gmail.com>
2024-07-09 17:37:02 +0800

    udpate

--- bin/reth/src/commands/evm.rs
+++ bin/reth/src/commands/evm.rs
@@ -6,9 +6,12 @@ use reth_db::{init_db, DatabaseEnv, tables, transaction::DbTx};
 use reth_primitives::{BlockNumber, Header};
 use std::sync::Arc;
 use std::path::{Path, PathBuf};
+use std::thread;
+use std::thread::JoinHandle;
 use reth_blockchain_tree::{BlockchainTree, BlockchainTreeConfig, ShareableBlockchainTree, TreeExternals};
 use reth_blockchain_tree::noop::NoopBlockchainTree;
-use reth_consensus::Consensus;
+use reth_consensus::{Consensus, PostExecutionInput};
+use reth_consensus_common::validation;
 use reth_db_api::database::Database;
 use reth_evm::execute::{BatchExecutor, BlockExecutionOutput, BlockExecutorProvider, Executor};
 use reth_execution_types::ExecutionOutcome;
@@ -26,8 +29,12 @@ use crate::providers::{BlockNumReader, OriginalValuesKnown, ProviderError, State
 pub struct EvmCommand {
     #[command(flatten)]
     env: EnvironmentArgs,
-    /// The block number to fetch the header for
-    block_number: BlockHashOrNumber,
+    /// begin block number
+    #[arg(long, alias = "begin", short = 'b')]
+    begin_number: u64,
+    /// end block number
+    #[arg(long, alias = "end", short = 'e')]
+    end_number: u64,
 }
 
 impl EvmCommand {
@@ -52,53 +59,62 @@ impl EvmCommand {
         let provider = provider_factory.provider()?;
 
         let last = provider.last_block_number()?;
-        let target = match self.block_number {
-                BlockHashOrNumber::Hash(hash) => provider
-                    .block_number(hash)?
-                    .ok_or_else(|| eyre::eyre!("Block hash not found in database: {hash:?}"))?,
-                BlockHashOrNumber::Number(num) => num,
+
+        if self.begin_number > self.end_number {
+            eyre::bail!("the begin block number is higher than the end block number")
+        }
+        if  self.end_number > last {
+            eyre::bail!("The end block number is higher than the latest block number")
+        }
+
+
+        // 获取 CPU 核心数，减一作为线程数
+        let cpu_count = self.get_cpu_count();
+        let thread_count = cpu_count - 1;
+
+        // 计算每个线程处理的区间大小
+        let range_per_thread = (self.end_number - self.begin_number + 1) / thread_count as u64;
+
+        let mut threads: Vec<JoinHandle<bool>> = Vec::with_capacity(thread_count);
+
+        for i in 0..thread_count as u64 {
+            let thread_start = self.begin_number + i * range_per_thread;
+            let thread_end = if i == thread_count as u64 - 1 {
+                self.end_number
+            } else {
+                thread_start + range_per_thread - 1
             };
 
-        if target > last {
-            eyre::bail!("Target block number is higher than the latest block number")
+            threads.push(thread::spawn(move || {
+
+                let db = StateProviderDatabase::new(blockchain_db.history_by_block_number(thread_start-1)?);
+                let executor = block_executor!(provider_factory.chain_spec()).executor(db);
+
+                for target in thread_start..=thread_end {
+
+                    let td = provider.header_td_by_number(target)?
+                        .ok_or_else(|| ProviderError::HeaderNotFound(target.into()))?;
+
+                    let block =  provider.sealed_block_with_senders(target.into(), TransactionVariant::WithHash)?
+                        .ok_or_else(|| ProviderError::HeaderNotFound(target.into()))?;
+
+                    let BlockExecutionOutput { state, receipts, requests, .. } =
+                        executor.execute((&block.clone().unseal(),td).into())?;
+
+                    let consensus: Arc<dyn Consensus> =
+                        Arc::new(EthBeaconConsensus::new(provider_factory.chain_spec()));
+                    consensus.validate_block_post_execution(&block.clone().unseal(), PostExecutionInput::new(&receipts, &requests))?;
+                }
+            });
         }
 
-
-        let td = provider.header_td_by_number(target)?
-            .ok_or_else(|| ProviderError::HeaderNotFound(target.into()))?;
-
-        let block =  provider.sealed_block_with_senders(target.into(), TransactionVariant::WithHash)?
-            .ok_or_else(|| ProviderError::HeaderNotFound(target.into()))?;
-
-
-        let db = StateProviderDatabase::new(blockchain_db.history_by_block_number(target-1)?);
-        let executor = block_executor!(provider_factory.chain_spec()).executor(db);
-
-        let BlockExecutionOutput { state, receipts, requests, .. } =
-            executor.execute((&block.clone().unseal(),td).into())?;
-
-        let execution_outcome = ExecutionOutcome::new(
-            state,
-            receipts.into(),
-            block.number,
-            vec![requests.into()],
-        );
-
-        debug!(target: "reth::cli", ?execution_outcome, "Executed block");
-
-        let hashed_post_state = execution_outcome.hash_state_slow();
-        let state_root = execution_outcome
-            .hash_state_slow()
-            .state_root(provider_factory.provider()?.tx_ref())?;
-
-        if state_root != block.state_root {
-            eyre::bail!(
-                        "state root mismatch. expected: {}. got: {}",
-                        block.state_root,
-                        state_root
-                    );
-        }
+        threads.into_iter().all(|b| b.join().unwrap());
 
         Ok(())
+    }
+
+    // 获取系统 CPU 核心数
+    fn get_cpu_count(&self) -> usize {
+        num_cpus::get()
     }
 }
