a8774bd9a2aa36a45b108bcce02863535794c1e1
Matthias Seitz <matthias.seitz@outlook.de>
2024-07-10 13:04:29 +0200

    chore: rm redundant clippy allow

--- crates/net/eth-wire-types/src/lib.rs
+++ crates/net/eth-wire-types/src/lib.rs
@@ -6,7 +6,6 @@
     issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
-#![allow(clippy::needless_lifetimes)] // side effect of optimism fields
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 mod status;
