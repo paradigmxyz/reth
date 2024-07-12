d2ea7c561c6610cd3714506a735da192e8301dce
Loocapro <lucaprovini1989@gmail.com>
2024-07-12 14:30:34 +0200

    removing optimism flags from reth bin and op specific commands

--- bin/reth/src/cli/mod.rs
+++ bin/reth/src/cli/mod.rs
@@ -154,12 +154,6 @@ impl<Ext: clap::Args + fmt::Debug> Cli<Ext> {
             Commands::Import(command) => runner.run_blocking_until_ctrl_c(
                 command.execute(|chain_spec| block_executor!(chain_spec)),
             ),
-            #[cfg(feature = "optimism")]
-            Commands::ImportOp(command) => runner.run_blocking_until_ctrl_c(command.execute()),
-            #[cfg(feature = "optimism")]
-            Commands::ImportReceiptsOp(command) => {
-                runner.run_blocking_until_ctrl_c(command.execute())
-            }
             Commands::DumpGenesis(command) => runner.run_blocking_until_ctrl_c(command.execute()),
             Commands::Db(command) => runner.run_blocking_until_ctrl_c(command.execute()),
             Commands::Stage(command) => runner.run_command_until_exit(|ctx| {
@@ -200,14 +194,6 @@ pub enum Commands<Ext: clap::Args + fmt::Debug = NoArgs> {
     /// This syncs RLP encoded blocks from a file.
     #[command(name = "import")]
     Import(import::ImportCommand),
-    /// This syncs RLP encoded OP blocks below Bedrock from a file, without executing.
-    #[cfg(feature = "optimism")]
-    #[command(name = "import-op")]
-    ImportOp(reth_optimism_cli::ImportOpCommand),
-    /// This imports RLP encoded receipts from a file.
-    #[cfg(feature = "optimism")]
-    #[command(name = "import-receipts-op")]
-    ImportReceiptsOp(reth_optimism_cli::ImportReceiptsOpCommand),
     /// Dumps genesis block JSON configuration to stdout.
     DumpGenesis(dump_genesis::DumpGenesisCommand),
     /// Database debugging utilities
