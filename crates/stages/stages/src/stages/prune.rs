5fa7031d44c2e42b3c73d1c5e20743f5494db5f4
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-07-15 14:18:11 +0100

    may

--- crates/stages/stages/src/stages/prune.rs
+++ crates/stages/stages/src/stages/prune.rs
@@ -49,7 +49,7 @@ impl<DB: Database> Stage<DB> for PruneStage {
         if result.is_finished() {
             Ok(ExecOutput { checkpoint: StageCheckpoint::new(input.target()), done: true })
         } else {
-            // We cannot set the checkpoint yet, because prune segments have different highest
+            // We cannot set the checkpoint yet, because prune segments may have different highest
             // pruned block numbers
             Ok(ExecOutput { checkpoint: input.checkpoint(), done: false })
         }
