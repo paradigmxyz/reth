abca52d0602020e485923ee7425bf47a9e52151e
Matthias Seitz <matthias.seitz@outlook.de>
2024-07-16 11:44:21 +0200

    fix merge

--- crates/optimism/cli/src/lib.rs
+++ crates/optimism/cli/src/lib.rs
@@ -10,15 +10,6 @@
 // The `optimism` feature must be enabled to use this crate.
 #![cfg(feature = "optimism")]
 
-/// Optimism chain specification parser.
-pub mod chainspec;
-/// Optimism CLI commands.
-pub mod commands;
-
-pub use commands::{import::ImportOpCommand, import_receipts::ImportReceiptsOpCommand};
-
-use std::{ffi::OsString, fmt, sync::Arc};
-
 use chainspec::OpChainSpecParser;
 use clap::{command, value_parser, Parser};
 use commands::Commands;
