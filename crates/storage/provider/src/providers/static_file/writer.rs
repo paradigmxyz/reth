986b29e773facd6a4f9b09e1863d0e9195f85f0c
joshieDo <93316087+joshieDo@users.noreply.github.com>
2024-07-17 18:41:22 +0200

    chore: use each prune segment method on `remove_blocks_above` (#9568)

--- crates/storage/provider/src/providers/static_file/writer.rs
+++ crates/storage/provider/src/providers/static_file/writer.rs
@@ -338,7 +338,7 @@ impl StaticFileProviderRW {
     ///
     /// # Note
     /// Commits to the configuration file at the end.
-    pub fn truncate(
+    fn truncate(
         &mut self,
         segment: StaticFileSegment,
         num_rows: u64,
