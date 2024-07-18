b1d40e4b4f18854b795229b4fae125ff4f3ae7a2
nk_ysg <nk_ysg@163.com>
2024-07-18 10:51:47 +0800

    chore(reth-network-api): no_std support

--- crates/net/network-api/src/lib.rs
+++ crates/net/network-api/src/lib.rs
@@ -18,6 +18,7 @@ pub use error::NetworkError;
 pub use reputation::{Reputation, ReputationChangeKind};
 use reth_eth_wire::{capability::Capabilities, DisconnectReason, EthVersion, Status};
 use reth_network_peers::NodeRecord;
+#[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 use std::{future::Future, net::SocketAddr, sync::Arc, time::Instant};
 
@@ -246,7 +247,8 @@ impl std::fmt::Display for Direction {
 }
 
 /// The status of the network being ran by the local node.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct NetworkStatus {
     /// The local node client version.
     pub client_version: String,
