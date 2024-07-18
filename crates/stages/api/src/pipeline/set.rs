0a7625c0f0c9944630a874112b7b16bfcc5cc063
Matthias Seitz <matthias.seitz@outlook.de>
2024-07-18 20:27:47 +0200

    chore: track_caller for functions that panic

--- crates/stages/api/src/pipeline/set.rs
+++ crates/stages/api/src/pipeline/set.rs
@@ -190,6 +190,7 @@ where
     /// # Panics
     ///
     /// Panics if the stage is not in this set.
+    #[track_caller]
     pub fn disable(mut self, stage_id: StageId) -> Self {
         let entry =
             self.stages.get_mut(&stage_id).expect("Cannot disable a stage that is not in the set.");
@@ -198,6 +199,7 @@ where
     }
 
     /// Disables all given stages. See [`disable`](Self::disable).
+    #[track_caller]
     pub fn disable_all(mut self, stages: &[StageId]) -> Self {
         for stage_id in stages {
             let entry = self
@@ -212,6 +214,7 @@ where
     /// Disables the given stage if the given closure returns true.
     ///
     /// See [`Self::disable`]
+    #[track_caller]
     pub fn disable_if<F>(self, stage_id: StageId, f: F) -> Self
     where
         F: FnOnce() -> bool,
@@ -225,6 +228,7 @@ where
     /// Disables all given stages if the given closure returns true.
     ///
     /// See [`Self::disable`]
+    #[track_caller]
     pub fn disable_all_if<F>(self, stages: &[StageId], f: F) -> Self
     where
         F: FnOnce() -> bool,
