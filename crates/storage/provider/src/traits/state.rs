9882638e8e93ee1c5df6a7709d51b9011ff1e2a1
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-07-23 17:22:15 -0400

    fix docs

--- crates/storage/provider/src/traits/state.rs
+++ crates/storage/provider/src/traits/state.rs
@@ -2,8 +2,7 @@ use reth_execution_types::ExecutionOutcome;
 use reth_storage_errors::provider::ProviderResult;
 use revm::db::OriginalValuesKnown;
 
-/// A helper trait for [`ExecutionOutcome`](reth_execution_types::ExecutionOutcome) to
-/// write state and receipts to storage.
+/// A helper trait for [`ExecutionOutcome`] to write state and receipts to storage.
 pub trait StateWriter {
     /// Write the data and receipts to the database or static files if `static_file_producer` is
     /// `Some`. It should be `None` if there is any kind of pruning/filtering over the receipts.
