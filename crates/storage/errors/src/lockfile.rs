17c776b39a760651a82b1708bf8b7e3ed90cc288
Miguel Oliveira <migueloliveiradev@gmail.com>
2024-07-24 12:35:12 -0300

    add variant of StorageLockError

--- crates/storage/errors/src/lockfile.rs
+++ crates/storage/errors/src/lockfile.rs
@@ -3,6 +3,8 @@ use reth_fs_util::FsPathError;
 #[cfg(not(feature = "std"))]
 use alloc::string::{String, ToString};
 
+use crate::provider::ProviderError;
+
 /// Storage lock error.
 #[derive(Debug, Clone, PartialEq, Eq, thiserror_no_std::Error)]
 pub enum StorageLockError {
@@ -12,11 +14,19 @@ pub enum StorageLockError {
     /// Indicates other unspecified errors.
     #[error("{0}")]
     Other(String),
+    /// Represents an error from the Provider
+    #[error("provider error: {0}")]
+    ProviderError(Box<ProviderError>),
 }
 
-/// TODO: turn into variant once `ProviderError`
 impl From<FsPathError> for StorageLockError {
     fn from(source: FsPathError) -> Self {
         Self::Other(source.to_string())
+    }
+}
+
+impl From<ProviderError> for StorageLockError {
+    fn from(source: ProviderError) -> Self {
+        Self::ProviderError(Box::new(source))
     }
 }
