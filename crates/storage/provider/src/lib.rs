41e1a4cbb503c361c262f24d5e41e4299efd1776
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-07-24 10:26:03 -0400

    feat: move hashed state and trie writing to provider (#9636)

--- crates/storage/provider/src/lib.rs
+++ crates/storage/provider/src/lib.rs
@@ -34,7 +34,10 @@ pub use reth_storage_errors::provider::{ProviderError, ProviderResult};
 pub use reth_execution_types::*;
 
 pub mod bundle_state;
-pub use bundle_state::{OriginalValuesKnown, StateChanges, StateReverts};
+pub use bundle_state::{StateChanges, StateReverts};
+
+/// Re-export `OriginalValuesKnown`
+pub use revm::db::states::OriginalValuesKnown;
 
 /// Writer standalone type.
 pub mod writer;
