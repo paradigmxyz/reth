2afd9ad7ce08bae8891a247ff821e587435e9a6b
nk_ysg <nk_ysg@163.com>
2024-07-27 12:09:08 +0800

    fix clippy or_fun_call

--- crates/engine/tree/src/tree/mod.rs
+++ crates/engine/tree/src/tree/mod.rs
@@ -154,20 +154,14 @@ impl TreeState {
 
     /// Returns the maximum block number stored.
     pub(crate) fn max_block_number(&self) -> BlockNumber {
-        *self
-            .blocks_by_number
-            .last_key_value()
-            .unwrap_or_else(|| (&BlockNumber::default(), &vec![]))
-            .0
+        let block_default = (&BlockNumber::default(), &vec![]);
+        *self.blocks_by_number.last_key_value().unwrap_or(block_default).0
     }
 
     /// Returns the minimum block number stored.
     pub(crate) fn min_block_number(&self) -> BlockNumber {
-        *self
-            .blocks_by_number
-            .first_key_value()
-            .unwrap_or_else(|| (&BlockNumber::default(), &vec![]))
-            .0
+        let block_default = (&BlockNumber::default(), &vec![]);
+        *self.blocks_by_number.first_key_value().unwrap_or(block_default).0
     }
 
     /// Returns the block number of the pending block: `head + 1`
