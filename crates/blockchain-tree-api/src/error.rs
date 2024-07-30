0d1c3bf9ef70ef5746c705f852a1051ee8074685
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-07-30 19:58:58 -0400

    fmt

--- crates/blockchain-tree-api/src/error.rs
+++ crates/blockchain-tree-api/src/error.rs
@@ -342,7 +342,9 @@ impl InsertBlockErrorKindTwo {
     ///
     /// This is intended to be used to determine if we should respond `INVALID` as a response when
     /// processing a new block.
-    pub fn ensure_validation_error(self) -> Result<InsertBlockValidationError, InsertBlockFatalError> {
+    pub fn ensure_validation_error(
+        self,
+    ) -> Result<InsertBlockValidationError, InsertBlockFatalError> {
         match self {
             Self::SenderRecovery => Ok(InsertBlockValidationError::SenderRecovery),
             Self::Consensus(err) => Ok(InsertBlockValidationError::Consensus(err)),
