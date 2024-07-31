e4496613f1361b8e8f845a7bff10eaf7d145a922
siddharth0a <maeng779@gmail.com>
2024-07-31 19:13:08 +0900

    add rollup. namespace

--- crates/optimism/node/src/args.rs
+++ crates/optimism/node/src/args.rs
@@ -31,7 +31,7 @@ pub struct RollupArgs {
     pub compute_pending_block: bool,
 
     /// Enable discv4 discovery protocol
-    #[arg(long = "discovery.v4")]
+    #[arg(long = "rollup.discovery.v4")]
     pub discovery_v4: bool,
 }
 
