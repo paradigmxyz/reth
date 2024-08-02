a134efe7343ee1a9df4c3c67a50db616c7662df8
Federico Gimenez <federico.gimenez@gmail.com>
2024-08-02 21:04:03 +0200

    use pending events queue

--- crates/engine/tree/src/download.rs
+++ crates/engine/tree/src/download.rs
@@ -9,10 +9,9 @@ use reth_network_p2p::{
     headers::client::HeadersClient,
 };
 use reth_primitives::{SealedBlock, SealedBlockWithSenders, B256};
-use reth_revm::primitives::FixedBytes;
 use std::{
     cmp::{Ordering, Reverse},
-    collections::{binary_heap::PeekMut, BinaryHeap, HashSet},
+    collections::{binary_heap::PeekMut, BinaryHeap, HashSet, VecDeque},
     sync::Arc,
     task::{Context, Poll},
 };
@@ -61,10 +60,8 @@ where
     set_buffered_blocks: BinaryHeap<Reverse<OrderedSealedBlockWithSenders>>,
     /// Engine download metrics.
     metrics: BlockDownloaderMetrics,
-    /// Set of hashes for in-flight full block requests from the previous poll
-    previous_full_block_hashes: HashSet<B256>,
-    /// Set of (`start_hash`, count) for in-flight block range requests from the previous poll
-    previous_block_range_requests: HashSet<(B256, u64)>,
+    /// Pending events to be emitted.
+    pending_events: VecDeque<DownloadOutcome>,
 }
 
 impl<Client> BasicBlockDownloader<Client>
@@ -79,8 +76,7 @@ where
             inflight_block_range_requests: Vec::new(),
             set_buffered_blocks: BinaryHeap::new(),
             metrics: BlockDownloaderMetrics::default(),
-            previous_block_range_requests: Default::default(),
-            previous_full_block_hashes: Default::default(),
+            pending_events: Default::default(),
         }
     }
 
@@ -120,6 +116,10 @@ where
             );
 
             let request = self.full_block_client.get_full_block_range(hash, count);
+            self.push_pending_event(DownloadOutcome::NewDownloadStarted {
+                remaining_blocks: request.count(),
+                target: request.start_hash(),
+            });
             self.inflight_block_range_requests.push(request);
         }
     }
@@ -132,6 +132,11 @@ where
         if self.is_inflight_request(hash) {
             return false
         }
+        self.push_pending_event(DownloadOutcome::NewDownloadStarted {
+            remaining_blocks: 1,
+            target: hash,
+        });
+
         trace!(
             target: "consensus::engine::sync",
             ?hash,
@@ -157,43 +162,14 @@ where
         // TODO: full block range metrics
     }
 
-    /// Check if there are any new requests in `inflight_full_block_requests`
-    /// or `inflight_block_range_requests` that weren't there in the previous poll.
-    /// Returns the number of remaining blocks and the target block hash.
-    fn check_new_downloads(&mut self) -> Option<(u64, B256)> {
-        // Check for new full block requests
-        for request in &self.inflight_full_block_requests {
-            let hash = *request.hash();
-            if !self.previous_full_block_hashes.contains(&hash) {
-                self.previous_full_block_hashes.insert(hash);
-                return Some((1, hash));
-            }
-        }
-
-        // Check for new block range requests
-        for request in &self.inflight_block_range_requests {
-            let start_hash = FixedBytes(*request.start_hash());
-            let count = request.count();
-            if !self.previous_block_range_requests.contains(&(start_hash, count)) {
-                self.previous_block_range_requests.insert((start_hash, count));
-                return Some((count, start_hash));
-            }
-        }
-
-        None
+    /// Adds a pending event to the FIFO queue.
+    fn push_pending_event(&mut self, pending_event: DownloadOutcome) {
+        self.pending_events.push_back(pending_event);
     }
 
-    fn update_previous_requests(&mut self) {
-        self.previous_full_block_hashes.clear();
-        self.previous_full_block_hashes
-            .extend(self.inflight_full_block_requests.iter().map(|req| *req.hash()));
-
-        self.previous_block_range_requests.clear();
-        self.previous_block_range_requests.extend(
-            self.inflight_block_range_requests
-                .iter()
-                .map(|req| (B256::from(*req.start_hash()), req.count())),
-        );
+    /// Removes a pending event from the FIFO queue.
+    fn pop_pending_event(&mut self) -> Option<DownloadOutcome> {
+        self.pending_events.pop_front()
     }
 }
 
@@ -252,10 +228,9 @@ where
             return Poll::Pending;
         }
 
-        if let Some((remaining_blocks, target)) = self.check_new_downloads() {
-            return Poll::Ready(DownloadOutcome::NewDownloadStarted { remaining_blocks, target });
+        if let Some(pending_event) = self.pop_pending_event() {
+            return Poll::Ready(pending_event);
         }
-        self.update_previous_requests();
 
         // drain all unique element of the block buffer if there are any
         let mut downloaded_blocks: Vec<SealedBlockWithSenders> =
