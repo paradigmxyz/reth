210e47086bae664cfe7d677e39fa4e2f8a095cb1
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-08-05 14:16:40 +0100

    sort crates into not yet / ever

--- .github/assets/check_wasm.sh
+++ .github/assets/check_wasm.sh
@@ -5,15 +5,80 @@ set +e  # Disable immediate exit on error
 crates=($(cargo metadata --format-version=1 --no-deps | jq -r '.packages[].name' | grep '^reth' | sort))
 # Array of crates to exclude
 exclude_crates=(
-  # The following are not working yet
+  # The following are not working yet, but known to be fixable
+  reth-consensus
+  reth-exex-types # https://github.com/paradigmxyz/reth/issues/9946
+  reth-evm
+  # The following require investigation if they can be fixed
+  reth-auto-seal-consensus
+  reth-basic-payload-builder
+  reth-beacon-consensus
+  reth-bench
+  reth-blockchain-tree
+  reth-chain-state
+  reth-cli
+  reth-cli-commands
+  reth-cli-runner
+  reth-consensus-debug-client
+  reth-db-common
+  reth-discv4
+  reth-discv5
+  reth-dns-discovery
+  reth-downloaders
+  reth-e2e-test-utils
+  reth-engine-primitives
+  reth-engine-tree
+  reth-engine-util
+  reth-eth-wire
+  reth-ethereum-cli
+  reth-ethereum-engine
+  reth-ethereum-engine-primitives
+  reth-ethereum-payload-builder
+  reth-etl
   reth-evm-ethereum
+  reth-evm-optimism
+  reth-execution-errors
+  reth-exex
+  reth-exex-test-utils
+  reth-ipc
+  reth-net-nat
+  reth-network
+  reth-node-api
+  reth-node-builder
+  reth-node-core
+  reth-node-ethereum
+  reth-node-events
+  reth-node-metrics
+  reth-node-optimism
+  reth-optimism-cli
+  reth-optimism-payload-builder
+  reth-optimism-rpc
+  reth-payload-builder
+  reth-payload-primitives
+  reth-rpc
+  reth-rpc-api
+  reth-rpc-api-testing-util
+  reth-rpc-builder
+  reth-rpc-engine-api
+  reth-rpc-eth-api
+  reth-rpc-eth-types
+  reth-rpc-layer
+  reth-rpc-types
+  reth-stages
   reth-storage-errors
   # The following are not supposed to be working
-  reth-mdbx # database
-  reth-mdbx-sys # database
-  reth-static-file # sucds
+  reth # all of the crates below 
+  reth-db # mdbx
+  reth-libmdbx # mdbx
+  reth-mdbx-sys # mdbx
+  reth-nippy-jar # sucds
+  reth-provider # reth-db, reth-nippy-jar
+  reth-prune # reth-db
+  reth-stages-api # reth-provider, reth-prune
+  reth-static-file # reth-nippy-jar 
   reth-transaction-pool # c-kzg
-  reth-trie-db # database
+  reth-trie-db # reth-db 
+  reth-trie-parallel # reth-db
 )
 
 # Array to hold the results
