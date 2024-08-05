cf9b6bd9e33da77f4b3762c0da522f24ee9846f0
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-08-05 15:21:13 +0100

    feat(evm): support `wasm32-wasip1` (#10083)

--- crates/evm/execution-types/src/lib.rs
+++ crates/evm/execution-types/src/lib.rs
@@ -8,6 +8,9 @@
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
+#[cfg(not(feature = "std"))]
+extern crate alloc;
+
 mod chain;
 pub use chain::*;
 
