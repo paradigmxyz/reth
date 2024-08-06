33eece9d3b98435764549aef289dc368783c10b1
Nikolai Golub <nikolai@sovlabs.io>
2024-08-06 17:19:58 +0200

    Check features powerset for `reth-primitives` (#10138)

--- crates/primitives/src/constants/eip4844.rs
+++ crates/primitives/src/constants/eip4844.rs
@@ -1,5 +1,5 @@
 //! [EIP-4844](https://eips.ethereum.org/EIPS/eip-4844#parameters) protocol constants and utils for shard Blob Transactions.
-#[cfg(feature = "c-kzg")]
+#[cfg(all(feature = "c-kzg", feature = "std"))]
 pub use trusted_setup::*;
 
 pub use alloy_eips::eip4844::{
@@ -7,6 +7,12 @@ pub use alloy_eips::eip4844::{
     FIELD_ELEMENTS_PER_BLOB, FIELD_ELEMENT_BYTES, MAX_BLOBS_PER_BLOCK, MAX_DATA_GAS_PER_BLOCK,
     TARGET_BLOBS_PER_BLOCK, TARGET_DATA_GAS_PER_BLOCK, VERSIONED_HASH_VERSION_KZG,
 };
+
+// These 2 to silence unused
+#[cfg(all(feature = "c-kzg", not(feature = "std")))]
+use tempfile as _;
+#[cfg(all(not(feature = "c-kzg"), feature = "std"))]
+use thiserror as _;
 
 #[cfg(all(feature = "c-kzg", feature = "std"))]
 mod trusted_setup {
@@ -26,7 +32,7 @@ mod trusted_setup {
     }
 
     /// Error type for loading the trusted setup.
-    #[derive(Debug, thiserror_no_std::Error)]
+    #[derive(Debug, thiserror::Error)]
     pub enum LoadKzgSettingsError {
         /// Failed to create temp file to store bytes for loading [`KzgSettings`] via
         /// [`KzgSettings::load_trusted_setup_file`].
