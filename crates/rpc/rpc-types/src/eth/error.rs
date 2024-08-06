d66e199c1636fdc805a1a026aba0f86b0b6691a5
Nikolai Golub <nikolay.v.golub@gmail.com>
2024-08-06 12:52:35 +0200

    Fixes

--- crates/rpc/rpc-types/src/eth/error.rs
+++ crates/rpc/rpc-types/src/eth/error.rs
@@ -6,3 +6,10 @@ pub trait ToRpcError: std::error::Error + Send + Sync + 'static {
     /// Converts the error to a JSON-RPC error object.
     fn to_rpc_error(&self) -> jsonrpsee_types::ErrorObject<'static>;
 }
+
+#[cfg(feature = "default")]
+impl ToRpcError for jsonrpsee_types::ErrorObject<'static> {
+    fn to_rpc_error(&self) -> jsonrpsee_types::ErrorObject<'static> {
+        self.clone()
+    }
+}
