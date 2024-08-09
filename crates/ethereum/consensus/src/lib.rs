88d0cc4917a88156e61b1b59549c4b7ad100ff48
Mikhail Sozin <mikhail.sozin@chainstack.com>
2024-08-09 11:26:01 +0200

    lint and staff

--- crates/ethereum/consensus/src/lib.rs
+++ crates/ethereum/consensus/src/lib.rs
@@ -10,15 +10,22 @@
 
 use reth_chainspec::{ChainSpec, EthereumHardfork, EthereumHardforks};
 use reth_consensus::{Consensus, ConsensusError, PostExecutionInput};
-use reth_consensus_common::validation::{
-    validate_4844_header_standalone, validate_against_parent_4844,
-    validate_against_parent_eip1559_base_fee, validate_against_parent_hash_number,
-    validate_against_parent_timestamp, validate_block_pre_execution, validate_header_base_fee,
-    validate_header_extradata, validate_header_gas,
+use reth_consensus_common::{
+    difficulty::{
+        calc_difficulty_frontier, calc_difficulty_generic, calc_difficulty_homestead, BombDelay,
+    },
+    validation::{
+        validate_4844_header_standalone, validate_against_parent_4844,
+        validate_against_parent_eip1559_base_fee, validate_against_parent_hash_number,
+        validate_against_parent_timestamp, validate_block_pre_execution, validate_header_base_fee,
+        validate_header_extradata, validate_header_gas,
+    },
 };
-use reth_primitives::{constants::MINIMUM_GAS_LIMIT, BlockWithSenders, Header, SealedBlock, SealedHeader, EMPTY_OMMER_ROOT_HASH, U256, GotExpected};
-use std::{sync::Arc, time::SystemTime};
-use reth_consensus_common::difficulty::{BombDelay, calc_difficulty_frontier, calc_difficulty_generic, calc_difficulty_homestead};
+use reth_primitives::{
+    constants::MINIMUM_GAS_LIMIT, BlockWithSenders, GotExpected, Header, SealedBlock, SealedHeader,
+    EMPTY_OMMER_ROOT_HASH, U256,
+};
+use std::{cmp::Ordering, sync::Arc, time::SystemTime};
 
 mod validation;
 pub use validation::validate_block_post_execution;
@@ -85,31 +92,58 @@ impl EthBeaconConsensus {
     fn validate_difficuty_increment(
         &self,
         header: &Header,
-        parent: &Header
+        parent: &Header,
     ) -> Result<(), ConsensusError> {
         // Check difficulty for the block base on the current hardfork
         let calculated_difficulty;
 
         if self.chain_spec.fork(EthereumHardfork::Byzantium).active_at_block(header.number) {
-            calculated_difficulty = calc_difficulty_generic(header.timestamp, parent, BombDelay::Byzantium).map_err(|_| ConsensusError::DifficultyCalculationError)?;
-        } else if self.chain_spec.fork(EthereumHardfork::Constantinople).active_at_block(header.number) {
-            calculated_difficulty = calc_difficulty_generic(header.timestamp, parent, BombDelay::Constantinople).map_err(|_| ConsensusError::DifficultyCalculationError)?;
-        } else if self.chain_spec.fork(EthereumHardfork::MuirGlacier).active_at_block(header.number) {
-            calculated_difficulty = calc_difficulty_generic(header.timestamp, parent, BombDelay::Eip2384).map_err(|_| ConsensusError::DifficultyCalculationError)?;
+            calculated_difficulty =
+                calc_difficulty_generic(header.timestamp, parent, BombDelay::Byzantium)
+                    .map_err(|_| ConsensusError::DifficultyCalculationError)?;
+        } else if self
+            .chain_spec
+            .fork(EthereumHardfork::Constantinople)
+            .active_at_block(header.number)
+        {
+            calculated_difficulty =
+                calc_difficulty_generic(header.timestamp, parent, BombDelay::Constantinople)
+                    .map_err(|_| ConsensusError::DifficultyCalculationError)?;
+        } else if self.chain_spec.fork(EthereumHardfork::MuirGlacier).active_at_block(header.number)
+        {
+            calculated_difficulty =
+                calc_difficulty_generic(header.timestamp, parent, BombDelay::Eip2384)
+                    .map_err(|_| ConsensusError::DifficultyCalculationError)?;
         } else if self.chain_spec.fork(EthereumHardfork::London).active_at_block(header.number) {
-            calculated_difficulty = calc_difficulty_generic(header.timestamp, parent, BombDelay::Eip3554).map_err(|_| ConsensusError::DifficultyCalculationError)?;
-        } else if self.chain_spec.fork(EthereumHardfork::ArrowGlacier).active_at_block(header.number) {
-            calculated_difficulty = calc_difficulty_generic(header.timestamp, parent, BombDelay::Eip4345).map_err(|_| ConsensusError::DifficultyCalculationError)?;
-        } else if self.chain_spec.fork(EthereumHardfork::GrayGlacier).active_at_block(header.number) {
-            calculated_difficulty = calc_difficulty_generic(header.timestamp, parent, BombDelay::Eip5133).map_err(|_| ConsensusError::DifficultyCalculationError)?;
+            calculated_difficulty =
+                calc_difficulty_generic(header.timestamp, parent, BombDelay::Eip3554)
+                    .map_err(|_| ConsensusError::DifficultyCalculationError)?;
+        } else if self
+            .chain_spec
+            .fork(EthereumHardfork::ArrowGlacier)
+            .active_at_block(header.number)
+        {
+            calculated_difficulty =
+                calc_difficulty_generic(header.timestamp, parent, BombDelay::Eip4345)
+                    .map_err(|_| ConsensusError::DifficultyCalculationError)?;
+        } else if self.chain_spec.fork(EthereumHardfork::GrayGlacier).active_at_block(header.number)
+        {
+            calculated_difficulty =
+                calc_difficulty_generic(header.timestamp, parent, BombDelay::Eip5133)
+                    .map_err(|_| ConsensusError::DifficultyCalculationError)?;
         } else if self.chain_spec.fork(EthereumHardfork::Homestead).active_at_block(header.number) {
-            calculated_difficulty = calc_difficulty_homestead(header.timestamp, parent).map_err(|_| ConsensusError::DifficultyCalculationError)?;
+            calculated_difficulty = calc_difficulty_homestead(header.timestamp, parent)
+                .map_err(|_| ConsensusError::DifficultyCalculationError)?;
         } else {
-            calculated_difficulty = calc_difficulty_frontier(header.timestamp, parent).map_err(|_| ConsensusError::DifficultyCalculationError)?;
+            calculated_difficulty = calc_difficulty_frontier(header.timestamp, parent)
+                .map_err(|_| ConsensusError::DifficultyCalculationError)?;
         }
 
-        if !header.difficulty.cmp(&calculated_difficulty) {
-            return Err(ConsensusError::DifficultyDiff(GotExpected::new(header.difficulty, calculated_difficulty)));
+        if header.difficulty.cmp(&calculated_difficulty) != Ordering::Equal {
+            return Err(ConsensusError::DifficultyDiff(GotExpected::new(
+                header.difficulty,
+                calculated_difficulty,
+            )));
         }
 
         Ok(())
