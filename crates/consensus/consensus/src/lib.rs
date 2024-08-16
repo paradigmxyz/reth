b54d9a20b0a75dd6fabfb3528169da8aac4ddc21
Mikhail Sozin <mikhail.sozin@chainstack.com>
2024-08-16 22:10:29 +0200

    linter

--- crates/consensus/consensus/src/lib.rs
+++ crates/consensus/consensus/src/lib.rs
@@ -394,10 +394,10 @@ pub enum ConsensusError {
         timestamp: u64,
     },
     /// Error when arithmetical error during difficulty calculation was encountered
-    #[error("unexpected error during difficulty calculation")]
+    #[display(fmt = "unexpected error during difficulty calculation")]
     DifficultyCalculationError,
     /// Error when the block's difficulty is different from the expected difficulty.
-    #[error("block difficulty mismatch: {0}")]
+    #[display(fmt = "block difficulty mismatch: {_0}")]
     DifficultyDiff(GotExpected<U256>),
 }
 
