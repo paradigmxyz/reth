4712389fabd10e9d5cb521f28f9da37fd06afd4b
nk_ysg <nk_ysg@163.com>
2024-08-22 20:04:18 +0800

    feat: make js-feature  non default (#10447)

--- crates/rpc/rpc/src/debug.rs
+++ crates/rpc/rpc/src/debug.rs
@@ -34,8 +34,7 @@ use revm::{
     StateBuilder,
 };
 use revm_inspectors::tracing::{
-    js::{JsInspector, TransactionContext},
-    FourByteInspector, MuxInspector, TracingInspector, TracingInspectorConfig,
+    FourByteInspector, MuxInspector, TracingInspector, TracingInspectorConfig, TransactionContext,
 };
 use revm_primitives::{keccak256, HashMap};
 use std::sync::Arc;
@@ -388,6 +387,11 @@ where
                         return Ok(frame)
                     }
                 },
+                #[cfg(not(feature = "js-tracer"))]
+                GethDebugTracerType::JsTracer(_) => {
+                    Err(EthApiError::Unsupported("JS Tracer is not enabled").into())
+                }
+                #[cfg(feature = "js-tracer")]
                 GethDebugTracerType::JsTracer(code) => {
                     let config = tracer_config.into_json();
 
@@ -402,7 +406,8 @@ where
                             let db = db.0;
 
                             let mut inspector =
-                                JsInspector::new(code, config).map_err(Eth::Error::from_eth_err)?;
+                                revm_inspectors::tracing::js::JsInspector::new(code, config)
+                                    .map_err(Eth::Error::from_eth_err)?;
                             let (res, _) =
                                 this.eth_api().inspect(&mut *db, env.clone(), &mut inspector)?;
                             inspector.json_result(res, &env, db).map_err(Eth::Error::from_eth_err)
@@ -671,7 +676,8 @@ where
         opts: GethDebugTracingOptions,
         env: EnvWithHandlerCfg,
         db: &mut StateCacheDb<'_>,
-        transaction_context: Option<TransactionContext>,
+        #[cfg(not(feature = "js-tracer"))] _transaction_context: Option<TransactionContext>,
+        #[cfg(feature = "js-tracer")] transaction_context: Option<TransactionContext>,
     ) -> Result<(GethTrace, revm_primitives::EvmState), Eth::Error> {
         let GethDebugTracingOptions { config, tracer, tracer_config, .. } = opts;
 
@@ -737,14 +743,20 @@ where
                         return Ok((frame.into(), res.state))
                     }
                 },
+                #[cfg(not(feature = "js-tracer"))]
+                GethDebugTracerType::JsTracer(_) => {
+                    Err(EthApiError::Unsupported("JS Tracer is not enabled").into())
+                }
+                #[cfg(feature = "js-tracer")]
                 GethDebugTracerType::JsTracer(code) => {
                     let config = tracer_config.into_json();
-                    let mut inspector = JsInspector::with_transaction_context(
-                        code,
-                        config,
-                        transaction_context.unwrap_or_default(),
-                    )
-                    .map_err(Eth::Error::from_eth_err)?;
+                    let mut inspector =
+                        revm_inspectors::tracing::js::JsInspector::with_transaction_context(
+                            code,
+                            config,
+                            transaction_context.unwrap_or_default(),
+                        )
+                        .map_err(Eth::Error::from_eth_err)?;
                     let (res, env) = self.eth_api().inspect(&mut *db, env, &mut inspector)?;
 
                     let state = res.state.clone();
