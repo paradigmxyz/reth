313eb0ee2fa29049566347790e1b3aa566141a6a
Matthias Seitz <matthias.seitz@outlook.de>
2024-08-24 10:44:47 +0200

    clippy

--- crates/rpc/rpc-types-compat/src/transaction/mod.rs
+++ crates/rpc/rpc-types-compat/src/transaction/mod.rs
@@ -109,7 +109,7 @@ fn fill(
             mint: signed_tx.mint(),
             // only include is_system_tx if true: <https://github.com/ethereum-optimism/op-geth/blob/641e996a2dcf1f81bac9416cb6124f86a69f1de7/internal/ethapi/api.go#L1518-L1518>
             is_system_tx: (signed_tx.is_deposit() && signed_tx.is_system_transaction())
-                .then(|| true),
+                .then_some(true),
         }
         .into(),
         #[cfg(not(feature = "optimism"))]
