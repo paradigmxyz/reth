48c118fcf2b3ad5e23b1ec145f5942af9dee81c2
Md Amaan <intellectance022@gmail.com>
2024-08-25 23:30:21 +0530

    removed op-alloy types

--- crates/optimism/rpc/src/api.rs
+++ crates/optimism/rpc/src/api.rs
@@ -6,53 +6,6 @@ use reth_rpc_types::{BlockId, BlockNumberOrTag};
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, net::IpAddr};
 
-// https://github.com/ethereum-optimism/optimism/blob/develop/op-service/eth/id.go#L33
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct L2BlockRef {
-    pub hash: B256,
-    pub number: BlockNumber,
-    pub parent_hash: B256,
-    pub timestamp: u64,
-    pub l1origin: BlockId,
-    pub sequence_number: u64,
-}
-
-// https://github.com/ethereum-optimism/optimism/blob/develop/op-service/eth/id.go#L52
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct L1BlockRef {
-    pub hash: B256,
-    pub number: BlockNumber,
-    pub parent_hash: B256,
-    pub timestamp: u64,
-}
-
-// https://github.com/ethereum-optimism/optimism/blob/develop/op-service/eth/sync_status.go#L5
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct SyncStatus {
-    pub current_l1: L1BlockRef,
-    pub current_l1_finalized: L1BlockRef,
-    pub head_l1: L1BlockRef,
-    pub safe_l1: L1BlockRef,
-    pub finalized_l1: L1BlockRef,
-    pub unsafe_l2: L2BlockRef,
-    pub safe_l2: L2BlockRef,
-    pub finalized_l2: L2BlockRef,
-    pub pending_safe_l2: L2BlockRef,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct OutputResponse {
-    pub version: B256,
-    pub output_root: B256,
-    pub block_ref: L2BlockRef,
-    pub withdrawal_storage_root: B256,
-    pub state_root: B256,
-    pub sync_status: SyncStatus,
-}
-
 // https://github.com/ethereum-optimism/optimism/blob/c7ad0ebae5dca3bf8aa6f219367a95c15a15ae41/op-service/eth/types.go#L371
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
