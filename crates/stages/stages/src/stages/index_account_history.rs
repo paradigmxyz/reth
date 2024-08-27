3fb9f197e0421a2ea497feaf2ad85c171dac5985
Jennifer <jennifer@chorus.one>
2024-08-27 15:19:43 +0100

    refactor(provider): random_block and random_block_range functions (#10563)

--- crates/stages/stages/src/stages/index_account_history.rs
+++ crates/stages/stages/src/stages/index_account_history.rs
@@ -158,9 +158,9 @@ mod tests {
     };
     use reth_primitives::{address, BlockNumber, B256};
     use reth_provider::providers::StaticFileWriter;
-    use reth_testing_utils::{
-        generators,
-        generators::{random_block_range, random_changeset_range, random_contract_account_range},
+    use reth_testing_utils::generators::{
+        self, random_block_range, random_changeset_range, random_contract_account_range,
+        BlockRangeParams,
     };
     use std::collections::BTreeMap;
 
@@ -538,7 +538,11 @@ mod tests {
                 .into_iter()
                 .collect::<BTreeMap<_, _>>();
 
-            let blocks = random_block_range(&mut rng, start..=end, B256::ZERO, 0..3, None, None);
+            let blocks = random_block_range(
+                &mut rng,
+                start..=end,
+                BlockRangeParams { parent: Some(B256::ZERO), tx_count: 0..3, ..Default::default() },
+            );
 
             let (changesets, _) = random_changeset_range(
                 &mut rng,
