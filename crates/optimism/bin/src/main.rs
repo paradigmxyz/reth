29b77db43639fa27d42d22688731cd769a230cf8
Arsenii Kulikov <klkvrr@gmail.com>
2024-09-01 02:49:28 +0400

    fixes

--- crates/optimism/bin/src/main.rs
+++ crates/optimism/bin/src/main.rs
@@ -1,16 +1,14 @@
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![allow(missing_docs, rustdoc::missing_crate_level_docs)]
 // The `optimism` feature must be enabled to use this crate.
 #![cfg(feature = "optimism")]
 
 use clap::Parser;
-use reth::cli::Cli;
 use reth_node_builder::EngineNodeLauncher;
-use reth_node_optimism::{
-    args::RollupArgs, node::OptimismAddOns, rpc::SequencerClient, OptimismNode,
-};
+use reth_node_optimism::{args::RollupArgs, node::OptimismAddOns, OptimismNode};
+use reth_optimism_cli::Cli;
 use reth_optimism_rpc::eth::rpc::SequencerClient;
 use reth_provider::providers::BlockchainProvider2;
-use std::sync::Arc;
 
 // We use jemalloc for performance reasons
 #[cfg(all(feature = "jemalloc", unix))]
@@ -18,9 +16,6 @@ use std::sync::Arc;
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 fn main() {
-    use reth::args::utils::DefaultChainSpecParser;
-    use reth_optimism_cli::Cli;
-
     reth_cli_util::sigsegv_handler::install();
 
     // Enable backtraces unless a RUST_BACKTRACE value has already been explicitly provided.
@@ -28,58 +23,56 @@ fn main() {
         std::env::set_var("RUST_BACKTRACE", "1");
     }
 
-    if let Err(err) =
-        Cli::<DefaultChainSpecParser, RollupArgs>::parse().run(|builder, rollup_args| async move {
-            let enable_engine2 = rollup_args.experimental;
-            let sequencer_http_arg = rollup_args.sequencer_http.clone();
-            match enable_engine2 {
-                true => {
-                    let handle = builder
-                        .with_types_and_provider::<OptimismNode, BlockchainProvider2<_>>()
-                        .with_components(OptimismNode::components(rollup_args))
-                        .with_add_ons::<OptimismAddOns>()
-                        .extend_rpc_modules(move |ctx| {
-                            // register sequencer tx forwarder
-                            if let Some(sequencer_http) = sequencer_http_arg {
-                                ctx.registry
-                                    .eth_api()
-                                    .set_sequencer_client(SequencerClient::new(sequencer_http));
-                            }
+    if let Err(err) = Cli::<RollupArgs>::parse().run(|builder, rollup_args| async move {
+        let enable_engine2 = rollup_args.experimental;
+        let sequencer_http_arg = rollup_args.sequencer_http.clone();
+        match enable_engine2 {
+            true => {
+                let handle = builder
+                    .with_types_and_provider::<OptimismNode, BlockchainProvider2<_>>()
+                    .with_components(OptimismNode::components(rollup_args))
+                    .with_add_ons::<OptimismAddOns>()
+                    .extend_rpc_modules(move |ctx| {
+                        // register sequencer tx forwarder
+                        if let Some(sequencer_http) = sequencer_http_arg {
+                            ctx.registry
+                                .eth_api()
+                                .set_sequencer_client(SequencerClient::new(sequencer_http));
+                        }
 
-                            Ok(())
-                        })
-                        .launch_with_fn(|builder| {
-                            let launcher = EngineNodeLauncher::new(
-                                builder.task_executor().clone(),
-                                builder.config().datadir(),
-                            );
-                            builder.launch_with(launcher)
-                        })
-                        .await?;
+                        Ok(())
+                    })
+                    .launch_with_fn(|builder| {
+                        let launcher = EngineNodeLauncher::new(
+                            builder.task_executor().clone(),
+                            builder.config().datadir(),
+                        );
+                        builder.launch_with(launcher)
+                    })
+                    .await?;
 
-                    handle.node_exit_future.await
-                }
-                false => {
-                    let handle = builder
-                        .node(OptimismNode::new(rollup_args.clone()))
-                        .extend_rpc_modules(move |ctx| {
-                            // register sequencer tx forwarder
-                            if let Some(sequencer_http) = sequencer_http_arg {
-                                ctx.registry
-                                    .eth_api()
-                                    .set_sequencer_client(SequencerClient::new(sequencer_http));
-                            }
-
-                            Ok(())
-                        })
-                        .launch()
-                        .await?;
-
-                    handle.node_exit_future.await
-                }
+                handle.node_exit_future.await
             }
-        })
-    {
+            false => {
+                let handle = builder
+                    .node(OptimismNode::new(rollup_args.clone()))
+                    .extend_rpc_modules(move |ctx| {
+                        // register sequencer tx forwarder
+                        if let Some(sequencer_http) = sequencer_http_arg {
+                            ctx.registry
+                                .eth_api()
+                                .set_sequencer_client(SequencerClient::new(sequencer_http));
+                        }
+
+                        Ok(())
+                    })
+                    .launch()
+                    .await?;
+
+                handle.node_exit_future.await
+            }
+        }
+    }) {
         eprintln!("Error: {err:?}");
         std::process::exit(1);
     }
