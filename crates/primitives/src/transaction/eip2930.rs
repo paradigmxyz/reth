6a11090718dcb2480acf8a61ce8c6e0b3367a35f
Thomas Coratger <thomas.coratger@gmail.com>
2024-08-31 23:11:45 +0200

    add tests back

--- crates/primitives/src/transaction/eip2930.rs
+++ crates/primitives/src/transaction/eip2930.rs
@@ -1,1 +1,60 @@
 pub use alloy_consensus::transaction::TxEip2930;
+
+#[cfg(test)]
+mod tests {
+    use super::TxEip2930;
+    use crate::{
+        transaction::{signature::Signature, TxKind},
+        Address, Bytes, Transaction, TransactionSigned, U256,
+    };
+    use alloy_rlp::{Decodable, Encodable};
+
+    #[test]
+    fn test_decode_create() {
+        // tests that a contract creation tx encodes and decodes properly
+        let request = Transaction::Eip2930(TxEip2930 {
+            chain_id: 1u64,
+            nonce: 0,
+            gas_price: 1,
+            gas_limit: 2,
+            to: TxKind::Create,
+            value: U256::from(3),
+            input: Bytes::from(vec![1, 2]),
+            access_list: Default::default(),
+        });
+        let signature = Signature { odd_y_parity: true, r: U256::default(), s: U256::default() };
+        let tx = TransactionSigned::from_transaction_and_signature(request, signature);
+
+        let mut encoded = Vec::new();
+        tx.encode(&mut encoded);
+        assert_eq!(encoded.len(), tx.length());
+
+        let decoded = TransactionSigned::decode(&mut &*encoded).unwrap();
+        assert_eq!(decoded, tx);
+    }
+
+    #[test]
+    fn test_decode_call() {
+        let request = Transaction::Eip2930(TxEip2930 {
+            chain_id: 1u64,
+            nonce: 0,
+            gas_price: 1,
+            gas_limit: 2,
+            to: Address::default().into(),
+            value: U256::from(3),
+            input: Bytes::from(vec![1, 2]),
+            access_list: Default::default(),
+        });
+
+        let signature = Signature { odd_y_parity: true, r: U256::default(), s: U256::default() };
+
+        let tx = TransactionSigned::from_transaction_and_signature(request, signature);
+
+        let mut encoded = Vec::new();
+        tx.encode(&mut encoded);
+        assert_eq!(encoded.len(), tx.length());
+
+        let decoded = TransactionSigned::decode(&mut &*encoded).unwrap();
+        assert_eq!(decoded, tx);
+    }
+}
