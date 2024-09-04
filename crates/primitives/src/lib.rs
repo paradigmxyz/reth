710676ba0fa6cacb46a33a7c421737778d1419ce
Thomas Coratger <thomas.coratger@gmail.com>
2024-09-04 18:10:35 +0200

    fix cfg in primitives

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -63,6 +63,8 @@ pub use transaction::{
     PooledTransactionsElementEcRecovered,
 };
 
+#[cfg(feature = "optimism")]
+use reth_codecs as _;
 #[cfg(feature = "c-kzg")]
 pub use transaction::BlobTransactionValidationError;
 
