257112d6ce619ceb81b8421e967c93424b459483
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-09-05 15:21:48 +0200

    test(op): add test for assembling of op tx receipt RPC response (#10620)

--- crates/ethereum-forks/src/hardforks/optimism.rs
+++ crates/ethereum-forks/src/hardforks/optimism.rs
@@ -12,6 +12,11 @@ pub trait OptimismHardforks: EthereumHardforks {
     fn is_ecotone_active_at_timestamp(&self, timestamp: u64) -> bool {
         self.fork(OptimismHardfork::Ecotone).active_at_timestamp(timestamp)
     }
+
+    /// Returns `true` if [`Ecotone`](OptimismHardfork::Ecotone) is active at given block timestamp.
+    fn is_fjord_active_at_timestamp(&self, timestamp: u64) -> bool {
+        self.fork(OptimismHardfork::Ecotone).active_at_timestamp(timestamp)
+    }
 }
 
 impl OptimismHardforks for ChainHardforks {}
