4ffc1b11a5b1252ccd77f530c963e61da0a2879b
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-09-05 19:16:28 +0200

    Disable network flag --net-if for windows

--- crates/node/core/src/args/network.rs
+++ crates/node/core/src/args/network.rs
@@ -152,6 +152,7 @@ pub struct NetworkArgs {
     /// Name of network interface used to communicate with peers.
     ///
     /// If flags is set, but no value is passed, the default interface for docker `eth0` is tried.
+    #[cfg(not(target_os = "windows"))]
     #[arg(long = "net-if", conflicts_with = "addr")]
     pub net_if: Option<String>,
 }
