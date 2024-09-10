234da69f78d2b9ee8dc7946c0f7d78155ae226e5
Matthias Seitz <matthias.seitz@outlook.de>
2024-09-10 12:39:02 +0200

    feat: support custom chainspec parser for op

--- crates/optimism/bin/src/main.rs
+++ crates/optimism/bin/src/main.rs
@@ -6,7 +6,7 @@
 use clap::Parser;
 use reth_node_builder::EngineNodeLauncher;
 use reth_node_optimism::{args::RollupArgs, node::OptimismAddOns, OptimismNode};
-use reth_optimism_cli::Cli;
+use reth_optimism_cli::{chainspec::OpChainSpecParser, Cli};
 use reth_optimism_rpc::eth::rpc::SequencerClient;
 use reth_provider::providers::BlockchainProvider2;
 
@@ -23,56 +23,58 @@ fn main() {
         std::env::set_var("RUST_BACKTRACE", "1");
     }
 
-    if let Err(err) = Cli::<RollupArgs>::parse().run(|builder, rollup_args| async move {
-        let enable_engine2 = rollup_args.experimental;
-        let sequencer_http_arg = rollup_args.sequencer_http.clone();
-        match enable_engine2 {
-            true => {
-                let handle = builder
-                    .with_types_and_provider::<OptimismNode, BlockchainProvider2<_>>()
-                    .with_components(OptimismNode::components(rollup_args))
-                    .with_add_ons::<OptimismAddOns>()
-                    .extend_rpc_modules(move |ctx| {
-                        // register sequencer tx forwarder
-                        if let Some(sequencer_http) = sequencer_http_arg {
-                            ctx.registry
-                                .eth_api()
-                                .set_sequencer_client(SequencerClient::new(sequencer_http));
-                        }
+    if let Err(err) =
+        Cli::<OpChainSpecParser, RollupArgs>::parse().run(|builder, rollup_args| async move {
+            let enable_engine2 = rollup_args.experimental;
+            let sequencer_http_arg = rollup_args.sequencer_http.clone();
+            match enable_engine2 {
+                true => {
+                    let handle = builder
+                        .with_types_and_provider::<OptimismNode, BlockchainProvider2<_>>()
+                        .with_components(OptimismNode::components(rollup_args))
+                        .with_add_ons::<OptimismAddOns>()
+                        .extend_rpc_modules(move |ctx| {
+                            // register sequencer tx forwarder
+                            if let Some(sequencer_http) = sequencer_http_arg {
+                                ctx.registry
+                                    .eth_api()
+                                    .set_sequencer_client(SequencerClient::new(sequencer_http));
+                            }
 
-                        Ok(())
-                    })
-                    .launch_with_fn(|builder| {
-                        let launcher = EngineNodeLauncher::new(
-                            builder.task_executor().clone(),
-                            builder.config().datadir(),
-                        );
-                        builder.launch_with(launcher)
-                    })
-                    .await?;
+                            Ok(())
+                        })
+                        .launch_with_fn(|builder| {
+                            let launcher = EngineNodeLauncher::new(
+                                builder.task_executor().clone(),
+                                builder.config().datadir(),
+                            );
+                            builder.launch_with(launcher)
+                        })
+                        .await?;
 
-                handle.node_exit_future.await
+                    handle.node_exit_future.await
+                }
+                false => {
+                    let handle = builder
+                        .node(OptimismNode::new(rollup_args.clone()))
+                        .extend_rpc_modules(move |ctx| {
+                            // register sequencer tx forwarder
+                            if let Some(sequencer_http) = sequencer_http_arg {
+                                ctx.registry
+                                    .eth_api()
+                                    .set_sequencer_client(SequencerClient::new(sequencer_http));
+                            }
+
+                            Ok(())
+                        })
+                        .launch()
+                        .await?;
+
+                    handle.node_exit_future.await
+                }
             }
-            false => {
-                let handle = builder
-                    .node(OptimismNode::new(rollup_args.clone()))
-                    .extend_rpc_modules(move |ctx| {
-                        // register sequencer tx forwarder
-                        if let Some(sequencer_http) = sequencer_http_arg {
-                            ctx.registry
-                                .eth_api()
-                                .set_sequencer_client(SequencerClient::new(sequencer_http));
-                        }
-
-                        Ok(())
-                    })
-                    .launch()
-                    .await?;
-
-                handle.node_exit_future.await
-            }
-        }
-    }) {
+        })
+    {
         eprintln!("Error: {err:?}");
         std::process::exit(1);
     }
