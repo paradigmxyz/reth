94c44e979852e23b9a06fd35b73ac0eb536a9526
joshieDo <93316087+joshieDo@users.noreply.github.com>
2024-09-16 11:17:48 +0100

    Update crates/optimism/cli/src/commands/init_state/bedrock.rs
    Co-authored-by: Alexey Shekhirin <a.shekhirin@gmail.com>

--- crates/optimism/cli/src/commands/init_state/bedrock.rs
+++ crates/optimism/cli/src/commands/init_state/bedrock.rs
@@ -11,7 +11,7 @@ use reth_provider::{
 use reth_stages::{StageCheckpoint, StageId};
 use tracing::info;
 
-/// Creates a dummy chain (with no transactions) up to the last OVM block . It then, appends the
+/// Creates a dummy chain (with no transactions) up to the last OVM block and appends the
 /// first valid Bedrock block.
 pub(crate) fn setup_op_mainnet_without_ovm<DB: Database>(
     provider_rw: &DatabaseProviderRW<DB>,
