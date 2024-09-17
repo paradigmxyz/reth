031eaea10c2dcbe1a5bc345b58fd08422e4c2377
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-09-17 11:41:26 +0200

    Add EthChainSpec::Hardfork

--- crates/chainspec/src/api.rs
+++ crates/chainspec/src/api.rs
@@ -6,8 +6,8 @@ use crate::ChainSpec;
 
 /// Trait representing type configuring a chain spec.
 pub trait EthChainSpec: Send + Sync + Unpin + Debug + 'static {
-    // todo: make chain spec type generic over hardfork
-    //type Hardfork: Clone + Copy + 'static;
+    /// Hardfork type of network stack.
+    type Hardfork;
 
     /// Chain id.
     fn chain(&self) -> Chain;
@@ -17,6 +17,8 @@ impl<T> EthChainSpec for ChainSpec<T>
 where
     T: Send + Sync + Unpin + Debug + 'static,
 {
+    type Hardfork = T;
+
     fn chain(&self) -> Chain {
         self.chain
     }
