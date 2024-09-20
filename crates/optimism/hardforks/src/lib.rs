58ea7c5e0482f8c40cc2a25011b427122785fdc4
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-09-20 17:16:41 +0200

    Fix merge conflicts

--- crates/optimism/hardforks/src/lib.rs
+++ crates/optimism/hardforks/src/lib.rs
@@ -16,10 +16,10 @@ mod dev;
 pub use dev::DEV_HARDFORKS;
 pub use hardfork::OptimismHardfork;
 
-use reth_ethereum_forks::Hardforks;
+use reth_ethereum_forks::EthereumHardforks;
 
-/// Extends [`crate::EthereumHardforks`] with optimism helper methods.
-pub trait OptimismHardforks: Hardforks {
+/// Extends [`EthereumHardforks`] with optimism helper methods.
+pub trait OptimismHardforks: EthereumHardforks {
     /// Convenience method to check if [`OptimismHardfork::Bedrock`] is active at a given block
     /// number.
     fn is_bedrock_active_at_block(&self, block_number: u64) -> bool {
