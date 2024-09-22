18b5c20c3a2a62840c4f7ba537d6527863ecf05e
joshieDo <93316087+joshieDo@users.noreply.github.com>
2024-09-22 16:19:43 +0100

    clippy

--- crates/storage/provider/src/providers/static_file/manager.rs
+++ crates/storage/provider/src/providers/static_file/manager.rs
@@ -12,7 +12,7 @@ use alloy_primitives::{keccak256, Address, BlockHash, BlockNumber, TxHash, TxNum
 use dashmap::DashMap;
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use parking_lot::RwLock;
-use reth_chainspec::{Chain, ChainInfo, ChainSpecProvider, EthChainSpec};
+use reth_chainspec::{ChainInfo, ChainSpecProvider};
 use reth_db::{
     lockfile::StorageLock,
     static_file::{iter_static_files, HeaderMask, ReceiptMask, StaticFileCursor, TransactionMask},
@@ -639,7 +639,8 @@ impl StaticFileProvider {
         // If we detect an OVM import was done (block #1 <https://optimistic.etherscan.io/block/1>), skip it.
         // More on [#11099](https://github.com/paradigmxyz/reth/pull/11099).
         #[cfg(feature = "optimism")]
-        if provider.chain_spec().chain() == Chain::optimism_mainnet() &&
+        if reth_chainspec::EthChainSpec::chain(&provider.chain_spec()) ==
+            reth_chainspec::Chain::optimism_mainnet() &&
             provider
                 .block_number(reth_optimism_primitives::bedrock::OVM_HEADER_1_HASH)?
                 .is_some()
