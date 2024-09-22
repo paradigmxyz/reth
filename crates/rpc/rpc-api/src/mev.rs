f4d11f48bc5552c12499813fe28493c96ce71601
0xriazaka.eth <168359025+0xriazaka@users.noreply.github.com>
2024-09-22 13:48:33 +0100

    Split MevApi trait into two #11036 (#11081)
    Co-authored-by: Emilia Hane <elsaemiliaevahane@gmail.com>

--- crates/rpc/rpc-api/src/mev.rs
+++ crates/rpc/rpc-api/src/mev.rs
@@ -6,7 +6,21 @@ use jsonrpsee::proc_macros::rpc;
 /// Mev rpc interface.
 #[cfg_attr(not(feature = "client"), rpc(server, namespace = "mev"))]
 #[cfg_attr(feature = "client", rpc(server, client, namespace = "mev"))]
-pub trait MevApi {
+pub trait MevSimApi {
+    /// Similar to `mev_sendBundle` but instead of submitting a bundle to the relay, it returns
+    /// a simulation result. Only fully matched bundles can be simulated.
+    #[method(name = "simBundle")]
+    async fn sim_bundle(
+        &self,
+        bundle: SendBundleRequest,
+        sim_overrides: SimBundleOverrides,
+    ) -> jsonrpsee::core::RpcResult<SimBundleResponse>;
+}
+
+/// Mev rpc interface.
+#[cfg_attr(not(feature = "client"), rpc(server, namespace = "mev"))]
+#[cfg_attr(feature = "client", rpc(server, client, namespace = "mev"))]
+pub trait MevFullApi {
     /// Submitting bundles to the relay. It takes in a bundle and provides a bundle hash as a
     /// return value.
     #[method(name = "sendBundle")]
