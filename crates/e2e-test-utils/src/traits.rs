7b3f0dbb15747ad184c2b8bf230de593a7232933
Thomas Coratger <thomas.coratger@gmail.com>
2024-09-22 23:27:46 +0200

    rpc: rm useless reexports in reth-rpc-types

--- crates/e2e-test-utils/src/traits.rs
+++ crates/e2e-test-utils/src/traits.rs
@@ -1,7 +1,5 @@
-use reth::rpc::types::{
-    engine::ExecutionPayloadEnvelopeV3, optimism::OptimismExecutionPayloadEnvelopeV3,
-    ExecutionPayloadV3,
-};
+use op_alloy_rpc_types_engine::OptimismExecutionPayloadEnvelopeV3;
+use reth::rpc::types::{engine::ExecutionPayloadEnvelopeV3, ExecutionPayloadV3};
 
 /// The execution payload envelope type.
 pub trait PayloadEnvelopeExt: Send + Sync + std::fmt::Debug {
