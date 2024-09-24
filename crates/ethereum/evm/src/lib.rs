89b88e32dfbaf88c7b0569674480b5b6203cbed9
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2024-09-25 01:15:19 +0200

    primitives: rm `alloy_genesis` reexport (#11181)

--- crates/ethereum/evm/src/lib.rs
+++ crates/ethereum/evm/src/lib.rs
@@ -194,12 +194,13 @@ impl ConfigureEvm for EthEvmConfig {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use alloy_genesis::Genesis;
     use alloy_primitives::{B256, U256};
     use reth_chainspec::{Chain, ChainSpec, MAINNET};
     use reth_evm::execute::ProviderError;
     use reth_primitives::{
         revm_primitives::{BlockEnv, CfgEnv, SpecId},
-        Genesis, Header, KECCAK_EMPTY,
+        Header, KECCAK_EMPTY,
     };
     use reth_revm::{
         db::{CacheDB, EmptyDBTyped},
