4884c0003adc7ae85b768bbc8582e61a4539a322
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2024-09-25 13:07:05 +0200

    primitives: rm `TxKind` reexport (#11197)

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -75,7 +75,7 @@ pub use alloy_primitives::{
     eip191_hash_message, hex, hex_literal, keccak256, ruint,
     utils::format_ether,
     Address, BlockHash, BlockNumber, Bloom, BloomInput, Bytes, ChainId, Selector, StorageKey,
-    StorageValue, TxHash, TxIndex, TxKind, TxNumber, B128, B256, B512, B64, U128, U256, U64, U8,
+    StorageValue, TxHash, TxIndex, TxNumber, B128, B256, B512, B64, U128, U256, U64, U8,
 };
 pub use reth_ethereum_forks::*;
 pub use revm_primitives::{self, JumpTable};
