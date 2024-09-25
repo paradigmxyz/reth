b5eb28aaa55db7291d3bda55894803492e10a159
Thomas Coratger <thomas.coratger@gmail.com>
2024-09-25 11:31:49 +0200

    rpc: replace NetworkTypes AT by Ethereum for EthApi

--- crates/rpc/rpc/src/eth/core.rs
+++ crates/rpc/rpc/src/eth/core.rs
@@ -3,7 +3,7 @@
 
 use std::sync::Arc;
 
-use alloy_network::AnyNetwork;
+use alloy_network::{AnyNetwork, Ethereum};
 use alloy_primitives::U256;
 use derive_more::Deref;
 use reth_node_api::{BuilderProvider, FullNodeComponents};
@@ -129,8 +129,7 @@ where
     Self: Send + Sync,
 {
     type Error = EthApiError;
-    // todo: replace with alloy_network::Ethereum
-    type NetworkTypes = AnyNetwork;
+    type NetworkTypes = Ethereum;
     type TransactionCompat = EthTxBuilder;
 }
 
