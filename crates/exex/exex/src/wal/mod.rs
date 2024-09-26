4ccb31fb256f6882223a4d6990a9588a5b524205
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-09-26 20:03:42 +0100

    feat(exex): add parent hash to WAL block cache, index by hashes

--- crates/exex/exex/src/wal/mod.rs
+++ crates/exex/exex/src/wal/mod.rs
@@ -339,6 +339,7 @@ mod tests {
                 CachedBlock {
                     action: CachedBlockAction::Commit,
                     block: (blocks[0].number, blocks[0].hash()).into(),
+                    parent_hash: blocks[0].parent_hash,
                 },
             ),
             (
@@ -346,6 +347,7 @@ mod tests {
                 CachedBlock {
                     action: CachedBlockAction::Commit,
                     block: (blocks[1].number, blocks[1].hash()).into(),
+                    parent_hash: blocks[1].parent_hash,
                 },
             ),
         ];
@@ -361,6 +363,7 @@ mod tests {
             CachedBlock {
                 action: CachedBlockAction::Revert,
                 block: (blocks[1].number, blocks[1].hash()).into(),
+                parent_hash: blocks[1].parent_hash,
             },
         )];
         assert_eq!(
@@ -414,6 +417,7 @@ mod tests {
                 CachedBlock {
                     action: CachedBlockAction::Commit,
                     block: (block_1_reorged.number, block_1_reorged.hash()).into(),
+                    parent_hash: block_1_reorged.parent_hash,
                 },
             ),
             (
@@ -421,6 +425,7 @@ mod tests {
                 CachedBlock {
                     action: CachedBlockAction::Commit,
                     block: (blocks[2].number, blocks[2].hash()).into(),
+                    parent_hash: blocks[2].parent_hash,
                 },
             ),
         ];
@@ -451,6 +456,7 @@ mod tests {
                 CachedBlock {
                     action: CachedBlockAction::Revert,
                     block: (blocks[2].number, blocks[2].hash()).into(),
+                    parent_hash: blocks[2].parent_hash,
                 },
             ),
             (
@@ -458,6 +464,7 @@ mod tests {
                 CachedBlock {
                     action: CachedBlockAction::Commit,
                     block: (block_2_reorged.number, block_2_reorged.hash()).into(),
+                    parent_hash: block_2_reorged.parent_hash,
                 },
             ),
             (
@@ -465,6 +472,7 @@ mod tests {
                 CachedBlock {
                     action: CachedBlockAction::Commit,
                     block: (blocks[3].number, blocks[3].hash()).into(),
+                    parent_hash: blocks[3].parent_hash,
                 },
             ),
         ];
