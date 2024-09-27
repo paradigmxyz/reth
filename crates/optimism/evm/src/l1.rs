0446ec471a18c1520d3e0d671650ab868b888d90
Starkey <landthelord592@gmail.com>
2024-09-27 21:29:21 +0630

    optimism: fix 'expecte'  (#11290)

--- crates/optimism/evm/src/l1.rs
+++ crates/optimism/evm/src/l1.rs
@@ -352,7 +352,7 @@ mod tests {
         let expected_l1_blob_base_fee = U256::from_be_bytes(hex!(
             "0000000000000000000000000000000000000000000000000000000d5ea528d2" // 57422457042
         ));
-        let expecte_l1_blob_base_fee_scalar = U256::from(810949);
+        let expected_l1_blob_base_fee_scalar = U256::from(810949);
 
         // test
 
@@ -361,7 +361,7 @@ mod tests {
         assert_eq!(l1_block_info.l1_base_fee, expected_l1_base_fee);
         assert_eq!(l1_block_info.l1_base_fee_scalar, expected_l1_base_fee_scalar);
         assert_eq!(l1_block_info.l1_blob_base_fee, Some(expected_l1_blob_base_fee));
-        assert_eq!(l1_block_info.l1_blob_base_fee_scalar, Some(expecte_l1_blob_base_fee_scalar));
+        assert_eq!(l1_block_info.l1_blob_base_fee_scalar, Some(expected_l1_blob_base_fee_scalar));
     }
 
     #[test]
