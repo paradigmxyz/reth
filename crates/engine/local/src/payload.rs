a4751bbd139e254ac394358ebc5cf0909703a9e0
Gregory Edison <gregory.edison1993@gmail.com>
2024-09-30 14:49:22 +0200

    implement the eth payload builder

--- crates/engine/local/src/payload.rs
+++ crates/engine/local/src/payload.rs
@@ -1,21 +1,27 @@
+//! The implementation of the [`PayloadAttributesBuilder`] for the [`LocalEngineService`].
+
+use alloy_primitives::{Address, B256};
 use reth_ethereum_engine_primitives::EthPayloadAttributes;
-use reth_payload_primitives::{PayloadAttributesBuilder, PayloadTypes};
-use std::convert::Infallible;
+use reth_payload_primitives::PayloadAttributesBuilder;
+use std::{convert::Infallible, time::UNIX_EPOCH};
 
-/// The attributes builder for Ethereum payload.
+/// The attributes builder for local Ethereum payload.
 #[derive(Debug)]
-pub struct EthPayloadAttributesBuilder;
+pub struct EthLocalPayloadAttributesBuilder;
 
-impl PayloadAttributesBuilder for EthPayloadAttributesBuilder {
+impl PayloadAttributesBuilder for EthLocalPayloadAttributesBuilder {
     type PayloadAttributes = EthPayloadAttributes;
     type Error = Infallible;
 
     fn build(&self) -> Result<Self::PayloadAttributes, Self::Error> {
+        let ts = std::time::SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("cannot be earlier than UNIX_EPOCH");
 
         Ok(EthPayloadAttributes {
-            timestamp: ,
-            prev_randao: Default::default(),
-            suggested_fee_recipient: Default::default(),
+            timestamp: ts.as_secs(),
+            prev_randao: B256::random(),
+            suggested_fee_recipient: Address::random(),
             withdrawals: None,
             parent_beacon_block_root: None,
         })
