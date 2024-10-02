ab8254c49fa408c69fd7f28be95bbed87c4d127b
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-10-02 08:57:23 +0200

    Simplify Block impl for placeholder OpBlock

--- crates/optimism/primitives/src/block.rs
+++ crates/optimism/primitives/src/block.rs
@@ -1,25 +1,14 @@
 //! Optimism block primitive.
 
 use derive_more::Deref;
-use reth_node_api::{Block, BlockBody};
+use reth_node_api::Block;
+use reth_primitives::{BlockBody, Header};
 
+/// An Optimism block.
 #[derive(Debug, Deref)]
 pub struct OpBlock(reth_primitives::Block);
 
-#[derive(Debug, Deref)]
-pub struct OpHeader(reth_primitives::Header);
-
-#[derive(Debug, Deref)]
-pub struct OpBlockBody(reth_primitives::BlockBody);
-
-#[derive(Debug, Deref)]
-pub struct OpSignedTransaction(reth_primitives::TransactionSigned);
-
 impl Block for OpBlock {
-    type Header = OpHeader;
-    type Body = OpBlockBody;
-}
-
-impl BlockBody for OpBlockBody {
-    type SignedTransaction = OpSignedTransaction;
+    type Header = Header;
+    type Body = BlockBody;
 }
