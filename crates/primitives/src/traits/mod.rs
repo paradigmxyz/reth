d08c5c1ae2b7a5bb51f775e6f8a1f0fc00f1c302
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-10-02 16:16:28 +0200

    Fix export SignedTransaction

--- crates/primitives/src/traits/mod.rs
+++ crates/primitives/src/traits/mod.rs
@@ -1,9 +1,9 @@
 //! Abstractions of primitive data types
 
 pub mod block;
-pub mod signed;
+pub mod transaction;
 
-pub use signed::SignedTransaction;
+pub use transaction::signed::SignedTransaction;
 pub use block::{body::BlockBody, Block};
 
 pub use alloy_consensus::BlockHeader;
