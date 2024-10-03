a622ebd00b9c8b806ca9280158a4938a1749cd6c
varun-doshi <doshivarun202@gmail.com>
2024-10-04 00:46:59 +0530

    removed unused gas_limit

--- crates/rpc/rpc/src/debug.rs
+++ crates/rpc/rpc/src/debug.rs
@@ -506,7 +506,6 @@ where
         let opts = opts.unwrap_or_default();
         let block = block.ok_or(EthApiError::HeaderNotFound(target_block))?;
         let GethDebugTracingCallOptions { tracing_options, mut state_overrides, .. } = opts;
-        let gas_limit = self.inner.eth_api.call_gas_limit();
 
         // we're essentially replaying the transactions in the block here, hence we need the state
         // that points to the beginning of the block, which is the state at the parent block
