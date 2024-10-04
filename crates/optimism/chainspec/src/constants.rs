d9a7cb493144ea87857192913ca15358458a9d9d
Matthias Seitz <matthias.seitz@outlook.de>
2024-10-04 17:44:47 +0200

    chore: rm deposit contract config for op (#11479)

--- crates/optimism/chainspec/src/constants.rs
+++ crates/optimism/chainspec/src/constants.rs
@@ -1,8 +1,5 @@
 //! OP stack variation of chain spec constants.
 
-use alloy_primitives::{address, b256};
-use reth_chainspec::DepositContract;
-
 //------------------------------- BASE MAINNET -------------------------------//
 
 /// Max gas limit on Base: <https://basescan.org/block/17208876>
@@ -12,10 +9,3 @@ pub const BASE_MAINNET_MAX_GAS_LIMIT: u64 = 105_000_000;
 
 /// Max gas limit on Base Sepolia: <https://sepolia.basescan.org/block/12506483>
 pub const BASE_SEPOLIA_MAX_GAS_LIMIT: u64 = 45_000_000;
-
-/// Deposit contract address: `0x00000000219ab540356cbb839cbe05303d7705fa`
-pub(crate) const MAINNET_DEPOSIT_CONTRACT: DepositContract = DepositContract::new(
-    address!("00000000219ab540356cbb839cbe05303d7705fa"),
-    11052984,
-    b256!("649bbc62d0e31342afea4e5cd82d4049e7e1ee912fc0889aa790803be39038c5"),
-);
