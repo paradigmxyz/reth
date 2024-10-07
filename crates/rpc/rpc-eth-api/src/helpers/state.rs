7244cf1df42cb8912b3ea4969392c06d313dee41
Matthias Seitz <matthias.seitz@outlook.de>
2024-10-07 17:22:49 +0200

    make tests pass

--- crates/rpc/rpc-eth-api/src/helpers/state.rs
+++ crates/rpc/rpc-eth-api/src/helpers/state.rs
@@ -9,7 +9,8 @@ use reth_errors::RethError;
 use reth_evm::ConfigureEvmEnv;
 use reth_primitives::{BlockId, Header, KECCAK_EMPTY};
 use reth_provider::{
-    BlockIdReader, ChainSpecProvider, StateProvider, StateProviderBox, StateProviderFactory,
+    BlockIdReader, BlockNumReader, ChainSpecProvider, StateProvider, StateProviderBox,
+    StateProviderFactory,
 };
 use reth_rpc_eth_types::{EthApiError, EthStateCache, PendingBlockEnv, RpcInvalidTransactionError};
 use reth_rpc_types_compat::proof::from_primitive_account_proof;
@@ -129,17 +130,15 @@ pub trait EthState: LoadState + SpawnBlocking {
         &self,
         address: Address,
         block_id: BlockId,
-    ) -> impl Future<Output = Result<Option<Account>, Self::Error>> + Send
-    where
-        Self: EthApiSpec,
-    {
+    ) -> impl Future<Output = Result<Option<Account>, Self::Error>> + Send {
         self.spawn_blocking_io(move |this| {
             let state = this.state_at_block_id(block_id)?;
             let account = state.basic_account(address).map_err(Self::Error::from_eth_err)?;
             let Some(account) = account else { return Ok(None) };
 
             // Check whether the distance to the block exceeds the maximum configured proof window.
-            let chain_info = this.chain_info().map_err(Self::Error::from_eth_err)?;
+            let chain_info =
+                LoadState::provider(&this).chain_info().map_err(Self::Error::from_eth_err)?;
             let block_number = LoadState::provider(&this)
                 .block_number_for_id(block_id)
                 .map_err(Self::Error::from_eth_err)?
