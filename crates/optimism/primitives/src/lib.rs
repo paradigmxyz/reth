b3d42c1b54d0a999d2a2e183662f0fc2b4f107da
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-10-07 17:55:56 +0200

    Revert reth-optimism-primitives to main

--- crates/optimism/primitives/src/lib.rs
+++ crates/optimism/primitives/src/lib.rs
@@ -6,7 +6,5 @@
     issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
 )]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
-// The `optimism` feature must be enabled to use this crate.
-#![cfg(feature = "optimism")]
 
 pub mod bedrock;
