d5ebe55e5be806842740634bcf38ee02dd4d9e0d
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-10-08 12:00:55 +0200

    Remove optimism code from reth-primitives-traits

--- crates/primitives-traits/src/signature.rs
+++ crates/primitives-traits/src/signature.rs
@@ -8,24 +8,16 @@ pub trait Signature: Sized + Send + Sync {
     fn decode_with_eip155_chain_id(buf: &mut &[u8]) -> alloy_rlp::Result<(Self, Option<u64>)>;
 }
 
+// todo: add optimism type that wraps Signature, to impl separately for OP to account for system
+// null signature
 impl Signature for alloy_primitives::Signature {
     fn decode_with_eip155_chain_id(buf: &mut &[u8]) -> alloy_rlp::Result<(Self, Option<u64>)> {
         let v: Parity = alloy_rlp::Decodable::decode(buf)?;
         let r: U256 = alloy_rlp::Decodable::decode(buf)?;
         let s: U256 = alloy_rlp::Decodable::decode(buf)?;
 
-        #[cfg(not(feature = "optimism"))]
         if matches!(v, Parity::Parity(_)) {
             return Err(alloy_rlp::Error::Custom("invalid parity for legacy transaction"));
-        }
-
-        #[cfg(feature = "optimism")]
-        // pre bedrock system transactions were sent from the zero address as legacy
-        // transactions with an empty signature
-        //
-        // NOTE: this is very hacky and only relevant for op-mainnet pre bedrock
-        if matches!(v, Parity::Parity(false)) && r.is_zero() && s.is_zero() {
-            return Ok((Self::new(r, s, Parity::Parity(false)), None))
         }
 
         Ok((Self::new(r, s, v), v.chain_id()))
