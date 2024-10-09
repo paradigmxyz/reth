4f9749ea9197d2c264202038438e543004e79f4a
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-10-09 20:44:31 +0200

    Fix merge conflicts

--- crates/storage/codecs/src/alloy/transaction/mod.rs
+++ crates/storage/codecs/src/alloy/transaction/mod.rs
@@ -6,25 +6,17 @@ pub(crate) mod legacy;
 
 #[cfg(test)]
 mod tests {
-    use crate::alloy::transaction::{
-        eip1559::TxEip1559, eip2930::TxEip2930, eip4844::TxEip4844, eip7702::TxEip7702,
-        legacy::TxLegacy,
-    };
-
     // each value in the database has an extra field named flags that encodes metadata about other
     // fields in the value, e.g. offset and length.
     //
     // this check is to ensure we do not inadvertently add too many fields to a struct which would
     // expand the flags field and break backwards compatibility
-<<<<<<< Updated upstream
-=======
 
     use crate::alloy::transaction::{
         eip1559::TxEip1559, eip2930::TxEip2930, eip4844::TxEip4844, eip7702::TxEip7702,
         legacy::TxLegacy,
     };
 
->>>>>>> Stashed changes
     #[test]
     fn test_ensure_backwards_compatibility() {
         assert_eq!(TxEip4844::bitflag_encoded_bytes(), 5);
@@ -33,16 +25,4 @@ mod tests {
         assert_eq!(TxEip2930::bitflag_encoded_bytes(), 3);
         assert_eq!(TxEip7702::bitflag_encoded_bytes(), 4);
     }
-<<<<<<< Updated upstream
-
-    #[cfg(feature = "optimism")]
-    use crate::alloy::transaction::optimism::TxDeposit;
-
-    #[cfg(feature = "optimism")]
-    #[test]
-    fn test_ensure_backwards_compatibility_optimism() {
-        assert_eq!(TxDeposit::bitflag_encoded_bytes(), 2);
-    }
-=======
->>>>>>> Stashed changes
 }
