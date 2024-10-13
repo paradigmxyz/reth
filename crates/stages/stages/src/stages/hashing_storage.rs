205ff3e0d3a029e23658ac613e197a3c6d08b3a8
Matthias Seitz <matthias.seitz@outlook.de>
2024-10-13 15:13:40 +0200

    Revert "chore(stages): reduce the progress logging  (#11653)"
    This reverts commit de736a53cc7216141d900f4e145f06e892d6cdd7.

--- crates/stages/stages/src/stages/hashing_storage.rs
+++ crates/stages/stages/src/stages/hashing_storage.rs
@@ -1,4 +1,3 @@
-use crate::log_progress;
 use alloy_primitives::{bytes::BufMut, keccak256, B256};
 use itertools::Itertools;
 use reth_config::config::{EtlConfig, HashingConfig};
@@ -20,7 +19,6 @@ use reth_storage_errors::provider::ProviderResult;
 use std::{
     fmt::Debug,
     sync::mpsc::{self, Receiver},
-    time::Instant,
 };
 use tracing::*;
 
@@ -119,17 +117,17 @@ where
 
             collect(&mut channels, &mut collector)?;
 
-            let total = collector.len();
-            let mut last_log = Instant::now();
+            let total_hashes = collector.len();
+            let interval = (total_hashes / 10).max(1);
             let mut cursor = tx.cursor_dup_write::<tables::HashedStorages>()?;
             for (index, item) in collector.iter()?.enumerate() {
-                log_progress!(
-                    "sync::stages::hashing_storage",
-                    index,
-                    total,
-                    last_log,
-                    "Inserting hashes"
-                );
+                if index > 0 && index % interval == 0 {
+                    info!(
+                        target: "sync::stages::hashing_storage",
+                        progress = %format!("{:.2}%", (index as f64 / total_hashes as f64) * 100.0),
+                        "Inserting hashes"
+                    );
+                }
 
                 let (addr_key, value) = item?;
                 cursor.append_dup(
