6fb271036dbaf0ab9c9215959baec5833ac1cc88
Arsenii Kulikov <klkvrr@gmail.com>
2024-10-15 18:51:40 +0400

    feat: move RPC launch to add-ons (#11532)

--- crates/optimism/node/tests/e2e/utils.rs
+++ crates/optimism/node/tests/e2e/utils.rs
@@ -1,7 +1,9 @@
 use alloy_genesis::Genesis;
 use alloy_primitives::{Address, B256};
 use reth::{rpc::types::engine::PayloadAttributes, tasks::TaskManager};
-use reth_e2e_test_utils::{transaction::TransactionTestContext, wallet::Wallet, NodeHelperType};
+use reth_e2e_test_utils::{
+    transaction::TransactionTestContext, wallet::Wallet, Adapter, NodeHelperType,
+};
 use reth_optimism_chainspec::OpChainSpecBuilder;
 use reth_optimism_node::{
     node::OptimismAddOns, OptimismBuiltPayload, OptimismNode, OptimismPayloadBuilderAttributes,
@@ -11,7 +13,7 @@ use std::sync::Arc;
 use tokio::sync::Mutex;
 
 /// Optimism Node Helper type
-pub(crate) type OpNode = NodeHelperType<OptimismNode, OptimismAddOns>;
+pub(crate) type OpNode = NodeHelperType<OptimismNode, OptimismAddOns<Adapter<OptimismNode>>>;
 
 pub(crate) async fn setup(num_nodes: usize) -> eyre::Result<(Vec<OpNode>, TaskManager, Wallet)> {
     let genesis: Genesis = serde_json::from_str(include_str!("../assets/genesis.json")).unwrap();
