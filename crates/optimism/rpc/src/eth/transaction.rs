15783635d4a135fc3b2973533c0f6192039cda83
Matthias Seitz <matthias.seitz@outlook.de>
2024-10-15 21:18:21 +0200

    chore: include hash in trace (#11762)

--- crates/optimism/rpc/src/eth/transaction.rs
+++ crates/optimism/rpc/src/eth/transaction.rs
@@ -40,7 +40,7 @@ where
         // On optimism, transactions are forwarded directly to the sequencer to be included in
         // blocks that it builds.
         if let Some(client) = self.raw_tx_forwarder().as_ref() {
-            tracing::debug!( target: "rpc::eth",  "forwarding raw transaction to");
+            tracing::debug!(target: "rpc::eth", hash = %pool_transaction.hash(), "forwarding raw transaction to sequencer");
             let _ = client.forward_raw_transaction(&tx).await.inspect_err(|err| {
                     tracing::debug!(target: "rpc::eth", %err, hash=% *pool_transaction.hash(), "failed to forward raw transaction");
                 });
