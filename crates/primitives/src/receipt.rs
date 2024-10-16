5ca410c02435285c71f875ef0a931f9cda363c5f
Tuan Tran <tropicaldog17@gmail.com>
2024-10-16 11:46:53 +0700

    bet

--- crates/primitives/src/receipt.rs
+++ crates/primitives/src/receipt.rs
@@ -5,7 +5,8 @@ use crate::{
     EIP7702_TX_TYPE_ID,
 };
 use alloc::{vec, vec::Vec};
-use alloy_primitives::{Bloom, Bytes, Log, B256};
+use alloy_eips::eip2718::Encodable2718;
+use alloy_primitives::{Bloom, Log, B256};
 use alloy_rlp::{length_of_length, Decodable, Encodable, RlpDecodable, RlpEncodable};
 use bytes::{Buf, BufMut};
 use core::{cmp::Ordering, ops::Deref};
@@ -204,17 +205,8 @@ impl<'a> arbitrary::Arbitrary<'a> for Receipt {
     }
 }
 
-/// A trait for enveloped encoding of receipts.
-pub trait EnvelopedEncoding {
-    /// Returns the enveloped encoded receipt.
-    fn envelope_encoded(&self) -> Bytes;
-
-    /// Encodes the receipt into its "raw" format.
-    fn encode_enveloped(&self, out: &mut dyn bytes::BufMut);
-}
-
 // Implement the trait for ReceiptWithBloom
-impl EnvelopedEncoding for ReceiptWithBloom {
+impl Encodable2718 for ReceiptWithBloom {
     /// Encodes the receipt into its "raw" format.
     /// This format is also referred to as "binary" encoding.
     ///
@@ -224,13 +216,18 @@ impl EnvelopedEncoding for ReceiptWithBloom {
     /// of the receipt:
     /// - EIP-1559, 2930 and 4844 transactions: `tx-type || rlp([status, cumulativeGasUsed,
     ///   logsBloom, logs])`
-    fn envelope_encoded(&self) -> Bytes {
-        let mut buf = Vec::new();
-        self.encode_enveloped(&mut buf);
-        buf.into()
+    fn type_flag(&self) -> Option<u8> {
+        match self.receipt.tx_type {
+            TxType::Legacy => None,
+            tx_type => Some(tx_type as u8),
+        }
     }
 
-    fn encode_enveloped(&self, out: &mut dyn bytes::BufMut) {
+    fn encode_2718_len(&self) -> usize {
+        self.as_encoder().length()
+    }
+
+    fn encode_2718(&self, out: &mut dyn alloy_rlp::BufMut) {
         self.encode_inner(out, false)
     }
 }
@@ -246,7 +243,7 @@ impl ReceiptWithBloom {
         let b = &mut &**buf;
         let rlp_head = alloy_rlp::Header::decode(b)?;
         if !rlp_head.list {
-            return Err(alloy_rlp::Error::UnexpectedString)
+            return Err(alloy_rlp::Error::UnexpectedString);
         }
         let started_len = b.len();
 
@@ -291,7 +288,7 @@ impl ReceiptWithBloom {
             return Err(alloy_rlp::Error::ListLengthMismatch {
                 expected: rlp_head.payload_length,
                 got: consumed,
-            })
+            });
         }
         *buf = *b;
         Ok(this)
@@ -448,7 +445,7 @@ impl ReceiptWithBloomEncoder<'_> {
     fn encode_inner(&self, out: &mut dyn BufMut, with_header: bool) {
         if matches!(self.receipt.tx_type, TxType::Legacy) {
             self.encode_fields(out);
-            return
+            return;
         }
 
         let mut payload = Vec::new();
