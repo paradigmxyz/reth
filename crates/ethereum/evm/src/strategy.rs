3438d6429fe5e3a3e801fca17b30c8378165aa9a
Federico Gimenez <fgimenez@users.noreply.github.com>
2024-10-16 13:20:42 +0200

    chore: rename executor and provider Generic -> Basic (#11788)

--- crates/ethereum/evm/src/strategy.rs
+++ crates/ethereum/evm/src/strategy.rs
@@ -274,7 +274,7 @@ mod tests {
     use alloy_primitives::{b256, fixed_bytes, keccak256, Bytes, TxKind, B256};
     use reth_chainspec::{ChainSpecBuilder, ForkCondition};
     use reth_evm::execute::{
-        BatchExecutor, BlockExecutorProvider, Executor, GenericBlockExecutorProvider,
+        BasicBlockExecutorProvider, BatchExecutor, BlockExecutorProvider, Executor,
     };
     use reth_execution_types::BlockExecutionOutput;
     use reth_primitives::{
@@ -328,11 +328,11 @@ mod tests {
 
     fn executor_provider(
         chain_spec: Arc<ChainSpec>,
-    ) -> GenericBlockExecutorProvider<EthExecutionStrategyFactory> {
+    ) -> BasicBlockExecutorProvider<EthExecutionStrategyFactory> {
         let strategy_factory =
             EthExecutionStrategyFactory::new(chain_spec.clone(), EthEvmConfig::new(chain_spec));
 
-        GenericBlockExecutorProvider::new(strategy_factory)
+        BasicBlockExecutorProvider::new(strategy_factory)
     }
 
     #[test]
