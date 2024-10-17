538ec04aad7e6468684687fa24db97af0bd43808
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-10-17 22:12:55 +0200

    no-std

--- crates/storage/codecs/src/lib.rs
+++ crates/storage/codecs/src/lib.rs
@@ -78,7 +78,7 @@ pub trait Compact: Sized {
     }
 }
 
-impl Compact for String {
+impl Compact for alloc::string::String {
     fn to_compact<B>(&self, buf: &mut B) -> usize
     where
         B: bytes::BufMut + AsMut<[u8]>,
