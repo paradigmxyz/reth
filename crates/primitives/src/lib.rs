3dcdd8c11d5c77d63b1eec81b605790fb8db6642
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2024-10-17 22:29:31 +0200

    primitives: use alloy `DEV_GENESIS_HASH` constant (#11849)

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -39,7 +39,7 @@ pub use block::{
 };
 #[cfg(feature = "reth-codec")]
 pub use compression::*;
-pub use constants::{DEV_GENESIS_HASH, HOLESKY_GENESIS_HASH, KECCAK_EMPTY, SEPOLIA_GENESIS_HASH};
+pub use constants::{HOLESKY_GENESIS_HASH, KECCAK_EMPTY, SEPOLIA_GENESIS_HASH};
 pub use receipt::{
     gas_spent_by_transactions, Receipt, ReceiptWithBloom, ReceiptWithBloomRef, Receipts,
 };
