b8a50ed98e0ac3040cf6a7d86466dc09a74b1869
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2024-10-22 12:34:53 +0200

    chore(primitives): use alloy eth conversion constants (#11892)

--- crates/exex/exex/src/backfill/test_utils.rs
+++ crates/exex/exex/src/backfill/test_utils.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use alloy_consensus::TxEip2930;
+use alloy_consensus::{constants::ETH_TO_WEI, TxEip2930};
 use alloy_genesis::{Genesis, GenesisAccount};
 use alloy_primitives::{b256, Address, TxKind, U256};
 use eyre::OptionExt;
@@ -10,8 +10,7 @@ use reth_evm::execute::{
 };
 use reth_evm_ethereum::execute::EthExecutorProvider;
 use reth_primitives::{
-    constants::ETH_TO_WEI, Block, BlockBody, BlockWithSenders, Header, Receipt,
-    SealedBlockWithSenders, Transaction,
+    Block, BlockBody, BlockWithSenders, Header, Receipt, SealedBlockWithSenders, Transaction,
 };
 use reth_provider::{
     providers::ProviderNodeTypes, BlockWriter as _, ExecutionOutcome, LatestStateProviderRef,
