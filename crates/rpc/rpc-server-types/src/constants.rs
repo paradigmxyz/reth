57a21fcb9efc274675ea00a644ebbfa627940c6d
Julian Meyer <julianmeyer2000@gmail.com>
2024-10-23 09:22:51 -0700

    chore: increase max proof window (#12001)

--- crates/rpc/rpc-server-types/src/constants.rs
+++ crates/rpc/rpc-server-types/src/constants.rs
@@ -51,9 +51,9 @@ pub const DEFAULT_MAX_SIMULATE_BLOCKS: u64 = 256;
 /// The default eth historical proof window.
 pub const DEFAULT_ETH_PROOF_WINDOW: u64 = 0;
 
-/// Maximum eth historical proof window. Equivalent to roughly one and a half months of data on a 12
-/// second block time, and a week on a 2 second block time.
-pub const MAX_ETH_PROOF_WINDOW: u64 = 7 * 24 * 60 * 60 / 2;
+/// Maximum eth historical proof window. Equivalent to roughly 6 months of data on a 12
+/// second block time, and a month on a 2 second block time.
+pub const MAX_ETH_PROOF_WINDOW: u64 = 28 * 24 * 60 * 60 / 2;
 
 /// GPO specific constants
 pub mod gas_oracle {
