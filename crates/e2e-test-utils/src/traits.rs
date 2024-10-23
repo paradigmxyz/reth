4e3c1145cb8351f09a69e8e6abf7ed9b214eb2c0
Arsenii Kulikov <klkvrr@gmail.com>
2024-10-23 18:30:24 +0400

    wip

--- crates/e2e-test-utils/src/traits.rs
+++ crates/e2e-test-utils/src/traits.rs
@@ -1,3 +1,4 @@
+use alloy_rpc_types::engine::ExecutionPayloadEnvelopeV4;
 use op_alloy_rpc_types_engine::OpExecutionPayloadEnvelopeV3;
 use reth::rpc::types::engine::{ExecutionPayloadEnvelopeV3, ExecutionPayloadV3};
 
@@ -14,6 +15,12 @@ impl PayloadEnvelopeExt for OpExecutionPayloadEnvelopeV3 {
 }
 
 impl PayloadEnvelopeExt for ExecutionPayloadEnvelopeV3 {
+    fn execution_payload(&self) -> ExecutionPayloadV3 {
+        self.execution_payload.clone()
+    }
+}
+
+impl PayloadEnvelopeExt for ExecutionPayloadEnvelopeV4 {
     fn execution_payload(&self) -> ExecutionPayloadV3 {
         self.execution_payload.clone()
     }
