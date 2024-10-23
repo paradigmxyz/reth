9b4948bf57c02d850c98edcba0515c03972e5a05
youngkashew <evchip@pm.me>
2024-10-23 10:54:58 +0700

    map max_size and growth_step args from Option<ByteSize> to Option<usize> in with_geometry

--- crates/node/core/src/args/database.rs
+++ crates/node/core/src/args/database.rs
@@ -60,7 +60,7 @@ impl DatabaseArgs {
             .with_log_level(self.log_level)
             .with_exclusive(self.exclusive)
             .with_max_read_transaction_duration(max_read_transaction_duration)
-            .with_geometry(self.max_size, self.growth_step)
+            .with_geometry(self.max_size.map(|s| s.0), self.growth_step.map(|s| s.0))
     }
 }
 
