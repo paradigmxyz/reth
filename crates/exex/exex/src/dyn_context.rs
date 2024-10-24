531804693f633429d6e2e3ce03a06027b6cd32c5
0xurb <urbadeil@gmail.com>
2024-10-24 14:19:01 +0100

    feat: add notifications to `ExExContextDyn`

--- crates/exex/exex/src/dyn_context.rs
+++ crates/exex/exex/src/dyn_context.rs
@@ -1,18 +1,17 @@
 //! Mirrored version of [`ExExContext`](`crate::ExExContext`)
 //! without generic abstraction over [Node](`reth_node_api::FullNodeComponents`)
 
-use std::sync::Arc;
+use std::{fmt::Debug, sync::Arc};
 
 use reth_chainspec::{EthChainSpec, Head};
 use reth_node_api::FullNodeComponents;
 use reth_node_core::node_config::NodeConfig;
 use tokio::sync::mpsc;
 
-use crate::{ExExContext, ExExEvent};
+use crate::{ExExContext, ExExEvent, ExExNotificationsStream};
 
-// TODO(0xurb) - add `node` and `notifications` after abstractions
+// TODO(0xurb) - add `node` after abstractions
 /// Captures the context that an `ExEx` has access to.
-#[derive(Debug)]
 pub struct ExExContextDyn {
     /// The current head of the blockchain at launch.
     pub head: Head,
@@ -28,9 +27,33 @@ pub struct ExExContextDyn {
     /// Additionally, the exex can pre-emptively emit a `FinishedHeight` event to specify what
     /// blocks to receive notifications for.
     pub events: mpsc::UnboundedSender<ExExEvent>,
+    /// Channel to receive [`ExExNotification`](crate::ExExNotification)s.
+    ///
+    /// # Important
+    ///
+    /// Once an [`ExExNotification`](crate::ExExNotification) is sent over the channel, it is
+    /// considered delivered by the node.
+    pub notifications: Box<dyn ExExNotificationsStream>,
 }
 
-impl<Node: FullNodeComponents> From<ExExContext<Node>> for ExExContextDyn {
+impl Debug for ExExContextDyn {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ExExContext")
+            .field("head", &self.head)
+            .field("config", &self.config)
+            .field("reth_config", &self.reth_config)
+            .field("events", &self.events)
+            .field("notifications", &self.notifications)
+            .finish()
+    }
+}
+
+impl<Node> From<ExExContext<Node>> for ExExContextDyn
+where
+    Node: FullNodeComponents,
+    Node::Provider: Debug,
+    Node::Executor: Debug,
+{
     fn from(ctx: ExExContext<Node>) -> Self {
         // convert `NodeConfig` with generic over chainspec into `NodeConfig<Box<dyn EthChainSpec>`
         let chain: Arc<Box<dyn EthChainSpec + 'static>> =
@@ -50,7 +73,14 @@ impl<Node: FullNodeComponents> From<ExExContext<Node>> for ExExContextDyn {
             dev: ctx.config.dev,
             pruning: ctx.config.pruning,
         };
+        let notifications = Box::new(ctx.notifications) as Box<dyn ExExNotificationsStream>;
 
-        Self { head: ctx.head, config, reth_config: ctx.reth_config, events: ctx.events }
+        Self {
+            head: ctx.head,
+            config,
+            reth_config: ctx.reth_config,
+            events: ctx.events,
+            notifications,
+        }
     }
 }
