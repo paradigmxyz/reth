9f33ff105e816b9f4a4d73ff649a3fcedb61269a
Arsenii Kulikov <klkvrr@gmail.com>
2024-10-24 15:12:34 +0400

    feat: Add more complex E2E test (#12005)

--- crates/e2e-test-utils/src/traits.rs
+++ crates/e2e-test-utils/src/traits.rs
@@ -1,4 +1,5 @@
-use op_alloy_rpc_types_engine::OpExecutionPayloadEnvelopeV3;
+use alloy_rpc_types::engine::ExecutionPayloadEnvelopeV4;
+use op_alloy_rpc_types_engine::{OpExecutionPayloadEnvelopeV3, OpExecutionPayloadEnvelopeV4};
 use reth::rpc::types::engine::{ExecutionPayloadEnvelopeV3, ExecutionPayloadV3};
 
 /// The execution payload envelope type.
@@ -13,7 +14,19 @@ impl PayloadEnvelopeExt for OpExecutionPayloadEnvelopeV3 {
     }
 }
 
+impl PayloadEnvelopeExt for OpExecutionPayloadEnvelopeV4 {
+    fn execution_payload(&self) -> ExecutionPayloadV3 {
+        self.execution_payload.clone()
+    }
+}
+
 impl PayloadEnvelopeExt for ExecutionPayloadEnvelopeV3 {
+    fn execution_payload(&self) -> ExecutionPayloadV3 {
+        self.execution_payload.clone()
+    }
+}
+
+impl PayloadEnvelopeExt for ExecutionPayloadEnvelopeV4 {
     fn execution_payload(&self) -> ExecutionPayloadV3 {
         self.execution_payload.clone()
     }
