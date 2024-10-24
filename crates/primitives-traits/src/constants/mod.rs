79daed24ffd389960d1ad5a7a309e6f710b18c0a
Thomas Coratger <thomas.coratger@gmail.com>
2024-10-24 14:20:08 +0200

    more rm

--- crates/primitives-traits/src/constants/mod.rs
+++ crates/primitives-traits/src/constants/mod.rs
@@ -23,12 +23,6 @@ pub const MIN_PROTOCOL_BASE_FEE: u64 = 7;
 /// Same as [`MIN_PROTOCOL_BASE_FEE`] but as a U256.
 pub const MIN_PROTOCOL_BASE_FEE_U256: U256 = U256::from_limbs([7u64, 0, 0, 0]);
 
-/// Base fee max change denominator as defined in [EIP-1559](https://eips.ethereum.org/EIPS/eip-1559)
-pub const EIP1559_DEFAULT_BASE_FEE_MAX_CHANGE_DENOMINATOR: u64 = 8;
-
-/// Elasticity multiplier as defined in [EIP-1559](https://eips.ethereum.org/EIPS/eip-1559)
-pub const EIP1559_DEFAULT_ELASTICITY_MULTIPLIER: u64 = 2;
-
 /// Minimum gas limit allowed for transactions.
 pub const MINIMUM_GAS_LIMIT: u64 = 5000;
 
