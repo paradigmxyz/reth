2dbbe0f0c16822599a9836a3df9eaed0dce14aba
Thomas Coratger <thomas.coratger@gmail.com>
2024-10-24 08:11:23 +0200

    primitive-traits: use alloy BEACON_CONSENSUS_REORG_UNWIND_DEPTH constant

--- crates/primitives-traits/src/constants/mod.rs
+++ crates/primitives-traits/src/constants/mod.rs
@@ -80,16 +80,6 @@ pub const OP_SYSTEM_TX_FROM_ADDR: Address = address!("deaddeaddeaddeaddeaddeadde
 /// To address from Optimism system txs: `0x4200000000000000000000000000000000000015`
 pub const OP_SYSTEM_TX_TO_ADDR: Address = address!("4200000000000000000000000000000000000015");
 
-/// The number of blocks to unwind during a reorg that already became a part of canonical chain.
-///
-/// In reality, the node can end up in this particular situation very rarely. It would happen only
-/// if the node process is abruptly terminated during ongoing reorg and doesn't boot back up for
-/// long period of time.
-///
-/// Unwind depth of `3` blocks significantly reduces the chance that the reorged block is kept in
-/// the database.
-pub const BEACON_CONSENSUS_REORG_UNWIND_DEPTH: u64 = 3;
-
 /// Max seconds from current time allowed for blocks, before they're considered future blocks.
 ///
 /// This is only used when checking whether or not the timestamp for pre-merge blocks is in the
