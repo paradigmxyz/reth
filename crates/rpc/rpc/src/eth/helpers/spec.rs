a98dc3973ff733feb11a7c8b50c60459dfaa9351
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-10-27 02:56:29 +0800

    chore(rpc): simplify trait bounds on `EthApiSpec` impl (#12101)

--- crates/rpc/rpc/src/eth/helpers/spec.rs
+++ crates/rpc/rpc/src/eth/helpers/spec.rs
@@ -3,20 +3,17 @@ use reth_chainspec::EthereumHardforks;
 use reth_network_api::NetworkInfo;
 use reth_provider::{BlockNumReader, ChainSpecProvider, StageCheckpointReader};
 use reth_rpc_eth_api::{helpers::EthApiSpec, RpcNodeCore};
-use reth_transaction_pool::TransactionPool;
 
 use crate::EthApi;
 
 impl<Provider, Pool, Network, EvmConfig> EthApiSpec for EthApi<Provider, Pool, Network, EvmConfig>
 where
-    Self: RpcNodeCore<Provider = Provider, Network = Network>,
-    Pool: TransactionPool + 'static,
-    Provider: ChainSpecProvider<ChainSpec: EthereumHardforks>
-        + BlockNumReader
-        + StageCheckpointReader
-        + 'static,
-    Network: NetworkInfo + 'static,
-    EvmConfig: Send + Sync,
+    Self: RpcNodeCore<
+        Provider: ChainSpecProvider<ChainSpec: EthereumHardforks>
+                      + BlockNumReader
+                      + StageCheckpointReader,
+        Network: NetworkInfo,
+    >,
 {
     fn starting_block(&self) -> U256 {
         self.inner.starting_block()
