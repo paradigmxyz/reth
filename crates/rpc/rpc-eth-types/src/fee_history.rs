8605d04a09679904ef25594729ac6b83dfcacfcb
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2024-10-28 09:30:06 +0100

    refactor: rm re-exports of alloy eip 4844 constants (#12120)

--- crates/rpc/rpc-eth-types/src/fee_history.rs
+++ crates/rpc/rpc-eth-types/src/fee_history.rs
@@ -366,7 +366,7 @@ impl FeeHistoryEntry {
             gas_used_ratio: block.gas_used as f64 / block.gas_limit as f64,
             base_fee_per_blob_gas: block.blob_fee(),
             blob_gas_used_ratio: block.blob_gas_used() as f64 /
-                reth_primitives::constants::eip4844::MAX_DATA_GAS_PER_BLOCK as f64,
+                alloy_eips::eip4844::MAX_DATA_GAS_PER_BLOCK as f64,
             excess_blob_gas: block.excess_blob_gas,
             blob_gas_used: block.blob_gas_used,
             gas_used: block.gas_used,
