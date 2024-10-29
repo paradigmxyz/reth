339b42efefe78ce8c8410f6288b9699bf185f132
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-10-28 21:57:28 -0400

    fix(ecies): bound initial header body size

--- crates/net/ecies/src/algorithm.rs
+++ crates/net/ecies/src/algorithm.rs
@@ -650,6 +650,8 @@ impl ECIES {
         out.extend_from_slice(tag.as_slice());
     }
 
+    /// Reads the `RLPx` header, setting up the MAC and AES, returning the body size contained in
+    /// the header.
     pub fn read_header(&mut self, data: &mut [u8]) -> Result<usize, ECIESError> {
         // If the data is not large enough to fit the header and mac bytes, return an error
         //
