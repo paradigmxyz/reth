ca00baf9eb0d49fcc21cc144baddd92d7e2696e5
Deon <110722148+DanielEmmanuel1@users.noreply.github.com>
2024-10-30 15:54:42 +0100

    Update lib.rs

--- crates/storage/codecs/derive/src/lib.rs
+++ crates/storage/codecs/derive/src/lib.rs
@@ -20,43 +20,43 @@ use syn::{
 mod arbitrary;
 mod compact;
 
-/// Derives the `Compact` trait for custom structs, optimizing serialization with a possible
+/// Derives the Compact trait for custom structs, optimizing serialization with a possible
 /// bitflag struct.
 ///
 /// ## Implementation:
-/// The derived `Compact` implementation leverages a bitflag struct when needed to manage the
+/// The derived Compact implementation leverages a bitflag struct when needed to manage the
 /// presence of certain field types, primarily for compacting fields efficiently. This bitflag
 /// struct records information about fields that require a small, fixed number of bits for their
-/// encoding, such as `bool`, `Option<T>`, or other small types.
+/// encoding, such as bool, Option<T>, or other small types.
 ///
 /// ### Bit Sizes for Fields:
 /// The amount of bits used to store a field size is determined by the field's type. For specific
-/// types, a fixed number of bits is allocated (from `fn get_bit_size`):
-/// - `bool`, `Option<T>`, `TransactionKind`, `Signature`: **1 bit**
-/// - `TxType`: **2 bits**
-/// - `u64`, `BlockNumber`, `TxNumber`, `ChainId`, `NumTransactions`: **4 bits**
-/// - `u128`: **5 bits**
-/// - `U256`: **6 bits**
+/// types, a fixed number of bits is allocated (from fn get_bit_size):
+/// - bool, Option<T>, TransactionKind, Signature: *1 bit*
+/// - TxType: *2 bits*
+/// - u64, BlockNumber, TxNumber, ChainId, NumTransactions: *4 bits*
+/// - u128: *5 bits*
+/// - U256: *6 bits*
 ///
 /// ### Warning: Extending structs, unused bits and backwards compatibility:
-/// When the bitflag only has one bit left (for example, when adding many `Option<T>` fields),
-/// you should introduce a new struct (e.g., `TExtension`) with additional fields, and use
-/// `Option<TExtension>` in the original struct. This approach allows further field extensions while
+/// When the bitflag only has one bit left (for example, when adding many Option<T> fields),
+/// you should introduce a new struct (e.g., TExtension) with additional fields, and use
+/// Option<TExtension> in the original struct. This approach allows further field extensions while
 /// maintaining backward compatibility.
 ///
 /// ### Limitations:
-/// - Fields not listed above, or types such `Vec`, or large composite types, should manage their
+/// - Fields not listed above, or types such Vec, or large composite types, should manage their
 ///   own encoding and do not rely on the bitflag struct.
-/// - `Bytes` fields and any types containing a `Bytes` field should be placed last to ensure
+/// - Bytes fields and any types containing a Bytes field should be placed last to ensure
 ///   efficient decoding.
-#[proc_macro_derive(Compact, attributes(maybe_zero))]
+#[proc_macro_derive(Compact, attributes(maybe_zero, reth_codecs))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let is_zstd = false;
     compact::derive(input, is_zstd)
 }
 
-/// Adds `zstd` compression to derived [`Compact`].
-#[proc_macro_derive(CompactZstd, attributes(maybe_zero))]
+/// Adds zstd compression to derived [Compact].
+#[proc_macro_derive(CompactZstd, attributes(maybe_zero, reth_codecs))]
 pub fn derive_zstd(input: TokenStream) -> TokenStream {
     let is_zstd = true;
     compact::derive(input, is_zstd)
@@ -64,16 +64,16 @@ pub fn derive_zstd(input: TokenStream) -> TokenStream {
 
 /// Generates tests for given type.
 ///
-/// If `compact` or `rlp` is passed to `add_arbitrary_tests`, there will be proptest roundtrip tests
+/// If compact or rlp is passed to add_arbitrary_tests, there will be proptest roundtrip tests
 /// generated. An integer value passed will limit the number of proptest cases generated (default:
 /// 256).
 ///
 /// Examples:
-/// * `#[add_arbitrary_tests]`: will derive arbitrary with no tests.
-/// * `#[add_arbitrary_tests(rlp)]`: will derive arbitrary and generate rlp roundtrip proptests.
-/// * `#[add_arbitrary_tests(rlp, 10)]`: will derive arbitrary and generate rlp roundtrip proptests.
+/// * #[add_arbitrary_tests]: will derive arbitrary with no tests.
+/// * #[add_arbitrary_tests(rlp)]: will derive arbitrary and generate rlp roundtrip proptests.
+/// * #[add_arbitrary_tests(rlp, 10)]: will derive arbitrary and generate rlp roundtrip proptests.
 ///   Limited to 10 cases.
-/// * `#[add_arbitrary_tests(compact, rlp)]`. will derive arbitrary and generate rlp and compact
+/// * #[add_arbitrary_tests(compact, rlp)]. will derive arbitrary and generate rlp and compact
 ///   roundtrip proptests.
 #[proc_macro_attribute]
 pub fn add_arbitrary_tests(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -113,13 +113,13 @@ impl Parse for GenerateTestsInput {
 
 /// Generates tests for given type based on passed parameters.
 ///
-/// See `arbitrary::maybe_generate_tests` for more information.
+/// See arbitrary::maybe_generate_tests for more information.
 ///
 /// Examples:
-/// * `generate_tests!(#[rlp] MyType, MyTypeTests)`: will generate rlp roundtrip tests for `MyType`
-///   in a module named `MyTypeTests`.
-/// * `generate_tests!(#[compact, 10] MyType, MyTypeTests)`: will generate compact roundtrip tests
-///   for `MyType` limited to 10 cases.
+/// * generate_tests!(#[rlp] MyType, MyTypeTests): will generate rlp roundtrip tests for MyType
+///   in a module named MyTypeTests.
+/// * generate_tests!(#[compact, 10] MyType, MyTypeTests): will generate compact roundtrip tests
+///   for MyType limited to 10 cases.
 #[proc_macro]
 pub fn generate_tests(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as GenerateTestsInput);
