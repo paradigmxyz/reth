cdb67148b23b81e185001c752fd4f4f01fcc58b8
Matthias Seitz <matthias.seitz@outlook.de>
2024-10-31 20:51:28 +0100

    Update crates/chainspec/src/api.rs

--- crates/chainspec/src/api.rs
+++ crates/chainspec/src/api.rs
@@ -89,7 +89,7 @@ impl EthChainSpec for ChainSpec {
     }
 
     fn display_hardforks(&self) -> Box<dyn Display> {
-        Box::new(ChainSpec::display_hardforks(self))
+        Box::new(Self::display_hardforks(self))
     }
 
     fn genesis_header(&self) -> &Header {
