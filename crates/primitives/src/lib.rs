21eda7350c9fedf18fb391bc13ee381d40e5e383
Thomas Coratger <thomas.coratger@gmail.com>
2024-11-04 19:24:42 +0100

    primitives: rm alloy BlobTransactionValidationError reexport

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -54,9 +54,6 @@ pub use transaction::{
     PooledTransactionsElementEcRecovered,
 };
 
-#[cfg(feature = "c-kzg")]
-pub use transaction::BlobTransactionValidationError;
-
 pub use transaction::{
     util::secp256k1::{public_key_to_address, recover_signer_unchecked, sign_message},
     InvalidTransactionError, Signature, Transaction, TransactionMeta, TransactionSigned,
--- crates/transaction-pool/src/traits.rs
+++ crates/transaction-pool/src/traits.rs
@@ -11,15 +11,19 @@ use alloy_consensus::{
     constants::{EIP1559_TX_TYPE_ID, EIP4844_TX_TYPE_ID, EIP7702_TX_TYPE_ID},
     Transaction as _,
 };
-use alloy_eips::{eip2718::Encodable2718, eip2930::AccessList, eip4844::BlobAndProofV1};
+use alloy_eips::{
+    eip2718::Encodable2718,
+    eip2930::AccessList,
+    eip4844::{BlobAndProofV1, BlobTransactionValidationError},
+};
 use alloy_primitives::{Address, TxHash, TxKind, B256, U256};
 use futures_util::{ready, Stream};
 use reth_eth_wire_types::HandleMempoolData;
 use reth_execution_types::ChangedAccount;
 use reth_primitives::{
     kzg::KzgSettings, transaction::TryFromRecoveredTransactionError, BlobTransactionSidecar,
-    BlobTransactionValidationError, PooledTransactionsElement,
-    PooledTransactionsElementEcRecovered, SealedBlock, Transaction, TransactionSignedEcRecovered,
+    PooledTransactionsElement, PooledTransactionsElementEcRecovered, SealedBlock, Transaction,
+    TransactionSignedEcRecovered,
 };
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
