2b58a4292452c77d5f4e0724ed5c3cd8b6b1c0e9
Ayodeji Akinola <akinayodeji4all@gmail.com>
2024-11-06 22:18:22 +0100

    wip: undo changed files

--- crates/rpc/rpc-layer/src/compression_layer.rs
+++ crates/rpc/rpc-layer/src/compression_layer.rs
@@ -1,23 +1,26 @@
 use jsonrpsee_http_client::{HttpRequest, HttpResponse};
 use pin_project::pin_project;
 use std::{
-   future::Future,
-   pin::Pin,
-   task::{Context, ready, Poll},
+    future::Future,
+    pin::Pin,
+    task::{ready, Context, Poll},
 };
 use tower::{Layer, Service};
 
 #[derive(Debug, Clone, Copy)]
 enum CompressionKind {
-   Gzip,
+    Gzip,
+    Deflate,
+    Brotli,
+    Zstd,
 }
 
-/// Encoding middleware service that checks Accept-Encoding on request
+/// Compression middleware service that checks Accept-Encoding on request
 /// and compresses response if needed
 #[derive(Clone, Debug)]
 pub struct CompressionService<S> {
-   inner: S,
-   compression: Option<CompressionKind>
+    inner: S,
+    compression: Option<CompressionKind>,
 }
 
 /// Layer for adding compression support
@@ -25,76 +28,141 @@ pub struct CompressionLayer;
 
 impl CompressionLayer {
     pub const fn new() -> Self {
-       Self
+        Self
     }
 }
 
 impl<S> Layer<S> for CompressionLayer {
-   type Service = CompressionService<S>;
+    type Service = CompressionService<S>;
 
-   fn layer(&self, inner: S) -> Self::Service {
-       CompressionService { 
-           inner,
-           compression: None
-       }
-   }
+    fn layer(&self, inner: S) -> Self::Service {
+        CompressionService { inner, compression: None }
+    }
 }
 
 impl<S> Service<HttpRequest> for CompressionService<S>
 where
-   S: Service<HttpRequest, Response = HttpResponse>,
+    S: Service<HttpRequest, Response = HttpResponse>,
 {
-   type Response = HttpResponse;
-   type Error = S::Error;
-   type Future = ResponseFuture<S::Future>;
+    type Response = HttpResponse;
+    type Error = S::Error;
+    type Future = ResponseFuture<S::Future>;
 
-   fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-       self.inner.poll_ready(cx)
-   }
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
 
-   fn call(&mut self, req: HttpRequest) -> Self::Future {
-       // Check Accept-Encoding in request
-       self.compression = req.headers()
-           .get("Accept-Encoding")
-           .and_then(|header_value| header_value.to_str().ok())
-           .and_then(|ptr_str| ptr_str.contains("gzip").then_some(CompressionKind::Gzip));
+    fn call(&mut self, req: HttpRequest) -> Self::Future {
+        // Check Accept-Encoding in request and set the appropriate compression type
+        self.compression = req
+            .headers()
+            .get("Accept-Encoding")
+            .and_then(|header_value| header_value.to_str().ok())
+            .and_then(|ptr_str| {
+                if ptr_str.contains("gzip") {
+                    Some(CompressionKind::Gzip)
+                } else if ptr_str.contains("deflate") {
+                    Some(CompressionKind::Deflate)
+                } else if ptr_str.contains("br") {
+                    Some(CompressionKind::Brotli)
+                } else if ptr_str.contains("zstd") {
+                    Some(CompressionKind::Zstd)
+                } else {
+                    None
+                }
+            });
 
-       ResponseFuture {
-           inner: self.inner.call(req),
-           compression: self.compression,
-       }
-   }
+        ResponseFuture { inner: self.inner.call(req), compression: self.compression }
+    }
 }
 
 #[pin_project]
 #[allow(missing_debug_implementations)]
 pub struct ResponseFuture<F> {
-   #[pin]
-   inner: F,
-   compression: Option<CompressionKind>,
+    #[pin]
+    inner: F,
+    compression: Option<CompressionKind>,
 }
 
 impl<F, E> Future for ResponseFuture<F>
 where
-   F: Future<Output = Result<HttpResponse, E>>,
+    F: Future<Output = Result<HttpResponse, E>>,
 {
-   type Output = F::Output;
+    type Output = F::Output;
 
-   fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-       let this = self.project();
-       let response = ready!(this.inner.poll(cx)?);
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        let mut response = ready!(this.inner.poll(cx)?);
 
-       match *this.compression {
-           Some(CompressionKind::Gzip) => {
-               let mut compressed_response = response;
-               compressed_response.headers_mut().insert(
-                   "Content-Encoding", 
-                   "gzip".parse().unwrap()
-               );
-               // Compress with gzip...
-               Poll::Ready(Ok(compressed_response))
-           },
-           None => Poll::Ready(Ok(response))
-       }
-   }
+        if let Some(compression) = this.compression {
+            let header_value = match compression {
+                CompressionKind::Gzip => "gzip",
+                CompressionKind::Deflate => "deflate",
+                CompressionKind::Brotli => "br",
+                CompressionKind::Zstd => "zstd",
+            };
+            if let Ok(value) = header_value.parse() {
+                response.headers_mut().insert("Content-Encoding", value);
+            }
+        }
+
+        Poll::Ready(Ok(response))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use http::header::{ACCEPT_ENCODING, CONTENT_ENCODING};
+    use jsonrpsee_http_client::{HttpBody, HttpRequest, HttpResponse};
+    use std::future::ready;
+    use tower::{Service, ServiceBuilder, ServiceExt};
+
+    use crate::CompressionLayer;
+
+    #[derive(Clone)]
+    struct MockRequestService;
+
+    impl Service<HttpRequest> for MockRequestService {
+        type Response = HttpResponse;
+        type Error = std::convert::Infallible;
+        type Future = std::future::Ready<Result<Self::Response, Self::Error>>;
+
+        fn poll_ready(
+            &mut self,
+            _: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Result<(), Self::Error>> {
+            std::task::Poll::Ready(Ok(()))
+        }
+
+        fn call(&mut self, _: HttpRequest) -> Self::Future {
+            // Create a large test payload that will benefit from compression
+            let large_string = "compress test data ".repeat(1000);
+            let body = HttpBody::from(large_string);
+            let response = HttpResponse::builder().body(body).unwrap();
+            ready(Ok(response))
+        }
+    }
+
+    #[tokio::test]
+    async fn test_compression_decompression() -> Result<(), Box<dyn std::error::Error>> {
+        // 1. Create service with compression
+        let mut compression_service =
+            ServiceBuilder::new().layer(CompressionLayer::new()).service(MockRequestService);
+
+        // 2. Create request that accepts gzip
+        let request =
+            HttpRequest::builder().header(ACCEPT_ENCODING, "gzip").body(HttpBody::empty()).unwrap();
+
+        // 3. Get compressed response
+        let compressed_response = compression_service.ready().await?.call(request).await?;
+
+        // Verify compression header
+        assert_eq!(
+            compressed_response.headers().get(CONTENT_ENCODING).unwrap(),
+            "gzip",
+            "Response should indicate gzip compression"
+        );
+
+        Ok(())
+    }
 }
