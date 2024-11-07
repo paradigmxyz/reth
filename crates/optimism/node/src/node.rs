da6b6274834bd68eafea14780aabb0f7fde3b91b
Arsenii Kulikov <klkvrr@gmail.com>
2024-11-08 01:29:49 +0400

    chore: remove auto-seal consensus (#12385)

--- crates/optimism/node/src/node.rs
+++ crates/optimism/node/src/node.rs
@@ -467,11 +467,7 @@ where
     type Consensus = Arc<dyn reth_consensus::Consensus>;
 
     async fn build_consensus(self, ctx: &BuilderContext<Node>) -> eyre::Result<Self::Consensus> {
-        if ctx.is_dev() {
-            Ok(Arc::new(reth_auto_seal_consensus::AutoSealConsensus::new(ctx.chain_spec())))
-        } else {
-            Ok(Arc::new(OpBeaconConsensus::new(ctx.chain_spec())))
-        }
+        Ok(Arc::new(OpBeaconConsensus::new(ctx.chain_spec())))
     }
 }
 
