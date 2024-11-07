29da7d744a52f9d77d27cc78705dc30e265f20bf
Arsenii Kulikov <klkvrr@gmail.com>
2024-11-07 23:31:17 +0400

    fix: `eth_getProof` response (#12370)

--- crates/rpc/rpc-types-compat/src/proof.rs
+++ crates/rpc/rpc-types-compat/src/proof.rs
@@ -5,16 +5,18 @@ use alloy_rpc_types_eth::{EIP1186AccountProofResponse, EIP1186StorageProof};
 use reth_trie_common::{AccountProof, StorageProof};
 
 /// Creates a new rpc storage proof from a primitive storage proof type.
-pub fn from_primitive_storage_proof(proof: StorageProof) -> EIP1186StorageProof {
-    EIP1186StorageProof {
-        key: JsonStorageKey::Hash(proof.key),
-        value: proof.value,
-        proof: proof.proof,
-    }
+pub fn from_primitive_storage_proof(
+    proof: StorageProof,
+    slot: JsonStorageKey,
+) -> EIP1186StorageProof {
+    EIP1186StorageProof { key: slot, value: proof.value, proof: proof.proof }
 }
 
 /// Creates a new rpc account proof from a primitive account proof type.
-pub fn from_primitive_account_proof(proof: AccountProof) -> EIP1186AccountProofResponse {
+pub fn from_primitive_account_proof(
+    proof: AccountProof,
+    slots: Vec<JsonStorageKey>,
+) -> EIP1186AccountProofResponse {
     let info = proof.info.unwrap_or_default();
     EIP1186AccountProofResponse {
         address: proof.address,
@@ -23,6 +25,13 @@ pub fn from_primitive_account_proof(proof: AccountProof) -> EIP1186AccountProofR
         nonce: info.nonce,
         storage_hash: proof.storage_root,
         account_proof: proof.proof,
-        storage_proof: proof.storage_proofs.into_iter().map(from_primitive_storage_proof).collect(),
+        storage_proof: proof
+            .storage_proofs
+            .into_iter()
+            .filter_map(|proof| {
+                let input_slot = slots.iter().find(|s| s.as_b256() == proof.key)?;
+                Some(from_primitive_storage_proof(proof, *input_slot))
+            })
+            .collect(),
     }
 }
