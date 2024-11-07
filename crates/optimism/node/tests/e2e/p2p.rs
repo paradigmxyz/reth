f3804dae670c2cdc3bb3b5135daf384af01f61ed
sevazhidkov <zhidkovseva@gmail.com>
2024-11-07 19:27:49 +0000

    move test utils to node library

--- crates/optimism/node/tests/e2e/p2p.rs
+++ crates/optimism/node/tests/e2e/p2p.rs
@@ -1,6 +1,6 @@
-use crate::utils::{advance_chain, setup};
 use alloy_rpc_types_engine::PayloadStatusEnum;
 use reth::blockchain_tree::error::BlockchainTreeError;
+use reth_optimism_node::utils::{advance_chain, setup};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
