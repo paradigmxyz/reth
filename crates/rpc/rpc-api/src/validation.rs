809595ace1624571105a96efe85fd9b3234848bc
stevencartavia <corderosteven6@gmail.com>
2024-11-07 22:55:52 -0600

    replace BuilderBlockValidationRequestV3 with alloy type

--- crates/rpc/rpc-api/src/validation.rs
+++ crates/rpc/rpc-api/src/validation.rs
@@ -2,28 +2,12 @@
 
 use alloy_primitives::B256;
 use alloy_rpc_types_beacon::relay::{
-    BuilderBlockValidationRequest, BuilderBlockValidationRequestV2, SignedBidSubmissionV3,
-    SignedBidSubmissionV4,
+    BuilderBlockValidationRequest, BuilderBlockValidationRequestV2,
+    BuilderBlockValidationRequestV3, SignedBidSubmissionV4,
 };
 use jsonrpsee::proc_macros::rpc;
 use serde::{Deserialize, Serialize};
 use serde_with::{serde_as, DisplayFromStr};
-
-/// A Request to validate a [`SignedBidSubmissionV3`]
-///
-/// <https://github.com/flashbots/builder/blob/7577ac81da21e760ec6693637ce2a81fe58ac9f8/eth/block-validation/api.go#L198-L202>
-#[serde_as]
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct BuilderBlockValidationRequestV3 {
-    /// The request to be validated.
-    #[serde(flatten)]
-    pub request: SignedBidSubmissionV3,
-    /// The registered gas limit for the validation request.
-    #[serde_as(as = "DisplayFromStr")]
-    pub registered_gas_limit: u64,
-    /// The parent beacon block root for the validation request.
-    pub parent_beacon_block_root: B256,
-}
 
 /// A Request to validate a [`SignedBidSubmissionV4`]
 ///
