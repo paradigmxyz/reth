c24e0b2369a8df771af06f3fd6be5abb2f3045ee
Khanh Hoa <hoa@notional.ventures>
2024-11-08 09:24:04 +0700

    feat: add Optimism engine API

--- crates/optimism/rpc/src/lib.rs
+++ crates/optimism/rpc/src/lib.rs
@@ -16,4 +16,23 @@ pub mod sequencer;
 
 pub use error::{OpEthApiError, OpInvalidTransactionError, SequencerClientError};
 pub use eth::{OpEthApi, OpReceiptBuilder};
+use op_alloy_rpc_types_engine::{ProtocolVersion, SuperchainSignal};
 pub use sequencer::SequencerClient;
+
+/// Engine API extension for Optimism superchain signaling
+#[cfg_attr(not(feature = "client"), rpc(server, namespace = "engine"))]
+#[cfg_attr(feature = "client", rpc(server, client, namespace = "engine"))]
+pub trait OpEngineApiExt {
+    /// Signal superchain v1 message
+    ///
+    /// The execution engine SHOULD warn when the recommended version is newer than the current
+    /// version. The execution engine SHOULD take safety precautions if it does not meet
+    /// the required version.
+    ///
+    /// # Returns
+    /// The latest supported OP-Stack protocol version of the execution engine.
+    ///
+    /// See: <https://specs.optimism.io/protocol/exec-engine.html#engine_signalsuperchainv1>
+    #[method(name = "signalSuperchainV1")]
+    async fn signal_superchain_v1(&self, signal: SuperchainSignal) -> RpcResult<ProtocolVersion>;
+}
