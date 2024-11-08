935df37a857a630ce9d919dba83816b5e364f233
Federico Gimenez <federico.gimenez@gmail.com>
2024-11-08 14:16:01 +0100

    use StorageProof::storage_multiproof

--- crates/trie/parallel/src/parallel_proof.rs
+++ crates/trie/parallel/src/parallel_proof.rs
@@ -1,7 +1,7 @@
 use crate::{
     parallel_root::ParallelStateRootError, stats::ParallelTrieTracker, StorageRootTargets,
 };
-use alloy_primitives::B256;
+use alloy_primitives::{map::HashSet, B256};
 use alloy_rlp::{BufMut, Encodable};
 use itertools::Itertools;
 use reth_db::DatabaseError;
@@ -10,19 +10,17 @@ use reth_provider::{
     providers::ConsistentDbView, BlockReader, DBProvider, DatabaseProviderFactory, ProviderError,
 };
 use reth_trie::{
-    hashed_cursor::{HashedCursorFactory, HashedPostStateCursorFactory, HashedStorageCursor},
+    hashed_cursor::{HashedCursorFactory, HashedPostStateCursorFactory},
     node_iter::{TrieElement, TrieNodeIter},
-    prefix_set::{PrefixSet, PrefixSetMut, TriePrefixSetsMut},
+    prefix_set::{PrefixSetMut, TriePrefixSetsMut},
+    proof::StorageProof,
     trie_cursor::{InMemoryTrieCursorFactory, TrieCursorFactory},
     walker::TrieWalker,
-    HashBuilder, MultiProof, Nibbles, StorageMultiProof, TrieAccount, TrieInput,
+    HashBuilder, MultiProof, Nibbles, TrieAccount, TrieInput,
 };
 use reth_trie_common::proof::ProofRetainer;
 use reth_trie_db::{DatabaseHashedCursorFactory, DatabaseTrieCursorFactory};
-use std::{
-    collections::{HashMap, HashSet},
-    sync::Arc,
-};
+use std::{collections::HashMap, sync::Arc};
 use tracing::debug;
 
 #[cfg(feature = "metrics")]
@@ -69,7 +67,7 @@ where
         // Extend prefix sets with targets
         let mut prefix_sets = self.input.prefix_sets.clone();
         prefix_sets.extend(TriePrefixSetsMut {
-            account_prefix_set: PrefixSetMut::from(targets.keys().map(Nibbles::unpack)),
+            account_prefix_set: PrefixSetMut::from(targets.keys().copied().map(Nibbles::unpack)),
             storage_prefix_sets: targets
                 .iter()
                 .filter(|&(_hashed_address, slots)| (!slots.is_empty()))
@@ -94,9 +92,8 @@ where
             storage_root_targets.into_iter().sorted_unstable_by_key(|(address, _)| *address)
         {
             let view = self.view.clone();
-            let targets = targets
-                .get(&hashed_address)
-                .map_or(Vec::new(), |slots| slots.iter().map(Nibbles::unpack).collect());
+            let target_slots: HashSet<B256> =
+                targets.get(&hashed_address).cloned().unwrap_or_default();
 
             let trie_nodes_sorted = trie_nodes_sorted.clone();
             let hashed_state_sorted = hashed_state_sorted.clone();
@@ -115,14 +112,18 @@ where
                         &hashed_state_sorted,
                     );
 
-                    Ok(storage_multiproof(
+                    StorageProof::new_hashed(
                         trie_cursor_factory,
                         hashed_cursor_factory,
-                        prefix_set,
                         hashed_address,
-                        targets,
                     )
-                    .map_err(ProviderError::Database)?)
+                    .with_prefix_set_mut(PrefixSetMut::from(prefix_set.iter().cloned()))
+                    .storage_multiproof(target_slots)
+                    .map_err(|e| {
+                        ParallelStateRootError::StorageRoot(StorageRootError::Database(
+                            DatabaseError::Other(e.to_string()),
+                        ))
+                    })
                 })();
                 let _ = tx.send(result);
             });
@@ -167,7 +168,7 @@ where
                     let storage_multiproof = match storage_proofs.remove(&hashed_address) {
                         Some(rx) => rx.recv().map_err(|_| {
                             ParallelStateRootError::StorageRoot(StorageRootError::Database(
-                                reth_db::DatabaseError::Other(format!(
+                                DatabaseError::Other(format!(
                                     "channel closed for {hashed_address}"
                                 )),
                             ))
@@ -176,16 +177,20 @@ where
                         // be a possibility of re-adding a non-modified leaf to the hash builder.
                         None => {
                             tracker.inc_missed_leaves();
-                            storage_multiproof(
+                            StorageProof::new_hashed(
                                 trie_cursor_factory.clone(),
                                 hashed_cursor_factory.clone(),
-                                Default::default(), // TODO: check?
                                 hashed_address,
-                                targets.get(&hashed_address).map_or(Vec::new(), |slots| {
-                                    slots.iter().map(Nibbles::unpack).collect()
-                                }),
                             )
-                            .map_err(ProviderError::Database)?
+                            .with_prefix_set_mut(Default::default())
+                            .storage_multiproof(
+                                targets.get(&hashed_address).cloned().unwrap_or_default(),
+                            )
+                            .map_err(|e| {
+                                ParallelStateRootError::StorageRoot(StorageRootError::Database(
+                                    DatabaseError::Other(e.to_string()),
+                                ))
+                            })?
                         }
                     };
 
@@ -206,47 +211,4 @@ where
 
         Ok(MultiProof { account_subtree: hash_builder.take_proof_nodes(), storages })
     }
-}
-
-/// Generate a storage multiproof according to specified targets.
-pub fn storage_multiproof<T, H>(
-    trie_cursor_factory: T,
-    hashed_cursor_factory: H,
-    prefix_set: PrefixSet, // must already include targets
-    hashed_address: B256,
-    targets: Vec<Nibbles>,
-) -> Result<StorageMultiProof, DatabaseError>
-where
-    T: TrieCursorFactory,
-    H: HashedCursorFactory,
-{
-    let mut hashed_storage_cursor = hashed_cursor_factory.hashed_storage_cursor(hashed_address)?;
-
-    // short circuit on empty storage
-    if hashed_storage_cursor.is_storage_empty()? {
-        return Ok(StorageMultiProof::empty())
-    }
-
-    let trie_cursor = trie_cursor_factory.storage_trie_cursor(hashed_address)?;
-    let walker = TrieWalker::new(trie_cursor, prefix_set);
-
-    let retainer = ProofRetainer::from_iter(targets);
-    let mut hash_builder = HashBuilder::default().with_proof_retainer(retainer);
-    let mut storage_node_iter = TrieNodeIter::new(walker, hashed_storage_cursor);
-    while let Some(node) = storage_node_iter.try_next()? {
-        match node {
-            TrieElement::Branch(node) => {
-                hash_builder.add_branch(node.key, node.value, node.children_are_in_trie);
-            }
-            TrieElement::Leaf(hashed_slot, value) => {
-                hash_builder.add_leaf(
-                    Nibbles::unpack(hashed_slot),
-                    alloy_rlp::encode_fixed_size(&value).as_ref(),
-                );
-            }
-        }
-    }
-
-    let root = hash_builder.root();
-    Ok(StorageMultiProof { root, subtree: hash_builder.take_proof_nodes() })
 }
