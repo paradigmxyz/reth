4d3d552127b9565010362a158337976d547e95a4
Matthias Seitz <matthias.seitz@outlook.de>
2024-11-09 17:39:18 +0100

    chore: add Sync to PayloadBuilder

--- crates/payload/primitives/src/traits.rs
+++ crates/payload/primitives/src/traits.rs
@@ -11,7 +11,7 @@ use tokio::sync::oneshot;
 
 /// A type that can request, subscribe to and resolve payloads.
 #[async_trait::async_trait]
-pub trait PayloadBuilder: Send + Unpin {
+pub trait PayloadBuilder: Send + Sync + Unpin {
     /// The Payload type for the builder.
     type PayloadType: PayloadTypes;
     /// The error type returned by the builder.
