68108f8f779be97b4f4305cf03f317f3747b15c3
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-11-10 17:53:04 +0100

    Add blanket impl for TxType

--- crates/primitives-traits/src/tx_type.rs
+++ crates/primitives-traits/src/tx_type.rs
@@ -1,7 +1,8 @@
+use core::fmt::{Debug, Display};
+
 use alloy_eips::eip2718::Eip2718Error;
 use alloy_primitives::{U64, U8};
 use alloy_rlp::{Decodable, Encodable};
-use core::fmt::{Debug, Display};
 
 /// Trait representing the behavior of a transaction type.
 pub trait TxType:
@@ -23,5 +24,27 @@ pub trait TxType:
     + Send
     + Sync
     + 'static
+{
+}
+
+impl<T> TxType for T where
+    T: Into<u8>
+        + Into<U8>
+        + PartialEq
+        + Eq
+        + PartialEq<u8>
+        + TryFrom<u8, Error = Eip2718Error>
+        + TryFrom<u64>
+        + TryFrom<U64>
+        + Debug
+        + Display
+        + Clone
+        + Copy
+        + Default
+        + Encodable
+        + Decodable
+        + Send
+        + Sync
+        + 'static
 {
 }
