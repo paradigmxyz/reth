8e0da8fbbe7fa4a9cfc6e476208792c80d8c9baf
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-11-10 17:10:44 +0100

    Improve usability transaction primitive traits

--- crates/primitives-traits/src/lib.rs
+++ crates/primitives-traits/src/lib.rs
@@ -27,7 +27,7 @@ pub use receipt::Receipt;
 pub mod transaction;
 pub use transaction::{
     signed::{FullSignedTx, SignedTransaction},
-    FullTransaction, Transaction,
+    AlloyTransactionExt, FullTransaction, Transaction,
 };
 
 mod integer_list;
@@ -56,7 +56,7 @@ pub use tx_type::TxType;
 pub mod header;
 #[cfg(any(test, feature = "arbitrary", feature = "test-utils"))]
 pub use header::test_utils;
-pub use header::{BlockHeader, Header, HeaderError, SealedHeader};
+pub use header::{Header, HeaderError, SealedHeader};
 
 /// Bincode-compatible serde implementations for common abstracted types in Reth.
 ///
@@ -69,3 +69,12 @@ pub use header::{BlockHeader, Header, HeaderError, SealedHeader};
 pub mod serde_bincode_compat {
     pub use super::header::{serde_bincode_compat as header, serde_bincode_compat::*};
 }
+
+/// Helper trait that requires arbitrary implementation if the feature is enabled.
+#[cfg(feature = "arbitrary")]
+pub trait MaybeArbitrary: for<'a> arbitrary::Arbitrary<'a> {}
+#[cfg(not(feature = "arbitrary"))]
+pub trait MaybeArbitrary {}
+
+#[cfg(feature = "arbitrary")]
+impl<T> MaybeArbitrary for T where T: for<'a> arbitrary::Arbitrary<'a> {}
--- crates/primitives-traits/src/transaction/mod.rs
+++ crates/primitives-traits/src/transaction/mod.rs
@@ -1,13 +1,20 @@
 //! Transaction abstraction
 
+pub mod signed;
+
 use core::{fmt::Debug, hash::Hash};
 
-use alloy_primitives::{TxKind, B256};
+use alloy_primitives::B256;
 
 use reth_codecs::Compact;
 use serde::{Deserialize, Serialize};
 
-pub mod signed;
+use crate::{MaybeArbitrary, TxType};
+
+/// Helper trait that unifies all behaviour required by transaction to support full node operations.
+pub trait FullTransaction: Transaction + Compact {}
+
+impl<T> FullTransaction for T where T: Transaction + Compact {}
 
 #[allow(dead_code)]
 /// Abstraction of a transaction.
@@ -19,43 +26,50 @@ pub trait Transaction:
     + PartialEq
     + Hash
     + Serialize
+    + for<'de> Deserialize<'de>
     + alloy_rlp::Encodable
     + alloy_rlp::Decodable
-    + for<'de> Deserialize<'de>
-    + alloy_consensus::Transaction
+    + AlloyTransactionExt
     + MaybeArbitrary
 {
+}
+
+impl<T> Transaction for T where
+    T: Debug
+        + Default
+        + Clone
+        + Eq
+        + PartialEq
+        + Hash
+        + Serialize
+        + for<'de> Deserialize<'de>
+        + alloy_rlp::Encodable
+        + alloy_rlp::Decodable
+        + AlloyTransactionExt
+        + MaybeArbitrary
+{
+}
+
+/// Extension trait of [`alloy_consensus::Transaction`].
+pub trait AlloyTransactionExt: alloy_consensus::Transaction {
+    /// Transaction envelope type ID.
+    type Type: TxType;
+
     /// Heavy operation that return signature hash over rlp encoded transaction.
     /// It is only for signature signing or signer recovery.
     fn signature_hash(&self) -> B256;
 
-    /// Gets the transaction's [`TxKind`], which is the address of the recipient or
-    /// [`TxKind::Create`] if the transaction is a contract creation.
-    fn kind(&self) -> TxKind;
-
-    /// Returns true if the tx supports dynamic fees
+    /// Returns `true` if the tx supports dynamic fees
     fn is_dynamic_fee(&self) -> bool;
 
     /// Returns the effective gas price for the given base fee.
     fn effective_gas_price(&self, base_fee: Option<u64>) -> u128;
 
-    /// This encodes the transaction _without_ the signature, and is only suitable for creating a
-    /// hash intended for signing.
-    fn encode_without_signature(&self, out: &mut dyn bytes::BufMut);
-
-    /// Calculates a heuristic for the in-memory size of the [Transaction].
+    /// Calculates a heuristic for the in-memory size of the transaction.
     fn size(&self) -> usize;
+
+    /// Returns the transaction type.
+    fn tx_type(&self) -> Self::Type {
+        Self::Type::try_from(self.ty()).expect("should decode tx type id")
+    }
 }
-
-#[cfg(not(feature = "arbitrary"))]
-/// Helper trait that requires arbitrary implementation if the feature is enabled.
-pub trait MaybeArbitrary {}
-
-#[cfg(feature = "arbitrary")]
-/// Helper trait that requires arbitrary implementation if the feature is enabled.
-pub trait MaybeArbitrary: for<'a> arbitrary::Arbitrary<'a> {}
-
-/// Helper trait that unifies all behaviour required by transaction to support full node operations.
-pub trait FullTransaction: Transaction + Compact {}
-
-impl<T> FullTransaction for T where T: Transaction + Compact {}
