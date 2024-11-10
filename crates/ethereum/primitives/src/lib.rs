05d29415a10228bc6140200cf24649fea28be208
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-11-10 13:42:31 +0100

    Fix deps

--- crates/ethereum/primitives/src/lib.rs
+++ crates/ethereum/primitives/src/lib.rs
@@ -7,6 +7,7 @@
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 use reth_node_types::NodePrimitives;
 use reth_primitives::{Block, Receipt, TransactionSigned};
