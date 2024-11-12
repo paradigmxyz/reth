419a2833b2836d9ee31b69638c577d99c32f3da8
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-11-12 11:13:21 +0100

    Update docs
    Co-authored-by: Federico Gimenez <fgimenez@users.noreply.github.com>

--- crates/primitives-traits/src/receipt.rs
+++ crates/primitives-traits/src/receipt.rs
@@ -22,6 +22,6 @@ pub trait Receipt:
     /// Returns transaction type.
     fn tx_type(&self) -> u8;
 
-    /// Calculates the receipts root of all receipts in a block.
+    /// Calculates the receipts root of the given receipts.
     fn receipts_root(receipts: &[&Self]) -> B256;
 }
