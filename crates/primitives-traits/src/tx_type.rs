61165a86d42ab82d103d0a5a23409570deea6045
Özgün Özerk <ozgunozerk.elo@gmail.com>
2024-11-12 12:07:05 +0300

    feat(op): define OpTxType (#12443)

--- crates/primitives-traits/src/tx_type.rs
+++ crates/primitives-traits/src/tx_type.rs
@@ -26,3 +26,25 @@ pub trait TxType:
     + Decodable
 {
 }
+
+impl<T> TxType for T where
+    T: Send
+        + Sync
+        + Unpin
+        + Clone
+        + Copy
+        + Default
+        + fmt::Debug
+        + fmt::Display
+        + PartialEq
+        + Eq
+        + PartialEq<u8>
+        + Into<u8>
+        + Into<U8>
+        + TryFrom<u8, Error = Eip2718Error>
+        + TryFrom<u64>
+        + TryFrom<U64>
+        + Encodable
+        + Decodable
+{
+}
