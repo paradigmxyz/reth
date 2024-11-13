9e77d916e1b6758313d33ac0d6874476e1cbf7eb
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-11-13 14:35:59 +0100

    chore(sdk): improve usability tx primitive traits (#12437)

--- crates/primitives-traits/src/transaction/mod.rs
+++ crates/primitives-traits/src/transaction/mod.rs
@@ -1,17 +1,20 @@
 //! Transaction abstraction
 
+pub mod signed;
+
 use core::{fmt, hash::Hash};
 
-use alloy_primitives::{TxKind, B256};
-
+use alloy_primitives::B256;
 use reth_codecs::Compact;
 use serde::{Deserialize, Serialize};
 
-use crate::InMemorySize;
+use crate::{InMemorySize, MaybeArbitrary, TxType};
 
-pub mod signed;
+/// Helper trait that unifies all behaviour required by transaction to support full node operations.
+pub trait FullTransaction: Transaction + Compact {}
 
-#[allow(dead_code)]
+impl<T> FullTransaction for T where T: Transaction + Compact {}
+
 /// Abstraction of a transaction.
 pub trait Transaction:
     Send
@@ -24,41 +27,42 @@ pub trait Transaction:
     + PartialEq
     + Hash
     + Serialize
-    + alloy_rlp::Encodable
-    + alloy_rlp::Decodable
     + for<'de> Deserialize<'de>
-    + alloy_consensus::Transaction
+    + TransactionExt
     + InMemorySize
     + MaybeArbitrary
 {
+}
+
+impl<T> Transaction for T where
+    T: Send
+        + Sync
+        + Unpin
+        + Clone
+        + Default
+        + fmt::Debug
+        + Eq
+        + PartialEq
+        + Hash
+        + Serialize
+        + for<'de> Deserialize<'de>
+        + TransactionExt
+        + InMemorySize
+        + MaybeArbitrary
+{
+}
+
+/// Extension trait of [`alloy_consensus::Transaction`].
+pub trait TransactionExt: alloy_consensus::Transaction {
+    /// Transaction envelope type ID.
+    type Type: TxType;
+
     /// Heavy operation that return signature hash over rlp encoded transaction.
     /// It is only for signature signing or signer recovery.
     fn signature_hash(&self) -> B256;
 
-    /// Gets the transaction's [`TxKind`], which is the address of the recipient or
-    /// [`TxKind::Create`] if the transaction is a contract creation.
-    fn kind(&self) -> TxKind;
-
-    /// Returns true if the tx supports dynamic fees
-    fn is_dynamic_fee(&self) -> bool;
-
-    /// Returns the effective gas price for the given base fee.
-    fn effective_gas_price(&self, base_fee: Option<u64>) -> u128;
-
-    /// This encodes the transaction _without_ the signature, and is only suitable for creating a
-    /// hash intended for signing.
-    fn encode_without_signature(&self, out: &mut dyn bytes::BufMut);
+    /// Returns the transaction type.
+    fn tx_type(&self) -> Self::Type {
+        Self::Type::try_from(self.ty()).expect("should decode tx type id")
+    }
 }
-
-#[cfg(not(feature = "arbitrary"))]
-/// Helper trait that requires arbitrary implementation if the feature is enabled.
-pub trait MaybeArbitrary {}
-
-#[cfg(feature = "arbitrary")]
-/// Helper trait that requires arbitrary implementation if the feature is enabled.
-pub trait MaybeArbitrary: for<'a> arbitrary::Arbitrary<'a> {}
-
-/// Helper trait that unifies all behaviour required by transaction to support full node operations.
-pub trait FullTransaction: Transaction + Compact {}
-
-impl<T> FullTransaction for T where T: Transaction + Compact {}
