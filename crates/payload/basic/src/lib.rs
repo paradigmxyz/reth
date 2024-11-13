4711ff7eb203df867b709255452c6fd609c451c2
Arsenii Kulikov <klkvrr@gmail.com>
2024-11-13 20:56:49 +0400

    fix match

--- crates/payload/basic/src/lib.rs
+++ crates/payload/basic/src/lib.rs
@@ -769,7 +769,7 @@ impl<Payload> BuildOutcome<Payload> {
     /// Consumes the type and returns the payload if the outcome is `Better`.
     pub fn into_payload(self) -> Option<Payload> {
         match self {
-            Self::Better { payload, .. } => Some(payload),
+            Self::Better { payload, .. } | Self::Freeze(payload) => Some(payload),
             _ => None,
         }
     }
