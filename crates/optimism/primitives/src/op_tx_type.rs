4bf278f1f3eab37183b3983b9c39e771c3151bdb
kamuik16 <krishang.s@ahduni.edu.in>
2024-11-14 18:38:10 +0530

    feat: implement Compact for OpTxType

--- crates/optimism/primitives/src/op_tx_type.rs
+++ crates/optimism/primitives/src/op_tx_type.rs
@@ -2,6 +2,7 @@
 //! `OpTxType` implements `reth_primitives_traits::TxType`.
 //! This type is required because a `Compact` impl is needed on the deposit tx type.
 
+use alloy_consensus::constants::EIP7702_TX_TYPE_ID;
 use alloy_primitives::{U64, U8};
 use alloy_rlp::{Decodable, Encodable, Error};
 use bytes::BufMut;
@@ -10,8 +11,26 @@ use derive_more::{
     derive::{From, Into},
     Display,
 };
-use op_alloy_consensus::OpTxType as AlloyOpTxType;
+use op_alloy_consensus::{OpTxType as AlloyOpTxType, DEPOSIT_TX_TYPE_ID};
 use std::convert::TryFrom;
+
+/// Identifier parameter for legacy transaction
+#[cfg(any(test, feature = "reth-codec"))]
+pub(crate) const COMPACT_IDENTIFIER_LEGACY: usize = 0;
+
+/// Identifier parameter for EIP-2930 transaction
+#[cfg(any(test, feature = "reth-codec"))]
+pub(crate) const COMPACT_IDENTIFIER_EIP2930: usize = 1;
+
+/// Identifier parameter for EIP-1559 transaction
+#[cfg(any(test, feature = "reth-codec"))]
+pub(crate) const COMPACT_IDENTIFIER_EIP1559: usize = 2;
+
+/// For backwards compatibility purposes only 2 bits of the type are encoded in the identifier
+/// parameter. In the case of a [`COMPACT_EXTENDED_IDENTIFIER_FLAG`], the full transaction type is
+/// read from the buffer as a single byte.
+#[cfg(any(test, feature = "reth-codec"))]
+pub(crate) const COMPACT_EXTENDED_IDENTIFIER_FLAG: usize = 3;
 
 /// Wrapper type for `AlloyOpTxType` to implement `TxType` trait.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Display, Ord, Hash, From, Into)]
@@ -94,10 +113,55 @@ impl Decodable for OpTxType {
     }
 }
 
+#[cfg(any(test, feature = "reth-codec"))]
+impl reth_codecs::Compact for OpTxType {
+    fn to_compact<B>(&self, buf: &mut B) -> usize
+    where
+        B: bytes::BufMut + AsMut<[u8]>,
+    {
+        match self.0 {
+            AlloyOpTxType::Legacy => COMPACT_IDENTIFIER_LEGACY,
+            AlloyOpTxType::Eip2930 => COMPACT_IDENTIFIER_EIP2930,
+            AlloyOpTxType::Eip1559 => COMPACT_IDENTIFIER_EIP1559,
+            AlloyOpTxType::Eip7702 => {
+                buf.put_u8(EIP7702_TX_TYPE_ID);
+                COMPACT_EXTENDED_IDENTIFIER_FLAG
+            }
+            AlloyOpTxType::Deposit => {
+                buf.put_u8(DEPOSIT_TX_TYPE_ID);
+                COMPACT_EXTENDED_IDENTIFIER_FLAG
+            }
+        }
+    }
+
+    fn from_compact(mut buf: &[u8], identifier: usize) -> (Self, &[u8]) {
+        use bytes::Buf;
+        (
+            match identifier {
+                COMPACT_IDENTIFIER_LEGACY => OpTxType(AlloyOpTxType::Legacy),
+                COMPACT_IDENTIFIER_EIP2930 => OpTxType(AlloyOpTxType::Eip2930),
+                COMPACT_IDENTIFIER_EIP1559 => OpTxType(AlloyOpTxType::Eip1559),
+                COMPACT_EXTENDED_IDENTIFIER_FLAG => {
+                    let extended_identifier = buf.get_u8();
+                    match extended_identifier {
+                        EIP7702_TX_TYPE_ID => OpTxType(AlloyOpTxType::Eip7702),
+                        DEPOSIT_TX_TYPE_ID => OpTxType(AlloyOpTxType::Deposit),
+                        _ => panic!("Unsupported OpTxType identifier: {extended_identifier}"),
+                    }
+                }
+                _ => panic!("Unknown identifier for OpTxType: {identifier}"),
+            },
+            buf,
+        )
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use bytes::BytesMut;
+    use reth_codecs::Compact;
+    use rstest::rstest;
 
     #[test]
     fn test_from_alloy_op_tx_type() {
@@ -185,5 +249,43 @@ mod tests {
         let mut buf: &[u8] = &[255];
         let result = OpTxType::decode(&mut buf);
         assert!(result.is_err());
+    }
+
+    #[rstest]
+    #[case(OpTxType(AlloyOpTxType::Legacy), COMPACT_IDENTIFIER_LEGACY, vec![])]
+    #[case(OpTxType(AlloyOpTxType::Eip2930), COMPACT_IDENTIFIER_EIP2930, vec![])]
+    #[case(OpTxType(AlloyOpTxType::Eip1559), COMPACT_IDENTIFIER_EIP1559, vec![])]
+    #[case(OpTxType(AlloyOpTxType::Eip7702), COMPACT_EXTENDED_IDENTIFIER_FLAG, vec![EIP7702_TX_TYPE_ID])]
+    #[case(OpTxType(AlloyOpTxType::Deposit), COMPACT_EXTENDED_IDENTIFIER_FLAG, vec![DEPOSIT_TX_TYPE_ID])]
+    fn test_txtype_to_compact(
+        #[case] tx_type: OpTxType,
+        #[case] expected_identifier: usize,
+        #[case] expected_buf: Vec<u8>,
+    ) {
+        let mut buf = vec![];
+        let identifier = tx_type.to_compact(&mut buf);
+
+        assert_eq!(
+            identifier, expected_identifier,
+            "Unexpected identifier for OpTxType {tx_type:?}",
+        );
+        assert_eq!(buf, expected_buf, "Unexpected buffer for OpTxType {tx_type:?}",);
+    }
+
+    #[rstest]
+    #[case(OpTxType(AlloyOpTxType::Legacy), COMPACT_IDENTIFIER_LEGACY, vec![])]
+    #[case(OpTxType(AlloyOpTxType::Eip2930), COMPACT_IDENTIFIER_EIP2930, vec![])]
+    #[case(OpTxType(AlloyOpTxType::Eip1559), COMPACT_IDENTIFIER_EIP1559, vec![])]
+    #[case(OpTxType(AlloyOpTxType::Eip7702), COMPACT_EXTENDED_IDENTIFIER_FLAG, vec![EIP7702_TX_TYPE_ID])]
+    #[case(OpTxType(AlloyOpTxType::Deposit), COMPACT_EXTENDED_IDENTIFIER_FLAG, vec![DEPOSIT_TX_TYPE_ID])]
+    fn test_txtype_from_compact(
+        #[case] expected_type: OpTxType,
+        #[case] identifier: usize,
+        #[case] buf: Vec<u8>,
+    ) {
+        let (actual_type, remaining_buf) = OpTxType::from_compact(&buf, identifier);
+
+        assert_eq!(actual_type, expected_type, "Unexpected TxType for identifier {identifier}");
+        assert!(remaining_buf.is_empty(), "Buffer not fully consumed for identifier {identifier}");
     }
 }
