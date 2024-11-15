0280f246577b1090f87b1e0301e414b092297b76
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-11-15 19:52:53 +0100

    Shrink scope

--- crates/primitives-traits/src/header/mod.rs
+++ crates/primitives-traits/src/header/mod.rs
@@ -7,9 +7,6 @@ pub use error::HeaderError;
 #[cfg(any(test, feature = "test-utils", feature = "arbitrary"))]
 pub mod test_utils;
 
-use alloy_consensus::Header;
-use alloy_primitives::{Address, BlockNumber, B256, U256};
-
 /// Bincode-compatible header type serde implementations.
 #[cfg(feature = "serde-bincode-compat")]
 pub mod serde_bincode_compat {
