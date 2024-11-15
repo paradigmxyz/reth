dbe3104c71163706ed48308972dab7367b731013
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-11-15 20:31:09 +0100

    Feature gate serialization for types in reth-primitives-traits

--- crates/primitives-traits/src/account.rs
+++ crates/primitives-traits/src/account.rs
@@ -24,7 +24,8 @@ const EOF_BYTECODE_ID: u8 = 3;
 const EIP7702_BYTECODE_ID: u8 = 4;
 
 /// An Ethereum account.
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Default, Serialize, Deserialize, Compact)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default, Compact)]
 #[cfg_attr(any(test, feature = "arbitrary"), derive(arbitrary::Arbitrary))]
 #[add_arbitrary_tests(compact)]
 pub struct Account {
@@ -60,7 +61,8 @@ impl Account {
 /// Bytecode for an account.
 ///
 /// A wrapper around [`revm::primitives::Bytecode`][RevmBytecode] with encoding/decoding support.
-#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize, Deref)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, Clone, Default, PartialEq, Eq, Deref)]
 pub struct Bytecode(pub RevmBytecode);
 
 impl Bytecode {
