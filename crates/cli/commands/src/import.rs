31ee39f18494312104e2f99323629746d4ae68fa
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2024-11-16 05:39:49 +0100

    rm generics when useless (#12581)

--- crates/cli/commands/src/import.rs
+++ crates/cli/commands/src/import.rs
@@ -203,7 +203,7 @@ where
 
     let max_block = file_client.max_block().unwrap_or(0);
 
-    let pipeline = Pipeline::<N>::builder()
+    let pipeline = Pipeline::builder()
         .with_tip_sender(tip_tx)
         // we want to sync all blocks the file client provides or 0 if empty
         .with_max_block(max_block)
