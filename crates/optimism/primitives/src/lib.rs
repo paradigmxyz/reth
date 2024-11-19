47badb27f6d118aeec047a1c70a1ede1b679c776
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-11-19 19:31:19 +0100

    Fix merge conflicts op

--- crates/optimism/primitives/src/lib.rs
+++ crates/optimism/primitives/src/lib.rs
@@ -13,14 +13,14 @@ pub mod tx_type;
 pub use tx_type::OpTxType;
 
 use alloy_consensus::Header;
-use reth_node_types::NodePrimitives;
+use reth_node_types::FullNodePrimitives;
 use reth_primitives::{Block, BlockBody, Receipt, TransactionSigned};
 
 /// Optimism primitive types.
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct OpPrimitives;
 
-impl NodePrimitives for OpPrimitives {
+impl FullNodePrimitives for OpPrimitives {
     type Block = Block;
     type BlockHeader = Header;
     type BlockBody = BlockBody;
