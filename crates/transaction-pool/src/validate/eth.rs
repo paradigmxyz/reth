9d3f8cc6a27e59396ede209f3d439eb334e24eed
Matthias Seitz <matthias.seitz@outlook.de>
2024-11-21 14:31:03 +0100

    docs: add additional eth validator docs (#12742)

--- crates/transaction-pool/src/validate/eth.rs
+++ crates/transaction-pool/src/validate/eth.rs
@@ -107,6 +107,19 @@ where
 }
 
 /// A [`TransactionValidator`] implementation that validates ethereum transaction.
+///
+/// It supports all known ethereum transaction types:
+/// - Legacy
+/// - EIP-2718
+/// - EIP-1559
+/// - EIP-4844
+/// - EIP-7702
+///
+/// And enforces additional constraints such as:
+/// - Maximum transaction size
+/// - Maximum gas limit
+///
+/// And adheres to the configured [`LocalTransactionConfig`].
 #[derive(Debug)]
 pub(crate) struct EthTransactionValidatorInner<Client, T> {
     /// Spec of the chain
