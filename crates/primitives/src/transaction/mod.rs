e080e98d3733af36ca55fa6ecb5820929d1ba161
Matthias Seitz <matthias.seitz@outlook.de>
2024-11-22 01:07:04 +0100

    chore: rm tx alias re-export (#12762)

--- crates/primitives/src/transaction/mod.rs
+++ crates/primitives/src/transaction/mod.rs
@@ -6,7 +6,6 @@ use alloy_consensus::{
     TxEip4844, TxEip7702, TxLegacy,
 };
 use alloy_eips::{
-    eip1898::BlockHashOrNumber,
     eip2718::{Decodable2718, Eip2718Error, Eip2718Result, Encodable2718},
     eip2930::AccessList,
     eip7702::SignedAuthorization,
@@ -60,9 +59,6 @@ pub mod signature;
 
 pub(crate) mod util;
 mod variant;
-
-/// Either a transaction hash or number.
-pub type TxHashOrNumber = BlockHashOrNumber;
 
 /// Expected number of transactions where we can expect a speed-up by recovering the senders in
 /// parallel.
