7e519a26b6092598fc5318097723fc8c2a501174
Federico Gimenez <federico.gimenez@gmail.com>
2024-11-22 10:45:19 +0100

    fmt

--- crates/engine/tree/src/tree/root.rs
+++ crates/engine/tree/src/tree/root.rs
@@ -177,7 +177,8 @@ where
 
                 if destroyed || !account.storage.is_empty() {
                     let storage = account.storage.into_iter().filter_map(|(slot, value)| {
-                        value.is_changed()
+                        value
+                            .is_changed()
                             .then(|| (keccak256(B256::from(slot)), value.present_value))
                     });
                     hashed_state_update
