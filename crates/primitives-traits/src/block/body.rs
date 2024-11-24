5c97d4ec569ba0aad87f1bdf4936c3a7451a4707
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-11-24 09:05:45 +0100

    Default impl BlockBody::recover_signers

--- crates/primitives-traits/src/block/body.rs
+++ crates/primitives-traits/src/block/body.rs
@@ -1,10 +1,26 @@
 //! Block body abstraction.
 
 use alloc::fmt;
+#[cfg(feature = "std")]
+use std::sync::LazyLock;
 
-use alloy_consensus::Transaction;
+use alloy_primitives::Address;
+#[cfg(feature = "std")]
+use once_cell as _;
+#[cfg(not(feature = "std"))]
+use once_cell::sync::Lazy as LazyLock;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
-use crate::{FullSignedTx, InMemorySize, MaybeSerde};
+use crate::{FullSignedTx, InMemorySize, MaybeSerde, SignedTransaction};
+
+/// Expected number of transactions where we can expect a speed-up by recovering the senders in
+/// parallel.
+pub static PARALLEL_SENDER_RECOVERY_THRESHOLD: LazyLock<usize> =
+    LazyLock::new(|| match rayon::current_num_threads() {
+        0..=1 => usize::MAX,
+        2..=8 => 10,
+        _ => 5,
+    });
 
 /// Helper trait that unifies all behaviour required by transaction to support full node operations.
 pub trait FullBlockBody: BlockBody<Transaction: FullSignedTx> {}
@@ -28,8 +44,17 @@ pub trait BlockBody:
     + MaybeSerde
 {
     /// Ordered list of signed transactions as committed in block.
-    type Transaction: Transaction;
+    type Transaction: SignedTransaction;
 
     /// Returns reference to transactions in block.
     fn transactions(&self) -> &[Self::Transaction];
+
+    /// Recover signer addresses for all transactions in the block body.
+    fn recover_signers(&self) -> Option<Vec<Address>> {
+        if self.transactions().len() < *PARALLEL_SENDER_RECOVERY_THRESHOLD {
+            self.transactions().iter().map(|tx| tx.recover_signer()).collect()
+        } else {
+            self.transactions().par_iter().map(|tx| tx.recover_signer()).collect()
+        }
+    }
 }
