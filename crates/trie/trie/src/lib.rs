f2d8616b549a1ba276a0d32d7075a755c7ac3e72
Roman Krasiuk <rokrassyuk@gmail.com>
2024-11-26 13:09:12 +0100

    chore(trie): move trie updates to `reth-trie-common` (#12863)

--- crates/trie/trie/src/lib.rs
+++ crates/trie/trie/src/lib.rs
@@ -50,9 +50,6 @@ pub mod witness;
 mod trie;
 pub use trie::{StateRoot, StorageRoot};
 
-/// Buffer for trie updates.
-pub mod updates;
-
 /// Utilities for state root checkpoint progress.
 mod progress;
 pub use progress::{IntermediateStateRootState, StateRootProgress};
@@ -62,17 +59,6 @@ pub mod stats;
 
 // re-export for convenience
 pub use reth_trie_common::*;
-
-/// Bincode-compatible serde implementations for trie types.
-///
-/// `bincode` crate allows for more efficient serialization of trie types, because it allows
-/// non-string map keys.
-///
-/// Read more: <https://github.com/paradigmxyz/reth/issues/11370>
-#[cfg(all(feature = "serde", feature = "serde-bincode-compat"))]
-pub mod serde_bincode_compat {
-    pub use super::updates::serde_bincode_compat as updates;
-}
 
 /// Trie calculation metrics.
 #[cfg(feature = "metrics")]
