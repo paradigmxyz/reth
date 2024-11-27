8d70e8921b2f932e544f3cfe1cf662df9ef197ec
Tien Nguyen <116023870+htiennv@users.noreply.github.com>
2024-11-28 02:59:50 +0700

    chore: pass generic header to validate_header_base_fee (#12921)

--- crates/optimism/consensus/src/lib.rs
+++ crates/optimism/consensus/src/lib.rs
@@ -93,7 +93,7 @@ impl Consensus for OpBeaconConsensus {
 impl HeaderValidator for OpBeaconConsensus {
     fn validate_header(&self, header: &SealedHeader) -> Result<(), ConsensusError> {
         validate_header_gas(header)?;
-        validate_header_base_fee(header, &self.chain_spec)
+        validate_header_base_fee(header.header(), &self.chain_spec)
     }
 
     fn validate_header_against_parent(
