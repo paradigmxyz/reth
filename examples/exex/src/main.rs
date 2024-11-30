2f999a756d9f38006266afc49544bcdb23275478
-f <kunalarora1729@gmail.com>
2024-11-30 18:39:24 +0530

    exex not done

--- examples/exex/src/main.rs
+++ examples/exex/src/main.rs
@@ -52,49 +52,111 @@ fn main() -> eyre::Result<()> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use reth_execution_types::{Chain, ExecutionOutcome};
     use reth_exex_test_utils::{test_exex_context, PollOnce};
-    use reth_exex::Wal;
-    use std::pin::pin;
+    use reth_exex::{ExExEvent, ExExHandle, ExExManager, ExExNotification, ExExNotificationSource, Wal};
+    use reth_node_ethereum::EthExecutorProvider;
+    use tokio::sync::watch;
+    use reth_chain_state::ForkChoiceStream;
+    use std::{pin::pin, sync::Arc, task::{Context, Poll}};
 
     #[tokio::test]
     async fn test_exex() -> eyre::Result<()> {
+        // need exexManager?
+
         // Initialize a test Execution Extension context with all dependencies
         let (ctx, mut handle) = test_exex_context().await?;
-
-        // Save the current head of the chain to check the finished height against it later
         let head = ctx.head;
 
+        let chain = Chain::from_block(
+            handle.genesis.clone(),
+            ExecutionOutcome::default(),
+            None,
+        );
 
-        
-        let wal = Wal::new(handle.wal_directory.path()).unwrap();
-        println!("wal: {:?}", wal.num_blocks());
+        let wal = Wal::new(handle.wal_directory.path())?;
+        let (finalized_headers_tx, rx) = watch::channel(None);
+        let finalized_header_stream = ForkChoiceStream::new(rx);
 
-        // Send a notification to the Execution Extension that the chain has been committed
-        handle
-            .send_notification_chain_committed(Chain::from_block(
-                handle.genesis.clone(),
-                ExecutionOutcome::default(),
-                None,
-            ))
-            .await?;
+        let (exex_handle, events_tx, mut notifications) = ExExHandle::new(
+            "test_exex".to_string(),
+            head,
+            handle.provider_factory.clone(),
+            EthExecutorProvider::mainnet(),
+            wal.handle(),
+        );
 
-        // Initialize the Execution Extension
+        let mut exex_manager = pin!(ExExManager::new(
+            handle.provider_factory.clone(),
+            vec![exex_handle],
+            2, // buffer size
+            wal,
+            finalized_header_stream
+        ));
+
+        let mut cx = Context::from_waker(futures::task::noop_waker_ref());
+
+        exex_manager
+            .handle()
+            .send(ExExNotificationSource::Pipeline, ExExNotification::ChainCommitted {
+                new: Arc::new(chain.clone()),
+            })?;
+
+        assert!(exex_manager.as_mut().poll(&mut cx)?.is_pending());
+        // assert_eq!(
+        //     notifications.try_poll_next_unpin(&mut cx)?,
+        //     Poll::Ready(Some(ExExNotification::ChainCommitted {
+        //         new: Arc::new(chain.clone())
+        //     }))
+        // );
+
         let mut exex = pin!(super::exex_init(ctx).await?);
-
-        println!("wal: {:?}", wal.num_blocks());
-
-        // Check that the Execution Extension did not emit any events until we polled it
-        handle.assert_events_empty();
-
-        // Poll the Execution Extension once to process incoming notifications
         exex.poll_once().await?;
 
-        println!("wal: {:?}", wal.num_blocks());
+        events_tx.send(ExExEvent::FinishedHeight((head.number, head.hash).into()))?;
 
-        // Check that the Execution Extension emitted a `FinishedHeight` event with the correct
-        // height
-        handle.assert_event_finished_height((head.number, head.hash).into())?;
+        finalized_headers_tx.send(Some(handle.genesis.header.clone()))?;
+
+        // assert_eq!(
+        //     exex_manager.wal.iter_notifications()?.next().transpose()?,
+        //     None,
+        //     "WAL should be empty after finalization"
+        // );
+
+        // Verify FinishedHeight event was emitted
+        // handle.assert_event_finished_height((head.number, head.hash).into())?;
+
+        // is 
+        // println!("wal: {:?}", wal.num_blocks());
+        // Send a notification to the Execution Extension that the chain has been committed
+        // handle
+        //     .send_notification_chain_committed(chain)
+        //     .await?;
+
+        
+
+        // finalized_headers_tx.send(Some(handle.genesis.header.clone()))?;
+
+        // let mut cx = Context::from_waker(futures::task::noop_waker_ref());
+        // assert!(exex_manager.as_mut().poll(&mut cx).is_pending());
+
+        // Check that the Execution Extension did not emit any events until we polled it
+        // handle.assert_events_empty();
+
+        // Poll the Execution Extension once to process incoming notifications
+        // exex.poll_once().await?;
+
+        // wal.finalize((head.number, head.hash).into())?;
+        
+
+        // println!("wal 2: {:?}", wal.num_blocks());
+
+        // assert_eq!(
+        //     wal.iter_notifications()?.next().transpose()?,
+        //     None,
+        //     "WAL should be empty after finalization"
+        // );
 
         // assert!(ctx.state)
 
