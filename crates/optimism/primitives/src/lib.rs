f76d529f6b268754eda0a11acd7509d27ab246e8
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-12-02 22:04:45 -0600

    Fix merge conflicts

--- crates/optimism/primitives/src/lib.rs
+++ crates/optimism/primitives/src/lib.rs
@@ -7,7 +7,7 @@
 )]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 // The `optimism` feature must be enabled to use this crate.
-#![cfg(feature = "optimism")]
+#![cfg(feature = "op")]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(not(feature = "std"), no_std)]
 
