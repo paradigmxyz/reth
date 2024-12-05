f56d868df3ddcebe96f4e71e24bb8b404c162831
Arsenii Kulikov <klkvrr@gmail.com>
2024-12-05 19:19:12 +0400

    fix doc

--- crates/rpc/rpc-eth-types/src/utils.rs
+++ crates/rpc/rpc-eth-types/src/utils.rs
@@ -9,7 +9,7 @@ use super::{EthApiError, EthResult};
 
 /// Recovers a [`SignedTransaction`] from an enveloped encoded byte stream.
 ///
-/// See [`Decodable2718::decode_2718`]
+/// See [`alloy_eips::eip2718::Decodable2718::decode_2718`]
 pub fn recover_raw_transaction<T: SignedTransaction>(data: Bytes) -> EthResult<RecoveredTx<T>> {
     if data.is_empty() {
         return Err(EthApiError::EmptyRawTransactionData)
