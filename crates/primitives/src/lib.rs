77cdee9b0bf245034549137aa19e1d5c9f98f786
Matthias Seitz <matthias.seitz@outlook.de>
2024-12-09 23:05:02 +0100

    feat: extract zstd compressors

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -27,8 +27,6 @@ pub use traits::*;
 #[cfg(feature = "alloy-compat")]
 mod alloy_compat;
 mod block;
-#[cfg(feature = "reth-codec")]
-mod compression;
 pub mod proofs;
 mod receipt;
 pub use reth_static_file_types as static_file;
@@ -38,8 +36,6 @@ pub use block::{generate_valid_header, valid_header_strategy};
 pub use block::{
     Block, BlockBody, BlockWithSenders, SealedBlock, SealedBlockFor, SealedBlockWithSenders,
 };
-#[cfg(feature = "reth-codec")]
-pub use compression::*;
 pub use receipt::{gas_spent_by_transactions, Receipt, Receipts};
 pub use reth_primitives_traits::{
     logs_bloom, Account, Bytecode, GotExpected, GotExpectedBoxed, Header, HeaderError, Log,
