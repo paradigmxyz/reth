ea6be9a0edfbe676ab8aa37d94383da58f144908
moricho <ikeda.morito@gmail.com>
2024-12-11 01:56:25 +0900

    remove the other methods from `Transaction`

--- crates/primitives-traits/src/transaction/mod.rs
+++ crates/primitives-traits/src/transaction/mod.rs
@@ -7,8 +7,6 @@ pub mod tx_type;
 use crate::{InMemorySize, MaybeArbitrary, MaybeCompact, MaybeSerde};
 use core::{fmt, hash::Hash};
 
-use alloy_consensus::constants::{EIP1559_TX_TYPE_ID, EIP2930_TX_TYPE_ID, LEGACY_TX_TYPE_ID};
-
 /// Helper trait that unifies all behaviour required by transaction to support full node operations.
 pub trait FullTransaction: Transaction + MaybeCompact {}
 
@@ -29,23 +27,6 @@ pub trait Transaction:
     + MaybeSerde
     + MaybeArbitrary
 {
-    /// Returns true if the transaction is a legacy transaction.
-    #[inline]
-    fn is_legacy(&self) -> bool {
-        self.ty() == LEGACY_TX_TYPE_ID
-    }
-
-    /// Returns true if the transaction is an EIP-2930 transaction.
-    #[inline]
-    fn is_eip2930(&self) -> bool {
-        self.ty() == EIP2930_TX_TYPE_ID
-    }
-
-    /// Returns true if the transaction is an EIP-1559 transaction.
-    #[inline]
-    fn is_eip1559(&self) -> bool {
-        self.ty() == EIP1559_TX_TYPE_ID
-    }
 }
 
 impl<T> Transaction for T where
