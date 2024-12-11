b97fe1b8f823eae97ed883bdfbf4d318c13da8c6
Arsenii Kulikov <klkvrr@gmail.com>
2024-12-11 20:01:28 +0400

    fix

--- crates/e2e-test-utils/src/engine_api.rs
+++ crates/e2e-test-utils/src/engine_api.rs
@@ -1,5 +1,5 @@
 use crate::traits::PayloadEnvelopeExt;
-use alloy_primitives::{B256, U256};
+use alloy_primitives::{B256, U64};
 use alloy_rpc_types_engine::{ForkchoiceState, PayloadStatusEnum};
 use jsonrpsee::{
     core::client::ClientT,
@@ -75,7 +75,7 @@ impl<E: EngineTypes, ChainSpec: EthereumHardforks> EngineApiTestContext<E, Chain
                 versioned_hashes,
                 payload_builder_attributes.parent_beacon_block_root().unwrap(),
                 requests,
-                U256::from(payload_builder_attributes.target_blobs_per_block().unwrap()),
+                U64::from(payload_builder_attributes.target_blobs_per_block().unwrap()),
             )
             .await?
         } else {
