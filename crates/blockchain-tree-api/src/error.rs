17821d8fb5761ec8f56b7ef6f7d4cfb64d49f6bd
pellekrab <78560773+PelleKrab@users.noreply.github.com>
2024-12-12 15:49:14 -0700

    fmt / zepter

--- crates/blockchain-tree-api/src/error.rs
+++ crates/blockchain-tree-api/src/error.rs
@@ -167,7 +167,6 @@ impl std::fmt::Debug for InsertBlockError {
 }
 
 #[derive(thiserror::Error, Debug)]
-
 #[error("Failed to insert block (hash={}, number={}, parent_hash={}): {kind}", 
         .block.hash(),
         .block.number,
