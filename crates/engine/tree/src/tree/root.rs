6d4226e48ed420a61f37296e79a6ed44022f2a19
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2024-12-12 17:29:10 +0000

    Revert "Revert "Revert "Revert "perf(engine): use ParallelProof::multiproof in StateRootTask""""
    This reverts commit 9028ef8f673a48d3482976c481e066b6b36fb618.

--- crates/engine/tree/src/tree/root.rs
+++ crates/engine/tree/src/tree/root.rs
@@ -15,8 +15,8 @@ use reth_trie::{
     updates::{TrieUpdates, TrieUpdatesSorted},
     HashedPostState, HashedPostStateSorted, HashedStorage, MultiProof, Nibbles, TrieInput,
 };
-use reth_trie_db::{DatabaseHashedCursorFactory, DatabaseProof, DatabaseTrieCursorFactory};
-use reth_trie_parallel::root::ParallelStateRootError;
+use reth_trie_db::{DatabaseHashedCursorFactory, DatabaseTrieCursorFactory};
+use reth_trie_parallel::{proof::ParallelProof, root::ParallelStateRootError};
 use reth_trie_sparse::{
     errors::{SparseStateTrieResult, SparseTrieError},
     SparseStateTrie,
@@ -321,21 +321,8 @@ where
             "Spawning proof task"
         );
         rayon::spawn(move || {
-            let provider = match view.provider_ro() {
-                Ok(provider) => provider,
-                Err(error) => {
-                    error!(target: "engine::root", ?error, "Could not get provider");
-                    return;
-                }
-            };
+            let result = ParallelProof::new(view, input.clone()).multiproof(proof_targets);
 
-            // TODO: replace with parallel proof
-            let result = Proof::overlay_multiproof(
-                provider.tx_ref(),
-                // TODO(alexey): this clone can be expensive, we should avoid it
-                input.as_ref().clone(),
-                proof_targets,
-            );
             match result {
                 Ok(proof) => {
                     let _ = state_root_message_sender.send(StateRootMessage::ProofCalculated {
