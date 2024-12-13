f99174093c8a254f11ef1e7f3d7ee7f14125c9d0
Matthias Seitz <matthias.seitz@outlook.de>
2024-12-13 13:17:37 +0100

    fix

--- crates/primitives-traits/src/transaction/signed.rs
+++ crates/primitives-traits/src/transaction/signed.rs
@@ -7,17 +7,8 @@ use crate::{
 use alloc::{fmt, vec::Vec};
 use alloy_consensus::{transaction::PooledTransaction, SignableTransaction};
 use alloy_eips::eip2718::{Decodable2718, Encodable2718};
-use alloy_primitives::{keccak256, Address, PrimitiveSignature as Signature, TxHash, B256, U256};
+use alloy_primitives::{keccak256, Address, PrimitiveSignature as Signature, TxHash, B256};
 use core::hash::Hash;
-
-/// The order of the secp256k1 curve, divided by two. Signatures that should be checked according
-/// to EIP-2 should have an S value less than or equal to this.
-///
-/// `57896044618658097711785492504343953926418782139537452191302581570759080747168`
-pub const SECP256K1N_HALF: U256 = U256::from_be_bytes([
-    0x7F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-    0x5D, 0x57, 0x6E, 0x73, 0x57, 0xA4, 0x50, 0x1D, 0xDF, 0xE9, 0x2F, 0x46, 0x68, 0x1B, 0x20, 0xA0,
-]);
 
 /// Helper trait that unifies all behaviour required by block to support full node operations.
 pub trait FullSignedTx: SignedTransaction + FillTxEnv + MaybeCompact {}
