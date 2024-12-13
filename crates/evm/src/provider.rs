7569a5fbf086c4196812ca8d199e02921db25fcc
Ayodeji Akinola <akinayodeji4all@gmail.com>
2024-12-13 08:41:46 +0100

    more fixes

--- crates/evm/src/provider.rs
+++ crates/evm/src/provider.rs
@@ -5,13 +5,13 @@ use alloy_consensus::Header;
 use reth_storage_errors::provider::ProviderResult;
 
 /// A provider type that knows chain specific information required to configure a
-/// [`CfgEnvWithHandlerCfg`].
+/// [`EvmEnv`].
 ///
 /// This type is mainly used to provide required data to configure the EVM environment that is
 /// not part of the block and stored separately (on disk), for example the total difficulty.
 #[auto_impl::auto_impl(&, Arc)]
 pub trait EvmEnvProvider<H = Header>: Send + Sync {
-    /// Fills the default [`CfgEnvWithHandlerCfg`] and [BlockEnv] fields with values specific to the
+    /// Fills the default [`EvmEnv`] fields with values specific to the
     /// given block header.
     fn env_with_header<EvmConfig>(
         &self,
