7f842514da43505f02cdcde454b04d1c32f5f3df
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2024-12-20 09:14:13 +0200

    Update root.rs
    Co-authored-by: Federico Gimenez <fgimenez@users.noreply.github.com>

--- crates/engine/tree/src/tree/root.rs
+++ crates/engine/tree/src/tree/root.rs
@@ -248,7 +248,7 @@ fn evm_state_to_hashed_post_state(update: EvmState) -> HashedPostState {
             } else if changed_storage_iter.peek().is_some() {
                 hashed_state.storages.insert(
                     hashed_address,
-                    HashedStorage::from_iter(destroyed, changed_storage_iter),
+                    HashedStorage::from_iter(false, changed_storage_iter),
                 );
             }
         }
