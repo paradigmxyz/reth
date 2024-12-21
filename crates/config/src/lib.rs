b6f218c195fa52c9caff9be17dc45e4fa7cec1ec
Dmitry <98899785+mdqst@users.noreply.github.com>
2024-12-21 21:56:25 +0300

    Fix incorrect favicon URL Update lib.rs

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -2,7 +2,7 @@
 
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
-    html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
+    html_favicon_url = "https://avatars.githubusercontent.com/u/97369466?s=256"
     issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
