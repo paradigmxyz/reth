f305e1a32cc7c350b502d906c536264d345f04c9
tiendn <15717476+tiendn@users.noreply.github.com>
2024-12-23 07:57:58 +0700

    fix: use external document https instead of http scheme

--- crates/storage/libmdbx-rs/mdbx-sys/libmdbx/cmake/compiler.cmake
+++ crates/storage/libmdbx-rs/mdbx-sys/libmdbx/cmake/compiler.cmake
@@ -4,7 +4,7 @@
 ##  you may not use this file except in compliance with the License.
 ##  You may obtain a copy of the License at
 ##
-##      http://www.apache.org/licenses/LICENSE-2.0
+##      https://www.apache.org/licenses/LICENSE-2.0
 ##
 ##  Unless required by applicable law or agreed to in writing, software
 ##  distributed under the License is distributed on an "AS IS" BASIS,
@@ -830,7 +830,7 @@ macro(setup_compile_flags)
 
   if(CMAKE_COMPILER_IS_GNU${CMAKE_PRIMARY_LANG}
       AND CMAKE_${CMAKE_PRIMARY_LANG}_COMPILER_VERSION VERSION_LESS 5)
-    # G++ bug. http://gcc.gnu.org/bugzilla/show_bug.cgi?id=31488
+    # G++ bug. https://gcc.gnu.org/bugzilla/show_bug.cgi?id=31488
     add_compile_flags("CXX" "-Wno-invalid-offsetof")
   endif()
   if(MINGW)
