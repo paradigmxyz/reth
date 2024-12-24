edabeabe671ddee9a3128a34e9d7954412cfd769
Matthias Seitz <matthias.seitz@outlook.de>
2024-12-24 14:38:24 +0100

    chore: remove redundant generic

--- crates/rpc/rpc-eth-api/src/ext.rs
+++ crates/rpc/rpc-eth-api/src/ext.rs
@@ -1,6 +1,5 @@
 //! `eth_` Extension traits.
 
-use alloy_json_rpc::RpcObject;
 use alloy_primitives::{Bytes, B256};
 use alloy_rpc_types_eth::erc4337::ConditionalOptions;
 use jsonrpsee::{core::RpcResult, proc_macros::rpc};
@@ -8,7 +7,7 @@ use jsonrpsee::{core::RpcResult, proc_macros::rpc};
 /// Extension trait for `eth_` namespace for L2s.
 #[cfg_attr(not(feature = "client"), rpc(server, namespace = "eth"))]
 #[cfg_attr(feature = "client", rpc(server, client, namespace = "eth"))]
-pub trait L2EthApiExt<T: RpcObject> {
+pub trait L2EthApiExt {
     /// Sends signed transaction with the given condition.
     #[method(name = "sendRawTransactionConditional")]
     async fn send_raw_transaction_conditional(
