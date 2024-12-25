3fbb23035f1295c668b7cf02b2ba6d64345c80f7
greg <82421016+greged93@users.noreply.github.com>
2024-12-25 09:42:57 +0100

    fix: debug client etherscan (#13542)
    Signed-off-by: Gregory Edison <gregory.edison1993@gmail.com>
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/consensus/debug-client/src/providers/etherscan.rs
+++ crates/consensus/debug-client/src/providers/etherscan.rs
@@ -32,13 +32,17 @@ impl EtherscanBlockProvider {
     /// `BlockNumberOrTag::Earliest`, `BlockNumberOrTag::Pending`, `BlockNumberOrTag::Number(u64)`
     /// are supported.
     pub async fn load_block(&self, block_number_or_tag: BlockNumberOrTag) -> eyre::Result<Block> {
+        let tag = match block_number_or_tag {
+            BlockNumberOrTag::Number(num) => format!("{num:#02x}"),
+            tag => tag.to_string(),
+        };
         let block: EtherscanBlockResponse = self
             .http_client
             .get(&self.base_url)
             .query(&[
                 ("module", "proxy"),
                 ("action", "eth_getBlockByNumber"),
-                ("tag", &block_number_or_tag.to_string()),
+                ("tag", &tag),
                 ("boolean", "true"),
                 ("apikey", &self.api_key),
             ])
