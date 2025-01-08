21784ebc418ffe3c46fc447188a412584aa2c125
Arsenii Kulikov <klkvrr@gmail.com>
2025-01-08 17:02:49 +0300

    refactor: reduce `Hardforks` trait usage (#13728)

--- crates/consensus/common/src/validation.rs
+++ crates/consensus/common/src/validation.rs
@@ -25,8 +25,7 @@ pub fn validate_header_base_fee<H: BlockHeader, ChainSpec: EthereumHardforks>(
     header: &H,
     chain_spec: &ChainSpec,
 ) -> Result<(), ConsensusError> {
-    if chain_spec.is_fork_active_at_block(EthereumHardfork::London, header.number()) &&
-        header.base_fee_per_gas().is_none()
+    if chain_spec.is_london_active_at_block(header.number()) && header.base_fee_per_gas().is_none()
     {
         return Err(ConsensusError::BaseFeeMissing)
     }
@@ -253,23 +252,25 @@ pub fn validate_against_parent_eip1559_base_fee<
     parent: &H,
     chain_spec: &ChainSpec,
 ) -> Result<(), ConsensusError> {
-    if chain_spec.fork(EthereumHardfork::London).active_at_block(header.number()) {
+    if chain_spec.is_london_active_at_block(header.number()) {
         let base_fee = header.base_fee_per_gas().ok_or(ConsensusError::BaseFeeMissing)?;
 
-        let expected_base_fee =
-            if chain_spec.fork(EthereumHardfork::London).transitions_at_block(header.number()) {
-                alloy_eips::eip1559::INITIAL_BASE_FEE
-            } else {
-                // This BaseFeeMissing will not happen as previous blocks are checked to have
-                // them.
-                let base_fee = parent.base_fee_per_gas().ok_or(ConsensusError::BaseFeeMissing)?;
-                calc_next_block_base_fee(
-                    parent.gas_used(),
-                    parent.gas_limit(),
-                    base_fee,
-                    chain_spec.base_fee_params_at_timestamp(header.timestamp()),
-                )
-            };
+        let expected_base_fee = if chain_spec
+            .ethereum_fork_activation(EthereumHardfork::London)
+            .transitions_at_block(header.number())
+        {
+            alloy_eips::eip1559::INITIAL_BASE_FEE
+        } else {
+            // This BaseFeeMissing will not happen as previous blocks are checked to have
+            // them.
+            let base_fee = parent.base_fee_per_gas().ok_or(ConsensusError::BaseFeeMissing)?;
+            calc_next_block_base_fee(
+                parent.gas_used(),
+                parent.gas_limit(),
+                base_fee,
+                chain_spec.base_fee_params_at_timestamp(header.timestamp()),
+            )
+        };
         if expected_base_fee != base_fee {
             return Err(ConsensusError::BaseFeeDiff(GotExpected {
                 expected: expected_base_fee,
