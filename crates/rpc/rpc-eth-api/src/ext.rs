f98ea5a7403381cc871d41b8fc93713fa8b69b70
Omar Carey <omarcarey@Omars-MacBook-Pro-2.local>
2025-01-09 23:02:29 -0800

    add blanket implementation of L2EthApiExtServer for T

--- crates/rpc/rpc-eth-api/src/ext.rs
+++ crates/rpc/rpc-eth-api/src/ext.rs
@@ -3,6 +3,9 @@
 use alloy_primitives::{Bytes, B256};
 use alloy_rpc_types_eth::erc4337::ConditionalOptions;
 use jsonrpsee::{core::RpcResult, proc_macros::rpc};
+use tracing::trace;
+
+use crate::helpers::{EthTransactions, FullEthApi};
 
 /// Extension trait for `eth_` namespace for L2s.
 #[cfg_attr(not(feature = "client"), rpc(server, namespace = "eth"))]
@@ -15,4 +18,21 @@ pub trait L2EthApiExt {
         bytes: Bytes,
         condition: ConditionalOptions,
     ) -> RpcResult<B256>;
+}
+
+#[async_trait::async_trait]
+impl<T> L2EthApiExtServer for T
+where
+    T: FullEthApi,
+    jsonrpsee_types::error::ErrorObject<'static>: From<T::Error>,
+{
+    /// Handler for: `eth_sendRawTransactionConditional`
+    async fn send_raw_transaction_conditional(
+        &self,
+        bytes: Bytes,
+        condition: ConditionalOptions,
+    ) -> RpcResult<B256> {
+        trace!(target: "rpc::eth", ?bytes, "Serving eth_sendRawTransaction");
+        Ok(EthTransactions::send_raw_transaction(self, bytes).await?)
+    }
 }
