8b6c6d444363aa22842006533e47ab84ea7ccd25
Federico Gimenez <fgimenez@users.noreply.github.com>
2025-01-11 12:15:06 +0100

    chore: make clippy happy (#13772)

--- crates/storage/provider/src/providers/blockchain_provider.rs
+++ crates/storage/provider/src/providers/blockchain_provider.rs
@@ -1867,7 +1867,7 @@ mod tests {
             provider.account_block_changeset(last_database_block).unwrap(),
             database_changesets
                 .into_iter()
-                .last()
+                .next_back()
                 .unwrap()
                 .into_iter()
                 .sorted_by_key(|(address, _, _)| *address)
