a5b91ad2447486590603bec4b9aca93e8d964bd0
Matthias Seitz <matthias.seitz@outlook.de>
2025-01-11 18:08:01 +0100

    rm conflicts with

--- crates/node/core/src/args/engine.rs
+++ crates/node/core/src/args/engine.rs
@@ -4,25 +4,25 @@ use clap::Args;
 
 use crate::node_config::{DEFAULT_MEMORY_BLOCK_BUFFER_TARGET, DEFAULT_PERSISTENCE_THRESHOLD};
 
-/// Parameters for configuring the engine
+/// Parameters for configuring the engine driver.
 #[derive(Debug, Clone, Args, PartialEq, Eq)]
 #[command(next_help_heading = "Engine")]
 pub struct EngineArgs {
     /// Configure persistence threshold for engine experimental.
-    #[arg(long = "engine.persistence-threshold", conflicts_with = "legacy", default_value_t = DEFAULT_PERSISTENCE_THRESHOLD)]
+    #[arg(long = "engine.persistence-threshold", default_value_t = DEFAULT_PERSISTENCE_THRESHOLD)]
     pub persistence_threshold: u64,
 
     /// Configure the target number of blocks to keep in memory.
-    #[arg(long = "engine.memory-block-buffer-target", conflicts_with = "legacy", default_value_t = DEFAULT_MEMORY_BLOCK_BUFFER_TARGET)]
+    #[arg(long = "engine.memory-block-buffer-target", default_value_t = DEFAULT_MEMORY_BLOCK_BUFFER_TARGET)]
     pub memory_block_buffer_target: u64,
 
     /// Enable state root task
-    #[arg(long = "engine.state-root-task", conflicts_with = "legacy")]
+    #[arg(long = "engine.state-root-task")]
     pub state_root_task_enabled: bool,
 
     /// Enable comparing trie updates from the state root task to the trie updates from the regular
     /// state root calculation.
-    #[arg(long = "engine.state-root-task-compare-updates", conflicts_with = "legacy")]
+    #[arg(long = "engine.state-root-task-compare-updates")]
     pub state_root_task_compare_updates: bool,
 }
 
