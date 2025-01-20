5b4a1e4398498bb060efd5a12710f319d92773f2
Arsenii Kulikov <klkvrr@gmail.com>
2025-01-20 21:08:35 +0400

    chore: move receipts pruning to provider

--- crates/revm/src/batch.rs
+++ crates/revm/src/batch.rs
@@ -3,11 +3,11 @@
 use alloc::vec::Vec;
 
 use alloy_eips::eip7685::Requests;
-use alloy_primitives::{map::HashSet, Address, BlockNumber, Log};
-use reth_execution_errors::{BlockExecutionError, InternalBlockExecutionError};
+use alloy_primitives::{BlockNumber, Log};
+use reth_execution_errors::{BlockExecutionError};
 use reth_primitives::Receipts;
 use reth_primitives_traits::Receipt;
-use reth_prune_types::{PruneMode, PruneModes, PruneSegmentError, MINIMUM_PRUNING_DISTANCE};
+use reth_prune_types::{PruneModes};
 use revm::db::states::bundle_state::BundleRetention;
 
 /// Takes care of:
@@ -31,11 +31,6 @@ pub struct BlockBatchRecord<T = reth_primitives::Receipt> {
     /// A transaction may have zero or more requests, so the length of the inner vector is not
     /// guaranteed to be the same as the number of transactions.
     requests: Vec<Requests>,
-    /// Memoized address pruning filter.
-    ///
-    /// Empty implies that there is going to be addresses to include in the filter in a future
-    /// block. None means there isn't any kind of configuration.
-    pruning_address_filter: Option<(u64, HashSet<Address>)>,
     /// First block will be initialized to `None`
     /// and be set to the block number of first block executed.
     first_block: Option<BlockNumber>,
@@ -49,7 +44,6 @@ impl<T> Default for BlockBatchRecord<T> {
             prune_modes: Default::default(),
             receipts: Default::default(),
             requests: Default::default(),
-            pruning_address_filter: Default::default(),
             first_block: Default::default(),
             tip: Default::default(),
         }
@@ -59,8 +53,6 @@ impl<T> Default for BlockBatchRecord<T> {
 impl<T> BlockBatchRecord<T> {
     /// Create a new receipts recorder with the given pruning configuration.
     pub fn new(prune_modes: PruneModes) -> Self
-    where
-        T: Default,
     {
         Self { prune_modes, ..Default::default() }
     }
@@ -133,61 +125,9 @@ impl<T> BlockBatchRecord<T> {
     where
         T: Receipt<Log = Log>,
     {
-        let mut receipts = receipts.into_iter().map(Some).collect();
-        // Prune receipts if necessary.
-        self.prune_receipts(&mut receipts).map_err(InternalBlockExecutionError::from)?;
+        let receipts = receipts.into_iter().map(Some).collect();
         // Save receipts.
         self.receipts.push(receipts);
-        Ok(())
-    }
-
-    /// Prune receipts according to the pruning configuration.
-    fn prune_receipts(&mut self, receipts: &mut Vec<Option<T>>) -> Result<(), PruneSegmentError>
-    where
-        T: Receipt<Log = Log>,
-    {
-        let (Some(first_block), Some(tip)) = (self.first_block, self.tip) else { return Ok(()) };
-
-        let block_number = first_block + self.receipts.len() as u64;
-
-        // Block receipts should not be retained
-        if self.prune_modes.receipts == Some(PruneMode::Full) ||
-            // [`PruneSegment::Receipts`] takes priority over [`PruneSegment::ContractLogs`]
-            self.prune_modes.receipts.is_some_and(|mode| mode.should_prune(block_number, tip))
-        {
-            receipts.clear();
-            return Ok(())
-        }
-
-        // All receipts from the last 128 blocks are required for blockchain tree, even with
-        // [`PruneSegment::ContractLogs`].
-        let prunable_receipts =
-            PruneMode::Distance(MINIMUM_PRUNING_DISTANCE).should_prune(block_number, tip);
-        if !prunable_receipts {
-            return Ok(())
-        }
-
-        let contract_log_pruner = self.prune_modes.receipts_log_filter.group_by_block(tip, None)?;
-
-        if !contract_log_pruner.is_empty() {
-            let (prev_block, filter) =
-                self.pruning_address_filter.get_or_insert_with(|| (0, Default::default()));
-            for (_, addresses) in contract_log_pruner.range(*prev_block..=block_number) {
-                filter.extend(addresses.iter().copied());
-            }
-        }
-
-        if let Some((_, filter)) = &self.pruning_address_filter {
-            for receipt in receipts.iter_mut() {
-                // If there is an address_filter, it does not contain any of the
-                // contract addresses, then remove this receipt.
-                let inner_receipt = receipt.as_ref().expect("receipts have not been pruned");
-                if !inner_receipt.logs().iter().any(|log| filter.contains(&log.address)) {
-                    receipt.take();
-                }
-            }
-        }
-
         Ok(())
     }
 
