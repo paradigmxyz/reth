82461424a1fc41d67cb548f794948cf23ff2a72a
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-01-22 16:54:46 +0100

    Add no_std support for reth-engine-primitives

--- crates/engine/primitives/src/lib.rs
+++ crates/engine/primitives/src/lib.rs
@@ -7,6 +7,9 @@
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+extern crate alloc;
 
 mod error;
 
