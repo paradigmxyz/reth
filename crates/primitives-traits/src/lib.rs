9455a2f9cd214e6882cd0818c491d68b0aa0c83b
Matthias Seitz <matthias.seitz@outlook.de>
2025-01-27 17:48:01 +0100

    fix

--- crates/primitives-traits/src/lib.rs
+++ crates/primitives-traits/src/lib.rs
@@ -50,7 +50,7 @@
 //! [`SealedBlock::try_recover_unchecked`] and [`SealedBlock::try_recover_unchecked`]. The `_unchecked` suffix indicates that this function recovers the signer _without ensuring that the signature has a low `s` value_, in other words this rule introduced in [EIP-2](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-2.md) is ignored.
 //! Hence this function is necessary when dealing with pre EIP-2 transactions on the ethereum
 //! mainnet. Newer transactions must always be recovered with the regular `recover` functions, see
-//! also [recover_signer](crypto::secp256k1::recover_signer).
+//! also [`recover_signer`](crypto::secp256k1::recover_signer).
 
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
