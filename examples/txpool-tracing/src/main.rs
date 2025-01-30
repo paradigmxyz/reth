755907c9425a2a9e70d1a6caf60f6aede06b1960
Khanh Hoa <hoa@notional.ventures>
2025-01-30 14:05:09 +0700

    feat: remove transaction_to_call_request helper fn

--- examples/txpool-tracing/src/main.rs
+++ examples/txpool-tracing/src/main.rs
@@ -16,7 +16,7 @@ use clap::Parser;
 use futures_util::StreamExt;
 use reth::{
     builder::NodeHandle, chainspec::EthereumChainSpecParser, cli::Cli,
-    rpc::compat::transaction::transaction_to_call_request, transaction_pool::TransactionPool,
+    rpc::types::TransactionRequest, transaction_pool::TransactionPool,
 };
 use reth_node_ethereum::node::EthereumNode;
 
@@ -44,7 +44,8 @@ fn main() {
                     if let Some(recipient) = tx.to() {
                         if args.is_match(&recipient) {
                             // trace the transaction with `trace_call`
-                            let callrequest = transaction_to_call_request(tx.to_consensus());
+                            let callrequest =
+                                TransactionRequest::from_recovered_transaction(tx.to_consensus());
                             let tracerequest = TraceCallRequest::new(callrequest)
                                 .with_trace_type(TraceType::Trace);
                             if let Ok(trace_result) = traceapi.trace_call(tracerequest).await {
