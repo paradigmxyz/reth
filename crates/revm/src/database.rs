41b12dcd82038e9bbe397c5d8b8f700432e781ea
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-02-01 00:56:09 +0100

    Fix docs

--- crates/revm/src/database.rs
+++ crates/revm/src/database.rs
@@ -64,8 +64,7 @@ impl<T: StateProvider> EvmStateProvider for T {
 }
 
 /// A [Database] and [`DatabaseRef`] implementation that uses [`EvmStateProvider`] as the underlying
-/// data source. All [`StateProvider`](reth_storage_api::StateProvider) types also implement
-/// [`EvmStateProvider`].
+/// data source. All [`StateProvider`] types also implement [`EvmStateProvider`].
 #[derive(Debug, Clone)]
 pub struct StateProviderDatabase<DB>(pub DB);
 
