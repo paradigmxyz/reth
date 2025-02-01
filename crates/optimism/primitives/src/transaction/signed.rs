a12bd957dd8673864cdce0b7b011bd14ae35db60
Matthias Seitz <matthias.seitz@outlook.de>
2025-02-01 08:41:18 +0100

    add unreachable

--- crates/optimism/primitives/src/transaction/signed.rs
+++ crates/optimism/primitives/src/transaction/signed.rs
@@ -166,6 +166,7 @@ impl From<OpTxEnvelope> for OpTransactionSigned {
             OpTxEnvelope::Eip1559(tx) => tx.into(),
             OpTxEnvelope::Eip7702(tx) => tx.into(),
             OpTxEnvelope::Deposit(tx) => tx.into(),
+            _ => unreachable!(),
         }
     }
 }
