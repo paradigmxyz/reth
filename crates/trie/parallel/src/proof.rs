5c523ed6613ecb87a648ffd40b6b7d7296042513
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-02-04 14:45:25 +0000

    init storages with empty roots

--- crates/trie/parallel/src/proof.rs
+++ crates/trie/parallel/src/proof.rs
@@ -236,12 +236,12 @@ where
 
         // Initialize all storage multiproofs as empty.
         // Storage multiproofs for non empty tries will be overwritten if necessary.
-        let mut storages: B256HashMap<_> = targets
-            .storages
+        let mut storages = targets
+            .accounts
             .iter()
-            .filter(|(_, slots)| !slots.is_empty())
-            .map(|(key, _)| (*key, StorageMultiProof::empty()))
-            .collect();
+            .chain(targets.storages.keys())
+            .map(|key| (*key, StorageMultiProof::empty()))
+            .collect::<B256HashMap<_>>();
         let mut account_rlp = Vec::with_capacity(TRIE_ACCOUNT_RLP_MAX_SIZE);
         let mut account_node_iter = TrieNodeIter::new(
             walker,
