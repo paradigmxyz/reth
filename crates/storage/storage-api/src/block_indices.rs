b479b3439c7bfcad36c30332e9a7cd64d5edcc22
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-02-04 11:23:35 +0100

    feat(no_std): Add `no_std` support for `reth-storage-api` (#14187)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/storage/storage-api/src/block_indices.rs
+++ crates/storage/storage-api/src/block_indices.rs
@@ -1,7 +1,8 @@
+use alloc::vec::Vec;
 use alloy_primitives::BlockNumber;
+use core::ops::RangeInclusive;
 use reth_db_models::StoredBlockBodyIndices;
 use reth_storage_errors::provider::ProviderResult;
-use std::ops::RangeInclusive;
 
 ///  Client trait for fetching block body indices related data.
 #[auto_impl::auto_impl(&, Arc)]
