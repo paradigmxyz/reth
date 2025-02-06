6708c2dc46b2a2fd10d5816ebbbacac5acd3f478
greg <82421016+greged93@users.noreply.github.com>
2025-02-06 20:35:46 +0100

    fix: try_into_block (#153)
    Signed-off-by: Gregory Edison <gregory.edison1993@gmail.com>

--- crates/scroll/engine-primitives/src/payload/mod.rs
+++ crates/scroll/engine-primitives/src/payload/mod.rs
@@ -9,14 +9,17 @@ pub use built::ScrollBuiltPayload;
 use alloc::sync::Arc;
 use core::marker::PhantomData;
 
-use alloy_consensus::Block;
+use alloy_consensus::{proofs, EMPTY_OMMER_ROOT_HASH};
 use alloy_eips::eip2718::Decodable2718;
+use alloy_rlp::BufMut;
 use alloy_rpc_types_engine::{
     ExecutionPayload, ExecutionPayloadEnvelopeV2, ExecutionPayloadEnvelopeV3,
-    ExecutionPayloadEnvelopeV4, ExecutionPayloadSidecar, ExecutionPayloadV1, PayloadError,
+    ExecutionPayloadEnvelopeV4, ExecutionPayloadSidecar, ExecutionPayloadV1, ExecutionPayloadV2,
+    ExecutionPayloadV3, PayloadError,
 };
 use reth_engine_primitives::EngineTypes;
 use reth_payload_primitives::{BuiltPayload, PayloadTypes};
+use reth_primitives::{Block, BlockBody, Header};
 use reth_primitives_traits::{NodePrimitives, SealedBlock};
 use reth_scroll_chainspec::ScrollChainSpec;
 use reth_scroll_forks::ScrollHardfork;
@@ -72,19 +75,234 @@ impl PayloadTypes for ScrollPayloadTypes {
 
 /// Tries to create a new unsealed block from the given payload, sidecar and chain specification.
 /// Sets the base fee of the block to `None` before the Curie hardfork.
+/// Scroll implementation of the [`ExecutionPayload::try_into_block`], which will fail with
+/// [`PayloadError::ExtraData`] due to the Scroll blocks containing extra data for the Clique
+/// consensus.
 pub fn try_into_block<T: Decodable2718>(
-    payload: ExecutionPayload,
+    value: ExecutionPayload,
     sidecar: &ExecutionPayloadSidecar,
     chainspec: Arc<ScrollChainSpec>,
 ) -> Result<Block<T>, PayloadError> {
-    let mut block = payload.try_into_block_with_sidecar(sidecar)?;
+    let mut block = match value {
+        ExecutionPayload::V1(payload) => try_payload_v1_to_block(payload, chainspec)?,
+        ExecutionPayload::V2(payload) => try_payload_v2_to_block(payload, chainspec)?,
+        ExecutionPayload::V3(payload) => try_payload_v3_to_block(payload, chainspec)?,
+    };
 
-    let basefee = chainspec
-        .is_fork_active_at_block(ScrollHardfork::Curie, block.number)
-        .then(|| block.base_fee_per_gas)
-        .flatten();
-
-    block.header.base_fee_per_gas = basefee;
+    block.header.parent_beacon_block_root = sidecar.parent_beacon_block_root();
+    block.header.requests_hash = sidecar.requests_hash();
 
     Ok(block)
+}
+
+/// Tries to convert an [`ExecutionPayloadV1`] to [`Block`].
+fn try_payload_v1_to_block<T: Decodable2718>(
+    payload: ExecutionPayloadV1,
+    chainspec: Arc<ScrollChainSpec>,
+) -> Result<Block<T>, PayloadError> {
+    // WARNING: It’s allowed for a base fee in EIP1559 to increase unbounded. We assume that
+    // it will fit in an u64. This is not always necessarily true, although it is extremely
+    // unlikely not to be the case, a u64 maximum would have 2^64 which equates to 18 ETH per
+    // gas.
+    let basefee = chainspec
+        .is_fork_active_at_block(ScrollHardfork::Curie, payload.block_number)
+        .then_some(payload.base_fee_per_gas)
+        .map(|b| b.try_into())
+        .transpose()
+        .map_err(|_| PayloadError::BaseFee(payload.base_fee_per_gas))?;
+
+    let transactions = payload
+        .transactions
+        .iter()
+        .map(|tx| {
+            let mut buf = tx.as_ref();
+
+            let tx = T::decode_2718(&mut buf).map_err(alloy_rlp::Error::from)?;
+
+            if !buf.is_empty() {
+                return Err(alloy_rlp::Error::UnexpectedLength);
+            }
+
+            Ok(tx)
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+
+    // Reuse the encoded bytes for root calculation
+    let transactions_root =
+        proofs::ordered_trie_root_with_encoder(&payload.transactions, |item, buf| {
+            buf.put_slice(item)
+        });
+
+    let header = Header {
+        parent_hash: payload.parent_hash,
+        beneficiary: payload.fee_recipient,
+        state_root: payload.state_root,
+        transactions_root,
+        receipts_root: payload.receipts_root,
+        withdrawals_root: None,
+        logs_bloom: payload.logs_bloom,
+        number: payload.block_number,
+        gas_limit: payload.gas_limit,
+        gas_used: payload.gas_used,
+        timestamp: payload.timestamp,
+        mix_hash: payload.prev_randao,
+        base_fee_per_gas: basefee,
+        blob_gas_used: None,
+        excess_blob_gas: None,
+        parent_beacon_block_root: None,
+        requests_hash: None,
+        extra_data: payload.extra_data,
+        // Defaults
+        ommers_hash: EMPTY_OMMER_ROOT_HASH,
+        difficulty: Default::default(),
+        nonce: Default::default(),
+    };
+
+    Ok(Block { header, body: BlockBody { transactions, ..Default::default() } })
+}
+
+/// Tries to convert an [`ExecutionPayloadV2`] to [`Block`].
+fn try_payload_v2_to_block<T: Decodable2718>(
+    payload: ExecutionPayloadV2,
+    chainspec: Arc<ScrollChainSpec>,
+) -> Result<Block<T>, PayloadError> {
+    // this performs the same conversion as the underlying V1 payload, but calculates the
+    // withdrawals root and adds withdrawals
+    let mut base_sealed_block = try_payload_v1_to_block(payload.payload_inner, chainspec)?;
+    let withdrawals_root = proofs::calculate_withdrawals_root(&payload.withdrawals);
+    base_sealed_block.body.withdrawals = Some(payload.withdrawals.into());
+    base_sealed_block.header.withdrawals_root = Some(withdrawals_root);
+    Ok(base_sealed_block)
+}
+
+/// Tries to convert an [`ExecutionPayloadV3`] to [`Block`].
+fn try_payload_v3_to_block<T: Decodable2718>(
+    payload: ExecutionPayloadV3,
+    chainspec: Arc<ScrollChainSpec>,
+) -> Result<Block<T>, PayloadError> {
+    // this performs the same conversion as the underlying V2 payload, but inserts the blob gas
+    // used and excess blob gas
+    let mut base_block = try_payload_v2_to_block(payload.payload_inner, chainspec)?;
+
+    base_block.header.blob_gas_used = Some(payload.blob_gas_used);
+    base_block.header.excess_blob_gas = Some(payload.excess_blob_gas);
+
+    Ok(base_block)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloy_primitives::{Address, Bloom, B256, U256};
+    use alloy_rpc_types_engine::ExecutionPayloadV1;
+    use arbitrary::{Arbitrary, Unstructured};
+    use rand::Rng;
+    use reth_scroll_chainspec::SCROLL_MAINNET;
+    use reth_scroll_primitives::ScrollTransactionSigned;
+
+    #[test]
+    fn test_can_convert_execution_v1_payload_into_block() -> eyre::Result<()> {
+        let mut bytes = [0u8; 1024];
+        rand::thread_rng().fill(bytes.as_mut_slice());
+        let mut u = Unstructured::new(&bytes);
+
+        let mut extra_data = [0u8; 64];
+        rand::thread_rng().fill(extra_data.as_mut_slice());
+
+        let execution_payload = ExecutionPayload::V1(ExecutionPayloadV1 {
+            parent_hash: B256::random(),
+            fee_recipient: Address::random(),
+            state_root: B256::random(),
+            receipts_root: B256::random(),
+            logs_bloom: Bloom::random(),
+            prev_randao: B256::random(),
+            block_number: u64::arbitrary(&mut u)?,
+            gas_limit: u64::arbitrary(&mut u)?,
+            gas_used: u64::arbitrary(&mut u)?,
+            timestamp: u64::arbitrary(&mut u)?,
+            extra_data: extra_data.into(),
+            base_fee_per_gas: U256::from(u64::arbitrary(&mut u)?),
+            block_hash: B256::random(),
+            transactions: vec![],
+        });
+
+        let _: Block<ScrollTransactionSigned> =
+            try_into_block(execution_payload, &Default::default(), SCROLL_MAINNET.clone())?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_can_convert_execution_v2_payload_into_block() -> eyre::Result<()> {
+        let mut bytes = [0u8; 1024];
+        rand::thread_rng().fill(bytes.as_mut_slice());
+        let mut u = Unstructured::new(&bytes);
+
+        let mut extra_data = [0u8; 64];
+        rand::thread_rng().fill(extra_data.as_mut_slice());
+
+        let execution_payload = ExecutionPayload::V2(ExecutionPayloadV2 {
+            payload_inner: ExecutionPayloadV1 {
+                parent_hash: B256::random(),
+                fee_recipient: Address::random(),
+                state_root: B256::random(),
+                receipts_root: B256::random(),
+                logs_bloom: Bloom::random(),
+                prev_randao: B256::random(),
+                block_number: u64::arbitrary(&mut u)?,
+                gas_limit: u64::arbitrary(&mut u)?,
+                gas_used: u64::arbitrary(&mut u)?,
+                timestamp: u64::arbitrary(&mut u)?,
+                extra_data: extra_data.into(),
+                base_fee_per_gas: U256::from(u64::arbitrary(&mut u)?),
+                block_hash: B256::random(),
+                transactions: vec![],
+            },
+            withdrawals: vec![],
+        });
+
+        let _: Block<ScrollTransactionSigned> =
+            try_into_block(execution_payload, &Default::default(), SCROLL_MAINNET.clone())?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_can_convert_execution_v3_payload_into_block() -> eyre::Result<()> {
+        let mut bytes = [0u8; 1024];
+        rand::thread_rng().fill(bytes.as_mut_slice());
+        let mut u = Unstructured::new(&bytes);
+
+        let mut extra_data = [0u8; 64];
+        rand::thread_rng().fill(extra_data.as_mut_slice());
+
+        let execution_payload = ExecutionPayload::V3(ExecutionPayloadV3 {
+            payload_inner: ExecutionPayloadV2 {
+                payload_inner: ExecutionPayloadV1 {
+                    parent_hash: B256::random(),
+                    fee_recipient: Address::random(),
+                    state_root: B256::random(),
+                    receipts_root: B256::random(),
+                    logs_bloom: Bloom::random(),
+                    prev_randao: B256::random(),
+                    block_number: u64::arbitrary(&mut u)?,
+                    gas_limit: u64::arbitrary(&mut u)?,
+                    gas_used: u64::arbitrary(&mut u)?,
+                    timestamp: u64::arbitrary(&mut u)?,
+                    extra_data: extra_data.into(),
+                    base_fee_per_gas: U256::from(u64::arbitrary(&mut u)?),
+                    block_hash: B256::random(),
+                    transactions: vec![],
+                },
+                withdrawals: vec![],
+            },
+            blob_gas_used: 0,
+            excess_blob_gas: 0,
+        });
+
+        let _: Block<ScrollTransactionSigned> =
+            try_into_block(execution_payload, &Default::default(), SCROLL_MAINNET.clone())?;
+
+        Ok(())
+    }
 }
