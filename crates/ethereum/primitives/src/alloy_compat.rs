d754743352af84b5360175145d1e634decf9c548
PoulavBhowmick03 <bpoulav@gmail.com>
2025-02-09 22:52:15 +0530

    Added from<alloy_rpc_types_eth::Transaction> for TransactionSigned

--- crates/ethereum/primitives/src/alloy_compat.rs
+++ crates/ethereum/primitives/src/alloy_compat.rs
@@ -4,6 +4,7 @@ use crate::{Transaction, TransactionSigned};
 use alloc::string::ToString;
 use alloy_consensus::TxEnvelope;
 use alloy_network::{AnyRpcTransaction, AnyTxEnvelope};
+use alloy_rpc_types_eth::Transaction as AlloyRpcTransaction;
 use alloy_serde::WithOtherFields;
 
 impl TryFrom<AnyRpcTransaction> for TransactionSigned {
@@ -15,6 +16,44 @@ impl TryFrom<AnyRpcTransaction> for TransactionSigned {
         let WithOtherFields { inner: tx, other: _ } = tx;
 
         let (transaction, signature, hash) = match tx.inner {
+            AnyTxEnvelope::Ethereum(TxEnvelope::Legacy(tx)) => {
+                let (tx, signature, hash) = tx.into_parts();
+                (Transaction::Legacy(tx), signature, hash)
+            }
+            AnyTxEnvelope::Ethereum(TxEnvelope::Eip2930(tx)) => {
+                let (tx, signature, hash) = tx.into_parts();
+                (Transaction::Eip2930(tx), signature, hash)
+            }
+            AnyTxEnvelope::Ethereum(TxEnvelope::Eip1559(tx)) => {
+                let (tx, signature, hash) = tx.into_parts();
+                (Transaction::Eip1559(tx), signature, hash)
+            }
+            AnyTxEnvelope::Ethereum(TxEnvelope::Eip4844(tx)) => {
+                let (tx, signature, hash) = tx.into_parts();
+                (Transaction::Eip4844(tx.into()), signature, hash)
+            }
+            AnyTxEnvelope::Ethereum(TxEnvelope::Eip7702(tx)) => {
+                let (tx, signature, hash) = tx.into_parts();
+                (Transaction::Eip7702(tx), signature, hash)
+            }
+            _ => return Err(ConversionError::Custom("unknown transaction type".to_string())),
+        };
+
+        Ok(Self::new(transaction, signature, hash))
+    }
+}
+
+impl<T> TryFrom<AlloyRpcTransaction<T>> for TransactionSigned
+where
+    T: Into<AnyTxEnvelope>,
+{
+    type Error = alloy_rpc_types::ConversionError;
+
+    fn try_from(tx: AlloyRpcTransaction<T>) -> Result<Self, Self::Error> {
+        use alloy_rpc_types::ConversionError;
+
+        let envelope = tx.inner.into();
+        let (transaction, signature, hash) = match envelope {
             AnyTxEnvelope::Ethereum(TxEnvelope::Legacy(tx)) => {
                 let (tx, signature, hash) = tx.into_parts();
                 (Transaction::Legacy(tx), signature, hash)
