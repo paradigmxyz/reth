eeefb61a3662b9f4c42b12dd64eae8d20764393f
Arsenii Kulikov <klkvrr@gmail.com>
2025-02-10 20:08:08 +0400

    wip

--- examples/custom-node/src/lib.rs
+++ examples/custom-node/src/lib.rs
@@ -10,17 +10,30 @@
 #![cfg(feature = "optimism")]
 
 use chainspec::CustomChainSpec;
-use engine::CustomEngineTypes;
+use engine::{CustomEngineTypes, CustomPayloadBuilder};
 use primitives::CustomNodePrimitives;
-use reth_node_api::{FullNodeTypes, NodeTypes, NodeTypesWithEngine};
-use reth_node_builder::{components::ComponentsBuilder, Node, NodeAdapter, NodeComponentsBuilder};
-use reth_optimism_node::{engine::OpPayloadTypes, node::{OpAddOns, OpConsensusBuilder, OpExecutorBuilder, OpNetworkBuilder, OpPayloadBuilder, OpPoolBuilder}, OpEngineTypes, OpNode};
+use reth_node_api::{FullNodeTypes, NodeTypes, NodeTypesWithEngine, PrimitivesTy, TxTy};
+use reth_node_builder::{
+    components::{ComponentsBuilder, PayloadServiceBuilder},
+    Node, NodeAdapter, NodeComponentsBuilder,
+};
+use reth_optimism_node::{
+    engine::OpPayloadTypes,
+    node::{
+        OpAddOns, OpConsensusBuilder, OpExecutorBuilder, OpNetworkBuilder, OpPayloadBuilder,
+        OpPoolBuilder,
+    },
+    OpEngineTypes, OpEvmConfig, OpNode,
+};
 use reth_storage_api::EthStorage;
+use reth_transaction_pool::{PoolTransaction, TransactionPool};
 use reth_trie_db::MerklePatriciaTrie;
+use txpool::CustomTxPool;
 
 pub mod chainspec;
 pub mod engine;
 pub mod primitives;
+pub mod txpool;
 
 #[derive(Debug, Clone)]
 pub struct CustomNode(OpNode);
@@ -47,7 +60,8 @@ impl NodeTypesWithEngine for CustomNode {
 //     >;
 
 //     type AddOns =
-//         OpAddOns<NodeAdapter<N, <Self::ComponentsBuilder as NodeComponentsBuilder<N>>::Components>>;
+//         OpAddOns<NodeAdapter<N, <Self::ComponentsBuilder as
+// NodeComponentsBuilder<N>>::Components>>;
 
 //     fn components_builder(&self) -> Self::ComponentsBuilder {
 //         self.0.components()
@@ -57,3 +71,21 @@ impl NodeTypesWithEngine for CustomNode {
 //         self.0.add_ons()
 //     }
 // }
+
+/// Custom payload builder.
+struct CustomPayloadServiceBuilder(reth_optimism_node::node::OpPayloadBuilder);
+
+impl<Node> PayloadServiceBuilder<Node, CustomTxPool<Node::Provider>> for CustomPayloadServiceBuilder
+where
+    Node: FullNodeTypes<Types = CustomNode>,
+{
+    type PayloadBuilder = CustomPayloadBuilder<Node::Provider>;
+
+    async fn build_payload_builder(
+        &self,
+        ctx: &reth_node_builder::BuilderContext<Node>,
+        pool: CustomTxPool<Node::Provider>,
+    ) -> eyre::Result<Self::PayloadBuilder> {
+        Ok(CustomPayloadBuilder::new(pool, ctx.provider().clone(), ctx.chain_spec().clone()))
+    }
+}
