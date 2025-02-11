c4cb3f048dc6258218febb4df9fa7b40333c6b75
Federico Gimenez <fgimenez@users.noreply.github.com>
2025-02-11 13:25:48 +0100

    feat(engine): use Hash Builder as a fallback to the Sparse Trie on newPayload (#14387)

--- crates/engine/tree/src/tree/mod.rs
+++ crates/engine/tree/src/tree/mod.rs
@@ -2899,9 +2899,12 @@ where
                         compare_trie_updates(
                             in_memory_trie_cursor,
                             task_trie_updates.clone(),
-                            regular_updates,
+                            regular_updates.clone(),
                         )
                         .map_err(ProviderError::from)?;
+                        if task_state_root != sealed_block.header().state_root() {
+                            return Ok((regular_root, regular_updates, time_from_last_update));
+                        }
                     } else {
                         debug!(target: "engine::tree", "Regular state root does not match block state root");
                     }
