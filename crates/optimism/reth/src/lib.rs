bc735f9526740c71d435605fec811e3587610b5b
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-02-12 18:48:44 +0100

    Fix docs

--- crates/optimism/reth/src/lib.rs
+++ crates/optimism/reth/src/lib.rs
@@ -27,6 +27,7 @@ pub mod primitives {
 pub mod consensus {
     #[doc(inline)]
     pub use reth_consensus::*;
+    /// Consensus rule checks.
     pub mod validation {
         #[doc(inline)]
         pub use reth_consensus_common::validation::*;
