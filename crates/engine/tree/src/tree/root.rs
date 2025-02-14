b0451e1655f286b4db7b5731ca80bd73160c2ad0
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-02-14 12:46:53 +0000

    fix logs

--- crates/engine/tree/src/tree/root.rs
+++ crates/engine/tree/src/tree/root.rs
@@ -286,15 +286,17 @@ fn evm_state_to_hashed_post_state(update: EvmState) -> HashedPostState {
     for (address, account) in update {
         if account.is_touched() {
             let hashed_address = keccak256(address);
-            trace!(target: "engine::root", ?address, ?hashed_address, "Adding account to state update");
 
             let destroyed = account.is_selfdestructed();
 
             // If the account is self-destructed in the same transaction as it was created, we can
             // skip it, as it will not affect the trie.
             if account.is_created() && destroyed {
+                trace!(target: "engine::root", ?address, ?hashed_address, "Skipping account in state update, as it is self-destructed");
                 continue
             }
+
+            trace!(target: "engine::root", ?address, ?hashed_address, "Adding account to state update");
 
             let info = if destroyed { None } else { Some(account.info.into()) };
             hashed_state.accounts.insert(hashed_address, info);
