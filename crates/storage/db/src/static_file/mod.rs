9a9be1e203b1c9dfe83487f1c00b9dee89b215f8
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-02-17 18:52:12 +0100

    alloc

--- crates/storage/db/src/static_file/mod.rs
+++ crates/storage/db/src/static_file/mod.rs
@@ -34,9 +34,7 @@ pub fn iter_static_files(path: &Path) -> Result<SortedStaticFiles, NippyJarError
     let mut static_files = SortedStaticFiles::default();
     let entries = reth_fs_util::read_dir(path)
         .map_err(|err| NippyJarError::Custom(err.to_string()))?
-        .filter_map(Result::ok)
-        .collect::<Vec<_>>();
-
+        .filter_map(Result::ok);
     for entry in entries {
         if entry.metadata().is_ok_and(|metadata| metadata.is_file()) {
             if let Some((segment, _)) =
@@ -65,7 +63,7 @@ pub fn iter_static_files(path: &Path) -> Result<SortedStaticFiles, NippyJarError
 
     for range_list in static_files.values_mut() {
         // Sort by block end range.
-        range_list.sort_by(|a, b| a.0.end().cmp(&b.0.end()));
+        range_list.sort_by_key(|(r, _)| r.end());
     }
 
     Ok(static_files)
