1dabed89bc7fa2bd43a24d1b6924b42dd374bf2a
kustrun <domen.balantic1@gmail.com>
2025-02-18 14:31:53 +0100

    feat(node-type): Make NodeType & impls Clone.

--- crates/storage/provider/src/test_utils/mock.rs
+++ crates/storage/provider/src/test_utils/mock.rs
@@ -182,7 +182,7 @@ impl ExtendedAccount {
 }
 
 /// Mock node.
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct MockNode;
 
 impl NodeTypes for MockNode {
@@ -341,7 +341,7 @@ impl<T: SignedTransaction, ChainSpec: EthChainSpec> TransactionsProvider
                         excess_blob_gas: block.header.excess_blob_gas,
                         timestamp: block.header.timestamp,
                     };
-                    return Ok(Some((tx.clone(), meta)))
+                    return Ok(Some((tx.clone(), meta)));
                 }
             }
         }
@@ -353,7 +353,7 @@ impl<T: SignedTransaction, ChainSpec: EthChainSpec> TransactionsProvider
         let mut current_tx_number: TxNumber = 0;
         for block in lock.values() {
             if current_tx_number + (block.body.transactions.len() as TxNumber) > id {
-                return Ok(Some(block.header.number))
+                return Ok(Some(block.header.number));
             }
             current_tx_number += block.body.transactions.len() as TxNumber;
         }
