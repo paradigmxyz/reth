6d75f0461d4f40563bf837c4204add2104e68229
Joseph Zhao <zhaozihan542259545@gmail.com>
2025-02-25 22:30:37 +0800

    fix duplicate and optimize

--- crates/optimism/storage/src/chain.rs
+++ crates/optimism/storage/src/chain.rs
@@ -2,54 +2,13 @@ use alloy_consensus::Header;
 use alloy_primitives::BlockNumber;
 use core::marker::PhantomData;
 use reth_chainspec::{ChainSpecProvider, EthChainSpec, EthereumHardforks};
-use reth_db_api::{cursor::DbCursorRO, tables, transaction::DbTx};
 use reth_optimism_forks::OpHardforks;
 use reth_optimism_primitives::OpTransactionSigned;
-use reth_primitives_traits::{Block, BlockBody, FullBlockHeader, SignedTransaction};
-use reth_storage_api::{errors::ProviderResult, DBProvider, StorageLocation};
-
-/// Trait that implements how block bodies are written to the storage.
-#[auto_impl::auto_impl(&, Arc)]
-pub(crate) trait BlockBodyWriter<Provider, Body: BlockBody> {
-    /// Writes a set of block bodies to the storage.
-    fn write_block_bodies(
-        &self,
-        provider: &Provider,
-        bodies: Vec<(BlockNumber, Option<Body>)>,
-        write_to: StorageLocation,
-    );
-
-    /// Removes all block bodies above the given block number from the database.
-    fn remove_block_bodies_above(
-        &self,
-        _provider: &Provider,
-        _block: BlockNumber,
-        _remove_from: StorageLocation,
-    );
-}
-
-/// Input for reading a block body. Contains a header of block being read and a list of pre-fetched
-/// transactions.
-pub(crate) type ReadBodyInput<'a, B> =
-    (&'a <B as Block>::Header, Vec<<<B as Block>::Body as BlockBody>::Transaction>);
-
-/// Trait that implements how block bodies are read from the storage.
-///
-/// Note: Within the current abstraction, transactions persistence is handled separately, thus this
-/// trait is provided with transactions read beforehand and is expected to construct the block body
-/// from those transactions and additional data read from elsewhere.
-#[auto_impl::auto_impl(&, Arc)]
-pub(crate) trait BlockBodyReader<Provider> {
-    /// The block type.
-    type Block: Block;
-
-    /// Receives a list of block headers along with block transactions and returns the block bodies.
-    fn read_block_bodies(
-        &self,
-        provider: &Provider,
-        inputs: Vec<ReadBodyInput<'_, Self::Block>>,
-    ) -> ProviderResult<Vec<<Self::Block as Block>::Body>>;
-}
+use reth_primitives_traits::{Block, FullBlockHeader, SignedTransaction};
+use reth_storage_api::{
+    errors::{ProviderError, ProviderResult},
+    BlockBodyReader, BlockBodyWriter, DBProvider, ReadBodyInput, StorageLocation,
+};
 
 /// Optimism storage implementation.
 #[derive(Debug, Clone, Copy)]
@@ -66,8 +25,9 @@ where
         _provider: &Provider,
         _bodies: Vec<(u64, Option<alloy_consensus::BlockBody<T, H>>)>,
         _write_to: StorageLocation,
-    ) {
+    ) -> ProviderResult<()> {
         // noop
+        Ok(())
     }
 
     fn remove_block_bodies_above(
@@ -75,8 +35,9 @@ where
         _provider: &Provider,
         _block: BlockNumber,
         _remove_from: StorageLocation,
-    ) {
+    ) -> ProviderResult<()> {
         // noop
+        Ok(())
     }
 }
 
@@ -95,30 +56,16 @@ where
     ) -> ProviderResult<Vec<<Self::Block as Block>::Body>> {
         let chain_spec = provider.chain_spec();
 
-        let mut withdrawals_cursor = provider.tx_ref().cursor_read::<tables::BlockWithdrawals>()?;
-
-        let mut bodies = Vec::with_capacity(inputs.len());
-
-        for (header, transactions) in inputs {
+        for (header, _) in inputs {
             // If we are past shanghai, then all blocks should have a withdrawal list,
             // even if empty
-            let withdrawals = if chain_spec.is_shanghai_active_at_timestamp(header.timestamp()) {
-                withdrawals_cursor
-                    .seek_exact(header.number())?
-                    .map(|(_, w)| w.withdrawals)
-                    .unwrap_or_default()
-                    .into()
+            if chain_spec.is_shanghai_active_at_timestamp(header.timestamp()) {
+                continue;
             } else {
-                None
+                return Err(ProviderError::InvalidStorageOutput);
             };
-
-            bodies.push(alloy_consensus::BlockBody {
-                transactions,
-                withdrawals,
-                ..Default::default()
-            });
         }
 
-        Ok(bodies)
+        Ok(vec![])
     }
 }
