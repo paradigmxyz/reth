bffdda43129c207deeccf6ec2f0f30b884da5e45
Luca Provini <lucaprovini1989@gmail.com>
2025-02-28 14:17:41 +0100

    Stateful overridable handshake (#14567)
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- examples/bsc-p2p/src/main.rs
+++ examples/bsc-p2p/src/main.rs
@@ -10,34 +10,33 @@
 //!
 //! Credits to: <https://blog.merkle.io/blog/fastest-transaction-network-eth-polygon-bsc>
 
-#![warn(unused_crate_dependencies)]
-
-use chainspec::{boot_nodes, bsc_chain_spec};
+use chainspec::{boot_nodes, bsc_chain_spec, head};
+use handshake::BscHandshake;
 use reth_discv4::Discv4ConfigBuilder;
-use reth_network::{NetworkConfig, NetworkEvent, NetworkEventListenerProvider, NetworkManager};
-use reth_network_api::{
-    events::{PeerEvent, SessionInfo},
-    PeersInfo,
+use reth_network::{
+    EthNetworkPrimitives, NetworkConfig, NetworkEvent, NetworkEventListenerProvider,
+    NetworkManager, PeersInfo,
 };
-use reth_primitives::{ForkHash, ForkId};
+use reth_network_api::events::{PeerEvent, SessionInfo};
+use reth_provider::noop::NoopProvider;
 use reth_tracing::{
-    tracing::info, tracing_subscriber::filter::LevelFilter, LayerInfo, LogFormat, RethTracer,
-    Tracer,
+    tracing_subscriber::filter::LevelFilter, LayerInfo, LogFormat, RethTracer, Tracer,
 };
 use secp256k1::{rand, SecretKey};
 use std::{
     net::{Ipv4Addr, SocketAddr},
+    sync::Arc,
     time::Duration,
 };
 use tokio_stream::StreamExt;
+use tracing::info;
 
-pub mod chainspec;
+mod chainspec;
+mod handshake;
+mod upgrade_status;
 
 #[tokio::main]
 async fn main() {
-    // The ECDSA private key used to create our enode identifier.
-    let secret_key = SecretKey::new(&mut rand::thread_rng());
-
     let _ = RethTracer::new()
         .with_stdout(LayerInfo::new(
             LogFormat::Terminal,
@@ -47,39 +46,32 @@ async fn main() {
         ))
         .init();
 
-    // The local address we want to bind to
     let local_addr = SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), 30303);
 
-    // The network configuration
-    let mut net_cfg = NetworkConfig::builder(secret_key)
-        .listener_addr(local_addr)
-        // we need to explicitly set the network mode to PoW to allow block propagation, see docs
+    let secret_key = SecretKey::new(&mut rand::thread_rng());
+
+    let net_cfg = NetworkConfig::builder(secret_key)
+        .boot_nodes(boot_nodes())
+        .set_head(head())
         .with_pow()
-        .build_with_noop_provider(bsc_chain_spec())
-        .set_discovery_v4(
-            Discv4ConfigBuilder::default()
-                .add_boot_nodes(boot_nodes())
-                // Set Discv4 lookup interval to 1 second
-                .lookup_interval(Duration::from_secs(1))
-                .build(),
-        );
+        .listener_addr(local_addr)
+        .eth_rlpx_handshake(Arc::new(BscHandshake::default()))
+        .build(NoopProvider::eth(bsc_chain_spec()));
 
-    // latest BSC forkId, we need to override this to allow connections from BSC nodes
-    let fork_id = ForkId { hash: ForkHash([0x07, 0xb5, 0x43, 0x28]), next: 0 };
-    net_cfg.fork_filter.set_current_fork_id(fork_id);
-    let net_manager = NetworkManager::eth(net_cfg).await.unwrap();
+    let net_cfg = net_cfg.set_discovery_v4(
+        Discv4ConfigBuilder::default()
+            .add_boot_nodes(boot_nodes())
+            .lookup_interval(Duration::from_millis(500))
+            .build(),
+    );
+    let net_manager = NetworkManager::<EthNetworkPrimitives>::new(net_cfg).await.unwrap();
 
-    // The network handle is our entrypoint into the network.
     let net_handle = net_manager.handle().clone();
     let mut events = net_handle.event_listener();
 
-    // NetworkManager is a long running task, let's spawn it
     tokio::spawn(net_manager);
-    info!("Looking for BSC peers...");
 
     while let Some(evt) = events.next().await {
-        // For the sake of the example we only print the session established event
-        // with the chain specific details
         match evt {
             NetworkEvent::ActivePeerSession { info, .. } => {
                 let SessionInfo { status, client_version, peer_id, .. } = info;
@@ -88,9 +80,7 @@ async fn main() {
             NetworkEvent::Peer(PeerEvent::SessionClosed { peer_id, reason }) => {
                 info!(peers=%net_handle.num_connected_peers() , %peer_id, ?reason, "Session closed.");
             }
-
             _ => {}
         }
     }
-    // We will be disconnected from peers since we are not able to answer to network requests
 }
