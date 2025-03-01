47554172b330fdd6a09244edfbca96558f599fff
Matthias Seitz <matthias.seitz@outlook.de>
2025-03-01 06:59:32 +0100

    deps

--- crates/optimism/storage/src/lib.rs
+++ crates/optimism/storage/src/lib.rs
@@ -21,7 +21,7 @@ mod tests {
         CompactClientVersion, CompactU256, CompactU64, StoredBlockBodyIndices,
         StoredBlockWithdrawals,
     };
-    use reth_primitives::Account;
+    use reth_primitives_traits::Account;
     use reth_prune_types::{PruneCheckpoint, PruneMode, PruneSegment};
     use reth_stages_types::{
         AccountHashingCheckpoint, CheckpointBlockRange, EntitiesCheckpoint, ExecutionCheckpoint,
