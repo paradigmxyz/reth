5f657e83f58289daaabbd54162858aa47d277035
Matthias Seitz <matthias.seitz@outlook.de>
2025-03-03 09:54:58 +0100

    chore: make clippy happy

--- crates/primitives-traits/src/block/recovered.rs
+++ crates/primitives-traits/src/block/recovered.rs
@@ -530,7 +530,7 @@ pub(super) mod serde_bincode_compat {
         serde_bincode_compat::{self, SerdeBincodeCompat},
         Block,
     };
-    use alloc::{borrow::Cow, vec::Vec};
+    use alloc::borrow::Cow;
     use alloy_primitives::Address;
     use serde::{Deserialize, Deserializer, Serialize, Serializer};
     use serde_with::{DeserializeAs, SerializeAs};
@@ -563,7 +563,7 @@ pub(super) mod serde_bincode_compat {
             bound = "serde_bincode_compat::SealedBlock<'a, T>: Serialize + serde::de::DeserializeOwned"
         )]
         block: serde_bincode_compat::SealedBlock<'a, T>,
-        senders: Cow<'a, Vec<Address>>,
+        senders: Cow<'a, [Address]>,
     }
 
     impl<'a, T: Block<Header: SerdeBincodeCompat, Body: SerdeBincodeCompat> + 'static>
