9da8f07d556b8971013f58dc26e53031bae26154
Luca Provini <luca.provini@usemerkle.com>
2025-03-10 17:27:10 +0100

    tokio spawn to futures unordered

--- examples/bsc-p2p/src/block_import/service.rs
+++ examples/bsc-p2p/src/block_import/service.rs
@@ -1,4 +1,5 @@
 use alloy_rpc_types::engine::PayloadStatusEnum;
+use futures::{stream::FuturesUnordered, StreamExt};
 use reth_engine_primitives::{BeaconConsensusEngineHandle, EngineTypes};
 use reth_network::{
     import::{BlockImportError, BlockImportOutcome, BlockValidation},
@@ -11,7 +12,7 @@ use reth_primitives_traits::Block;
 use std::{
     future::Future,
     pin::Pin,
-    task::{ready, Context, Poll},
+    task::{Context, Poll},
 };
 use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
 use tracing::error;
@@ -61,6 +62,8 @@ impl<T: EngineTypes> ImportHandle<T> {
     }
 }
 
+type ImportFuture = Pin<Box<dyn Future<Output = ()> + Send>>;
+
 pub(crate) struct ImportService<T: EngineTypes> {
     /// The handle to communicate with the engine service
     engine: BeaconConsensusEngineHandle<T>,
@@ -68,13 +71,52 @@ pub(crate) struct ImportService<T: EngineTypes> {
     from_network: UnboundedReceiver<IncomingBlock<T>>,
     /// Send the outcome of the import to the network
     to_network: UnboundedSender<Outcome<T>>,
+    /// Pending block imports.
+    pending_imports: FuturesUnordered<ImportFuture>,
 }
 
 impl<T: EngineTypes> ImportService<T> {
     /// Create a new block import service
     pub(crate) fn new(engine: BeaconConsensusEngineHandle<T>) -> (Self, ImportHandle<T>) {
         let (handle, from_network, to_network) = ImportHandle::new();
-        (Self { engine, from_network, to_network }, handle)
+        (
+            Self { engine, from_network, to_network, pending_imports: FuturesUnordered::new() },
+            handle,
+        )
+    }
+
+    /// Add a new block import task to the pending imports
+    fn import(&mut self, block: BlockMsg<T>, peer_id: PeerId) {
+        let engine = self.engine.clone();
+        let to_network = self.to_network.clone();
+
+        let fut = async move {
+            let sealed_block = block.block.block.clone().seal();
+            let payload = T::block_to_payload(sealed_block);
+            let payload_result = engine.new_payload(payload).await;
+            let outcome = match payload_result {
+                Ok(payload_status) => match payload_status.status {
+                    PayloadStatusEnum::Valid => Outcome::<T> {
+                        peer: peer_id,
+                        result: Ok(BlockValidation::ValidBlock { block }),
+                    },
+                    PayloadStatusEnum::Invalid { validation_error } => Outcome::<T> {
+                        peer: peer_id,
+                        result: Err(BlockImportError::InvalidPayload(validation_error)),
+                    },
+                    _ => return,
+                },
+                Err(err) => {
+                    Outcome::<T> { peer: peer_id, result: Err(BlockImportError::Engine(err)) }
+                }
+            };
+
+            // TODO: FCU handling and send to engine
+
+            let _ = to_network.send(outcome);
+        };
+
+        self.pending_imports.push(Box::pin(fut));
     }
 }
 
@@ -82,44 +124,20 @@ impl<T: EngineTypes> Future for ImportService<T> {
     type Output = Result<(), Box<dyn std::error::Error>>;
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        loop {
-            let (block, peer_id) = match ready!(self.from_network.poll_recv(cx)) {
-                Some(msg) => msg,
-                None => {
-                    error!("BSC block import service channel closed, service should terminate");
-                    return Poll::Ready(Ok(()));
-                }
-            };
-            // TODO: validate header
-            let sealed_block = block.block.block.clone().seal();
-            let payload = T::block_to_payload(sealed_block);
-            let engine = self.engine.clone();
-            let to_network = self.to_network.clone();
+        // Process completed imports
+        let _ = self.pending_imports.poll_next_unpin(cx);
 
-            tokio::spawn(async move {
-                let payload_result = engine.new_payload(payload).await;
-
-                let outcome = match payload_result {
-                    Ok(payload_status) => match payload_status.status {
-                        PayloadStatusEnum::Valid => Outcome::<T> {
-                            peer: peer_id,
-                            result: Ok(BlockValidation::ValidBlock { block }),
-                        },
-                        PayloadStatusEnum::Invalid { validation_error } => Outcome::<T> {
-                            peer: peer_id,
-                            result: Err(BlockImportError::InvalidPayload(validation_error)),
-                        },
-                        _ => return,
-                    },
-                    Err(err) => {
-                        Outcome::<T> { peer: peer_id, result: Err(BlockImportError::Engine(err)) }
-                    }
-                };
-
-                // TODO: FCU handling and send to engine
-
-                let _ = to_network.send(outcome);
-            });
+        // Process new blocks
+        while let Poll::Ready(Some((block, peer_id))) = self.from_network.poll_recv(cx) {
+            self.import(block, peer_id);
         }
+
+        // Check if channel is closed
+        if self.from_network.is_closed() {
+            error!("BSC block import service channel closed, service should terminate");
+            return Poll::Ready(Ok(()));
+        }
+
+        Poll::Pending
     }
 }
