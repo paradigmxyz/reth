e32f392b4c33d145d4a09ecbe97de30c133d330c
Dan Cline <6798349+Rjected@users.noreply.github.com>
2025-03-10 17:17:39 -0400

    chore: send result directly when calculating proof

--- crates/trie/parallel/src/proof_task.rs
+++ crates/trie/parallel/src/proof_task.rs
@@ -10,10 +10,9 @@
 
 use crate::root::ParallelStateRootError;
 use alloy_primitives::{map::B256Set, B256};
-use reth_db_api::{transaction::DbTx, Database};
+use reth_db_api::transaction::DbTx;
 use reth_provider::{
-    providers::ConsistentDbView, BlockReader, DBProvider, DatabaseProviderFactory, ProviderResult,
-    StateCommitmentProvider,
+    providers::ConsistentDbView, BlockReader, DBProvider, DatabaseProviderFactory, FactoryTx, ProviderResult, StateCommitmentProvider
 };
 use reth_trie::{
     hashed_cursor::HashedPostStateCursorFactory, proof::StorageProof,
@@ -38,58 +37,67 @@ type StorageProofResult = Result<StorageMultiProof, ParallelStateRootError>;
 /// A task that manages sending multiproof requests to a number of tasks that have longer-running
 /// database transactions
 #[derive(Debug)]
-pub struct ProofTaskManager<Tx> {
+pub struct ProofTaskManager<Factory: DatabaseProviderFactory> {
+    /// Max number of database transactions to create
+    max_concurrency: usize,
+    /// Number of database transactions created
+    total_transactions: usize,
+    /// Consistent view provider used for creating transactions on-demand
+    view: ConsistentDbView<Factory>,
+    /// Proof task context shared across all proof tasks
+    task_ctx: ProofTaskCtx,
     /// Storage proof input pending execution
     pending_proofs: VecDeque<(StorageProofInput, Sender<StorageProofResult>)>,
     /// The underlying handle from which to spawn proof tasks
     executor: Handle,
     /// The proof task transactions, containing owned cursor factories that are reused for proof
     /// calculation.
-    proof_task_txs: Vec<ProofTaskTx<Tx>>,
+    proof_task_txs: Vec<ProofTaskTx<FactoryTx<Factory>>>,
     /// The currently running proof tasks.
-    running_proof_tasks: Vec<RunningProofTask<Tx>>,
+    running_proof_tasks: Vec<RunningProofTask<FactoryTx<Factory>>>,
     /// A receiver for new proof tasks.
     proof_task_rx: Receiver<ProofTaskMessage>,
 }
 
-impl<Tx> ProofTaskManager<Tx> {
+impl<Factory: DatabaseProviderFactory> ProofTaskManager<Factory> {
     /// Creates a new [`ProofTaskManager`] with the given max concurrency, creating that number of
     /// cursor factories.
     ///
     /// Returns an error if the consistent view provider fails to create a read-only transaction.
-    pub fn new<Factory>(
+    pub fn new(
         executor: Handle,
         view: ConsistentDbView<Factory>,
         task_ctx: ProofTaskCtx,
         max_concurrency: usize,
         proof_task_rx: Receiver<ProofTaskMessage>,
-    ) -> ProviderResult<Self>
-    where
-        Factory: DatabaseProviderFactory<Provider: BlockReader, DB: Database<TX = Tx>>
-            + StateCommitmentProvider
-            + 'static,
-    {
+    ) -> ProviderResult<Self> {
         // initialize proof task txs
-        let mut proof_task_txs = Vec::with_capacity(max_concurrency);
-        for _ in 0..max_concurrency {
-            let provider_ro = view.provider_ro()?;
-            let tx = provider_ro.into_tx();
-            proof_task_txs.push(ProofTaskTx::new(tx, task_ctx.clone()));
-        }
+        // let mut proof_task_txs = Vec::with_capacity(max_concurrency);
+        // for _ in 0..max_concurrency {
+        //     let provider_ro = view.provider_ro()?;
+        //     let tx = provider_ro.into_tx();
+        //     proof_task_txs.push(ProofTaskTx::new(tx, task_ctx.clone()));
+        // }
 
         Ok(Self {
+            max_concurrency,
+            total_transactions: 0,
+            view,
+            task_ctx,
             pending_proofs: VecDeque::new(),
             executor,
-            proof_task_txs,
+            proof_task_txs: Vec::new(),
             running_proof_tasks: Vec::new(),
             proof_task_rx,
         })
     }
 }
 
-impl<Tx> ProofTaskManager<Tx>
+impl<Factory> ProofTaskManager<Factory>
 where
-    Tx: DbTx + 'static,
+    Factory: DatabaseProviderFactory<Provider: BlockReader>
+        + StateCommitmentProvider
+        + 'static,
 {
     /// Spawns the proof task on the executor, with the input multiproof targets.
     ///
@@ -102,10 +110,28 @@ where
         self.pending_proofs.push_back((input, sender));
     }
 
+    /// Gets either the next available transaction, or creates a new one if all are in use and the
+    /// total number of transactions created is less than the max concurrency.
+    pub fn get_or_create_tx(&mut self) -> Option<ProofTaskTx<FactoryTx<Factory>>> {
+        if let Some(proof_task_tx) = self.proof_task_txs.pop() {
+            return Some(proof_task_tx);
+        }
+
+        if self.total_transactions < self.max_concurrency {
+            // TODO: propagate this result
+            let provider_ro = self.view.provider_ro().unwrap();
+            let tx = provider_ro.into_tx();
+            self.total_transactions += 1;
+            return Some(ProofTaskTx::new(tx, self.task_ctx.clone()));
+        }
+
+        None
+    }
+
     /// Spawns the next queued proof task on the executor with the given input, if there are any
     /// transactions available.
     pub fn try_spawn_next(&mut self) {
-        let Some(proof_task_tx) = self.proof_task_txs.pop() else { return };
+        let Some(proof_task_tx) = self.get_or_create_tx() else { return };
 
         let Some((pending_proof, sender)) = self.pending_proofs.pop_front() else {
             // if there are no targets to do anything with put the tx back
@@ -115,11 +141,11 @@ where
 
         let (tx, rx) = channel();
         self.executor.spawn_blocking(move || {
-            let output = proof_task_tx.storage_proof(pending_proof);
+            let output = proof_task_tx.storage_proof(pending_proof, sender);
             let _ = tx.send(output);
         });
 
-        self.running_proof_tasks.push(RunningProofTask::new(rx, sender));
+        self.running_proof_tasks.push(RunningProofTask::new(rx));
     }
 
     /// Loops, managing the proof tasks, and sending new tasks to the executor.
@@ -149,10 +175,10 @@ where
 
             // poll each task
             for (idx, running_task) in self.running_proof_tasks.iter().enumerate() {
-                let RunningProofTask { task_receiver, sender } = running_task;
+                let RunningProofTask { task_receiver } = running_task;
                 match task_receiver.try_recv() {
-                    Ok(ProofTaskOutput { tx, result }) => {
-                        let _ = sender.send(result);
+                    Ok(ProofTaskOutput { tx }) => {
+                        // return the tx back to the pool
                         self.proof_task_txs.push(tx);
                     }
                     // If the result is empty or the sender is disconnected, we cannot really do
@@ -197,7 +223,11 @@ where
     Tx: DbTx,
 {
     /// Calcultes a storage proof for the given hashed address, and desired preficx set.
-    pub fn storage_proof(self, input: StorageProofInput) -> ProofTaskOutput<Tx> {
+    pub fn storage_proof(
+        self,
+        input: StorageProofInput,
+        result_sender: Sender<StorageProofResult>,
+    ) -> ProofTaskOutput<Tx> {
         debug!(
             target: "trie::parallel_proof",
             hashed_address=?input.hashed_address,
@@ -236,7 +266,11 @@ where
             "Completed storage proof task calculation"
         );
 
-        ProofTaskOutput { tx: self, result }
+        // send the result back
+        let _ = result_sender.send(result);
+
+        // return the tx
+        ProofTaskOutput { tx: self }
     }
 }
 
@@ -246,8 +280,6 @@ where
 pub struct ProofTaskOutput<Tx> {
     /// The tx to be returned back for later use.
     tx: ProofTaskTx<Tx>,
-    /// The result of proof calculation.
-    result: StorageProofResult,
 }
 
 /// This represents an input for a storage proof.
@@ -296,17 +328,12 @@ impl ProofTaskCtx {
 pub struct RunningProofTask<Tx> {
     /// The receiver for the database tx and proof result.
     task_receiver: Receiver<ProofTaskOutput<Tx>>,
-    /// The sender to return the proof result back to the caller.
-    sender: Sender<StorageProofResult>,
 }
 
 impl<Tx> RunningProofTask<Tx> {
     /// Creates a new [`RunningProofTask`].
-    pub const fn new(
-        task_receiver: Receiver<ProofTaskOutput<Tx>>,
-        sender: Sender<StorageProofResult>,
-    ) -> Self {
-        Self { task_receiver, sender }
+    pub const fn new(task_receiver: Receiver<ProofTaskOutput<Tx>>) -> Self {
+        Self { task_receiver }
     }
 }
 
