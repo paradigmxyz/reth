9096e6f9a2b214df97f49be03e7d3f7f92715e46
Roman Krasiuk <rokrassyuk@gmail.com>
2025-03-10 17:59:44 +0100

    feat(ress): limit active connections (#14928)

--- crates/net/ress/src/handlers.rs
+++ crates/net/ress/src/handlers.rs
@@ -7,9 +7,17 @@ use reth_eth_wire::{
 };
 use reth_network::protocol::{ConnectionHandler, OnNotSupported, ProtocolHandler};
 use reth_network_api::{test_utils::PeersHandle, Direction, PeerId};
-use std::{fmt, net::SocketAddr};
+use std::{
+    fmt,
+    net::SocketAddr,
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+};
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::UnboundedReceiverStream;
+use tracing::*;
 
 /// The events that can be emitted by our custom protocol.
 #[derive(Debug)]
@@ -23,6 +31,11 @@ pub enum ProtocolEvent {
         /// Sender part for forwarding commands.
         to_connection: mpsc::UnboundedSender<RessPeerRequest>,
     },
+    /// Number of max active connections exceeded. New connection was rejected.
+    MaxActiveConnectionsExceeded {
+        /// The current number
+        num_active: u64,
+    },
 }
 
 /// Protocol state is an helper struct to store the protocol events.
@@ -30,6 +43,20 @@ pub enum ProtocolEvent {
 pub struct ProtocolState {
     /// Protocol event sender.
     pub events_sender: mpsc::UnboundedSender<ProtocolEvent>,
+    /// The number of active connections.
+    pub active_connections: Arc<AtomicU64>,
+}
+
+impl ProtocolState {
+    /// Create new protocol state.
+    pub fn new(events_sender: mpsc::UnboundedSender<ProtocolEvent>) -> Self {
+        Self { events_sender, active_connections: Arc::default() }
+    }
+
+    /// Returns the current number of active connections.
+    pub fn active_connections(&self) -> u64 {
+        self.active_connections.load(Ordering::Relaxed)
+    }
 }
 
 /// The protocol handler takes care of incoming and outgoing connections.
@@ -41,6 +68,8 @@ pub struct RessProtocolHandler<P> {
     pub node_type: NodeType,
     /// Peers handle.
     pub peers_handle: PeersHandle,
+    /// The maximum number of active connections.
+    pub max_active_connections: u64,
     /// Current state of the protocol.
     pub state: ProtocolState,
 }
@@ -50,6 +79,7 @@ impl<P> fmt::Debug for RessProtocolHandler<P> {
         f.debug_struct("RessProtocolHandler")
             .field("node_type", &self.node_type)
             .field("peers_handle", &self.peers_handle)
+            .field("max_active_connections", &self.max_active_connections)
             .field("state", &self.state)
             .finish_non_exhaustive()
     }
@@ -61,16 +91,44 @@ where
 {
     type ConnectionHandler = Self;
 
-    fn on_incoming(&self, _socket_addr: SocketAddr) -> Option<Self::ConnectionHandler> {
-        Some(self.clone())
+    fn on_incoming(&self, socket_addr: SocketAddr) -> Option<Self::ConnectionHandler> {
+        let num_active = self.state.active_connections();
+        if num_active >= self.max_active_connections {
+            trace!(
+                target: "ress::net",
+                num_active, max_connections = self.max_active_connections, %socket_addr,
+                "ignoring incoming connection, max active reached"
+            );
+            let _ = self
+                .state
+                .events_sender
+                .send(ProtocolEvent::MaxActiveConnectionsExceeded { num_active });
+            None
+        } else {
+            Some(self.clone())
+        }
     }
 
     fn on_outgoing(
         &self,
-        _socket_addr: SocketAddr,
-        _peer_id: PeerId,
+        socket_addr: SocketAddr,
+        peer_id: PeerId,
     ) -> Option<Self::ConnectionHandler> {
-        Some(self.clone())
+        let num_active = self.state.active_connections();
+        if num_active >= self.max_active_connections {
+            trace!(
+                target: "ress::net",
+                num_active, max_connections = self.max_active_connections, %socket_addr, %peer_id,
+                "ignoring outgoing connection, max active reached"
+            );
+            let _ = self
+                .state
+                .events_sender
+                .send(ProtocolEvent::MaxActiveConnectionsExceeded { num_active });
+            None
+        } else {
+            Some(self.clone())
+        }
     }
 }
 
@@ -105,10 +163,14 @@ where
     ) -> Self::Connection {
         let (tx, rx) = mpsc::unbounded_channel();
 
+        // Emit connection established event.
         self.state
             .events_sender
             .send(ProtocolEvent::Established { direction, peer_id, to_connection: tx })
             .ok();
+
+        // Increment the number of active sessions.
+        self.state.active_connections.fetch_add(1, Ordering::Relaxed);
 
         RessProtocolConnection::new(
             self.provider.clone(),
@@ -117,6 +179,7 @@ where
             peer_id,
             conn,
             UnboundedReceiverStream::from(rx),
+            self.state.active_connections,
         )
     }
 }
