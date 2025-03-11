ccd7f85a736e6e3ac614556c918d2c38fae1ff3c
Gregory Edison <gregory.edison1993@gmail.com>
2025-03-11 15:15:12 +0100

    feat: scroll alloy hardforks
    Signed-off-by: Gregory Edison <gregory.edison1993@gmail.com>

---
+++ crates/scroll/alloy/hardforks/src/hardfork.rs
@@ -0,0 +1,71 @@
+//! Hard forks of scroll protocol.
+
+use alloy_hardforks::{hardfork, ForkCondition};
+
+hardfork!(
+    /// The name of the Scroll hardfork
+    #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+    ScrollHardfork {
+        /// Archimedes: scroll test hardfork.
+        Archimedes,
+        /// Bernoulli: <https://scroll.io/blog/blobs-are-here-scrolls-bernoulli-upgrade>.
+        Bernoulli,
+        /// Curie: <https://scroll.io/blog/compressing-the-gas-scrolls-curie-upgrade>.
+        Curie,
+        /// Darwin: <https://scroll.io/blog/proof-recursion-scrolls-darwin-upgrade>.
+        Darwin,
+        /// DarwinV2 <https://x.com/Scroll_ZKP/status/1830565514755584269>.
+        DarwinV2,
+    }
+);
+
+impl ScrollHardfork {
+    /// Scroll mainnet list of hardforks.
+    pub fn scroll_mainnet() -> [(Self, ForkCondition); 5] {
+        [
+            (Self::Archimedes, ForkCondition::Block(0)),
+            (Self::Bernoulli, ForkCondition::Block(5220340)),
+            (Self::Curie, ForkCondition::Block(7096836)),
+            (Self::Darwin, ForkCondition::Timestamp(1724227200)),
+            (Self::DarwinV2, ForkCondition::Timestamp(1725264000)),
+        ]
+    }
+
+    /// Scroll sepolia list of hardforks.
+    pub fn scroll_sepolia() -> [(Self, ForkCondition); 5] {
+        [
+            (Self::Archimedes, ForkCondition::Block(0)),
+            (Self::Bernoulli, ForkCondition::Block(3747132)),
+            (Self::Curie, ForkCondition::Block(4740239)),
+            (Self::Darwin, ForkCondition::Timestamp(1723622400)),
+            (Self::DarwinV2, ForkCondition::Timestamp(1724832000)),
+        ]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn check_scroll_hardfork_from_str() {
+        let hardfork_str = ["BernOulLi", "CUrie", "DaRwIn", "DaRwInV2"];
+        let expected_hardforks = [
+            ScrollHardfork::Bernoulli,
+            ScrollHardfork::Curie,
+            ScrollHardfork::Darwin,
+            ScrollHardfork::DarwinV2,
+        ];
+
+        let hardforks: Vec<ScrollHardfork> =
+            hardfork_str.iter().map(|h| ScrollHardfork::from_str(h).unwrap()).collect();
+
+        assert_eq!(hardforks, expected_hardforks);
+    }
+
+    #[test]
+    fn check_nonexistent_hardfork_from_str() {
+        assert!(ScrollHardfork::from_str("not a hardfork").is_err());
+    }
+}
---
+++ crates/scroll/alloy/hardforks/src/lib.rs
@@ -0,0 +1,84 @@
+//! Scroll-Reth hard forks.
+
+use alloy_hardforks::{EthereumHardfork, EthereumHardforks, ForkCondition};
+
+pub use hardfork::ScrollHardfork;
+pub mod hardfork;
+
+/// Extends [`EthereumHardforks`] with scroll helper methods.
+#[auto_impl::auto_impl(&, Arc)]
+pub trait ScrollHardforks: EthereumHardforks {
+    /// Retrieves [`ForkCondition`] by an [`ScrollHardfork`]. If `fork` is not present, returns
+    /// [`ForkCondition::Never`].
+    fn scroll_fork_activation(&self, fork: ScrollHardfork) -> ForkCondition;
+
+    /// Convenience method to check if [`Bernoulli`](ScrollHardfork::Bernoulli) is active at a given
+    /// block number.
+    fn is_bernoulli_active_at_block(&self, block_number: u64) -> bool {
+        self.scroll_fork_activation(ScrollHardfork::Bernoulli).active_at_block(block_number)
+    }
+
+    /// Returns `true` if [`Curie`](ScrollHardfork::Curie) is active at given block block number.
+    fn is_curie_active_at_block(&self, block_number: u64) -> bool {
+        self.scroll_fork_activation(ScrollHardfork::Curie).active_at_block(block_number)
+    }
+
+    /// Returns `true` if [`Darwin`](ScrollHardfork::Darwin) is active at given block timestamp.
+    fn is_darwin_active_at_timestamp(&self, timestamp: u64) -> bool {
+        self.scroll_fork_activation(ScrollHardfork::Darwin).active_at_timestamp(timestamp)
+    }
+
+    /// Returns `true` if [`DarwinV2`](ScrollHardfork::DarwinV2) is active at given block timestamp.
+    fn is_darwin_v2_active_at_timestamp(&self, timestamp: u64) -> bool {
+        self.scroll_fork_activation(ScrollHardfork::DarwinV2).active_at_timestamp(timestamp)
+    }
+}
+
+/// A type allowing to configure activation [`ForkCondition`]s for a given list of
+/// [`ScrollHardfork`]s.
+#[derive(Debug, Clone)]
+pub struct ScrollChainHardforks {
+    /// Scroll hardfork activations.
+    forks: Vec<(ScrollHardfork, ForkCondition)>,
+}
+
+impl ScrollChainHardforks {
+    /// Creates a new [`ScrollChainHardforks`] with the given list of forks.
+    pub fn new(forks: impl IntoIterator<Item = (ScrollHardfork, ForkCondition)>) -> Self {
+        let mut forks = forks.into_iter().collect::<Vec<_>>();
+        forks.sort();
+        Self { forks }
+    }
+
+    /// Creates a new [`ScrollChainHardforks`] with Scroll mainnet configuration.
+    pub fn scroll_mainnet() -> Self {
+        Self::new(ScrollHardfork::scroll_mainnet())
+    }
+
+    /// Creates a new [`ScrollChainHardforks`] with Scroll Sepolia configuration.
+    pub fn scroll_sepolia() -> Self {
+        Self::new(ScrollHardfork::scroll_sepolia())
+    }
+}
+
+impl EthereumHardforks for ScrollChainHardforks {
+    fn ethereum_fork_activation(&self, fork: EthereumHardfork) -> ForkCondition {
+        if fork < EthereumHardfork::ArrowGlacier {
+            ForkCondition::Block(0)
+        } else if fork <= EthereumHardfork::Shanghai {
+            self.scroll_fork_activation(ScrollHardfork::Bernoulli)
+        } else {
+            ForkCondition::Never
+        }
+    }
+}
+
+impl ScrollHardforks for ScrollChainHardforks {
+    fn scroll_fork_activation(&self, fork: ScrollHardfork) -> ForkCondition {
+        let Ok(idx) = self.forks.binary_search_by(|(f, _)| f.cmp(&fork)) else {
+            return ForkCondition::Never;
+        };
+
+        self.forks[idx].1
+    }
+}
