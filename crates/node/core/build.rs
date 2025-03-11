816be8e63af3ccddc6c721cbb355a17b83459f2b
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-03-12 00:44:51 +0530

    Update build.rs

--- crates/node/core/build.rs
+++ crates/node/core/build.rs
@@ -1,30 +1,23 @@
 #![allow(missing_docs)]
 
 use std::{env, error::Error};
-use vergen::{Emitter, BuildBuilder, CargoBuilder};
+use vergen::{BuildBuilder, CargoBuilder, Emitter};
 use vergen_git2::Git2Builder;
 
 fn main() -> Result<(), Box<dyn Error>> {
-let mut emitter = Emitter::default();
-    let build_builder = BuildBuilder::default()
-    .build_timestamp(true) 
-    .build()?; 
+    let mut emitter = Emitter::default();
+
+    let build_builder = BuildBuilder::default().build_timestamp(true).build()?;
 
     emitter.add_instructions(&build_builder)?;
 
-    let cargo_builder = CargoBuilder::default()
-        .features(true)       
-        .target_triple(true) 
-        .build()?; 
+    let cargo_builder = CargoBuilder::default().features(true).target_triple(true).build()?;
 
     emitter.add_instructions(&cargo_builder)?;
 
-    let git_builder = Git2Builder::default()
-        .describe(true, true, None) 
-        .dirty(true)               
-        .sha(false)                  
-        .build()?; 
-    
+    let git_builder =
+        Git2Builder::default().describe(true, true, None).dirty(true).sha(false).build()?;
+
     emitter.add_instructions(&git_builder)?;
 
     emitter.emit_and_set()?;
