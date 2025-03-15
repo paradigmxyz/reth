f54a4f8d7d00167e96e673839e33f0a70b585cb5
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-03-15 16:40:02 +0530

    docs

--- crates/engine/tree/src/tree/mod.rs
+++ crates/engine/tree/src/tree/mod.rs
@@ -830,7 +830,7 @@ where
 
     /// When the Consensus layer receives a new block via the consensus gossip protocol,
     /// the transactions in the block are sent to the execution layer in the form of a
-    /// [`EngineTypes::ExecutionData`]. The Execution layer executes the transactions and validates
+    /// [`PayloadTypes::ExecutionData`]. The Execution layer executes the transactions and validates
     /// the state in the block header, then passes validation data back to Consensus layer, that
     /// adds the block to the head of its own blockchain and attests to it. The block is then
     /// broadcast over the consensus p2p network in the form of a "Beacon block".
