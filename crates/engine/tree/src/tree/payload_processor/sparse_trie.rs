6a471124f22fcf5c99075a2fae5107e70cdd23eb
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-03-17 16:20:23 +0000

    perf(tree): do not wait for the sparse trie to drop

--- crates/engine/tree/src/tree/payload_processor/sparse_trie.rs
+++ crates/engine/tree/src/tree/payload_processor/sparse_trie.rs
@@ -19,7 +19,7 @@ use reth_trie_sparse::{
     SparseStateTrie,
 };
 use std::{
-    sync::mpsc,
+    sync::mpsc::{self, Sender},
     time::{Duration, Instant},
 };
 use tracing::{debug, trace, trace_span};
@@ -44,12 +44,19 @@ impl<F> SparseTrieTask<F>
 where
     F: DatabaseProviderFactory<Provider: BlockReader> + StateCommitmentProvider,
 {
+    /// Runs the sparse trie task to completion and sends the outcome to the provided channel.
+    ///
+    /// We do not return the outcome directly to avoid waiting for the [`SparseStateTrie`] to drop.
+    pub(super) fn run(self, tx: Sender<Result<StateRootComputeOutcome, ParallelStateRootError>>) {
+        tx.send(self.run_inner()).unwrap();
+    }
+
     /// Runs the sparse trie task to completion.
     ///
     /// This waits for new incoming [`SparseTrieUpdate`].
     ///
     /// This concludes once the last trie update has been received.
-    pub(super) fn run(self) -> Result<StateRootComputeOutcome, ParallelStateRootError> {
+    fn run_inner(self) -> Result<StateRootComputeOutcome, ParallelStateRootError> {
         let now = Instant::now();
         let provider_ro = self.config.consistent_view.provider_ro()?;
         let in_memory_trie_cursor = InMemoryTrieCursorFactory::new(
