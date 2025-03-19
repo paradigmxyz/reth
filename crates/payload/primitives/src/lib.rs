dd5fe386eb6048eb6db5969e44cdd833e2dda46b
steven <corderosteven6@gmail.com>
2025-03-18 19:51:50 -0600

    docs

--- crates/payload/primitives/src/lib.rs
+++ crates/payload/primitives/src/lib.rs
@@ -47,8 +47,7 @@ pub trait PayloadTypes: Send + Sync + Unpin + core::fmt::Debug + Clone + 'static
         + Clone
         + Unpin;
 
-    /// Converts a [`BuiltPayload`] into an
-    /// [`PayloadTypes:ExecutionData`].
+    /// Converts a [`BuiltPayload`] into an [`ExecutionData`].
     fn block_to_payload(
         block: SealedBlock<
             <<Self::BuiltPayload as BuiltPayload>::Primitives as NodePrimitives>::Block,
