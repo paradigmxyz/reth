b04ecf09cf3ec99af61dd75fa2dc2f3bff71210b
Himess <95512809+Himess@users.noreply.github.com>
2025-03-21 03:15:51 +0300

    Fix: Remove clippy::owned_cow in recovered.rs

--- crates/primitives-traits/src/block/recovered.rs
+++ crates/primitives-traits/src/block/recovered.rs
@@ -563,7 +563,6 @@ pub(super) mod serde_bincode_compat {
             bound = "serde_bincode_compat::SealedBlock<'a, T>: Serialize + serde::de::DeserializeOwned"
         )]
         block: serde_bincode_compat::SealedBlock<'a, T>,
-        #[allow(clippy::owned_cow)]
         senders: Cow<'a, Vec<Address>>,
     }
 
