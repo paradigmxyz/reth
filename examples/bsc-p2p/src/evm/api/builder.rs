62e965b3db3e740c2154f5ec112d5001065673e6
Loocapro <lucaprovini1989@gmail.com>
2025-03-23 16:50:25 +0100

    bsc evm
    chore: remove serde from reth-provider (#15194)
    
    chore: remove serde default (#15186)
    
    Remove default serde feature from reth-network-api (#15226)
    
    feat: precompiles wip

---
+++ examples/bsc-p2p/src/evm/api/builder.rs
@@ -0,0 +1,49 @@
+use super::BscEvm;
+use crate::evm::{spec::BscSpecId, transaction::BscTxTr};
+use revm::{
+    context::{Cfg, JournalOutput},
+    context_interface::{Block, JournalTr},
+    handler::instructions::EthInstructions,
+    interpreter::interpreter::EthInterpreter,
+    Context, Database,
+};
+
+/// Trait that allows for optimism OpEvm to be built.
+pub trait BscBuilder: Sized {
+    /// Type of the context.
+    type Context;
+
+    /// Build the op.
+    fn build_bsc(self)
+        -> BscEvm<Self::Context, (), EthInstructions<EthInterpreter, Self::Context>>;
+
+    /// Build the op with an inspector.
+    fn build_bsc_with_inspector<INSP>(
+        self,
+        inspector: INSP,
+    ) -> BscEvm<Self::Context, INSP, EthInstructions<EthInterpreter, Self::Context>>;
+}
+
+impl<BLOCK, TX, CFG, DB, JOURNAL> BscBuilder for Context<BLOCK, TX, CFG, DB, JOURNAL>
+where
+    BLOCK: Block,
+    TX: BscTxTr,
+    CFG: Cfg<Spec = BscSpecId>,
+    DB: Database,
+    JOURNAL: JournalTr<Database = DB, FinalOutput = JournalOutput>,
+{
+    type Context = Self;
+
+    fn build_bsc(
+        self,
+    ) -> BscEvm<Self::Context, (), EthInstructions<EthInterpreter, Self::Context>> {
+        BscEvm::new(self, ())
+    }
+
+    fn build_bsc_with_inspector<INSP>(
+        self,
+        inspector: INSP,
+    ) -> BscEvm<Self::Context, INSP, EthInstructions<EthInterpreter, Self::Context>> {
+        BscEvm::new(self, inspector)
+    }
+}
