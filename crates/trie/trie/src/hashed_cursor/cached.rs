4e42d886bdf086a6290ef5ff19c8f0c93d1bb771
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-03-24 15:55:42 +0000

    add comments

--- crates/trie/trie/src/hashed_cursor/cached.rs
+++ crates/trie/trie/src/hashed_cursor/cached.rs
@@ -6,6 +6,7 @@ use reth_storage_errors::db::DatabaseError;
 
 use super::{HashedCursor, HashedCursorFactory, HashedStorageCursor};
 
+/// The hashed cursor factory that creates cursors that cache the visited keys.
 #[derive(Clone, Debug)]
 pub struct CachedHashedCursorFactory<CF> {
     cursor_factory: CF,
@@ -17,7 +18,8 @@ impl<CF> CachedHashedCursorFactory<CF> {
         Self { cursor_factory }
     }
 
-    pub const fn inner(&self) -> &CF {
+    #[cfg(test)]
+    pub(crate) const fn inner(&self) -> &CF {
         &self.cursor_factory
     }
 }
@@ -38,11 +40,27 @@ impl<CF: HashedCursorFactory> HashedCursorFactory for CachedHashedCursorFactory<
     }
 }
 
+/// The hashed cursor that caches the visited keys.
 #[derive(Debug, Default)]
 pub struct CachedHashedCursor<C, T> {
     cursor: C,
+    /// The current key.
+    ///
+    /// Set to `B256::ZERO` if the cursor has just been created, is empty, has been seeked to a
+    /// non- existent key, or has been advanced beyond the last key.
     current_key: B256,
+    /// The cache of [`Self::seek`] calls.
+    ///
+    /// The key is the seeked key, and the value is the result of the seek.
+    ///
+    /// This map is also populated:
+    /// - During the [`Self::seek`] calls using the key that the cursor actually seeked to.
+    /// - During the [`Self::next`] calls using the key that the cursor actually advanced to.
     cached_seeks: B256Map<Option<(B256, T)>>,
+    /// The cache of [`Self::next`] calls.
+    ///
+    /// The key is the previous key before calling [`Self::next`], and the value is the result of
+    /// the call.
     cached_nexts: B256Map<Option<(B256, T)>>,
 }
 
@@ -66,18 +84,41 @@ where
 {
     type Value = T;
 
+    /// Seeks to the given key.
+    ///
+    /// If the key is already cached, the value will be returned from the cache.
+    /// Otherwise, the underlying cursor will be seeked to the given key.
+    ///
+    /// The result of the seek will be cached, and the key that the underlying cursor seeked to
+    /// will be cached as well if it differs from the seeked key.
     fn seek(&mut self, key: B256) -> Result<Option<(B256, Self::Value)>, DatabaseError> {
-        Ok(match self.cached_seeks.entry(key) {
-            Entry::Occupied(entry) => *entry.get(),
+        let (result, actual_seek) = match self.cached_seeks.entry(key) {
+            Entry::Occupied(entry) => (*entry.get(), None),
             Entry::Vacant(entry) => {
                 let result = self.cursor.seek(key)?;
                 self.current_key = result.as_ref().map(|(k, _)| *k).unwrap_or(B256::ZERO);
                 entry.insert(result);
-                result
+
+                let actual_seek = result.filter(|(k, _)| k != &key).map(|(k, _)| k);
+
+                (result, actual_seek)
             }
-        })
+        };
+
+        if let Some(actual_seek) = actual_seek {
+            self.cached_seeks.insert(actual_seek, result);
+        }
+
+        Ok(result)
     }
 
+    /// Advances to the next key.
+    ///
+    /// If the next value after the current key is already cached, it will be returned from the
+    /// cache. Otherwise, the underlying cursor will be advanced to the next key.
+    ///
+    /// The result of the advance will be cached in both the cache of [`Self::seek`] calls and the
+    /// cache of [`Self::next`] calls.
     fn next(&mut self) -> Result<Option<(B256, Self::Value)>, DatabaseError> {
         Ok(match self.cached_nexts.entry(self.current_key) {
             Entry::Occupied(entry) => *entry.get(),
