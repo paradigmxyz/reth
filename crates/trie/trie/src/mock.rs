451090cc1044e829c13a58d79d58a35f7ddf6e81
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-03-25 15:12:39 +0000

    Revert "Revert "smarter eviction""
    This reverts commit da60546664130ca3445a7b362c1d2f98ca7a505e.

--- crates/trie/trie/src/mock.rs
+++ crates/trie/trie/src/mock.rs
@@ -10,9 +10,9 @@ pub struct KeyVisit<T> {
 /// The type of key visit.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum KeyVisitType<T> {
-    /// Seeked exact key.
+    /// Sought exact key.
     SeekExact(T),
-    /// Seeked non-exact key, returning the next key if no exact match found.
+    /// Sought non-exact key, returning the next key if no exact match found.
     SeekNonExact(T),
     /// Next key.
     Next,
