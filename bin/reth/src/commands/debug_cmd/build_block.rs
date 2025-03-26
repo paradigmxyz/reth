2471c3e74b3a6e834e34f90638a473ed266a7d9d
Veer Chaurasia <142890355+VeerChaurasia@users.noreply.github.com>
2025-03-26 16:07:41 +0530

    Remove support for loading custom trusted setup (#15290)

--- bin/reth/src/commands/debug_cmd/build_block.rs
+++ bin/reth/src/commands/debug_cmd/build_block.rs
@@ -48,10 +48,6 @@ pub struct Command<C: ChainSpecParser> {
     #[command(flatten)]
     env: EnvironmentArgs<C>,
 
-    /// Overrides the KZG trusted setup by reading from the supplied file.
-    #[arg(long, value_name = "PATH")]
-    trusted_setup_file: Option<PathBuf>,
-
     #[arg(long)]
     parent_beacon_block_root: Option<B256>,
 
@@ -99,16 +95,9 @@ impl<C: ChainSpecParser<ChainSpec = ChainSpec>> Command<C> {
         ))
     }
 
-    /// Loads the trusted setup params from a given file path or falls back to
-    /// `EnvKzgSettings::Default`.
+    /// Returns the default KZG settings
     fn kzg_settings(&self) -> eyre::Result<EnvKzgSettings> {
-        if let Some(ref trusted_setup_file) = self.trusted_setup_file {
-            EnvKzgSettings::load_from_trusted_setup_file(trusted_setup_file).wrap_err_with(|| {
-                format!("Failed to load trusted setup file: {:?}", trusted_setup_file)
-            })
-        } else {
-            Ok(EnvKzgSettings::Default)
-        }
+        Ok(EnvKzgSettings::Default)
     }
 
     /// Execute `debug in-memory-merkle` command
