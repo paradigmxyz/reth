597cae28abb611c6847dea4d1fd5c2a5198a675e
Matthias Seitz <matthias.seitz@outlook.de>
2025-03-27 10:59:49 +0100

    touchups

--- crates/optimism/primitives/src/lib.rs
+++ crates/optimism/primitives/src/lib.rs
@@ -23,11 +23,6 @@ pub mod transaction;
 pub use transaction::{signed::OpTransactionSigned, tx_type::OpTxType};
 
 mod receipt;
-#[cfg(feature = "supervisor")]
-pub mod supervisor;
-#[cfg(feature = "supervisor")]
-pub use supervisor::SupervisorClient;
-
 pub use receipt::{DepositReceipt, OpReceipt};
 
 /// Optimism-specific block type.
