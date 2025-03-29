e523bc2525fedad8140047553c24768d42313b15
Shane K Moore <41407272+shane-moore@users.noreply.github.com>
2025-03-29 01:05:23 -0700

    chore: add blob validation histogram metric to tsx pool validation (#15369)

--- crates/transaction-pool/src/validate/eth.rs
+++ crates/transaction-pool/src/validate/eth.rs
@@ -6,6 +6,7 @@ use crate::{
     error::{
         Eip4844PoolTransactionError, Eip7702PoolTransactionError, InvalidPoolTransactionError,
     },
+    metrics::TxPoolValidationMetrics,
     traits::TransactionOrigin,
     validate::{ValidTransaction, ValidationTask, MAX_INIT_CODE_BYTE_SIZE},
     EthBlobTransactionSidecar, EthPoolTransaction, LocalTransactionConfig,
@@ -34,6 +35,7 @@ use std::{
         atomic::{AtomicBool, AtomicU64},
         Arc,
     },
+    time::Instant,
 };
 use tokio::sync::Mutex;
 
@@ -176,6 +178,8 @@ pub(crate) struct EthTransactionValidatorInner<Client, T> {
     max_tx_input_bytes: usize,
     /// Marker for the transaction type
     _marker: PhantomData<T>,
+    /// Metrics for tsx pool validation
+    validation_metrics: TxPoolValidationMetrics,
 }
 
 // === impl EthTransactionValidatorInner ===
@@ -478,6 +482,7 @@ where
                     }
                 }
                 EthBlobTransactionSidecar::Present(blob) => {
+                    let now = Instant::now();
                     // validate the blob
                     if let Err(err) = transaction.validate_blob(&blob, self.kzg_settings.get()) {
                         return TransactionValidationOutcome::Invalid(
@@ -487,6 +492,8 @@ where
                             ),
                         )
                     }
+                    // Record the duration of successful blob validation as histogram
+                    self.validation_metrics.blob_validation_duration.record(now.elapsed());
                     // store the extracted blob
                     maybe_blob_sidecar = Some(blob);
                 }
@@ -812,6 +819,7 @@ impl<Client> EthTransactionValidatorBuilder<Client> {
             local_transactions_config,
             max_tx_input_bytes,
             _marker: Default::default(),
+            validation_metrics: TxPoolValidationMetrics::default(),
         };
 
         EthTransactionValidator { inner: Arc::new(inner) }
