8718981a3986210bf8b2a1922de3578bf931bde2
Vineet Pant <10172895+vineetpant@users.noreply.github.com>
2025-03-31 14:58:20 +0530

    fix typo in rpc call

--- crates/optimism/rpc/src/sequencer.rs
+++ crates/optimism/rpc/src/sequencer.rs
@@ -84,7 +84,7 @@ impl SequencerClient {
     ) -> Result<(), SequencerClientError> {
         let params = json!([format!("0x{}", hex::encode(tx)), condition]);
 
-        self.send_rpc_call("eth_sendRawTransaction", params).await.inspect_err(|err| {
+        self.send_rpc_call("eth_sendRawTransactionConditional", params).await.inspect_err(|err| {
             warn!(
                 target: "rpc::eth",
                 %err,
