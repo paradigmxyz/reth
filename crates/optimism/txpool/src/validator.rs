19e610277805c8cf53c79856c8e8ad99e8767bc6
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-03-31 08:28:03 -0100

    Shrink scope

--- crates/optimism/txpool/src/validator.rs
+++ crates/optimism/txpool/src/validator.rs
@@ -1,6 +1,6 @@
 use crate::{
     interop::{MaybeInteropTransaction, TransactionInterop},
-    supervisor::{is_valid_cross_tx, InteropTxValidatorError, SupervisorClient},
+    supervisor::{is_valid_cross_tx, SupervisorClient},
     InvalidCrossTx,
 };
 use alloy_consensus::{BlockHeader, Transaction};
@@ -183,28 +183,13 @@ where
         // Interop cross tx validation
         match self.is_valid_cross_tx(&transaction).await {
             Some(Err(err)) => {
-                match err {
+                let err = match err {
                     InvalidCrossTx::CrossChainTxPreInterop => {
-                        return TransactionValidationOutcome::Invalid(
-                            transaction,
-                            InvalidTransactionError::TxTypeNotSupported.into(),
-                        )
+                        InvalidTransactionError::TxTypeNotSupported.into()
                     }
-                    InvalidCrossTx::ValidationError(
-                        InteropTxValidatorError::InvalidInboxEntry(e),
-                    ) if e.is_msg_at_least_cross_unsafe() => {
-                        // message is currently invalid w.r.t. locally configured min safety
-                        // level, but is at least cross-unsafe already. pass as valid anyway, in
-                        // order to store in pool. interop validity is
-                        // revalidated in block building.
-                    }
-                    err => {
-                        return TransactionValidationOutcome::Invalid(
-                            transaction,
-                            InvalidPoolTransactionError::Other(Box::new(err)),
-                        )
-                    }
-                }
+                    err => InvalidPoolTransactionError::Other(Box::new(err)),
+                };
+                return TransactionValidationOutcome::Invalid(transaction, err)
             }
             Some(Ok(_)) => {
                 // valid interop tx
