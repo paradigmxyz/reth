72cac8c86a286d9a012889a24d6dc89402727dcb
Udoagwa Franklin <frankudoags@gmail.com>
2025-04-01 13:43:38 +0100

    wip: use Network error and expect to handle errors

--- crates/cli/commands/src/db/bootnode.rs
+++ crates/cli/commands/src/db/bootnode.rs
@@ -3,10 +3,12 @@ use rand::thread_rng;
 use reth_discv4::{Discv4, Discv4Config};
 use reth_discv5::{Config, Discv5};
 use reth_net_nat::NatResolver;
+use reth_network::error::{NetworkError, ServiceKind};
 use reth_network_peers::NodeRecord;
 use secp256k1::SECP256K1;
 use std::{net::SocketAddr, str::FromStr};
-/// The arguments for the `reth bootnode` command.
+
+/// The arguments for the `reth db bootnode` command.
 /// see https://github.com/ethereum/go-ethereum/blob/14eb8967be7acc54c5dc9a416151ac45c01251b6/cmd/bootnode/main.go#L39-L48
 /// for ref
 #[derive(Parser, Debug)]
@@ -33,21 +35,22 @@ pub struct Command {
 }
 
 impl Command {
-    pub async fn execute(self) -> eyre::Result<()> {
-        // Implement bootnode logic here
+    pub async fn execute(self) -> Result<(), NetworkError> {
         println!("Bootnode started with config: {:?}", self);
         // generate a (random) keypair
         let mut rng = thread_rng();
-        let (sk, pk) = SECP256K1.generate_keypair(&mut rng);
+        let (sk, _pk) = SECP256K1.generate_keypair(&mut rng);
 
-        let socket_addr = SocketAddr::from_str(&self.addr)?;
+        let socket_addr = SocketAddr::from_str(&self.addr).expect("Invalid addr");
         let local_enr = NodeRecord::from_secret_key(socket_addr, &sk);
 
         let discv4_future = async {
             let config = Discv4Config::builder().external_ip_resolver(Some(self.nat)).build();
 
             let (discv4, mut discv4_service) =
-                Discv4::bind(socket_addr, local_enr, sk, config).await.unwrap();
+                Discv4::bind(socket_addr, local_enr, sk, config).await.map_err(|err| {
+                    NetworkError::from_io_error(err, ServiceKind::Discovery(socket_addr))
+                })?;
 
             let discv4_updates = discv4_service.update_stream();
             let discv4_service = discv4_service.spawn();
@@ -57,14 +60,15 @@ impl Command {
 
         let discv5_future = async {
             if self.v5 == false {
-                return Ok((None, None));
+                return Ok::<Option<Discv5>, NetworkError>(None);
             }
             let config = Config::builder(socket_addr).build();
-            let (discv5, discv5_updates, _local_enr_discv5) = Discv5::start(&sk, config).await.unwrap();
-            Ok((Some(discv5), Some(discv5_updates.into())))
+            let (discv5, _discv5_updates, _local_enr_discv5) =
+                Discv5::start(&sk, config).await.map_err(|e| NetworkError::Discv5Error(e))?;
+            Ok(Some(discv5))
         };
 
-        let _ = tokio::try_join!(discv4_future, discv5_future).unwrap();
+        let _ = tokio::try_join!(discv4_future, discv5_future)?;
         Ok(())
     }
 }
