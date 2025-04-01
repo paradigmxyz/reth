5e3c636c802b9d9f4cea1b0e654f5f19a1580011
Francesco <ufoscout@gmail.com>
2025-04-01 11:03:50 +0200

    fix main

--- bin/reth/src/main.rs
+++ bin/reth/src/main.rs
@@ -4,10 +4,9 @@
 static ALLOC: reth_cli_util::allocator::Allocator = reth_cli_util::allocator::new_allocator();
 
 use clap::Parser;
-use reth::{args::RessArgs, cli::Cli, ress::install_ress_subprotocol};
+use reth::{args::RessArgs, ress::install_ress_subprotocol};
 use reth_ethereum_cli::chainspec::EthereumChainSpecParser;
 use reth_node_builder::NodeHandle;
-use reth_node_ethereum::EthereumNode;
 use tracing::info;
 
 fn main() {
@@ -25,8 +24,27 @@ fn main() {
     if let Err(err) =
         Cli::<EthereumChainSpecParser, RessArgs>::parse().run(async move |builder, ress_args| {
             info!(target: "reth::cli", "Launching node");
-            let NodeHandle { node, node_exit_future } =
+            let NodeHandle { node, node_exit_future, bitfinity_import } =
                 builder.node(EthereumNode::default()).launch_with_debug_capabilities().await?;
+
+            {
+                let blockchain_provider = node.provider.clone();
+                let config = node.config.config.clone();
+                let chain = node.chain_spec();
+                let datadir = node.data_dir.clone();
+                let (provider_factory, bitfinity) = bitfinity_import.clone().expect("Bitfinity import not configured");                    
+
+                // Init bitfinity import
+                let import = BitfinityImportCommand::new(
+                    config,
+                    datadir,
+                    chain,
+                    bitfinity.clone(),
+                    provider_factory,
+                    blockchain_provider,
+                );
+                let _import_handle = import.schedule_execution().await?;
+			}
 
             // Install ress subprotocol.
             if ress_args.enabled {
@@ -39,25 +57,6 @@ fn main() {
                     node.add_ons_handle.engine_events.new_listener(),
                 )?;
             }
-
-			{
-			        let blockchain_provider = handle.node.provider.clone();
-                    let config = handle.node.config.config.clone();
-                    let chain = handle.node.chain_spec();
-                    let datadir = handle.node.data_dir.clone();
-                    let (provider_factory, bitfinity) = handle.bitfinity_import.clone().expect("Bitfinity import not configured");                    
-
-                    // Init bitfinity import
-                    let import = BitfinityImportCommand::new(
-                        config,
-                        datadir,
-                        chain,
-                        bitfinity.clone(),
-                        provider_factory,
-                        blockchain_provider,
-                    );
-                    let _import_handle = import.schedule_execution().await?;
-			}
             
             node_exit_future.await
         })
