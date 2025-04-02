c0e521a5c6860b3dd30ced8ee2d0673a50c945d9
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-04-02 14:09:21 +0100

    set as / 2

--- crates/engine/tree/src/tree/payload_processor/mod.rs
+++ crates/engine/tree/src/tree/payload_processor/mod.rs
@@ -149,7 +149,7 @@ where
 
         // We set it to half of the proof task concurrency, because often for each multiproof we
         // spawn one Tokio task for the account proof, and one Tokio task for the storage proof.
-        let max_multi_proof_task_concurrency = 128;
+        let max_multi_proof_task_concurrency = max_proof_task_concurrency / 2;
         let multi_proof_task = MultiProofTask::new(
             state_root_config,
             self.executor.clone(),
