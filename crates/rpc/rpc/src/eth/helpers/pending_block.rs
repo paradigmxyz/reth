f75b127bb2838c9e2b2e0a97307cadf7c850600d
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-04-03 12:48:55 +0200

    Fix conflicts

--- crates/rpc/rpc/src/eth/helpers/pending_block.rs
+++ crates/rpc/rpc/src/eth/helpers/pending_block.rs
@@ -1,21 +1,12 @@
 //! Support for building a pending block with transactions from local view of mempool.
 
 use alloy_consensus::BlockHeader;
-use reth_chainspec::{ChainSpecProvider, EthChainSpec, EthereumHardforks};
+use reth_ethereum_primitives::EthPrimitives;
 use reth_evm::{ConfigureEvm, NextBlockEnvAttributes};
-use reth_node_api::NodePrimitives;
-use reth_primitives_traits::SealedHeader;
-use reth_rpc_eth_api::{
-    helpers::{LoadPendingBlock, SpawnBlocking},
-    types::RpcTypes,
-    FromEvmError, RpcNodeCore,
-};
+use reth_primitives_traits::{BlockTy, HeaderTy, ReceiptTy, SealedHeader};
+use reth_rpc_eth_api::{helpers::LoadPendingBlock, FullEthApiTypes};
 use reth_rpc_eth_types::PendingBlock;
-use reth_storage_api::{
-    BlockReader, BlockReaderIdExt, ProviderBlock, ProviderHeader, ProviderReceipt, ProviderTx,
-    StateProviderFactory,
-};
-use reth_transaction_pool::{PoolTransaction, TransactionPool};
+use reth_storage_api::BlockReader;
 use revm_primitives::B256;
 
 use crate::EthApi;
@@ -23,48 +14,23 @@ use crate::EthApi;
 impl<Provider, Pool, Network, EvmConfig> LoadPendingBlock
     for EthApi<Provider, Pool, Network, EvmConfig>
 where
-    Self: SpawnBlocking<
-            NetworkTypes: RpcTypes<Header = alloy_rpc_types_eth::Header>,
-            Error: FromEvmError<Self::Evm>,
-        > + RpcNodeCore<
-            Provider: BlockReaderIdExt<
-                Transaction = reth_ethereum_primitives::TransactionSigned,
-                Block = reth_ethereum_primitives::Block,
-                Receipt = reth_ethereum_primitives::Receipt,
-                Header = alloy_consensus::Header,
-            > + ChainSpecProvider<ChainSpec: EthChainSpec + EthereumHardforks>
-                          + StateProviderFactory,
-            Pool: TransactionPool<
-                Transaction: PoolTransaction<Consensus = ProviderTx<Self::Provider>>,
-            >,
-            Evm: ConfigureEvm<
-                Primitives: NodePrimitives<
-                    BlockHeader = ProviderHeader<Self::Provider>,
-                    SignedTx = ProviderTx<Self::Provider>,
-                    Receipt = ProviderReceipt<Self::Provider>,
-                    Block = ProviderBlock<Self::Provider>,
-                >,
-                NextBlockEnvCtx = NextBlockEnvAttributes,
-            >,
-        >,
-    Provider: BlockReader<
-        Block = reth_ethereum_primitives::Block,
-        Receipt = reth_ethereum_primitives::Receipt,
-    >,
+    Self: FullEthApiTypes<Primitives = EthPrimitives>,
+    Provider: BlockReader<Block = BlockTy<Self::Primitives>, Receipt = ReceiptTy<Self::Primitives>>,
+    EvmConfig: ConfigureEvm<NextBlockEnvCtx = NextBlockEnvAttributes>,
 {
     #[inline]
     fn pending_block(
         &self,
     ) -> &tokio::sync::Mutex<
-        Option<PendingBlock<ProviderBlock<Self::Provider>, ProviderReceipt<Self::Provider>>>,
+        Option<PendingBlock<BlockTy<Self::Primitives>, ReceiptTy<Self::Primitives>>>,
     > {
         self.inner.pending_block()
     }
 
     fn next_env_attributes(
         &self,
-        parent: &SealedHeader<ProviderHeader<Self::Provider>>,
-    ) -> Result<<Self::Evm as reth_evm::ConfigureEvm>::NextBlockEnvCtx, Self::Error> {
+        parent: &SealedHeader<HeaderTy<Self::Primitives>>,
+    ) -> Result<<Self::Evm as ConfigureEvm>::NextBlockEnvCtx, Self::Error> {
         Ok(NextBlockEnvAttributes {
             timestamp: parent.timestamp().saturating_add(12),
             suggested_fee_recipient: parent.beneficiary(),
