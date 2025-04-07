4f5f9b2e87338720c9f7d0a0a82e812504692175
Luca Provini <luca.provini@usemerkle.com>
2025-04-07 13:58:36 +0200

    it test: network fetching header and bodies, ready to execute them

--- examples/bsc-sdk/tests/it/sync_blocks.rs
+++ examples/bsc-sdk/tests/it/sync_blocks.rs
@@ -1,34 +1,25 @@
 use alloy_eips::HashOrNumber;
+use alloy_primitives::B256;
 use example_bsc_sdk::{
-    chainspec::{
-        bsc::{bsc_mainnet, head},
-        BscChainSpec,
-    },
-    node::{
-        network::{boot_nodes, handshake::BscHandshake},
-        BscNode,
-    },
+    chainspec::{bsc::bsc_mainnet, BscChainSpec},
+    node::BscNode,
 };
 use reth::{
     args::RpcServerArgs,
     builder::{Node, NodeBuilder, NodeConfig, NodeHandle},
     tasks::TaskManager,
-    transaction_pool::test_utils::testing_pool,
 };
-use reth_discv4::Discv4Config;
 use reth_eth_wire::HeadersDirection;
-use reth_network::{
-    BlockDownloaderProvider, NetworkEventListenerProvider, NetworkManager, NetworkSyncUpdater,
-    PeersInfo, SyncState,
+use reth_network::{BlockDownloaderProvider, NetworkSyncUpdater, PeersInfo, SyncState};
+use reth_network_p2p::{
+    headers::client::{HeadersClient, HeadersRequest},
+    BodiesClient,
 };
-use secp256k1::SecretKey;
-
-use reth_network_p2p::headers::client::{HeadersClient, HeadersRequest};
-use reth_provider::{noop::NoopProvider, providers::BlockchainProvider};
-use secp256k1::rand;
+use reth_provider::providers::BlockchainProvider;
 use std::{sync::Arc, time::Duration};
 use tokio::task;
-use tokio_stream::StreamExt;
+
+use tracing::info;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn can_sync_blocks() -> eyre::Result<()> {
@@ -49,57 +40,10 @@ async fn can_sync_blocks() -> eyre::Result<()> {
         .with_add_ons(BscNode::default().add_ons())
         .launch()
         .await?;
-    let h = node.network.clone();
-    let mut events = node.network.event_listener();
+    let handle = node.network.clone();
 
-    h.update_sync_state(SyncState::Syncing);
-
-    task::spawn(async move {
-        loop {
-            tokio::time::sleep(Duration::from_secs(5)).await;
-            dbg!(h.num_connected_peers());
-        }
-    });
-    while let Some(ev) = events.next().await {
-        dbg!(ev);
-    }
-
-    Ok(())
-}
-
-#[tokio::test(flavor = "multi_thread")]
-#[ignore]
-async fn test_connect_to_trusted_peer() {
-    reth_tracing::init_test_tracing();
-    let secret_key = SecretKey::new(&mut rand::thread_rng());
-    let mut discv4 = Discv4Config::builder();
-    discv4.add_boot_nodes(boot_nodes()).lookup_interval(Duration::from_millis(500));
-
-    let client = NoopProvider::eth(Arc::new(bsc_mainnet()));
-    let config = reth_network::NetworkConfigBuilder::eth(secret_key)
-        .set_head(head())
-        .with_pow()
-        .eth_rlpx_handshake(Arc::new(BscHandshake::default()))
-        .discovery(discv4)
-        .build(client.clone());
-    let transactions_manager_config = config.transactions_manager_config.clone();
-    let (handle, network, transactions, requests) = NetworkManager::new(config)
-        .await
-        .unwrap()
-        .into_builder()
-        .request_handler(client)
-        .transactions(testing_pool(), transactions_manager_config)
-        .split_with_handle();
-
-    let mut events = handle.event_listener();
-
-    tokio::task::spawn(async move {
-        tokio::join!(network, requests, transactions);
-    });
-
+    handle.update_sync_state(SyncState::Syncing);
     let h = handle.clone();
-    h.update_sync_state(SyncState::Syncing);
-
     task::spawn(async move {
         loop {
             tokio::time::sleep(Duration::from_secs(5)).await;
@@ -109,17 +53,39 @@ async fn test_connect_to_trusted_peer() {
 
     let fetcher = handle.fetch_client().await.unwrap();
 
-    let headers = fetcher
-        .get_headers(HeadersRequest {
-            start: HashOrNumber::Number(1),
-            limit: 10,
-            direction: HeadersDirection::Rising,
-        })
-        .await;
+    let headers = {
+        loop {
+            let Ok(headers) = fetcher
+                .get_headers(HeadersRequest {
+                    start: HashOrNumber::Number(1),
+                    limit: 10,
+                    direction: HeadersDirection::Rising,
+                })
+                .await
+            else {
+                continue
+            };
 
-    dbg!(&headers);
+            info!("Received {} headers", headers.1.len());
 
-    while let Some(ev) = events.next().await {
-        dbg!(ev);
-    }
+            if headers.1.len() == 10 {
+                break headers.1;
+            }
+        }
+    };
+
+    info!("Successfully retrieved {} headers", headers.len());
+    let hashes: Vec<B256> = headers.iter().map(|h| h.hash_slow()).collect::<Vec<_>>();
+    let bodies = {
+        loop {
+            let Ok(bodies) = fetcher.get_block_bodies(hashes.clone()).await else { continue };
+            if bodies.1.len() == hashes.len() {
+                break bodies;
+            }
+        }
+    };
+
+    info!("Successfully retrieved {} bodies", bodies.1.len());
+
+    Ok(())
 }
