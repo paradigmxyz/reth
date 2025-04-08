dc367ad785f02240762be23146f3506aa05f854e
Yohan Nelson <yohan.nelson@gmail.com>
2025-04-08 17:38:55 +0530

    fix: eth_callMany uses vec bundle (#15596)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/rpc/rpc-eth-api/src/core.rs
+++ crates/rpc/rpc-eth-api/src/core.rs
@@ -232,7 +232,7 @@ pub trait EthApi<T: RpcObject, B: RpcObject, R: RpcObject, H: RpcObject> {
     #[method(name = "callMany")]
     async fn call_many(
         &self,
-        bundle: Bundle,
+        bundles: Vec<Bundle>,
         state_context: Option<StateContext>,
         state_override: Option<StateOverride>,
     ) -> RpcResult<Vec<EthCallResponse>>;
@@ -656,12 +656,12 @@ where
     /// Handler for: `eth_callMany`
     async fn call_many(
         &self,
-        bundle: Bundle,
+        bundles: Vec<Bundle>,
         state_context: Option<StateContext>,
         state_override: Option<StateOverride>,
     ) -> RpcResult<Vec<EthCallResponse>> {
-        trace!(target: "rpc::eth", ?bundle, ?state_context, ?state_override, "Serving eth_callMany");
-        Ok(EthCall::call_many(self, bundle, state_context, state_override).await?)
+        trace!(target: "rpc::eth", ?bundles, ?state_context, ?state_override, "Serving eth_callMany");
+        Ok(EthCall::call_many(self, bundles, state_context, state_override).await?)
     }
 
     /// Handler for: `eth_createAccessList`
