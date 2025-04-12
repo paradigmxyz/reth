edf9bdd6106ff05654659c0f04e07aa32fd11914
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-04-12 21:28:11 +0100

    add RLPExecutionWitness -- temporary as its inefficient especially as we have a lot of bytecode

---
+++ crates/ress/protocol/src/execution_witness.rs
@@ -0,0 +1,21 @@
+use alloy_primitives::Bytes;
+use alloy_rlp::{RlpDecodable, RlpEncodable};
+use alloy_rpc_types_debug::ExecutionWitness;
+
+/// Temporary wrapper around ExecutionWitness so that it is RLPDecodable
+#[cfg_attr(any(test, feature = "arbitrary"), derive(arbitrary::Arbitrary))]
+#[derive(Debug, RlpDecodable, RlpEncodable, Clone, Default, PartialEq, Eq)]
+pub struct RLPExecutionWitness {
+    /// Serialized hashed post state
+    pub state: Vec<Bytes>,
+    /// Serialized bytecode
+    pub codes: Vec<Bytes>,
+    /// Serialized headers
+    pub headers: Vec<Bytes>,
+}
+
+impl From<ExecutionWitness> for RLPExecutionWitness {
+    fn from(value: ExecutionWitness) -> Self {
+        RLPExecutionWitness { state: value.state, codes: value.codes, headers: value.headers }
+    }
+}
