93d6f53d51b13952292e33534045949bed9de3ab
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-04-12 15:38:52 +0100

    replace doc link

--- crates/primitives-traits/src/transaction/signed.rs
+++ crates/primitives-traits/src/transaction/signed.rs
@@ -52,7 +52,7 @@ pub trait SignedTransaction:
 
     /// Recover signer from signature and hash.
     ///
-    /// Returns `RecoveryError` if the transaction's signature is invalid following [EIP-2](https://eips.ethereum.org/EIPS/eip-2), see also `reth_primitives::transaction::recover_signer`.
+    /// Returns `RecoveryError` if the transaction's signature is invalid following [EIP-2](https://eips.ethereum.org/EIPS/eip-2), see also `reth_primitive_traits::crypto::secp256k1::recover_signer`.
     ///
     /// Note:
     ///
