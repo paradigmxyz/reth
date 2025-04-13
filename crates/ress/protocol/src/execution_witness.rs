78df17a310a434336b9a2696f8099ece3fb80e2d
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-04-13 15:25:30 +0100

    add metric methods to RLPExecutionWitness

--- crates/ress/protocol/src/execution_witness.rs
+++ crates/ress/protocol/src/execution_witness.rs
@@ -14,6 +14,35 @@ pub struct RLPExecutionWitness {
     pub headers: Vec<Bytes>,
 }
 
+impl RLPExecutionWitness {
+    /// Returns true if the witness is empty
+    pub fn is_empty(&self) -> bool {
+        self.state.is_empty() && self.codes.is_empty() && self.headers.is_empty()
+    }
+
+    /// Returns the total number of bytes needed to represent
+    /// the witness
+    pub fn size(&self) -> usize {
+        self.state.len() + self.codes.len() + self.headers.len()
+    }
+
+    /// Returns the number of trie nodes in the witness
+    pub fn node_count(&self) -> usize {
+        self.state.len()
+    }
+
+    /// Returns the bytecodes in the witness
+    pub fn code_count(&self) -> usize {
+        self.codes.len()
+    }
+
+    /// Returns the average code size
+    pub fn average_code_size(&self) -> usize {
+        let total_code_size: usize = self.codes.iter().map(|code| code.len()).sum();
+        total_code_size / self.code_count()
+    }
+}
+
 impl From<ExecutionWitness> for RLPExecutionWitness {
     fn from(value: ExecutionWitness) -> Self {
         Self { state: value.state, codes: value.codes, headers: value.headers }
