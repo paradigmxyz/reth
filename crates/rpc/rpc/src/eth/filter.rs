75e467b58f2aede2b13362972415410f0df4d589
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-04-15 16:28:14 +0200

    Fix broken doctests

--- crates/rpc/rpc/src/eth/filter.rs
+++ crates/rpc/rpc/src/eth/filter.rs
@@ -96,10 +96,10 @@ where
     /// use reth_provider::noop::NoopProvider;
     /// use reth_rpc::{EthApi, EthFilter};
     /// use reth_tasks::TokioTaskExecutor;
-    /// use reth_transaction_pool::noop::NoopTransactionPool;
+    /// use reth_transaction_pool::{EthPooledTransaction, noop::NoopTransactionPool};
     /// let eth_api = EthApi::builder(
     ///     NoopProvider::default(),
-    ///     NoopTransactionPool::default(),
+    ///     NoopTransactionPool::<EthPooledTransaction>default(),
     ///     NoopNetwork::default(),
     ///     EthEvmConfig::mainnet(),
     /// )
