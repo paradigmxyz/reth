2e251970e88a298cc7ad494d92bc10d6de753b9e
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-04-17 13:25:36 +0100

    create a stub for tracing and alias alloc

--- crates/trie/sparse/src/lib.rs
+++ crates/trie/sparse/src/lib.rs
@@ -1,6 +1,24 @@
 //! The implementation of sparse MPT.
 
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(not(feature = "std"))]
+extern crate alloc as std;
+
+/// A macro that expands to a trace! call when std feature is enabled,
+/// and does nothing when `no_std` is enabled.
+#[macro_export]
+macro_rules! trace {
+    ($($args:tt)*) => {
+        #[cfg(feature = "std")]
+        reth_tracing::tracing::trace!($($args)*);
+        #[cfg(not(feature = "std"))]
+        {
+            // Currently in no_std, trace! is a noop
+        };
+    };
+}
 
 mod state;
 pub use state::*;
