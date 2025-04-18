2123358a8fe6286a39533c8597b23f5fd5d3efab
Roman Krasiuk <rokrassyuk@gmail.com>
2025-04-18 13:35:07 +0200

    make fn const

--- crates/trie/trie/src/witness.rs
+++ crates/trie/trie/src/witness.rs
@@ -86,7 +86,7 @@ impl<T, H> TrieWitness<T, H> {
     /// Set `always_include_root_node` to true. Root node will be included even on empty state.
     /// This setting is useful if the caller wants to verify the witness against the
     /// parent state root.
-    pub fn always_include_root_node(mut self) -> Self {
+    pub const fn always_include_root_node(mut self) -> Self {
         self.always_include_root_node = true;
         self
     }
