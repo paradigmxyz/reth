b1e0f71fa0000fa786a9028ff7044ca37f6302c3
kevaundray <kevtheappdev@gmail.com>
2025-04-19 09:46:09 +0100

    fix: Check for the maximum allowed gas limit in the block (#15800)

--- crates/primitives-traits/src/constants/mod.rs
+++ crates/primitives-traits/src/constants/mod.rs
@@ -10,6 +10,10 @@ pub const RETH_CLIENT_VERSION: &str = concat!("reth/v", env!("CARGO_PKG_VERSION"
 /// Minimum gas limit allowed for transactions.
 pub const MINIMUM_GAS_LIMIT: u64 = 5000;
 
+/// Maximum gas limit allowed for block.
+/// In hex this number is `0x7fffffffffffffff`
+pub const MAXIMUM_GAS_LIMIT_BLOCK: u64 = 2u64.pow(63) - 1;
+
 /// The bound divisor of the gas limit, used in update calculations.
 pub const GAS_LIMIT_BOUND_DIVISOR: u64 = 1024;
 
