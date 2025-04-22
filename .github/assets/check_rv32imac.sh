21c0b276cfff435064781a2d2a200c5151ecd08d
kevaundray <kevtheappdev@gmail.com>
2025-04-22 03:29:58 +0100

    chore: Add `reth-primitives` to `no-std` CI checks (#15823)

--- .github/assets/check_rv32imac.sh
+++ .github/assets/check_rv32imac.sh
@@ -4,6 +4,7 @@ set +e  # Disable immediate exit on error
 # Array of crates to check
 crates_to_check=(
     reth-codecs-derive
+    reth-primitives
     reth-primitives-traits
     reth-network-peers
     reth-trie-common
