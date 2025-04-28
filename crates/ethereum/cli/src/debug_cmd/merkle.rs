eee3b60946705e82f60be17ad706b5ca4dcdbcdf
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-04-29 01:25:36 +0530

    Update merkle.rs

--- crates/ethereum/cli/src/debug_cmd/merkle.rs
+++ crates/ethereum/cli/src/debug_cmd/merkle.rs
@@ -307,7 +307,7 @@ impl<C: ChainSpecParser<ChainSpec = ChainSpec>> Command<C> {
 
 impl<C: ChainSpecParser> Command<C> {
     /// Returns the underlying chain being used to run this command
-    pub fn chain_spec(&self) -> Option<&Arc<C::ChainSpec>> {
+    pub const fn chain_spec(&self) -> Option<&Arc<C::ChainSpec>> {
         Some(&self.env.chain)
     }
 }
