ffd9ff9458277ded4fa8211e92089c1d0400ac29
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-04-29 01:57:45 +0530

    Update unwind.rs

--- crates/cli/commands/src/stage/unwind.rs
+++ crates/cli/commands/src/stage/unwind.rs
@@ -160,7 +160,7 @@ impl<C: ChainSpecParser<ChainSpec: EthChainSpec + EthereumHardforks>> Command<C>
 }
 
 impl<C: ChainSpecParser> Command<C> {
-    /// Returns the underlying chain being used to run this command
+    /// Return the underlying chain being used to run this command
     pub fn chain_spec(&self) -> Option<&Arc<C::ChainSpec>> {
         Some(&self.env.chain)
     }
