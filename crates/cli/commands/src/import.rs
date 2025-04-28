dc988783e90ce21335b0f54ecfab8debd5a9ba28
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-04-28 23:40:31 +0530

    Update import.rs

--- crates/cli/commands/src/import.rs
+++ crates/cli/commands/src/import.rs
@@ -161,6 +161,9 @@ impl<C: ChainSpecParser<ChainSpec: EthChainSpec + EthereumHardforks>> ImportComm
 
         Ok(())
     }
+}
+
+impl<C: ChainSpecParser> ImportCommand<C> {
     /// Returns the underlying chain being used to run this command
     pub fn chain_spec(&self) -> Option<&Arc<C::ChainSpec>> {
         Some(&self.env.chain)
