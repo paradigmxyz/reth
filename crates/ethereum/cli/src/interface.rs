8d3cf898f807497bcec036843ea2e68c753041e0
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-04-29 01:00:04 +0530

    Update interface.rs

--- crates/ethereum/cli/src/interface.rs
+++ crates/ethereum/cli/src/interface.rs
@@ -261,9 +261,7 @@ impl<C: ChainSpecParser, Ext: clap::Args + fmt::Debug> Commands<C, Ext> {
             Self::Stage(cmd) => cmd.chain_spec(),
             Self::P2P(cmd) => cmd.chain_spec(),
             #[cfg(feature = "dev")]
-            Self::TestVectors(cmd) => {
-                cmd.chain_spec().map(|c| c as &Arc<C::ChainSpec>)
-            },
+            Self::TestVectors(_) => None,
             Self::Config(_) => None,
             Self::Debug(cmd) => cmd.chain_spec(),
             Self::Recover(cmd) => cmd.chain_spec(),
