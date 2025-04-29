d69c42402fa9f9e9c24d73d59e4deeb82f1cdeb1
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-04-29 14:50:15 +0530

    chore: Relaxing ChainSpecParser bound in Commands impl (#15976)
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/cli/commands/src/import_era.rs
+++ crates/cli/commands/src/import_era.rs
@@ -90,7 +90,9 @@ impl<C: ChainSpecParser<ChainSpec: EthChainSpec + EthereumHardforks>> ImportEraC
 
         Ok(())
     }
+}
 
+impl<C: ChainSpecParser> ImportEraCommand<C> {
     /// Returns the underlying chain being used to run this command
     pub fn chain_spec(&self) -> Option<&Arc<C::ChainSpec>> {
         Some(&self.env.chain)
