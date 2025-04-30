aea780388dcc12f850a35020bbe105dd8968712d
Soubhik-10 <soubhiksmp2004@gmail.com>
2025-04-30 18:18:42 +0530

    full path

--- crates/primitives-traits/src/typed.rs
+++ crates/primitives-traits/src/typed.rs
@@ -1,8 +1,6 @@
 //! Trait for checking type of transaction envelope.
 
 use alloy_consensus::{transaction::EthereumTxEnvelope, TxType};
-#[cfg(feature = "op")]
-use op_alloy_consensus::{transaction::OpTxEnvelope, OpTxType};
 
 /// Trait for checking if a transaction envelope supports a given EIP-2718 type ID.
 pub trait IsTyped2718 {
@@ -12,7 +10,7 @@ pub trait IsTyped2718 {
 
 impl IsTyped2718 for TxType {
     fn is_type(type_id: u8) -> bool {
-        matches!(type_id, 0x01..=0x04)
+        matches!(type_id, 0x0..=0x04)
     }
 }
 
@@ -23,15 +21,15 @@ impl<T: IsTyped2718> IsTyped2718 for EthereumTxEnvelope<T> {
 }
 
 #[cfg(feature = "op")]
-impl IsTyped2718 for OpTxType {
+impl IsTyped2718 for op_alloy_consensus::OpTxType {
     fn is_type(type_id: u8) -> bool {
         matches!(type_id, 0x01 | 0x02 | 0x03 | 0x04 | 0x7E)
     }
 }
 
 #[cfg(feature = "op")]
-impl IsTyped2718 for OpTxEnvelope {
+impl IsTyped2718 for op_alloy_consensus::OpTxEnvelope {
     fn is_type(type_id: u8) -> bool {
-        OpTxType::is_type(type_id)
+        op_alloy_consensus::OpTxType::is_type(type_id)
     }
 }
