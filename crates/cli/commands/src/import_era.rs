57a6782838b35d65d04da54da376588b7f57fec6
Roman Hodulák <roman.hodulak@polyglot-software.com>
2025-05-02 16:03:24 +0200

    refactor(era): Make future `'static` and remove `tokio-scoped`
    Removes the reliance on this crate and its layer of complexity

--- crates/cli/commands/src/import_era.rs
+++ crates/cli/commands/src/import_era.rs
@@ -73,9 +73,9 @@ impl<C: ChainSpecParser<ChainSpec: EthChainSpec + EthereumHardforks>> ImportEraC
         let provider_factory = &provider_factory.provider_rw()?.0;
 
         if let Some(path) = self.import.path {
-            let mut stream = read_dir(path)?;
+            let stream = read_dir(path)?;
 
-            era::import(&mut stream, provider_factory, &mut hash_collector)?;
+            era::import(stream, provider_factory, &mut hash_collector)?;
         } else {
             let url = match self.import.url {
                 Some(url) => url,
@@ -84,9 +84,9 @@ impl<C: ChainSpecParser<ChainSpec: EthChainSpec + EthereumHardforks>> ImportEraC
             let folder = data_dir().ok_or_eyre("Missing data directory")?.join("era");
             let folder = folder.into_boxed_path();
             let client = EraClient::new(Client::new(), url, folder);
-            let mut stream = EraStream::new(client, EraStreamConfig::default());
+            let stream = EraStream::new(client, EraStreamConfig::default());
 
-            era::import(&mut stream, provider_factory, &mut hash_collector)?;
+            era::import(stream, provider_factory, &mut hash_collector)?;
         }
 
         Ok(())
