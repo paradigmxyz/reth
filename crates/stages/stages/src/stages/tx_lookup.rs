d98651974593b5ce193a277692af30753013c036
Merkel Tranjes <140164174+rnkrtt@users.noreply.github.com>
2025-05-06 20:55:20 +0200

    Update tx_lookup.rs

--- crates/stages/stages/src/stages/tx_lookup.rs
+++ crates/stages/stages/src/stages/tx_lookup.rs
@@ -460,7 +460,7 @@ mod tests {
         ///
         /// 1. If there are any entries in the [`tables::TransactionHashNumbers`] table above a
         ///    given block number.
-        /// 2. If the is no requested block entry in the bodies table, but
+        /// 2. If there is no requested block entry in the bodies table, but
         ///    [`tables::TransactionHashNumbers`] is    not empty.
         fn ensure_no_hash_by_block(&self, number: BlockNumber) -> Result<(), TestRunnerError> {
             let body_result = self
