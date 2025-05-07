dc07f99552de12eec0947a2a431431560fb31814
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-05-07 14:43:58 +0200

    Fix conflict

--- crates/optimism/node-types/src/lib.rs
+++ crates/optimism/node-types/src/lib.rs
@@ -6,7 +6,6 @@
     issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
 )]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
-#![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
 pub mod engine;
