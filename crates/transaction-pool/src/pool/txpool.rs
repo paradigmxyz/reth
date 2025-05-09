649b9740f67d0b483f4d1f99bab0b615f72a9dac
Matthias Seitz <matthias.seitz@outlook.de>
2025-05-09 14:39:23 +0200

    chore: remove_auths doc

--- crates/transaction-pool/src/pool/txpool.rs
+++ crates/transaction-pool/src/pool/txpool.rs
@@ -1618,6 +1618,9 @@ impl<T: PoolTransaction> AllTransactions<T> {
         result
     }
 
+    /// Removes any pending auths for the given transaction.
+    ///
+    /// This is a noop for non EIP-7702 transactions.
     fn remove_auths(&mut self, tx: &PoolInternalTransaction<T>) {
         let Some(auths) = &tx.transaction.authority_ids else { return };
 
@@ -1977,6 +1980,7 @@ impl<T: PoolTransaction> AllTransactions<T> {
     #[cfg(any(test, feature = "test-utils"))]
     pub(crate) fn assert_invariants(&self) {
         assert_eq!(self.by_hash.len(), self.txs.len(), "by_hash.len() != txs.len()");
+        assert!(self.auths.len() <= self.txs.len(), "auths > txs.len()");
     }
 }
 
