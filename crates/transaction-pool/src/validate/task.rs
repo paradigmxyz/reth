3b346dea5fb058c3513680b4d04ebf589e986912
keanji-x <jianxie0@gmail.com>
2025-05-13 22:20:16 +0800

    fix task

--- crates/transaction-pool/src/validate/task.rs
+++ crates/transaction-pool/src/validate/task.rs
@@ -161,6 +161,41 @@ where
 {
     type Transaction = <V as TransactionValidator>::Transaction;
 
+    async fn validate_transactions(
+        &self,
+        transactions: Vec<(TransactionOrigin, Self::Transaction)>,
+    ) -> Vec<TransactionValidationOutcome<Self::Transaction>> {
+        let hashes: Vec<_> = transactions.iter().map(|(_, tx)| *tx.hash()).collect();
+        let (tx, rx) = oneshot::channel();
+        {
+            let res = {
+                let to_validation_task = self.to_validation_task.clone();
+                let validator = self.validator.clone();
+                let fut = Box::pin(async move {
+                    let res = validator.validate_transactions(transactions).await;
+                    let _ = tx.send(res);
+                });
+                let to_validation_task = to_validation_task.lock().await;
+                to_validation_task
+                    .send(fut)
+                    .await
+            };
+            if res.is_err() {
+                return hashes.into_iter().map(|hash| TransactionValidationOutcome::Error(
+                    hash,
+                    Box::new(TransactionValidatorError::ValidationServiceUnreachable),
+                )).collect();
+            }
+        }
+        match rx.await {
+            Ok(res) => res,
+            Err(_) => hashes.into_iter().map(|hash| TransactionValidationOutcome::Error(
+                hash,
+                Box::new(TransactionValidatorError::ValidationServiceUnreachable),
+            )).collect(),
+        }
+    }
+
     async fn validate_transaction(
         &self,
         origin: TransactionOrigin,
