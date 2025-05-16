2d364e588f2355ca8f67c44197cb91ad58ce4c19
Denis Kolodin <deniskolodin@gmail.com>
2025-05-16 16:40:08 +0200

    feat: custom name for opentelemetry

--- crates/tracing-otlp/src/lib.rs
+++ crates/tracing-otlp/src/lib.rs
@@ -4,7 +4,7 @@
 //! applications. It allows for easily capturing and exporting distributed traces to compatible
 //! backends like Jaeger, Zipkin, or any other OpenTelemetry-compatible tracing system.
 
-use opentelemetry::{trace::TracerProvider, KeyValue};
+use opentelemetry::{trace::TracerProvider, KeyValue, Value};
 use opentelemetry_otlp::SpanExporter;
 use opentelemetry_sdk::{
     trace::{SdkTracer, SdkTracerProvider},
@@ -19,24 +19,20 @@ use tracing_subscriber::registry::LookupSpan;
 ///
 /// This layer can be added to a [tracing_subscriber::Registry] to enable OpenTelemetry tracing
 /// with OTLP export.
-pub fn layer<S>() -> OpenTelemetryLayer<S, SdkTracer>
+pub fn layer<S>(service_name: impl Into<Value>) -> OpenTelemetryLayer<S, SdkTracer>
 where
     for<'span> S: Subscriber + LookupSpan<'span>,
 {
     let exporter = SpanExporter::builder().with_http().build().unwrap();
 
-    let provider = SdkTracerProvider::builder()
-        .with_resource(resource())
-        .with_batch_exporter(exporter)
+    let resource = Resource::builder()
+        .with_service_name(service_name)
+        .with_schema_url([KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION"))], SCHEMA_URL)
         .build();
+
+    let provider =
+        SdkTracerProvider::builder().with_resource(resource).with_batch_exporter(exporter).build();
 
     let tracer = provider.tracer("reth-otlp");
     tracing_opentelemetry::layer().with_tracer(tracer)
-}
-
-fn resource() -> Resource {
-    Resource::builder()
-        .with_service_name(env!("CARGO_PKG_NAME"))
-        .with_schema_url([KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION"))], SCHEMA_URL)
-        .build()
 }
