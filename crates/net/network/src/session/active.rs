2aea84743328334589d560e4fcdb4143291e53f7
stevencartavia <112043913+stevencartavia@users.noreply.github.com>
2025-05-19 03:48:22 -0600

    feat: introduce Receipt69 variant (#15827)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/net/network/src/session/active.rs
+++ crates/net/network/src/session/active.rs
@@ -254,6 +254,11 @@ impl<N: NetworkPrimitives> ActiveSession<N> {
             EthMessage::Receipts(resp) => {
                 on_response!(resp, GetReceipts)
             }
+            EthMessage::Receipts69(resp) => {
+                // TODO: remove mandatory blooms
+                let resp = resp.map(|receipts| receipts.into_with_bloom());
+                on_response!(resp, GetReceipts)
+            }
             EthMessage::Other(bytes) => self.try_emit_broadcast(PeerMessage::Other(bytes)).into(),
         }
     }
