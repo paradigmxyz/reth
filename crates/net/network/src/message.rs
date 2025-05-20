ebd52022e1515814cfec510d78d27ac9c04404c7
Shourya Chaudhry <149008800+18aaddy@users.noreply.github.com>
2025-05-20 16:17:23 +0530

    feat: add BlockRangeUpdate message for eth/69 (#16346)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/net/network/src/message.rs
+++ crates/net/network/src/message.rs
@@ -8,10 +8,10 @@ use alloy_consensus::{BlockHeader, ReceiptWithBloom};
 use alloy_primitives::{Bytes, B256};
 use futures::FutureExt;
 use reth_eth_wire::{
-    message::RequestPair, BlockBodies, BlockHeaders, EthMessage, EthNetworkPrimitives,
-    GetBlockBodies, GetBlockHeaders, NetworkPrimitives, NewBlock, NewBlockHashes,
-    NewPooledTransactionHashes, NodeData, PooledTransactions, Receipts, SharedTransactions,
-    Transactions,
+    message::RequestPair, BlockBodies, BlockHeaders, BlockRangeUpdate, EthMessage,
+    EthNetworkPrimitives, GetBlockBodies, GetBlockHeaders, NetworkPrimitives, NewBlock,
+    NewBlockHashes, NewPooledTransactionHashes, NodeData, PooledTransactions, Receipts,
+    SharedTransactions, Transactions,
 };
 use reth_eth_wire_types::RawCapabilityMessage;
 use reth_network_api::PeerRequest;
@@ -56,6 +56,8 @@ pub enum PeerMessage<N: NetworkPrimitives = EthNetworkPrimitives> {
     PooledTransactions(NewPooledTransactionHashes),
     /// All `eth` request variants.
     EthRequest(PeerRequest<N>),
+    /// Announces when `BlockRange` is updated.
+    BlockRangeUpdated(BlockRangeUpdate),
     /// Any other or manually crafted eth message.
     ///
     /// Caution: It is expected that this is a valid `eth_` capability message.
