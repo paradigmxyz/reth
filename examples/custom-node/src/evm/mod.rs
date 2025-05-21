e20c7118c87316f74b37976d8acc7d6184d50f76
Roman Hodulák <roman.hodulak@polyglot-software.com>
2025-05-21 16:29:39 +0200

    feat(examples): Add `CustomEvm` for the execution of `CustomTransaction` in the `custom_node` example (#16394)

--- examples/custom-node/src/evm/mod.rs
+++ examples/custom-node/src/evm/mod.rs
@@ -1,9 +1,11 @@
+mod alloy;
 mod assembler;
 mod config;
 mod env;
 mod executor;
 
+pub use alloy::{CustomContext, CustomEvm};
 pub use assembler::CustomBlockAssembler;
 pub use config::CustomEvmConfig;
-pub use env::CustomTxEnv;
+pub use env::{CustomEvmTransaction, CustomTxEnv};
 pub use executor::CustomBlockExecutor;
