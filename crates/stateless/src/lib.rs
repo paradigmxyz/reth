3cedf350f0fe471b5e9277f5f5bd7ba4f08dff88
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-05-21 23:35:56 +0100

    rename: ClientInput -> StatelessInput

--- crates/stateless/src/lib.rs
+++ crates/stateless/src/lib.rs
@@ -45,11 +45,11 @@ pub use alloy_rpc_types_debug::ExecutionWitness;
 
 use reth_ethereum_primitives::Block;
 
-/// ClientInput is a convenience structure for serializing the input needed
+/// StatelessInput is a convenience structure for serializing the input needed
 /// for the stateless validation function.
 #[serde_with::serde_as]
 #[derive(Clone, Debug, Default, serde::Serialize, serde::Deserialize)]
-pub struct ClientInput {
+pub struct StatelessInput {
     /// The block being executed in the stateless validation function
     #[serde_as(
         as = "reth_primitives_traits::serde_bincode_compat::Block<reth_ethereum_primitives::TransactionSigned, alloy_consensus::Header>"
