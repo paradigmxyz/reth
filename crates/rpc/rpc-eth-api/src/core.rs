033277afdf619dab965ee1702d75af2376309541
Matthias Seitz <matthias.seitz@outlook.de>
2025-05-24 09:25:50 +0200

    chore: make clippy happy (#16455)
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- crates/rpc/rpc-eth-api/src/core.rs
+++ crates/rpc/rpc-eth-api/src/core.rs
@@ -247,7 +247,7 @@ pub trait EthApi<T: RpcObject, B: RpcObject, R: RpcObject, H: RpcObject> {
     /// It returns list of addresses and storage keys used by the transaction, plus the gas
     /// consumed when the access list is added. That is, it gives you the list of addresses and
     /// storage keys that will be used by that transaction, plus the gas consumed if the access
-    /// list is included. Like eth_estimateGas, this is an estimation; the list could change
+    /// list is included. Like `eth_estimateGas`, this is an estimation; the list could change
     /// when the transaction is actually mined. Adding an accessList to your transaction does
     /// not necessary result in lower gas usage compared to a transaction without an access
     /// list.
