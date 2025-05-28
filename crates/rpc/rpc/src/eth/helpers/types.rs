f1c9e1f6e164f06dbfcaf1f9de441328f94be37e
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-05-28 22:45:34 +0530

    chore: removed otterscan_api_truncate_input function

--- crates/rpc/rpc/src/eth/helpers/types.rs
+++ crates/rpc/rpc/src/eth/helpers/types.rs
@@ -74,11 +74,6 @@ where
         let signature = Signature::new(Default::default(), Default::default(), false);
         Ok(tx.into_signed(signature).into())
     }
-
-    fn otterscan_api_truncate_input(tx: &mut Self::Transaction) {
-        let input = tx.inner.inner_mut().input_mut();
-        *input = input.slice(..4);
-    }
 }
 
 //tests for simulate
