9787b46fecb03b941bdfd6b82247db6c106aac11
Roman Hodulák <roman.hodulak@polyglot-software.com>
2025-06-02 21:00:12 +0200

    fix(stages): Add missing `index.html` suffix to derived URLs from Chain for ERA stage

--- crates/node/core/src/args/era.rs
+++ crates/node/core/src/args/era.rs
@@ -14,10 +14,11 @@ impl TryToUrl for ChainKind {
     fn try_to_url(&self) -> eyre::Result<Url> {
         Ok(match self {
             Self::Named(NamedChain::Mainnet) => {
-                Url::parse("https://era.ithaca.xyz/era1/").expect("URL should be valid")
+                Url::parse("https://era.ithaca.xyz/era1/index.html").expect("URL should be valid")
             }
             Self::Named(NamedChain::Sepolia) => {
-                Url::parse("https://era.ithaca.xyz/sepolia-era1/").expect("URL should be valid")
+                Url::parse("https://era.ithaca.xyz/sepolia-era1/index.html")
+                    .expect("URL should be valid")
             }
             chain => return Err(eyre!("No known host for ERA files on chain {chain:?}")),
         })
