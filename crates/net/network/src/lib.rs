e4fa78f43650554e18d11625209a7fe3f3d16c26
Suyash Nayan <89125422+7suyash7@users.noreply.github.com>
2025-06-04 21:11:46 +0530

    feat(txns): Implement flexible TxType filtering policy in TransactionManager (#16495)
    Signed-off-by: 7suyash7 <suyashnyn1@gmail.com>
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- crates/net/network/src/lib.rs
+++ crates/net/network/src/lib.rs
@@ -168,7 +168,7 @@ pub use manager::NetworkManager;
 pub use metrics::TxTypesCounter;
 pub use network::{NetworkHandle, NetworkProtocols};
 pub use swarm::NetworkConnectionState;
-pub use transactions::{FilterAnnouncement, MessageFilter};
+pub use transactions::MessageFilter;
 
 /// re-export p2p interfaces
 pub use reth_network_p2p as p2p;
--- crates/net/network/src/test_utils/testnet.rs
+++ crates/net/network/src/test_utils/testnet.rs
@@ -6,8 +6,9 @@ use crate::{
     eth_requests::EthRequestHandler,
     protocol::IntoRlpxSubProtocol,
     transactions::{
-        config::TransactionPropagationKind, TransactionsHandle, TransactionsManager,
-        TransactionsManagerConfig,
+        config::{StrictEthAnnouncementFilter, TransactionPropagationKind},
+        policy::NetworkPolicies,
+        TransactionsHandle, TransactionsManager, TransactionsManagerConfig,
     },
     NetworkConfig, NetworkConfigBuilder, NetworkHandle, NetworkManager,
 };
@@ -398,7 +399,13 @@ pub struct Peer<C, Pool = TestPool> {
     #[pin]
     request_handler: Option<EthRequestHandler<C, EthNetworkPrimitives>>,
     #[pin]
-    transactions_manager: Option<TransactionsManager<Pool, EthNetworkPrimitives>>,
+    transactions_manager: Option<
+        TransactionsManager<
+            Pool,
+            EthNetworkPrimitives,
+            NetworkPolicies<TransactionPropagationKind, StrictEthAnnouncementFilter>,
+        >,
+    >,
     pool: Option<Pool>,
     client: C,
     secret_key: SecretKey,
@@ -528,12 +535,15 @@ where
         let (tx, rx) = unbounded_channel();
         network.set_transactions(tx);
 
+        let announcement_policy = StrictEthAnnouncementFilter::default();
+        let policies = NetworkPolicies::new(policy, announcement_policy);
+
         let transactions_manager = TransactionsManager::with_policy(
             network.handle().clone(),
             pool.clone(),
             rx,
             config,
-            policy,
+            policies,
         );
 
         Peer {
