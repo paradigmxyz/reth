b5e09a40c27c27dbb5903ab2f05cc7492614064b
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-06-05 13:54:07 +0530

    fix: add basePath /reth in vocs.config

--- book/vocs/vocs.config.ts
+++ book/vocs/vocs.config.ts
@@ -4,6 +4,7 @@ import { sidebar } from './sidebar'
 export default defineConfig({
   title: 'Reth',
   sidebar,
+  basePath: '/reth',
   vite: {
     // https://vite.dev/guide/static-deploy.html#github-pages
     // Deploying to owner.github.io/reth - base must be set to '/reth
