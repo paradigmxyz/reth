03fcb332bc2113d65b370cf90025c6b0863495e4
Roman Hodulák <roman.hodulak@polyglot-software.com>
2025-06-09 11:12:29 +0200

    feat(rpc): Implement `TransactionCompat` for generic RPC response builder (#16694)

--- crates/rpc/rpc/src/eth/mod.rs
+++ crates/rpc/rpc/src/eth/mod.rs
@@ -15,9 +15,6 @@ pub use core::{EthApi, EthApiFor};
 pub use filter::EthFilter;
 pub use pubsub::EthPubSub;
 
-pub use helpers::{
-    signer::DevSigner,
-    types::{EthTxBuilder, EthereumEthApiTypes},
-};
+pub use helpers::signer::DevSigner;
 
 pub use reth_rpc_eth_api::{EthApiServer, EthApiTypes, FullEthApiServer, RpcNodeCore};
