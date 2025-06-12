dda25be46a5e758dfb60b5833949eda61ddc0393
Luis_ <73004377+Another-DevX@users.noreply.github.com>
2025-06-11 19:07:02 -0500

    feat: make EthEvmConfig generic over chainSpec (#16758)
    Co-authored-by: Arsenii Kulikov <klkvrr@gmail.com>

--- examples/precompile-cache/src/main.rs
+++ examples/precompile-cache/src/main.rs
@@ -166,7 +166,7 @@ impl<Node> ExecutorBuilder<Node> for MyExecutorBuilder
 where
     Node: FullNodeTypes<Types: NodeTypes<ChainSpec = ChainSpec, Primitives = EthPrimitives>>,
 {
-    type EVM = EthEvmConfig<MyEvmFactory>;
+    type EVM = EthEvmConfig<ChainSpec, MyEvmFactory>;
 
     async fn build_evm(self, ctx: &BuilderContext<Node>) -> eyre::Result<Self::EVM> {
         let evm_config = EthEvmConfig::new_with_evm_factory(
