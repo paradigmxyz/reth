d51dba8642a1da9a1e7a9de82b64acc1cd39467e
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-06-20 12:16:18 +0100

    topNav

--- book/vocs/vocs.config.ts
+++ book/vocs/vocs.config.ts
@@ -3,8 +3,59 @@ import { sidebar } from './sidebar'
 
 export default defineConfig({
   title: 'Reth',
+  logoUrl: '/reth/reth-prod.png',
+  iconUrl: '/reth/reth-prod.png',
+  ogImageUrl: '/reth/reth-prod.png',
   sidebar,
   basePath: '/reth',
+  topNav: [
+    { text: 'Run', link: '/run/ethereum' },
+    { text: 'SDK', link: '/sdk/overview' },
+    { text: 'GitHub', link: 'https://github.com/paradigmxyz/reth' },
+    {
+      text: 'v1.4.8',
+      items: [
+        {
+          text: 'Releases',
+          link: 'https://github.com/paradigmxyz/reth/releases'
+        },
+        {
+          text: 'Contributing',
+          link: 'https://github.com/paradigmxyz/reth/blob/main/CONTRIBUTING.md'
+        }
+      ]
+    }
+  ],
+  socials: [
+    {
+      icon: 'github',
+      link: 'https://github.com/paradigmxyz/reth',
+    },
+    {
+      icon: 'telegram',
+      link: 'https://t.me/paradigm_reth',
+    },
+  ],
+  sponsors: [
+    {
+      name: 'Collaborators',
+      height: 120,
+      items: [
+        [
+          {
+            name: 'Paradigm',
+            link: 'https://paradigm.xyz',
+            image: 'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/paradigm-light.svg',
+          },
+          {
+            name: 'Ithaca',
+            link: 'https://ithaca.xyz',
+            image: 'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/ithaca-light.svg',
+          }
+        ]
+      ]
+    }
+  ],
   vite: {
     // https://vite.dev/guide/static-deploy.html#github-pages
     // Deploying to owner.github.io/reth - base must be set to '/reth
