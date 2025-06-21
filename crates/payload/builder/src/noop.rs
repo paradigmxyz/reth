4e7a3b1ffbb7619fea705c5220afb307402b7e8f
Matthias Seitz <matthias.seitz@outlook.de>
2025-06-21 08:40:31 +0200

    touchups

--- crates/payload/builder/src/noop.rs
+++ crates/payload/builder/src/noop.rs
@@ -2,7 +2,6 @@
 
 use crate::{service::PayloadServiceCommand, PayloadBuilderHandle};
 use futures_util::{ready, StreamExt};
-use reth_ethereum_engine_primitives::EthPayloadTypes;
 use reth_payload_primitives::{PayloadBuilderAttributes, PayloadTypes};
 use std::{
     future::Future,
@@ -14,7 +13,7 @@ use tokio_stream::wrappers::UnboundedReceiverStream;
 
 /// A service task that does not build any payloads.
 #[derive(Debug)]
-pub struct NoopPayloadBuilderService<T: PayloadTypes = EthPayloadTypes> {
+pub struct NoopPayloadBuilderService<T: PayloadTypes> {
     /// Receiver half of the command channel.
     command_rx: UnboundedReceiverStream<PayloadServiceCommand<T>>,
 }
@@ -59,7 +58,7 @@ where
     }
 }
 
-impl Default for NoopPayloadBuilderService {
+impl<T: PayloadTypes> Default for NoopPayloadBuilderService<T> {
     fn default() -> Self {
         let (service, _) = Self::new();
         service
