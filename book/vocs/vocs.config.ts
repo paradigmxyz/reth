3b2511cb7619041f33e75d49b07f8889e988c56e
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-06-24 14:45:38 +0530

    pre-merge chores: rm preview workflow, rm /reth basePath

--- book/vocs/vocs.config.ts
+++ book/vocs/vocs.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   iconUrl: '/logo.png',
   ogImageUrl: '/reth-prod.png',
   sidebar,
-  basePath: '/reth',
   topNav: [
     { text: 'Run', link: '/run/ethereum' },
     { text: 'SDK', link: '/sdk/overview' },
@@ -56,11 +55,6 @@ export default defineConfig({
       ]
     }
   ],
-  vite: {
-    // https://vite.dev/guide/static-deploy.html#github-pages
-    // Deploying to owner.github.io/reth - base must be set to '/reth
-    base: '/reth',
-  },
   theme: {
     accentColor: {
       light: '#1f1f1f',
