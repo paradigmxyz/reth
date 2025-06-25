ae8f4e808a6eb8d752ded1196aae22be11e104d1
Aditya Pandey <adityapandey@Adityas-MacBook-Air.local>
2025-06-25 23:35:51 +0530

    refactor: simplified logic by restoring original approach

--- crates/trie/sparse-parallel/src/trie.rs
+++ crates/trie/sparse-parallel/src/trie.rs
@@ -1241,17 +1241,15 @@ mod tests {
             .lower_subtries
             .iter()
             .filter_map(Option::as_ref)
-            .flat_map(|subtrie| subtrie.nodes.iter())
-            .sorted_by_key(|(path, _)| *path);
-        let upper_sparse_nodes = sparse_trie.upper_subtrie.nodes.iter().collect::<Vec<_>>();
+            .flat_map(|subtrie| subtrie.nodes.iter());
 
-        let sparse_nodes = lower_sparse_nodes
-            .into_iter()
-            .chain(upper_sparse_nodes)
-            .sorted_by_key(|(path, _)| *path);
+        let upper_sparse_nodes = sparse_trie.upper_subtrie.nodes.iter();
+
+        let all_sparse_nodes =
+            lower_sparse_nodes.chain(upper_sparse_nodes).sorted_by_key(|(path, _)| *path);
 
         for ((proof_node_path, proof_node), (sparse_node_path, sparse_node)) in
-            proof_nodes.zip(sparse_nodes)
+            proof_nodes.zip(all_sparse_nodes)
         {
             assert_eq!(&proof_node_path, sparse_node_path);
 
