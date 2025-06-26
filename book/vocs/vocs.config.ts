7013c2cb3b96f7596010d121d80f3001cb38f9dc
Matthias Seitz <matthias.seitz@outlook.de>
2025-06-26 14:12:14 +0200

    chore: bump version in docs

--- book/vocs/vocs.config.ts
+++ book/vocs/vocs.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     { text: 'SDK', link: '/sdk/overview' },
     { text: 'GitHub', link: 'https://github.com/paradigmxyz/reth' },
     {
-      text: 'v1.4.8',
+      text: 'v1.5.0',
       items: [
         {
           text: 'Releases',
