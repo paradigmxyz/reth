85154423cbc7c167e3bcda446ef9e30c18100a23
Léa Narzis <78718413+lean-apple@users.noreply.github.com>
2025-07-04 22:46:53 +0200

    fix: fix import in doc code

--- crates/ethereum/primitives/src/receipt.rs
+++ crates/ethereum/primitives/src/receipt.rs
@@ -369,6 +369,7 @@ pub(super) mod serde_bincode_compat {
     ///
     /// Intended to use with the [`serde_with::serde_as`] macro in the following way:
     /// ```rust
+    /// use alloy_consensus::TxType;
     /// use reth_ethereum_primitives::{serde_bincode_compat, Receipt};
     /// use serde::{de::DeserializeOwned, Deserialize, Serialize};
     /// use serde_with::serde_as;
