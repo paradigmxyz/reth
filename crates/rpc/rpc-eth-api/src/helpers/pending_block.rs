7981ccbce2f47b307fdce3f5d9c7825f6ab5c5f3
frankudoags <frankudoags.com>
2025-07-04 23:02:30 +0100

    refactor: use Arc for pending block and receipts to improve memory management as they're often cloned

--- crates/rpc/rpc-eth-api/src/helpers/pending_block.rs
+++ crates/rpc/rpc-eth-api/src/helpers/pending_block.rs
@@ -30,7 +30,10 @@ use reth_transaction_pool::{
     TransactionPool,
 };
 use revm::context_interface::Block;
-use std::time::{Duration, Instant};
+use std::{
+    sync::Arc,
+    time::{Duration, Instant},
+};
 use tokio::sync::Mutex;
 use tracing::debug;
 
@@ -92,7 +95,7 @@ pub trait LoadPendingBlock:
 
                 return Ok(PendingBlockEnv::new(
                     evm_env,
-                    PendingBlockEnvOrigin::ActualPending(block, receipts),
+                    PendingBlockEnvOrigin::ActualPending(Arc::new(block), Arc::new(receipts)),
                 ));
             }
         }
@@ -127,8 +130,8 @@ pub trait LoadPendingBlock:
     ) -> impl Future<
         Output = Result<
             Option<(
-                RecoveredBlock<<Self::Provider as BlockReader>::Block>,
-                Vec<ProviderReceipt<Self::Provider>>,
+                Arc<RecoveredBlock<<Self::Provider as BlockReader>::Block>>,
+                Arc<Vec<ProviderReceipt<Self::Provider>>>,
             )>,
             Self::Error,
         >,
@@ -181,11 +184,11 @@ pub trait LoadPendingBlock:
             let now = Instant::now();
             *lock = Some(PendingBlock::new(
                 now + Duration::from_secs(1),
-                sealed_block.clone(),
-                receipts.clone(),
+                Arc::new(sealed_block.clone()),
+                Arc::new(receipts.clone()),
             ));
 
-            Ok(Some((sealed_block, receipts)))
+            Ok(Some((Arc::new(sealed_block), Arc::new(receipts))))
         }
     }
 
