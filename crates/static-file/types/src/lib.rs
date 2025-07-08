7d2c798ffe401830bcce5d8b9735b718aa96d077
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-07-08 16:35:06 +0300

    Update lib.rs

--- crates/static-file/types/src/lib.rs
+++ crates/static-file/types/src/lib.rs
@@ -112,9 +112,9 @@ impl StaticFileTargets {
         .iter()
         .all(|(target_block_range, highest_static_file_block)| {
             target_block_range.is_none_or(|target_block_range| {
-                *target_block_range.start() ==
-                    highest_static_file_block.map_or(0, |highest_static_file_block| {
-                        highest_static_file_block + 1
+                *target_block_range.start()
+                    == highest_static_file_block
+                        .map_or(0, |highest_static_file_block| highest_static_file_block + 1)
                     })
             })
         })
