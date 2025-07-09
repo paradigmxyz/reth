61608b29e71a799b908cb7bb6306b2f283bb0483
Federico Gimenez <federico.gimenez@gmail.com>
2025-07-09 18:19:58 +0000

    fmt

--- crates/e2e-test-utils/src/testsuite/mod.rs
+++ crates/e2e-test-utils/src/testsuite/mod.rs
@@ -20,7 +20,6 @@ use reth_rpc_builder::auth::AuthServerHandle;
 use std::sync::Arc;
 use url::Url;
 
-
 /// Client handles for both regular RPC and Engine API endpoints
 #[derive(Clone)]
 pub struct NodeClient {
