0b588e25859240166ce6df05658f7249d1b5119e
Alvarez <140459501+prestoalvarez@users.noreply.github.com>
2025-07-12 15:11:44 +0200

    Update mod.rs

--- crates/engine/tree/src/tree/payload_processor/mod.rs
+++ crates/engine/tree/src/tree/payload_processor/mod.rs
@@ -59,7 +59,7 @@ where
     N: NodePrimitives,
     Evm: ConfigureEvm<Primitives = N>,
 {
-    /// The executor used by to spawn tasks.
+    /// The executor used to spawn tasks.
     executor: WorkloadExecutor,
     /// The most recent cache used for execution.
     execution_cache: ExecutionCache,
@@ -397,7 +397,7 @@ where
 
     /// Helper method that handles sparse trie task spawning.
     ///
-    /// If we have a stored trie, we will re-use it for spawning. If we do not have a stored trie,
+    /// If we have a stored trie, we will reuse it for spawning. If we do not have a stored trie,
     /// we will create a new trie based on the configured trie type (parallel or serial).
     fn spawn_sparse_trie_task<BPF>(
         &self,
