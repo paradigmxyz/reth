3ed4af80be1917b3668520e9700233101450b4ee
ongyimeng <ongyimeng@gmail.com>
2025-07-15 21:02:43 +0800

    fix: prevent silent failure in base fee calculation

--- crates/optimism/evm/src/lib.rs
+++ crates/optimism/evm/src/lib.rs
@@ -189,7 +189,7 @@ where
             basefee: self
                 .chain_spec()
                 .next_block_base_fee(&parent, attributes.timestamp)
-                .unwrap_or_default(),
+                .ok_or(EIP1559ParamError::NoEIP1559Params)?,
             // calculate excess gas based on parent block's blob gas usage
             blob_excess_gas_and_price,
         };
