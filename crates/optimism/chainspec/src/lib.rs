3c135fda1717e0a923a1c641919d4928665a4505
ongyimeng <ongyimeng@gmail.com>
2025-07-15 19:20:30 +0800

    refactor: move decode_holocene_base_fee fn into separate basefee.rs

--- crates/optimism/chainspec/src/lib.rs
+++ crates/optimism/chainspec/src/lib.rs
@@ -64,7 +64,7 @@ use alloy_genesis::Genesis;
 use alloy_hardforks::Hardfork;
 use alloy_primitives::{B256, U256};
 use derive_more::{Constructor, Deref, From, Into};
-use op_alloy_consensus::{decode_holocene_extra_data, EIP1559ParamError};
+
 use reth_chainspec::{
     BaseFeeParams, BaseFeeParamsKind, ChainSpec, ChainSpecBuilder, DepositContract,
     DisplayHardforks, EthChainSpec, EthereumHardforks, ForkFilter, ForkId, Hardforks, Head,
@@ -230,28 +230,7 @@ impl OpChainSpec {
     }
 }
 
-/// Extracts the Holocene 1599 parameters from the encoded extra data from the parent header.
-///
-/// Caution: Caller must ensure that holocene is active in the parent header.
-///
-/// See also [Base fee computation](https://github.com/ethereum-optimism/specs/blob/main/specs/protocol/holocene/exec-engine.md#base-fee-computation)
-pub fn decode_holocene_base_fee<H>(
-    chain_spec: impl EthChainSpec + OpHardforks,
-    parent: &H,
-    timestamp: u64,
-) -> Result<u64, EIP1559ParamError>
-where
-    H: BlockHeader,
-{
-    let (elasticity, denominator) = decode_holocene_extra_data(parent.extra_data())?;
-    let base_fee_params = if elasticity == 0 && denominator == 0 {
-        chain_spec.base_fee_params_at_timestamp(timestamp)
-    } else {
-        BaseFeeParams::new(denominator as u128, elasticity as u128)
-    };
 
-    Ok(parent.next_block_base_fee(base_fee_params).unwrap_or_default())
-}
 
 impl EthChainSpec for OpChainSpec {
     type Header = Header;
