efdb990fd9b3643fa70c80bb05218a9f8caa8118
Brian Picciano <me@mediocregopher.com>
2025-07-16 10:19:37 +0200

    clippy

--- crates/trie/sparse/src/trie.rs
+++ crates/trie/sparse/src/trie.rs
@@ -558,7 +558,7 @@ impl SparseTrieInterface for SerialSparseTrie {
 
     fn reveal_nodes(&mut self, mut nodes: Vec<RevealedSparseNode>) -> SparseTrieResult<()> {
         nodes.sort_unstable_by(|a, b| a.path.cmp(&b.path));
-        for node in nodes.into_iter() {
+        for node in nodes {
             self.reveal_node(node.path, node.node, node.masks)?;
         }
         Ok(())
