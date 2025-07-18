c332b0cc72b3b195dbaf713ec32043100f6bd3db
Arsenii Kulikov <klkvrr@gmail.com>
2025-07-18 19:47:56 +0300

    refactor: more strict bounds on RpcNodeCore
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/rpc/rpc-eth-api/src/node.rs
+++ crates/rpc/rpc-eth-api/src/node.rs
@@ -1,12 +1,14 @@
 //! Helper trait for interfacing with [`FullNodeComponents`].
 
-use reth_chainspec::{ChainSpecProvider, EthereumHardforks};
+use reth_chain_state::CanonStateSubscriptions;
+use reth_chainspec::{ChainSpecProvider, EthChainSpec, EthereumHardforks};
 use reth_evm::ConfigureEvm;
+use reth_network_api::NetworkInfo;
 use reth_node_api::{FullNodeComponents, NodePrimitives, PrimitivesTy};
 use reth_primitives_traits::{BlockTy, HeaderTy, ReceiptTy, TxTy};
 use reth_rpc_eth_types::EthStateCache;
 use reth_storage_api::{
-    BlockReader, BlockReaderIdExt, NodePrimitivesProvider, StateProviderFactory,
+    BlockReader, BlockReaderIdExt, StageCheckpointReader, StateProviderFactory,
 };
 use reth_transaction_pool::{PoolTransaction, TransactionPool};
 
@@ -28,18 +30,22 @@ pub trait RpcNodeCore: Clone + Send + Sync + Unpin + 'static {
             Receipt = ReceiptTy<Self::Primitives>,
             Header = HeaderTy<Self::Primitives>,
             Transaction = TxTy<Self::Primitives>,
-        > + NodePrimitivesProvider<Primitives = Self::Primitives>
-        + ChainSpecProvider<ChainSpec: EthereumHardforks>
-        + StateProviderFactory
+        > + ChainSpecProvider<
+            ChainSpec: EthChainSpec<Header = HeaderTy<Self::Primitives>> + EthereumHardforks,
+        > + StateProviderFactory
+        + CanonStateSubscriptions<Primitives = Self::Primitives>
+        + StageCheckpointReader
         + Send
         + Sync
+        + Clone
+        + Unpin
         + 'static;
     /// The transaction pool of the node.
     type Pool: TransactionPool<Transaction: PoolTransaction<Consensus = TxTy<Self::Primitives>>>;
     /// The node's EVM configuration, defining settings for the Ethereum Virtual Machine.
     type Evm: ConfigureEvm<Primitives = Self::Primitives> + Send + Sync + 'static;
     /// Network API.
-    type Network: Send + Sync + Clone;
+    type Network: NetworkInfo + Clone;
 
     /// Returns the transaction pool of the node.
     fn pool(&self) -> &Self::Pool;
@@ -90,4 +96,66 @@ where
 pub trait RpcNodeCoreExt: RpcNodeCore<Provider: BlockReader> {
     /// Returns handle to RPC cache service.
     fn cache(&self) -> &EthStateCache<Self::Primitives>;
+}
+
+/// An adapter that allows to construct [`RpcNodeCore`] from components.
+#[derive(Debug, Clone)]
+pub struct RpcNodeCoreAdapter<Provider, Pool, Network, Evm> {
+    provider: Provider,
+    pool: Pool,
+    network: Network,
+    evm_config: Evm,
+}
+
+impl<Provider, Pool, Network, Evm> RpcNodeCoreAdapter<Provider, Pool, Network, Evm> {
+    /// Creates a new `RpcNodeCoreAdapter` instance.
+    pub const fn new(provider: Provider, pool: Pool, network: Network, evm_config: Evm) -> Self {
+        Self { provider, pool, network, evm_config }
+    }
+}
+
+impl<Provider, Pool, Network, Evm> RpcNodeCore for RpcNodeCoreAdapter<Provider, Pool, Network, Evm>
+where
+    Provider: BlockReaderIdExt<
+            Block = BlockTy<Evm::Primitives>,
+            Receipt = ReceiptTy<Evm::Primitives>,
+            Header = HeaderTy<Evm::Primitives>,
+            Transaction = TxTy<Evm::Primitives>,
+        > + ChainSpecProvider<
+            ChainSpec: EthChainSpec<Header = HeaderTy<Evm::Primitives>> + EthereumHardforks,
+        > + StateProviderFactory
+        + CanonStateSubscriptions<Primitives = Evm::Primitives>
+        + StageCheckpointReader
+        + Send
+        + Sync
+        + Unpin
+        + Clone
+        + 'static,
+    Evm: ConfigureEvm + Clone + 'static,
+    Pool: TransactionPool<Transaction: PoolTransaction<Consensus = TxTy<Evm::Primitives>>>
+        + Unpin
+        + 'static,
+    Network: NetworkInfo + Clone + Unpin + 'static,
+{
+    type Primitives = Evm::Primitives;
+    type Provider = Provider;
+    type Pool = Pool;
+    type Evm = Evm;
+    type Network = Network;
+
+    fn pool(&self) -> &Self::Pool {
+        &self.pool
+    }
+
+    fn evm_config(&self) -> &Self::Evm {
+        &self.evm_config
+    }
+
+    fn network(&self) -> &Self::Network {
+        &self.network
+    }
+
+    fn provider(&self) -> &Self::Provider {
+        &self.provider
+    }
 }
--- crates/rpc/rpc/src/eth/helpers/call.rs
+++ crates/rpc/rpc/src/eth/helpers/call.rs
@@ -1,21 +1,19 @@
 //! Contains RPC handler implementations specific to endpoints that call/execute within evm.
 
 use crate::EthApi;
-use reth_errors::ProviderError;
-use reth_evm::{ConfigureEvm, TxEnvFor};
+use reth_evm::TxEnvFor;
 use reth_rpc_convert::RpcConvert;
 use reth_rpc_eth_api::{
-    helpers::{estimate::EstimateCall, Call, EthCall, LoadPendingBlock, LoadState, SpawnBlocking},
-    FromEvmError, FullEthApiTypes, RpcNodeCore, RpcNodeCoreExt,
+    helpers::{estimate::EstimateCall, Call, EthCall},
+    FromEvmError, RpcNodeCore,
 };
 use reth_rpc_eth_types::EthApiError;
-use reth_storage_api::BlockReader;
 
 impl<N, Rpc> EthCall for EthApi<N, Rpc>
 where
     N: RpcNodeCore,
     EthApiError: FromEvmError<N::Evm>,
-    Rpc: RpcConvert<Primitives = N::Primitives>,
+    Rpc: RpcConvert<Primitives = N::Primitives, Error = EthApiError, TxEnv = TxEnvFor<N::Evm>>,
 {
 }
 
@@ -23,7 +21,7 @@ impl<N, Rpc> Call for EthApi<N, Rpc>
 where
     N: RpcNodeCore,
     EthApiError: FromEvmError<N::Evm>,
-    Rpc: RpcConvert<Primitives = N::Primitives>,
+    Rpc: RpcConvert<Primitives = N::Primitives, Error = EthApiError, TxEnv = TxEnvFor<N::Evm>>,
 {
     #[inline]
     fn call_gas_limit(&self) -> u64 {
@@ -40,6 +38,6 @@ impl<N, Rpc> EstimateCall for EthApi<N, Rpc>
 where
     N: RpcNodeCore,
     EthApiError: FromEvmError<N::Evm>,
-    Rpc: RpcConvert<Primitives = N::Primitives>,
+    Rpc: RpcConvert<Primitives = N::Primitives, Error = EthApiError, TxEnv = TxEnvFor<N::Evm>>,
 {
 }
