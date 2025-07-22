f25237715e494d604067310f4c1fecd884f2354b
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-07-22 12:57:03 +0100

    nix flake update

--- flake.nix
+++ flake.nix
@@ -6,62 +6,88 @@
     };
     utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, utils, ... }@inputs: utils.lib.eachDefaultSystem (system:
-    let
-      # toml info
-      cargoTOML = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
-      packageVersion = cargoTOML.workspace.package.version;
-      rustVersion = cargoTOML.workspace.package.rust-version;
-      rustPkg = pkgs.rust-bin.stable."${rustVersion}".default.override {
-        extensions = [ "rust-src" "rust-analyzer" "clippy" "rustfmt" ];
-      };
-      # platform packages
-      macPackages = pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [ Security CoreFoundation CoreServices ]);
-      linuxPackages = pkgs.lib.optionals pkgs.stdenv.isLinux (with pkgs; [
-        libclang.lib
-        llvmPackages.libcxxClang
-      ]);
-      overlays = [ (import inputs.rust-overlay) ];
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-      cargoDeps = pkgs.rustPlatform.importCargoLock {
-        lockFile = ./Cargo.lock;
-      };
-      rustPlatform = pkgs.makeRustPlatform {
-        rustc = rustPkg;
-        cargo = rustPkg;
-      };
-    in
+  outputs =
     {
-      devShell = pkgs.mkShell {
-        hardeningDisable = [ "fortify" ];
-        buildInputs = with pkgs; [
-          macPackages
-          linuxPackages
-          rustPkg
-          cargoDeps
-        ];
-        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-      };
-
-      defaultPackage = rustPlatform.buildRustPackage {
-        pname = "reth";
-        version = packageVersion;
-        cargoLock = {
+      self,
+      nixpkgs,
+      utils,
+      ...
+    }@inputs:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        # toml info
+        cargoTOML = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
+        packageVersion = cargoTOML.workspace.package.version;
+        rustVersion = cargoTOML.workspace.package.rust-version;
+        rustPkg = pkgs.rust-bin.stable."${rustVersion}".default.override {
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+            "clippy"
+            "rustfmt"
+          ];
+        };
+        # platform packages
+        macPackages = pkgs.lib.optionals pkgs.stdenv.isDarwin (
+          with pkgs.darwin.apple_sdk.frameworks;
+          [
+            Security
+            CoreFoundation
+            CoreServices
+          ]
+        );
+        linuxPackages = pkgs.lib.optionals pkgs.stdenv.isLinux (
+          with pkgs;
+          [
+            libclang.lib
+            llvmPackages.libcxxClang
+          ]
+        );
+        overlays = [ (import inputs.rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        cargoDeps = pkgs.rustPlatform.importCargoLock {
           lockFile = ./Cargo.lock;
         };
-        checkFlags = [
-          #this test breaks Read Only FS sandbox
-          "--skip=cli::tests::parse_env_filter_directives"
-        ];
-        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-        nativeBuildInputs = (with pkgs;[
-          # common packages
-        ]) ++ macPackages ++ linuxPackages;
-        src = ./.;
-      };
-    }
-  );
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = rustPkg;
+          cargo = rustPkg;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          hardeningDisable = [ "fortify" ];
+          buildInputs = with pkgs; [
+            macPackages
+            linuxPackages
+            rustPkg
+            cargoDeps
+          ];
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        };
+
+        defaultPackage = rustPlatform.buildRustPackage {
+          pname = "reth";
+          version = packageVersion;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          checkFlags = [
+            #this test breaks Read Only FS sandbox
+            "--skip=cli::tests::parse_env_filter_directives"
+          ];
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          nativeBuildInputs =
+            (with pkgs; [
+              # common packages
+            ])
+            ++ macPackages
+            ++ linuxPackages;
+          src = ./.;
+        };
+      }
+    );
 }
