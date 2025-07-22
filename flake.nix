2ee0afd5f7f4ab88d781042052beea247f010ef7
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-07-22 23:16:15 +0100

    use rustup for devshell, compile default package in maxperf

--- flake.nix
+++ flake.nix
@@ -9,7 +9,6 @@
   };
   outputs =
     {
-      self,
       nixpkgs,
       utils,
       ...
@@ -17,27 +16,13 @@
     utils.lib.eachDefaultSystem (
       system:
       let
-        # toml info
+        # Rust
         cargoTOML = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
         packageVersion = cargoTOML.workspace.package.version;
         rustVersion = cargoTOML.workspace.package.rust-version;
-        rustPkg = pkgs.rust-bin.stable."${rustVersion}".default.override {
-          extensions = [
-            "rust-src"
-            "rust-analyzer"
-            "clippy"
-            "rustfmt"
-          ];
-        };
-        # platform packages
-        macPackages = pkgs.lib.optionals pkgs.stdenv.isDarwin (
-          with pkgs.darwin.apple_sdk.frameworks;
-          [
-            Security
-            CoreFoundation
-            CoreServices
-          ]
-        );
+        rustPkg = pkgs.rust-bin.stable."${rustVersion}".default;
+
+        # Packages
         linuxPackages = pkgs.lib.optionals pkgs.stdenv.isLinux (
           with pkgs;
           [
@@ -52,6 +37,7 @@
         cargoDeps = pkgs.rustPlatform.importCargoLock {
           lockFile = ./Cargo.lock;
         };
+
         rustPlatform = pkgs.makeRustPlatform {
           rustc = rustPkg;
           cargo = rustPkg;
@@ -59,15 +45,18 @@
       in
       {
         devShell = pkgs.mkShell {
-          hardeningDisable = [ "fortify" ];
           buildInputs = with pkgs; [
-            macPackages
             linuxPackages
-            rustPkg
+            rustup
             cargoDeps
           ];
-          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
+          # We want to be able to use `cargo +nightly`, so we need to install nightly toolchain via `rustup`
+          shellHook = ''
+            ${pkgs.lib.getExe pkgs.rustup} -q toolchain install ${rustVersion} nightly > /dev/null
+            ${pkgs.lib.getExe pkgs.rustup} -q default ${rustVersion} > /dev/null
+            ${pkgs.lib.getExe pkgs.rustup} -q component add --toolchain nightly rust-analyzer clippy rustfmt > /dev/null
+          '';
         };
 
         defaultPackage = rustPlatform.buildRustPackage {
@@ -76,16 +65,18 @@
           cargoLock = {
             lockFile = ./Cargo.lock;
           };
-          checkFlags = [
-            #this test breaks Read Only FS sandbox
-            "--skip=cli::tests::parse_env_filter_directives"
+          RUSTFLAGS = "-C target-cpu=native";
+          buildType = "maxperf";
+          buildFeatures = [
+            "jemalloc"
+            "asm-keccak"
           ];
+          doCheck = false;
           LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
           nativeBuildInputs =
             (with pkgs; [
-              # common packages
+              perl # Required for asm-keccak feature
             ])
-            ++ macPackages
             ++ linuxPackages;
           src = ./.;
         };
