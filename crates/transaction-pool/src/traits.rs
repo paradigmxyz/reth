5aad871128b531d2324d6fece54153f0235b6b4f
0xKitsune <0xKitsune@protonmail.com>
2025-07-29 13:22:32 -0400

    chore: remove unneeded hash trait

--- crates/transaction-pool/src/traits.rs
+++ crates/transaction-pool/src/traits.rs
@@ -737,7 +737,7 @@ pub struct NewBlobSidecar {
 ///
 /// Depending on where the transaction was picked up, it affects how the transaction is handled
 /// internally, e.g. limits for simultaneous transaction of one sender.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Default, Hash)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Default)]
 pub enum TransactionOrigin {
     /// Transaction is coming from a local source.
     #[default]
