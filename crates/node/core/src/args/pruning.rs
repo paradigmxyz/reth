a1ef7c46b5dd65a38365648548865f29111effe8
Matthias Seitz <matthias.seitz@outlook.de>
2025-08-04 20:17:38 +0200

    fix

--- crates/node/core/src/args/pruning.rs
+++ crates/node/core/src/args/pruning.rs
@@ -1,5 +1,6 @@
 //! Pruning and full node arguments
 
+#[allow(rustdoc::broken_intra_doc_links)]
 use crate::{args::error::ReceiptsLogError, primitives::EthereumHardfork};
 use alloy_primitives::{Address, BlockNumber};
 use clap::{builder::RangedU64ValueParser, Args};
