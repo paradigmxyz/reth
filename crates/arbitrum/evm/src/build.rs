09e35302f34befd379ba8b450539c89297e20d25
Til Jordan <til@bloctopus.io>
2025-08-08 04:59:10 +0000

    feat(arbitrum/evm): add ArbNextBlockEnvAttributes and ArbBlockExecutionCtx scaffolds

--- crates/arbitrum/evm/src/build.rs
+++ crates/arbitrum/evm/src/build.rs
@@ -1,10 +1,18 @@
 #![allow(unused)]
 use alloc::sync::Arc;
+use alloy_primitives::{Bytes, B256};
 
 #[derive(Debug, Clone)]
 pub struct ArbBlockExecutorFactory<R, CS> {
     receipt_builder: R,
     spec: Arc<CS>,
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct ArbBlockExecutionCtx {
+    pub parent_hash: B256,
+    pub parent_beacon_block_root: Option<B256>,
+    pub extra_data: Bytes,
 }
 
 impl<R: Clone, CS> ArbBlockExecutorFactory<R, CS> {
