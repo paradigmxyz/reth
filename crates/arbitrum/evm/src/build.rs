0d6ca1d75b7c74baccdad3ee0db076458edc8ee8
Til Jordan <til@bloctopus.io>
2025-08-09 18:26:36 +0000

    arb(evm): introduce ArbTransaction newtype and ArbEvmFactory; wire into block executor/config; satisfy alloy_evm trait bounds

--- crates/arbitrum/evm/src/build.rs
+++ crates/arbitrum/evm/src/build.rs
@@ -2,63 +2,74 @@
 extern crate alloc;
 
 use alloc::{boxed::Box, sync::Arc};
+use core::marker::PhantomData;
 use std::sync::Mutex;
-use alloy_evm::{eth::EthEvmContext, precompiles::PrecompilesMap, EvmFactory};
-use alloy_primitives::{address, Address, Bytes, U256, B256};
-use reth_evm_ethereum::{
-    EthEvm, EthEvmConfig,
-    revm::{
-        context::{Context, TxEnv},
-        handler::EthPrecompiles,
-        inspector::{Inspector, NoOpInspector},
-        interpreter::interpreter::EthInterpreter,
-        precompile::{PrecompileFn, PrecompileOutput, PrecompileResult},
-        primitives::hardfork::SpecId,
-    },
+use alloy_consensus::Transaction;
+use alloy_evm::{eth::EthEvmContext, precompiles::PrecompilesMap};
+use alloy_primitives::{Address, B256};
+use reth_evm_ethereum::{EthEvmConfig};
+use crate::ArbEvmFactory;
+use revm::{
+    context::TxEnv,
+    inspector::Inspector,
+    interpreter::interpreter::EthInterpreter
 };
-use reth_evm::{
-    block::{
-        builder::{BlockAssembler, BlockBuilder},
-        executor::{BasicBlockExecutor, BlockExecutionError},
-    },
-    evm::{ConfigureEvm, EvmEnv, EvmFor, InspectorFor},
-    primitives::{Block as PrimitivesBlock, Database, NodePrimitives, SealedBlock, SealedHeader, State},
-    ContextInterface, ContextProviders, State as _,
-};
+use reth_evm::execute::{BlockAssembler, BlockAssemblerInput};
+use reth_execution_errors::BlockExecutionError;
+use reth_execution_types::BlockExecutionResult as RethBlockExecutionResult;
+use reth_evm::{OnStateHook};
+use alloy_evm::Database;
 use alloy_evm::block::{BlockExecutorFactory, BlockExecutorFor, CommitChanges, ExecutableTx, BlockExecutor as AlloyBlockExecutor};
 use alloy_evm::eth::{EthBlockExecutionCtx, EthBlockExecutor};
-use reth_evm::execute::BlockExecutionResult as RethBlockExecutionResult;
-use reth_evm::OnStateHook;
-use reth_evm_ethereum::revm::context::result::ExecutionResult;
+use revm::context::result::ExecutionResult;
 
-use crate::predeploys::{PredeployCallContext, PredeployRegistry};
-use crate::execute::{DefaultArbOsHooks, ArbTxProcessorState, ArbStartTxContext, ArbGasChargingContext, ArbEndTxContext};
+use crate::predeploys::PredeployRegistry;
+use crate::execute::{DefaultArbOsHooks, ArbTxProcessorState, ArbStartTxContext, ArbGasChargingContext, ArbEndTxContext, ArbOsHooks};
 
-#[derive(Debug, Clone)]
-pub struct ArbBlockExecutorFactory<R, CS> {
+pub struct ArbBlockExecutorFactory<R, CS>
+where
+    R: Clone,
+{
     receipt_builder: R,
     spec: Arc<CS>,
     predeploys: Arc<Mutex<PredeployRegistry>>,
     evm_factory: ArbEvmFactory,
 }
 
+impl<R: Clone, CS> Clone for ArbBlockExecutorFactory<R, CS> {
+    fn clone(&self) -> Self {
+        Self {
+            receipt_builder: self.receipt_builder.clone(),
+            spec: self.spec.clone(),
+            predeploys: self.predeploys.clone(),
+            evm_factory: self.evm_factory.clone(),
+        }
+    }
+}
+impl<R: Clone, CS> core::fmt::Debug for ArbBlockExecutorFactory<R, CS> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("ArbBlockExecutorFactory").finish()
+    }
+}
+
 #[derive(Debug, Clone, Default)]
 pub struct ArbBlockExecutionCtx {
     pub parent_hash: B256,
     pub parent_beacon_block_root: Option<B256>,
-    pub extra_data: Bytes,
+    pub extra_data: alloy_primitives::bytes::Bytes,
 }
 
 pub struct ArbBlockExecutor<'a, Evm, CS, RB: alloy_evm::eth::receipt_builder::ReceiptBuilder> {
-    inner: EthBlockExecutor<'a, Evm, &'a Arc<CS>, &'a RB>,
+    inner: EthBlockExecutor<'a, Evm, alloy_evm::eth::spec::EthSpec, &'a RB>,
     hooks: DefaultArbOsHooks,
     tx_state: ArbTxProcessorState,
+    _phantom: PhantomData<CS>,
 }
 
 impl<R: Clone, CS> ArbBlockExecutorFactory<R, CS> {
     pub fn new(receipt_builder: R, spec: Arc<CS>) -> Self {
         let predeploys = Arc::new(Mutex::new(PredeployRegistry::with_default_addresses()));
-        let evm_factory = ArbEvmFactory { predeploys: predeploys.clone() };
+        let evm_factory = ArbEvmFactory::default();
         Self { receipt_builder, spec, predeploys, evm_factory }
     }
 
@@ -67,15 +78,16 @@ impl<R: Clone, CS> ArbBlockExecutorFactory<R, CS> {
     }
 
     pub fn evm_factory(&self) -> ArbEvmFactory {
-        ArbEvmFactory { predeploys: self.predeploys.clone() }
+        ArbEvmFactory::default()
     }
 }
 
-impl<'db, DB, E, CS, RB> AlloyBlockExecutor for ArbBlockExecutor<'_, E, CS, RB>
+impl<E, CS, RB> AlloyBlockExecutor for ArbBlockExecutor<'_, E, CS, RB>
 where
-    RB: alloy_evm::eth::receipt_builder::ReceiptBuilder,
-    DB: Database + 'db,
-    E: reth_evm::Evm<DB = &'db mut State<DB>, Tx = TxEnv>,
+    RB: alloy_evm::eth::receipt_builder::ReceiptBuilder<Transaction = reth_arbitrum_primitives::ArbTransactionSigned, Receipt = reth_arbitrum_primitives::ArbReceipt>,
+    E: reth_evm::Evm,
+    E::Tx: alloy_evm::tx::FromRecoveredTx<reth_arbitrum_primitives::ArbTransactionSigned> + alloy_evm::tx::FromTxWithEncoded<reth_arbitrum_primitives::ArbTransactionSigned>,
+    for<'a> alloy_evm::eth::EthBlockExecutor<'a, E, alloy_evm::eth::spec::EthSpec, &'a RB>: alloy_evm::block::BlockExecutor<Transaction = reth_arbitrum_primitives::ArbTransactionSigned, Receipt = reth_arbitrum_primitives::ArbReceipt, Evm = E>,
 {
     type Transaction = reth_arbitrum_primitives::ArbTransactionSigned;
     type Receipt = reth_arbitrum_primitives::ArbReceipt;
@@ -97,7 +109,7 @@ where
         let gas_limit = tx.tx().gas_limit();
 
         let block_env = self.evm().block();
-        let block_basefee = block_env.basefee;
+        let block_basefee = alloy_primitives::U256::from(block_env.basefee);
         let block_coinbase = block_env.beneficiary;
 
         let start_ctx = ArbStartTxContext {
@@ -109,16 +121,31 @@ where
             executed_on_chain: true,
             is_eth_call: false,
         };
-        self.hooks.start_tx(self.evm_mut(), &mut self.tx_state, &start_ctx);
+        {
+            let mut state = core::mem::take(&mut self.tx_state);
+            {
+                let evm = self.inner.evm_mut();
+                self.hooks.start_tx::<E>(evm, &mut state, &start_ctx);
+            }
+            self.tx_state = state;
+        }
 
         let gas_ctx = ArbGasChargingContext {
             intrinsic_gas: 21_000,
-            calldata,
+            calldata: calldata.to_vec(),
             basefee: block_basefee,
             is_executed_on_chain: true,
             skip_l1_charging: false,
         };
-        let _ = self.hooks.gas_charging(self.evm_mut(), &mut self.tx_state, &gas_ctx);
+        {
+            let mut state = core::mem::take(&mut self.tx_state);
+            let res = {
+                let evm = self.inner.evm_mut();
+                self.hooks.gas_charging::<E>(evm, &mut state, &gas_ctx)
+            };
+            self.tx_state = state;
+            let _ = res;
+        }
 
         let res = self.inner.execute_transaction_with_commit_condition(tx, f);
 
@@ -128,7 +155,14 @@ where
             gas_limit,
             basefee: block_basefee,
         };
-        self.hooks.end_tx(self.evm_mut(), &mut self.tx_state, &end_ctx);
+        {
+            let mut state = core::mem::take(&mut self.tx_state);
+            {
+                let evm = self.inner.evm_mut();
+                self.hooks.end_tx::<E>(evm, &mut state, &end_ctx);
+            }
+            self.tx_state = state;
+        }
 
         res
     }
@@ -149,92 +183,12 @@ where
         self.inner.evm()
     }
 }
- 
-#[derive(Debug, Clone, Default)]
-pub struct ArbEvmFactory {
-    predeploys: Arc<Mutex<PredeployRegistry>>,
-}
 
-impl EvmFactory for ArbEvmFactory {
-    type Evm<DB: Database, I: Inspector<EthEvmContext<DB>, EthInterpreter>> =
-        EthEvm<DB, I, PrecompilesMap>;
-    type Tx = TxEnv;
-    type Error<DBError: core::error::Error + Send + Sync + 'static> =
-        reth_evm_ethereum::revm::context_interface::result::EVMError<DBError>;
-    type HaltReason = reth_evm_ethereum::revm::context_interface::result::HaltReason;
-    type Context<DB: Database> = EthEvmContext<DB>;
-    type Spec = SpecId;
-    type Precompiles = PrecompilesMap;
-
-    fn create_evm<DB: Database>(&self, db: DB, input: EvmEnv) -> Self::Evm<DB, NoOpInspector> {
-        let mut evm = Context::mainnet()
-            .with_db(db)
-            .with_cfg(input.cfg_env)
-            .with_block(input.block_env)
-            .build_mainnet_with_inspector(NoOpInspector {})
-            .with_precompiles(PrecompilesMap::from_static(EthPrecompiles::default().precompiles));
-
-        let mut custom = PrecompilesMap::default();
-        let reg = self.predeploys.clone();
-
-        fn mk_handler(
-            reg: Arc<Mutex<PredeployRegistry>>,
-            addr: Address,
-        ) -> (Address, PrecompileFn) {
-            let f = move |input: &[u8], ctx: &mut EthEvmContext<_>| -> PrecompileResult {
-                let gas_limit = ctx.env.tx().gas_limit();
-                let value = U256::from(ctx.env.tx().caller_value());
-                let block = &ctx.env.block;
-                let cfg = &ctx.env.cfg;
-
-                let call_ctx = PredeployCallContext {
-                    block_number: block.number.to(),
-                    block_hashes: alloc::vec::Vec::new(),
-                    chain_id: U256::from(cfg.chain_id),
-                    os_version: 0,
-                    time: block.timestamp.to(),
-                    origin: ctx.env.tx().caller(),
-                    caller: ctx.env.tx().caller(),
-                    depth: ctx.depth as u64,
-                    basefee: block.basefee,
-                };
-                let bytes = Bytes::copy_from_slice(input);
-                let (ret, gas_left, success) = {
-                    let mut guard = reg.lock().expect("lock predeploy registry");
-                    guard.dispatch(&call_ctx, addr, &bytes, gas_limit, value).unwrap_or_default()
-                };
-                let out = PrecompileOutput::new(gas_left, ret);
-                if success {
-                    PrecompileResult::Ok(out)
-                } else {
-                    PrecompileResult::Error { exit_status: Default::default(), output: out }
-                }
-            };
-            (addr, f as PrecompileFn)
-        }
-
-        let sys = address!("0000000000000000000000000000000000000064");
-        let retry = address!("000000000000000000000000000000000000006e");
-        let owner = address!("0000000000000000000000000000000000000070");
-        let atab = address!("0000000000000000000000000000000000000066");
-
-        custom.extend([mk_handler(reg.clone(), sys), mk_handler(reg.clone(), retry), mk_handler(reg.clone(), owner), mk_handler(reg, atab)]);
-
-        evm = evm.with_precompiles(custom);
-        EthEvm::new(evm, false)
-    }
-
-    fn create_evm_with_inspector<DB: Database, I: Inspector<Self::Context<DB>, EthInterpreter>>(
-        &self,
-        db: DB,
-        input: EvmEnv,
-        inspector: I,
-    ) -> Self::Evm<DB, I> {
-        EthEvm::new(self.create_evm(db, input).into_inner().with_inspector(inspector), true)
-    }
-}
-
-impl<R: Clone, CS> BlockExecutorFactory for ArbBlockExecutorFactory<R, CS> {
+impl<R, CS> BlockExecutorFactory for ArbBlockExecutorFactory<R, CS>
+where
+    R: Clone + 'static + alloy_evm::eth::receipt_builder::ReceiptBuilder<Transaction = reth_arbitrum_primitives::ArbTransactionSigned, Receipt = reth_arbitrum_primitives::ArbReceipt>,
+    CS: 'static,
+{
     type EvmFactory = ArbEvmFactory;
     type ExecutionCtx<'a> = ArbBlockExecutionCtx;
     type Transaction = reth_arbitrum_primitives::ArbTransactionSigned;
@@ -246,28 +200,30 @@ impl<R: Clone, CS> BlockExecutorFactory for ArbBlockExecutorFactory<R, CS> {
 
     fn create_executor<'a, DB, I>(
         &'a self,
-        evm: EthEvm<&'a mut State<DB>, I, PrecompilesMap>,
+        evm: <Self::EvmFactory as reth_evm::EvmFactory>::Evm<&'a mut revm::database::State<DB>, I>,
         ctx: ArbBlockExecutionCtx,
     ) -> impl BlockExecutorFor<'a, Self, DB, I>
     where
-        DB: Database + 'a,
-        I: InspectorFor<Self, &'a mut State<DB>> + 'a,
+        DB: Database + 'a + core::fmt::Debug,
+        I: revm::inspector::Inspector<<Self::EvmFactory as reth_evm::EvmFactory>::Context<&'a mut revm::database::State<DB>>> + 'a,
+        <DB as revm::Database>::Error: Send + Sync,
     {
         let eth_ctx: EthBlockExecutionCtx<'a> = EthBlockExecutionCtx {
             parent_hash: ctx.parent_hash,
             parent_beacon_block_root: ctx.parent_beacon_block_root,
-            extra_data: ctx.extra_data,
-            ..Default::default()
+            ommers: Default::default(),
+            withdrawals: None,
         };
         ArbBlockExecutor {
             inner: EthBlockExecutor::new(
                 evm,
                 eth_ctx,
-                &self.spec,
+                alloy_evm::eth::spec::EthSpec::mainnet(),
                 &self.receipt_builder,
             ),
             hooks: Default::default(),
             tx_state: Default::default(),
+            _phantom: core::marker::PhantomData::<CS>,
         }
     }
 }
