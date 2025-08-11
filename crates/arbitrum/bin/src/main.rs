f3cda9e273cea320688533c45972c552f82d2ee1
Til Jordan <til@bloctopus.io>
2025-08-11 19:07:16 +0000

    arb-reth: wire ArbNode CLI, ArbEthApi using ArbNetwork RpcTypes; fix EngineValidatorAddOn default

--- crates/arbitrum/bin/src/main.rs
+++ crates/arbitrum/bin/src/main.rs
@@ -1,15 +1,9 @@
 #![allow(missing_docs, rustdoc::missing_crate_level_docs)]
 
 use clap::Parser;
-use reth_cli_runner::CliRunner;
-use reth_cli_commands::launcher::FnLauncher;
-use reth_cli::Cli as EthCli;
-use reth_node_builder::{NodeBuilder, WithLaunchContext};
-use reth_db::DatabaseEnv;
-use std::sync::Arc;
-use tracing::info;
-
+use reth_ethereum_cli::{chainspec::EthereumChainSpecParser, Cli};
 use reth_arbitrum_node::{args::RollupArgs, ArbNode};
+use tracing::info;
 
 #[global_allocator]
 static ALLOC: reth_cli_util::allocator::Allocator = reth_cli_util::allocator::new_allocator();
@@ -21,14 +15,13 @@ fn main() {
         std::env::set_var("RUST_BACKTRACE", "1");
     }
 
-    if let Err(err) = EthCli::<reth_cli::chainspec::ChainSpecParser, RollupArgs>::parse().with_runner(
-        CliRunner::try_default_runtime().expect("runtime"),
-        |builder: WithLaunchContext<NodeBuilder<Arc<DatabaseEnv>, reth_chainspec::ChainSpec>>, rollup_args: RollupArgs| async move {
+    if let Err(err) =
+        Cli::<EthereumChainSpecParser, RollupArgs>::parse().run(async move |builder, rollup_args| {
             info!(target: "reth::cli", "Launching arb-reth node");
-            let handle = builder.node(ArbNode::new(rollup_args)).launch_with_debug_capabilities().await?;
+            let handle = builder.node(ArbNode::new(rollup_args)).launch().await?;
             handle.node_exit_future.await
-        },
-    ) {
+        })
+    {
         eprintln!("Error: {err:?}");
         std::process::exit(1);
     }
--- crates/arbitrum/node/src/node.rs
+++ crates/arbitrum/node/src/node.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
-use jsonrpsee_core::server::RpcModule;
 use reth_arbitrum_rpc::ArbNitroRpc;
+use reth_arbitrum_rpc::ArbNitroApiServer;
 
 use super::args::RollupArgs;
 
@@ -13,19 +13,16 @@ impl ArbNode {
     pub fn new(rollup_args: RollupArgs) -> Self {
         Self { args: rollup_args }
     }
-
-    pub fn arb_rpc_module() -> RpcModule<()> {
-        ArbNitroRpc::default().into_rpc_module()
-    }
 }
 use std::sync::Arc;
 
 use reth_chainspec::ChainSpec;
-use reth_node_api::{DebugNode, Node, NodeTypes, PayloadAttributesBuilder};
+use reth_node_api::{FullNodeComponents, NodeTypes, PayloadAttributesBuilder};
+use reth_node_builder::{DebugNode, Node};
 use reth_node_builder::{
     components::{
         BasicPayloadServiceBuilder, ComponentsBuilder, ConsensusBuilder, ExecutorBuilder,
-        NetworkBuilder, PoolBuilder,
+        NetworkBuilder, PoolBuilder, NoopPayloadBuilder,
     },
     node::{FullNodeTypes, NodeTypes as _},
     rpc::{
@@ -36,7 +33,8 @@ use reth_node_builder::{
     BuilderContext, DebugNode as _, NodeAdapter,
 };
 use reth_payload_primitives::PayloadTypes;
-use reth_provider::{providers::ProviderFactoryBuilder, EthStorage};
+use reth_provider::providers::ProviderFactoryBuilder;
+use reth_arbitrum_storage::ArbStorage;
 use reth_rpc_api::servers::DebugApiServer;
 use reth_rpc_server_types::RethRpcModule;
 use reth_trie_db::MerklePatriciaTrie;
@@ -49,8 +47,8 @@ impl NodeTypes for ArbNode {
     type Primitives = reth_arbitrum_primitives::ArbPrimitives;
     type ChainSpec = ChainSpec;
     type StateCommitment = MerklePatriciaTrie;
-    type Storage = EthStorage;
-    type Payload = crate::engine::ArbEngineTypes<reth_payload_builder::EthPayloadTypes>;
+    type Storage = ArbStorage;
+    type Payload = crate::engine::ArbEngineTypes<reth_arbitrum_payload::ArbPayloadTypes>;
 }
 
 #[derive(Debug, Default, Clone, Copy)]
@@ -61,7 +59,7 @@ where
     Types: NodeTypes<ChainSpec = ChainSpec, Primitives = reth_arbitrum_primitives::ArbPrimitives>,
     N: FullNodeTypes<Types = Types>,
 {
-    type EVM = ArbEvmConfig<ChainSpec, Types::Primitives>;
+    type EVM = ArbEvmConfig<Arc<ChainSpec>, Types::Primitives>;
 
     async fn build_evm(self, ctx: &BuilderContext<N>) -> eyre::Result<Self::EVM> {
         let evm_config =
@@ -75,37 +73,13 @@ pub struct ArbPoolBuilder;
 
 impl<Types, N> PoolBuilder<N> for ArbPoolBuilder
 where
-    Types: NodeTypes<ChainSpec = ChainSpec>,
+    Types: NodeTypes<ChainSpec = ChainSpec, Primitives = reth_arbitrum_primitives::ArbPrimitives>,
     N: FullNodeTypes<Types = Types>,
 {
-    type Pool = reth_transaction_pool::EthTransactionPool<
-        N::Provider,
-        reth_transaction_pool::blobstore::DiskFileBlobStore,
-    >;
+    type Pool = reth_transaction_pool::noop::NoopTransactionPool<reth_arbitrum_txpool::ArbPooledTransaction>;
 
-    async fn build_pool(self, ctx: &BuilderContext<N>) -> eyre::Result<Self::Pool> {
-        let blob_store =
-            reth_node_builder::components::create_blob_store_with_cache(ctx, None)?;
-        let validator =
-            reth_transaction_pool::TransactionValidationTaskExecutor::eth_builder(
-                ctx.provider().clone(),
-            )
-            .with_head_timestamp(ctx.head().timestamp)
-            .with_max_tx_input_bytes(ctx.config().txpool.max_tx_input_bytes)
-            .kzg_settings(ctx.kzg_settings()?)
-            .with_local_transactions_config(
-                ctx.pool_config().local_transactions_config.clone(),
-            )
-            .set_tx_fee_cap(ctx.config().rpc.rpc_tx_fee_cap)
-            .with_max_tx_gas_limit(ctx.config().txpool.max_tx_gas_limit)
-            .with_minimum_priority_fee(ctx.config().txpool.minimum_priority_fee)
-            .with_additional_tasks(ctx.config().txpool.additional_validation_tasks)
-            .build_with_tasks(ctx.task_executor().clone(), blob_store.clone());
-
-        let pool = reth_node_builder::components::TxPoolBuilder::new(ctx)
-            .with_validator(validator)
-            .build_and_spawn_maintenance_task(blob_store, ctx.pool_config())?;
-        Ok(pool)
+    async fn build_pool(self, _ctx: &BuilderContext<N>) -> eyre::Result<Self::Pool> {
+        Ok(reth_transaction_pool::noop::NoopTransactionPool::new())
     }
 }
 
@@ -115,7 +89,11 @@ pub struct ArbNetworkBuilder;
 impl<N, Pool> NetworkBuilder<N, Pool> for ArbNetworkBuilder
 where
     N: FullNodeTypes<Types: NodeTypes<ChainSpec = ChainSpec>>,
-    Pool: reth_transaction_pool::TransactionPool + Unpin + 'static,
+    Pool: reth_transaction_pool::TransactionPool<
+            Transaction: reth_transaction_pool::PoolTransaction<
+                Consensus = reth_node_types::TxTy<N::Types>
+            >
+        > + Unpin + 'static,
 {
     type Network = reth_network::NetworkHandle<
         reth_network::primitives::BasicNetworkPrimitives<
@@ -140,7 +118,7 @@ pub struct ArbConsensusBuilder;
 
 impl<N> ConsensusBuilder<N> for ArbConsensusBuilder
 where
-    N: FullNodeTypes<Types: NodeTypes<ChainSpec = ChainSpec>>,
+    N: FullNodeTypes<Types: NodeTypes<ChainSpec = ChainSpec, Primitives = reth_arbitrum_primitives::ArbPrimitives>>,
 {
     type Consensus =
         std::sync::Arc<reth_ethereum_consensus::EthBeaconConsensus<ChainSpec>>;
@@ -155,7 +133,7 @@ where
 pub type ArbNodeComponents<N> = ComponentsBuilder<
     N,
     ArbPoolBuilder,
-    BasicPayloadServiceBuilder<reth_ethereum_node::EthereumPayloadBuilder>,
+    reth_node_builder::components::NoopPayloadServiceBuilder,
     ArbNetworkBuilder,
     ArbExecutorBuilder,
     ArbConsensusBuilder,
@@ -164,10 +142,10 @@ pub type ArbNodeComponents<N> = ComponentsBuilder<
 #[derive(Debug)]
 pub struct ArbAddOns<
     N: FullNodeComponents,
-    EthB: EthApiBuilder<N> = reth_ethereum_node::EthereumEthApiBuilder,
+    EthB: EthApiBuilder<N> = reth_arbitrum_rpc::ArbEthApiBuilder,
     PVB = (),
     EB = ArbEngineApiBuilder<PVB>,
-    EVB = BasicEngineValidatorBuilder<PVB>,
+    EVB = BasicEngineValidatorBuilder<crate::validator::ArbPayloadValidatorBuilder>,
     RpcM = Identity,
 > {
     pub rpc_add_ons: RpcAddOns<N, EthB, PVB, EB, EVB, RpcM>,
@@ -176,7 +154,7 @@ pub struct ArbAddOns<
 impl<N> Default for ArbAddOns<N>
 where
     N: FullNodeComponents,
-    reth_ethereum_node::EthereumEthApiBuilder: EthApiBuilder<N>,
+    reth_arbitrum_rpc::ArbEthApiBuilder: EthApiBuilder<N>,
 {
     fn default() -> Self {
         Self { rpc_add_ons: RpcAddOns::default() }
@@ -214,16 +192,15 @@ where
     }
 }
 
-impl<N, EthB, PVB, EB, EVB, Attrs, RpcM> reth_node_api::NodeAddOns<N>
+impl<N, EthB, PVB, EB, EVB, RpcM> reth_node_api::NodeAddOns<N>
     for ArbAddOns<N, EthB, PVB, EB, EVB, RpcM>
 where
-    N: FullNodeComponents,
+    N: FullNodeComponents<Types: NodeTypes<ChainSpec: reth_chainspec::EthereumHardforks>>,
     EthB: EthApiBuilder<N>,
     PVB: Send,
     EB: EngineApiBuilder<N>,
     EVB: EngineValidatorBuilder<N>,
     RpcM: RethRpcMiddleware,
-    Attrs: serde::de::DeserializeOwned,
 {
     type Handle = RpcHandle<N, EthB::EthApi>;
 
@@ -236,7 +213,7 @@ where
             .launch_add_ons_with(ctx, move |container| {
                 let reth_node_builder::rpc::RpcModuleContainer { modules, .. } = container;
                 let arb_rpc = ArbNitroRpc::default();
-                modules.merge(arb_rpc.into_rpc())?;
+                modules.merge_configured(arb_rpc.into_rpc())?;
                 Ok(())
             })
             .await
@@ -246,7 +223,7 @@ where
 impl<N, EthB, PVB, EB, EVB, RpcM> RethRpcAddOns<N>
     for ArbAddOns<N, EthB, PVB, EB, EVB, RpcM>
 where
-    N: FullNodeComponents,
+    N: FullNodeComponents<Types: NodeTypes<ChainSpec: reth_chainspec::EthereumHardforks>>,
     EthB: EthApiBuilder<N>,
     PVB: Send,
     EB: EngineApiBuilder<N>,
@@ -256,9 +233,26 @@ where
     type EthApi = EthB::EthApi;
 
     fn hooks_mut(&mut self) -> &mut RpcHooks<N, Self::EthApi> {
-        self.rpc_add_ons.hooks_mut()
+        &mut self.rpc_add_ons.hooks
     }
 }
+impl<N, EthB, PVB, EB, EVB, RpcM> reth_node_builder::rpc::EngineValidatorAddOn<N>
+    for ArbAddOns<N, EthB, PVB, EB, EVB, RpcM>
+where
+    N: FullNodeComponents,
+    EthB: EthApiBuilder<N>,
+    PVB: Send,
+    EB: EngineApiBuilder<N>,
+    EVB: EngineValidatorBuilder<N> + Default,
+    RpcM: RethRpcMiddleware,
+{
+    type ValidatorBuilder = EVB;
+
+    fn engine_validator_builder(&self) -> Self::ValidatorBuilder {
+        EVB::default()
+    }
+}
+
 
 impl<N> Node<N> for ArbNode
 where
@@ -271,27 +265,27 @@ where
             N,
             <Self::ComponentsBuilder as reth_node_builder::components::NodeComponentsBuilder<N>>::Components,
         >,
-        reth_ethereum_node::EthereumEthApiBuilder,
+        reth_arbitrum_rpc::ArbEthApiBuilder,
         (),
-        ArbEngineApiBuilder<BasicEngineValidatorBuilder<()>>,
-        BasicEngineValidatorBuilder<()>,
+        ArbEngineApiBuilder<crate::validator::ArbPayloadValidatorBuilder>,
+        BasicEngineValidatorBuilder<crate::validator::ArbPayloadValidatorBuilder>,
         Identity,
     >;
 
-    fn components_builder(&self) -> Self::ComponentsBuilder {
+    fn components_builder(&self) -> <Self as Node<N>>::ComponentsBuilder {
         ComponentsBuilder::default()
             .node_types::<N>()
             .pool(ArbPoolBuilder::default())
             .executor(ArbExecutorBuilder::default())
-            .payload(BasicPayloadServiceBuilder::default())
+            .payload(reth_node_builder::components::NoopPayloadServiceBuilder::default())
             .network(ArbNetworkBuilder::default())
             .consensus(ArbConsensusBuilder::default())
     }
 
-    fn add_ons(&self) -> Self::AddOns {
-        let engine_api = ArbEngineApiBuilder {
-            engine_validator_builder: BasicEngineValidatorBuilder::default(),
-        };
+    fn add_ons(&self) -> <Self as Node<N>>::AddOns {
+        let engine_api = ArbEngineApiBuilder::new(
+            crate::validator::ArbPayloadValidatorBuilder::default(),
+        );
         ArbAddOns::default().with_engine_api(engine_api)
     }
 }
@@ -300,16 +294,16 @@ impl<N> DebugNode<N> for ArbNode
 where
     N: FullNodeComponents<Types = Self>,
 {
-    type RpcBlock = alloy_rpc_types_eth::Block;
+    type RpcBlock = alloy_rpc_types_eth::Block<reth_arbitrum_primitives::ArbTransactionSigned>;
 
     fn rpc_to_primitive_block(
-        rpc_block: Self::RpcBlock,
+        rpc_block: <Self as DebugNode<N>>::RpcBlock,
     ) -> reth_node_api::BlockTy<Self> {
         rpc_block.into_consensus()
     }
 
     fn local_payload_attributes_builder(
-        chain_spec: &Self::ChainSpec,
+        chain_spec: &<Self as reth_node_api::NodeTypes>::ChainSpec,
     ) -> impl PayloadAttributesBuilder<
         <Self::Payload as PayloadTypes>::PayloadAttributes,
     > {
