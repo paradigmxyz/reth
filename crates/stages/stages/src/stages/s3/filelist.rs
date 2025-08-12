6e691c0f38be6cd740b11091e08f69ec4e981b38
Mablr <59505383+mablr@users.noreply.github.com>
2025-08-12 20:29:04 +0200

    chore: Remove `BlockMeta` variants and unused code (#17835)

--- crates/stages/stages/src/stages/s3/filelist.rs
+++ crates/stages/stages/src/stages/s3/filelist.rs
@@ -6,16 +6,10 @@ pub(crate) static DOWNLOAD_FILE_LIST: [[(&str, B256); 3]; 2] = [
         ("static_file_transactions_0_499999", B256::ZERO),
         ("static_file_transactions_0_499999.off", B256::ZERO),
         ("static_file_transactions_0_499999.conf", B256::ZERO),
-        // ("static_file_blockmeta_0_499999", B256::ZERO),
-        // ("static_file_blockmeta_0_499999.off", B256::ZERO),
-        // ("static_file_blockmeta_0_499999.conf", B256::ZERO),
     ],
     [
         ("static_file_transactions_500000_999999", B256::ZERO),
         ("static_file_transactions_500000_999999.off", B256::ZERO),
         ("static_file_transactions_500000_999999.conf", B256::ZERO),
-        // ("static_file_blockmeta_500000_999999", B256::ZERO),
-        // ("static_file_blockmeta_500000_999999.off", B256::ZERO),
-        // ("static_file_blockmeta_500000_999999.conf", B256::ZERO),
     ],
 ];
