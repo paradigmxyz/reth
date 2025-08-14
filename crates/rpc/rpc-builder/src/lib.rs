907448ff3b0935bdeb1fdc11eb79f37309e0a219
Mablr <59505383+mablr@users.noreply.github.com>
2025-08-14 07:40:29 +0200

    feat(rpc): Add support for custom Tokio runtime configuration in EthereumAddOns (#17693)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/rpc/rpc-builder/src/lib.rs
+++ crates/rpc/rpc-builder/src/lib.rs
@@ -1192,7 +1192,8 @@ impl<RpcMiddleware> RpcServerConfig<RpcMiddleware> {
     }
 
     /// Configures a custom tokio runtime for the rpc server.
-    pub fn with_tokio_runtime(mut self, tokio_runtime: tokio::runtime::Handle) -> Self {
+    pub fn with_tokio_runtime(mut self, tokio_runtime: Option<tokio::runtime::Handle>) -> Self {
+        let Some(tokio_runtime) = tokio_runtime else { return self };
         if let Some(http_server_config) = self.http_server_config {
             self.http_server_config =
                 Some(http_server_config.custom_tokio_runtime(tokio_runtime.clone()));
