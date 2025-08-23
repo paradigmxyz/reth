b78f7eedfbf322257d5bef34e32d7ef02971384e
Til Jordan <til@bloctopus.io>
2025-08-23 21:07:07 +0000

    arbitrum-rpc: remove duplicate impls in eth/block.rs introduced in previous commit

--- crates/arbitrum/rpc/src/eth/block.rs
+++ crates/arbitrum/rpc/src/eth/block.rs
@@ -72,27 +72,3 @@ where
         }
     }
 }
-
-use crate::eth::ArbEthApi;
-use reth_rpc_eth_api::{
-    helpers::{EthBlocks, LoadBlock},
-    FromEvmError, RpcConvert, RpcNodeCore,
-};
-use reth_storage_api::HeaderProvider;
-use crate::error::ArbEthApiError;
-
-impl<N, Rpc> EthBlocks for ArbEthApi<N, Rpc>
-where
-    N: RpcNodeCore,
-    ArbEthApiError: FromEvmError<N::Evm>,
-    Rpc: RpcConvert<Primitives = N::Primitives, Error = ArbEthApiError>,
-{
-}
-
-impl<N, Rpc> LoadBlock for ArbEthApi<N, Rpc>
-where
-    N: RpcNodeCore<Provider: HeaderProvider>,
-    ArbEthApiError: FromEvmError<N::Evm>,
-    Rpc: RpcConvert<Primitives = N::Primitives, Error = ArbEthApiError>,
-{
-}
