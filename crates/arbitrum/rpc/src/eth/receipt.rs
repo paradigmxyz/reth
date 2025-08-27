1f4738f42cb13936a290302cfb7c677204344b2e
Til Jordan <til@bloctopus.io>
2025-08-27 07:18:33 +0000

    arbitrum/rpc: preserve 2718 receipt types (0x68/0x69/0x6a/0x6b) instead of always Legacy in RPC

--- crates/arbitrum/rpc/src/eth/receipt.rs
+++ crates/arbitrum/rpc/src/eth/receipt.rs
@@ -28,10 +28,25 @@ where
         &self,
         receipts: vec::Vec<ConvertReceiptInput<'_, N>>,
     ) -> Result<vec::Vec<Self::RpcReceipt>, Self::Error> {
+        use reth_arbitrum_primitives::{ArbTransactionSigned, ArbTypedTransaction};
         let mut out = Vec::with_capacity(receipts.len());
         for input in receipts {
+            let ty_opt = input.tx.as_ref().downcast_ref::<ArbTransactionSigned>().map(|arb_tx| {
+                match &**arb_tx {
+                    ArbTypedTransaction::SubmitRetryable(_) => Some(arb_alloy_consensus::tx::ArbTxType::ArbitrumSubmitRetryableTx.as_u8()),
+                    ArbTypedTransaction::Retry(_) => Some(arb_alloy_consensus::tx::ArbTxType::ArbitrumRetryTx.as_u8()),
+                    ArbTypedTransaction::Internal(_) => Some(arb_alloy_consensus::tx::ArbTxType::ArbitrumInternalTx.as_u8()),
+                    ArbTypedTransaction::Deposit(_) => Some(arb_alloy_consensus::tx::ArbTxType::ArbitrumDepositTx.as_u8()),
+                    _ => None,
+                }
+            }).flatten();
+
             out.push(build_receipt(&input, None, |receipt_with_bloom| {
-                ReceiptEnvelope::Legacy(receipt_with_bloom)
+                if let Some(ty) = ty_opt {
+                    ReceiptEnvelope::Eip2718 { ty, inner: receipt_with_bloom }
+                } else {
+                    ReceiptEnvelope::Legacy(receipt_with_bloom)
+                }
             }));
         }
         Ok(out)
