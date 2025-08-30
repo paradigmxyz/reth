e9ac5d1a5968d5f06c8658773fae3a4205bb734f
Soubhik-10 <soubhiksmp2004@gmail.com>
2025-08-30 12:16:24 +0530

    print

--- crates/engine/local/src/miner.rs
+++ crates/engine/local/src/miner.rs
@@ -215,8 +215,9 @@ where
         };
 
         let block = payload.block();
-
+        println!("Block is: {:#?}", block);
         let payload = T::block_to_payload(payload.block().clone());
+        println!("Payload is {:#?}", payload);
         let res = self.to_engine.new_payload(payload).await?;
 
         if !res.is_valid() {
