4517e2935c3c1e2f026e020bfe27403fcac2fc70
Dan Cline <6798349+Rjected@users.noreply.github.com>
2025-09-02 16:00:06 -0400

    fix(download): use updated merkle base URL

--- crates/cli/commands/src/download.rs
+++ crates/cli/commands/src/download.rs
@@ -17,7 +17,7 @@ use tokio::task;
 use tracing::info;
 
 const BYTE_UNITS: [&str; 4] = ["B", "KB", "MB", "GB"];
-const MERKLE_BASE_URL: &str = "https://snapshots.merkle.io";
+const MERKLE_BASE_URL: &str = "https://downloads.merkle.io";
 const EXTENSION_TAR_FILE: &str = ".tar.lz4";
 
 #[derive(Debug, Parser)]
