99cecaebc53ecd9152e91923c525cb6cf7f19940
Matthias Seitz <matthias.seitz@outlook.de>
2025-09-02 19:54:46 +0200

    chore: improve flashblock logs

--- crates/optimism/flashblocks/src/service.rs
+++ crates/optimism/flashblocks/src/service.rs
@@ -85,6 +85,7 @@ where
                 let _ = tx.send(block).inspect_err(|e| tracing::error!("{e}"));
             }
         }
+        debug!("Flashblock service completed")
     }
 
     /// Returns the cached reads at the given head hash.
@@ -207,8 +208,8 @@ where
             if fut.poll_unpin(cx).is_ready() {
                 // if we have a new canonical message, we know the currently tracked flashblock is
                 // invalidated
-                if this.current.take().is_some() {
-                    trace!("Clearing current flashblock on new canonical block");
+                if let Some(current) = this.current.take() {
+                    trace!(parent_hash=%current.block().parent_hash(), block_number=current.block().number(), "Clearing current flashblock on new canonical block");
                     return Poll::Ready(Some(Ok(None)))
                 }
             }
@@ -224,6 +225,7 @@ where
                 // built a new pending block
                 this.current = Some(new_pending.clone());
                 this.rebuild = false;
+                trace!(parent_hash=%new_pending.block().parent_hash(), block_number=new_pending.block().number(), flash_blocks=this.blocks.count(),  "Clearing current flashblock on new canonical block");
                 return Poll::Ready(Some(Ok(Some(new_pending))));
             }
             Ok(None) => {
