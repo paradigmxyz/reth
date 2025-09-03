bd9491c97ef874a23beb1354e004da296948fd1c
Roman Hodulák <roman.hodulak@polyglot-software.com>
2025-09-03 12:37:12 +0200

    refactor(optimism): Extract pending block building responsibility out of `FlashBlockService`

---
+++ crates/optimism/flashblocks/src/worker.rs
@@ -0,0 +1,124 @@
+use crate::ExecutionPayloadBaseV1;
+use alloy_eips::{eip2718::WithEncoded, BlockNumberOrTag};
+use alloy_primitives::B256;
+use reth_chain_state::{CanonStateSubscriptions, ExecutedBlock};
+use reth_errors::RethError;
+use reth_evm::{
+    execute::{BlockBuilder, BlockBuilderOutcome},
+    ConfigureEvm,
+};
+use reth_execution_types::ExecutionOutcome;
+use reth_primitives_traits::{
+    AlloyBlockHeader, BlockTy, HeaderTy, NodePrimitives, ReceiptTy, Recovered,
+};
+use reth_revm::{cached::CachedReads, database::StateProviderDatabase, db::State};
+use reth_rpc_eth_types::{EthApiError, PendingBlock};
+use reth_storage_api::{noop::NoopProvider, BlockReaderIdExt, StateProviderFactory};
+use std::{
+    sync::Arc,
+    time::{Duration, Instant},
+};
+use tracing::trace;
+
+/// The `FlashBlockBuilder` builds [`PendingBlock`] out of a sequence of transactions.
+#[derive(Debug)]
+pub(crate) struct FlashBlockBuilder<EvmConfig, Provider> {
+    evm_config: EvmConfig,
+    provider: Provider,
+    /// Cached state reads for the current block.
+    /// Current `PendingBlock` is built out of a sequence of `FlashBlocks`, and executed again when
+    /// fb received on top of the same block. Avoid redundant I/O across multiple executions
+    /// within the same block.
+    cached_state: Option<(B256, CachedReads)>,
+}
+
+impl<EvmConfig, Provider> FlashBlockBuilder<EvmConfig, Provider> {
+    pub(crate) const fn new(evm_config: EvmConfig, provider: Provider) -> Self {
+        Self { evm_config, provider, cached_state: None }
+    }
+}
+
+pub(crate) struct BuildArgs<I> {
+    pub base: ExecutionPayloadBaseV1,
+    pub transactions: I,
+}
+
+impl<N, EvmConfig, Provider> FlashBlockBuilder<EvmConfig, Provider>
+where
+    N: NodePrimitives,
+    EvmConfig: ConfigureEvm<Primitives = N, NextBlockEnvCtx: From<ExecutionPayloadBaseV1> + Unpin>,
+    Provider: StateProviderFactory
+        + CanonStateSubscriptions<Primitives = N>
+        + BlockReaderIdExt<
+            Header = HeaderTy<N>,
+            Block = BlockTy<N>,
+            Transaction = N::SignedTx,
+            Receipt = ReceiptTy<N>,
+        > + Unpin,
+{
+    /// Returns the [`PendingBlock`] made purely out of transactions and [`ExecutionPayloadBaseV1`]
+    /// in `args`.
+    ///
+    /// Returns `None` if the flashblock doesn't attach to the latest header.
+    pub(crate) fn execute<I: IntoIterator<Item = WithEncoded<Recovered<N::SignedTx>>>>(
+        &mut self,
+        args: BuildArgs<I>,
+    ) -> eyre::Result<Option<PendingBlock<N>>> {
+        trace!("Building new pending block from flashblocks");
+
+        let latest = self
+            .provider
+            .latest_header()?
+            .ok_or(EthApiError::HeaderNotFound(BlockNumberOrTag::Latest.into()))?;
+        let latest_hash = latest.hash();
+
+        if args.base.parent_hash != latest_hash {
+            trace!(flashblock_parent = ?args.base.parent_hash, local_latest=?latest.num_hash(),"Skipping non consecutive flashblock");
+            // doesn't attach to the latest block
+            return Ok(None)
+        }
+
+        let state_provider = self.provider.history_by_block_hash(latest.hash())?;
+
+        let mut request_cache = self
+            .cached_state
+            .take()
+            .filter(|(hash, _)| hash == &latest_hash)
+            .map(|(_, state)| state)
+            .unwrap_or_default();
+        let cached_db = request_cache.as_db_mut(StateProviderDatabase::new(&state_provider));
+        let mut state = State::builder().with_database(cached_db).with_bundle_update().build();
+
+        let mut builder = self
+            .evm_config
+            .builder_for_next_block(&mut state, &latest, args.base.into())
+            .map_err(RethError::other)?;
+
+        builder.apply_pre_execution_changes()?;
+
+        for tx in args.transactions {
+            let _gas_used = builder.execute_transaction(tx)?;
+        }
+
+        let BlockBuilderOutcome { execution_result, block, hashed_state, .. } =
+            builder.finish(NoopProvider::default())?;
+
+        let execution_outcome = ExecutionOutcome::new(
+            state.take_bundle(),
+            vec![execution_result.receipts],
+            block.number(),
+            vec![execution_result.requests],
+        );
+
+        self.cached_state.replace((latest_hash, request_cache));
+
+        Ok(Some(PendingBlock::with_executed_block(
+            Instant::now() + Duration::from_secs(1),
+            ExecutedBlock {
+                recovered_block: block.into(),
+                execution_output: Arc::new(execution_outcome),
+                hashed_state: Arc::new(hashed_state),
+            },
+        )))
+    }
+}
