c7661324f8f2946ca09150352c9c4a7a4b4c157a
Yong Kang <chiayongkang@hotmail.com>
2025-09-10 05:24:22 +0000

    remove: delete obsolete trie corruption test file
    - Removed the `trie_corruption_test.rs` file, which contained a detailed test suite for reproducing the trie corruption bug during reorgs using mainnet data.
    - This file is no longer needed following the introduction of a simplified test suite that does not rely on mainnet data.

--- crates/e2e-test-utils/tests/e2e-testsuite/main.rs
+++ crates/e2e-test-utils/tests/e2e-testsuite/main.rs
@@ -1,6 +1,6 @@
 //! Example tests using the test suite framework.
 
-mod trie_corruption_test;
+mod simplified_trie_corruption_test;
 
 use alloy_primitives::{Address, B256};
 use alloy_rpc_types_engine::PayloadAttributes;
