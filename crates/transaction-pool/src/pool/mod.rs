6ccf1dc6c5112a9d35eeb802812a0323d8626468
developeruche <developeruche@gmail.com>
2025-09-16 07:35:57 +0100

    manual remove of ; inserted by fmt tool

--- crates/transaction-pool/src/pool/mod.rs
+++ crates/transaction-pool/src/pool/mod.rs
@@ -649,7 +649,7 @@ where
             if listener.kind.is_propagate_only() && !propagate_allowed {
                 // only emit this hash to listeners that are only allowed to receive propagate only
                 // transactions, such as network
-                return !listener.sender.is_closed();
+                return !listener.sender.is_closed()
             }
 
             // broadcast all pending transactions to the listener
@@ -664,7 +664,7 @@ where
             if listener.kind.is_propagate_only() && !event.transaction.propagate {
                 // only emit this hash to listeners that are only allowed to receive propagate only
                 // transactions, such as network
-                return !listener.sender.is_closed();
+                return !listener.sender.is_closed()
             }
 
             listener.send(event.clone())
@@ -675,7 +675,7 @@ where
     fn on_new_blob_sidecar(&self, tx_hash: &TxHash, sidecar: &BlobTransactionSidecarVariant) {
         let mut sidecar_listeners = self.blob_transaction_sidecar_listener.lock();
         if sidecar_listeners.is_empty() {
-            return;
+            return
         }
         let sidecar = Arc::new(sidecar.clone());
         sidecar_listeners.retain_mut(|listener| {
@@ -736,7 +736,7 @@ where
         let mut listener = self.event_listener.write();
         if listener.is_empty() {
             // nothing to notify
-            return;
+            return
         }
 
         match tx {
@@ -816,7 +816,7 @@ where
         hashes: Vec<TxHash>,
     ) -> Vec<Arc<ValidPoolTransaction<T::Transaction>>> {
         if hashes.is_empty() {
-            return Vec::new();
+            return Vec::new()
         }
         let removed = self.pool.write().remove_transactions(hashes);
 
@@ -832,7 +832,7 @@ where
         hashes: Vec<TxHash>,
     ) -> Vec<Arc<ValidPoolTransaction<T::Transaction>>> {
         if hashes.is_empty() {
-            return Vec::new();
+            return Vec::new()
         }
         let removed = self.pool.write().remove_transactions_and_descendants(hashes);
 
@@ -864,7 +864,7 @@ where
         A: HandleMempoolData,
     {
         if announcement.is_empty() {
-            return;
+            return
         }
         let pool = self.get_pool_data();
         announcement.retain_by_hash(|tx| !pool.contains(tx))
@@ -965,7 +965,7 @@ where
     /// If no transaction exists, it is skipped.
     pub fn get_all(&self, txs: Vec<TxHash>) -> Vec<Arc<ValidPoolTransaction<T::Transaction>>> {
         if txs.is_empty() {
-            return Vec::new();
+            return Vec::new()
         }
         self.get_pool_data().get_all(txs).collect()
     }
@@ -978,7 +978,7 @@ where
         txs: Vec<TxHash>,
     ) -> Vec<Arc<ValidPoolTransaction<T::Transaction>>> {
         if txs.is_empty() {
-            return Vec::new();
+            return Vec::new()
         }
         self.get_pool_data().get_all(txs).filter(|tx| tx.propagate).collect()
     }
@@ -986,7 +986,7 @@ where
     /// Notify about propagated transactions.
     pub fn on_propagated(&self, txs: PropagatedTransactions) {
         if txs.0.is_empty() {
-            return;
+            return
         }
         let mut listener = self.event_listener.write();
 
@@ -1113,9 +1113,9 @@ where
         loop {
             let next = self.iter.next()?;
             if self.kind.is_propagate_only() && !next.propagate {
-                continue;
+                continue
             }
-            return Some(*next.hash());
+            return Some(*next.hash())
         }
     }
 }
@@ -1137,12 +1137,12 @@ where
         loop {
             let next = self.iter.next()?;
             if self.kind.is_propagate_only() && !next.propagate {
-                continue;
+                continue
             }
             return Some(NewTransactionEvent {
                 subpool: SubPool::Pending,
                 transaction: next.clone(),
-            });
+            })
         }
     }
 }
--- crates/transaction-pool/src/validate/eth.rs
+++ crates/transaction-pool/src/validate/eth.rs
@@ -270,7 +270,7 @@ where
                     return Err(TransactionValidationOutcome::Invalid(
                         transaction,
                         InvalidTransactionError::Eip2930Disabled.into(),
-                    ));
+                    ))
                 }
             }
             EIP1559_TX_TYPE_ID => {
@@ -279,7 +279,7 @@ where
                     return Err(TransactionValidationOutcome::Invalid(
                         transaction,
                         InvalidTransactionError::Eip1559Disabled.into(),
-                    ));
+                    ))
                 }
             }
             EIP4844_TX_TYPE_ID => {
@@ -288,7 +288,7 @@ where
                     return Err(TransactionValidationOutcome::Invalid(
                         transaction,
                         InvalidTransactionError::Eip4844Disabled.into(),
-                    ));
+                    ))
                 }
             }
             EIP7702_TX_TYPE_ID => {
@@ -297,7 +297,7 @@ where
                     return Err(TransactionValidationOutcome::Invalid(
                         transaction,
                         InvalidTransactionError::Eip7702Disabled.into(),
-                    ));
+                    ))
                 }
             }
 
@@ -315,7 +315,7 @@ where
             return Err(TransactionValidationOutcome::Invalid(
                 transaction,
                 InvalidPoolTransactionError::Eip2681,
-            ));
+            ))
         }
 
         // Reject transactions over defined size to prevent DOS attacks
@@ -332,7 +332,7 @@ where
                         tx_input_len,
                         self.max_tx_input_bytes,
                     ),
-                ));
+                ))
             }
         } else {
             // ensure the size of the non-blob transaction
@@ -341,14 +341,14 @@ where
                 return Err(TransactionValidationOutcome::Invalid(
                     transaction,
                     InvalidPoolTransactionError::OversizedData(tx_size, self.max_tx_input_bytes),
-                ));
+                ))
             }
         }
 
         // Check whether the init code size has been exceeded.
         if self.fork_tracker.is_shanghai_activated() {
             if let Err(err) = transaction.ensure_max_init_code_size(MAX_INIT_CODE_BYTE_SIZE) {
-                return Err(TransactionValidationOutcome::Invalid(transaction, err));
+                return Err(TransactionValidationOutcome::Invalid(transaction, err))
             }
         }
 
@@ -362,7 +362,7 @@ where
                     transaction_gas_limit,
                     block_gas_limit,
                 ),
-            ));
+            ))
         }
 
         // Check individual transaction gas limit if configured
@@ -374,7 +374,7 @@ where
                         transaction_gas_limit,
                         max_tx_gas_limit,
                     ),
-                ));
+                ))
             }
         }
 
@@ -383,7 +383,7 @@ where
             return Err(TransactionValidationOutcome::Invalid(
                 transaction,
                 InvalidTransactionError::TipAboveFeeCap.into(),
-            ));
+            ))
         }
 
         // determine whether the transaction should be treated as local
@@ -406,7 +406,7 @@ where
                                 max_tx_fee_wei,
                                 tx_fee_cap_wei,
                             },
-                        ));
+                        ))
                     }
                 }
             }
@@ -425,7 +425,7 @@ where
                         .minimum_priority_fee
                         .expect("minimum priority fee is expected inside if statement"),
                 },
-            ));
+            ))
         }
 
         // Checks for chainid
@@ -434,7 +434,7 @@ where
                 return Err(TransactionValidationOutcome::Invalid(
                     transaction,
                     InvalidTransactionError::ChainIdMismatch.into(),
-                ));
+                ))
             }
         }
 
@@ -444,19 +444,19 @@ where
                 return Err(TransactionValidationOutcome::Invalid(
                     transaction,
                     InvalidTransactionError::TxTypeNotSupported.into(),
-                ));
+                ))
             }
 
             if transaction.authorization_list().is_none_or(|l| l.is_empty()) {
                 return Err(TransactionValidationOutcome::Invalid(
                     transaction,
                     Eip7702PoolTransactionError::MissingEip7702AuthorizationList.into(),
-                ));
+                ))
             }
         }
 
         if let Err(err) = ensure_intrinsic_gas(&transaction, &self.fork_tracker) {
-            return Err(TransactionValidationOutcome::Invalid(transaction, err));
+            return Err(TransactionValidationOutcome::Invalid(transaction, err))
         }
 
         // light blob tx pre-checks
@@ -466,7 +466,7 @@ where
                 return Err(TransactionValidationOutcome::Invalid(
                     transaction,
                     InvalidTransactionError::TxTypeNotSupported.into(),
-                ));
+                ))
             }
 
             let blob_count = transaction.blob_count().unwrap_or(0);
@@ -477,7 +477,7 @@ where
                     InvalidPoolTransactionError::Eip4844(
                         Eip4844PoolTransactionError::NoEip4844Blobs,
                     ),
-                ));
+                ))
             }
 
             let max_blob_count = self.fork_tracker.max_blob_count();
@@ -490,7 +490,7 @@ where
                             permitted: max_blob_count,
                         },
                     ),
-                ));
+                ))
             }
         }
 
@@ -501,7 +501,7 @@ where
             return Err(TransactionValidationOutcome::Invalid(
                 transaction,
                 InvalidTransactionError::GasLimitTooHigh.into(),
-            ));
+            ))
         }
 
         Ok(transaction)
@@ -534,12 +534,12 @@ where
 
         // Checks for nonce
         if let Err(err) = self.validate_sender_nonce(&transaction, &account) {
-            return TransactionValidationOutcome::Invalid(transaction, err);
+            return TransactionValidationOutcome::Invalid(transaction, err)
         }
 
         // checks for max cost not exceedng account_balance
         if let Err(err) = self.validate_sender_balance(&transaction, &account) {
-            return TransactionValidationOutcome::Invalid(transaction, err);
+            return TransactionValidationOutcome::Invalid(transaction, err)
         }
 
         // heavy blob tx validation
@@ -599,7 +599,7 @@ where
             };
 
             if !is_eip7702 {
-                return Ok(Err(InvalidTransactionError::SignerAccountHasBytecode.into()));
+                return Ok(Err(InvalidTransactionError::SignerAccountHasBytecode.into()))
             }
         }
         Ok(Ok(()))
@@ -618,7 +618,7 @@ where
                 tx: tx_nonce,
                 state: sender.nonce,
             }
-            .into());
+            .into())
         }
         Ok(())
     }
@@ -636,7 +636,7 @@ where
             return Err(InvalidTransactionError::InsufficientFunds(
                 GotExpected { got: sender.balance, expected }.into(),
             )
-            .into());
+            .into())
         }
         Ok(())
     }
@@ -654,7 +654,7 @@ where
             match transaction.take_blob() {
                 EthBlobTransactionSidecar::None => {
                     // this should not happen
-                    return Err(InvalidTransactionError::TxTypeNotSupported.into());
+                    return Err(InvalidTransactionError::TxTypeNotSupported.into())
                 }
                 EthBlobTransactionSidecar::Missing => {
                     // This can happen for re-injected blob transactions (on re-org), since the blob
@@ -666,7 +666,7 @@ where
                     } else {
                         return Err(InvalidPoolTransactionError::Eip4844(
                             Eip4844PoolTransactionError::MissingEip4844BlobSidecar,
-                        ));
+                        ))
                     }
                 }
                 EthBlobTransactionSidecar::Present(sidecar) => {
@@ -676,19 +676,19 @@ where
                         if sidecar.is_eip4844() {
                             return Err(InvalidPoolTransactionError::Eip4844(
                                 Eip4844PoolTransactionError::UnexpectedEip4844SidecarAfterOsaka,
-                            ));
+                            ))
                         }
                     } else if sidecar.is_eip7594() {
                         return Err(InvalidPoolTransactionError::Eip4844(
                             Eip4844PoolTransactionError::UnexpectedEip7594SidecarBeforeOsaka,
-                        ));
+                        ))
                     }
 
                     // validate the blob
                     if let Err(err) = transaction.validate_blob(&sidecar, self.kzg_settings.get()) {
                         return Err(InvalidPoolTransactionError::Eip4844(
                             Eip4844PoolTransactionError::InvalidEip4844Blob(err),
-                        ));
+                        ))
                     }
                     // Record the duration of successful blob validation as histogram
                     self.validation_metrics.blob_validation_duration.record(now.elapsed());
