06f97b48f64dd1e01bb6a73b4e128b1defc7c673
Nathaniel Bajo <nathanielbajo@Nathaniels-MacBook-Pro.local>
2025-09-19 15:11:01 +0100

    fix ci

--- crates/optimism/rpc/src/eth/transaction.rs
+++ crates/optimism/rpc/src/eth/transaction.rs
@@ -23,7 +23,7 @@ use reth_transaction_pool::{
 };
 use std::{
     fmt::{Debug, Formatter},
-    future::Future,
+    future::Future, time::Duration,
 };
 
 impl<N, Rpc> EthTransactions for OpEthApi<N, Rpc>
@@ -34,6 +34,10 @@ where
 {
     fn signers(&self) -> &SignersForRpc<Self::Provider, Self::NetworkTypes> {
         self.inner.eth_api.signers()
+    }
+
+    fn send_raw_transaction_sync_timeout(&self) -> Duration {
+        self.inner.eth_api.send_raw_transaction_sync_timeout()
     }
 
     /// Decodes and recovers the transaction and submits it to the pool.
