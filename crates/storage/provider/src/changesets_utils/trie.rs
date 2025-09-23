a2c0c4adcf8eea3046df09717c08ef1a89929e20
Brian Picciano <me@mediocregopher.com>
2025-09-23 11:51:06 +0200

    Simplify StorageTrieCurrentValuesIter implementation

--- crates/storage/provider/src/changesets_utils/trie.rs
+++ crates/storage/provider/src/changesets_utils/trie.rs
@@ -2,19 +2,16 @@ use alloy_primitives::B256;
 use itertools::{merge_join_by, EitherOrBoth};
 use reth_db_api::{cursor::DbDupCursorRO, tables, DatabaseError};
 use reth_trie::{BranchNodeCompact, Nibbles, StorageTrieEntry, StoredNibblesSubKey};
-use std::{
-    cmp::{Ord, Ordering},
-    iter::Peekable,
-};
+use std::cmp::{Ord, Ordering};
 
 /// Combines a sorted iterator of trie node paths and a [`tables::StoragesTrie`] cursor into a new
 /// iterator which produces the current values of all given paths in the same order.
 #[derive(Debug)]
-pub struct StorageTrieCurrentValuesIter<'cursor, P: Iterator<Item: std::fmt::Debug>, C> {
+pub struct StorageTrieCurrentValuesIter<'cursor, P, C> {
     /// The address of the account whose storage is being iterated.
     hashed_address: B256,
     /// Sorted iterator of node paths which we want the values of.
-    paths: Peekable<P>,
+    paths: P,
     /// Cursor over [`tables::StoragesTrie`].
     cursor: &'cursor mut C,
     /// Current value at the cursor, allows us to treat the cursor as a peekable iterator.
@@ -23,7 +20,6 @@ pub struct StorageTrieCurrentValuesIter<'cursor, P: Iterator<Item: std::fmt::Deb
 
 impl<'cursor, P, C> StorageTrieCurrentValuesIter<'cursor, P, C>
 where
-    P: Iterator<Item = Nibbles>,
     C: DbDupCursorRO<tables::StoragesTrie>,
 {
     /// Instantiate a [`StorageTrieCurrentValuesIter`] from a sorted paths iterator and a cursor.
@@ -32,8 +28,7 @@ where
         paths: P,
         cursor: &'cursor mut C,
     ) -> Result<Self, DatabaseError> {
-        let mut new_self =
-            Self { hashed_address, paths: paths.peekable(), cursor, cursor_current: None };
+        let mut new_self = Self { hashed_address, paths, cursor, cursor_current: None };
         new_self.seek_cursor(Nibbles::default())?;
         Ok(new_self)
     }
@@ -55,48 +50,43 @@ where
     type Item = Result<(Nibbles, Option<BranchNodeCompact>), DatabaseError>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            match (self.paths.peek(), self.cursor_current.as_mut()) {
-                (None, _) => {
-                    // If there are no more paths then there is no further possible output.
-                    return None
-                }
-                (Some(path), None) => {
-                    // If there is a path but the cursor is empty then that path has no node.
-                    let path = *path;
-                    self.paths.next();
-                    return Some(Ok((path, None)))
-                }
-                (Some(path), Some((cursor_path, cursor_node))) => {
-                    // There is both a path and a cursor value, compare their paths.
-                    match path.cmp(cursor_path) {
-                        Ordering::Less => {
-                            // If the path is behind the cursor then there is no value for that
-                            // path, increment the path iter and produce None.
-                            let path = *path;
-                            self.paths.next();
-                            return Some(Ok((path, None)))
-                        }
-                        Ordering::Equal => {
-                            // If the target path and cursor's path match then there is a value for
-                            // that path, increment the path iter and return the value. We don't
-                            // increment the cursor here, that will be handled on the next call to
-                            // `next` in the `Ordering::Greater` branch if the path iter is not
-                            // None.
-                            let cursor_node = core::mem::take(cursor_node);
-                            self.paths.next();
-                            return Some(Ok((*cursor_path, Some(cursor_node))))
-                        }
-                        Ordering::Greater => {
-                            // If the path is ahead of the cursor then seek the cursor forward and
-                            // loop. The cursor will either seek to the path or beyond it.
-                            let path = *path;
-                            if let Err(err) = self.seek_cursor(path) {
-                                return Some(Err(err))
-                            }
-                        }
-                    }
-                }
+        let Some(curr_path) = self.paths.next() else {
+            // If there are no more paths then there is no further possible output.
+            return None
+        };
+
+        // If the path is ahead of the cursor then seek the cursor forward to catch up. The cursor
+        // will seek either to `curr_path` or beyond it.
+        if self.cursor_current.as_ref().is_some_and(|(cursor_path, _)| curr_path > *cursor_path) {
+            if let Err(err) = self.seek_cursor(curr_path) {
+                return Some(Err(err))
+            }
+        }
+
+        // If there is a path but the cursor is empty then that path has no node.
+        if self.cursor_current.is_none() {
+            return Some(Ok((curr_path, None)))
+        }
+
+        let (cursor_path, cursor_node) =
+            self.cursor_current.as_mut().expect("already checked for None");
+
+        // There is both a path and a cursor value, compare their paths.
+        match curr_path.cmp(cursor_path) {
+            Ordering::Less => {
+                // If the path is behind the cursor then there is no value for that
+                // path, produce None.
+                Some(Ok((curr_path, None)))
+            }
+            Ordering::Equal => {
+                // If the target path and cursor's path match then there is a value for that path,
+                // return the value. We don't seek the cursor here, that will be handled on the
+                // next call to `next` after checking that `paths` isn't None.
+                let cursor_node = core::mem::take(cursor_node);
+                Some(Ok((*cursor_path, Some(cursor_node))))
+            }
+            Ordering::Greater => {
+                panic!("cursor was seeked to {curr_path:?}, but produced a node at a lower path {cursor_path:?}")
             }
         }
     }
@@ -153,8 +143,8 @@ pub fn storage_trie_wiped_changeset_iter(
         EitherOrBoth::Both(changed, _wiped) => {
             // A path of a changed node (given in `paths`) was found with a previous value in the
             // database. The changed node must have a value which is equal to the one found by the
-            // `all_nodes` iterator. If the changed node had no previous value (None) it wouldn't be
-            // returned by `all_nodes` and so would be in the Left branch.
+            // `all_nodes` iterator. If the changed node had no previous value (None) it wouldn't
+            // be returned by `all_nodes` and so would be in the Left branch.
             //
             // Due to the ordering closure passed to `merge_join_by` it's not possible for either
             // value to be an error here.
