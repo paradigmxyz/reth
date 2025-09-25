1cbe414d16cadad4526800d1d4f66bdbe1c1b9a0
anim001k <140460766+anim001k@users.noreply.github.com>
2025-09-25 13:40:40 +0200

    Update install_geth.sh

--- .github/assets/install_geth.sh
+++ .github/assets/install_geth.sh
@@ -7,11 +7,19 @@ set -eo pipefail
 GETH_BUILD=${GETH_BUILD:-"1.13.4-3f907d6a"}
 
 name="geth-linux-amd64-$GETH_BUILD"
+url="https://gethstore.blob.core.windows.net/builds/$name.tar.gz"
+tarball="$name.tar.gz"
 
 mkdir -p "$HOME/bin"
-wget "https://gethstore.blob.core.windows.net/builds/$name.tar.gz"
-tar -xvf "$name.tar.gz"
-rm "$name.tar.gz"
+wget -O "$tarball" "$url"
+# Optional integrity check: set GETH_SHA256 to expected checksum to verify the tarball.
+# Example: export GETH_SHA256="<expected_sha256>"; the script will verify and abort on mismatch.
+if [ -n "${GETH_SHA256:-}" ]; then
+  echo "$GETH_SHA256  $tarball" | sha256sum -c -
+fi
+
+tar -xvf "$tarball"
+rm "$tarball"
 mv "$name/geth" "$HOME/bin/geth"
 rm -rf "$name"
 chmod +x "$HOME/bin/geth"
