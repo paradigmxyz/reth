0f71404bc2b5121a0c4aefa106cd01383a3728e5
Yong Kang <chiayongkang@hotmail.com>
2025-09-25 08:35:23 +0000

    fix: address review comments from mattsse
    - Restore original logic flow in backfill_sync_target to maintain exact
      equivalence (first check buffered_finalized, then potentially overwrite
      with downloaded_block)
    - Simplify set_value macro in test_utils to work with Rust 2024 without
      needing allow(rust_2024_incompatible_pat) attribute
    - Note: The 'use' syntax comment is just acknowledging new Rust 2024
      capability, no change needed

--- crates/transaction-pool/src/test_utils/mock.rs
+++ crates/transaction-pool/src/test_utils/mock.rs
@@ -1,7 +1,5 @@
 //! Mock types.
 
-#![allow(rust_2024_incompatible_pat)]
-
 use crate::{
     identifier::{SenderIdentifiers, TransactionId},
     pool::txpool::TxPool,
@@ -55,48 +53,23 @@ pub fn mock_tx_pool() -> MockTxPool {
 }
 
 /// Sets the value for the field
-#[allow(unused_macros)]
 macro_rules! set_value {
-    // For mutable references
-    (&mut $this:expr => $field:ident) => {{
-        let new_value = $field;
-        match $this {
-            &mut MockTransaction::Legacy { ref mut $field, .. } => {
-                *$field = new_value.into();
-            }
-            &mut MockTransaction::Eip1559 { ref mut $field, .. } => {
-                *$field = new_value.into();
-            }
-            &mut MockTransaction::Eip4844 { ref mut $field, .. } => {
-                *$field = new_value.into();
-            }
-            &mut MockTransaction::Eip2930 { ref mut $field, .. } => {
-                *$field = new_value.into();
-            }
-            &mut MockTransaction::Eip7702 { ref mut $field, .. } => {
-                *$field = new_value.into();
-            }
-        }
-        // Ensure the tx cost is always correct after each mutation.
-        $this.update_cost();
-    }};
-    // For owned values
     ($this:expr => $field:ident) => {{
         let new_value = $field;
         match $this {
-            MockTransaction::Legacy { ref mut $field, .. } => {
+            MockTransaction::Legacy { $field, .. } => {
                 *$field = new_value.into();
             }
-            MockTransaction::Eip1559 { ref mut $field, .. } => {
+            MockTransaction::Eip1559 { $field, .. } => {
                 *$field = new_value.into();
             }
-            MockTransaction::Eip4844 { ref mut $field, .. } => {
+            MockTransaction::Eip4844 { $field, .. } => {
                 *$field = new_value.into();
             }
-            MockTransaction::Eip2930 { ref mut $field, .. } => {
+            MockTransaction::Eip2930 { $field, .. } => {
                 *$field = new_value.into();
             }
-            MockTransaction::Eip7702 { ref mut $field, .. } => {
+            MockTransaction::Eip7702 { $field, .. } => {
                 *$field = new_value.into();
             }
         }
@@ -124,7 +97,7 @@ macro_rules! make_setters_getters {
         paste! {$(
             /// Sets the value of the specified field.
             pub fn [<set_ $name>](&mut self, $name: $t) -> &mut Self {
-                set_value!(&mut self => $name);
+                set_value!(self => $name);
                 self
             }
 
