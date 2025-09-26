e2aa2fa7b45a4eec14bc60ad5c08f9476a8c7074
Brian Picciano <me@mediocregopher.com>
2025-09-26 12:26:02 +0200

    fmt

--- crates/storage/provider/src/changesets_utils/trie.rs
+++ crates/storage/provider/src/changesets_utils/trie.rs
@@ -48,8 +48,8 @@ where
 
         // If the path is ahead of the cursor then seek the cursor forward to catch up. The cursor
         // will seek either to `curr_path` or beyond it.
-        if self.cursor_current.as_ref().is_some_and(|(cursor_path, _)| curr_path > *cursor_path)
-            && let Err(err) = self.seek_cursor(curr_path)
+        if self.cursor_current.as_ref().is_some_and(|(cursor_path, _)| curr_path > *cursor_path) &&
+            let Err(err) = self.seek_cursor(curr_path)
         {
             return Some(Err(err))
         }
