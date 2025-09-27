49ee57504b52de23c45d8858647ede33c5efaa61
Avory <avorycorelli@gmail.com>
2025-09-27 20:19:04 +0300

    Update proof.rs

--- crates/trie/db/src/proof.rs
+++ crates/trie/db/src/proof.rs
@@ -11,13 +11,13 @@ use reth_trie::{
 };
 
 /// Extends [`Proof`] with operations specific for working with a database transaction.
-pub trait DatabaseProof<'a, TX> {
-    /// Create a new [Proof] from database transaction.
-    fn from_tx(tx: &'a TX) -> Self;
+pub trait DatabaseProof<'a> {
+    /// Create a new [`Proof`] instance from database transaction.
+    fn from_tx(tx: &'a Self::Tx) -> Self;
 
     /// Generates the state proof for target account based on [`TrieInput`].
     fn overlay_account_proof(
-        tx: &'a TX,
+        &self,
         input: TrieInput,
         address: Address,
         slots: &[B256],
@@ -25,59 +25,68 @@ pub trait DatabaseProof<'a, TX> {
 
     /// Generates the state [`MultiProof`] for target hashed account and storage keys.
     fn overlay_multiproof(
-        tx: &'a TX,
+        &self,
         input: TrieInput,
         targets: MultiProofTargets,
     ) -> Result<MultiProof, StateProofError>;
+
+    /// Associated type for the database transaction.
+    type Tx;
 }
 
-impl<'a, TX: DbTx> DatabaseProof<'a, TX>
+impl<'a, TX: DbTx> DatabaseProof<'a>
     for Proof<DatabaseTrieCursorFactory<'a, TX>, DatabaseHashedCursorFactory<'a, TX>>
 {
-    /// Create a new [Proof] instance from database transaction.
-    fn from_tx(tx: &'a TX) -> Self {
+    type Tx = TX;
+
+    fn from_tx(tx: &'a Self::Tx) -> Self {
         Self::new(DatabaseTrieCursorFactory::new(tx), DatabaseHashedCursorFactory::new(tx))
     }
-
     fn overlay_account_proof(
-        tx: &'a TX,
+        &self,
         input: TrieInput,
         address: Address,
         slots: &[B256],
     ) -> Result<AccountProof, StateProofError> {
         let nodes_sorted = input.nodes.into_sorted();
         let state_sorted = input.state.into_sorted();
-        Self::from_tx(tx)
-            .with_trie_cursor_factory(InMemoryTrieCursorFactory::new(
-                DatabaseTrieCursorFactory::new(tx),
-                &nodes_sorted,
-            ))
-            .with_hashed_cursor_factory(HashedPostStateCursorFactory::new(
-                DatabaseHashedCursorFactory::new(tx),
-                &state_sorted,
-            ))
-            .with_prefix_sets_mut(input.prefix_sets)
-            .account_proof(address, slots)
+        Self::new(
+            self.trie_cursor_factory().clone(),
+            self.hashed_cursor_factory().clone(),
+        )
+        .with_trie_cursor_factory(InMemoryTrieCursorFactory::new(
+            self.trie_cursor_factory().clone(),
+            &nodes_sorted,
+        ))
+        .with_hashed_cursor_factory(HashedPostStateCursorFactory::new(
+            self.hashed_cursor_factory().clone(),
+            &state_sorted,
+        ))
+        .with_prefix_sets_mut(input.prefix_sets)
+        .account_proof(address, slots)
     }
 
     fn overlay_multiproof(
-        tx: &'a TX,
+        &self,
         input: TrieInput,
         targets: MultiProofTargets,
     ) -> Result<MultiProof, StateProofError> {
         let nodes_sorted = input.nodes.into_sorted();
         let state_sorted = input.state.into_sorted();
-        Self::from_tx(tx)
-            .with_trie_cursor_factory(InMemoryTrieCursorFactory::new(
-                DatabaseTrieCursorFactory::new(tx),
-                &nodes_sorted,
-            ))
-            .with_hashed_cursor_factory(HashedPostStateCursorFactory::new(
-                DatabaseHashedCursorFactory::new(tx),
-                &state_sorted,
-            ))
-            .with_prefix_sets_mut(input.prefix_sets)
-            .multiproof(targets)
+        Self::new(
+            self.trie_cursor_factory().clone(),
+            self.hashed_cursor_factory().clone(),
+        )
+        .with_trie_cursor_factory(InMemoryTrieCursorFactory::new(
+            self.trie_cursor_factory().clone(),
+            &nodes_sorted,
+        ))
+        .with_hashed_cursor_factory(HashedPostStateCursorFactory::new(
+            self.hashed_cursor_factory().clone(),
+            &state_sorted,
+        ))
+        .with_prefix_sets_mut(input.prefix_sets)
+        .multiproof(targets)
     }
 }
 
