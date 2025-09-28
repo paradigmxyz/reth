688ba0bf3cf84d08bcdec0ac5c7fc5c1e9dd0f0e
Matthias Seitz <matthias.seitz@outlook.de>
2025-09-28 12:53:45 +0200

    chore: remove doc_auto_cfg feature (#18758)

--- crates/storage/rpc-provider/src/lib.rs
+++ crates/storage/rpc-provider/src/lib.rs
@@ -22,7 +22,7 @@
     issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 use alloy_consensus::{constants::KECCAK_EMPTY, BlockHeader};
 use alloy_eips::{BlockHashOrNumber, BlockNumberOrTag};
