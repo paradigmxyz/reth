c3a6f93b950658d358a9d46f61f99d66f8bed925
Matthias Seitz <matthias.seitz@outlook.de>
2025-10-01 09:42:21 +0200

    touchups

--- crates/optimism/flashblocks/src/payload.rs
+++ crates/optimism/flashblocks/src/payload.rs
@@ -148,8 +148,8 @@ pub struct PendingFlashBlock<N: NodePrimitives> {
     pub last_flashblock_index: u64,
     /// The last Flashblock block hash,
     pub last_flashblock_hash: B256,
-    /// Whether the [`PendingBlock`] has a properly calculated stateroot.
-    pub has_caluculated_state_root: bool,
+    /// Whether the [`PendingBlock`] has a properly computed stateroot.
+    pub has_computed_state_root: bool,
 }
 
 impl<N: NodePrimitives> PendingFlashBlock<N> {
@@ -158,13 +158,13 @@ impl<N: NodePrimitives> PendingFlashBlock<N> {
         pending: PendingBlock<N>,
         last_flashblock_index: u64,
         last_flashblock_hash: B256,
-        has_caluculated_state_root: bool,
+        has_computed_state_root: bool,
     ) -> Self {
-        Self { pending, last_flashblock_index, last_flashblock_hash, has_caluculated_state_root }
+        Self { pending, last_flashblock_index, last_flashblock_hash, has_computed_state_root }
     }
 
     /// Returns the properly calculated state root for that block if it was computed.
     pub fn computed_state_root(&self) -> Option<B256> {
-        self.has_caluculated_state_root.then_some(self.pending.block().state_root())
+        self.has_computed_state_root.then_some(self.pending.block().state_root())
     }
 }
