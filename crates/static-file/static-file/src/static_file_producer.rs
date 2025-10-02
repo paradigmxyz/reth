f7641de5b43bfd49bf0f3438a9b932b72a9bfe3b
Matthias Seitz <matthias.seitz@outlook.de>
2025-10-02 13:22:43 +0200

    docs: apply spelling and grammar fixes (#18836)
    Co-authored-by: Jennifer Paffrath <jenpaff0@gmail.com>
    Co-authored-by: Max <max@digi.net>

--- crates/static-file/static-file/src/static_file_producer.rs
+++ crates/static-file/static-file/src/static_file_producer.rs
@@ -30,7 +30,7 @@ pub type StaticFileProducerResult = ProviderResult<StaticFileTargets>;
 pub type StaticFileProducerWithResult<Provider> =
     (StaticFileProducer<Provider>, StaticFileProducerResult);
 
-/// Static File producer. It's a wrapper around [`StaticFileProducer`] that allows to share it
+/// Static File producer. It's a wrapper around [`StaticFileProducerInner`] that allows to share it
 /// between threads.
 #[derive(Debug)]
 pub struct StaticFileProducer<Provider>(Arc<Mutex<StaticFileProducerInner<Provider>>>);
