383421ee2f65b4c15c94bec84acde9ede1b83a4f
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-10-15 10:57:21 +0530

    fixes

--- .github/assets/hive/build_simulators.sh
+++ .github/assets/hive/build_simulators.sh
@@ -13,7 +13,7 @@ go build .
 echo "Building images"
 
 ./hive -client reth --sim "ethereum/eest" \
-  --sim.buildarg fixtures=https://github.com/ethereum/execution-spec-tests/releases/download/bal@v1.2.0/fixtures_bal.tar.gz \
+  --sim.buildarg fixtures=https://github.com/ethereum/execution-spec-tests/releases/download/bal@v1.3.0/fixtures_bal.tar.gz \
   --sim.buildarg branch=main \
   --sim.timelimit 1s || true &
 ./hive -client reth --sim "ethereum/engine" -sim.timelimit 1s || true &
