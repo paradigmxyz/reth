3b4df1a481c9cb435a9c2cb06754866cc4553643
Avory <avorycorelli@gmail.com>
2025-10-16 23:44:08 +0300

    Update id.rs

--- crates/stages/types/src/id.rs
+++ crates/stages/types/src/id.rs
@@ -25,7 +25,6 @@ pub enum StageId {
     TransactionLookup,
     IndexStorageHistory,
     IndexAccountHistory,
-    MerkleChangeSets,
     Prune,
     Finish,
     /// Other custom stage with a provided string identifier.
@@ -40,7 +39,7 @@ static ENCODED_STAGE_IDS: OnceLock<HashMap<StageId, Vec<u8>>> = OnceLock::new();
 
 impl StageId {
     /// All supported Stages
-    pub const ALL: [Self; 16] = [
+    pub const ALL: [Self; 15] = [
         Self::Era,
         Self::Headers,
         Self::Bodies,
@@ -54,7 +53,6 @@ impl StageId {
         Self::TransactionLookup,
         Self::IndexStorageHistory,
         Self::IndexAccountHistory,
-        Self::MerkleChangeSets,
         Self::Prune,
         Self::Finish,
     ];
@@ -90,7 +88,6 @@ impl StageId {
             Self::TransactionLookup => "TransactionLookup",
             Self::IndexAccountHistory => "IndexAccountHistory",
             Self::IndexStorageHistory => "IndexStorageHistory",
-            Self::MerkleChangeSets => "MerkleChangeSets",
             Self::Prune => "Prune",
             Self::Finish => "Finish",
             Self::Other(s) => s,
@@ -114,7 +111,7 @@ impl StageId {
 
     /// Get a pre-encoded raw Vec, for example, to be used as the DB key for
     /// `tables::StageCheckpoints` and `tables::StageCheckpointProgresses`
-    pub fn get_pre_encoded(&self) -> Option<&Vec<u8>> {
+    pub const fn get_pre_encoded(&self) -> Option<&Vec<u8>> {
         #[cfg(not(feature = "std"))]
         {
             None
