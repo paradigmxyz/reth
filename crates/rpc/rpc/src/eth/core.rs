3a0a6dea7f0537337c328aeed3a7a0e7995cce69
Merkel Tranjes <140164174+rnkrtt@users.noreply.github.com>
2025-10-16 16:22:46 +0200

    refactor: simplify GasCap conversion in EthApi

--- crates/rpc/rpc/src/eth/core.rs
+++ crates/rpc/rpc/src/eth/core.rs
@@ -366,7 +366,7 @@ where
             signers,
             eth_cache,
             gas_oracle,
-            gas_cap: gas_cap.into().into(),
+            gas_cap: gas_cap.into().0,
             max_simulate_blocks,
             eth_proof_window,
             starting_block,
