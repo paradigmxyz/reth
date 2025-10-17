acc60f8bcdf4b320a3d98bb560dc464e7cb78d04
Andrew Huang <k.andrew.huang@gmail.com>
2025-10-17 12:15:56 -0700

    Add support for init-state for op-reth chains that are not op-mainnet, from a non-0 block.

--- crates/optimism/cli/src/commands/init_state.rs
+++ crates/optimism/cli/src/commands/init_state.rs
@@ -15,8 +15,10 @@ use reth_provider::{
     BlockNumReader, ChainSpecProvider, DBProvider, DatabaseProviderFactory,
     StaticFileProviderFactory, StaticFileWriter,
 };
-use std::{io::BufReader, sync::Arc};
+use std::{io::BufReader, sync::Arc, path::Path};
 use tracing::info;
+use alloy_rlp::Decodable;
+use reth_node_builder::NodePrimitives;
 
 /// Initializes the database with the genesis block.
 #[derive(Debug, Parser)]
@@ -24,17 +26,34 @@ pub struct InitStateCommandOp<C: ChainSpecParser> {
     #[command(flatten)]
     init_state: reth_cli_commands::init_state::InitStateCommand<C>,
 
-    /// **Optimism Mainnet Only**
-    ///
-    /// Specifies whether to initialize the state without relying on OVM historical data.
+    /// Specifies whether to initialize the state without relying on OVM or EVM historical data.
     ///
     /// When enabled, and before inserting the state, it creates a dummy chain up to the last OVM
-    /// block (#105235062) (14GB / 90 seconds). It then, appends the Bedrock block.
+    /// block (#105235062) (14GB / 90 seconds). It then, appends the Bedrock block. This is hardcoded
+    /// for OP mainnet, for other OP chains you will need to pass in a header.
     ///
     /// - **Note**: **Do not** import receipts and blocks beforehand, or this will fail or be
     ///   ignored.
     #[arg(long, default_value = "false")]
     without_ovm: bool,
+}
+
+/// Reads the header RLP from a file and returns the Header.
+///
+/// This supports both raw rlp bytes and rlp hex string.
+pub(crate) fn read_header_from_file<H>(path: &Path) -> Result<H, eyre::Error>
+where
+    H: Decodable,
+{
+    let buf = if let Ok(content) = reth_fs_util::read_to_string(path) {
+        alloy_primitives::hex::decode(content.trim())?
+    } else {
+        // If UTF-8 decoding fails, read as raw bytes
+        reth_fs_util::read(path)?
+    };
+
+    let header = H::decode(&mut &buf[..])?;
+    Ok(header)
 }
 
 impl<C: ChainSpecParser<ChainSpec = OpChainSpec>> InitStateCommandOp<C> {
@@ -51,19 +70,38 @@ impl<C: ChainSpecParser<ChainSpec = OpChainSpec>> InitStateCommandOp<C> {
         let provider_rw = provider_factory.database_provider_rw()?;
 
         // OP-Mainnet may want to bootstrap a chain without OVM historical data
-        if provider_factory.chain_spec().is_optimism_mainnet() && self.without_ovm {
+        if self.without_ovm {
             let last_block_number = provider_rw.last_block_number()?;
 
             if last_block_number == 0 {
-                reth_cli_commands::init_state::without_evm::setup_without_evm(
-                    &provider_rw,
-                    SealedHeader::new(BEDROCK_HEADER, BEDROCK_HEADER_HASH),
-                    |number| {
-                        let mut header = Header::default();
-                        header.set_number(number);
-                        header
-                    },
-                )?;
+                if provider_factory.chain_spec().is_optimism_mainnet() {
+                    reth_cli_commands::init_state::without_evm::setup_without_evm(
+                        &provider_rw,
+                        SealedHeader::new(BEDROCK_HEADER, BEDROCK_HEADER_HASH),
+                        |number| {
+                            let mut header = Header::default();
+                            header.set_number(number);
+                            header
+                        },
+                    )?;
+                } else {
+                    // ensure header, total difficulty and header hash are provided
+                    let header = self.init_state.header.ok_or_else(|| eyre::eyre!("Header file must be provided"))?;
+                    let header: Header = read_header_from_file(&header).unwrap();
+
+                    let header_hash = self.init_state.header_hash.unwrap_or_else(|| header.hash_slow());
+
+                    reth_cli_commands::init_state::without_evm::setup_without_evm(
+                        &provider_rw,
+                        SealedHeader::new(header, header_hash),
+                        |number| {
+                            let mut header =
+                                <<N::Primitives as NodePrimitives>::BlockHeader>::default();
+                            header.set_number(number);
+                            header
+                        },
+                    )?;
+                }
 
                 // SAFETY: it's safe to commit static files, since in the event of a crash, they
                 // will be unwound according to database checkpoints.
