48dce07c4d16f3f38fb4b22a889ac20bad34bc39
Mablr <59505383+mablr@users.noreply.github.com>
2025-10-23 11:22:07 +0200

    fix: update CLI args

--- crates/node/core/src/args/metric.rs
+++ crates/node/core/src/args/metric.rs
@@ -14,14 +14,18 @@ pub struct MetricArgs {
     /// URL for pushing Prometheus metrics to a push gateway.
     ///
     /// If set, the node will periodically push metrics to the specified push gateway URL.
-    #[arg(long = "metrics.push.url", value_name = "PUSH_GATEWAY_URL", help_heading = "Metrics")]
+    #[arg(
+        long = "metrics.prometheus.push.url",
+        value_name = "PUSH_GATEWAY_URL",
+        help_heading = "Metrics"
+    )]
     pub push_gateway_url: Option<String>,
 
     /// Interval in seconds for pushing metrics to push gateway.
     ///
     /// Default: 5 seconds
     #[arg(
-        long = "metrics.push.interval",
+        long = "metrics.prometheus.push.interval",
         default_value = "5",
         value_parser = parse_duration_from_secs,
         value_name = "SECONDS",
