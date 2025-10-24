96e8ddbfcfe880d358dafcac0a5b0c0a9a4956cf
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-10-24 16:18:58 +0100

    chore(trie): do not create a parent span for proof worker handle

--- crates/trie/parallel/src/proof.rs
+++ crates/trie/parallel/src/proof.rs
@@ -336,7 +336,7 @@ mod tests {
 
         let task_ctx =
             ProofTaskCtx::new(Default::default(), Default::default(), Default::default());
-        let (proof_worker_handle, _) =
+        let proof_worker_handle =
             ProofWorkerHandle::new(rt.handle().clone(), consistent_view, task_ctx, 1, 1);
 
         let parallel_result = ParallelProof::new(
