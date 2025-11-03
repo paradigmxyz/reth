53fb28cffa7b688b0d9e2e0f95506a71be6533cf
Ragnar <rodiondenmark@gmail.com>
2025-11-03 12:00:34 +0000

    cargo fmt

--- crates/evm/execution-errors/src/trie.rs
+++ crates/evm/execution-errors/src/trie.rs
@@ -40,8 +40,8 @@ pub enum StateRootError {
 impl From<StateRootError> for DatabaseError {
     fn from(err: StateRootError) -> Self {
         match err {
-            StateRootError::Database(err) |
-            StateRootError::StorageRootError(StorageRootError::Database(err)) => err,
+            StateRootError::Database(err)
+            | StateRootError::StorageRootError(StorageRootError::Database(err)) => err,
         }
     }
 }
