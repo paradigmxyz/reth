e44376ef9e71c1179cb40067276edb3c7d36e051
Léa Narzis <78718413+lean-apple@users.noreply.github.com>
2025-11-07 07:20:36 +0100

    style: place up eip 4844 setup

--- crates/ethereum/node/src/node.rs
+++ crates/ethereum/node/src/node.rs
@@ -478,14 +478,14 @@ where
 
         let validator = TransactionValidationTaskExecutor::eth_builder(ctx.provider().clone())
             .with_head_timestamp(ctx.head().timestamp)
+            .set_eip4844(!blobs_disabled)
+            .kzg_settings(ctx.kzg_settings()?)
             .with_max_tx_input_bytes(ctx.config().txpool.max_tx_input_bytes)
             .with_local_transactions_config(pool_config.local_transactions_config.clone())
             .set_tx_fee_cap(ctx.config().rpc.rpc_tx_fee_cap)
             .with_max_tx_gas_limit(ctx.config().txpool.max_tx_gas_limit)
             .with_minimum_priority_fee(ctx.config().txpool.minimum_priority_fee)
             .with_additional_tasks(ctx.config().txpool.additional_validation_tasks)
-            .set_eip4844(!blobs_disabled)
-            .kzg_settings(ctx.kzg_settings()?)
             .build_with_tasks(ctx.task_executor().clone(), blob_store.clone());
 
         if !blobs_disabled && validator.validator().eip4844() {
