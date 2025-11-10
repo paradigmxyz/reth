a5038dda0527ebb7616efe196c064c3bfacee7de
pellekrab <78560773+PelleKrab@users.noreply.github.com>
2025-11-10 12:07:57 -0700

    fix(metrics): fixed IL block building metrics names

--- crates/ethereum/payload/src/metrics.rs
+++ crates/ethereum/payload/src/metrics.rs
@@ -4,14 +4,14 @@ use reth_metrics::metrics;
 
 /// Record inclusion list transaction included when building a block
 pub(crate) fn record_inclusion_list_transaction_included() {
-    metrics::counter!("ef_execution_block_building_inclusion_list_transactions_included_total")
+    metrics::counter!("ef_execution_inclusion_list_block_building_transactions_included_total")
         .increment(1);
 }
 
 /// Record inclusion list transaction excluded when building a block with reason
 pub(crate) fn record_inclusion_list_transaction_excluded(reason: &'static str) {
     metrics::counter!(
-        "ef_execution_block_building_inclusion_list_transactions_excluded_total",
+        "ef_execution_inclusion_list_block_building_transactions_excluded_total",
         "reason" => reason
     )
     .increment(1);
