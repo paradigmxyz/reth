96993dd0732eac1abba8a94df86baa3082cc0313
Léa Narzis <78718413+lean-apple@users.noreply.github.com>
2025-11-13 20:26:21 +0100

    refactor(era-downloader): support both era and era1 file types in downloader (#19617)

--- crates/era-downloader/tests/it/main.rs
+++ crates/era-downloader/tests/it/main.rs
@@ -13,12 +13,20 @@ use futures::Stream;
 use reqwest::IntoUrl;
 use reth_era_downloader::HttpClient;
 
-pub(crate) const NIMBUS: &[u8] = include_bytes!("../res/nimbus.html");
-pub(crate) const ETH_PORTAL: &[u8] = include_bytes!("../res/ethportal.html");
-pub(crate) const ITHACA: &[u8] = include_bytes!("../res/ithaca.html");
-pub(crate) const CHECKSUMS: &[u8] = include_bytes!("../res/checksums.txt");
-pub(crate) const MAINNET_0: &[u8] = include_bytes!("../res/mainnet-00000-5ec1ffb8.era1");
-pub(crate) const MAINNET_1: &[u8] = include_bytes!("../res/mainnet-00001-a5364e9a.era1");
+pub(crate) const ERA1_NIMBUS: &[u8] = include_bytes!("../res/era1-nimbus.html");
+pub(crate) const ERA1_ETH_PORTAL: &[u8] = include_bytes!("../res/ethportal.html");
+pub(crate) const ERA1_ITHACA: &[u8] = include_bytes!("../res/era1-ithaca.html");
+pub(crate) const ERA1_CHECKSUMS: &[u8] = include_bytes!("../res/checksums.txt");
+pub(crate) const ERA1_MAINNET_0: &[u8] =
+    include_bytes!("../res/era1-files/mainnet-00000-5ec1ffb8.era1");
+pub(crate) const ERA1_MAINNET_1: &[u8] =
+    include_bytes!("../res/era1-files/mainnet-00001-a5364e9a.era1");
+
+pub(crate) const ERA_NIMBUS: &[u8] = include_bytes!("../res/era-nimbus.html");
+pub(crate) const ERA_MAINNET_0: &[u8] =
+    include_bytes!("../res/era-files/mainnet-00000-4b363db9.era");
+pub(crate) const ERA_MAINNET_1: &[u8] =
+    include_bytes!("../res/era-files/mainnet-00001-40cf2f3c.era");
 
 /// An HTTP client pre-programmed with canned answers to received calls.
 /// Panics if it receives an unknown call.
@@ -33,22 +41,32 @@ impl HttpClient for StubClient {
         let url = url.into_url().unwrap();
 
         Ok(futures::stream::iter(vec![Ok(match url.as_str() {
-            "https://mainnet.era1.nimbus.team/" => Bytes::from_static(NIMBUS),
-            "https://era1.ethportal.net/" => Bytes::from_static(ETH_PORTAL),
-            "https://era.ithaca.xyz/era1/index.html" => Bytes::from_static(ITHACA),
+            // Era1 urls
+            "https://mainnet.era1.nimbus.team/" => Bytes::from_static(ERA1_NIMBUS),
+            "https://era1.ethportal.net/" => Bytes::from_static(ERA1_ETH_PORTAL),
+            "https://era.ithaca.xyz/era1/index.html" => Bytes::from_static(ERA1_ITHACA),
             "https://mainnet.era1.nimbus.team/checksums.txt" |
             "https://era1.ethportal.net/checksums.txt" |
-            "https://era.ithaca.xyz/era1/checksums.txt" => Bytes::from_static(CHECKSUMS),
+            "https://era.ithaca.xyz/era1/checksums.txt" => Bytes::from_static(ERA1_CHECKSUMS),
             "https://era1.ethportal.net/mainnet-00000-5ec1ffb8.era1" |
             "https://mainnet.era1.nimbus.team/mainnet-00000-5ec1ffb8.era1" |
             "https://era.ithaca.xyz/era1/mainnet-00000-5ec1ffb8.era1" => {
-                Bytes::from_static(MAINNET_0)
+                Bytes::from_static(ERA1_MAINNET_0)
             }
             "https://era1.ethportal.net/mainnet-00001-a5364e9a.era1" |
             "https://mainnet.era1.nimbus.team/mainnet-00001-a5364e9a.era1" |
             "https://era.ithaca.xyz/era1/mainnet-00001-a5364e9a.era1" => {
-                Bytes::from_static(MAINNET_1)
+                Bytes::from_static(ERA1_MAINNET_1)
             }
+            // Era urls
+            "https://mainnet.era.nimbus.team/" => Bytes::from_static(ERA_NIMBUS),
+            "https://mainnet.era.nimbus.team/mainnet-00000-4b363db9.era" => {
+                Bytes::from_static(ERA_MAINNET_0)
+            }
+            "https://mainnet.era.nimbus.team/mainnet-00001-40cf2f3c.era" => {
+                Bytes::from_static(ERA_MAINNET_1)
+            }
+
             v => unimplemented!("Unexpected URL \"{v}\""),
         })]))
     }
