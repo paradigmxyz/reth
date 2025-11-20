4a1e6120b1351d2131edfb47eb780caa8480fc49
Dumi Loghin <dumiloghin@gmail.com>
2025-11-20 11:21:04 +0800

    improve build image (#45)

--- build_image.sh
+++ build_image.sh
@@ -1,1 +1,15 @@
-docker build -t op-reth -f DockerfileOp .
+#!/bin/bash
+
+GITHASH=$(git rev-parse --short HEAD)
+TAG="op-reth:$GITHASH"
+
+echo "🔨 Building Reth Docker image: $TAG ..."
+
+docker build -t $TAG -f DockerfileOp .
+
+if [ $# -gt 0 ] && [ "$1" == "-l" ]; then
+    docker tag $TAG op-reth:latest
+    echo "🔖 Tagged $TAG as op-reth:latest"
+else
+    echo "💡 Use -l to tag the image as latest"
+fi
