71cd419f7f734934a6cb91c86f87bca70ad583c0
Soubhik Singha Mahapatra <soubhiksmp2004@gmail.com>
2025-12-09 19:09:40 +0530

    more tracing

--- crates/ethereum/consensus/src/validation.rs
+++ crates/ethereum/consensus/src/validation.rs
@@ -73,7 +73,12 @@ where
         };
         if let Some(bal) = block_access_list {
             let bal_hash = alloy_primitives::keccak256(alloy_rlp::encode(bal));
-            if let Some(body_bal) = block.body().block_access_list() {
+            let block_bal = block.body().block_access_list();
+            tracing::debug!("Block Bal :{:?}", block_bal);
+            if let Some(body_bal) = block_bal {
+                if body_bal.len() == 0 {
+                    tracing::debug!("Hit Empty BAL : Block is {:?}", block);
+                }
                 verify_bal(body_bal, bal)?;
             }
 
