115f4da946ae71494cc0530fea4b093c5514b458
Léa Narzis <78718413+lean-apple@users.noreply.github.com>
2025-12-13 17:23:13 +0100

    chore: fmt

--- crates/rpc/rpc-eth-types/src/simulate.rs
+++ crates/rpc/rpc-eth-types/src/simulate.rs
@@ -6,7 +6,7 @@ use crate::{
 };
 use alloy_consensus::{transaction::TxHashRef, BlockHeader, Transaction as _};
 use alloy_eips::eip2718::WithEncoded;
-use alloy_network::{TransactionBuilder, NetworkTransactionBuilder};
+use alloy_network::{NetworkTransactionBuilder, TransactionBuilder};
 use alloy_rpc_types_eth::{
     simulate::{SimCallResult, SimulateError, SimulatedBlock},
     BlockTransactionsKind,
