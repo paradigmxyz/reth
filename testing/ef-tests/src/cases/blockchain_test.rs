191c049950058a545b5e8655a8749609f4eb4aca
figtracer <1gusredo@gmail.com>
2025-12-19 17:09:52 +0000

    remove unnecessary clone in ef-tests

--- testing/ef-tests/src/cases/blockchain_test.rs
+++ testing/ef-tests/src/cases/blockchain_test.rs
@@ -218,7 +218,7 @@ fn run_case(
     .unwrap();
 
     provider
-        .insert_block(&genesis_block.clone())
+        .insert_block(&genesis_block)
         .map_err(|err| Error::block_failed(0, Default::default(), err))?;
 
     // Increment block number for receipts static file
