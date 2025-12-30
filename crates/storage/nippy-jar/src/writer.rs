9436edc021b17d7e1fad00d627938eaa3b762b1c
YK <chiayongkang@hotmail.com>
2025-12-30 14:03:08 +0800

    Update crates/storage/nippy-jar/src/writer.rs

--- crates/storage/nippy-jar/src/writer.rs
+++ crates/storage/nippy-jar/src/writer.rs
@@ -48,7 +48,7 @@ pub struct NippyJarWriter<H: NippyJarHeader = ()> {
 impl<H: NippyJarHeader> NippyJarWriter<H> {
     /// Creates a [`NippyJarWriter`] from [`NippyJar`].
     ///
-    /// It will **always** attempt to heal any inconsistent state when called.
+    /// Creates a `NippyJarWriter` from a `NippyJar`, healing any inconsistent state.
     pub fn new(jar: NippyJar<H>) -> Result<Self, NippyJarError> {
         let (data_file, offsets_file, is_created) =
             Self::create_or_open_files(jar.data_path(), &jar.offsets_path())?;
