81f53487fb596b04b0747965839fc65afc842cb6
cuiweixie <cuiweixie@gmail.com>
2026-01-03 15:42:48 +0800

    fix: make clippy happy

--- crates/rpc/rpc-eth-types/src/gas_oracle.rs
+++ crates/rpc/rpc-eth-types/src/gas_oracle.rs
@@ -197,12 +197,8 @@ where
         let mut price = if results.is_empty() {
             inner.last_price.price
         } else {
-            // results.sort_unstable();
             let k = (results.len() - 1) * self.oracle_config.percentile.min(100) as usize / 100;
-            *(&mut *results).select_nth_unstable(k).1
-            // *results.get((results.len() - 1) * self.oracle_config.percentile as usize /
-            // 100).expect(     "gas price index is a percent of nonzero array length,
-            // so a value always exists", )
+            (*results).select_nth_unstable(k).1
         };
 
         // constrain to the max price
