975e99665b0996d7dda1c54dda119ec7c23d1165
bobtajson <152420524+bobtajson@users.noreply.github.com>
2026-01-05 14:51:06 +0100

    Update in_memory.rs

--- crates/chain-state/src/in_memory.rs
+++ crates/chain-state/src/in_memory.rs
@@ -1467,7 +1467,6 @@ mod tests {
         assert_eq!(parents[0].block().recovered_block().number, 2);
         assert_eq!(parents[1].block().recovered_block().number, 1);
 
-        let parents: Vec<_> = chain[0].parent_state_chain();
         assert_eq!(chain[0].parent_state_chain().count(), 0);
     }
 
