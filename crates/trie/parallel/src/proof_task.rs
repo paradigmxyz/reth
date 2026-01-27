af3601c65d2b1cbf0ffa04d3f3390c18c997bfb5
Arsenii Kulikov <klkvrr@gmail.com>
2026-01-27 19:17:49 +0400

    feat: more metrics (#21481)

--- crates/trie/parallel/src/proof_task.rs
+++ crates/trie/parallel/src/proof_task.rs
@@ -1302,6 +1302,7 @@ where
             target: "trie::proof_task",
             "Account multiproof calculation",
             targets = targets.len(),
+            num_slots = targets.values().map(|slots| slots.len()).sum::<usize>(),
             worker_id=self.worker_id,
         );
         let _span_guard = span.enter();
