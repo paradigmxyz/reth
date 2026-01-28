e63cf8de782d9b2cfc5dc97ba18387fa86202bfd
Soubhik Singha Mahapatra <soubhiksmp2004@gmail.com>
2026-01-28 19:21:26 +0530

    use correct branch

--- .github/assets/hive/build_simulators.sh
+++ .github/assets/hive/build_simulators.sh
@@ -13,7 +13,7 @@ go build .
 echo "Building images"
 ./hive -client reth --sim "ethereum/eels" \
   --sim.buildarg fixtures=https://github.com/ethereum/execution-spec-tests/releases/download/bal@v5.0.0/fixtures_bal.tar.gz \
-  --sim.buildarg branch=forks/amsterdam \
+  --sim.buildarg branch=eips/amsterdam/eip-7843 \
   --sim.timelimit 1s || true &
 ./hive -client reth --sim "ethereum/engine" -sim.timelimit 1s || true &
 ./hive -client reth --sim "devp2p" -sim.timelimit 1s || true &
