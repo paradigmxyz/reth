41d35bef5179084017d4bde8ee580a258b92eef0
thuricela <154839958+thuricela@users.noreply.github.com>
2026-01-29 12:45:31 +0100

    Update util.rs

--- crates/stages/api/src/util.rs
+++ crates/stages/api/src/util.rs
@@ -3,14 +3,14 @@ pub(crate) mod opt {
     /// value of the [Option]. If the [Option] is `None`, then an option containing the passed in
     /// value will be returned.
     pub(crate) fn max<T: Ord + Copy>(a: Option<T>, b: T) -> Option<T> {
-        a.map_or(Some(b), |v| Some(std::cmp::max(v, b)))
+        Some(a.map_or(b, |v| std::cmp::max(v, b)))
     }
 
     /// Get an [Option] with the minimum value, compared between the passed in value and the inner
     /// value of the [Option]. If the [Option] is `None`, then an option containing the passed in
     /// value will be returned.
     pub(crate) fn min<T: Ord + Copy>(a: Option<T>, b: T) -> Option<T> {
-        a.map_or(Some(b), |v| Some(std::cmp::min(v, b)))
+        Some(a.map_or(b, |v| std::cmp::min(v, b)))
     }
 
     #[cfg(test)]
