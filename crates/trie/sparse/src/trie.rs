b7cb06b88f7b2e9a2482d75233a447c1a058ce4c
Matthias Seitz <matthias.seitz@outlook.de>
2026-01-30 18:42:11 +0100

    update take updates

--- crates/trie/sparse/src/trie.rs
+++ crates/trie/sparse/src/trie.rs
@@ -979,7 +979,17 @@ impl SparseTrieTrait for SerialSparseTrie {
     }
 
     fn take_updates(&mut self) -> SparseTrieUpdates {
-        self.updates.take().unwrap_or_default()
+        match self.updates.take() {
+            Some(updates) => {
+                // NOTE: we need to preserve Some case
+                self.updates = Some(SparseTrieUpdates::with_capacity(
+                    updates.updated_nodes.len(),
+                    updates.removed_nodes.len(),
+                ));
+                updates
+            }
+            None => SparseTrieUpdates::default(),
+        }
     }
 
     fn wipe(&mut self) {
