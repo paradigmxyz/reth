55ac92d80cd6adb5db7cab045954d3b7f8c05f93
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2026-02-04 17:22:54 +0530

    chore: replaced cumulative gas used with gas spent

--- crates/ethereum/evm/src/receipt.rs
+++ crates/ethereum/evm/src/receipt.rs
@@ -14,13 +14,15 @@ impl ReceiptBuilder for RethReceiptBuilder {
     type Receipt = Receipt;
 
     fn build_receipt<E: Evm>(&self, ctx: ReceiptBuilderCtx<'_, TxType, E>) -> Self::Receipt {
-        let ReceiptBuilderCtx { tx_type, result, cumulative_gas_used, .. } = ctx;
+        let ReceiptBuilderCtx { tx_type, result, cumulative_gas_used, gas_spent, .. } = ctx;
+        tracing::debug!("cumulative gas used: {:?}", cumulative_gas_used);
+        tracing::debug!("gas spent: {:?}", gas_spent);
         Receipt {
             tx_type,
             // Success flag was added in `EIP-658: Embedding transaction status code in
             // receipts`.
             success: result.is_success(),
-            cumulative_gas_used,
+            cumulative_gas_used: gas_spent.unwrap_or_default(),
             logs: result.into_logs(),
         }
     }
