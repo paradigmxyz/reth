ec13650e5a895a49fc0e5a75589eaaa404c11f19
alex017 <dishes-18pole@icloud.com>
2026-02-04 15:12:25 +0100

    Update lib.rs

--- crates/net/dns/src/lib.rs
+++ crates/net/dns/src/lib.rs
@@ -165,7 +165,7 @@ impl<R: Resolver> DnsDiscoveryService<R> {
 
     /// Starts discovery with all configured bootstrap links
     pub fn bootstrap(&mut self) {
-        for link in self.bootstrap_dns_networks.clone() {
+        for link in self.bootstrap_dns_networks.iter().cloned() {
             self.sync_tree_with_link(link);
         }
     }
