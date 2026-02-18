080ff004e320cc387dd58fcd170a42add76f251a
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2026-02-18 23:06:22 +0530

    chore: fixed bal devnet error (#22325)
    Co-authored-by: Soubhik Singha Mahapatra <soubhiksmp2004@gmail.com>

--- crates/rpc/rpc-eth-types/src/simulate.rs
+++ crates/rpc/rpc-eth-types/src/simulate.rs
@@ -386,13 +386,13 @@ where
                     ..Default::default()
                 }
             }
-            ExecutionResult::Success { output, gas_used, logs, .. } =>
+            ExecutionResult::Success { output, gas, logs, .. } =>
             {
                 #[allow(clippy::needless_update)]
                 SimCallResult {
                     return_data: output.into_data(),
                     error: None,
-                    gas_used,
+                    gas_used: gas.used(),
                     logs: logs
                         .into_iter()
                         .map(|log| {
@@ -413,28 +413,6 @@ where
                     ..Default::default()
                 }
             }
-            ExecutionResult::Success { output, gas, logs, .. } => SimCallResult {
-                return_data: output.into_data(),
-                error: None,
-                gas_used: gas.used(),
-                logs: logs
-                    .into_iter()
-                    .map(|log| {
-                        log_index += 1;
-                        alloy_rpc_types_eth::Log {
-                            inner: log,
-                            log_index: Some(log_index - 1),
-                            transaction_index: Some(index as u64),
-                            transaction_hash: Some(*tx.tx_hash()),
-                            block_hash: Some(block.hash()),
-                            block_number: Some(block.header().number()),
-                            block_timestamp: Some(block.header().timestamp()),
-                            ..Default::default()
-                        }
-                    })
-                    .collect(),
-                status: true,
-            },
         };
 
         calls.push(call);
