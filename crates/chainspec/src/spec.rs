714508bc8a781bc63505ff626671a4af19a888fd
Soubhik Singha Mahapatra <soubhiksmp2004@gmail.com>
2026-02-24 23:48:23 +0530

    rm some logs

--- crates/chainspec/src/spec.rs
+++ crates/chainspec/src/spec.rs
@@ -44,7 +44,9 @@ use reth_primitives_traits::{sync::LazyLock, BlockHeader, SealedHeader};
 
 /// Helper method building a [`Header`] given [`Genesis`] and [`ChainHardforks`].
 pub fn make_genesis_header(genesis: &Genesis, hardforks: &ChainHardforks) -> Header {
-    tracing::info!("Making genesis header with genesis: {:?}", genesis);
+    tracing::info!("Making genesis header with genesis: {:?}", genesis.config);
+    tracing::info!("Making genesis header with genesis: {:?}", genesis.base_fee_per_gas);
+
     // If London is activated at genesis, we set the initial base fee as per EIP-1559.
     let base_fee_per_gas = hardforks
         .fork(EthereumHardfork::London)
