695a1cbae3b5940ee432283fd8cc355cde6120b8
Soubhik Singha Mahapatra <soubhiksmp2004@gmail.com>
2026-02-26 17:43:04 +0530

    docs

--- crates/net/p2p/src/bals/client.rs
+++ crates/net/p2p/src/bals/client.rs
@@ -1,24 +1,25 @@
-use std::{
-    ops::RangeInclusive,
-    pin::Pin,
-    task::{ready, Context, Poll},
-};
-
 use crate::{download::DownloadClient, error::PeerRequestResult, priority::Priority};
 use alloy_primitives::B256;
 use auto_impl::auto_impl;
-use futures::{Future, FutureExt};
+use futures::Future;
 use reth_eth_wire_types::BlockAccessLists;
-use reth_primitives_traits::BlockBody;
 
+/// A client capable of downloading block access lists.
 #[auto_impl(&, Arc, Box)]
 pub trait BlockAccessListsClient: DownloadClient {
-    type Output: Future<Output = PeerRequestResult<BlockAccessLists>> + Send + Sync + Unpin;
+    /// The bal type this client fetches.
+    type BlockAccessLists: Future<Output = PeerRequestResult<BlockAccessLists>>
+        + Send
+        + Sync
+        + Unpin;
 
-    fn get_block_access_lists(&self, hashes: Vec<B256>) -> Self::Output;
+    ///  Fetches the block access lists for given hashes.
+    fn get_block_access_lists(&self, hashes: Vec<B256>) -> Self::BlockAccessLists;
+
+    ///  Fetches the block access lists for given hashes with priority
     fn get_block_access_lists_with_priority(
         &self,
         hashes: Vec<B256>,
         priority: Priority,
-    ) -> Self::Output;
+    ) -> Self::BlockAccessLists;
 }
