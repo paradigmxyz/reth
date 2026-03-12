73c009ce0e149b3aafe495b5bf7e37fb03ab9c6a
joohhnnn <386201195@qq.com>
2026-03-12 14:34:02 +0800

    lint

--- crates/engine/tree/src/tree/precompile_cache.rs
+++ crates/engine/tree/src/tree/precompile_cache.rs
@@ -169,11 +169,11 @@ where
     }
 
     fn call(&self, input: PrecompileInput<'_>) -> PrecompileResult {
-        if let Some(entry) = &self.cache.get(input.data, self.spec_id.clone()) {
-            if input.gas >= entry.gas_used() {
-                self.increment_by_one_precompile_cache_hits();
-                return entry.to_precompile_result()
-            }
+        if let Some(entry) = &self.cache.get(input.data, self.spec_id.clone())
+            && input.gas >= entry.gas_used()
+        {
+            self.increment_by_one_precompile_cache_hits();
+            return entry.to_precompile_result()
         }
 
         let calldata = input.data;
