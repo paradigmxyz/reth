a973fe9518224e2eabc8a827c0ec570917f0abae
Matthias Seitz <matthias.seitz@outlook.de>
2026-05-07 15:20:09 +0200

    refactor(evm): simplify BAL hash assembly

--- crates/ethereum/evm/src/build.rs
+++ crates/ethereum/evm/src/build.rs
@@ -91,12 +91,6 @@ where
             };
         }
 
-        let bal_hash = if self.chain_spec.is_amsterdam_active_at_timestamp(timestamp) {
-            block_access_list_hash
-        } else {
-            None
-        };
-
         let header = Header {
             parent_hash: ctx.parent_hash,
             ommers_hash: EMPTY_OMMER_ROOT_HASH,
@@ -119,7 +113,7 @@ where
             blob_gas_used: block_blob_gas_used,
             excess_blob_gas,
             requests_hash,
-            block_access_list_hash: bal_hash,
+            block_access_list_hash,
             slot_number: ctx.slot_number,
         };
 
