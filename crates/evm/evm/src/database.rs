10396cd0f8b7255832da0f6facef167b982bd9ad
Oliver Nordbjerg <hi@notbjerg.me>
2026-06-04 18:34:35 +0200

    refactor(evm): remove parked revm database adapter

--- crates/evm/evm/src/database.rs
+++ crates/evm/evm/src/database.rs
@@ -6,9 +6,6 @@ use reth_primitives_traits::Account;
 use reth_storage_api::{AccountReader, BlockHashReader, BytecodeReader, StateProvider};
 use reth_storage_errors::provider::ProviderResult;
 
-#[cfg(any())]
-pub use revm::database::State;
-
 /// A helper trait responsible for providing state necessary for EVM execution.
 pub trait EvmStateProvider {
     /// Get basic account information.
@@ -90,52 +87,5 @@ impl<DB> Deref for StateProviderDatabase<DB> {
 impl<DB> DerefMut for StateProviderDatabase<DB> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
-    }
-}
-
-#[cfg(any())]
-mod revm_database {
-    use super::*;
-    use reth_storage_errors::provider::ProviderError;
-    use revm::{bytecode::Bytecode, state::AccountInfo, Database, DatabaseRef};
-
-    impl<DB: EvmStateProvider> Database for StateProviderDatabase<DB> {
-        type Error = ProviderError;
-
-        fn basic(&mut self, address: Address) -> Result<Option<AccountInfo>, Self::Error> {
-            self.basic_ref(address)
-        }
-
-        fn code_by_hash(&mut self, code_hash: B256) -> Result<Bytecode, Self::Error> {
-            self.code_by_hash_ref(code_hash)
-        }
-
-        fn storage(&mut self, address: Address, index: U256) -> Result<U256, Self::Error> {
-            self.storage_ref(address, index)
-        }
-
-        fn block_hash(&mut self, number: u64) -> Result<B256, Self::Error> {
-            self.block_hash_ref(number)
-        }
-    }
-
-    impl<DB: EvmStateProvider> DatabaseRef for StateProviderDatabase<DB> {
-        type Error = <Self as Database>::Error;
-
-        fn basic_ref(&self, address: Address) -> Result<Option<AccountInfo>, Self::Error> {
-            Ok(self.basic_account(&address)?.map(Into::into))
-        }
-
-        fn code_by_hash_ref(&self, code_hash: B256) -> Result<Bytecode, Self::Error> {
-            Ok(self.bytecode_by_hash(&code_hash)?.unwrap_or_default().0)
-        }
-
-        fn storage_ref(&self, address: Address, index: U256) -> Result<U256, Self::Error> {
-            Ok(self.0.storage(address, B256::new(index.to_be_bytes()))?.unwrap_or_default())
-        }
-
-        fn block_hash_ref(&self, number: u64) -> Result<B256, Self::Error> {
-            Ok(self.0.block_hash(number)?.unwrap_or_default())
-        }
     }
 }
