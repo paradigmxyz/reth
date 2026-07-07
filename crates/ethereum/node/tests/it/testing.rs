f1b2f6e507684701028fdb45b4ac30c6852a3d8c
Gustavo Figueiredo <me@figtracer.com>
2026-07-07 14:55:13 +0100

    test(ethereum): use unused ports for testing rpc node

--- crates/ethereum/node/tests/it/testing.rs
+++ crates/ethereum/node/tests/it/testing.rs
@@ -24,7 +24,8 @@ use tokio::sync::oneshot;
 #[tokio::test(flavor = "multi_thread")]
 async fn testing_rpc_build_block_works() -> eyre::Result<()> {
     let runtime = Runtime::test();
-    let mut rpc_args = reth_node_core::args::RpcServerArgs::default().with_http();
+    let mut rpc_args =
+        reth_node_core::args::RpcServerArgs::default().with_unused_ports().with_http();
     rpc_args.http_api = Some(RpcModuleSelection::from_iter([RethRpcModule::Testing]));
     let tempdir = tempdir().expect("temp datadir");
     let datadir_args = DatadirArgs {
@@ -35,6 +36,7 @@ async fn testing_rpc_build_block_works() -> eyre::Result<()> {
         pprof_dumps_path: Some(tempdir.path().join("pprof")),
     };
     let config = NodeConfig::test()
+        .with_unused_ports()
         .with_disabled_discovery()
         .with_datadir_args(datadir_args)
         .with_rpc(rpc_args);
@@ -98,7 +100,8 @@ async fn testing_rpc_build_block_works() -> eyre::Result<()> {
 #[tokio::test(flavor = "multi_thread")]
 async fn testing_rpc_commit_block_works() -> eyre::Result<()> {
     let runtime = Runtime::test();
-    let mut rpc_args = reth_node_core::args::RpcServerArgs::default().with_http();
+    let mut rpc_args =
+        reth_node_core::args::RpcServerArgs::default().with_unused_ports().with_http();
     rpc_args.http_api = Some(RpcModuleSelection::from_iter([
         RethRpcModule::Debug,
         RethRpcModule::Eth,
@@ -120,6 +123,7 @@ async fn testing_rpc_commit_block_works() -> eyre::Result<()> {
             .build(),
     );
     let config = NodeConfig::test()
+        .with_unused_ports()
         .with_disabled_discovery()
         .with_chain(chain_spec)
         .with_datadir_args(datadir_args)
